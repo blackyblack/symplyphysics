33d7a49dcedfd97d3d55df86f70b9ce2d20c9463
Andrey Lekar <andrey_lekar@adoriasoft.com>
2021-08-15 01:43:10 +0200

    Cleaner code

--- symplyphysics/acceleration_from_speed_difference/module.py
+++ symplyphysics/acceleration_from_speed_difference/module.py
@@ -1,18 +1,18 @@
-import sympy
+from sympy import symbols, Function, Eq, pretty, solve, dsolve
 
 # Description
 ## Acceleration definition: A = dv/dt
 
-acceleration_, time_ = sympy.symbols('acceleration time')
-velocity_function_ = sympy.Function('velocity')
-law = sympy.Eq(acceleration_, sympy.Derivative(velocity_function_(time_), time_))
+acceleration_, time_ = symbols('acceleration time')
+velocity_function_ = Function('velocity')
+law = Eq(acceleration_, velocity_function_(time_).diff(time_))
 
 def print():
-    return sympy.pretty(law, use_unicode=False)
+    return pretty(law, use_unicode=False)
 
 def calculate_linear_acceleration(velocity_start, velocity_end, time):
     # solve differential equation with initial conditions
-    dsolved = sympy.solvers.ode.dsolve(law, velocity_function_(time_), ics = {velocity_function_(0): velocity_start})
+    dsolved = dsolve(law, velocity_function_(time_), ics = {velocity_function_(0): velocity_start})
     # calculate acceleration for given velocity change over time
-    solved = sympy.solvers.solve(dsolved.subs(velocity_function_(time_), velocity_end).subs(time_, time))
+    solved = solve(dsolved.subs(velocity_function_(time_), velocity_end).subs(time_, time))
     return solved[0].evalf()
--- symplyphysics/force_from_acceleration/module.py
+++ symplyphysics/force_from_acceleration/module.py
@@ -1,13 +1,13 @@
-import sympy
+from sympy import symbols, Eq, pretty, solve
 
 # Description
 ## Newton's second law: F = m * a
 
-force_, mass_, acceleration_ = sympy.symbols('force mass acceleration')
-law = sympy.Eq(force_, mass_ * acceleration_)
+force_, mass_, acceleration_ = symbols('force mass acceleration')
+law = Eq(force_, mass_ * acceleration_)
 
 def print():
-    return sympy.pretty(law, use_unicode=False)
+    return pretty(law, use_unicode=False)
 
 def calculate_force(mass, acceleration):
-    return sympy.solvers.solve(law.subs(mass_, mass).subs(acceleration_, acceleration))[0].evalf()
+    return solve(law.subs(mass_, mass).subs(acceleration_, acceleration))[0].evalf()
