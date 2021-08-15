4d82c412c761ced75a6681014118066cf3f31c7a
Andrey Lekar <andrey_lekar@adoriasoft.com>
2021-08-15 14:13:05 +0200

    Allow nonlinear acceleration calculation

--- symplyphysics/acceleration_from_speed_difference/module.py
+++ symplyphysics/acceleration_from_speed_difference/module.py
@@ -1,18 +1,23 @@
 from sympy import symbols, Function, Eq, pretty, solve, dsolve
+from sympy.utilities.lambdify import lambdify, implemented_function
 
 # Description
 ## Acceleration definition: A = dv/dt
 
-acceleration_, time_ = symbols('acceleration time')
-velocity_function_ = Function('velocity')
-law = Eq(acceleration_, velocity_function_(time_).diff(time_))
+time_ = symbols('time')
+acceleration_, velocity_function_ = symbols('acceleration velocity', cls = Function)
+law = Eq(acceleration_(time_), velocity_function_(time_).diff(time_))
 
 def print():
     return pretty(law, use_unicode=False)
 
 def calculate_linear_acceleration(velocity_start, velocity_end, time):
-    # solve differential equation with initial conditions
-    dsolved = dsolve(law, velocity_function_(time_), ics = {velocity_function_(0): velocity_start})
-    # calculate acceleration for given velocity change over time
-    solved = solve(dsolved.subs(velocity_function_(time_), velocity_end).subs(time_, time))
-    return solved[0].evalf()
+    velocity_function = implemented_function('velocity_function', lambda x: x * (velocity_end - velocity_start) / time)
+    velocity_function_lambda = lambdify(time_, velocity_function(time_))
+    # solve differential equation with custom function
+    dsolved = dsolve(law.subs(velocity_function_(time_), velocity_function_lambda(time_)), acceleration_(time_))
+    # calculate acceleration at the given point of time
+    ## Note: since acceleration is constant, any time argument can be passed here.
+    ## For nonlinear functions 'time' should be passed for a correct answer.
+    solved = solve(dsolved.subs(time_, time))
+    return solved[0][acceleration_(time)].evalf()
