97e9a6d3ea7990aa6f0f46fd7df79b2f951a0294
Andrey Lekar <andrey_lekar@adoriasoft.com>
2021-08-21 01:37:24 +0200

    Sample termodynamics

---
+++ symplyphysics/constants/boltzmann.py
@@ -0,0 +1,5 @@
+# Boltzmann constant (also known as k)
+
+## Unit is Joule/Kelvin (J/K = m^2*kg/(s^2*K))
+
+value = 1.38064852e-23
---
+++ symplyphysics/laws/termodynamics/pressure_from_temperature_and_volume.py
@@ -0,0 +1,24 @@
+from sympy import symbols, Eq, pretty, solve
+from symplyphysics.constants.ideal_gas import value as ideal_gas_constant
+
+# Description
+## Ideal gas law: P * V = n * R * T
+## Where:
+## P is pressure,
+## V is volume,
+## n is number of moles,
+## R is ideal gas constant,
+## T is temperature
+
+pressure, volume, mole_count, temperature, R_constant = symbols('pressure volume mole_count temperature R_constant')
+law = Eq(pressure, mole_count * temperature * R_constant / volume)
+
+def print():
+    return pretty(law, use_unicode=False)
+
+def calculate_pressure(volume_, temperature_, mole_count_):
+    return solve(law
+      .subs(R_constant, ideal_gas_constant)
+      .subs(volume, volume_)
+      .subs(temperature, temperature_)
+      .subs(mole_count, mole_count_))[0].evalf()
