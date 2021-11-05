b1d5c0784624ed0e8337a1391db502199be54ab4
Andrey Lekar <andrey_lekar@adoriasoft.com>
2021-11-06 01:17:54 +0200

    Another infinite multiplication factor formula

---
+++ symplyphysics/laws/nuclear/infinite_multiplication_factor_from_macroscopic_fission_cross_section.py
@@ -0,0 +1,39 @@
+from sympy.core.singleton import S
+from symplyphysics import (
+    symbols, Eq, pretty, solve, Quantity, units,
+    validate_input, validate_output, expr_to_quantity, convert_to
+)
+
+# Description
+## Infinite multiplication factor: k_infinite = v * Σf / Σa
+## Where:
+## v - average number of neutrons produced per fission.
+## Σf - macroscopic fission cross-section of the fuel.
+##   See [macroscopic transport cross-section](./macroscopic_transport_cross_section.py) implementation.
+## Σa - macroscopic absorption cross-section of the fuel.
+## k_infinite - infinite multiplication factor.
+##   See [infinite multiplication factor](./infinite_multiplication_factor.py)
+
+neutrons_per_fission = symbols('neutrons_per_fission')
+macroscopic_fission_cross_section = symbols('macroscopic_fission_cross_section')
+macroscopic_absorption_cross_section = symbols('macroscopic_absorption_cross_section')
+infinite_multiplication_factor = symbols('infinite_multiplication_factor')
+
+law = Eq(infinite_multiplication_factor,
+    neutrons_per_fission * macroscopic_fission_cross_section / macroscopic_absorption_cross_section)
+
+def print():
+    return pretty(law, use_unicode=False)
+
+@validate_input(macroscopic_fission_cross_section_=(1 / units.length), macroscopic_absorption_cross_section_=(1 / units.length))
+def calculate_multiplication_factor(neutrons_per_fission_: float,
+    macroscopic_fission_cross_section_: Quantity,
+    macroscopic_absorption_cross_section_: Quantity) -> float:
+
+    result_factor_expr = solve(law, infinite_multiplication_factor, dict=True)[0][infinite_multiplication_factor]
+    result_expr = result_factor_expr.subs({
+        neutrons_per_fission: neutrons_per_fission_,
+        macroscopic_fission_cross_section: macroscopic_fission_cross_section_,
+        macroscopic_absorption_cross_section: macroscopic_absorption_cross_section_})
+    result_factor = expr_to_quantity(result_expr, 'inf_multiplication_factor')
+    return convert_to(result_factor, S.One).n()
---
+++ test/nuclear/infinite_multiplication_factor_from_macroscopic_fission_cross_section_test.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+
+from symplyphysics import (
+    units, SI, errors
+)
+
+from symplyphysics.laws.nuclear import infinite_multiplication_factor_from_macroscopic_fission_cross_section as multiplication_factor
+
+@fixture
+def test_args():
+    # Uranium-235 thermal neutrons per fission is 2.42
+    neutrons_per_fission = 2.42
+    macro_fission_cross_section = units.Quantity('macro_fission_cross_section')
+    SI.set_quantity_dimension(macro_fission_cross_section, 1 / units.length)
+    # Uranium-235 macroscopic fission cross-section
+    SI.set_quantity_scale_factor(macro_fission_cross_section, 2.811 / units.centimeter)
+    macro_abs_cross_section = units.Quantity('macro_abs_cross_section')
+    SI.set_quantity_dimension(macro_abs_cross_section, 1 / units.length)
+    # Uranium-235 + Uranium-238 macroscopic absorption cross-section
+    SI.set_quantity_scale_factor(macro_abs_cross_section, (3.352 + 0.117) / units.centimeter)
+
+    Args = namedtuple('Args', ['v', 'Sf', 'Sa'])
+    return Args(v = neutrons_per_fission, Sf = macro_fission_cross_section, Sa = macro_abs_cross_section)
+
+def test_basic_multiplication_factor(test_args):
+    result = multiplication_factor.calculate_multiplication_factor(test_args.v, test_args.Sf, test_args.Sa)
+    assert result == approx(1.96, 0.01)
+
+def test_bad_macroscopic_cross_section(test_args):
+    Sb = units.Quantity('Sb')
+    SI.set_quantity_dimension(Sb, units.length)
+    SI.set_quantity_scale_factor(Sb, 3 * units.meter)
+
+    with raises(errors.UnitsError):
+        multiplication_factor.calculate_multiplication_factor(test_args.v, Sb, test_args.Sa)
+
+    with raises(TypeError):
+        multiplication_factor.calculate_multiplication_factor(test_args.v, 100, test_args.Sa)
+
+    with raises(errors.UnitsError):
+        multiplication_factor.calculate_multiplication_factor(test_args.v, test_args.Sf, Sb)
+
+    with raises(TypeError):
+        multiplication_factor.calculate_multiplication_factor(test_args.v, test_args.Sf, 100)
