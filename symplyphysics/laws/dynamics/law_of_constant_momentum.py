16d6dfdcd791eb7a99fb7ba0143fcd4ed2f0ca10
paradoxx84 <paradoxx84@yandex.ru>
2022-06-24 08:06:40 +0500

    Update law_of_constant_momentum.py

--- symplyphysics/laws/dynamics/law_of_constant_momentum.py
+++ symplyphysics/laws/dynamics/law_of_constant_momentum.py
@@ -4,6 +4,11 @@ from symplyphysics import (
 )
 
 # Description
-## If there is no external force is applied to system of N objects, the summary momentum of this system remains constant during and after any interactions between objects
+## If there is no external force is applied to system of objects, the summary momentum of this system remains constant during and after any interactions between objects
 
+mass1, mass2, velocity1, velocity2 = symbols('mass1 mass2 velocity1 velocity2')
+law = Eq(mass2 * velocity2, mass1 * velocity1)
+
+def print():
+    return pretty(law, use_unicode=False)
 
