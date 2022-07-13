b877c60430cbfd8bb912aae12b72900e53a43a54
paradoxx84 <paradoxx84@yandex.ru>
2022-07-13 16:17:52 +0500

    Create electric_charge_is_constant_in_isolated_system_test.py

---
+++ test/electricity/electric_charge_is_constant_in_isolated_system_test.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+
+from symplyphysics import (
+    units, convert_to, SI, errors
+)
+from symplyphysics.laws.electricity import electric_charge_is_constant_in_isolated_system as charge_law
+
