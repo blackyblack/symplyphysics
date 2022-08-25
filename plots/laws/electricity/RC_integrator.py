b5ef14b929c3a6880270c27bccfdbe73d037882c
Andrew Lekar <andrey_lekar@adoriasoft.com>
2022-08-25 15:30:16 +0200

    Derivation of the charge function on the RC node

--- plots/laws/electricity/RC_integrator.py
+++ plots/laws/electricity/RC_integrator.py
@@ -9,12 +9,34 @@
 ## and externally bypassed to ground with 0.1uF capacitor. MCU starts after Reset pin reaches 2.5V.
 ## We'll try to estimate this period of time with lelp of diagrams.
 
-from sympy import solve, pretty, symbols
+from sympy import solve, dsolve, symbols, Function, Eq
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
 from symplyphysics.definitions import current_is_charge_derivative as charge_definition
 from symplyphysics.definitions import capacitance_from_charge_and_voltage as capacitance_definition
 from symplyphysics.laws.electricity import current_is_proportional_to_voltage as ohms_law
+
+time = symbols('time')
+voltage_initial, resistance = symbols('voltage_initial resistance')
+
+# derive charge function for the RC integrator node
+
+voltage_function = symbols('voltage_function', cls = Function)
+
+capacitance_eq = capacitance_definition.definition.subs({capacitance_definition.charge: charge_definition.charge_function(time), capacitance_definition.voltage: voltage_function(time)})
+charge_eq = charge_definition.definition.subs(charge_definition.time, time)
+
+# TODO: prove that ohms_law.voltage = voltage_initial - voltage_function(time)
+current_eq = ohms_law.law.subs({ohms_law.voltage: (voltage_initial - voltage_function(time)), ohms_law.resistance: resistance, ohms_law.current: charge_definition.current_function(time)})
+
+derived_law = [capacitance_eq, charge_eq, current_eq]
+solved_charge_function = solve(derived_law, (charge_definition.current_function(time), voltage_function(time), charge_definition.charge_function(time)), dict=True)[0][charge_definition.charge_function(time)]
+
+charge_diff_eq = Eq(charge_definition.charge_function(time), solved_charge_function)
+
+C1 = symbols('C1')
+charge_on_capacitor_eq = dsolve(charge_diff_eq).subs(C1, 1)
+charge_on_capacitor_function = solve(charge_on_capacitor_eq, charge_definition.charge_function(time), dict=True)[0][charge_definition.charge_function(time)]
 
 initial_voltage = 3.3
 example_capacitance = 0.0001
@@ -22,7 +44,7 @@ example_impedance = 48000
 
 tau = example_capacitance * example_impedance
 
-time = symbols('time')
+
 
 capacitor_voltage = capacitance_definition.calculate_voltage()
 
