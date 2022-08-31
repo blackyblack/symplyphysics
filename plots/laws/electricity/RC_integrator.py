d73ccb1783ceb88f945fa8de40c03094b64ce979
paradoxx84 <paradoxx84@yandex.ru>
2022-08-31 16:30:55 +0500

    doesnt work

--- plots/laws/electricity/RC_integrator.py
+++ plots/laws/electricity/RC_integrator.py
@@ -3,19 +3,21 @@
 # Description
 ## Time constant of RC Tau = R*C (seconds).
 ## If some voltage is applied to RC integrator, capacitor starts to charge and it's voltage rises. 
+## Capacitor voltage will never reach initial voltage.
 ## Voltage on the capacitor reaches 63% of initial voltage after 1*Tau seconds, and reaches 95% after 3*Tau seconds
 ## independently of initial voltage.
 ## For example, MCU is power-supplied with 3.3V. It's Reset pin is pulled up to power rail with internal resistor of 48kOhm
 ## and externally bypassed to ground with 0.1uF capacitor. MCU starts after Reset pin reaches 2.5V.
 ## We'll try to estimate this period of time with lelp of diagrams.
 
+from cmath import exp
 from sympy import solve, dsolve, symbols, Function, Eq
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
 from symplyphysics.definitions import current_is_charge_derivative as charge_definition
 from symplyphysics.definitions import capacitance_from_charge_and_voltage as capacitance_definition
 from symplyphysics.laws.electricity import current_is_proportional_to_voltage as ohms_law
-from symplyphysics.laws.electricity import sums_of_all_voltages_in_loop_is_zero as kirchhof_law_2
+# from symplyphysics.laws.electricity import sums_of_all_voltages_in_loop_is_zero as kirchhof_law_2
 
 time = symbols('time')
 voltage_initial, resistance = symbols('voltage_initial resistance')
@@ -45,36 +47,44 @@ example_impedance = 48000
 
 tau = example_capacitance * example_impedance
 
-capacitor_voltage = capacitance_definition.calculate_voltage()
+# capacitor_voltage = capacitance_definition.calculate_voltage()
 
 tau_lim1 = symbols('tau_lim1')
 tau_lim3 = symbols('tau_lim3')
 
 # U(t) shoud be U0(1 - e**(-t/RC))
-p1 = plot(
-    capacitor_voltage(time),
-    (time, 0, 10),
+# capacitor_voltage = initial_voltage * (1 - exp(-time/(example_capacitance * example_impedance)))
+
+voltage_function = symbols('voltage_function', cls = Function)
+definition = Eq(voltage_function, initial_voltage * (1 - exp(-time/(example_capacitance * example_impedance))))
+plot_func = solve(definition, voltage_function, dict=True)[0][voltage_function]
+
+UC = plot(
+    plot_func,
+    (time, 0, 10 * tau),
     line_color='blue',
     title='Capacitor voltage',    
     legend=True,
     backend=MatplotlibBackend,
     show=False)
 
-'''
+
 voltage063 = plot(
     0.63 * initial_voltage,
-    (time, 0, 10 * tau),
+    (time, 0, 8 * tau),
     line_color='red',    
+    suptitle='0.63 of U0',    
     backend=MatplotlibBackend,
     show=False)
-p1.append(voltage063[0])
+UC.append(voltage063[0])    
 
 voltage095 = plot(
     0.95 * initial_voltage,
-    (time, 0, 10 * tau),
+    (time, 0, 8 * tau),
     line_color='red',    
+    suptitle='0.95 of U0',
     backend=MatplotlibBackend,
     show=False)
-p1.append(voltage095[0])
-'''
-p1.show()
+UC.append(voltage095[0])
+
+UC.show()
