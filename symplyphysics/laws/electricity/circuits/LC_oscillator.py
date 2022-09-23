3c8c4fd293573f2184760ad452c534ca5f9bc5d3
Andrey Lekar <andrey_lekar@adoriasoft.com>
2022-09-23 15:27:27 +0200

    Prove LC oscillator. WIP

--- symplyphysics/laws/electricity/circuits/LC_oscillator.py
+++ symplyphysics/laws/electricity/circuits/LC_oscillator.py
@@ -1,8 +1,14 @@
-from sympy import sqrt, pi
+from sympy import sqrt, pi, Derivative
 from symplyphysics import (
-    symbols, Eq, pretty, solve, Quantity, units,
+    symbols, Eq, pretty, solve, Quantity, units, Function,
     validate_input, validate_output, expr_to_quantity
 )
+
+from symplyphysics.laws.electricity.circuits import sum_of_all_currents_through_an_electrical_node_is_zero as kirchhoff_law
+from symplyphysics.laws.electricity.circuits import sum_of_all_voltages_in_loop_is_zero as kirchhoff_law_2
+from symplyphysics.definitions import capacitance_from_charge_and_voltage as capacitance_definition
+from symplyphysics.definitions import self_induction_voltage_is_current_derivative as induction_voltage_definition
+from symplyphysics.definitions import current_is_charge_derivative as charge_definition
 
 # Description
 ## LC-oscillator is the circuit of inductor and capacitor.
@@ -22,20 +28,70 @@ law = Eq(oscillation_period, 2 * pi * sqrt(inductance * capacitance))
 def print():
     return pretty(law, use_unicode=False)
 
-## Let's assume we initially have capacitor charged to U0 voltage. In the zero time we connect this capacitor to inductor.
+## Derive the same law from the capacitor, charge and self-induction voltage laws
+
+## Let's assume we initially have capacitor charged to U0 voltage. In the zero time we connect this capacitor to inductor in a closed loop.
 ## So voltage on capacitor is always equals to voltage on inductor and the current through capacitor equals to current through inductor.
 
-## Current through capacitor is charge derivative, charge of capacitor is voltage of capacitor * capacitance
+## 1. Prove that capacitor_current_function(time) = inductor_current_function(time)
+
+time = symbols('time')
+capacitor_current_function, inductor_current_function = symbols('capacitor_current_function inductor_current_function', cls = Function)
+
+two_currents_law = kirchhoff_law.law.subs(kirchhoff_law.currents_total, 2).doit()
+# capacitor current is in, inductor current is out
+two_currents_applied = two_currents_law.subs({
+    kirchhoff_law.current[1]: capacitor_current_function(time),
+    kirchhoff_law.current[2]: -1 * inductor_current_function(time)})
+capacitor_current_applied = solve(two_currents_applied, capacitor_current_function(time), dict=True)[0][capacitor_current_function(time)]
+capacitor_current_eq = Eq(capacitor_current_function(time), capacitor_current_applied)
+
+assert capacitor_current_eq.lhs == capacitor_current_function(time)
+assert capacitor_current_eq.rhs == inductor_current_function(time)
+
+## 2. Prove that capacitor_voltage_function(time) = inductor_voltage_function(time)
+
+capacitor_voltage_function = symbols('capacitor_voltage_function', cls = Function)
+inductor_voltage_function = symbols('inductor_voltage_function', cls = Function)
+
+two_voltages_law = kirchhoff_law_2.law.subs(kirchhoff_law_2.voltages_total, 2).doit()
+# capacitor is voltage source, inductor is voltage consumer
+two_voltages_applied = two_voltages_law.subs({
+    kirchhoff_law_2.voltage[1]: -1 * inductor_voltage_function(time),
+    kirchhoff_law_2.voltage[2]: capacitor_voltage_function(time)})
+inductor_voltage_applied = solve(two_voltages_applied, inductor_voltage_function(time), dict=True)[0][inductor_voltage_function(time)]
+
+assert inductor_voltage_applied == capacitor_voltage_function(time)
+
+## 3. Prove that capacitor current derivative equals to capacitance * (second order derivative of voltage of capacitor)
+
+## charge of capacitor is voltage of capacitor * capacitance
+capacitor_charge_law = capacitance_definition.definition.subs(
+    {capacitance_definition.charge: charge_definition.charge_function(time),
+    capacitance_definition.voltage: capacitor_voltage_function(time)})
+capacitor_charge_applied = solve(capacitor_charge_law, charge_definition.charge_function(time), dict=True)[0][charge_definition.charge_function(time)]
+
 ## I_c(t) = C * U_c'(t)
-## Derive rsh and lhs:
+capacitor_current_law = charge_definition.definition.subs({charge_definition.time: time, charge_definition.charge_function(time): capacitor_charge_applied})
 ## I_c'(t) = C * U_c"(t)
+capacitor_current_law_derivative = Eq(Derivative(capacitor_current_law.lhs, time), Derivative(capacitor_current_law.rhs, time))
+
+## 3. Prove that inductor voltage equals to -1 * capacitance * inductance * (second order derivative of voltage of capacitor)
 
 ## Inductor voltage is the self-inductance.
-## U_i(t) = - L * I_i'(t) => I_i'(t) = U_i(t) / L
+inductor_voltage_law = induction_voltage_definition.definition.subs(
+    {induction_voltage_definition.time: time,
+    induction_voltage_definition.self_induction_voltage_function(time): capacitor_voltage_function(time),
+    induction_voltage_definition.current_function(time): charge_definition.current_function(time)})
 
-## U-C = U_l = U, I_c = I_l = I
+derived_law = [inductor_voltage_law, capacitor_current_law_derivative]
 
-## C * U"(t) = -L U(t). U"(t) = - 1/LC * U(t)
+## U"(t) = - 1/LC * U(t)
+capacitor_voltage_solved = solve(derived_law, (Derivative(charge_definition.current_function(time)), capacitor_voltage_function(time)), dict=True)[0][capacitor_voltage_function(time)]
+voltage_diff_eq = Eq(capacitor_voltage_function(time), capacitor_voltage_solved)
+
+## 4. Solve differential equation and find period of the harmonic oscillator
+
 ## U(t) = U0 * cos(wt), where w = 1/sqrt(LC) and the period T is 2*pi/w = 2 * pi * sqrt(LC)
 
 @validate_input(inductance_=units.inductance, capacitance_=units.capacitance)
