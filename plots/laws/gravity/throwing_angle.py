9faa8641ea43895f71ae4ec3dc3758de0c2b0cca
paradoxx84 <paradoxx84@yandex.ru>
2022-10-25 12:52:05 +0500

    example done

--- plots/laws/gravity/throwing_angle.py
+++ plots/laws/gravity/throwing_angle.py
@@ -7,6 +7,7 @@
 ## Vertical speed is accelerated by the gravitational acceleration.
 ## Horizontal speed is constant.
 
+from math import pi
 from sympy import sin, cos
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
@@ -30,18 +31,18 @@ g = 9.806
 end_of_flight = Eq(0, V_vertical * time - g * time**2 /2)
 flight_time = solve(end_of_flight, time, dict=True)[0][time]
 
-print(flight_time) #should be 2 solvations, we need one is not zero
+print(flight_time) #should be 2 solvations, we need one which is not zero
 ##kostil
 flight_time = 2 * V_vertical / g
 
 Length = symbols('Length', cls = Function)
-Length = V_horizontal * flight_time(alpha)
+Length = (V_horizontal * flight_time).subs({V0: 1})
 
 print(Length)
 
 Len = plot(
-    Length(alpha),
-    (alpha, 0, 90),
+    Length,
+    (alpha, 0, pi / 2),
     line_color='blue',    
     title='Length',    
     label = 'Length',    
@@ -49,79 +50,13 @@ Len = plot(
     annotations = {},
     backend=MatplotlibBackend,    
     show=False)
-Len.show()
-
-'''
-applied_law = rc_node.law.subs({rc_node.time: time, rc_node.resistance: example_impedance, rc_node.capacitance: example_capacitance, rc_node.initial_voltage: initial_voltage})
-capacitor_voltage_function = solve(applied_law, rc_node.capacitor_voltage_function(time), dict=True)[0][rc_node.capacitor_voltage_function(time)]
-
-capacitor_current_function = (initial_voltage - capacitor_voltage_function) / example_impedance
-
-UC = plot(
-    capacitor_voltage_function,
-    (time, 0, 8 * rc_time_constant),
-    line_color='blue',    
-    title='Capacitor voltage',    
-    label = 'Capacitor voltage',    
-    legend=True,    
-    annotations = {},
-    backend=MatplotlibBackend,    
-    show=False)        
-
-IC = plot(
-    capacitor_current_function,
-    (time, 0, 8 * rc_time_constant),
-    line_color='orange',    
-    label='Capacitor current',    
-    legend=True,    
-    backend=MatplotlibBackend,
-    show=False)
-UC.append(IC[0])    
-
-voltage063 = plot(
-    0.63 * initial_voltage,
-    (time, 0, rc_time_constant),
-    line_color='yellow',        
-    label='U = 0.63 of U0',    
-    backend=MatplotlibBackend,
-    show=False)
-UC.append(voltage063[0])    
 
 tau_line = plot(
-    1000 * (time - rc_time_constant) * 0.63 * initial_voltage,
-    (time, rc_time_constant, rc_time_constant + 0.001),
-    label = 'time = Tau',
+    100 * (time - pi / 4),
+    (time, pi / 4, pi / 4 + 0.001),
+    label = 'angle = pi / 4',
     line_color='yellow',
     show=False,
-    )
-UC.append(tau_line[0])
-
-voltage095 = plot(
-    0.95 * initial_voltage,
-    (time, 0, 3 * rc_time_constant),
-    line_color='green',     
-    label='U = 0.95 of U0',
-    backend=MatplotlibBackend,
-    show=False)
-UC.append(voltage095[0])
-
-tau3_line = plot(
-    1000 * (time - 3 * rc_time_constant) * 0.95 * initial_voltage,
-    (time, 3 * rc_time_constant, 3 * rc_time_constant + 0.001),
-    label = 'time = 3 * Tau',
-    line_color='green',
-    show=False,
-    )
-UC.append(tau3_line[0])
-
-voltageFull = plot(
-    initial_voltage,
-    (time, 0, 8 * rc_time_constant),
-    line_color='red',      
-    label='U0',
-    backend=MatplotlibBackend,
-    show=False)
-UC.append(voltageFull[0])
-
-UC.show()
-'''
+)
+Len.append(tau_line[0])
+Len.show()
