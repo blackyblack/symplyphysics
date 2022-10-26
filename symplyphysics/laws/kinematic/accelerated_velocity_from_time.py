f352f021d30e9a2d6c7c7db747e5f7ed1b998db1
paradoxx84 <paradoxx84@yandex.ru>
2022-10-26 08:34:41 +0500

    some low_level kinematical laws

--- symplyphysics/laws/kinematic/accelerated_velocity_from_time.py
+++ symplyphysics/laws/kinematic/accelerated_velocity_from_time.py
@@ -2,27 +2,25 @@ from symplyphysics import (
     symbols, Eq, pretty, solve, Quantity, units,
     validate_input, validate_output, expr_to_quantity
 )
-from sympy.physics.units import gravitational_constant
 
 # Description
-## Field strength of gravity field E = G * M / R^2, where
-## G is the gravitational constant
-## M is the mass of field generator
-## R is the distance to the generator's mass center
-## Gravity force applied to object F = E * m, where
-## E is the gravity field strength
-## m is mass of the object.
-## So gravity field strength in any point is gravity acceleration in this point.
+## Accelerated velocity is time dependent and increases with time if acceleration is co-directed with velocity and decreases if they are counter-directed.
+## Initial velocity is velocity at the start of observation, when time is 0.
 
-gravity_force, generator_mass, object_mass, distance_between_mass_centers  = symbols('gravity_force generator_mass object_mass distance')
-law = Eq(gravity_force, gravitational_constant * generator_mass * object_mass / (distance_between_mass_centers ** 2))
+velocity = symbols('velocity')
+time = symbols('time')
+acceleration = symbols('acceleration')
+initial_velocity = symbols('initial_velocity')
+
+law = Eq(velocity, initial_velocity + acceleration * time)
 
 def print():
     return pretty(law, use_unicode=False)
-
+'''
 @validate_input(generator_mass_=units.mass, object_mass_=units.mass, distance_between_mass_centers_=units.length)
 @validate_output(units.force)
 def calculate_force(generator_mass_: Quantity, object_mass_: Quantity, distance_between_mass_centers_: Quantity) -> Quantity:
     result_force_expr = solve(law, gravity_force, dict=True)[0][gravity_force]
     result_expr = result_force_expr.subs({generator_mass: generator_mass_, object_mass: object_mass_, distance_between_mass_centers: distance_between_mass_centers_})
     return expr_to_quantity(result_expr, 'gravity_force')
+'''
