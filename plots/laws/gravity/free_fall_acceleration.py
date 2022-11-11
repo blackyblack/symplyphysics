a33023f5c194c05244d2eeb81fb9d72d00ea3f5b
Oleg <oleh.zanin@gmail.com>
2022-11-11 20:29:41 +0100

    11.11.22

--- plots/laws/gravity/free_fall_acceleration.py
+++ plots/laws/gravity/free_fall_acceleration.py
@@ -5,20 +5,17 @@ from symplyphysics import (
     units, convert_to
 )
 from symplyphysics.laws.gravity import free_fall_acceleration_from_height as acceleration
-height = symbols('height')
 print("Formula is:\n{}".format(acceleration.print()))
 gravity_constant=convert_to(acceleration.gravity_constant, units.newton * units.meter**2 / units.kilogram**2).subs(units.newton * units.meter**2 / units.kilogram**2, 1).evalf(5)
 solved = solve(acceleration.law, acceleration.acceleration_free_fall, dict=True)[0][acceleration.acceleration_free_fall]
-result_acceleration = solved.subs({
-             acceleration.height: height,
-             acceleration.gravity_constant: gravity_constant,
-             acceleration.earth_mass: 5.976e+24,
-             acceleration.earth_radius: 6.371e+6})
+result_acceleration = solved.subs({acceleration.gravity_constant: gravity_constant,
+                                   acceleration.earth_mass: 5.976e+24,
+                                   acceleration.earth_radius: 6.371e+6})
 print("\nFree fall accelleration function on Earth surface  is:\n{}".format(
     pretty(result_acceleration, use_unicode=False)))
 p1 = plot(
     result_acceleration,
-    (height, 0, 10000),
+    (acceleration.height, 0, 10000),
     ylim=(9.74, 9.85),
     axis_center=(0.0, 9.75),
     line_color='red',
