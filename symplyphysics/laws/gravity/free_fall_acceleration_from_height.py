2a4d57f5770ee9513102142ea5bd2cf8c8bc5a27
Oleg <oleh.zanin@gmail.com>
2022-11-11 10:52:51 +0100

    changed 11.11.2022

--- symplyphysics/laws/gravity/free_fall_acceleration_from_height.py
+++ symplyphysics/laws/gravity/free_fall_acceleration_from_height.py
@@ -1,9 +1,7 @@
-from sympy.physics.units import gravitational_constant
 from symplyphysics import (
     symbols, Eq, pretty, solve, Quantity, units,
     validate_input, validate_output, expr_to_quantity
 )
-gravity_constant = gravitational_constant
 # Description
 # Accelerate free fall law: g(h)=(G*M)/(R+h)**2
 # where :
@@ -11,14 +9,12 @@ gravity_constant = gravitational_constant
 # M - Earth mass constant         5.976e+24 kg
 # R - Earth radius constant       6.371e+6 m
 # h - height
-
-acceleration_free_fall, gravity_constant, earth_mass, earth_radius, height = symbols('acceleration_free_fall gravity_constant earth_mass earth_radius height')
+gravity_constant=units.gravitational_constant
+acceleration_free_fall, earth_mass, earth_radius, height = symbols('acceleration_free_fall earth_mass earth_radius height')
 law = Eq(acceleration_free_fall, gravity_constant * earth_mass / (earth_radius + height)**2)
-
 def print():
     return pretty(law, use_unicode=False)
-
-@validate_input(height_=units.length,earth_mass_=units.mass,earth_radius_=units.length,gravity_constant_=units.force*units.length**2/units.mass**2)
+@validate_input(height_=units.length,earth_mass_= units.mass,earth_radius_= units.length)
 @validate_output(units.acceleration)
 def calculate_acceleration(height_: Quantity,earth_mass_:Quantity,earth_radius_:Quantity,gravity_constant_:Quantity) -> Quantity:
     result_accel_expr = solve(law, acceleration_free_fall, dict=True)[0][acceleration_free_fall]
