e1c3b4228d523911b56945e2c7bb1f01ae075292
Oleg <oleh.zanin@gmail.com>
2022-11-24 11:57:49 +0100

    Joule - Lenz law fix tests errors

--- symplyphysics/definitions/amount_energy_from_mass_and_temperature.py
+++ symplyphysics/definitions/amount_energy_from_mass_and_temperature.py
@@ -19,11 +19,12 @@ def print():
     return pretty(law, use_unicode=False)
 
 @validate_input(specific_heat_capacity_=units.energy / (units.mass * units.temperature), body_mass_=units.mass,
-    final_temperature_=units.temperature, initial_temperature_=units.temperature)
+    final_temperature_=units.temperature, initial_temperature_=units.temperature
+)
 @validate_output(units.energy)
 def calculate_amount_energy(specific_heat_capacity_: Quantity, body_mass_: Quantity, final_temperature_: Quantity,
     initial_temperature_: Quantity) -> Quantity:
-    result_amount_energy_expr = solve(law, amount_energy_for_body_heat, dict=True)[0][amount_energy_for_body_heat]
+    result_amount_energy_expr = solve(law, amount_energy, dict=True)[0][amount_energy]
     result_expr = result_amount_energy_expr.subs({specific_heat_capacity: specific_heat_capacity_,
                 body_mass: body_mass_, final_temperature: final_temperature_, initial_temperature: initial_temperature_})
     return expr_to_quantity(result_expr, 'amount_energy')
