24a82c7f36ed922f0adaa571c9a7f4f65aa6593d
Oleg <oleh.zanin@gmail.com>
2022-11-25 19:46:38 +0100

    review example

--- examples/electricity/amount_energy_from_voltage_time_resistance.py
+++ examples/electricity/amount_energy_from_voltage_time_resistance.py
@@ -18,9 +18,9 @@ from symplyphysics.definitions import power_is_proportional_voltage_and_current 
 density_example = units.Quantity('density_example')
 SI.set_quantity_dimension(density_example, units.mass / units.volume)
 SI.set_quantity_scale_factor(density_example, 1000 * units.kilogram / units.meter**3)
-P_example = units.Quantity('P_example')
-SI.set_quantity_dimension(P_example, units.power)
-SI.set_quantity_scale_factor(P_example, 1500 * units.watt)
+power_example = units.Quantity('power_example')
+SI.set_quantity_dimension(power_example, units.power)
+SI.set_quantity_scale_factor(power_example, 1500 * units.watt)
 v_example = units.Quantity('v_example')
 SI.set_quantity_dimension(v_example, units.volume)
 SI.set_quantity_scale_factor(v_example, 0.5 * units.liter)
@@ -35,7 +35,6 @@ SI.set_quantity_dimension(t2_example, units.temperature)
 SI.set_quantity_scale_factor(t2_example, (35 + 273) * units.kelvin)
 Celsius = symbols('Celsius')
 body_mass = solve(density.definition, density.mass, dict=True)[0][density.mass]
-resistance = solve(ohm_law.law, ohm_law.resistance, dict=True)[0][ohm_law.resistance]
 current_1 = solve(ohm_law.law, ohm_law.current, dict=True)[0][ohm_law.current]
 current_2 = solve(operate_power.law,operate_power.current, dict=True)[0][operate_power.current]
 current = Eq(current_1, current_2)
@@ -45,7 +44,7 @@ law = Eq(operate_energy.law.subs({operate_energy.body_mass: body_mass}),
 solved = solve(law, joule_lenz_law.time, dict=True)[0][joule_lenz_law.time]
 time = Eq(joule_lenz_law.time, solved)
 print("\nFormula is:\n\n {}".format(pretty(time, use_unicode=False)))
-law = law.subs({operate_power.power: P_example, operate_energy.body_mass: body_mass,
+law = law.subs({operate_power.power: power_example, operate_energy.body_mass: body_mass,
     operate_energy.specific_heat_capacity:C_example,
     operate_energy.temperature_begin: t1_example, operate_energy.temperature_end: t2_example,
     density.density: density_example, density.volume: v_example
@@ -53,7 +52,7 @@ law = law.subs({operate_power.power: P_example, operate_energy.body_mass: body_m
 solved_example = solve(law, joule_lenz_law.time, dict=True)[0][joule_lenz_law.time]
 print("\n For Power = {} {}, water volume = {} {}, temperature_begin = {} {}, temperature_end = {} {}: cooling time = {} {}\n"
     .format(
-        convert_to(P_example, units.watt).subs(units.watt, 1).evalf(5),
+        convert_to(power_example, units.watt).subs(units.watt, 1).evalf(5),
         units.watt,
         convert_to(v_example, units.liter).subs(units.liter, 1).evalf(3),
         units.liter,
