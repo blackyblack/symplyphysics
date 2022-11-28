1ddf5b0d4ec7aec89bde6f6d9b05ed11edff5e6c
paradoxx84 <paradoxx84@yandex.ru>
2022-11-28 08:58:32 +0500

    throwing angle finalized

--- plots/laws/gravity/throwing_angle.py
+++ plots/laws/gravity/throwing_angle.py
@@ -6,7 +6,7 @@
 from math import pi
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
-from symplyphysics import symbols, Eq, solve, Function
+from symplyphysics import symbols, Eq, solve, Function, Derivative, simplify, dsolve, sin
 from symplyphysics.laws.kinematic import constant_acceleration_movement_is_parabolic as movement_law
 from symplyphysics.laws.kinematic import planar_projection_is_cosine as projector
 
@@ -24,15 +24,21 @@ gravitational_acceleration = symbols('gravitational_acceleration')
 ## The horizontal initial speed is projection of throwing_velocity to X, acceleration is 0.
 ## Flight ends when y == 0 again.
 
+initial_horizontal_velocity = solve(projector.law, projector.projection, dict=True)[0][projector.projection].subs(
+    {
+        projector.vector_length: throwing_velocity, projector.vector_angle: throwing_angle
+    })
+
+print('Initial horizontal velocity:')
+print(initial_horizontal_velocity)    
+
 initial_vertical_velocity = solve(projector.law, projector.projection, dict=True)[0][projector.projection].subs(
     {
         projector.vector_length: throwing_velocity, projector.vector_angle: pi / 2 - throwing_angle
     })
 
-initial_horizontal_velocity = solve(projector.law, projector.projection, dict=True)[0][projector.projection].subs(
-    {
-        projector.vector_length: throwing_velocity, projector.vector_angle: throwing_angle
-    })
+print('Initial vertical velocity:')
+print(initial_vertical_velocity)    
 
 x_function = symbols('x_function', cls=Function)
 x_function = movement_law.law.rhs.subs(
@@ -42,27 +48,52 @@ x_function = movement_law.law.rhs.subs(
         movement_law.constant_acceleration: 0
     })
 
+print('x(t)')
+print(x_function)
+
 y_function = symbols('y_function', cls=Function)
 y_function = movement_law.law.rhs.subs(
     {         
         movement_law.movement_time: time_argument, 
-        movement_law.constant_acceleration: -gravitational_acceleration        
+        movement_law.constant_acceleration: -gravitational_acceleration,
+        movement_law.initial_velocity: initial_vertical_velocity        
     })
 
-end_of_flight = Eq(0, y_function)
+print('y(t)')    
+print(y_function)
 
+end_of_flight = Eq(0, y_function)
 # there are 2 points of the trajectory with h = 0, the first one is the start point, and the second one is at destination
 # We need the second one
 flight_time = solve(end_of_flight, time_argument, dict=True)[1][time_argument]
+print('Flight time:')
+print(flight_time)
 
 flight_distance = x_function.subs({time_argument: flight_time})
+print('Flight distance')
 print(flight_distance)
 
-# Let's find needed angle analytically.
+flight_distance_plotted = symbols('flight_distance_plotted', cls=Function)
+flight_distance_plotted = flight_distance.subs(
+    {
+        throwing_velocity: 1,
+        gravitational_acceleration: 0.5    
+    })
+flight_distance_plotted = simplify(flight_distance_plotted)
+print('Plotted flight distance')
+print(flight_distance_plotted)
 
-'''
+# Let's find needed angle analytically.
+# We are to find maximum of flight_distance(throwing_angle) function with time_argument in (0, pi/2) interval.
+flight_distance_plotted = sin(2 * throwing_angle)
+distance_derivative = Derivative(flight_distance_plotted, throwing_angle).doit()
+max_law = Eq(0, distance_derivative)
+max_angle = solve(max_law, throwing_angle, dict=True)[0][throwing_angle]
+print('max_angle')
+print(max_angle)
+
 Len = plot(
-    flight_distance,
+    flight_distance_plotted,
     (throwing_angle, 0, pi / 2),
     line_color='blue',    
     title='Length',    
@@ -72,13 +103,12 @@ Len = plot(
     backend=MatplotlibBackend,    
     show=False)
 
-tau_line = plot(
-    1000 * (time_argument - pi / 4),
-    (time_argument, pi / 4, pi / 4 + 0.001),
-    label = 'angle = pi / 4',
+peak_line = plot(
+    1000 * (time_argument - max_angle),
+    (time_argument, pi / 4, max_angle + 0.001),
+    label = 'angle = 45°',
     line_color='yellow',
     show=False,
 )
-Len.append(tau_line[0])
+Len.append(peak_line[0])
 Len.show()
-'''
