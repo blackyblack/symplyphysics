40788380885349aa749ce870d8172da6b53a24b9
paradoxx84 <paradoxx84@yandex.ru>
2022-12-05 09:47:11 +0500

    inelastic collision reworked

--- examples/dynamics/absolutely_inelastic_collision.py
+++ examples/dynamics/absolutely_inelastic_collision.py
@@ -5,41 +5,49 @@
 # Let the first object be the bullet with mass of 10gram and velocity of 700m/s hits the second object - steady 75kg piece of plasticine, stucks in it and makes this plasticine move with some unknown velocity.
 
 from symplyphysics import (
-    units, convert_to, SI
+    units, convert_to, expr_to_quantity, SI, solve
 )
 from symplyphysics.laws.dynamics import  momentum_after_collision_equals_to_momentum_before as momentum_law
+from symplyphysics.definitions import momentum_is_mass_times_velocity as momentum_definition
 
-m1 = units.Quantity('m1')
-v1 = units.Quantity('v1')
-m2 = units.Quantity('m2')
-v2 = units.Quantity('v2')
-m = units.Quantity('m3')
+bullet_mass = units.Quantity("bullet_mass")
+bullet_velocity = units.Quantity("bullet_velocity")
+body_mass = units.Quantity("body_mass")
+body_velocity = units.Quantity("body_velocity")
 
-SI.set_quantity_dimension(m1, units.mass)
-SI.set_quantity_dimension(m2, units.mass)
-SI.set_quantity_dimension(m, units.mass)
+SI.set_quantity_dimension(bullet_mass, units.mass)
+SI.set_quantity_dimension(body_mass, units.mass)
 
-SI.set_quantity_dimension(v1, units.velocity)
-SI.set_quantity_dimension(v2, units.velocity)
+SI.set_quantity_dimension(bullet_velocity, units.velocity)
+SI.set_quantity_dimension(body_velocity, units.velocity)
 
-SI.set_quantity_scale_factor(m1, 10 * units.gram)
-SI.set_quantity_scale_factor(m2, 75 * units.kilogram)
-SI.set_quantity_scale_factor(m, m1 + m2 * units.kilogram)
+SI.set_quantity_scale_factor(bullet_velocity, 10 * units.gram)
+SI.set_quantity_scale_factor(body_mass, 75 * units.kilogram)
 
-SI.set_quantity_scale_factor(v1, 700 * units.meter / units.second)
-SI.set_quantity_scale_factor(v2, 0 * units.meter / units.second)
+SI.set_quantity_scale_factor(bullet_velocity, 700 * units.meter / units.second)
+SI.set_quantity_scale_factor(body_velocity, 0 * units.meter / units.second)
 
-print("Formula is:\n{}".format(momentum_law.print()))
-momentum_of_system = momentum_law.calculate_momentum_after(m1, v1, m2, v2)
-result = momentum_law.calculate_velocity(momentum_of_system, m)
+bullet_momentum_before_collision = momentum_definition.definition.rhs.subs({momentum_definition.mass: bullet_mass, momentum_definition.velocity: bullet_velocity})
+body_momentum_before_collision = momentum_definition.definition.rhs.subs({momentum_definition.mass: body_mass, momentum_definition.velocity: body_velocity})
+momentum_of_system_before_collision = bullet_momentum_before_collision + body_momentum_before_collision
 
-print("Velocity = {} {}; for two inelasticly collided objects with masses = {} {}, velocities = {} {}"
+momentum_after_collision = momentum_law.law.rhs.subs({momentum_law.momentum_before: momentum_of_system_before_collision})
+mass_after_collision = body_mass + bullet_mass
+velocity_after_collision = solve(momentum_definition.definition, momentum_definition.velocity, dict = True)[0][momentum_definition.velocity] .subs({
+    momentum_definition.momentum: momentum_after_collision, 
+    momentum_definition.mass: mass_after_collision})
+velocity_quantity = expr_to_quantity(velocity_after_collision, "velocity_after_collision")
+
+print("Velocity = {} {}; for two inelasticly collided objects with masses = {} {}, {} {}, velocities = {} {}, "
    .format(
-        convert_to(result, units.velocity).subs(units.velocity, 1).evalf(2),
-        units.velocity,
-        convert_to(m1, units.kilogram).subs(units.kilogram, 1).evalf(2),
+        convert_to(velocity_quantity, units.meter/units.second).subs({units.meter: 1, units.seconds: 1}).evalf(2),
+        units.meter / units.second,
+        convert_to(bullet_mass, units.kilogram).subs(units.kilogram, 1).evalf(2),
         units.kilogram,
-        convert_to(m2, units.kilogram).subs(units.kilogram, 1).evalf(2),
+        convert_to(body_mass, units.kilogram).subs(units.kilogram, 1).evalf(2),
         units.kilogram,
-        convert_to(v1, units.meter / units.second).subs({units.meter: 1, units.second: 1}).evalf(2),
-        units.meter / units.second))
+        convert_to(bullet_velocity, units.meter / units.second).subs({units.meter: 1, units.second: 1}).evalf(2),
+        units.meter / units.second),
+        convert_to(body_velocity, units.meter / units.second).subs({units.meter: 1, units.second: 1}).evalf(2),
+        units.meter / units.second)
+        
--- symplyphysics/definitions/momentum_is_mass_times_velocity.py
+++ symplyphysics/definitions/momentum_is_mass_times_velocity.py
@@ -11,7 +11,7 @@ from symplyphysics import (
 ## V is it's velocity
 ## P is momentum
 
-momentum, mass, velocity = symbols('momentum mass velocity')
+momentum, mass, velocity = symbols("momentum mass velocity")
 definition = Eq(momentum, mass * velocity)
 
 definition_dimension_SI = units.kilogram * units.meter / units.second
@@ -29,7 +29,7 @@ def calculate_momentum(mass_: Quantity, velocity_: Quantity) -> Quantity:
     result_expr = solved.subs({
         mass: mass_,
         velocity: velocity_})
-    return expr_to_quantity(result_expr, 'momentum')
+    return expr_to_quantity(result_expr, "momentum")
 
 @validate_input(velocity_ =units.velocity, momentum_ =units.momentum)
 @validate_output(units.mass)
@@ -38,7 +38,7 @@ def calculate_mass(momentum_: Quantity, velocity_: Quantity) -> Quantity:
     result_expr = solved.subs({
         momentum: momentum_,
         velocity: velocity_})
-    return expr_to_quantity(result_expr, 'mass')
+    return expr_to_quantity(result_expr, "mass")
 
 @validate_input(momentum_ = units.momentum, mass_ = units.mass)
 @validate_output(units.velocity)
@@ -47,4 +47,4 @@ def calculate_velocity(momentum_: Quantity, mass_: Quantity) -> Quantity:
     result_expr = solved.subs({
         momentum: momentum_,
         mass: mass_})
-    return expr_to_quantity(result_expr, 'velocity')
+    return expr_to_quantity(result_expr, "velocity")
