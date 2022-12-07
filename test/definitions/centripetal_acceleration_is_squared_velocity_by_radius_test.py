49d8922b4c86be82a7a0b322d0b8b175e758b7a7
paradoxx84 <paradoxx84@yandex.ru>
2022-12-07 12:57:24 +0500

    fixes

--- test/definitions/centripetal_acceleration_is_squared_velocity_by_radius_test.py
+++ test/definitions/centripetal_acceleration_is_squared_velocity_by_radius_test.py
@@ -4,7 +4,7 @@ from pytest import approx, fixture, raises
 from symplyphysics import (
     units, convert_to, SI, errors
 )
-from symplyphysics.definitions import centripetal_acceleration_is_squared_velocity_by_radius as centripetal_acceleration_def
+from symplyphysics.laws.kinematic import centripetal_acceleration_is_squared_velocity_by_radius as centripetal_acceleration_def
 
 # Description
 ## The object is rotating with linear velocity of 10 m/s tied to the center with 0/5 m thread, it's centripetal acceleration should be 200 m/s/s (according to calc.ru online calculator)
@@ -22,7 +22,7 @@ def test_args():
     Args = namedtuple('Args', ['lin_velocity', 'curve_radius'])
     return Args(lin_velocity = lin_velocity, curve_radius = curve_radius)
 
-def test_basic_period(test_args):
+def test_basic_acceleration(test_args):
     result = centripetal_acceleration_def.calculate_acceleration(test_args.lin_velocity, test_args.curve_radius)
     assert SI.get_dimension_system().equivalent_dims(result.dimension, units.acceleration)
     result_acceleration = convert_to(result, units.meter / units.second**2).subs({units.meter: 1, units.second: 1}).evalf(2)    
