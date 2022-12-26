05d487d843aa22d102190b8c425928a60d8b0b1b
paradoxx84 <paradoxx84@yandex.ru>
2022-12-26 09:43:02 +0500

    centripetal velocity proof

--- symplyphysics/laws/kinematic/centripetal_acceleration_is_squared_velocity_by_radius.py
+++ symplyphysics/laws/kinematic/centripetal_acceleration_is_squared_velocity_by_radius.py
@@ -1,6 +1,6 @@
 from symplyphysics import (
     symbols, Eq, pretty, Quantity, units, solve, simplify,
-    validate_input, validate_output, expr_to_quantity
+    validate_input, validate_output, expr_to_quantity, Function
 )
 from symplyphysics.laws.kinematic import planar_projection_is_cosine as projector
 from symplyphysics.definitions import period_from_circular_frequency as circular_frequency_definition
@@ -8,13 +8,17 @@ from symplyphysics import pi, sin, cos
 from sympy import diff, sqrt, atan
 
 # Description
-## Centripetal acceleration is type of acceleration which is perpendicular to velocity vector and directed to the curve center.
-## Centripetal acceleration only changes the velocity vector direction and not the velocity vector length.
-## Law: an = V**2 / R
+## When the object moves not straight (but along some curve), acceleration not only changes the quantity of velocity, but also the velocity direction.
+## Velocity vector in any moment of observation is tangental to trajectory.
+## In every moment of observation acceleration vector might be represented as sum of two vectors. One of them is tangental to velocity and the other one is normal and directed to center of curve.
+## The tangental acceleration affects the velocity vector length and it is known as tangental acceleration, 
+## and normal acceleration afects the velocity direction and also known as centripetal (that's because it's directed to center of curve).
+## Law: an(t) = V(t)**2 / R
 ## Where:
-## an is centripetal acceleration (aka normal acceleration),
-## V is linear velocity,
-## R is curve radius.
+## t is the moment of observation
+## an is momental centripetal (normal) acceleration,
+## V is momental linear velocity,
+## R is curve radius in this point of trajectory.
 
 centripetal_acceleration = symbols("centripetal_acceleration")
 linear_velocity = symbols("linear_velocity")
@@ -22,49 +26,33 @@ curve_radius = symbols("curve_radius")
 
 definition = Eq(centripetal_acceleration, linear_velocity**2 / curve_radius)
 
-## Circular movement might be described with 2-dimensional coordinate system as any circle is 2-D.
-## Object position is described with it's radius-vector R - the vector from zero coordinates to the object and with angle between X-axis and this radius-vector.
-## Tgis angle is called phase. 
-## The angle between Y-axis and radius-vector is (pi/2 - phase).
-## Let center of circle have zero coordinates. 
-## Let the alpha be 0 at the start of observation.
+# Proof
+## Let's assume we are having movement in 2-D space.
+## Object position is described with it's radius-vector R - the vector from zero coordinates to the object and with angle alpha between X-axis and this radius-vector.
 
-phase = symbols("phase")
-observation_time = symbols("_observation_time")
-period = symbols("period")
+time = symbols("time")
+alpha = symbols("alpha", cls = Function)
 
-## Phase is the function of time. Circular velocity is the derivative of phase by time. If the spinning is uniform (harmonic) with period of T, circular velocity is constant.
-circular_frequency = solve(circular_frequency_definition.definition, circular_frequency_definition.circular_frequency, dict = True)[0][circular_frequency_definition.circular_frequency]
-phase = circular_frequency * observation_time
-
-x_coordinate = projector.law.rhs.subs({projector.vector_length:curve_radius, projector.vector_angle: phase})
-y_coordinate = projector.law.rhs.subs({projector.vector_length:curve_radius, projector.vector_angle: pi/2 - phase})
-## BTW x and y at any moment of time satisfy equation x**2 + y**2 == R**2, but i don't know if it is gonna be useful here or not.
+x_coordinate = projector.law.rhs.subs({projector.vector_length:curve_radius, projector.vector_angle: alpha(time)})
+y_coordinate = projector.law.rhs.subs({projector.vector_length:curve_radius, projector.vector_angle: pi/2 - alpha(time)})
 
 ## Velocity projections are derivatives of respective coordinates.
-x_velocity = diff(x_coordinate, observation_time)
-y_velocity = diff(y_coordinate, observation_time)
-velocity_vector_length = simplify(sqrt(x_velocity**2 + y_velocity**2))
+x_velocity = diff(x_coordinate, time)
+y_velocity = diff(y_coordinate, time)
+velocity_vector_length = sqrt(x_velocity**2 + y_velocity**2)
 velocity_vector_angle = atan(y_velocity / x_velocity)
-## Velocity vector length is independent from alpha:
-assert(velocity_vector_length == 2*pi*sqrt(curve_radius**2/period**2))
-
-## Linear velocity vector angle is -(pi/2 - phase)
-assert(velocity_vector_angle == -atan(cos(2*pi*observation_time/period)/sin(2*pi*observation_time/period)))
-
-##TODO with properly defined circular velocity it is all too difficult for understanding. let's define it first.
-
 
 ## Acceleration projections are derivatives of respective velocities.
-x_acceleration = diff(x_velocity, observation_time)
-y_acceleration = diff(y_velocity, observation_time)
+x_acceleration = diff(x_velocity, time)
+y_acceleration = diff(y_velocity, time)
+full_acceleration_vector_length = sqrt(x_acceleration**2 + y_acceleration**2)
+full_acceleration_vector_angle = atan(y_acceleration / x_acceleration)
 
-acceleration_vector_length = simplify(sqrt(x_acceleration**2 + y_acceleration**2))
-print(f"{acceleration_vector_length}")
-acceleration_angle = atan(y_acceleration / x_acceleration)
-
-## We can prove angle between velocity and acceleration is pi/2
-## also we can prove velocity is tangent.
+angle_between_velocity_and_acceleration = full_acceleration_vector_angle - velocity_vector_angle
+normal_acceleration_vector_length = simplify(full_acceleration_vector_length * sin(angle_between_velocity_and_acceleration))
+print(f"{normal_acceleration_vector_length}")
+## sqrt(curve_radius**2*(Derivative(alpha(time), time)**4 + Derivative(alpha(time), (time, 2))**2))*sin(atan((sin(alpha(time))*Derivative(alpha(time), time)**2 - cos(alpha(time))*Derivative(alpha(time), (time, 2)))/(sin(alpha(time))*Derivative(alpha(time), (time, 2)) + cos(alpha(time))*Derivative(alpha(time), time)**2)) + atan(1/tan(alpha(time))))
+##TODO it needs little manual simplification and we'll get an = V**2 / R.
 
 def print():
     return pretty(definition, use_unicode=False)
