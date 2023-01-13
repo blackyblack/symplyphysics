a4fc3c2fe59c5c33091812699fd76f10b6e5de49
Andrey Lekar <andrey_lekar@adoriasoft.com>
2023-01-13 11:02:48 +0200

    Fix kinetic energy of spinning body

--- symplyphysics/laws/dynamics/kinetic_energy_of_spinning_body_is_moment_of_inertia_times_half_of_squared_angular_velocity.py
+++ symplyphysics/laws/dynamics/kinetic_energy_of_spinning_body_is_moment_of_inertia_times_half_of_squared_angular_velocity.py
@@ -1,5 +1,3 @@
-from collections import namedtuple
-
 from symplyphysics import (
     symbols, Eq, pretty, solve, Quantity, units,
     validate_input, validate_output, expr_to_quantity, SI
@@ -16,28 +14,16 @@ from sympy.physics.units.definitions.dimension_definitions import angle as angle
 kinetic_energy, object_inertia_moment, angular_velocity = symbols('kinetic_energy object_inertia_moment angular_velocity')
 law = Eq(kinetic_energy, object_inertia_moment * angular_velocity**2 / 2)
 
-I = units.Quantity('I')
-SI.set_quantity_dimension(I, units.mass * units.length**2)
-SI.set_quantity_scale_factor(I, 2 * units.kilogram * units.meter**2)
-
-w = units.Quantity('w')
-SI.set_quantity_dimension(w, angle_type / units.time)
-SI.set_quantity_scale_factor(w, 3 * units.radians / units.second)
-
-ang_velocity_radians = w.scale_factor
-result_energy_expr = solve(law, kinetic_energy, dict=True)[0][kinetic_energy]
-print(f"{result_energy_expr}")
-result_expr = result_energy_expr.subs({object_inertia_moment: I, angular_velocity: ang_velocity_radians})
-print(f"{result_expr}")
 
 def print():
     return pretty(law, use_unicode=False)
 
-@validate_input(moment_ = units.mass * units.length**2, ang_velocity_= angle_type / units.time)
+@validate_input(inertia_moment_=units.mass * units.length**2, angular_velocity_=angle_type / units.time)
 @validate_output(units.energy)
-def calculate_energy(moment_: Quantity, ang_velocity_: Quantity) -> Quantity:
-    ang_velocity_radians = ang_velocity_.scale_factor    
+def calculate_energy(inertia_moment_: Quantity, angular_velocity_: Quantity) -> Quantity:
     result_energy_expr = solve(law, kinetic_energy, dict=True)[0][kinetic_energy]
-    result_expr = result_energy_expr.subs({object_inertia_moment: moment_, angular_velocity: ang_velocity_radians})
-    return expr_to_quantity(result_expr, 'energy')
+    #HACK: sympy angles are always in radians and angular velocity cannot be properly converted to velocity
+    SI.set_quantity_dimension(angular_velocity_, 1 / units.time)
+    result_expr = result_energy_expr.subs({object_inertia_moment: inertia_moment_, angular_velocity: angular_velocity_})
+    return expr_to_quantity(result_expr, 'kinetic_energy')
 
