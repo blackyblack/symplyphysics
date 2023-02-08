a59dbc576763ad735972f77c4dfd928aa7bb90fa
paradoxx84 <92771253+paradoxx84@users.noreply.github.com>
2023-02-08 14:10:46 +0500

    mechanical work (#54)
    * start
    
    * work works
    
    * fixes

--- symplyphysics/core/vectors/vectors.py
+++ symplyphysics/core/vectors/vectors.py
@@ -1,5 +1,5 @@
 from typing import Any, List
-from sympy import Expr
+from sympy import Expr, nan
 from sympy.vector import CoordSys3D, Vector as SympyVector, express
 from sympy.vector.operators import _get_coord_systems
 
@@ -74,3 +74,108 @@ def vector_rebase(vector_: Vector, coord_system: CoordSys3D=None) -> Vector:
     sympy_vector = sympy_vector_from_vector(vector_)
     transformed_vector = express(sympy_vector, coord_system, variables=True)
     return vector_from_sympy_vector(transformed_vector)
+
+def extended_express(expr: Expr, system: CoordSys3D, system2: CoordSys3D=None, variables=False):
+    if system._transformation_from_parent_lambda is not None:
+        return _extended_express(expr, system)
+    return express(expr, system, system2, variables)
+
+# This is experimental feature from SymPy (not merged yet)
+def _extended_express(expr: Expr, coordsys: CoordSys3D):
+    """
+    express for vectors allowing transformation between
+    cartesian (x,y,z) and non-cartesian frames (eg cylindrical)
+    Parameters
+    ==========
+    expr : Vector
+        The expression to re-express in CoordSys3D 'coordsys'
+    system: CoordSys3D
+        The coordinate system the expr is to be expressed in
+    Examples
+    =========
+    >>> from sympy.vector import extended_express, CoordSys3D
+    >>> from sympy import pi
+    >>> N = CoordSys3D('N')
+    >>> C = N.create_new('C', transformation='cylindrical', \
+        vector_names = ('r', 'theta', 'z'))
+    >>> extended_express(5*N.i,C)
+    5*C.r
+    >>> extended_express(5*N.j,C)
+    5*C.r + pi/2*C.theta
+    >>> extended_express(10*N.k,C)
+    10*C.z
+    >>> extended_express(6*C.r,N)
+    6*N.i
+    >>> extended_express(6*C.r + pi/2*C.theta,N)
+    6*N.j
+    Where a coordinate is undefined (eg theta with
+    extended_express(N.k), it is mapped to zero.
+    The non-cartesian frame must be defined as a child of the
+    cartesian frame as above.
+    The output is expressed in terms of the base vectors of the
+    derived coordinate systems. Currently they default to i,j
+    and k which are confusing for non-cartesian systems so it
+    is recommended to explicitly name them as above.
+    See Also
+    ========
+    CoordSys3D.transformation_from_parent,
+    CoordSys3D.transformation_to_parent
+    """
+    if not isinstance(coordsys, CoordSys3D):
+        raise TypeError("system should be a CoordSys3D instance")
+
+    parts = expr.separate()
+
+    # get different coordinate frames in vector, cry if more than one
+    if isinstance(parts, dict):
+        # we have been given zero vector
+        if(len(parts) == 0): return SympyVector.zero
+        if(len(parts) > 1):  raise ValueError("Does not support \
+            expressions containing multiple base coordinate frames")
+
+        foundframe = tuple(parts.keys())[0]
+        foundvector = tuple(parts.values())[0]
+    else:
+        foundframe = parts.system
+        foundvector = parts
+
+    # we should now have found the only frame in the vector,
+    # and we now have to convert it to the given frame
+
+    from_frame = foundframe
+    to_frame = coordsys
+
+    from_coeffs1 = from_frame.base_vectors()
+    from_coeffs2 = from_frame.base_scalars()
+    to_coeffs = to_frame.base_vectors()    # output with vectors
+
+    if from_frame._parent == to_frame:
+        transform_function = from_frame._transformation_lambda
+    else:
+        if to_frame._parent == from_frame:
+            transform_function =  \
+                to_frame._transformation_from_parent_lambda
+        else:
+            raise ValueError("Cannot link Coordinate frames")
+    args = []
+
+    for i, j in zip(from_coeffs1, from_coeffs2):
+        # could be expressed in either for inertial frame
+        coeff1 = foundvector.coeff(i)
+        # understand both N.i and N.x (or C.r and C.i)
+        coeff2 = foundvector.coeff(j)
+        if coeff1 != 0 and coeff2 != 0:
+            raise ValueError("Cannot express vector with both base \
+                    vectors and base scalars - check your vector")
+        args.append(coeff1 + coeff2) # only one can be non-zero
+
+    vals = transform_function(*args)
+
+    ans = SympyVector.zero
+    for v, c in zip(vals, to_coeffs):
+        if v is nan:  # v is nan ie infinity from an arctan
+            v = 0
+        # use to solve for cylindrical coords
+        # where theta is undefined
+        ans += v * c
+    return ans
