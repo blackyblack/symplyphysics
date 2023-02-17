28db600345a77a674d8be8d7ae3fd5ec2b947228
paradoxx84 <paradoxx84@yandex.ru>
2023-02-17 08:18:37 +0500

    please help me with scalar with vector multiplication

--- symplyphysics/laws/dynamics/spring_reaction_from_deformation.py
+++ symplyphysics/laws/dynamics/spring_reaction_from_deformation.py
@@ -21,22 +21,20 @@ law = Eq(response_force, scale_vector(-elastic_coefficient, deformation))
 
 def print():
     return pretty(law, use_unicode=False)
-'''
-@validate_input(force_=units.force, distance_=units.length, force_angle=angle_type, distance_angle=angle_type)
-@validate_output(units.energy)
-def calculate_work(force_: Quantity, distance_: Quantity, force_angle: Quantity, distance_angle: Quantity) -> Quantity:
-    result_work_expr = solve(law, work, dict=True)[0][work]
+
+
+@validate_input(coefficient_=units.force / units.length, deformation_=units.length, deformation_angle_=angle_type)
+@validate_output(units.force)
+def calculate_force(coefficient_: Quantity, deformation_: Quantity, deformation_angle_: Quantity) -> Quantity:
+    result_force_expr = solve(law, response_force, dict=True)[0][response_force]
     Ca = CoordSys3D("Ca")
     Cy = Ca.create_new("Cy", transformation="cylindrical")
     #HACK: sympy angles are always in radians
-    force_angle_radians = force_angle.scale_factor
-    distance_angle_radians = distance_angle.scale_factor
-
-    # Convert to Cartesian coordinates as Dot product does not work properly for Cylindrical coordinates
-    force_vector = force_ * Cy.i + force_angle_radians * Cy.j
-    distance_vector = distance_ * Cy.i + distance_angle_radians * Cy.j
-    force_vector_cartesian = extended_express(force_vector, Ca)
-    distance_vector_cartesian = extended_express(distance_vector, Ca)
-    result_expr = result_work_expr.subs({force: force_vector_cartesian, distance: distance_vector_cartesian}).doit()
-    return expr_to_quantity(result_expr, "vector_work")
-    '''
+    deformation_angle_radians = deformation_angle_.scale_factor
+    
+    # Convert to Cartesian coordinates as nothing works properly for Cylindrical coordinates
+    def_vector = deformation_ * Cy.i + deformation_angle_radians * Cy.j    
+    def_vector_cartesian = extended_express(def_vector, Ca)    
+    result_expr = result_force_expr.subs({elastic_coefficient: coefficient_, deformation: def_vector_cartesian}).doit()
+    return expr_to_quantity(result_expr, "force_vector")
+    
