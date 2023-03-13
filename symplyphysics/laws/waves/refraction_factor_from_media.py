57e0e3fdafff2c60416dfdf0e2b6e6c5f9a357f8
paradoxx84 <paradoxx84@yandex.ru>
2023-03-13 09:35:11 +0500

    refraction factor

--- symplyphysics/laws/waves/refraction_factor_from_media.py
+++ symplyphysics/laws/waves/refraction_factor_from_media.py
@@ -1,6 +1,6 @@
 from symplyphysics import (
     symbols, Eq, pretty, solve, units, Quantity,
-    validate_input, validate_output, expr_to_quantity, sqrt
+    validate_input, validate_output, expr_to_quantity, sqrt, SI
 )
 
 # Description
@@ -20,10 +20,10 @@ law = Eq(refraction_factor, sqrt(relative_dielectric_permeability * relative_mag
 def print():
     return pretty(law, use_unicode=False)
 
-@validate_input()
-@validate_output()
+#@validate_input()
+#@validate_output()
 def calculate_refraction_factor(epsilon_: Quantity, mu_: Quantity) -> Quantity:        
-    result_expr = solve(law, refraction_factor, dict=True)[0][refraction_factor]
+    result_expr = solve(law, refraction_factor, dict=True)[0][refraction_factor]    
     factor_applied = result_expr.subs({relative_dielectric_permeability: epsilon_, 
-                                          relative_magnetic_permeability: mu_})
+                                       relative_magnetic_permeability: mu_})    
     return expr_to_quantity(factor_applied, 'refraction_factor')
