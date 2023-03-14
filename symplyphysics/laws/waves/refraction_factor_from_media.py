eceade8761b1b86248bd779d414bdae8f9672a48
paradoxx84 <paradoxx84@yandex.ru>
2023-03-14 07:46:24 +0500

    finalized

--- symplyphysics/laws/waves/refraction_factor_from_media.py
+++ symplyphysics/laws/waves/refraction_factor_from_media.py
@@ -1,6 +1,5 @@
 from symplyphysics import (
-    symbols, Eq, pretty, solve, units, Quantity,
-    validate_input, validate_output, expr_to_quantity, sqrt, SI
+    symbols, Eq, pretty, solve, sqrt
 )
 
 # Description
@@ -20,10 +19,8 @@ law = Eq(refraction_factor, sqrt(relative_dielectric_permeability * relative_mag
 def print():
     return pretty(law, use_unicode=False)
 
-#@validate_input()
-#@validate_output()
-def calculate_refraction_factor(epsilon_: Quantity, mu_: Quantity) -> Quantity:        
+def calculate_refraction_factor(epsilon_: float, mu_: float) -> float:        
     result_expr = solve(law, refraction_factor, dict=True)[0][refraction_factor]    
     factor_applied = result_expr.subs({relative_dielectric_permeability: epsilon_, 
                                        relative_magnetic_permeability: mu_})    
-    return expr_to_quantity(factor_applied, 'refraction_factor')
+    return factor_applied
--- test/waves/refraction_factor_from_media_test.py
+++ test/waves/refraction_factor_from_media_test.py
@@ -13,12 +13,10 @@ from symplyphysics.laws.waves import refraction_factor_from_media as media_law
 @fixture
 def test_args():
     dielectric_permeability =  1.000576
-
     magnetic_permeability =  1.00000037
-
     Args = namedtuple('Args', ['dielectric_permeability', 'magnetic_permeability'])
     return Args(dielectric_permeability = dielectric_permeability, magnetic_permeability = magnetic_permeability)
 
 def test_basic_factor(test_args):
-    result = media_law.calculate_refraction_factor(test_args.dielectric_permeability, test_args.magnetic_permeability).evalf(5)
+    result = media_law.calculate_refraction_factor(test_args.dielectric_permeability, test_args.magnetic_permeability).evalf(6)
     assert result == approx(1.000292, 0.00001)
