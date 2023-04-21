a380e5e5ab3eccb39d0b7f1be105005d6e47e8ea
paradoxx84 <92771253+paradoxx84@users.noreply.github.com>
2023-04-21 11:53:37 +0500

    speed_of_light (#67)
    * speed_of_light
    
    please suggest the better way to present this
    
    * Fix speed of light assertion
    
    * fixes
    
    * fixes
    
    ---------
    
    Co-authored-by: Andrey Lekar <andrey_lekar@adoriasoft.com>

---
+++ symplyphysics/laws/waves/speed_of_light_from_fundamentals.py
@@ -0,0 +1,20 @@
+from symplyphysics import (
+    Eq, pretty, units, sqrt, convert_to
+)
+from sympy.physics.units import speed_of_light, magnetic_constant, electric_constant
+
+# Description
+## Speed of light in vacuum is fundamental but still might be calculated from other fundamentals.
+
+# Law: c = 1/sqrt(e0 * u0), where
+## C is speed of light in vacuum,
+## e0 is electric constant or vacuum permittivity,
+## u0 is magnetic constant or vacuum permeability.
+
+
+law = Eq(speed_of_light, 1 / sqrt(magnetic_constant * electric_constant))
+
+def print() -> str:    
+    return pretty(law, use_unicode=False)
+
+assert convert_to(law.lhs, units.meter / units.second) == convert_to(law.rhs, units.meter / units.second)
