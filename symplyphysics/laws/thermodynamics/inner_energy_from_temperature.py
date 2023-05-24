5817cd754cbdb6b7909f6bc7f772af1a518121fd
paradoxx84 <92771253+paradoxx84@users.noreply.github.com>
2023-05-24 12:57:24 +0500

    inner_energy_of_ideal_gas (#75)
    * inner_energy_of_ideal_gas
    
    * fixes

---
+++ symplyphysics/laws/thermodynamics/inner_energy_from_temperature.py
@@ -0,0 +1,33 @@
+from sympy import (Eq, solve)
+from symplyphysics import (units, expr_to_quantity, Quantity, Symbol, print_expression,
+    validate_input_symbols, validate_output_symbol)
+
+# Description
+## Inner energy of ideal gas is sum of kinetic energy of all it's molecules.
+## Inner energy of Ideal gas law: U = 1.5 * m * R * T / M
+## Where:
+## U is inner energy,
+## m is mass of a gas,
+## R is ideal gas constant,
+## T is absolute temperature in Kelvins,
+## M is mass of one mole of this gas.
+
+# Conditions
+## Gas is ideal - no potential energy of any molecules interaction.
+
+inner_energy = Symbol("energy", units.energy)
+mass_of_gas = Symbol("mass_of_gas", units.mass)
+temperature = Symbol("temperature", units.temperature)
+mole_mass = Symbol("mole_mass", units.mass / units.amount_of_substance)
+
+law = Eq(inner_energy, 1.5 * mass_of_gas * units.molar_gas_constant * temperature / mole_mass)
+
+def print() -> str:
+    return print_expression(law)
+
+@validate_input_symbols(mass_of_gas_=mass_of_gas, temperature_=temperature, mole_mass_=mole_mass)
+@validate_output_symbol(inner_energy)
+def calculate_inner_energy(mass_of_gas_: Quantity, temperature_: Quantity, mole_mass_: Quantity) -> Quantity:
+    solved = solve(law, inner_energy, dict=True)[0][inner_energy]
+    result_expr = solved.subs({mass_of_gas: mass_of_gas_, temperature: temperature_, mole_mass: mole_mass_})
+    return expr_to_quantity(result_expr)
---
+++ test/thermodynamics/inner_energy_from_temperature_test.py
@@ -0,0 +1,51 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    Quantity,
+    SI,
+    convert_to,
+)
+from symplyphysics.laws.thermodynamics import inner_energy_from_temperature as inner_energy_law
+
+# Description
+## With help of calculator on https://www.calculatoratoz.com/ru/molar-internal-energy-of-ideal-gas-calculator/Calc-1705 I calculated inner energy of 1 mole (4 gram) of Helium with it's molar mass 4 gramm/mole at 20 Celsius degrees.
+## It should be 3656 Joules of energy.
+
+CELSIUS_TO_KELVIN_OFFSET = int(273)
+
+@fixture
+def test_args():
+    m = Quantity(4 * units.gram)
+    T = Quantity((20 + CELSIUS_TO_KELVIN_OFFSET) * units.kelvin)
+    M = Quantity(4 * units.gram / units.mole)
+    Args = namedtuple("Args", ["m", "T", "M"])
+    return Args(m=m, T=T, M=M)
+
+def test_basic_energy(test_args):
+    result = inner_energy_law.calculate_inner_energy(test_args.m, test_args.T, test_args.M)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.energy)
+    result_pressure = convert_to(result, units.joule).subs(units.joule, 1).evalf(2)
+    assert result_pressure == approx(3656, 0.01)        
+
+def test_bad_mass(test_args):
+    mb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        inner_energy_law.calculate_inner_energy(mb, test_args.T, test_args.M)
+    with raises(TypeError):
+        inner_energy_law.calculate_inner_energy(100, test_args.T, test_args.M)
+
+def test_bad_temperature(test_args):
+    tb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        inner_energy_law.calculate_inner_energy(test_args.m, tb, test_args.M)
+    with raises(TypeError):
+        inner_energy_law.calculate_inner_energy(test_args.m, 100, test_args.M)
+
+def test_bad_mole_mass(test_args):
+    Mb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        inner_energy_law.calculate_inner_energy(test_args.m, test_args.T, Mb)
+    with raises(TypeError):
+        inner_energy_law.calculate_inner_energy(test_args.m, test_args.T, 100)
