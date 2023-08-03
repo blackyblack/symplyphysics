0294aec538a2b063004c944489175c2a39164511
Danis Valitov <crudblurr@gmail.com>
2023-08-03 18:01:47 +0500

    stefan-boltzmann constant was converted to Quantity

--- plots/laws/thermodynamics/dependence_of_radiation_power_on_temperature.py
+++ plots/laws/thermodynamics/dependence_of_radiation_power_on_temperature.py
@@ -2,17 +2,18 @@
 from sympy import solve, symbols
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
-from symplyphysics import (print_expression, units, convert_to)
-from symplyphysics.laws.thermodynamics import irradiation_of_black_body_from_temperature as stefan_boltzmann_law
+from symplyphysics import (print_expression, units, Quantity, convert_to)
+from symplyphysics.laws.thermodynamics import radiance_of_black_body_from_temperature as stefan_boltzmann_law
 
 print(f"Formula is:\n{stefan_boltzmann_law.print_law()}")
+
 solved = solve(stefan_boltzmann_law.law, stefan_boltzmann_law.radiance,
     dict=True)[0][stefan_boltzmann_law.radiance]
-radiance_temperature = solved.subs({
-    stefan_boltzmann_law.units.stefan_boltzmann_constant: 
-                            convert_to(units.stefan_boltzmann_constant,units.watt/units.meter**2/units.kelvin**4).evalf(5)
-})
 
+radiance_temperature = solved.subs({
+    stefan_boltzmann_law.units.stefan_boltzmann_constant: convert_to(Quantity(units.stefan_boltzmann_constant),
+                                                                     units.watt/units.meter**2/units.kelvin**4).evalf(6)
+})
 print(f"Radiance function is:\n{print_expression(radiance_temperature)}")
 p1 = plot(radiance_temperature, (stefan_boltzmann_law.temperature, 0, 400),
     line_color="black",
