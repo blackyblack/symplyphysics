12d770c22bd65eec134eb7d1fb098ed4809c684b
Danis Valitov <52219690+v-danis@users.noreply.github.com>
2023-08-15 21:56:43 +0500

    added columb law in scalar form (#109)
    * added trailing whitespace
    
    * removed the whitespace
    
    * added columb law in scalar form
    
    * fixed space
    
    * small fixes

---
+++ symplyphysics/laws/electricity/force_from_charge_and_distance.py
@@ -0,0 +1,43 @@
+from sympy import (Eq, solve)
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input, validate_output)
+
+# Description
+## Coulomb's law states that the force F between two point charges, q1 and q2, in a vacuum is proportional to their product
+## and inversely proportional to the square of their distance apart r.
+
+# Law: F = k_e * ((q_1 * q_2)/r_{12}^2), where
+## F is force between two electrically charged particles at rest (in vacuum),
+## k_e is a Coulomb constant,
+## q_1 and q_2 are the quantities of each charge,
+## r is the distance between the charges.
+
+# Note
+## The symbols Q1 and Q2 in the Coulomb's law equation represent the quantities of charge on the two interacting objects.
+## When using the "+" and "-" signs in the calculation of force,
+## the result will be that a "-" value for force is a sign of an attractive force
+## and a "+" value for force signifies a repulsive force. Mathematically, the force value
+## would be found to be positive when Q1 and Q2 are of like charge - either both "+" or both "-".
+## And the force value would be found to be negative when Q1 and Q2 are of opposite charge - one is "+"
+## and the other is "-". This is consistent with the concept that oppositely charged objects
+## have an attractive interaction and like charged objects have a repulsive interaction.
+
+force = Symbol("force", units.force)
+first_charge = Symbol("first_charge", units.charge)
+second_charge = Symbol("second_charge", units.charge)
+distance = Symbol("distance", units.length)
+
+law = Eq(force, (units.coulomb_constant * first_charge * second_charge)/(distance**2))
+
+def print_law() -> str:
+    return print_expression(law)
+
+@validate_input(first_charge_ = first_charge, second_charge_ = second_charge, distance_ = distance)
+@validate_output(force)
+def calculate_force(first_charge_: Quantity, second_charge_: Quantity, distance_: Quantity) -> Quantity:
+    solved = solve(law, force, dict=True)[0][force]
+    result_expr = solved.subs({
+        first_charge: first_charge_,
+        second_charge: second_charge_,
+        distance: distance_,
+    })
+    return Quantity(result_expr)
---
+++ test/electricity/force_from_charge_and_distance_test.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    convert_to,
+    Quantity,
+    SI,
+    prefixes
+)
+from symplyphysics.laws.electricity import force_from_charge_and_distance as couloumb_law_scalar
+
+# Description. With help of online calculator at https://www.wolframalpha.com/widgets/gallery/view.jsp?id=5227fa9a19dce7ba113f50a405dcaf09
+## The force between the charges q1 = 10 [nC] and q2 = -50 [nC], which are separated by a distance of r = 0.02 [m], is equivalent to -0.011234 [N].
+
+@fixture(name="test_args")
+def test_args_fixture():
+    q1 = Quantity(10 * prefixes.nano * units.coulomb)
+    q2 = Quantity(-50 * prefixes.nano * units.coulomb)
+    r = Quantity(0.02 * units.meter)
+    Args = namedtuple("Args", ["q1", "q2", "r"])
+    return Args(q1=q1, q2=q2, r=r)
+
+def test_basic_force(test_args):
+    result = couloumb_law_scalar.calculate_force(test_args.q1, test_args.q2, test_args.r)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.force)
+    result_force = convert_to(result, units.newton).evalf(7)
+    assert result_force == approx(-0.0112344, 0.00001)
+
+def test_bad_charge(test_args):
+    q0 = Quantity(1 * units.meter)
+    with raises(errors.UnitsError):
+        couloumb_law_scalar.calculate_force(q0, test_args.q2, test_args.r)
+    with raises(errors.UnitsError):
+        couloumb_law_scalar.calculate_force(test_args.q1, q0, test_args.r)
+    with raises(TypeError):
+        couloumb_law_scalar.calculate_force(1, test_args.q2, test_args.r)
+    with raises(TypeError):
+        couloumb_law_scalar.calculate_force(test_args.q1, 1, test_args.r)
+
+def test_bad_distance(test_args):
+    r0 = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        couloumb_law_scalar.calculate_force(test_args.q1, test_args.q2, r0)
+    with raises(TypeError):
+        couloumb_law_scalar.calculate_force(test_args.q1, test_args.q2, 1)
