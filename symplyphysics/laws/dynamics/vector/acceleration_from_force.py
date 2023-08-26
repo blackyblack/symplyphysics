5714a03db89da6ffbeb7343fcd1aeac9a0b7e3dd
Andrey Lekar <andrey_lekar@adoriasoft.com>
2023-08-26 12:10:59 +0200

    Vectors ready

--- symplyphysics/laws/dynamics/vector/acceleration_from_force.py
+++ symplyphysics/laws/dynamics/vector/acceleration_from_force.py
@@ -1,6 +1,5 @@
-from sympy import sympify
 from symplyphysics import (units, Quantity, Symbol, QuantityVector, Vector, scale_vector,
-    validate_input, validate_output)
+    validate_input, validate_output, list_of_quantities)
 
 # Description
 ## Newton's second law in vector form: a = 1/m * F
@@ -24,22 +23,14 @@ def force_law(acceleration_: Vector) -> Vector:
 @validate_input(mass_=mass, acceleration_=units.acceleration)
 @validate_output(units.force)
 def calculate_force(mass_: Quantity, acceleration_: QuantityVector) -> QuantityVector:
-    quantities_vector = Vector(acceleration_.components, acceleration_.coordinate_system)
-    result_force = force_law(quantities_vector)
-    force_components = []
-    for c in result_force.components:
-        with_mass = sympify(c).subs(mass, mass_)
-        force_components.append(Quantity(with_mass))
+    result_force = force_law(acceleration_)
+    force_components = list_of_quantities(result_force.components, {mass: mass_})
     return QuantityVector(force_components, acceleration_.coordinate_system)
 
 
 @validate_input(mass_=mass, force_=units.force)
 @validate_output(units.acceleration)
 def calculate_acceleration(mass_: Quantity, force_: QuantityVector) -> QuantityVector:
-    quantities_vector = Vector(force_.components, force_.coordinate_system)
-    result_acceleration = acceleration_law(quantities_vector)
-    acceleration_components = []
-    for c in result_acceleration.components:
-        with_mass = sympify(c).subs(mass, mass_)
-        acceleration_components.append(Quantity(with_mass))
+    result_acceleration = acceleration_law(force_)
+    acceleration_components = list_of_quantities(result_acceleration.components, {mass: mass_})
     return QuantityVector(acceleration_components, force_.coordinate_system)
--- symplyphysics/laws/dynamics/vector/spring_reaction_from_deformation.py
+++ symplyphysics/laws/dynamics/vector/spring_reaction_from_deformation.py
@@ -1,7 +1,5 @@
-from sympy import sympify
-from symplyphysics import (Vector, scale_vector, units, Quantity, Symbol, validate_input,
-    validate_output)
-from symplyphysics.core.vectors.vectors import QuantityVector
+from symplyphysics import (Vector, QuantityVector, scale_vector, units, Quantity, Symbol,
+    validate_input, validate_output, list_of_quantities)
 
 # Description
 ## Deformed spring is about to return back to it's undeformed state and responds with some force. Law is:
@@ -27,22 +25,16 @@ def deformation_law(force_: Vector) -> Vector:
 @validate_input(coefficient_=elastic_coefficient, deformation_=units.length)
 @validate_output(units.force)
 def calculate_force(coefficient_: Quantity, deformation_: QuantityVector) -> QuantityVector:
-    quantities_vector = Vector(deformation_.components, deformation_.coordinate_system)
-    result_force = force_law(quantities_vector)
-    force_components = []
-    for c in result_force.components:
-        with_coefficient = sympify(c).subs(elastic_coefficient, coefficient_)
-        force_components.append(Quantity(with_coefficient))
+    result_force = force_law(deformation_)
+    force_components = list_of_quantities(result_force.components,
+        {elastic_coefficient: coefficient_})
     return QuantityVector(force_components, deformation_.coordinate_system)
 
 
 @validate_input(coefficient_=elastic_coefficient, force_=units.force)
 @validate_output(units.length)
 def calculate_deformation(coefficient_: Quantity, force_: QuantityVector) -> QuantityVector:
-    quantities_vector = Vector(force_.components, force_.coordinate_system)
-    result_deformation = deformation_law(quantities_vector)
-    deformation_components = []
-    for c in result_deformation.components:
-        with_coefficient = sympify(c).subs(elastic_coefficient, coefficient_)
-        deformation_components.append(Quantity(with_coefficient))
+    result_deformation = deformation_law(force_)
+    deformation_components = list_of_quantities(result_deformation.components,
+        {elastic_coefficient: coefficient_})
     return QuantityVector(deformation_components, force_.coordinate_system)
