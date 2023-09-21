47172dc2119d1d0c1988a84d65098d8ea9189096
paradoxx84 <paradoxx84@yandex.ru>
2023-09-21 10:38:26 +0500

    fix

--- symplyphysics/laws/electricity/capacitor_impedance_is_capacitance_and_frequency_proportional.py
+++ symplyphysics/laws/electricity/capacitor_impedance_is_capacitance_and_frequency_proportional.py
@@ -4,27 +4,27 @@ from symplyphysics import (units, Quantity, Symbol, print_expression, validate_i
 
 # Description
 ## While the serial resistance of ideal capacitor is zero, its reactance depends on its capacitance and frequency.
-## Law: Xc = -j/wС, where
-## Xc is capacitor reactance,
+## Law: Zc = -j/wС, where
+## Xc is capacitor impedance,
 ## j is imaginary number,
 ## w is circular frequency,
 ## C is capacitance of capacitor.
 
-capacitor_reactance = Symbol("capacitor_reactance", units.impedance)
+capacitor_impedance = Symbol("capacitor_impedance", units.impedance)
 circular_frequency = Symbol("circular_frequency", angle_type / units.time)
 capacitor_capacitance = Symbol("capacitor_capacitance", units.capacitance)
 
-law = Eq(capacitor_reactance, -I / (circular_frequency * capacitor_capacitance))
+law = Eq(capacitor_impedance, -I / (circular_frequency * capacitor_capacitance))
 
 def print_law() -> str:
     return print_expression(law)
 
 
 @validate_input(capacitance_=capacitor_capacitance, circular_frequency_=circular_frequency)
-@validate_output(capacitor_reactance)
-def calculate_reactance(capacitance_: Quantity, circular_frequency_: Quantity) -> Quantity:
-    result_reactance_expr = solve(law, capacitor_reactance, dict=True)[0][capacitor_reactance]
-    result_expr = result_reactance_expr.subs({
+@validate_output(capacitor_impedance)
+def calculate_impedance(capacitance_: Quantity, circular_frequency_: Quantity) -> Quantity:
+    result_impedance_expr = solve(law, capacitor_impedance, dict=True)[0][capacitor_impedance]
+    result_expr = result_impedance_expr.subs({
         capacitor_capacitance: capacitance_,
         circular_frequency: circular_frequency_
     })
