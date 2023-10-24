871f70d9a69aea4d95b275da2062a696079b8ac5
paradoxx84 <paradoxx84@yandex.ru>
2023-10-24 11:28:04 +0500

    power factor

---
+++ symplyphysics/laws/electricity/power_factor_from_active_and_full_power.py
@@ -0,0 +1,32 @@
+from sympy import (I, Eq, solve)
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+    validate_output, dimensionless)
+
+# Description
+## Power factor is property of any AC consumer. Commonly not all power consumed from source makes useful work.
+## The part of consumed power which makes work called active power.
+## Part of it may be returned to source and it is also known as reactive power.
+## Law: Q = P / S, where
+## Q is power factor,
+## P is active power consumed,
+## S is total power consumed.
+
+full_power = Symbol("full_power", units.power)
+active_power = Symbol("circular_frequency", units.power)
+power_factor = Symbol("power_factor", dimensionless)
+
+law = Eq(power_factor, active_power / full_power)
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(active_power_=active_power, full_power_=full_power)
+@validate_output(power_factor)
+def calculate_power_factor(active_power_: Quantity, full_power_: Quantity) -> Quantity:
+    result_factor_expr = solve(law, power_factor, dict=True)[0][power_factor]
+    result_expr = result_factor_expr.subs({
+        active_power: active_power_,
+        full_power: full_power_
+    })
+    return Quantity(result_expr)
---
+++ test/electricity/power_factor_from_active_and_full_power_test.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (errors, units, convert_to, Quantity, SI, dimensionless)
+from symplyphysics.laws.electricity import power_factor_from_active_and_full_power as power_factor_law
+
+# Description
+## Assert we have a device wich consums 10 Watt of power and makes 3 Watt of work. Power factor of this consumer should be 3/10.
+
+@fixture(name="test_args")
+def test_args_fixture():
+    P = Quantity(3 * units.watt)
+    S = Quantity(10 * units.watt)    
+    Args = namedtuple("Args", ["P", "S"])
+    return Args(P=P, S=S)
+
+
+def test_basic_resistance(test_args):
+    result = power_factor_law.calculate_power_factor(test_args.P, test_args.S)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, dimensionless)
+    result_current = convert_to(result, dimensionless).evalf(6)
+    assert result_current == approx(0.3, 0.001)
+
+
+def test_bad_power(test_args):
+    pb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        power_factor_law.calculate_power_factor(pb, test_args.S)
+        power_factor_law.calculate_power_factor(test_args.P, pb)
+    with raises(TypeError):
+        power_factor_law.calculate_power_factor(100, test_args.S)
+        power_factor_law.calculate_power_factor(test_args.P, 100)
