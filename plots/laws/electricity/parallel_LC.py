387ea8a098b5afa83f9f8fddefd6a08fb7a475e0
paradoxx84 <paradoxx84@yandex.ru>
2023-11-20 14:21:19 +0500

    fixes

--- plots/laws/electricity/parallel_LC.py
+++ plots/laws/electricity/parallel_LC.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 # Description
-## Parallel LC circuit might be an object for resonans effect.
+## Parallel LC circuit might be an object for resonance effect.
 ## Total LC impedance depends on frequency of supplied energy.
-## Impedance have a single peak at the frequency corresponding to Tompson's frequency of this LC.
+## Impedance has a single peak at the frequency corresponding to Thompson's frequency of this LC.
 ## This is a resonant frequency of this circuit.
+## Also we have here a real LC with some resistive leakage. Resonant frequency stays the same as ideal LC, but resonant peak is not so sharp. The sharpness of resonant peak shows quality factor of LC circuit.
 
 from sympy import Abs, solve
 from sympy.plotting import plot
@@ -14,34 +15,48 @@ from symplyphysics.laws.electricity.circuits import admittance_of_parallel_dipol
 from symplyphysics.definitions import admittance_is_inversed_impedance as admittance_def
 from symplyphysics.laws.electricity import capacitor_impedance_from_capacitance_and_frequency as capacitor_impedance
 from symplyphysics.laws.electricity import coil_impedance_from_inductivity_and_frequency as coil_impedance
-from symplyphysics.laws.electricity.circuits import oscillation_period_for_capacitor_inductor_node as tompsons_formula
+from symplyphysics.laws.electricity.circuits import oscillation_period_for_capacitor_inductor_node as thompsons_formula
 from symplyphysics.laws.kinematic import period_from_angular_frequency as period_definition
 
 frequency_arg = Symbol("frequency_arg")
 
-EXAMPLE_CAPACITANCE = 0.00001
 EXAMPLE_INDUCTANCE = 0.00001
+EXAMPLE_CAPACITANCE = 0.00001
+EXAMPLE_R = 5000000
 
 # Parallel connection of dipoles summarizes their admittances.
 # First find impedances and then admittances
-L_impedance = coil_impedance.law.rhs.subs({coil_impedance.circular_frequency : frequency_arg})
-L_admittance = admittance_def.definition.rhs.subs({admittance_def.dipole_impedance: L_impedance})
+L_impedance = coil_impedance.law.rhs.subs({coil_impedance.circular_frequency: frequency_arg})
+L_admittance = admittance_def.definition.rhs.subs({admittance_def.dipole_impedance: L_impedance}).subs({coil_impedance.coil_inductivity: EXAMPLE_INDUCTANCE})
 
-C_impedance = capacitor_impedance.law.rhs.subs({capacitor_impedance.circular_frequency : frequency_arg})
-C_admittance = admittance_def.definition.rhs.subs({admittance_def.dipole_impedance: C_impedance})
+C_impedance = capacitor_impedance.law.rhs.subs({capacitor_impedance.circular_frequency: frequency_arg})
+C_admittance = admittance_def.definition.rhs.subs({admittance_def.dipole_impedance: C_impedance}).subs({capacitor_impedance.capacitor_capacitance:EXAMPLE_CAPACITANCE})
 
-total_impedance_from_admittance_law = solve(admittance_def.definition, admittance_def.dipole_impedance, dict=True)[0][admittance_def.dipole_impedance]
+R_impedance = EXAMPLE_R
+R_admittance = admittance_def.definition.rhs.subs({admittance_def.dipole_impedance: R_impedance})
 
-total_impedance_complex = total_impedance_from_admittance_law.subs({admittance_def.dipole_admittance: (L_admittance + C_admittance)})
+ideal_elements = tuple({L_admittance, C_admittance})
+real_elements = tuple({L_admittance, C_admittance, R_admittance})
 
-total_impedance_abs_to_plot = Abs(total_impedance_complex).subs({coil_impedance.coil_inductivity: EXAMPLE_INDUCTANCE, capacitor_impedance.capacitor_capacitance: EXAMPLE_CAPACITANCE})
+admittance_ideal = parallel_admittance_law.law.rhs.subs(parallel_admittance_law.admittances, ideal_elements).doit()
+print(f"ideal: ", admittance_ideal)
+admittance_real = parallel_admittance_law.law.rhs.subs(parallel_admittance_law.admittances, real_elements).doit()
+print(f"real: ", admittance_real)
 
-tomsons_period = tompsons_formula.law.rhs.subs({tompsons_formula.inductance: EXAMPLE_INDUCTANCE, tompsons_formula.capacitance: EXAMPLE_CAPACITANCE})
+impedance_from_admittance_law = solve(admittance_def.definition, admittance_def.dipole_impedance, dict=True)[0][admittance_def.dipole_impedance]
+
+impedance_ideal = impedance_from_admittance_law.subs({admittance_def.dipole_admittance: admittance_ideal})
+impedance_ideal_to_plot = Abs(impedance_ideal)
+
+impedance_real = impedance_from_admittance_law.subs({admittance_def.dipole_admittance: admittance_real})
+impedance_real_to_plot = Abs(impedance_real)
+
+thomsons_period = thompsons_formula.law.rhs.subs({thompsons_formula.inductance: EXAMPLE_INDUCTANCE, thompsons_formula.capacitance: EXAMPLE_CAPACITANCE})
 frequency_from_period = solve(period_definition.law, period_definition.circular_frequency, dict = True)[0][period_definition.circular_frequency]
-tomsons_frequency = frequency_from_period.subs({period_definition.period: tomsons_period})
+thomsons_frequency = frequency_from_period.subs({period_definition.period: thomsons_period})
 
 
-PLOT = plot(total_impedance_abs_to_plot, (frequency_arg, tomsons_frequency - 1.5, tomsons_frequency + 1.5),
+PLOT = plot(impedance_ideal_to_plot, (frequency_arg, thomsons_frequency - 1.2, thomsons_frequency + 1.2),
     line_color="blue",
     title="LC impedance",
     label="LC impedance",
@@ -50,7 +65,13 @@ PLOT = plot(total_impedance_abs_to_plot, (frequency_arg, tomsons_frequency - 1.5
     backend=MatplotlibBackend,
     show=False)
 
-freq_line = plot((frequency_arg - tomsons_frequency) * 1000000000,  (frequency_arg, tomsons_frequency - 0.001, tomsons_frequency + 0.001),
+real_impedance = plot(impedance_real_to_plot, (frequency_arg, thomsons_frequency - 1.2, thomsons_frequency + 1.2),
+    label="RLC impedance",
+    line_color="green",    
+    show=False)
+PLOT.append(real_impedance[0])
+
+freq_line = plot((frequency_arg - thomsons_frequency) * 1000000000,  (frequency_arg, thomsons_frequency - 0.001, thomsons_frequency + 0.001),
     label="frequency = 1 / sqrt(LC)",
     line_color="red",    
     show=False)
