7641d383cb4ce0a624c60c6038619a42e646457e
paradoxx84 <paradoxx84@yandex.ru>
2023-12-18 13:55:17 +0500

    fix

--- symplyphysics/laws/dynamics/friction_force_from_normal_force.py
+++ symplyphysics/laws/dynamics/friction_force_from_normal_force.py
@@ -28,4 +28,4 @@ def print_law() -> str:
 def calculate_friction_force(friction_factor_: float, normal_reaction_: Quantity) -> Quantity:
     result_expr = solve(law, friction_force, dict=True)[0][friction_force]
     friction_force_applied = result_expr.subs({friction_factor: friction_factor_, normal_reaction: normal_reaction_})
-    return Quantity(friction_force_applied, dimension=units.force)
+    return Quantity(friction_force_applied)
