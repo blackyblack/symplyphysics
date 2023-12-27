9c08690233749ac78a811f11ed26065c20d2893c
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2023-12-27 21:07:17 +0500

    reynolds num fix3
    Bad params tests added

--- test/hydro/reynolds_number_test.py
+++ test/hydro/reynolds_number_test.py
@@ -1,12 +1,12 @@
 from collections import namedtuple
-from pytest import approx, fixture
-from symplyphysics import Quantity, units
+from pytest import approx, fixture, raises
+from symplyphysics import Quantity, units, errors
 from symplyphysics.laws.hydro import reynolds_number
 
 
 # Description
-## There is pipe with diameter 0.1m, density 1000kg/m3, velocity 1m/s
-## and dynamic viscosity 0.000894Pa*s. The reynolds number should be 111856.823
+## There is pipe with diameter 0.1 m, density 1000 kg/m3, velocity 1 m/s
+## and dynamic viscosity 0.000894 Pa*s. The reynolds number should be 111856.823
 
 @fixture(name="test_args")
 def test_args_fixture():
@@ -22,3 +22,44 @@ def test_reynolds_number(test_args):
     result = reynolds_number.calculate_reynolds_number(
         test_args.d, test_args.rho, test_args.v, test_args.mu)
     assert result == approx(111856.823, 0.001)
+
+
+def test_bad_velocity(test_args):
+    bv = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        reynolds_number.calculate_reynolds_number(
+            test_args.d, test_args.rho, bv, test_args.mu)
+    with raises(TypeError):
+        reynolds_number.calculate_reynolds_number(
+            test_args.d, test_args.rho, 0, test_args.mu)
+       
+
+def test_bad_diameter(test_args):
+    bd = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        reynolds_number.calculate_reynolds_number(
+            bd, test_args.rho, test_args.v, test_args.mu)
+    with raises(TypeError):
+        reynolds_number.calculate_reynolds_number(
+            0, test_args.rho, test_args.v, test_args.mu)
+        
+
+def test_bad_density(test_args):
+    bd = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        reynolds_number.calculate_reynolds_number(
+            test_args.d, bd, test_args.v, test_args.mu)
+    with raises(TypeError):
+        reynolds_number.calculate_reynolds_number(
+            test_args.d, 10, test_args.v, test_args.mu)
+
+
+def test_bad_dynamic_viscosity(test_args):
+    bd = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        reynolds_number.calculate_reynolds_number(
+            test_args.d, test_args.rho, test_args.v, bd)
+    with raises(TypeError):
+        reynolds_number.calculate_reynolds_number(
+            test_args.d, test_args.rho, test_args.v, 0.05)
+
