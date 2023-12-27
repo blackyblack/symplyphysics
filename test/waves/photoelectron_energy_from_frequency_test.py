17d00b153fc87168dba61bdf9eb94ca53dd2d14f
Malofeeva Alina <alina9888@yandex.ru>
2023-12-27 12:28:57 +0400

    Test task. (#133)
    * photoeffect
    
    * Change filenames

---
+++ test/waves/photoelectron_energy_from_frequency_test.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    convert_to,
+    Quantity,
+    SI,
+)
+from symplyphysics.laws.waves import photoelectron_energy_from_frequency as photoeffect
+
+# Description
+## The metal surface with the work function 3.81e-19 J is irradiated with photons of frequency 9.23e14 Hz.
+## The maximum kinetic energy of the photoelectrons will be 2.3e-19 J
+## Source: https://zaochnik.ru/blog/zadachi-na-temu-fotony-i-fotoeffekt-s-resheniem/?ysclid=lqmlagmvdp85340848 (4)
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    frequency = Quantity(9.23e14 * units.hertz)
+    work = Quantity(3.81e-19 * units.joule)
+    Args = namedtuple("Args", ["frequency", "work"])
+    return Args(frequency=frequency, work=work)
+
+
+def test_basic_energy(test_args):
+    result = photoeffect.calculate_max_kinetic_energy(test_args.frequency, test_args.work)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.energy)
+    result_current = convert_to(result, units.joule).evalf(3)
+    assert result_current == approx(2.3e-19, 0.01)
+
+
+def test_bad_frequency(test_args):
+    fb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        photoeffect.calculate_max_kinetic_energy(fb, test_args.work)
+    with raises(TypeError):
+        photoeffect.calculate_max_kinetic_energy(100, test_args.work)
+
+
+def test_bad_work_function(test_args):
+    wb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        photoeffect.calculate_max_kinetic_energy(test_args.frequency, wb)
+    with raises(TypeError):
+        photoeffect.calculate_max_kinetic_energy(test_args.frequency, 100)
