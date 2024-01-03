49311b0cb1016c817a0a5a6fbeb9e25069f95674
Voldoned <110999247+Voldoned@users.noreply.github.com>
2024-01-03 22:09:58 +0300

    Update efficiency_factor_test.py
    [UPDATE] Fix naming style in 'test_args_fixture'

--- test/thermodynamics/efficiency_factor_test.py
+++ test/thermodynamics/efficiency_factor_test.py
@@ -9,24 +9,24 @@ from symplyphysics.laws.thermodynamics import efficiency_factor as efficiency_la
 
 @fixture(name="test_args")
 def test_args_fixture():
-    Q_h = Quantity(2 * units.joule)
-    Q_r = Quantity(1.5 * units.joule)
-    Args = namedtuple("Args", ["Q_h", "Q_r"])
-    return Args(Q_h=Q_h, Q_r=Q_r)
+    q_h = Quantity(2 * units.joule)
+    q_r = Quantity(1.5 * units.joule)
+    Args = namedtuple("Args", ["q_h", "q_r"])
+    return Args(q_h=q_h, q_r=q_r)
 
 
 def test_basic_efficiency(test_args):
-    result = efficiency_law.calculate_efficiency_factor(test_args.Q_h, test_args.Q_r)
+    result = efficiency_law.calculate_efficiency_factor(test_args.q_h, test_args.q_r)
     assert result == approx(0.25, 0.001)
 
 
 def test_bad_efficiency(test_args):
     q_rb = Quantity(1 * units.meter)
     with raises(errors.UnitsError):
-        efficiency_law.calculate_efficiency_factor(test_args.Q_h, q_rb)
+        efficiency_law.calculate_efficiency_factor(test_args.q_h, q_rb)
     with raises(errors.UnitsError):
-        efficiency_law.calculate_efficiency_factor(q_rb, test_args.Q_r)
+        efficiency_law.calculate_efficiency_factor(q_rb, test_args.q_r)
     with raises(TypeError):
-        efficiency_law.calculate_efficiency_factor(test_args.Q_h, 1.5)
+        efficiency_law.calculate_efficiency_factor(test_args.q_h, 1.5)
     with raises(TypeError):
-        efficiency_law.calculate_efficiency_factor(2, test_args.Q_h)
+        efficiency_law.calculate_efficiency_factor(2, test_args.q_h)
