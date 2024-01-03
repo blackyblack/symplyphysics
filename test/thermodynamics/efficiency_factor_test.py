51739044d810c5d4e91f6218ca25e7fcaf921680
Voldoned <110999247+Voldoned@users.noreply.github.com>
2024-01-03 21:22:32 +0300

    Update efficiency_factor_test.py
    [UPDATE] Fix name variable in 'test_basic_efficiency'

--- test/thermodynamics/efficiency_factor_test.py
+++ test/thermodynamics/efficiency_factor_test.py
@@ -17,7 +17,7 @@ def test_args_fixture():
 
 def test_basic_efficiency(test_args):
     result = efficiency_law.calculate_efficiency_factor(test_args.Q_h, test_args.Q_r)
-    assert result_efficiency == approx(0.25, 0.001)
+    assert result == approx(0.25, 0.001)
 
 
 def test_bad_efficiency(test_args):
