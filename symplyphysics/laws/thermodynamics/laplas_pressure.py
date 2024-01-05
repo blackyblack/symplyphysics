1d3b6774c404bf2eca33270fe84e660498d66ae5
Voldoned <vova.nederkin@gmail.com>
2024-01-05 03:50:06 +0300

    [UPDATE] Add description and reformat code

--- symplyphysics/laws/thermodynamics/laplas_pressure.py
+++ symplyphysics/laws/thermodynamics/laplas_pressure.py
@@ -1,6 +1,14 @@
 from sympy import (Eq, solve)
-from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
-    validate_output)
+from symplyphysics import (units, Quantity, Symbol, print_expression,
+                           validate_input, validate_output)
+
+# Description
+## Under the curved surface of the liquid, in addition to the internal pressure, additional pressure is created due to the curvature of the surface.
+## The excess pressure under the curved surface of the liquid is determined by the Laplace formula: P_l = 2 * sigma / R
+## Where:
+## P_l - Laplas pressure
+## sigma - surface tension of the liquid
+## R - radius of surface curvature
 
 
 surface_tension_of_the_liquid = Symbol("surface_tension_of_the_liquid", units.force / units.length)
@@ -15,14 +23,9 @@ def print_law() -> str:
     return print_expression(law)
 
 
-@validate_input(surface_tension_of_the_liquid_=surface_tension_of_the_liquid,
-                radius_of_curvature_=radius_of_curvature)
+@validate_input(surface_tension_of_the_liquid_=surface_tension_of_the_liquid, radius_of_curvature_=radius_of_curvature)
 @validate_output(laplas_pressure)
-def calculate_laplas_pressure(surface_tension_of_the_liquid_: Quantity,
-                              radius_of_curvature_: Quantity) -> Quantity:
+def calculate_laplas_pressure(surface_tension_of_the_liquid_: Quantity, radius_of_curvature_: Quantity) -> Quantity:
     solved = solve(law, laplas_pressure, dict=True)[0][laplas_pressure]
-    result_expr = solved.subs({
-        surface_tension_of_the_liquid: surface_tension_of_the_liquid_,
-        radius_of_curvature: radius_of_curvature_
-    })
+    result_expr = solved.subs({surface_tension_of_the_liquid: surface_tension_of_the_liquid_, radius_of_curvature: radius_of_curvature_})
     return Quantity(result_expr)
