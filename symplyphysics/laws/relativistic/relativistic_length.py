9cf5937032bfe9e9fe8c14f0adbaf588ca0a0ac5
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2024-01-06 20:28:47 +0500

    Update relativistic_length.py

--- symplyphysics/laws/relativistic/relativistic_length.py
+++ symplyphysics/laws/relativistic/relativistic_length.py
@@ -8,7 +8,7 @@ from symplyphysics import (Quantity, Symbol, print_expression, units,
 # Length contraction is the phenomenon that a moving object's length is
 # measured to be shorter than its proper length, which is the length as
 # measured in the object's own rest frame.
-# Law: l_rel = l / sqrt(1 - v**2 / c**2), where
+# Law: l_rel = l * sqrt(1 - v**2 / c**2), where
 # l is rest length,
 # v is velocity,
 # c is speed of light,
@@ -32,6 +32,6 @@ def print_law():
 def calculate_relativistic_length(rest_length_: Quantity, velocity_: Quantity) -> Quantity:
     result_expr = solve(law, relativistic_length, dict=True)[
         0][relativistic_length]
-    mass_applied = result_expr.subs(
+    length_applied = result_expr.subs(
         {rest_length: rest_length_, velocity: velocity_})
-    return Quantity(mass_applied)
+    return Quantity(length_applied)
