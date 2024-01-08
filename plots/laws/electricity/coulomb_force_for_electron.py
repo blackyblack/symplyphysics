19d3aa41423ebe4ae02c270698b2e4415dfe06ae
Voldoned <vova.nederkin@gmail.com>
2024-01-08 21:29:23 +0300

    [UPDATE] Add coulomb constant in Coulomb Law

--- plots/laws/electricity/coulomb_force_for_electron.py
+++ plots/laws/electricity/coulomb_force_for_electron.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from sympy import solve, symbols
+from sympy import solve, symbols, Symbol
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
 from symplyphysics import (print_expression, units, Quantity, convert_to)
@@ -10,15 +10,19 @@ CHARGE_OF_ELECTRON = -1.6 * 1E-19
 
 print(f"Formula is:\n{coulomb_law.print_law()}")
 
-distance = symbols("dictance")
+distance = Symbol("dictance")
+electrostatic_constant = Symbol("electrostatic_constant")
 
 force_equation = coulomb_law.law.subs({
     coulomb_law.first_charge: CHARGE_OF_ELECTRON,
     coulomb_law.second_charge: CHARGE_OF_ELECTRON,
-    coulomb_law.distance: distance
+    coulomb_law.distance: distance,
+    coulomb_law.units.coulomb_constant:
+    convert_to(Quantity(units.coulomb_constant),
+               units.newton * (units.meters / units.coulomb) ** 2)
 }).rhs
 
-p1 = plot(force_equation, (distance, 1, 50),
+p1 = plot(force_equation, distance,
           line_color="blue",
           title="Coluomb Law",
           xlabel="r, m",
