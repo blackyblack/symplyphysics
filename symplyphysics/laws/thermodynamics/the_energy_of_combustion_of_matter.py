d2102290710ffc1dcf5c5bdf8fa262a6dfbdf689
Voldoned <vova.nederkin@gmail.com>
2024-01-09 00:16:26 +0300

    [CREATE] Create new law and test: the energy of combustion of matter

---
+++ symplyphysics/laws/thermodynamics/the_energy_of_combustion_of_matter.py
@@ -0,0 +1,26 @@
+from sympy import (Eq, solve)
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+    validate_output)
+
+amount_energy = Symbol("amount_energy", units.energy)
+specific_heat_combustion = Symbol("specific_heat_combustion", units.energy / units.mass)
+mass_of_matter = Symbol("mass_of_matter", units.mass)
+
+law = Eq(amount_energy, specific_heat_combustion * mass_of_matter)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(specific_heat_combustion_=specific_heat_combustion,
+    mass_of_matter_=mass_of_matter)
+@validate_output(amount_energy)
+def calculate_amount_energy(specific_heat_combustion_: Quantity, mass_of_matter_: Quantity) -> Quantity:
+
+    result_amount_energy_expr = solve(law, amount_energy, dict=True)[0][amount_energy]
+    result_expr = result_amount_energy_expr.subs({
+        specific_heat_combustion: specific_heat_combustion_,
+        mass_of_matter: mass_of_matter_
+    })
+    return Quantity(result_expr)
---
+++ test/thermodynamics/the_energy_of_combustion_of_matter_test.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    Quantity,
+    SI,
+    convert_to,
+    prefixes,
+)
+from symplyphysics.laws.thermodynamics import the_energy_of_combustion_of_matter as amount_energy
+
+# Test example from https://easyfizika.ru/zadachi/termodinamika/skolko-tepla-vydelitsya-pri-sgoranii-2-kg-benzina/
+# When burning 2 kg of gasoline, 92 MJ energy should be released
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    k_q = Quantity(46 * prefixes.mega * units.joule / units.kilogram)
+    m = Quantity(2 * units.kilogram)
+    Args = namedtuple("Args", ["k_q", "m"])
+    return Args(k_q=k_q, m=m)
+
+
+def test_basic_amount(test_args):
+    result = amount_energy.calculate_amount_energy(test_args.k_q, test_args.m)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.energy)
+    result_energy = convert_to(result, prefixes.mega * units.joules).evalf(5)
+    assert result_energy == approx(92, 0.001)
+
+
+def test_bad_specific_heat_combustion(test_args):
+    k_qb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        amount_energy.calculate_amount_energy(k_qb, test_args.m)
+    with raises(TypeError):
+        amount_energy.calculate_amount_energy(100, test_args.m)
+
+
+def test_bad_body_mass(test_args):
+    mb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        amount_energy.calculate_amount_energy(test_args.k_q, mb)
+    with raises(TypeError):
+        amount_energy.calculate_amount_energy(test_args.k_q, 100)
