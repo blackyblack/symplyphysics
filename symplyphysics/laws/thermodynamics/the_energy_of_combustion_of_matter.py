6de209df24e9e3c422007635f56dc99fb071b204
Voldoned <vova.nederkin@gmail.com>
2024-01-09 00:30:16 +0300

    [ADD] Add description for law

--- symplyphysics/laws/thermodynamics/the_energy_of_combustion_of_matter.py
+++ symplyphysics/laws/thermodynamics/the_energy_of_combustion_of_matter.py
@@ -2,6 +2,14 @@ from sympy import (Eq, solve)
 from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
     validate_output)
 
+# Description
+## The amount of heat released during complete combustion of a mass
+## (for solid and liquid substances) or volumetric (for gaseous) unit of matter
+## Q = k_q * m
+## Where:
+## k_q - the heat of combustion attributed to a unit of mass or volume of fuel is called specific heat of combustion
+## m - mass of matter
+
 amount_energy = Symbol("amount_energy", units.energy)
 specific_heat_combustion = Symbol("specific_heat_combustion", units.energy / units.mass)
 mass_of_matter = Symbol("mass_of_matter", units.mass)
