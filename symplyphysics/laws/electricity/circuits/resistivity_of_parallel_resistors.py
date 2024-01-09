1729ca7f4c1c0d644312557cca68a982ae4c1e7a
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-01-09 18:56:14 +0400

    Parallel resistors (redone) (#175)
    * Add law and test for parallel resistors
    
    * Fix law implementation and tests
    
    * Replace inverse resistance with direct one
    
    * Implement a (probably dirty) workaround to represent a sum of inverse
    resistances

---
+++ symplyphysics/laws/electricity/circuits/resistivity_of_parallel_resistors.py
@@ -0,0 +1,37 @@
+from sympy import Eq, solve
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+from symplyphysics.core.operations.sum_array import InvSumArray
+from symplyphysics.core.symbols.symbols import tuple_of_symbols
+
+# Description
+## If resistors are connected in parallel, total resistance is the inverse sum of inverse resistance of each resistor.
+## Law: R_parallel = 1/sum(1/R[i]), where
+## R_parallel is total resistance,
+## R[i] is resistance of i-th resistor.
+
+
+resistances = Symbol("resistances", units.impedance)
+parallel_resistance = Symbol("parallel_resistance", units.impedance)
+law = Eq(parallel_resistance, 1 / InvSumArray(resistances), evaluate=False)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(resistances_=resistances)
+@validate_output(units.impedance)
+def calculate_parallel_resistance(resistances_: list[Quantity]) -> Quantity:
+    resistance_symbols = tuple_of_symbols("resistance", units.impedance, len(resistances_))
+    resistances_law = law.subs(resistances, resistance_symbols).doit()
+    solved = solve(resistances_law, parallel_resistance, dict=True)[0][parallel_resistance]
+    for from_, to_ in zip(resistance_symbols, resistances_):
+        solved = solved.subs(from_, to_)
+    return Quantity(solved)
