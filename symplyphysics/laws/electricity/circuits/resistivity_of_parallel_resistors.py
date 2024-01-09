b90f3c854127a9c475adc79a7650f8552000fe49
Andrey Lekar <andrey_lekar@adoriasoft.com>
2024-01-09 17:03:37 +0200

    Rework parallel resistors

--- symplyphysics/laws/electricity/circuits/resistivity_of_parallel_resistors.py
+++ symplyphysics/laws/electricity/circuits/resistivity_of_parallel_resistors.py
@@ -7,7 +7,7 @@ from symplyphysics import (
     validate_input,
     validate_output,
 )
-from symplyphysics.core.operations.sum_array import InvSumArray
+from symplyphysics.core.operations.sum_array import SumArray
 from symplyphysics.core.symbols.symbols import tuple_of_symbols
 
 # Description
@@ -17,20 +17,21 @@ from symplyphysics.core.symbols.symbols import tuple_of_symbols
 ## R[i] is resistance of i-th resistor.
 
 
-resistances = Symbol("resistances", units.impedance)
+inv_resistances = Symbol("resistances", 1 / units.impedance)
 parallel_resistance = Symbol("parallel_resistance", units.impedance)
-law = Eq(parallel_resistance, 1 / InvSumArray(resistances), evaluate=False)
+law = Eq(parallel_resistance, 1 / SumArray(inv_resistances), evaluate=False)
 
 
 def print_law() -> str:
     return print_expression(law)
 
 
-@validate_input(resistances_=resistances)
+@validate_input(resistances_=units.impedance)
 @validate_output(units.impedance)
 def calculate_parallel_resistance(resistances_: list[Quantity]) -> Quantity:
     resistance_symbols = tuple_of_symbols("resistance", units.impedance, len(resistances_))
-    resistances_law = law.subs(resistances, resistance_symbols).doit()
+    inv_resistance_symbols = tuple(1 / r for r in resistance_symbols)
+    resistances_law = law.subs(inv_resistances, inv_resistance_symbols).doit()
     solved = solve(resistances_law, parallel_resistance, dict=True)[0][parallel_resistance]
     for from_, to_ in zip(resistance_symbols, resistances_):
         solved = solved.subs(from_, to_)
