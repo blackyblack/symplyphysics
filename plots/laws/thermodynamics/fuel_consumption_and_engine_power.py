8d795d2adbeb3b9534499e6a3c946a344068b131
Voldoned <vova.nederkin@gmail.com>
2024-01-10 14:25:40 +0300

    [UPDATE] Going back to the changes of the previous commit. Transferring the equation output from the example to this file.

--- plots/laws/thermodynamics/fuel_consumption_and_engine_power.py
+++ plots/laws/thermodynamics/fuel_consumption_and_engine_power.py
@@ -1,82 +1,84 @@
-from sympy import symbols, Eq
+from sympy import symbols, Eq, solve
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
 from symplyphysics import print_expression
-from examples.thermodynamics import \
-    energy_from_combustion_of_gasoline as combustion_energy_example
+from symplyphysics.definitions import density_from_mass_volume as density_law
+from symplyphysics.laws.kinematic import distance_from_constant_velocity as distance_law
+from symplyphysics.laws.electricity import power_factor_from_active_and_full_power as efficiency_law
+from symplyphysics.laws.electricity import power_from_energy_time as power_law
+from symplyphysics.laws.thermodynamics import energy_from_combustion as combustion_energy_law
+
+# Create efficiency factor values from 15% to 75% in increments of 15%
+efficiency_factor_values = [0.15, 0.3, 0.45, 0.6, 0.75]
+
+volume_of_gasoline, velocity_of_car, efficiency_factor, power_of_car, distance = symbols(
+    "volume_of_gasoline,velocity_of_car,efficiency_factor,power_of_car, distance"
+)
+density_of_gasoline, gasoline_specific_heat_combustion = symbols(
+    "density_of_gasoline,gasoline_specific_heat_combustion"
+)
 
 # Note: fuel_consumption = volume_of_fuel / distance
 fuel_consumption = symbols("fuel_consumption")
 
-# Create efficiency factor values from 15% to 75% in increments of 15%
-EFFICIENCY_FACTOR_1 = 0.15
-EFFICIENCY_FACTOR_2 = 0.3
-EFFICIENCY_FACTOR_3 = 0.45
-EFFICIENCY_FACTOR_4 = 0.6
-EFFICIENCY_FACTOR_5 = 0.75
+velocity_equation = distance_law.law.subs({
+    distance_law.distance(distance_law.movement_time): distance,
+    distance_law.constant_velocity: velocity_of_car,
+    distance_law.initial_position: 0
+})
+time_value = solve(velocity_equation, distance_law.movement_time, dict=True)[0][distance_law.movement_time]
 
-fuel_consumption_value = combustion_energy_example.volume_of_gasoline / combustion_energy_example.answer.rhs
+power_equation = power_law.law.subs({
+    power_law.time: time_value,
+    power_law.power: power_of_car
+})
+energy_from_power_value = solve(power_equation, power_law.energy, dict=True)[0][power_law.energy]
+
+density_of_gasoline_equation = density_law.definition.subs({
+    density_law.density: density_of_gasoline,
+    density_law.volume: volume_of_gasoline
+})
+mass_of_gasoline_value = solve(density_of_gasoline_equation, density_law.mass, dict=True)[0][density_law.mass]
+
+amount_heat_value = combustion_energy_law.law.subs({
+    combustion_energy_law.specific_heat_combustion: gasoline_specific_heat_combustion,
+    combustion_energy_law.mass_of_matter: mass_of_gasoline_value
+}).rhs
+
+efficiency_factor_equation = efficiency_law.law.subs({
+    efficiency_law.active_power: energy_from_power_value,
+    efficiency_law.full_power: amount_heat_value,
+    efficiency_law.power_factor: efficiency_factor
+})
+
+distance_value = solve(efficiency_factor_equation, distance, dict=True)[0][distance]
+
+fuel_consumption_value = volume_of_gasoline / distance_value
 fuel_consumption_equation = Eq(fuel_consumption, fuel_consumption_value)
 gasoline_consumption_equation = fuel_consumption_equation.subs({
-    combustion_energy_example.velocity_of_car: 15,
-    # 1 kilometers / hour = (1 / 3.6) meters / second,
-    combustion_energy_example.density_of_gasoline: 700,
-    # kilogram / (meter ** 3),
-    combustion_energy_example.gasoline_specific_heat_combustion: 46 * 1E6
-    # 1 megajoules / kilogram = 1E6 joules / kilogram
+    velocity_of_car: 15,      # 1 kilometers / hour = (1 / 3.6) meters / second,
+    density_of_gasoline: 700,     # kilogram / (meter ** 3),
+    gasoline_specific_heat_combustion: 46 * 1E6       # 1 megajoules / kilogram = 1E6 joules / kilogram
 })
 
 print(f"Formula is:\n{print_expression(gasoline_consumption_equation)}")
 
-gasoline_compustion_value_1 = gasoline_consumption_equation.subs({
-    combustion_energy_example.efficiency_factor: EFFICIENCY_FACTOR_1
-}).rhs
-gasoline_compustion_value_2 = gasoline_consumption_equation.subs({
-    combustion_energy_example.efficiency_factor: EFFICIENCY_FACTOR_2
-}).rhs
-gasoline_compustion_value_3 = gasoline_consumption_equation.subs({
-    combustion_energy_example.efficiency_factor: EFFICIENCY_FACTOR_3
-}).rhs
-gasoline_compustion_value_4 = gasoline_consumption_equation.subs({
-    combustion_energy_example.efficiency_factor: EFFICIENCY_FACTOR_4
-}).rhs
-gasoline_compustion_value_5 = gasoline_consumption_equation.subs({
-    combustion_energy_example.efficiency_factor: EFFICIENCY_FACTOR_5
-}).rhs
-
 # 1 m^3 / m = 10^(-3) liters / m = (10^(-3) / 10^(-3)) liters / km = 1 liters / km
 base_plot = plot(title="Coulomb Law",
                  xlabel="$N, W$",
-                 ylabel="$V/S, liters/km$",
+                 ylabel="$V/S, l/km$",
                  backend=MatplotlibBackend,
                  legend=True,
                  show=False)
 
-p1 = plot(gasoline_compustion_value_1,
-          (combustion_energy_example.power_of_car, 1_000, 100_000),
-          label="$\eta_{engine}=" + f"{EFFICIENCY_FACTOR_1}$",
-          show=False)
-p2 = plot(gasoline_compustion_value_2,
-          (combustion_energy_example.power_of_car, 1_000, 100_000),
-          label="$\eta_{engine}=" + f"{EFFICIENCY_FACTOR_2}$",
-          show=False)
-p3 = plot(gasoline_compustion_value_3,
-          (combustion_energy_example.power_of_car, 1_000, 100_000),
-          label="$\eta_{engine}=" + f"{EFFICIENCY_FACTOR_3}$",
-          show=False)
-p4 = plot(gasoline_compustion_value_4,
-          (combustion_energy_example.power_of_car, 1_000, 100_000),
-          label="$\eta_{engine}=" + f"{EFFICIENCY_FACTOR_4}$",
-          show=False)
-p5 = plot(gasoline_compustion_value_5,
-          (combustion_energy_example.power_of_car, 1_000, 100_000),
-          label="$\eta_{engine}=" + f"{EFFICIENCY_FACTOR_5}$",
-          show=False)
-
-base_plot.append(p1[0])
-base_plot.append(p2[0])
-base_plot.append(p3[0])
-base_plot.append(p4[0])
-base_plot.append(p5[0])
+# Create plots for every efficient factor in sequence and add plot to base plot
+for efficiency_factor_value in efficiency_factor_values:
+    gasoline_compustion_value = gasoline_consumption_equation.subs({
+        efficiency_factor: efficiency_factor_value
+    }).rhs
+    subplot = plot(gasoline_compustion_value, (power_of_car, 1_000, 100_000),
+                   label="$\eta_{engine}=" + f"{efficiency_factor_value}$",
+                   show=False)
+    base_plot.append(subplot[0])
 
 base_plot.show()
