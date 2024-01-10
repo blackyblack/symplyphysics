3e9b13c88bd3667ff0b411da39053b84feaaaec2
Aleksandr Slatov <alexslatov@gmail.com>
2024-01-10 16:45:14 +0400

    Fix names in test

--- test/hydro/volume_flux_is_constant_test.py
+++ test/hydro/volume_flux_is_constant_test.py
@@ -18,45 +18,45 @@ from symplyphysics.laws.hydro import volume_flux_is_constant as continuity_equat
 
 @fixture(name="test_args")
 def test_args_fixture():
-    area_start = Quantity(1 * units.meter**2)
-    v_start = Quantity(6 * units.meter / units.second)
-    area_end = Quantity(2 * units.meter**2)
-    Args = namedtuple("Args", ["area_start", "v_start", "area_end"])
+    area_before = Quantity(1 * units.meter**2)
+    speed_before = Quantity(6 * units.meter / units.second)
+    area_after = Quantity(2 * units.meter**2)
+    Args = namedtuple("Args", ["area_before", "speed_before", "area_after"])
     return Args(
-        area_start=area_start,
-        v_start=v_start,
-        area_end=area_end,
+        area_before=area_before,
+        speed_before=speed_before,
+        area_after=area_after,
     )
 
 
 def test_basic_flow_speed(test_args):
     result = continuity_equation.calculate_flow_speed(
-        test_args.area_start, test_args.v_start, test_args.area_end
+        test_args.area_before, test_args.speed_before, test_args.area_after
     )
     assert SI.get_dimension_system().equivalent_dims(result.dimension, units.velocity)
     result_velocity = convert_to(result, units.meter / units.second).evalf(3)
     assert result_velocity == approx(3, 0.001)
 
 
-def test_bad_area_start(test_args):
-    bad_area_start = Quantity(1 * units.coulomb)
+def test_bad_tube_area_before(test_args):
+    bad_area_before = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        continuity_equation.calculate_flow_speed(bad_area_start, test_args.v_start, test_args.area_end)
+        continuity_equation.calculate_flow_speed(bad_area_before, test_args.speed_before, test_args.area_after)
     with raises(TypeError):
-        continuity_equation.calculate_flow_speed(1, test_args.v_start, test_args.area_end)
+        continuity_equation.calculate_flow_speed(1, test_args.speed_before, test_args.area_after)
 
 
-def test_bad_v_start(test_args):
-    bad_v_start = Quantity(1 * units.coulomb)
+def test_bad_fluid_speed_before(test_args):
+    bad_fluid_speed_before = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        continuity_equation.calculate_flow_speed(test_args.area_start, bad_v_start, test_args.area_end)
+        continuity_equation.calculate_flow_speed(test_args.area_before, bad_fluid_speed_before, test_args.area_after)
     with raises(TypeError):
-        continuity_equation.calculate_flow_speed(test_args.area_start, 1, test_args.area_end)
+        continuity_equation.calculate_flow_speed(test_args.area_before, 1, test_args.area_after)
 
 
-def test_bad_area_end(test_args):
-    bad_area_end = Quantity(1 * units.coulomb)
+def test_bad_tube_area_after(test_args):
+    bad_area_after = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        continuity_equation.calculate_flow_speed(test_args.area_start, test_args.v_start, bad_area_end)
+        continuity_equation.calculate_flow_speed(test_args.area_before, test_args.speed_before, bad_area_after)
     with raises(TypeError):
-        continuity_equation.calculate_flow_speed(test_args.area_start, test_args.v_start, 1)
+        continuity_equation.calculate_flow_speed(test_args.area_before, test_args.speed_before, 1)
