d3870806bbde2c0440b0636b6870d2d13442dd39
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-01-11 21:45:35 +0400

    Add law and test for Bernoulli's equation (#187)
    * Add law and test for Bernoulli's equation
    
    * Simplify law and fix test accordingly
    
    * Create law and test for inner fluid pressure
    
    This law has been created in relation to Bernoulli's equation
    specifying what exactly inner fluid pressure is.
    
    * Minor fix to names of symbols
    
    * Fix dashes and replace symbols
    
    * Fix tests
    
    * Simplify inner_pressure_of_fluid_test
    * Fix mistaken number in bernoullis_equation_test
    
    * Rename law and test more descriptively
    
    * Remove commented out code

---
+++ symplyphysics/laws/hydro/inner_pressure_of_fluid_is_constant.py
@@ -0,0 +1,42 @@
+from sympy import Eq, solve, dsolve, Derivative
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    Function,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## Bernoulli's equation applied to an ideal liquid specifies that the inner 
+## pressure of the fluid is constant at all points along a streamline.
+
+# Law: d(P_inner)/dt = 0
+## P_inner - inner pressure of fluid at chosen point
+## t - time
+
+# Condition
+## The fluid must be ideal, i.e.
+## 1) nonviscous
+## 2) in steady (laminar) flow
+## 3) incompressible
+## 4) irrotational
+
+time = Symbol("time", units.time)
+inner_pressure = Function("inner_pressure", units.pressure)
+
+law = Eq(Derivative(inner_pressure(time), time), 0)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(inner_pressure_before_=inner_pressure)
+@validate_output(inner_pressure)
+def calculate_inner_pressure(inner_pressure_before_: Quantity) -> Quantity:
+    dsolved = dsolve(law, inner_pressure(time))
+    result_expr = dsolved.subs("C1", inner_pressure_before_).rhs
+    return Quantity(result_expr)
---
+++ test/hydro/inner_pressure_of_fluid_test.py
@@ -0,0 +1,50 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    convert_to,
+    Quantity,
+    SI,
+)
+import symplyphysics.laws.hydro.inner_pressure_of_fluid as inner_pressure
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    P_static = Quantity(10 * units.pascal)
+    P_dynamic = Quantity(2 * units.pascal)
+    P_hydrostatic = Quantity(3 * units.pascal)
+    Args = namedtuple("Args", "P_static, P_dynamic, P_hydrostatic")
+    return Args(
+        P_static=P_static,
+        P_dynamic=P_dynamic,
+        P_hydrostatic=P_hydrostatic,
+    )
+
+
+def test_basic_law(test_args):
+    result = inner_pressure.calculate_inner_pressure(
+        test_args.P_static,
+        test_args.P_dynamic,
+        test_args.P_hydrostatic,
+    )
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.pressure)
+    result_inner_pressure = convert_to(result, units.pascal).evalf(5)
+    assert result_inner_pressure == approx(15, 1e-5)
+
+
+def test_bad_static_pressure(test_args):
+    P_bad = Quantity(1 * units.second)
+    with raises(errors.UnitsError):
+        inner_pressure.calculate_inner_pressure(P_bad, test_args.P_dynamic, test_args.P_hydrostatic)
+    with raises(TypeError):
+        inner_pressure.calculate_inner_pressure(100, test_args.P_dynamic, test_args.P_hydrostatic)
+    with raises(errors.UnitsError):
+        inner_pressure.calculate_inner_pressure(test_args.P_static, P_bad, test_args.P_hydrostatic)
+    with raises(TypeError):
+        inner_pressure.calculate_inner_pressure(test_args.P_static, 100, test_args.P_hydrostatic)
+    with raises(errors.UnitsError):
+        inner_pressure.calculate_inner_pressure(test_args.P_static, test_args.P_dynamic, P_bad)
+    with raises(TypeError):
+        inner_pressure.calculate_inner_pressure(test_args.P_static, test_args.P_dynamic, 100)
