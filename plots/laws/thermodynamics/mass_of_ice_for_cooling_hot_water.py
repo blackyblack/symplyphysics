8315a18d3fbfcce814f22d5329262ce0d9a633a9
Voldoned <vova.nederkin@gmail.com>
2024-01-11 16:13:57 +0300

    [UPDATE] Rename symbol and variable. Update 'xlabel' and 'ylabel'.

--- plots/laws/thermodynamics/mass_of_ice_for_cooling_hot_water.py
+++ plots/laws/thermodynamics/mass_of_ice_for_cooling_hot_water.py
@@ -14,7 +14,7 @@ bath_volumes = [0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14]
 volume_of_bath = symbols("volume_of_bath")
 temperature_of_hot_water = symbols("temperature_of_hot_water")
 temperature_of_ice = symbols("temperature_of_ice")
-temperature_end = symbols("temperature_end")
+temperature_balance = symbols("temperature_balance")
 
 density_of_water = symbols("density_of_water")
 specific_heat_heating_water = symbols("specific_heat_heating_water")
@@ -43,7 +43,7 @@ energy_cooling_hot_water = energy_heating_law.law.subs({
     energy_heating_law.specific_heat_capacity: specific_heat_heating_water,
     energy_heating_law.body_mass: mass_of_hot_water_value,
     energy_heating_law.temperature_origin: temperature_of_hot_water,
-    energy_heating_law.temperature_end: temperature_end
+    energy_heating_law.temperature_end: temperature_balance
 })
 
 energy_to_heating_ice_equation = energy_heating_law.law.subs({
@@ -62,7 +62,7 @@ energy_to_heat_melted_ice_equation = energy_heating_law.law.subs({
     energy_heating_law.specific_heat_capacity: specific_heat_heating_water,
     energy_heating_law.body_mass: mass_of_ice,
     energy_heating_law.temperature_origin: temperature_melt_ice,
-    energy_heating_law.temperature_end: temperature_end
+    energy_heating_law.temperature_end: temperature_balance
 })
 
 thermodinamics_law_1_equation = thermodinamics_law_1.law.subs({
@@ -86,8 +86,8 @@ mass_of_ice_equation_to_plot = mass_of_ice_equation.subs({
 })
 
 base_plot = plot(title="The mass of ice required to cool the hot water to a set temperature",
-    xlabel="$T_{end}, K$",
-    ylabel="$m_{ice}, kg$",
+    xlabel=r"$T_\text{balance}, K$",
+    ylabel=r"$m_\text{ice}, kg$",
     backend=MatplotlibBackend,
     legend=True,
     show=False)
@@ -99,10 +99,10 @@ for bath_volume in bath_volumes:
 
     # Find the upper limit of the temperature scale, so as not to build a graph
     # on the temperature section where there will be a negative mass
-    temperature_supremum = solve(mass_of_ice_equation_to_subplot.subs({mass_of_ice: 0}), temperature_end,
-        dict=True)[0][temperature_end]
+    temperature_supremum = solve(mass_of_ice_equation_to_subplot.subs({mass_of_ice: 0}), temperature_balance,
+        dict=True)[0][temperature_balance]
 
-    subplot = plot(mass_of_ice_equation_to_subplot.rhs, (temperature_end, 280, temperature_supremum),
+    subplot = plot(mass_of_ice_equation_to_subplot.rhs, (temperature_balance, 280, temperature_supremum),
         label=r"$V_\text{bath}=" + f"{bath_volume}" + "\ m^3$",
         show=False)
     base_plot.append(subplot[0])
