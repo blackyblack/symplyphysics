d4615355b51208e4ed7c120039e1a5549bdf211d
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2024-01-11 18:05:01 +0500

    hagen poiseuille equation firtst commit (#179)
    * hagen poiseuille equation firtst commit
    
    * hagen poiseuille equation fix 1
    
    * hagen_poiseuille_equation fix 2
    
    removed round pi from law, test function fixed

---
+++ symplyphysics/laws/hydro/hagen_poiseuille_equation.py
@@ -0,0 +1,47 @@
+from sympy import Eq, solve, pi
+
+from symplyphysics import (Quantity, Symbol, print_expression, units,
+                           validate_input, validate_output)
+
+# Law: delta_p = 8 * mu * L * Q / (pi * R**4)
+# delta_p - pressure difference,
+# mu is viscosity,
+# L is length,
+# Q is the volumetric flow rate,
+# R is the pipe radius
+
+dynamic_viscosity = Symbol("dynamic_viscosity", units.pressure * units.time)
+length = Symbol("length", units.length)
+flow_rate = Symbol("flow_rate", units.volume / units.time)
+radius = Symbol("radius", units.length)
+delta_pressure = Symbol("delta_pressure", units.pressure)
+
+
+law = Eq(delta_pressure, 8 * dynamic_viscosity *
+         length * flow_rate / (pi * radius**4))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    dynamic_viscosity_=dynamic_viscosity,
+    length_=length,
+    flow_rate_=flow_rate,
+    radius_=radius,
+)
+@validate_output(delta_pressure)
+def calculate_delta_pressure(
+        dynamic_viscosity_: Quantity,
+        length_: Quantity,
+        flow_rate_: Quantity,
+        radius_: Quantity) -> Quantity:
+    result_expr = solve(law, delta_pressure, dict=True)[0][delta_pressure]
+    result_applied = result_expr.subs({
+        dynamic_viscosity: dynamic_viscosity_,
+        length: length_,
+        flow_rate: flow_rate_,
+        radius: radius_,
+    })
+    return Quantity(result_applied)
---
+++ test/hydro/hagen_poiseuille_equation_test.py
@@ -0,0 +1,52 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import Quantity, units, errors, convert_to
+from symplyphysics.laws.hydro import hagen_poiseuille_equation
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    mu = Quantity(0.000894 * units.pascal * units.second)
+    l = Quantity(0.1 * units.meter)
+    q = Quantity(1 * (units.meter**3 / units.second))
+    r = Quantity(0.1 * units.meter)
+    Args = namedtuple("Args", ["mu", "l", "q", "r"])
+    return Args(mu=mu, l=l, q=q, r=r)
+
+
+def test_basic(test_args):
+    result = hagen_poiseuille_equation.calculate_delta_pressure(test_args.mu, test_args.l, test_args.q, test_args.r)
+    result_pressure = convert_to(result, units.pascal).evalf(4)
+    assert result_pressure == approx(2.276, 0.001)
+
+
+def test_bad_dynamic_viscosity(test_args):
+    bad_mu = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        hagen_poiseuille_equation.calculate_delta_pressure(bad_mu, test_args.l, test_args.q, test_args.r)
+    with raises(TypeError):
+        hagen_poiseuille_equation.calculate_delta_pressure(0, test_args.l, test_args.q, test_args.r)
+
+
+def test_bad_length(test_args):
+    bad_l = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        hagen_poiseuille_equation.calculate_delta_pressure(test_args.mu, bad_l, test_args.q, test_args.r)
+    with raises(TypeError):
+        hagen_poiseuille_equation.calculate_delta_pressure(test_args.mu, 0, test_args.q, test_args.r)
+
+
+def test_bad_flow_rate(test_args):
+    bad_q = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        hagen_poiseuille_equation.calculate_delta_pressure(test_args.mu, test_args.l, bad_q, test_args.r)
+    with raises(TypeError):
+        hagen_poiseuille_equation.calculate_delta_pressure(test_args.mu, test_args.l, 0, test_args.r)
+
+
+def test_bad_radius(test_args):
+    bad_r = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        hagen_poiseuille_equation.calculate_delta_pressure(test_args.mu, test_args.l, test_args.q, bad_r)
+    with raises(TypeError):
+        hagen_poiseuille_equation.calculate_delta_pressure(test_args.mu, test_args.l, test_args.q, 0)
