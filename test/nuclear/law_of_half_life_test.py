095126518d15ff0aa93a6da59c0d94346f6a0979
Ростислав Житков <rosjit1@gmail.com>
2024-01-11 19:03:10 +0300

    Commit #1. the_law_of_half-life

---
+++ test/nuclear/law_of_half_life_test.py
@@ -0,0 +1,58 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    units,
+    SI,
+    dimensionless,
+    convert_to,
+    Quantity,
+    errors
+)
+from symplyphysics.laws.nuclear import law_of_half_life as number_of_cores_law
+
+# Description
+## It is known that for oxygen, the half-life is 124 seconds.
+## https://ru.wikipedia.org/wiki/Период_полураспада
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    number_of_cores_initial = 2e30
+    half_life = Quantity(90 * units.second)
+    decay_time = Quantity(90 * units.second)
+
+    Args = namedtuple("Args", ["number_of_cores_initial", "half_life", "decay_time"])
+    return Args(
+        number_of_cores_initial=number_of_cores_initial,
+        half_life=half_life, decay_time=decay_time)
+
+
+def test_basic_current(test_args):
+    result = number_of_cores_law.calculate_number_of_cores(test_args.number_of_cores_initial, test_args.half_life, test_args.decay_time)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, dimensionless)
+    result = convert_to(result, dimensionless).evalf(5)
+    assert result == approx(1e30, rel=0.01)
+
+
+def test_bad_number_of_cores_initial(test_args):
+    number_of_cores_initial = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        number_of_cores_law.calculate_number_of_cores(number_of_cores_initial, test_args.half_life, test_args.decay_time)
+    with raises(TypeError):
+        number_of_cores_law.calculate_number_of_cores(True, test_args.half_life, test_args.decay_time)
+
+
+def test_bad_half_life(test_args):
+    half_life = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        number_of_cores_law.calculate_number_of_cores(test_args.number_of_cores_initial, half_life, test_args.decay_time)
+    with raises(TypeError):
+        number_of_cores_law.calculate_number_of_cores(test_args.number_of_cores_initial, 100, test_args.decay_time)
+
+
+def test_bad_decay_time(test_args):
+    decay_time = Quantity(1 * units.kelvin)
+    with raises(errors.UnitsError):
+        number_of_cores_law.calculate_number_of_cores(test_args.number_of_cores_initial, test_args.half_life, decay_time)
+    with raises(TypeError):
+        number_of_cores_law.calculate_number_of_cores(test_args.number_of_cores_initial, test_args.half_life, 100)
