e7b35d50a1b25890439568b8095758a6d091ef42
Aleksandr Slatov <alexslatov@gmail.com>
2024-01-12 12:31:06 +0400

    Fix: Rename variables more descriptively

--- examples/optics/image_formed_by_two_lenses.py
+++ examples/optics/image_formed_by_two_lenses.py
@@ -1,45 +1,42 @@
 #!/usr/bin/env python3
 
 from sympy import symbols, solve
-from symplyphysics import units
 from symplyphysics.laws.optics import lens_focus_from_object_and_image as thin_lens
 
-# Two thin converging lenses of focal lengths F1 = 10 cm and F2 = 20 cm are separated
-# by 20 cm. An object is placed 15 cm to the left of the lens 1. What is the position 
-# of the final image?
+# Two thin converging lenses of focal lengths 10 cm and 20 cm are separated by 20 cm. 
+# An object is placed 15 cm to the left of lens 1. What is the position of the final image?
 
-F1, F2 = symbols("F1 F2")  # focal lengths
-d1 = symbols("d1")  # distance from lens 1 to object
-delta = symbols("delta")  # distance between the two lenses
+focal_length_1, focal_length_2 = symbols("focal_length_1 focal_length_2")
+distance_to_object_1 = symbols("distance_to_object_1")
+distance_between_lenses = symbols("distance_between_lenses")
 
 values = {
-    F1: 10,
-    F2: 20,
-    d1: 15,
-    delta: 20,
+    focal_length_1: 10,
+    focal_length_2: 20,
+    distance_to_object_1: 15,
+    distance_between_lenses: 20,
 }
 
 # Calculate the position of the image formed by lens 1 using the thin-lens equation
 
 thin_lens_1 = thin_lens.law.subs({
-    thin_lens.focus_distance: F1,
-    thin_lens.distance_to_object: d1,
+    thin_lens.focus_distance: focal_length_1,
+    thin_lens.distance_to_object: distance_to_object_1,
 })
-f1 = solve(thin_lens_1, thin_lens.distance_to_image)[0]
+distance_to_image_1 = solve(thin_lens_1, thin_lens.distance_to_image)[0]
 
 # Calculate the distance between lens 2 and the image from lens 1
 
-d2 = delta - f1
+distance_to_object_2 = distance_between_lenses - distance_to_image_1
 
 # Calculate the position of the image formed by lens 2 using the thin-lens equation
 
 thin_lens_2 = thin_lens.law.subs({
-    thin_lens.focus_distance: F2,
-    thin_lens.distance_to_object: d2
+    thin_lens.focus_distance: focal_length_2,
+    thin_lens.distance_to_object: distance_to_object_2
 })
-f2 = solve(thin_lens_2, thin_lens.distance_to_image)[0]
+distance_to_image_2 = solve(thin_lens_2, thin_lens.distance_to_image)[0]
+distance_to_image_2_value = distance_to_image_2.subs(values).evalf(3)
 
-result_f2 = f2.subs(values)
-
-side = "right" if result_f2 > 0 else "left"
-print(f"The image is located {abs(result_f2.evalf(3))} cm to the {side} of lens 2.")
+side = "right" if distance_to_image_2_value > 0 else "left"
+print(f"The image is located {abs(distance_to_image_2_value)} cm to the {side} of lens 2.")
