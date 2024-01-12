a553b3a778ea3471974eae2c37762734e8494ee7
Voldoned <vova.nederkin@gmail.com>
2024-01-12 17:00:00 +0300

    [UPDATE] Remove two conditions

--- symplyphysics/laws/conservation/mixture_mass_equal_sum_of_components_masses.py
+++ symplyphysics/laws/conservation/mixture_mass_equal_sum_of_components_masses.py
@@ -15,9 +15,7 @@ from symplyphysics.core.symbols.symbols import tuple_of_symbols
 # Conditions:
 ## - Mixture in a closed impenetrable volume, that is, molecules/atoms cannot leave it
 ##   and they are always inside;
-## - Chemical reactions between components are neglected;
 ## - Mass is not transformed to energy, for example due to annihilation.
-## - d(m_i) / dt = 0
 
 masses_of_components = Symbol("masses_of_components", units.mass)
 mass_of_mixture = Symbol("mass_of_mixture", units.mass)
