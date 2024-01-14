940b9a32ad0ecaceaa3ffbbccf6d9051cedf0b5d
Voldoned <vova.nederkin@gmail.com>
2024-01-14 20:52:45 +0300

    [FIX] Fix names of tests and variables

--- test/conservation/mixture_moles_amount_is_components_moles_amounts_sum_test.py
+++ test/conservation/mixture_moles_amount_is_components_moles_amounts_sum_test.py
@@ -18,22 +18,22 @@ def test_args_fixture():
     return Args(nu1=nu1, nu2=nu2)
 
 
-def test_basic_superposition(test_args):
+def test_basic_amount_of_moles(test_args):
     result = sum_of_moles_law.calculate_moles_count_of_mixture([test_args.nu1, test_args.nu2])
     assert SI.get_dimension_system().equivalent_dims(result.dimension, units.amount_of_substance)
-    result_mass = convert_to(result, units.moles).evalf(3)
-    assert result_mass == approx(30, 0.001)
+    result_amount_of_moles = convert_to(result, units.moles).evalf(3)
+    assert result_amount_of_moles == approx(30, 0.001)
 
 
 def test_three_masses_array(test_args):
     m3 = Quantity(5 * units.moles)
     result = sum_of_moles_law.calculate_moles_count_of_mixture([test_args.nu1, test_args.nu2, m3])
     assert SI.get_dimension_system().equivalent_dims(result.dimension, units.amount_of_substance)
-    result_mass = convert_to(result, units.moles).evalf(3)
-    assert result_mass == approx(35, 0.001)
+    result_amount_of_moles = convert_to(result, units.moles).evalf(3)
+    assert result_amount_of_moles == approx(35, 0.001)
 
 
-def test_bad_moles_count(test_args):
+def test_bad_amount_of_moles(test_args):
     nu_b = Quantity(1 * units.meter)
     with raises(errors.UnitsError):
         sum_of_moles_law.calculate_moles_count_of_mixture([nu_b, test_args.nu2])
