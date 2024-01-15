9af5668cd1484725733c547dfc747c9df6a44571
Voldoned <vova.nederkin@gmail.com>
2024-01-16 01:56:17 +0300

    [CREATE] Create class 'Fraction'

---
+++ symplyphysics/core/symbols/fraction.py
@@ -0,0 +1,9 @@
+from typing import Self
+
+
+class Fraction(float):
+
+    def __new__(cls, value: float) -> Self:
+        if value < 0 or value > 1.0:
+            raise AttributeError(f"Fraction value should be in range [0..1], got '{value}'")
+        return float.__new__(cls, value)
