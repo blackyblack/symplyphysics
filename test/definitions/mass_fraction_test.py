ea325837f3684750a2fa1b4e68af4d3966a1960c
Voldoned <110999247+Voldoned@users.noreply.github.com>
2024-01-16 15:06:59 +0300

    Create definition and test: mass fraction of solute (#216)
    * [CREATE] Create definition and test: mass fraction of solute
    
    * [UPDATE] Rename files. Update description. Fix return result in 'calculate_mass_fraction' function. Fix test and delete imports.
    
    * [CREATE] Create class 'Fraction'
    
    * [ADD] Add empty line in end of file
    
    * [UPDATE] Use class 'Fraction'. Rename test.
    
    * [CREATE] Create test case: if 'fraction_value < 0' or 'fraction_value > 1'

---
+++ test/definitions/mass_fraction_test.py
@@ -0,0 +1,44 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    Quantity,
+)
+from symplyphysics.definitions import mass_fraction as mass_fraction_law
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    m_i = Quantity(1 * units.kilogram)
+    m = Quantity(3 * units.kilogram)
+    Args = namedtuple("Args", ["m_i", "m"])
+    return Args(m_i=m_i, m=m)
+
+
+def test_basic_mass_fraction(test_args):
+    result = mass_fraction_law.calculate_mass_fraction(test_args.m_i, test_args.m)
+    assert result == approx(0.3333, 0.01)
+
+
+def test_bad_mass_fraction(test_args):
+    with raises(AttributeError):
+        mass_fraction_law.calculate_mass_fraction(test_args.m, test_args.m_i)
+    with raises(AttributeError):
+        mass_fraction_law.calculate_mass_fraction((-1) * test_args.m_i, test_args.m)
+
+
+def test_bad_mass_of_component(test_args):
+    mb = Quantity(1 * units.meter)
+    with raises(errors.UnitsError):
+        mass_fraction_law.calculate_mass_fraction(mb, test_args.m)
+    with raises(TypeError):
+        mass_fraction_law.calculate_mass_fraction(100, test_args.m)
+
+
+def test_bad_mass_of_mixture(test_args):
+    mb = Quantity(1 * units.meter)
+    with raises(errors.UnitsError):
+        mass_fraction_law.calculate_mass_fraction(test_args.m_i, mb)
+    with raises(TypeError):
+        mass_fraction_law.calculate_mass_fraction(test_args.m_i, 100)
