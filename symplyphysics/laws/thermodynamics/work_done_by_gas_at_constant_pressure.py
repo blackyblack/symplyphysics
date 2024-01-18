3f7100fe90ffc665b746df48c0788a6adb27926f
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2024-01-18 17:48:09 +0500

    Update work_done_by_gas_at_constant_pressure.py

--- symplyphysics/laws/thermodynamics/work_done_by_gas_at_constant_pressure.py
+++ symplyphysics/laws/thermodynamics/work_done_by_gas_at_constant_pressure.py
@@ -1,22 +1,26 @@
-from sympy import (Eq, solve, integrate)
+from sympy import Eq, solve
 from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
                            validate_output)
 
 # Description
-# Common formula for work done by gas is A = ∫p * dV
-# A is work done
-# p is pressure
-# dV is change in volume
+# Common formula for work done by gas is A = ∫p * dV, where
+# A is work done,
+# p is pressure,
+# dV is change in volume.
+# For process with constant pressure the equation can be transformed to
+# A = p * (V2 - V1), where
+# V1 is initial volume,
+# V2 is resulting volume,
+# p is pressure,
+# A is work done.
 
 
 pressure = Symbol("pressure", units.pressure)
-volume = Symbol("volume", units.volume)
 init_volume = Symbol("init_volume", units.volume)
 result_volume = Symbol("result_volume", units.volume)
 work = Symbol("work", units.energy)
 
-common_law = integrate(pressure, (volume, init_volume, result_volume))
-law = Eq(work, common_law)
+law = Eq(work, pressure * (result_volume - init_volume))
 
 
 def print_law():
