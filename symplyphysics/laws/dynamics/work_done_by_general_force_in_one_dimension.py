689819a572dd1da5900e9b51c66a2264f17f43e0
Aleksandr Slatov <alexslatov@gmail.com>
2024-01-19 15:21:37 +0400

    Add limits of integration to law and fix names

--- symplyphysics/laws/dynamics/work_done_by_general_force_in_one_dimension.py
+++ symplyphysics/laws/dynamics/work_done_by_general_force_in_one_dimension.py
@@ -1,4 +1,4 @@
-from sympy import Eq, Integral, symbols, solve
+from sympy import Eq, Integral
 from symplyphysics import (
     units,
     Quantity,
@@ -15,16 +15,19 @@ from symplyphysics import (
 ## from one position to another is to be found by integrating the force along the path of the
 ## object.
 
-# Law: W = Integral(F(x), x)
+# Law: W = Integral(F(x), (x, x_start, x_end))
 ## W - work done by force F
 ## F = F(x) - force in question
 ## x - position of the object on the axis
+## x_start, x_end - starting and ending positions of the object during its movement, respectively
 
 work = Symbol("work", units.energy)
 force = Function("force", units.force)
 position = Symbol("position", units.length)
+position_start = Symbol("position_start", units.length)
+position_end = Symbol("position_end", units.length)
 
-law = Eq(work, Integral(force(position), position))
+law = Eq(work, Integral(force(position), (position, position_start, position_end)))
 
 
 def print_law() -> str:
@@ -33,28 +36,29 @@ def print_law() -> str:
 
 # Assuming the force changes linearly with respect to position
 @validate_input(
-    force_before_=force,
-    force_after_=force,
-    position_before_=position,
-    position_after_=position,
+    force_start_=force,
+    force_end_=force,
+    position_start_=position,
+    position_end_=position,
 )
 @validate_output(work)
 def calculate_work(
-    force_before_: Quantity,
-    force_after_: Quantity,
-    position_before_: Quantity,
-    position_after_: Quantity,
+    force_start_: Quantity,
+    force_end_: Quantity,
+    position_start_: Quantity,
+    position_end_: Quantity,
 ) -> Quantity:
-    a, b = symbols("a b")
-    solved = solve(
-        [
-            Eq(force_before_, a * position_before_ + b),
-            Eq(force_after_, a * position_after_ + b),
-        ],
-        (a, b),
-        dict=True,
-    )[0]
-    force_function = solved[a] * position + solved[b]
-    expr = law.rhs.subs(force(position), force_function).doit()
-    value = expr.subs(position, position_after_) - expr.subs(position, position_before_)
-    return Quantity(value)
+    # Using the two-point line equation: (y - y1)/(y2 - y1) = (x - x1)/(x2 - x1)
+    force_function = (
+        (force_end_ - force_start_)
+        * (position - position_start_)
+        / (position_end - position_start_)
+        + force_start_
+    )
+    result = law.rhs.subs({
+        force(position): force_function,
+        position_start: position_start_,
+        position_end: position_end_,
+    })
+    result_work = result.doit()
+    return Quantity(result_work)
