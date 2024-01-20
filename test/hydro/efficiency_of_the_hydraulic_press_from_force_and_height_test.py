3cd1ad49c2008839cff51acb907079ad30c1c8d2
Darya2909 <39147553+Darya2909@users.noreply.github.com>
2024-01-20 02:06:36 +0200

    Update efficiency_of_the_hydraulic_press_from_force_and_height_test.py

--- test/hydro/efficiency_of_the_hydraulic_press_from_force_and_height_test.py
+++ test/hydro/efficiency_of_the_hydraulic_press_from_force_and_height_test.py
@@ -28,7 +28,7 @@ def test_args_fixture():
 
 
 def test_basic_efficiency(test_args):
-    resultt_volume = efficiency.calculate_efficiency(test_args.useful_force, test_args.useful_height, test_args.expended_force, test_args.expended_height)
+    result_volume = efficiency.calculate_efficiency(test_args.useful_force, test_args.useful_height, test_args.expended_force, test_args.expended_height)
     assert result_volume == approx(0.95, 0.001)
 
 
