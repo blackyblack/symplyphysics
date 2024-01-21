61b0592b2ab09f411c36018efb6d73095d4653f5
Aleksandr Slatov <alexslatov@gmail.com>
2024-01-21 12:21:14 +0400

    Create law and test for torque vector

---
+++ symplyphysics/laws/dynamics/vector/torque_vector_of_twisting_force.py
@@ -0,0 +1,30 @@
+from symplyphysics import (
+    units,
+    QuantityVector,
+    Vector,
+    cross_cartesian_vectors,
+    validate_input,
+    validate_output,
+    list_of_quantities,
+)
+
+# Description
+## Torque is a turning or twisting action on a body about a rotation axis due to a force.
+## It is defined as a cross product of the force vector and the position vector of the point
+## of force application.
+
+# Law: tau = [F, r]
+## tau - torque vector
+## F - force vector
+## r - position vector of the point of force applicaton
+
+
+def torque_definition(force_: Vector, position_: Vector) -> Vector:
+    return cross_cartesian_vectors(force_, position_)
+
+
+@validate_input(force_=units.force, position_=units.length)
+@validate_output(units.force * units.length)
+def calculate_torque(force_: QuantityVector, position_: QuantityVector) -> QuantityVector:
+    torque_vector = torque_definition(force_, position_)
+    return QuantityVector(torque_vector.components, force_.coordinate_system)
---
+++ test/dynamics/vector/torque_vector_of_twisting_force_test.py
@@ -0,0 +1,73 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    convert_to,
+    Quantity,
+    SI,
+    QuantityVector
+)
+from symplyphysics.laws.dynamics.vector import torque_vector_of_twisting_force as torque_def
+
+# Description
+## A force (1, -2, -1) N is applied at a point (0, 2, -3) m. The torque applied
+## should amount to (8, 3, 2) N*m.
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    F = QuantityVector([
+        Quantity(1.0 * units.newton),
+        Quantity(-2.0 * units.newton),
+        Quantity(-1.0 * units.newton)
+    ])
+    r = QuantityVector([
+        Quantity(0.0 * units.meter),
+        Quantity(2.0 * units.meter),
+        Quantity(-3.0 * units.meter),
+    ])
+    Args = namedtuple("Args", "F r")
+    return Args(F=F, r=r)
+
+
+def test_basic_law(test_args):
+    result = torque_def.calculate_torque(test_args.F, test_args.r)
+    for (result_component, correct_value) in zip(result.components, [8, 3, 2]):
+        assert SI.get_dimension_system().equivalent_dims(
+            result_component.dimension, units.force * units.length
+        )
+        result_value = convert_to(result_component, units.newton * units.meter).evalf(3)
+        assert result_value == approx(correct_value, 1e-3)
+
+
+def test_bad_force(test_args):
+    # Fb is vector, but not in the correct dimension
+    with raises(errors.UnitsError):
+        Fb = QuantityVector([
+            Quantity(1.0 * units.second),
+            Quantity(-2.0 * units.second),
+            Quantity(-1.0 * units.second)
+        ])
+        torque_def.calculate_torque(Fb, test_args.r)
+    # Fb is in the dimension of a force, but not a vector
+    with raises(AttributeError):
+        Fb = Quantity(1.0 * units.newton)
+        torque_def.calculate_torque(Fb, test_args.r)
+    with raises(TypeError):
+        torque_def.calculate_torque(100, test_args.r)
+
+
+def test_bad_distance(test_args):
+    with raises(errors.UnitsError):
+        rb = QuantityVector([
+            Quantity(1.0 * units.second),
+            Quantity(-2.0 * units.second),
+            Quantity(-1.0 * units.second)
+        ])
+        torque_def.calculate_torque(test_args.F, rb)
+    with raises(AttributeError):
+        rb = Quantity(1.0 * units.meter)
+        torque_def.calculate_torque(test_args.F, rb)
+    with raises(TypeError):
+        torque_def.calculate_torque(test_args.F, 100)
