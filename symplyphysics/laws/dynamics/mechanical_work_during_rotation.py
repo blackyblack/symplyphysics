54d130f4106ba647ca6d6ce1841b961662d9454c
Aleksandr Slatov <alexslatov@gmail.com>
2024-01-22 14:17:49 +0400

    Derive law from linear counterpart

--- symplyphysics/laws/dynamics/mechanical_work_during_rotation.py
+++ symplyphysics/laws/dynamics/mechanical_work_during_rotation.py
@@ -1,4 +1,4 @@
-from sympy import Eq
+from sympy import Eq, sin, solve
 from symplyphysics import (
     units,
     Quantity,
@@ -8,6 +8,10 @@ from symplyphysics import (
     validate_output,
     angle_type,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.dynamics import mechanical_work_from_force_and_move as linear_work_law
+from symplyphysics.laws.kinematic import angular_position_is_arc_length_over_radius as angular_position_def
+from symplyphysics.laws.dynamics import torque_due_to_twisting_force as torque_def
 
 # Description
 ## When a torque accelerates a rigid body in rotation about a fixed axis, the torque does work on
@@ -24,6 +28,50 @@ torque = Symbol("torque", units.force * units.length)
 angular_displacement = Symbol("angular_displacement", angle_type)
 
 law = Eq(work, torque * angular_displacement)
+
+
+# Derive law from non-rotational counterpart.
+## We assume a particle moving along a curved trajectory due to a force F applied to it.
+## The displacement of the particle should be small enough so that the position vector
+## is approximately the same at all points of the trajectory. In that case, the force vector
+## is assumed to form an angle phi with the position vector of the particle. Since only the
+## component of the force vector that is tangent to the path is the one accelerating the
+## particle, it does work on the particle.
+
+force = Symbol("force", units.force)
+angle = Symbol("angle", angle_type)
+radius = Symbol("radius", units.length)
+
+tangent_force = force * sin(angle)
+
+distance_traveled = solve(
+    angular_position_def.law,
+    angular_position_def.arc_length,
+)[0].subs({
+    angular_position_def.angular_position: angular_displacement,
+    angular_position_def.path_radius: radius,
+})
+
+work_derived = linear_work_law.law.rhs.subs({
+    linear_work_law.force: tangent_force,
+    linear_work_law.distance: distance_traveled,
+})
+
+torque_def_sub = torque_def.law.subs({
+    torque_def.torque: torque,
+    torque_def.force: force,
+    torque_def.distance_to_axis: radius,
+    torque_def.angle: angle,
+})
+work_derived_sub = solve(
+    [Eq(work, work_derived), torque_def_sub],
+    (radius, work),
+    dict=True
+)[0][work]
+
+work_from_law = law.rhs
+
+assert expr_equals(work_derived_sub, work_from_law)
 
 
 def print_law() -> str:
