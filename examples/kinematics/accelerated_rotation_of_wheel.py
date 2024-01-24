956127a1019c33e0c00c9e6c0c7551fae6e73100
Aleksandr Slatov <alexslatov@gmail.com>
2024-01-24 17:20:22 +0400

    Fix time derivatives and add law reference

--- examples/kinematics/accelerated_rotation_of_wheel.py
+++ examples/kinematics/accelerated_rotation_of_wheel.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-from sympy import solve, symbols, sqrt
-from symplyphysics import units, print_expression, Quantity, convert_to
+from sympy import solve, symbols
+from symplyphysics import (
+    units,
+    print_expression,
+    Quantity,
+    convert_to,
+    vector_magnitude,
+    Vector,
+)
 from symplyphysics.definitions import (
     angular_velocity_is_angle_derivative as angular_velocity_def,
     angular_acceleration_is_angular_velocity_derivative as angular_acceleration_def,
@@ -11,13 +18,14 @@ from symplyphysics.laws.kinematic import (
     tangential_acceleration_of_rotating_body as tangential_acceleration_law,
     centripetal_acceleration_is_squared_velocity_by_radius as centripetal_acceleration_law,
 )
+from symplyphysics.laws.kinematic.vector import acceleration_of_rotating_body as total_acceleration_law
 
 # Description
 ## A wheel is rotating about a fixed axis so that the angular displacement is expressed as k*t^2, where
 ## k = 0.2 rad/s^2. Find the total acceleration of the point on the wheel at the moment t = 2.5 s
 ## if the linear velocity of the point is 0.65 m/s at that time.
 
-time = symbols("time", positive=True)
+time = symbols("_time_", positive=True)
 factor = symbols("factor", real=True)
 linear_velocity = symbols("linear_velocity", real=True)
 
@@ -27,27 +35,24 @@ values = {
     linear_velocity: Quantity(0.65 * units.meter / units.second),
 }
 
-angular_position = lambda t: factor * t**2
+angular_velocity_eqn = angular_velocity_def.definition.subs(angular_velocity_def.time, time)
+angular_acceleration_eqn = angular_acceleration_def.definition.subs(angular_acceleration_def.time, time)
 
-angular_velocity = angular_velocity_def.definition.rhs.subs(
-    angular_velocity_def.angle_function(angular_velocity_def.time),
-    angular_position(angular_velocity_def.time),
-).doit().subs(
-    angular_velocity_def.time,
-    angular_acceleration_def.time,
-)
+angular_position = factor * time**2
 
-angular_acceleration = angular_acceleration_def.definition.rhs.subs(
-    angular_acceleration_def.angular_velocity(angular_acceleration_def.time),
+angular_velocity = angular_velocity_eqn.rhs.subs(
+    angular_velocity_def.angle_function(time),
+    angular_position,
+).doit()
+
+angular_acceleration = angular_acceleration_eqn.rhs.subs(
+    angular_acceleration_def.angular_velocity(time),
     angular_velocity,
 ).doit()
 
-angular_velocity = angular_velocity.subs(angular_acceleration_def.time, time)
-angular_acceleration = angular_acceleration.subs(angular_acceleration_def.time, time)
-
 rotation_radius = solve(
-    linear_velocity_law.law, 
-    linear_velocity_law.curve_radius
+    linear_velocity_law.law,
+    linear_velocity_law.curve_radius,
 )[0].subs({
     linear_velocity_law.linear_velocity: linear_velocity,
     linear_velocity_law.angular_velocity: angular_velocity,
@@ -63,7 +68,14 @@ centripetal_acceleration = centripetal_acceleration_law.law.rhs.subs({
     centripetal_acceleration_law.curve_radius: rotation_radius,
 })
 
-total_acceleration = sqrt(tangential_acceleration**2 + centripetal_acceleration**2).simplify()
+# Tangential and centripetal accelerations are perpendicular to one another. Therefore, we can
+# align the tangential acceleration with the x axis, and the centripetal one with the y axis.
+total_acceleration = vector_magnitude(
+    total_acceleration_law.acceleration_law(
+        Vector([tangential_acceleration, 0]),
+        Vector([0, centripetal_acceleration]),
+    )
+).simplify()
 total_acceleration_value = convert_to(
     Quantity(total_acceleration.subs(values)),
     units.meter / units.second**2,
