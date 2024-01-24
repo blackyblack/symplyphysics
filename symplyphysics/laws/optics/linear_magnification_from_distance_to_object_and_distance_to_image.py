e85510313f12ab7219228151bd7b971dfb5d9e10
Darya2909 <39147553+Darya2909@users.noreply.github.com>
2024-01-24 16:47:49 +0200

    linear_magnification_from_distance_to_object_and_distance_to_image (#268)
    * Test task. (#134)
    
    lens maker's equation
    
    * pressure from force and area
    
    * refactor
    
    * body weight in liquid
    
    * remove conflict
    
    * refactor
    
    * hydraulic_press
    
    * refactor
    
    * rename and add proof
    
    * add proof in code
    
    * refactor proof
    
    * add assert
    
    * volume_of_the_submerged_body_part_from_body_density_and_liquid_densit
    
    * delete conflict
    
    * refactor
    
    * add comment
    
    * edit
    
    * add condition in law file and refactor test
    
    * add assert and comments
    
    * the_efficiency_of_the_hydraulic_press_from_force_and_height
    
    * delete conflict
    
    * add changes
    
    * refactor exeption
    
    * edit
    
    * add test
    
    * add changes
    
    * delete sympify
    
    * add changes
    
    * edit
    
    * Update volume_of_heated_liquid_from_initial_volume_and_heating_temperature.py
    
    * add changes
    
    * refactor comments
    
    * linear_magnification_from_object_height_and_image_height
    
    * Delete symplyphysics/laws/thermodynamics/volume_of_heated_body_from_initial_volume_and_heating_temperature.py
    
    * refactor
    
    * delete
    
    * linear_magnification_from_distance_to_object_and_distance_to_image
    
    * refactor
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image_test.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image_test.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image_test.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update symplyphysics/laws/optics/linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    Co-authored-by: blackyblack <johnmalkovich@mail.ru>
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image_test.py
    
    ---------
    
    Co-authored-by: blackyblack <johnmalkovich@mail.ru>

---
+++ symplyphysics/laws/optics/linear_magnification_from_distance_to_object_and_distance_to_image.py
@@ -0,0 +1,46 @@
+from sympy import (Eq, solve, S)
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+                           validate_output, dimensionless, convert_to)
+
+# Description
+## Magnification, in optics, the size of an image relative to the size of the object creating it.
+## Depending on the position of the object in relation to the lens, the linear dimensions of the image change.
+## If magnfication is positive, it means image formed is virtual and erect.
+## If magnfication is negative, it means image formed is real and inverted.
+
+
+## Law: M = f / d
+## Where:
+## M is linear magnification produced by lense
+## f is distance from lens to image
+## d is distance from lens to object
+
+## Conditions
+## If virtual image is formed, f is negative.
+## If real image is formed, f is positive.
+## d is always negative as object is on left side of the lens.
+
+
+distance_to_object = Symbol("distance_to_object", units.length)
+distance_to_image = Symbol("distance_to_image", units.length)
+magnification = Symbol("magnification", dimensionless)
+
+law = Eq(magnification, distance_to_image / distance_to_object)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(distance_to_image_=distance_to_image, distance_to_object_=distance_to_object)
+@validate_output(magnification)
+def calculate_magnification(distance_to_image_ : Quantity, distance_to_object_: Quantity) -> float:
+    if distance_to_object_.scale_factor > 0:
+        raise ValueError("The distance to the object must be non-positive.")
+    result_expr = solve(law, magnification, dict=True)[0][magnification]
+    result_magnification = result_expr.subs({
+        distance_to_image: distance_to_image_,
+        distance_to_object: distance_to_object_,
+    })
+    result = Quantity(result_magnification)
+    return float(convert_to(result, S.One).evalf())
---
+++ test/optics/linear_magnification_from_distance_to_object_and_distance_to_image_test.py
@@ -0,0 +1,42 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    Quantity,
+    SI,
+    convert_to,
+    dimensionless
+)
+
+from symplyphysics.laws.optics import linear_magnification_from_distance_to_object_and_distance_to_image as magnification
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    distance_to_image = Quantity(0.175 * units.meter)
+    distance_to_object = Quantity(-0.07 * units.meter)
+    Args = namedtuple("Args", ["distance_to_image", "distance_to_object"])
+    return Args(
+        distance_to_image=distance_to_image,
+        distance_to_object=distance_to_object,
+               )
+
+## The minus sign means that the image is real and inverted.
+def test_basic_magnification(test_args):
+    result = magnification.calculate_magnification(test_args.distance_to_image, test_args.distance_to_object)
+    assert result == approx(-2.5, 0.001)
+
+
+def test_bad_distance(test_args):
+    bad_distance = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        magnification.calculate_magnification(bad_distance, test_args.distance_to_object)
+    with raises(errors.UnitsError):
+        magnification.calculate_magnification(test_args.distance_to_image, bad_distance)
+    with raises(TypeError):
+        magnification.calculate_magnification(100, test_args.distance_to_object)
+    with raises(TypeError):
+        magnification.calculate_magnification(test_args.distance_to_image, 100)
+    with raises(ValueError):
+        magnification.calculate_magnification(test_args.distance_to_image, test_args.distance_to_image)   
