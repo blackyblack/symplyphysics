75f47e34ab19c8ae189edf500d4199b02a87b82c
Darya2909 <39147553+Darya2909@users.noreply.github.com>
2024-01-25 15:16:01 +0200

    surface_charge_density_from_charge_and_area (#288)
    * Test task. (#134)
    
    lens maker's equation
    
    * pressure from force and area
    
    * refactor
    
    * body weight in liquid
    
    * remove conflict
    
    * refactor
    
    * hydraulic_press
    
    * refactor
    
    * rename and add proof
    
    * add proof in code
    
    * refactor proof
    
    * add assert
    
    * volume_of_the_submerged_body_part_from_body_density_and_liquid_densit
    
    * delete conflict
    
    * refactor
    
    * add comment
    
    * edit
    
    * add condition in law file and refactor test
    
    * add assert and comments
    
    * the_efficiency_of_the_hydraulic_press_from_force_and_height
    
    * delete conflict
    
    * add changes
    
    * refactor exeption
    
    * edit
    
    * add test
    
    * add changes
    
    * delete sympify
    
    * add changes
    
    * edit
    
    * Update volume_of_heated_liquid_from_initial_volume_and_heating_temperature.py
    
    * add changes
    
    * refactor comments
    
    * linear_magnification_from_object_height_and_image_height
    
    * Delete symplyphysics/laws/thermodynamics/volume_of_heated_body_from_initial_volume_and_heating_temperature.py
    
    * refactor
    
    * delete
    
    * linear_magnification_from_distance_to_object_and_distance_to_image
    
    * refactor
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image_test.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image_test.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image_test.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update symplyphysics/laws/optics/linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    Co-authored-by: blackyblack <johnmalkovich@mail.ru>
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image.py
    
    * Update linear_magnification_from_distance_to_object_and_distance_to_image_test.py
    
    * surface_charge_density_from_charge_and_area
    
    * Update surface_charge_density_from_charge_and_area.py
    
    ---------
    
    Co-authored-by: blackyblack <johnmalkovich@mail.ru>

---
+++ symplyphysics/definitions/surface_charge_density_from_charge_and_area.py
@@ -0,0 +1,37 @@
+from sympy import (Eq, solve)
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+                           validate_output)
+
+# Description
+## Surface charge density is the amount of charge per unit of a two-dimensional surface area. It is a measure of how much quantity of electric charge is accumulated over a surface.
+## Charge density can be either positive or negative, since electric charge can be either positive or negative.
+
+
+## Definition: σ = q / s
+## Where:
+## σ is surface charge density
+## q is charge
+## s is the area over which charge is distributed
+
+
+surface_charge_density = Symbol("surface_charge_density", units.charge / units.area)
+charge = Symbol("charge", units.charge)
+area = Symbol("area", units.area)
+
+definition = Eq(surface_charge_density, charge / area)
+
+definition_units_SI = units.coulomb / units.meter**2
+
+def print_definition() -> str:
+    return print_expression(definition)
+
+
+@validate_input(charge_=charge, area_=area)
+@validate_output(surface_charge_density)
+def calculate_surface_charge_density(charge_: Quantity, area_: Quantity) -> Quantity:
+    result_expr = solve(definition, surface_charge_density, dict=True)[0][surface_charge_density]
+    result_surface_charge_density = result_expr.subs({
+        charge: charge_,
+        area: area_,
+    })
+    return Quantity(result_surface_charge_density)
