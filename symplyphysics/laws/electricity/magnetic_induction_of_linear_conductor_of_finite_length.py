e80b24bf28e961fccd8b9ccd33d5693f63adfdb4
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-01-25 16:30:29 +0100

    Commit #1. magnetic_induction_of_linear_conductor_of_finite_length (#284)
    * Commit #1. magnetic_induction_of_linear_conductor_of_finite_length
    
    * Commit #2. Add changes

---
+++ symplyphysics/laws/electricity/magnetic_induction_of_linear_conductor_of_finite_length.py
@@ -0,0 +1,65 @@
+from sympy import (Eq, solve, pi, cos)
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    dimensionless,
+    angle_type
+)
+from sympy.physics.units import magnetic_constant
+
+# Description
+## Let there be a rectilinear conductor of finite length. Then its magnetic induction will depend on
+## the magnitude of the current and the material. It also depends on the perpendicular distance to the
+## conductor and on the angles between the lines drawn from the ends of the conductor to the point and
+## the conductor.
+
+## Law is: B = mu * mu0 * I * (cos(a1) + cos(a2)) / (4 * pi * r), where
+## B - induction,
+## mu - relative permeability of medium,
+## mu0 - magnetic constant,
+## I - current,
+## a1 - first angle,
+## a2 - second angle,
+## r - distance.
+
+# Conditions:
+## - Conductor should be rectilinear;
+## - Length of the conductor is finite.
+
+induction = Symbol("induction", units.magnetic_density)
+
+relative_permeability = Symbol("relative_permeability", dimensionless)
+current = Symbol("current", units.current)
+first_angle = Symbol("first_angle", angle_type)
+second_angle = Symbol("second_angle", angle_type)
+distance = Symbol("distance", units.length)
+
+law = Eq(induction, relative_permeability * magnetic_constant * current * (cos(first_angle) + cos(second_angle)) / (4 * pi * distance))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(relative_permeability_=relative_permeability,
+    current_=current,
+    first_angle_=first_angle,
+    second_angle_=second_angle,
+    distance_=distance)
+@validate_output(induction)
+def calculate_induction(relative_permeability_: float, current_: Quantity,
+    first_angle_: float | Quantity, second_angle_: float | Quantity,
+    distance_: Quantity) -> Quantity:
+    result_expr = solve(law, induction, dict=True)[0][induction]
+    result_expr = result_expr.subs({
+        relative_permeability: relative_permeability_,
+        current: current_,
+        first_angle: first_angle_,
+        second_angle: second_angle_,
+        distance: distance_
+    })
+    return Quantity(result_expr)
---
+++ test/electricity/magnetic_induction_of_linear_conductor_of_finite_length_test.py
@@ -0,0 +1,105 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from sympy import pi
+from symplyphysics import (units, SI, convert_to, Quantity, errors)
+from symplyphysics.laws.electricity import magnetic_induction_of_linear_conductor_of_finite_length as induction_law
+from sympy.physics.units import prefixes
+
+# Description
+## Let the current in the conductor be 1 ampere, the distance from the conductor is 2 meter and the
+## magnetic permeability of the medium is 10. Then, at the first angle of 45 degree (pi / 4 radian)
+## and the second angle of 60 degree (pi / 3 radian), the magnetic induction will be equal to 603 nanotesla.
+## https://www.indigomath.ru//raschety/EFaosd.html
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    relative_permeability = 10
+    current = Quantity(1 * units.ampere)
+    first_angle = pi / 4
+    second_angle = pi /3
+    distance = Quantity(2 * units.meter)
+
+    Args = namedtuple("Args", [
+        "relative_permeability", "current", "first_angle", "second_angle",
+        "distance"
+    ])
+    return Args(relative_permeability=relative_permeability,
+        current=current,
+        first_angle=first_angle,
+        second_angle=second_angle,
+        distance=distance)
+
+
+def test_basic_induction(test_args):
+    result = induction_law.calculate_induction(test_args.relative_permeability,
+        test_args.current, test_args.first_angle, test_args.second_angle,
+        test_args.distance)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.magnetic_density)
+    result = convert_to(result, prefixes.nano * units.tesla).evalf(5)
+    assert result == approx(603, rel=0.01)
+
+
+def test_swap_angle(test_args):
+    result = induction_law.calculate_induction(test_args.relative_permeability,
+        test_args.current, test_args.second_angle, test_args.first_angle,
+        test_args.distance)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.magnetic_density)
+    result = convert_to(result, prefixes.nano * units.tesla).evalf(5)
+    assert result == approx(603, rel=0.01)
+
+
+def test_bad_relative_permeability(test_args):
+    relative_permeability = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        induction_law.calculate_induction(relative_permeability,
+            test_args.current, test_args.first_angle, test_args.second_angle,
+            test_args.distance)
+    with raises(TypeError):
+        induction_law.calculate_induction(True, test_args.current,
+            test_args.first_angle, test_args.second_angle, test_args.distance)
+
+
+def test_bad_current(test_args):
+    current = Quantity(1 * units.coulomb)
+    first_angle = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        induction_law.calculate_induction(test_args.relative_permeability,
+            current, test_args.first_angle, test_args.second_angle,
+            test_args.distance)
+    with raises(TypeError):
+        induction_law.calculate_induction(test_args.relative_permeability, 100,
+            test_args.first_angle, test_args.second_angle, test_args.distance)
+
+
+def test_bad_angle(test_args):
+    first_angle = Quantity(1 * units.coulomb)
+    second_angle = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        induction_law.calculate_induction(test_args.relative_permeability,
+            test_args.current, first_angle, test_args.second_angle,
+            test_args.distance)
+    with raises(AttributeError):
+        induction_law.calculate_induction(test_args.relative_permeability,
+            test_args.current, True, test_args.second_angle,
+            test_args.distance)
+    with raises(errors.UnitsError):
+        induction_law.calculate_induction(test_args.relative_permeability,
+            test_args.current, test_args.first_angle, second_angle,
+            test_args.distance)
+    with raises(AttributeError):
+        induction_law.calculate_induction(test_args.relative_permeability,
+            test_args.current, test_args.first_angle, True,
+            test_args.distance)
+
+
+def test_bad_distance(test_args):
+    distance = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        induction_law.calculate_induction(test_args.relative_permeability,
+            test_args.current, test_args.first_angle, test_args.second_angle,
+            distance)
+    with raises(TypeError):
+        induction_law.calculate_induction(test_args.relative_permeability,
+            test_args.current, test_args.first_angle, test_args.second_angle,
+            100)
