5d01d3845f98cacbbdf79a2ad3e12072b51592c6
Ростислав Житков <rosjit1@gmail.com>
2024-01-26 16:00:08 +0300

    Commit #2. Add proof

--- symplyphysics/laws/electricity/radius_of_a_charged_particle_in_a_magnetic_field.py
+++ symplyphysics/laws/electricity/radius_of_a_charged_particle_in_a_magnetic_field.py
@@ -1,4 +1,4 @@
-from sympy import (Eq, solve)
+from sympy import (Eq, solve, pi)
 from symplyphysics import (
     units,
     Quantity,
@@ -7,6 +7,8 @@ from symplyphysics import (
     validate_input,
     validate_output,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.electricity import period_of_a_charged_particle_in_a_magnetic_field as period_law
 
 # Description
 ## Let an arbitrary particle move in a magnetic field around a circle. Then radius of its motion depends on
@@ -27,6 +29,23 @@ charge = Symbol("charge", units.charge)
 induction = Symbol("induction", units.magnetic_density)
 
 law = Eq(radius, mass * velocity / (charge * induction))
+
+# This law might be derived via period of a charged particle in a magnetic field.
+
+#TODO:
+## The following law should be added:
+## T = 2 * pi * radius / velocity
+
+law_applied = period_law.law.subs({
+    period_law.mass: mass,
+    period_law.charge: charge,
+    period_law.induction: induction,
+    period_law.period: 2 * pi * radius / velocity,
+})
+radius_derived = solve(law_applied, radius, dict=True)[0][radius]
+
+# Check if derived radius is same as declared.
+assert expr_equals(radius_derived, law.rhs)
 
 
 def print_law() -> str:
