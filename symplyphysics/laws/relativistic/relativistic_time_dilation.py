05180b6cb46a81bee6e50ce6ae94aa8a5ea37f00
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2024-01-27 14:59:55 +0500

    relativistic time dilation fix01

--- symplyphysics/laws/relativistic/relativistic_time_dilation.py
+++ symplyphysics/laws/relativistic/relativistic_time_dilation.py
@@ -7,18 +7,20 @@ from symplyphysics import (Quantity, Symbol, print_expression, units, validate_i
 
 # Description
 # Time dilation is the difference in elapsed time as measured by two clocks,
-# either due to a relative velocity between them
-# Law: t_rel = t * sqrt(1 - v**2 / c**2), where
-# t_rel is time elapsed for moving observer,
-# t is is time elapsed for rest observer,
+# either due to a relative velocity between them (special relativity),
+# or a difference in gravitational potential between their locations
+# (general relativity).
+# Law: t_rel = t / sqrt(1 - v**2 / c**2), where
+# t_rel is time that has passed as measured by a stationary observer (relative time),
+# t is time that has passed as measured by the traveling observer,
 # v is velocity,
 # c is speed of light.
 
-rest_time = Symbol("rest_time", units.time)
+moving_observer_time = Symbol("moving_observer_time", units.time)
 velocity = Symbol("velocity", units.velocity)
 relativistic_time = Symbol("relativistic_time", units.time)
 
-law = Eq(relativistic_time, rest_time /
+law = Eq(relativistic_time, moving_observer_time /
          sqrt(1 - velocity**2 / speed_of_light**2))
 
 
@@ -26,11 +28,11 @@ def print_law() -> str:
     return print_expression(law)
 
 
-@validate_input(rest_time_=rest_time, velocity_=velocity)
+@validate_input(moving_observer_time_=moving_observer_time, velocity_=velocity)
 @validate_output(relativistic_time)
-def calculate_relativistic_time(rest_time_: Quantity, velocity_: Quantity) -> Quantity:
+def calculate_relativistic_time(moving_observer_time_: Quantity, velocity_: Quantity) -> Quantity:
     result_expr = solve(law, relativistic_time, dict=True)[
         0][relativistic_time]
     time_applied = result_expr.subs(
-        {rest_time: rest_time_, velocity: velocity_})
+        {moving_observer_time: moving_observer_time_, velocity: velocity_})
     return Quantity(time_applied)
--- test/relativistic/relativistic_time_dilation_test.py
+++ test/relativistic/relativistic_time_dilation_test.py
@@ -10,38 +10,38 @@ from symplyphysics import (
 from symplyphysics.laws.relativistic import relativistic_time_dilation
 
 
-# Using calculations from the paper: https://www.omnicalculator.com/physics/time-dilation
+# Using calculations from the paper: https://www.omnicalculator.com/physics/proper_time-dilation
 
 
 @fixture(name="test_args")
 def test_args_fixture():
-    t0 = Quantity(10 * units.second)
-    v = Quantity(200_000_000 * (units.meter / units.second))
-    Args = namedtuple("Args", ["t0", "v"])
-    return Args(t0=t0, v=v)
+    proper_time = Quantity(10 * units.second)
+    velocity = Quantity(200_000_000 * (units.meter / units.second))
+    Args = namedtuple("Args", ["proper_time", "velocity"])
+    return Args(proper_time=proper_time, velocity=velocity)
 
 
 def test_basic_time(test_args):
     result = relativistic_time_dilation.calculate_relativistic_time(
-        test_args.t0, test_args.v)
+        test_args.proper_time, test_args.velocity)
     assert SI.get_dimension_system().equivalent_dims(result.dimension, units.time)
-    result_time = convert_to(result, units.time).evalf(4)
-    assert result_time == approx(13.423, 0.01)
+    result_proper_time = convert_to(result, units.time).evalf(4)
+    assert result_proper_time == approx(13.423, 0.01)
 
 
 def test_bad_time(test_args):
     bt = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        relativistic_time_dilation.calculate_relativistic_time(bt, test_args.v)
+        relativistic_time_dilation.calculate_relativistic_time(bt, test_args.velocity)
     with raises(TypeError):
-        relativistic_time_dilation.calculate_relativistic_time(1, test_args.v)
+        relativistic_time_dilation.calculate_relativistic_time(1, test_args.velocity)
 
 
 def test_bad_velocity(test_args):
     bv = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
         relativistic_time_dilation.calculate_relativistic_time(
-            test_args.t0, bv)
+            test_args.proper_time, bv)
     with raises(TypeError):
         relativistic_time_dilation.calculate_relativistic_time(
-            test_args.t0, 100)
+            test_args.proper_time, 100)
