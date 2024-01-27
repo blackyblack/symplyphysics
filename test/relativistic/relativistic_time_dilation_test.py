d0df77f18ede17061d782ba2553158bf6130c3d5
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2024-01-27 16:54:46 +0500

    Update relativistic_time_dilation_test.py

--- test/relativistic/relativistic_time_dilation_test.py
+++ test/relativistic/relativistic_time_dilation_test.py
@@ -10,23 +10,23 @@ from symplyphysics import (
 from symplyphysics.laws.relativistic import relativistic_time_dilation
 
 
-# Using calculations from the paper: https://www.omnicalculator.com/physics/proper_time-dilation
+# Using calculations from the paper: https://www.omnicalculator.com/physics/time-dilation
 
 
 @fixture(name="test_args")
 def test_args_fixture():
-    proper_time = Quantity(10 * units.second)
+    own_time = Quantity(10 * units.second)
     velocity = Quantity(200_000_000 * (units.meter / units.second))
-    Args = namedtuple("Args", ["proper_time", "velocity"])
-    return Args(proper_time=proper_time, velocity=velocity)
+    Args = namedtuple("Args", ["own_time", "velocity"])
+    return Args(own_time=own_time, velocity=velocity)
 
 
 def test_basic_time(test_args):
     result = relativistic_time_dilation.calculate_relativistic_time(
-        test_args.proper_time, test_args.velocity)
+        test_args.own_time, test_args.velocity)
     assert SI.get_dimension_system().equivalent_dims(result.dimension, units.time)
-    result_proper_time = convert_to(result, units.time).evalf(4)
-    assert result_proper_time == approx(13.423, 0.01)
+    result_own_time = convert_to(result, units.second).evalf(4)
+    assert result_own_time == approx(13.423, 0.01)
 
 
 def test_bad_time(test_args):
@@ -41,7 +41,7 @@ def test_bad_velocity(test_args):
     bv = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
         relativistic_time_dilation.calculate_relativistic_time(
-            test_args.proper_time, bv)
+            test_args.own_time, bv)
     with raises(TypeError):
         relativistic_time_dilation.calculate_relativistic_time(
-            test_args.proper_time, 100)
+            test_args.own_time, 100)
