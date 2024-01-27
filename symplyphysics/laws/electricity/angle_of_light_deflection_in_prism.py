af899502e6f4a65b3c878396a28eb0735596c630
Ростислав Житков <rosjit1@gmail.com>
2024-01-27 16:05:50 +0300

    Commit #2. Add changes

--- symplyphysics/laws/electricity/angle_of_light_deflection_in_prism.py
+++ symplyphysics/laws/electricity/angle_of_light_deflection_in_prism.py
@@ -14,6 +14,10 @@ from symplyphysics import (
 ## bounded by flat reflecting and refractive surfaces located at strictly defined angles to each other.
 ## With a small angle of incidence of the incoming beam, the angle of deflection of the beam depends only
 ## on the angle between the faces of the prism and the refractive index of the prism.
+## There is a ray falling on the prism and a ray coming out of the prism. Let's continue these rays inside
+## the prism. Then the angle of intersection of these rays, looking towards the output beam, will be called
+## the angle of deviation.
+## https://ru.wikipedia.org/wiki/Призма_(оптика)#:~:text=Призма%2C%20оптическая%20призма%20—%20тело%20из,определёнными%20углами%20друг%20к%20другу.
 
 ## Law is: b = a * (n - 1), where
 ## b - angle deviation,
@@ -34,7 +38,7 @@ def print_law() -> str:
 
 @validate_input(angle_faces_=angle_faces, refractive_index_=refractive_index)
 @validate_output(angle_deviation)
-def calculate_angle_deviation(angle_faces_: float | Quantity, refractive_index_: float) -> float | Quantity:
+def calculate_angle_deviation(angle_faces_: float | Quantity, refractive_index_: float) -> Quantity:
     result_expr = solve(law, angle_deviation, dict=True)[0][angle_deviation]
     result_expr = result_expr.subs({
         angle_faces: angle_faces_,
--- test/electricity/angle_of_light_deflection_in_prism_test.py
+++ test/electricity/angle_of_light_deflection_in_prism_test.py
@@ -1,18 +1,19 @@
 from collections import namedtuple
 from pytest import approx, fixture, raises
+from sympy import pi
 from symplyphysics import (units, SI, convert_to, Quantity, errors, dimensionless,)
 from symplyphysics.laws.electricity import angle_of_light_deflection_in_prism as angle_law
 
 # Description
-## Consider a prism with an angle between the faces equal to 45 degree and a refractive index of 15.
-## The angle of deviation will be 630 degrees.
+## Consider a prism with an angle between the faces equal to 45 degree (pi / 4 radian) and a refractive index of 2.5.
+## The angle of deviation will be 67.5 degree (1.1781 radian).
 ## https://www.indigomath.ru//raschety/s3Jncy.html
 
 
 @fixture(name="test_args")
 def test_args_fixture():
-    angle_faces = 45
-    refractive_index = 15
+    angle_faces = pi / 4
+    refractive_index = 2.5
 
     Args = namedtuple("Args", ["angle_faces", "refractive_index"])
     return Args(angle_faces=angle_faces, refractive_index=refractive_index)
@@ -22,14 +23,14 @@ def test_basic_angle_deviation(test_args):
     result = angle_law.calculate_angle_deviation(test_args.angle_faces, test_args.refractive_index)
     assert SI.get_dimension_system().equivalent_dims(result.dimension, dimensionless)
     result = convert_to(result, dimensionless).evalf(5)
-    assert result == approx(630, rel=0.01)
+    assert result == approx(1.1781, rel=0.01)
 
 
 def test_bad_angle_faces(test_args):
     angle_faces = Quantity(1 * units.joule)
     with raises(errors.UnitsError):
         angle_law.calculate_angle_deviation(angle_faces, test_args.refractive_index)
-    with raises(AttributeError):
+    with raises(TypeError):
         angle_law.calculate_angle_deviation(True, test_args.refractive_index)
 
 
@@ -37,5 +38,5 @@ def test_bad_refractive_index(test_args):
     refractive_index = Quantity(1 * units.joule)
     with raises(errors.UnitsError):
         angle_law.calculate_angle_deviation(test_args.angle_faces, refractive_index)
-    with raises(AttributeError):
+    with raises(TypeError):
         angle_law.calculate_angle_deviation(test_args.angle_faces, True)
