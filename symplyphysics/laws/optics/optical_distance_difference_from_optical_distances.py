aa4725eb0f6537edcb3136b3987990ef4ff7ecb0
Voldoned <vova.nederkin@gmail.com>
2024-01-29 17:35:44 +0300

    [FIX] Fix variables names

--- symplyphysics/laws/optics/optical_distance_difference_from_optical_distances.py
+++ symplyphysics/laws/optics/optical_distance_difference_from_optical_distances.py
@@ -12,24 +12,24 @@ from symplyphysics import Symbol, units, print_expression, Quantity, \
 # L1 - optical distance for 1st wave
 # L2 - optical distance for 2nd wave
 
-optical_distance_wave_1 = Symbol("", units.length)
-optical_distance_wave_2 = Symbol("", units.length)
-optical_difference_distance = Symbol("", units.length)
+optical_distance1 = Symbol("optical_distance1", units.length)
+optical_distance2 = Symbol("optical_distance2", units.length)
+optical_difference_distance = Symbol("optical_difference_distance", units.length)
 
-law = Eq(optical_difference_distance, optical_distance_wave_2 - optical_distance_wave_1)
+law = Eq(optical_difference_distance, optical_distance2 - optical_distance1)
 
 
 def print_law() -> str:
     return print_expression(law)
 
 
-@validate_input(optical_distance_wave_1_=optical_distance_wave_1, optical_distance_wave_2_=optical_distance_wave_2)
+@validate_input(optical_distance1_=optical_distance1, optical_distance2_=optical_distance2)
 @validate_output(optical_difference_distance)
-def calculate_optical_difference_distance(optical_distance_wave_1_: Quantity, optical_distance_wave_2_: Quantity) -> Quantity:
+def calculate_optical_difference_distance(optical_distance1_: Quantity, optical_distance2_: Quantity) -> Quantity:
     solved = solve(law, optical_difference_distance, dict=True)[0][optical_difference_distance]
     result_expr = solved.subs({
-        optical_distance_wave_1: optical_distance_wave_1_,
-        optical_distance_wave_2: optical_distance_wave_2_
+        optical_distance1: optical_distance1_,
+        optical_distance2: optical_distance2_
     })
     result = Quantity(result_expr)
     return result
