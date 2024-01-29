27dcbfeb4b0b8bf68cdc46cb410d5f8fe4b03f9d
Aleksandr Slatov <alexslatov@gmail.com>
2024-01-29 09:09:34 +0400

    Create law and test

---
+++ symplyphysics/laws/kinematic/rotational_inertia_about_axis_and_through_com.py
@@ -0,0 +1,58 @@
+from sympy import Eq
+from symplyphysics import (
+    units,
+    Symbol,
+    Quantity,
+    validate_input,
+    validate_output,
+    print_expression,
+)
+
+# Description
+## The parallel-axis theorem relates the rotational inertia of a body about any axis to
+## that of the same body about a parallel axis that extends through the body's com (center
+## of mass).
+
+# Law: I = I_com + M*(h**2)
+## I - rotational inertia about some axis
+## I_com - rotational inertia about such an axis that is parallel to the given one
+##         and that passes through the com (center of mass)
+## M - body mass
+## h - perpendicular distance between the two axes
+
+# Conditions:
+## - The two axis must be parallel to each other.
+## - One of them should pass through the body's com.
+
+rotational_inertia = Symbol("rotational_inertia", units.mass * units.length**2)
+rotational_inertia_through_com = Symbol("rotational_inertia_through_com", units.mass * units.length**2)
+mass = Symbol("mass", units.mass)
+distance_between_axes = Symbol("distance_between_axes", units.length)
+
+law = Eq(
+    rotational_inertia,
+    rotational_inertia_through_com + mass * distance_between_axes**2,
+)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    rotational_inertia_through_com_=rotational_inertia_through_com,
+    mass_=mass,
+    distance_between_axes_=distance_between_axes,
+)
+@validate_output(rotational_inertia)
+def calculate_rotational_inertia(
+    rotational_inertia_through_com_: Quantity,
+    mass_: Quantity,
+    distance_between_axes_: Quantity,
+) -> Quantity:
+    result = law.rhs.subs({
+        rotational_inertia_through_com: rotational_inertia_through_com_,
+        mass: mass_,
+        distance_between_axes: distance_between_axes_,
+    })
+    return Quantity(result)
