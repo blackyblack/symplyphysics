1ddd36c80fe4ff35d61bb078c68d1b41e89eebba
Aleksandr Slatov <alexslatov@gmail.com>
2024-01-29 08:17:15 +0400

    Derive law from general integral

--- symplyphysics/laws/kinematic/rotational_inertia_formulas/solid_disk_about_central_axis.py
+++ symplyphysics/laws/kinematic/rotational_inertia_formulas/solid_disk_about_central_axis.py
@@ -1,4 +1,4 @@
-from sympy import Eq, solve
+from sympy import Eq, pi
 from symplyphysics import (
     units,
     Quantity,
@@ -7,7 +7,9 @@ from symplyphysics import (
     validate_input,
     validate_output,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.laws.kinematic import rotational_inertia_cylindrical_integral as integral_law
+from symplyphysics.definitions import density_from_mass_volume as density_def
 
 # Description
 ## A solid disk (cylinder) rotates about its central axis (axis of cylindrical symmetry).
@@ -22,6 +24,29 @@ mass = Symbol("mass", units.mass)
 radius = Symbol("radius", units.length)
 
 law = Eq(rotational_inertia, mass * radius**2 / 2)
+
+
+# Derive law from general integral in cylindrical coordinates
+
+length = Symbol("length", units.length)
+volume = pi * radius**2 * length
+
+density = density_def.definition.rhs.subs({
+    density_def.mass: mass,
+    density_def.volume: volume,
+})
+
+rotational_inertia_derived = integral_law.law.rhs.subs({
+    integral_law.density(integral_law.radius, integral_law.polar_angle, integral_law.height): density,
+    integral_law.radius_start: 0,
+    integral_law.radius_end: radius,
+    integral_law.polar_angle_start: 0,
+    integral_law.polar_angle_end: 2*pi,
+    integral_law.height_start: 0,
+    integral_law.height_end: length,
+}).doit()
+
+assert expr_equals(rotational_inertia_derived, law.rhs)
 
 
 def print_law() -> str:
