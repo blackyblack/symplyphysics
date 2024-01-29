8d2e5716587160b881b44779dc59991a2594bc79
Aleksandr Slatov <alexslatov@gmail.com>
2024-01-29 06:43:30 +0400

    Yet another mypy fix

--- symplyphysics/laws/dynamics/vector/force_is_derivative_of_momentum.py
+++ symplyphysics/laws/dynamics/vector/force_is_derivative_of_momentum.py
@@ -49,5 +49,5 @@ def calculate_force(
             scale_vector(-1, momentum_before_)
         )
     )
-    result = [sympify(component.doit()) for component in force_law(momentum_function).components]
+    result = [sympify(component).doit() for component in force_law(momentum_function).components]
     return QuantityVector(result, momentum_before_.coordinate_system)
