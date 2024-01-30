5fbe1dadf6d0de51bada6726478f0b99a7a14401
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-01-30 16:25:48 +0100

    Commit #1. radiation_intensity (#326)

---
+++ symplyphysics/laws/optics/radiation_intensity_from_energy_area_time.py
@@ -0,0 +1,39 @@
+from sympy import (Eq, solve)
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+    validate_output,)
+
+# Description
+## Intensity is a scalar physical quantity that quantitatively characterizes the power carried by
+## a wave in the direction of propagation
+
+## Law: I = E / (S * t), where
+## I - intensity of incident radiation,
+## E - energy of incident radiation,
+## S - area,
+## t - time.
+
+intensity = Symbol("intensity", units.power / units.area)
+
+energy = Symbol("energy", units.energy)
+area = Symbol("area", units.area)
+time = Symbol("time", units.time)
+
+law = Eq(intensity, energy / (area * time))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(energy_=energy,
+    area_=area,
+    time_=time,)
+@validate_output(intensity)
+def calculate_intensity(energy_: Quantity, area_: Quantity, time_: Quantity) -> Quantity:
+    result_expr = solve(law, intensity, dict=True)[0][intensity]
+    intensity_applied = result_expr.subs({
+        energy: energy_,
+        area: area_,
+        time: time_
+    })
+    return Quantity(intensity_applied)
