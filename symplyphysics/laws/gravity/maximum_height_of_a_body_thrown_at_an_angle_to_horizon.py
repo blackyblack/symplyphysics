41772bccf78d3de18dbdbf05a980383bc2fcb7be
Ростислав Житков <rosjit1@gmail.com>
2024-01-31 22:50:15 +0300

    Commit #2. Add proof

--- symplyphysics/laws/gravity/maximum_height_of_a_body_thrown_at_an_angle_to_horizon.py
+++ symplyphysics/laws/gravity/maximum_height_of_a_body_thrown_at_an_angle_to_horizon.py
@@ -5,6 +5,7 @@ from symplyphysics import (units, Quantity, Symbol, print_expression, validate_i
 from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.laws.kinematic import constant_acceleration_movement_is_parabolic as distance_law
 from symplyphysics.laws.kinematic import planar_projection_is_cosine as projection_law
+from symplyphysics.laws.gravity import maximum_movement_time_of_a_body_thrown_at_an_angle_to_horizon as time_law
 
 # Description
 ## Let's say we throw the body at an angle to the horizon with some initial velocity.
@@ -25,28 +26,33 @@ angle = Symbol("angle", angle_type)
 
 law = Eq(height, initial_velocity**2 * sin(angle)**2 / (2 * earth_free_fall_acceleration))
 
-# # This law might be derived via "constant_acceleration_movement_is_parabolic" law
-# # and "planar_projection_is_cosine" law.
+# This law might be derived via "constant_acceleration_movement_is_parabolic" law, "planar_projection_is_cosine" law
+# and "maximum_movement_time_of_a_body_thrown_at_an_angle_to_horizon" law.
 
-# # The law seeks a projection on the horizontal axis, but a projection on the vertical axis is necessary,
-# # so the angle is represented as a "pi/2 - angle".
-# projection_law_applied = projection_law.law.subs({
-#     projection_law.vector_length: initial_velocity,
-#     projection_law.vector_angle: (pi / 2) - angle,
-# })
-# projection_derived = solve(projection_law_applied, projection_law.projection, dict=True)[0][projection_law.projection]
+# The law seeks a projection on the horizontal axis, but a projection on the vertical axis is necessary,
+# so the angle is represented as a "pi/2 - angle".
+projection_law_applied = projection_law.law.subs({
+    projection_law.vector_length: initial_velocity,
+    projection_law.vector_angle: (pi / 2) - angle,
+})
+projection_derived = solve(projection_law_applied, projection_law.projection, dict=True)[0][projection_law.projection]
 
-# # The acceleration of gravity is directed opposite to the vertical coordinate axis,
-# ## so there is a minus sign before the acceleration.
-# distance_law_applied = distance_law.law.subs({
-#     distance_law.initial_velocity : projection_derived,
-#     distance_law.constant_acceleration: -earth_free_fall_acceleration,
-#     distance_law.distance(distance_law.height): 0,
-# })
-# time_derived = solve(distance_law_applied, distance_law.height, dict=True)[1][distance_law.height]
+time_law_applied = time_law.law.subs({
+    time_law.initial_velocity : initial_velocity,
+    time_law.angle: angle,
+})
+time_derived = solve(time_law_applied, time_law.movement_time, dict=True)[0][time_law.movement_time]
 
-# # Check if derived movement time is same as declared.
-# assert expr_equals(time_derived, law.rhs)
+# The acceleration of gravity is directed opposite to the vertical coordinate axis,
+## so there is a minus sign before the acceleration.
+height_law_applied = distance_law.law.subs({
+    distance_law.initial_velocity: projection_derived,
+    distance_law.movement_time: time_derived / 2,
+    distance_law.constant_acceleration: -earth_free_fall_acceleration,
+})
+
+# Check if derived height is same as declared.
+assert expr_equals(height_law_applied.rhs, law.rhs)
 
 
 def print_law() -> str:
