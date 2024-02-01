fbef04ce35dbdc79f7c0bdc8c5eece24580dba1f
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-02-01 13:04:07 +0100

    Maximum height of a body thrown at an angle to horizon (#337)
    * Commit #1. maximum_height_of_a_body_thrown_at_an_angle_to_horizon
    
    * Commit #2. Add proof
    
    * Commit #3
    
    * Commit #3_1
    
    * Commit #3_2

---
+++ symplyphysics/laws/gravity/maximum_height_of_a_body_thrown_at_an_angle_to_horizon.py
@@ -0,0 +1,72 @@
+from sympy import (Eq, solve, sin, pi)
+from sympy.physics.units import acceleration_due_to_gravity as earth_free_fall_acceleration
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+    validate_output, angle_type)
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.kinematic import constant_acceleration_movement_is_parabolic as distance_law
+from symplyphysics.laws.kinematic import planar_projection_is_cosine as projection_law
+from symplyphysics.laws.kinematic import accelerated_velocity_from_time as velocity_law
+
+# Description
+## Let's say we throw the body at an angle to the horizon with some initial velocity.
+## Then the height of throw of the body will depend only on the initial velocity,
+## the angle of the throw and the acceleration of free fall.
+## The angle of the throw is the angle between the initial velocity vector and the horizontal axis.
+
+## Law is: h = v0^2 * sin(a)^2 / (2 * g), where
+## h - height of throw,
+## v0 - initial velocity,
+## a - angle of throw,
+## g - acceleration of free fall.
+
+height = Symbol("height", units.length)
+
+initial_velocity = Symbol("initial_velocity", units.velocity)
+angle = Symbol("angle", angle_type)
+
+law = Eq(height, initial_velocity**2 * sin(angle)**2 / (2 * earth_free_fall_acceleration))
+
+# This law might be derived via "constant_acceleration_movement_is_parabolic" law, "planar_projection_is_cosine" law
+# and "accelerated_velocity_from_time" law.
+
+# The law seeks a projection on the horizontal axis, but a projection on the vertical axis is necessary,
+# so the angle is represented as a "pi/2 - angle".
+projection_law_applied = projection_law.law.subs({
+    projection_law.vector_length: initial_velocity,
+    projection_law.vector_angle: (pi / 2) - angle,
+})
+vertical_projection_derived = solve(projection_law_applied, projection_law.projection, dict=True)[0][projection_law.projection]
+
+# Vertical velocity is zero in the highest point of trajectory.
+velocity_law_applied = velocity_law.law.subs({
+    velocity_law.initial_velocity: vertical_projection_derived,
+    velocity_law.velocity: 0,
+    velocity_law.acceleration: -earth_free_fall_acceleration,
+})
+time_derived = solve(velocity_law_applied, velocity_law.time, dict=True)[0][velocity_law.time]
+
+# The acceleration of gravity is directed opposite to the vertical coordinate axis,
+## so there is a minus sign before the acceleration.
+height_law_applied = distance_law.law.subs({
+    distance_law.initial_velocity: vertical_projection_derived,
+    distance_law.movement_time: time_derived,
+    distance_law.constant_acceleration: -earth_free_fall_acceleration,
+})
+
+# Check if derived height is same as declared.
+assert expr_equals(height_law_applied.rhs, law.rhs)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(initial_velocity_=initial_velocity, angle_=angle)
+@validate_output(height)
+def calculate_height(initial_velocity_: Quantity, angle_: float | Quantity) -> Quantity:
+    result_expr = solve(law, height, dict=True)[0][height]
+    result_expr = result_expr.subs({
+        initial_velocity: initial_velocity_,
+        angle: angle_,
+    })
+    return Quantity(result_expr)
---
+++ test/gravity/maximum_height_of_a_body_thrown_at_an_angle_to_horizon_test.py
@@ -0,0 +1,40 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from sympy import pi
+from symplyphysics import (units, SI, convert_to, Quantity, errors)
+from symplyphysics.laws.gravity import maximum_height_of_a_body_thrown_at_an_angle_to_horizon as height_law
+
+# Description
+## Let the initial velocity be 3 meter per second, the angle of the throw is 45 degree (pi / 4 radian),
+## and the acceleration of gravity is 9.8 [meter / second^2]. Then the maximum lifting height of the body thrown at
+## an angle to the horizon will be 0.23 meter.
+## https://www.omnicalculator.com/physics/projectile-motion
+
+@fixture(name="test_args")
+def test_args_fixture():
+    initial_velocity = Quantity(3 * (units.meter / units.second))
+    angle = pi / 4
+
+    Args = namedtuple("Args", ["initial_velocity", "angle"])
+    return Args(initial_velocity=initial_velocity, angle=angle)
+
+
+def test_basic_height(test_args):
+    result = height_law.calculate_height(test_args.initial_velocity, test_args.angle)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.length)
+    result = convert_to(result, units.meter).evalf(5)
+    assert result == approx(0.23, rel=0.01)
+
+
+def test_bad_initial_velocity(test_args):
+    initial_velocity = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        height_law.calculate_height(initial_velocity, test_args.angle)
+    with raises(TypeError):
+        height_law.calculate_height(100, test_args.angle)
+
+
+def test_bad_angle(test_args):
+    angle = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        height_law.calculate_height(test_args.initial_velocity, angle)
