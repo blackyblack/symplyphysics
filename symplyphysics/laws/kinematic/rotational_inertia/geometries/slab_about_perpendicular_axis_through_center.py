2b16539117fc7e32d48c56811479fe9782157611
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-01 05:30:25 +0400

    Derive law from integral definition

--- symplyphysics/laws/kinematic/rotational_inertia/geometries/slab_about_perpendicular_axis_through_center.py
+++ symplyphysics/laws/kinematic/rotational_inertia/geometries/slab_about_perpendicular_axis_through_center.py
@@ -1,4 +1,4 @@
-from sympy import Eq
+from sympy import Eq, sqrt
 from symplyphysics import (
     units,
     Quantity,
@@ -7,6 +7,11 @@ from symplyphysics import (
     validate_input,
     validate_output,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.kinematic.rotational_inertia import (
+    rotational_inertia_cartesian_integral as integral_law,
+)
+from symplyphysics.definitions import density_from_mass_volume as density_def
 
 # Description
 ## A solid slab is rotating about an axis that is perpendicular to the slab and goes through its center.
@@ -22,6 +27,34 @@ length = Symbol("length", units.length)
 width = Symbol("width", units.length)
 
 law = Eq(rotational_inertia, mass * (length**2 + width**2) / 12)
+
+
+# Derive this law from the integral definition of rotational inertia in cartesian coordiantes.
+# Condition: density of slab is constant.
+
+height = Symbol("height", units.length)
+volume = length * width * height
+
+density = density_def.definition.rhs.subs({
+    density_def.mass: mass,
+    density_def.volume: volume,
+})
+
+distance_to_axis = sqrt(integral_law.x**2 + integral_law.y**2)
+
+rotational_inertia_derived = integral_law.law.rhs.subs({
+    integral_law.density(integral_law.x, integral_law.y, integral_law.z): density,
+    integral_law.distance_to_axis(integral_law.x, integral_law.y, integral_law.z): distance_to_axis,
+    integral_law.x_start: -1 *  length / 2,
+    integral_law.y_start: -1 * width / 2,
+    integral_law.z_start: 0,
+    integral_law.x_end: length / 2,
+    integral_law.y_end: width / 2,
+    integral_law.z_end: height,
+}).doit().simplify()
+
+
+assert expr_equals(law.rhs, rotational_inertia_derived)
 
 
 def print_law() -> str:
