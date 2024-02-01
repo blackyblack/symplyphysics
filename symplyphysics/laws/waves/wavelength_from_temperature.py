91f48f15cf64f6cb747f8fced7b9664980a56633
paradoxx84 <paradoxx84@yandex.ru>
2024-02-01 08:27:09 +0500

    fix

--- symplyphysics/laws/waves/wavelength_from_temperature.py
+++ symplyphysics/laws/waves/wavelength_from_temperature.py
@@ -3,20 +3,20 @@ from sympy.physics.units import speed_of_light, planck, boltzmann
 from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input, validate_output)
 
 # Description
-## Any object with non-zero absolute temperature radiates energy. Absolutely black object is objects, wich doesn't reflect any radiation.
-## Wavelength of most intesive radiation depends on object's temperature and is described by Win's law.
+## Any object with non-zero absolute temperature radiates energy. Absolutely black object is objects, which doesn't reflect any radiation.
+## Wavelength of most intesive radiation depends on object's temperature and is described by Wien's law.
 
 # Law: lambda = b/T, where
 ## lambda is wavelength,
-## T is thermodinamical temperature of object,
-## b is Win's constant, b = ch/ka, where c is speed of light, h is Planc's constant, k is Boltzman's constant and a = 4,965114.
+## T is thermodynamical temperature of object,
+## b is Wien's constant, b = ch/ka, where c is speed of light, h is Planck's constant, k is Boltzmann's constant and a = 4,965114.
 
 intensive_wavelength = Symbol("intensive_wavelengh", units.length)
 object_temperature = Symbol("object_temperature", units.temperature)
 a = 4.965114
-wins_constant = speed_of_light * planck / (boltzmann * a)
+wiens_constant = speed_of_light * planck / (boltzmann * a)
 
-law = Eq(intensive_wavelength, wins_constant / object_temperature)
+law = Eq(intensive_wavelength, wiens_constant / object_temperature)
 
 
 def print_law() -> str:
