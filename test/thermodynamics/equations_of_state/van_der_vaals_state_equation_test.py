7818ca657c975a812f70cdf49ac480b490474f82
Voldoned <vova.nederkin@gmail.com>
2024-02-02 17:18:10 +0300

    [CREATE] Create law and test: Van-der-Vaals state equation

---
+++ test/thermodynamics/equations_of_state/van_der_vaals_state_equation_test.py
@@ -0,0 +1,73 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    Quantity,
+    SI,
+    convert_to,
+)
+from symplyphysics.laws.thermodynamics.equations_of_state import van_der_vaals_state_equation as vaals_law
+
+# Description
+## Test example from https://studfile.net/preview/1772224/page:8/ example 2.24
+
+# Find pressure in state 1. For this example koefficients for van-der-vaals state equation
+
+# a = (V/nu)^2 * (p2 * T1 - p1 * T2) / (T2 - T1) = 0.188 [Pa*m^6]
+# b = V - nu * R * (T2 - T1) / (p2 - p1) = 1.115 * 10^(-6) [m^3]
+# V = 0.25 [liters]
+# nu = 1 [moles]
+# T = 300 [K]
+
+# Pressure should be 90 atm
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    t = Quantity(300 * units.kelvins)
+    v = Quantity(0.25 * units.liters)
+    nu = Quantity(1 * units.amount_of_substance)
+    a = Quantity(0.188 * units.pascals * units.meter ** 6)
+    b = Quantity(1.115 * 1E-6 * units.meters ** 3)
+    Args = namedtuple("Args", ["t", "v", "nu", "a", "b"])
+    return Args(t=t, v=v, nu=nu, a=a, b=b)
+
+
+def test_basic_pressure(test_args):
+    result = vaals_law.calculate_pressure(test_args.v, test_args.t, test_args.nu, test_args.a, test_args.b)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.pressure)
+    result_ = convert_to(result, units.atm).evalf(5)
+    assert result_ == approx(90, 0.01)
+
+
+def test_bad_temperature(test_args):
+    tb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        vaals_law.calculate_pressure(test_args.v, tb, test_args.nu, test_args.a, test_args.b)
+    with raises(TypeError):
+        vaals_law.calculate_pressure(test_args.v, 100, test_args.nu, test_args.a, test_args.b)
+
+
+def test_bad_volume(test_args):
+    vb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        vaals_law.calculate_pressure(vb, test_args.t, test_args.nu, test_args.a, test_args.b)
+    with raises(TypeError):
+        vaals_law.calculate_pressure(100, test_args.t, test_args.nu, test_args.a, test_args.b)
+
+
+def test_bad_molecules_volume_parameter(test_args):
+    bb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        vaals_law.calculate_pressure(test_args.v, test_args.t, test_args.nu, test_args.a, bb)
+    with raises(TypeError):
+        vaals_law.calculate_pressure(test_args.v, test_args.t, test_args.nu, test_args.a, 100)
+
+
+def test_bad_bonding_forces_parameter(test_args):
+    ab = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        vaals_law.calculate_pressure(test_args.v, test_args.t, test_args.nu, ab, test_args.b)
+    with raises(TypeError):
+        vaals_law.calculate_pressure(test_args.v, test_args.t, test_args.nu, 100, test_args.b)
