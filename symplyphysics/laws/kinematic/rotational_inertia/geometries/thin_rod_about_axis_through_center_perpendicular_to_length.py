2895133b157610e8a7410babcf6fadefdea81570
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-02-02 15:03:14 +0400

    Law and proof: rotational inertia of thin rod (#339)
    * Create law and test
    
    * Derive law from definition
    
    * Create law and test
    
    * Derive law from definition
    
    * Simplify proof using already existing shape
    
    * Simplify proof using already existing shape

---
+++ symplyphysics/laws/kinematic/rotational_inertia/geometries/thin_rod_about_axis_through_center_perpendicular_to_length.py
@@ -0,0 +1,54 @@
+from sympy import Eq, Symbol as SymSymbol, solve, integrate
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.kinematic.rotational_inertia.geometries import (
+    slab_about_perpendicular_axis_through_center as slab_formula
+)
+
+# Description
+## A thin rod is rotating about an axis that passes through its center perpendicular to its length.
+
+# Law: I = 1/12 * M * L**2
+## I - rotational inertia of thin rod
+## M - mass of rod
+## L - length of rod
+
+rotational_inertia = Symbol("rotational_inertia", units.mass * units.length**2)
+mass = Symbol("mass", units.mass)
+length = Symbol("length", units.length)
+
+law = Eq(rotational_inertia, mass * length**2 / 12)
+
+
+# Derive law from formula for a slab rotating about the axis perpendicular to its lenght and width
+# passing through its center. The thin rod is a particular case of it, when the width of the slab
+# approaches zero.
+
+rotational_inertia_derived = slab_formula.law.rhs.subs({
+    slab_formula.mass: mass,
+    slab_formula.length: length,
+    slab_formula.width: 0,
+})
+
+assert expr_equals(law.rhs, rotational_inertia_derived)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(mass_=mass, length_=length)
+@validate_output(rotational_inertia)
+def calculate_rotational_inertia(mass_: Quantity, length_: Quantity) -> Quantity:
+    result = law.rhs.subs({
+        mass: mass_,
+        length: length_,
+    })
+    return Quantity(result)
---
+++ test/kinematic/rotational_inertia/geometries/thin_rod_about_axis_through_center_perpendicular_to_length_test.py
@@ -0,0 +1,47 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    convert_to,
+    Quantity,
+    SI,
+)
+from symplyphysics.laws.kinematic.rotational_inertia.geometries import (
+    thin_rod_about_axis_through_center_perpendicular_to_length as thin_rod_formula
+)
+
+# Description
+## A thin rod of length of 30 cm and a mass of 0.5 kg rotates about an axis that passes through
+## its center perpendicular to its length. Its rotaitonal inertia about that axis is 3.75e-3 kg*(m**2).
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    m = Quantity(0.5 * units.kilogram)
+    l = Quantity(30.0 * units.centimeter)
+    Args = namedtuple("Args", "m l")
+    return Args(m=m, l=l)
+
+
+def test_basic_law(test_args):
+    result = thin_rod_formula.calculate_rotational_inertia(test_args.m, test_args.l)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.mass * units.length**2)
+    result_value = convert_to(result, units.kilogram * units.meter**2).evalf(3)
+    assert result_value == approx(3.75e-3, 1e-3)
+
+
+def test_bad_mass(test_args):
+    mb = Quantity(1.0 * units.coulomb)
+    with raises(errors.UnitsError):
+        thin_rod_formula.calculate_rotational_inertia(mb, test_args.l)
+    with raises(TypeError):
+        thin_rod_formula.calculate_rotational_inertia(100, test_args.l)
+
+
+def test_bad_length(test_args):
+    lb = Quantity(1.0 * units.coulomb)
+    with raises(errors.UnitsError):
+        thin_rod_formula.calculate_rotational_inertia(test_args.m, lb)
+    with raises(TypeError):
+        thin_rod_formula.calculate_rotational_inertia(test_args.m, 100)
