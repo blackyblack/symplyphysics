f13e2b6ea40ac316696ac945b3176f4ac52cfafa
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-05 14:14:48 +0400

    Create law and test for linear momentum

---
+++ symplyphysics/definitions/vector/momentum_is_mass_times_velocity.py
@@ -0,0 +1,46 @@
+from symplyphysics import (
+    units,
+    Symbol,
+    Vector,
+    Quantity,
+    QuantityVector,
+    scale_vector,
+    validate_input,
+    validate_output,
+)
+from symplyphysics.core.dimensions import ScalarValue
+from symplyphysics.core.symbols.quantities import list_of_quantities
+
+# Description
+## A particle's linear momentum is a vector quantity defines as its velocity vector multiplied by its mass.
+
+# Definition: p = m * v
+## p - vector of linear momentum
+## m - mass
+## v - velocity vector
+
+mass = Symbol("mass", units.mass)
+
+
+def momentum_definition(velocity_: Vector) -> Vector:
+    return scale_vector(mass, velocity_)
+
+
+def velocity_from_momentum_law(momentum_: Vector) -> Vector:
+    return scale_vector(1 / mass, momentum_)
+
+
+@validate_input(mass_=mass, velocity_=units.velocity)
+@validate_output(units.momentum)
+def calculate_momentum(mass_: Quantity, velocity_: QuantityVector) -> QuantityVector:
+    momentum = momentum_definition(velocity_)
+    momentum_components = list_of_quantities(momentum.components, {mass: mass_})
+    return QuantityVector(momentum_components, velocity_.coordinate_system)
+
+
+@validate_input(mass_=mass, momentum_=units.momentum)
+@validate_output(units.velocity)
+def calculate_velocity_from_momentum(mass_: Quantity, momentum_: QuantityVector) -> QuantityVector:
+    velocity = velocity_from_momentum_law(momentum_)
+    velocity_components = list_of_quantities(velocity.components, {mass: mass_})
+    return QuantityVector(velocity_components, momentum_.coordinate_system)
---
+++ test/definitions/vector/momentum_is_mass_times_velocity_test.py
@@ -0,0 +1,107 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from sympy import cos, pi, sin
+from symplyphysics import (
+    units,
+    convert_to,
+    Quantity,
+    SI,
+    errors,
+    QuantityVector,
+)
+from symplyphysics.definitions.vector import momentum_is_mass_times_velocity as momentum_def
+
+# Description
+## A particle of mass m = 0.2 kg is moving in space, its velocity vector being (-1, 0, 2) m/s.
+## The vector of its linear momentum is (-0.2, 0, 0.4) kg*m/s.
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    m = Quantity(0.2 * units.kilogram)
+    v = QuantityVector([
+        Quantity(-1.0 * units.meter / units.second),
+        Quantity(0.0 * units.meter / units.second),
+        Quantity(2.0 * units.meter / units.second),
+    ])
+    p = QuantityVector([
+        Quantity(-0.2 * units.kilogram * units.meter / units.second),
+        Quantity(0.0 * units.kilogram * units.meter / units.second),
+        Quantity(0.4 * units.kilogram * units.meter / units.second),
+    ])
+    Args = namedtuple("Args", "m v p")
+    return Args(m=m, v=v, p=p)
+
+
+def test_momentum_definition(test_args):
+    result = momentum_def.calculate_momentum(test_args.m, test_args.v)
+    assert len(result.components) == 3
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.momentum)
+    for result_component, correct_component in zip(result.components, test_args.p.components):
+        result_value = convert_to(result_component, units.kilogram * units.meter / units.second)
+        correct_value = convert_to(correct_component, units.kilogram * units.meter / units.second)
+        assert result_value == approx(correct_value, 1e-3)
+
+
+def test_momentum_bad_mass(test_args):
+    m_bad_scalar = Quantity(1.0 * units.second)
+    with raises(errors.UnitsError):
+        momentum_def.calculate_momentum(m_bad_scalar, test_args.v)
+    with raises(TypeError):
+        momentum_def.calculate_momentum(100, test_args.v)
+
+
+def test_momentum_bad_velocity(test_args):
+    v_bad_vector = QuantityVector([
+        Quantity(1.0 * units.meter),
+        Quantity(1.0 * units.meter),
+        Quantity(1.0 * units.meter),
+    ])
+    with raises(errors.UnitsError):
+        momentum_def.calculate_momentum(test_args.m, v_bad_vector)
+
+    v_scalar = Quantity(1.0 * units.meter / units.second)
+    with raises(AttributeError):
+        momentum_def.calculate_momentum(test_args.m, v_scalar)
+
+    with raises(TypeError):
+        momentum_def.calculate_momentum(test_args.m, 100)
+    with raises(TypeError):
+        momentum_def.calculate_momentum(test_args.m, [100, 100])
+
+
+def test_velocity_law(test_args):
+    result = momentum_def.calculate_velocity_from_momentum(test_args.m, test_args.p)
+    assert len(result.components) == 3
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.velocity)
+    for result_component, correct_component in zip(result.components, test_args.v.components):
+        result_value = convert_to(result_component, units.meter / units.second)
+        correct_value = convert_to(correct_component, units.meter / units.second)
+        assert result_value == approx(correct_value, 1e-3)
+
+
+def test_velocity_bad_mass(test_args):
+    m_bad_scalar = Quantity(1.0 * units.second)
+    with raises(errors.UnitsError):
+        momentum_def.calculate_velocity_from_momentum(m_bad_scalar, test_args.p)
+    with raises(TypeError):
+        momentum_def.calculate_velocity_from_momentum(100, test_args.p)
+
+
+def test_velocity_bad_momentum(test_args):
+    p_bad_vector = QuantityVector([
+        Quantity(1.0 * units.meter),
+        Quantity(1.0 * units.meter),
+        Quantity(1.0 * units.meter),
+    ])
+    with raises(errors.UnitsError):
+        momentum_def.calculate_velocity_from_momentum(test_args.m, p_bad_vector)
+
+    p_scalar = Quantity(1.0 * units.kilogram * units.meter / units.second)
+    with raises(AttributeError):
+        momentum_def.calculate_velocity_from_momentum(test_args.m, p_scalar)
+
+    with raises(TypeError):
+        momentum_def.calculate_velocity_from_momentum(test_args.m, 100)
+    with raises(TypeError):
+        momentum_def.calculate_velocity_from_momentum(test_args.m, [100])
