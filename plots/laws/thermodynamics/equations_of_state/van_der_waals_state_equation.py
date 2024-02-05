a7afe102118d406aad31c3189238f5d5cce346f1
Voldoned <vova.nederkin@gmail.com>
2024-02-05 11:20:33 +0300

    [CREATE] Create plot: Van-der-Waals state equation (pressure from temperature)

---
+++ plots/laws/thermodynamics/equations_of_state/van_der_waals_state_equation.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from sympy import symbols, Eq, solve
+from sympy.plotting import plot
+from sympy.plotting.plot import MatplotlibBackend
+from symplyphysics import print_expression
+from symplyphysics.laws.thermodynamics.equations_of_state import van_der_waals_state_equation as van_der_waals_law
+
+# Convert from liters to m^3
+volume_values = np.array([0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45]) * 1E-3
+
+temperature = symbols("temperature")
+pressure = symbols("pressure")
+volume = symbols("volume")
+amount_of_substance = symbols("amount_of_substance")
+
+parameter_a = symbols("parameter_a")
+parameter_b = symbols("parameter_b")
+
+state_equation = van_der_waals_law.law.subs({
+    van_der_waals_law.pressure: pressure,
+    van_der_waals_law.temperature: temperature,
+    van_der_waals_law.volume: volume,
+    van_der_waals_law.amount_of_substance: amount_of_substance,
+    van_der_waals_law.bonding_forces_parameter: parameter_a,
+    van_der_waals_law.molecules_volume_parameter: parameter_b
+})
+pressure_eq = solve(state_equation, pressure, dict=True)[0][pressure]
+answer = Eq(pressure, pressure_eq)
+print(f"Total equation:\n{print_expression(answer)}")
+
+pressure_to_plots = pressure_eq.subs({
+    parameter_a: 0.188,     # [Pa * m^6 / mole^2]
+    parameter_b: 4.532 * 1E-5,      # [m^3 / mole]
+    amount_of_substance: 1      # [moles]
+})
+
+base_plot = plot(title="The Van der Waals state equation",
+    xlabel=r"$T, K$",
+    ylabel=r"$P, Pa$",
+    backend=MatplotlibBackend,
+    legend=True,
+    show=False)
+
+for volume_value in volume_values:
+    pressure_to_subplot = pressure_to_plots.subs({volume: volume_value})
+    subplot = plot(pressure_to_subplot,
+       (temperature, 100, 500),     # [K]
+       label=f"Volume is {volume_value}",
+       show=False)
+    base_plot.append(subplot[0])
+
+base_plot.show()
