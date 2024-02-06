970d258add6b15a92829df140ab7cf156b99fa4b
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-06 20:40:35 +0400

    Create law and test for angular momentum

---
+++ symplyphysics/laws/kinematic/angular_momentum_is_rotational_inertia_times_angular_velocity.py
@@ -0,0 +1,40 @@
+from sympy import Eq
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    angle_type,
+)
+
+# Description
+## For a rigid body rotating about a fixed axis, the component of its angular momentum parallel
+## to the rotation axis is found as the product of the body's rotatinal inertia and the magnitude
+## of its angular velocity.
+
+# Law: L = I * w
+## L - component of angular momentum parallel to rotational axis
+## I - rotational inertia
+## w - angular velocity
+
+angular_momentum = Symbol("angular_momentum", units.length * units.momentum)
+rotational_inertia = Symbol("rotational_inertia", units.mass * units.length**2)
+angular_velocity = Symbol("angular_velocity", angle_type / units.time)
+
+law = Eq(angular_momentum, rotational_inertia * angular_velocity)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(rotational_inertia_=rotational_inertia, angular_velocity_=angular_velocity)
+@validate_output(angular_momentum)
+def calculate_angular_momentum(rotational_inertia_: Quantity, angular_velocity_: Quantity) -> Quantity:
+    result = law.rhs.subs({
+        rotational_inertia: rotational_inertia_,
+        angular_velocity: angular_velocity_,
+    })
+    return Quantity(result)
---
+++ test/kinematic/angular_momentum_is_rotational_inertia_times_angular_velocity_test.py
@@ -0,0 +1,44 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    Quantity,
+)
+from symplyphysics.laws.kinematic import (
+    angular_momentum_is_rotational_inertia_times_angular_velocity as angular_momentum_law,
+)
+
+# Description
+## A rigid body with rotational inertia of 1.5 kg*m**2 is rotating about a fixed axis
+## with angular velocity of 4.0 rad/s. Its angular momentum amounts to 6.0 kg*m**2/s.
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    I = Quantity(1.5 * units.kilogram * units.meter**2)
+    w = Quantity(4.0 * units.radian / units.second)
+    Args = namedtuple("Args", "I w")
+    return Args(I=I, w=w)
+
+
+def test_law(test_args):
+    result = angular_momentum_law.calculate_angular_momentum(test_args.I, test_args.w)
+    assert_equal(result, 6.0 * units.kilogram * units.meter**2 / units.second)
+
+
+def test_bad_rotational_inertia(test_args):
+    Ib = Quantity(1.0 * units.second)
+    with raises(errors.UnitsError):
+        angular_momentum_law.calculate_angular_momentum(Ib, test_args.w)
+    with raises(TypeError):
+        angular_momentum_law.calculate_angular_momentum(100, test_args.w)
+
+
+def test_bad_angular_velocity(test_args):
+    wb = Quantity(1.0 * units.second)
+    with raises(errors.UnitsError):
+        angular_momentum_law.calculate_angular_momentum(test_args.I, wb)
+    with raises(TypeError):
+        angular_momentum_law.calculate_angular_momentum(test_args.I, 100)
