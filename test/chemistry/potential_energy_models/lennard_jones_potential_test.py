18ff87c0122cba48fa66da257911b0607d701803
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-06 04:31:03 +0400

    Show use of function in test file

--- test/chemistry/potential_energy_models/lennard_jones_potential_test.py
+++ test/chemistry/potential_energy_models/lennard_jones_potential_test.py
@@ -4,9 +4,9 @@ from symplyphysics import (
     errors,
     units,
     Quantity,
-    SI,
     convert_to,
     assert_approx,
+    equivalent_dims,
 )
 from symplyphysics.laws.chemistry.potential_energy_models import lennard_jones_potential
 
@@ -26,7 +26,7 @@ def test_args_fixture():
 
 def test_basic_law(test_args):
     result = lennard_jones_potential.calculate_potential(test_args.e, test_args.sigma, test_args.r)
-    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.energy)
+    assert equivalent_dims(result, units.energy)
     result_value = convert_to(result, units.joule).evalf(5)
     assert_approx(result_value, 2.6529e-13)
 
