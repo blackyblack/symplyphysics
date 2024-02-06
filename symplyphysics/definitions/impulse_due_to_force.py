6862c56f150e968b016f7d4a5498abf75ec93ee8
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-06 07:52:50 +0400

    Create law and test for impulse in collision

---
+++ symplyphysics/definitions/impulse_due_to_force.py
@@ -0,0 +1,54 @@
+from sympy import Eq, Integral
+from symplyphysics import (
+    units,
+    Quantity,
+    Function,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## The impulse due to force exerted on body during collision is the measure of both the
+## magnitude and duration of the collision.
+
+# Law: J = Integral(F(t), (t, t_i, t_f))
+## J - magnitude of impulse due to force F
+## F - magnitude of force
+## t - time
+## t_i, t_f - initial and final time of collision, respectively
+
+impulse = Symbol("impulse", units.momentum)
+force = Function("force", units.force)
+time = Symbol("time", units.time)
+time_start = Symbol("time_start", units.time)
+time_end = Symbol("time_end", units.time)
+
+law = Eq(impulse, Integral(force(time), (time, time_start, time_end)))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    force_start_=force,
+    force_end_=force,
+    time_start_=time_start,
+    time_end_=time_end,
+)
+@validate_output(impulse)
+def calculate_impulse(
+    force_start_: Quantity,
+    force_end_: Quantity,
+    time_start_: Quantity,
+    time_end_: Quantity,
+) -> Quantity:
+    force_function = force_start_ + (force_end_ - force_start_) / (time_end_ - time_start_) * (time - time_start_)
+    result = law.rhs.subs({
+        force(time): force_function,
+        time_start: time_start_,
+        time_end: time_end_,
+    }).doit()
+    return Quantity(result)
