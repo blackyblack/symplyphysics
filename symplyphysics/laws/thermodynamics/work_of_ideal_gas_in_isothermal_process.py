d81f812314d4fe960beb49b1f57d302615b3d9e1
Darya2909 <d9041261928@gmail.com>
2024-02-07 15:55:50 +0200

    work_of_ideal_gas_in_isothermal_process

---
+++ symplyphysics/laws/thermodynamics/work_of_ideal_gas_in_isothermal_process.py
@@ -0,0 +1,45 @@
+from sympy import (Eq, solve, log)
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+    validate_output)
+
+## Description
+## The isothermal process of expansion (or compression) of a gas can occur under conditions where heat exchange between the gas and the external environment is carried out at a constant temperature difference.
+## To do this, the heat capacity of the external environment must be large enough, and the expansion (or compression) process must be slow enough.
+
+## Law: A = (m / mu) * R * T * ln(V_2 / V_1)
+## Where:
+## A is work of gas
+## m is mass
+## mu is molar mass
+## V_1 is start volume
+## V_2 is final volume
+## R is ideal gas constant,
+## T is temperature
+
+work = Symbol("work", units.energy)
+mass = Symbol("mass", units.mass)
+molar_mass = Symbol("molar_mass", units.mass / units.amount_of_substance)
+start_volume = Symbol("start_volume", units.volume)
+final_volume = Symbol("final_volume", units.volume)
+temperature = Symbol("temperature", units.temperature)
+
+law = Eq(work, (mass / molar_mass) * units.molar_gas_constant * temperature * log(final_volume / start_volume))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(mass_=mass, molar_mass_=molar_mass, start_volume_=start_volume, final_volume_=final_volume, temperature_=temperature)
+@validate_output(work)
+def calculate_work(mass_: Quantity, molar_mass_: Quantity, start_volume_: Quantity, final_volume_: Quantity,
+    temperature_: Quantity) -> Quantity:
+    solved = solve(law, work, dict=True)[0][work]
+    result_expr = solved.subs({
+        mass: mass_, 
+        molar_mass: molar_mass_, 
+        start_volume: start_volume_,
+        final_volume: final_volume_,
+        temperature: temperature_
+    })
+    return Quantity(result_expr)
