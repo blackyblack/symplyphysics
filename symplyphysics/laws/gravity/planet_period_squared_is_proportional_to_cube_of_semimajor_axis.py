32f12d086f7dca8ed4ca4a967b59fc765daa5356
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-07 15:11:03 +0400

    Create law and test for law of periods

---
+++ symplyphysics/laws/gravity/planet_period_squared_is_proportional_to_cube_of_semimajor_axis.py
@@ -0,0 +1,45 @@
+from sympy import Eq, solve, pi
+from sympy.physics.units import gravitational_constant
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## Also known as Kepler's third law, the law of periods relates the period of rotation of any planet
+## to the semimajor axis of its orbit.
+
+# Law: T**2 = 4*pi**2 / (G*M) * a**3
+## T - planet's period of rotation
+## G - gravitational constant
+## M - mass of the attracting body, the Sun in case of the solar system
+## a - semimajor axis of the planet's orbit, its radius in case of a round orbit
+
+rotation_period = Symbol("rotation_period", units.time, positive=True)
+attracting_mass = Symbol("attracting_mass", units.mass, positive=True)
+semimajor_axis = Symbol("semimajor_axis", units.length, positive=True)
+
+law = Eq(
+    rotation_period**2,
+    4 * pi**2 / (gravitational_constant * attracting_mass) * semimajor_axis**3,
+)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(attracting_mass_=attracting_mass, semimajor_axis_=semimajor_axis)
+@validate_output(rotation_period)
+def calculate_rotation_period(
+    attracting_mass_: Quantity, semimajor_axis_: Quantity,
+) -> Quantity:
+    result = solve(law, rotation_period)[0].subs({
+        attracting_mass: attracting_mass_,
+        semimajor_axis: semimajor_axis_,
+    })
+    return Quantity(result)
---
+++ test/gravity/planet_period_squared_is_proportional_to_cube_of_semimajor_axis_test.py
@@ -0,0 +1,47 @@
+from collections import namedtuple
+from pytest import approx, fixture, raises
+from symplyphysics import (
+    errors,
+    units,
+    Quantity,
+    convert_to,
+    SI,
+)
+from symplyphysics.laws.gravity import (
+    planet_period_squared_is_proportional_to_cube_of_semimajor_axis as law_of_periods
+)
+
+# Description
+## A planet orbits around a star of mass M = 3e30 kg. The semimajor axis of its orbit is a = 3e9 km.
+## Its period of rotation around the star is about 73 years.
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    M = Quantity(3e30 * units.kilogram)
+    a = Quantity(3e9 * units.kilometer)
+    Args = namedtuple("Args", "M a")
+    return Args(M=M, a=a)
+
+
+def test_law(test_args):
+    result = law_of_periods.calculate_rotation_period(test_args.M, test_args.a)
+    assert SI.get_dimension_system().equivalent_dims(result.dimension, units.time)
+    result_value = convert_to(result, units.year).evalf(3)
+    assert result_value == approx(73.1, 1e-3)
+
+
+def test_bad_mass(test_args):
+    mb = Quantity(1.0 * units.coulomb)
+    with raises(errors.UnitsError):
+        law_of_periods.calculate_rotation_period(mb, test_args.a)
+    with raises(TypeError):
+        law_of_periods.calculate_rotation_period(100, test_args.a)
+
+
+def test_bad_length(test_args):
+    ab = Quantity(1.0 * units.coulomb)
+    with raises(errors.UnitsError):
+        law_of_periods.calculate_rotation_period(test_args.M, ab)
+    with raises(TypeError):
+        law_of_periods.calculate_rotation_period(test_args.M, 100)
