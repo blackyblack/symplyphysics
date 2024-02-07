5f6c60f7bd8cec5cebf0158f5f1f4992b7d04fd2
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-07 12:16:10 +0400

    Refactor law

--- symplyphysics/laws/geometry/vector/dot_product_is_proportional_to_cosine_between_vectors.py
+++ symplyphysics/laws/geometry/vector/dot_product_is_proportional_to_cosine_between_vectors.py
@@ -7,9 +7,9 @@ from symplyphysics import (
     vector_magnitude,
 )
 from symplyphysics.core.dimensions import ScalarValue
-from symplyphysics.laws.geometry import (
-    dot_product_is_proportional_to_cosine_between_vectors as cosine_scalar_law,
-)
+# from symplyphysics.laws.geometry import (
+#     dot_product_is_proportional_to_cosine_between_vectors as cosine_scalar_law,
+# )
 
 # Description:
 ## Dot product is scalar binary operation defined as the product of the norm of the vectors
@@ -28,13 +28,7 @@ def cosine_between_vectors_law(
     dot_product_ = dot_vectors(vector_left_, vector_right_)
     vector_left_norm_ = vector_magnitude(vector_left_)
     vector_right_norm_ = vector_magnitude(vector_right_)
-    return solve(
-        cosine_scalar_law.law, cos(cosine_scalar_law.angle_between_vectors)
-    )[0].subs({
-        cosine_scalar_law.dot_product: dot_product_,
-        cosine_scalar_law.vector_left_norm: vector_left_norm_,
-        cosine_scalar_law.vector_right_norm: vector_right_norm_,
-    })
+    return dot_product_ / (vector_left_norm_ * vector_right_norm_)
 
 
 def calculate_cosine_between_vectors(
