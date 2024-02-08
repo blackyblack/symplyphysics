ad7034e67ee8e64003576325bcdb302707088f60
Darya2909 <39147553+Darya2909@users.noreply.github.com>
2024-02-08 17:31:08 +0200

    Update work_of_ideal_gas_in_isothermal_process_test.py

--- test/thermodynamics/work_of_ideal_gas_in_isothermal_process_test.py
+++ test/thermodynamics/work_of_ideal_gas_in_isothermal_process_test.py
@@ -9,15 +9,15 @@ from symplyphysics import (
 
 from symplyphysics.laws.thermodynamics import work_of_ideal_gas_in_isothermal_process as work_of_ideal_gas
 
-## Source of numbers: https://edu.tltsu.ru/er/book_view.php?book_id=48e&page_id=3912
+## Source of numbers: https://easy-physic.ru/category/physic/ege1/molekulyarno-kineticheskaya-teoriya/termodinamika/rabota-gaza/rabota-gaza-slozhnye-zadachi
 
 @fixture(name="test_args")
 def test_args_fixture():
-    mass = Quantity(0.02 * units.kilogram)
+    mass = Quantity(0.005 * units.kilogram)
     molar_mass = Quantity(0.002 * units.kilogram / units.mole)
     start_volume = Quantity(0.22 * units.meter**3)
-    final_volume = Quantity(1.1 * units.meter**3)
-    temperature = Quantity(300 * units.kelvin)
+    final_volume = Quantity(0.66 * units.meter**3)
+    temperature = Quantity(284 * units.kelvin)
     Args = namedtuple("Args", ["mass", "molar_mass", "start_volume", "final_volume", "temperature"])
     return Args(
         mass=mass,
@@ -30,7 +30,7 @@ def test_args_fixture():
 
 def test_basic_law(test_args):
     result = work_of_ideal_gas.calculate_work(test_args.mass, test_args.molar_mass, test_args.start_volume, test_args.final_volume, test_args.temperature)
-    assert_equal(result, 40123 * units.joule)
+    assert_equal(result, 6481.8 * units.joule)
 
 
 def test_bad_mass(test_args):
