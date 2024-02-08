12aa6dd21ce92cd7e04ebf41f049931cce21026b
Darya2909 <d9041261928@gmail.com>
2024-02-08 22:47:01 +0200

    orbital_velocity_from_length_of_large_half_axis_and_planet_mass

---
+++ symplyphysics/laws/gravity/orbital_velocity_from_length_of_large_half_axis_and_planet_mass.py
@@ -0,0 +1,37 @@
+from sympy import Eq, solve, sqrt
+from sympy.physics.units import gravitational_constant
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+    validate_output)
+
+## Description
+## The orbital velocity of a body (usually a planet, a natural or artificial satellite, or a multiple star) is the speed at which it rotates around the barycenter of the system, usually around a more massive body.
+## Law: V = √(G * M * ((2 / r) - 1 / a))
+## Where:
+## V is orbital velocity
+## M is mass of planet
+## G is gravitational constant
+## r is the distance between the rotating body and the central body
+## a - is length of the large half-axis
+
+orbital_velocity = Symbol("orbital_velocity", units.velocity)
+planet_mass = Symbol("planet_mass", units.mass)
+distance = Symbol("distance", units.length)
+large_half_axis_length = Symbol("large_half_axis_length", units.length)
+
+law = Eq(orbital_velocity, sqrt(gravitational_constant * planet_mass * ((2 / distance) - (1 / large_half_axis_length))))
+
+
+def print_law():
+    print_expression(law)
+
+
+@validate_input(planet_mass_=planet_mass, distance_=distance, large_half_axis_length_=large_half_axis_length)
+@validate_output(orbital_velocity)
+def calculate_orbital_velocity(planet_mass_: Quantity, distance_: Quantity, large_half_axis_length_: Quantity) -> Quantity:
+    result_velocity_expr = solve(law, orbital_velocity, dict=True)[0][orbital_velocity]
+    result_expr = result_velocity_expr.subs({
+        planet_mass: planet_mass_,
+        distance: distance_,
+        large_half_axis_length: large_half_axis_length_
+    })
+    return Quantity(result_expr)
