ae8215fa3e3f3394bfebd6a2800c78b91c3b2074
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-08 06:40:54 +0400

    Create law and test for period of torsion pendulum

---
+++ symplyphysics/laws/dynamics/period_of_torsion_pendulum_from_rotational_inertia.py
@@ -0,0 +1,39 @@
+from sympy import Eq, pi, sqrt
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## A torsion pendulum is an angular version of a linear harmonic oscillator: a disk
+## oscillates in a horizontal plane; the reference line oscillates with some angular amplitude.
+## The element of elasticity is associated with the twisting of the suspension wire.
+
+# Law: T = 2 * pi * sqrt(I / kappa)
+## T - period of the torsion pendulum
+## I - rotational inertia of the disk
+## kappa - torsion constant, depends on the properties of the suspension wire
+
+oscillation_period = Symbol("oscillation_period", units.time)
+rotational_inertia = Symbol("rotational_inertia", units.mass * units.length**2)
+torsion_constant = Symbol("torsion_constant", units.mass * units.length**2 / units.time**2)
+
+law = Eq(oscillation_period, 2 * pi * sqrt(rotational_inertia / torsion_constant))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(rotational_inertia_=rotational_inertia, torsion_constant_=torsion_constant)
+@validate_output(oscillation_period)
+def calculate_period(rotational_inertia_: Quantity, torsion_constant_: Quantity) -> Quantity:
+    result = law.rhs.subs({
+        rotational_inertia: rotational_inertia_,
+        torsion_constant: torsion_constant_,
+    })
+    return Quantity(result)
---
+++ test/dynamics/period_of_torsion_pendulum_from_rotational_inertia_test.py
@@ -0,0 +1,44 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    Quantity,
+)
+from symplyphysics.laws.dynamics import (
+    period_of_torsion_pendulum_from_rotational_inertia as pendulum_law,
+)
+
+# Description
+## The period of a torsion pendulum with rotational inertia I = 2.0e-4 kg*m**2 and torsion constant
+## kappa = 7.1e-2 is 0.333 s.
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    I = Quantity(2.0e-4 * units.kilogram * units.meter**2)
+    kappa = Quantity(7.1e-2 * units.kilogram * units.meter**2 / units.second**2)
+    Args = namedtuple("Args", "I kappa")
+    return Args(I=I, kappa=kappa)
+
+
+def test_law(test_args):
+    result = pendulum_law.calculate_period(test_args.I, test_args.kappa)
+    assert_equal(result, 0.333 * units.second, tolerance=2e-3)
+
+
+def test_bad_rotational_inertia(test_args):
+    Ib = Quantity(1.0 * units.coulomb)
+    with raises(errors.UnitsError):
+        pendulum_law.calculate_period(Ib, test_args.kappa)
+    with raises(TypeError):
+        pendulum_law.calculate_period(100, test_args.kappa)
+
+
+def test_bad_torsion_constant(test_args):
+    kappab = Quantity(1.0 * units.coulomb)
+    with raises(errors.UnitsError):
+        pendulum_law.calculate_period(test_args.I, kappab)
+    with raises(TypeError):
+        pendulum_law.calculate_period(test_args.I, 100)
