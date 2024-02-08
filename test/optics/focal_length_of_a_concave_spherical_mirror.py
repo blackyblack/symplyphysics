6415b6a1c8e0649eb7158bf03c9042c029c5ad00
Voldoned <110999247+Voldoned@users.noreply.github.com>
2024-02-08 17:56:39 +0300

    Create law and test: focal length of a concave spherical mirror (#379)
    * [CREATE] Create law and test: focal length of a concave spherical mirror
    
    * [ADD] Add description
    
    * Fix proof
    
    * [UPDATE] Update description and fix name of test case
    
    ---------
    
    Co-authored-by: blackyblack <sam.and.tetris@gmail.com>

---
+++ test/optics/focal_length_of_a_concave_spherical_mirror.py
@@ -0,0 +1,32 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    Quantity,
+    prefixes,
+)
+from symplyphysics.laws.optics import focal_length_of_a_concave_spherical_mirror as mirror_focus_law
+
+# Test example from https://www.test-uz.ru/solutions_view.php?cat=6&num=15.5
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    r = Quantity(40 * prefixes.centi * units.meters)
+    Args = namedtuple("Args", ["r"])
+    return Args(r=r)
+
+
+def test_basic_focus_distance(test_args):
+    result = mirror_focus_law.calculate_focus_distance(test_args.r)
+    assert_equal(result, 20 * prefixes.centi * units.meters)
+
+
+def test_bad_radius():
+    rb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        mirror_focus_law.calculate_focus_distance(rb)
+    with raises(TypeError):
+        mirror_focus_law.calculate_focus_distance(100)
