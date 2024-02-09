2e9eccd99fc52cd39689499020e3f209fec7bfaa
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-09 14:08:10 +0400

    Reformulate description of law

--- symplyphysics/laws/kinematic/vector/rolling_velocity_is_translational_plus_rotational_velocities.py
+++ symplyphysics/laws/kinematic/vector/rolling_velocity_is_translational_plus_rotational_velocities.py
@@ -8,23 +8,14 @@ from symplyphysics import (
 )
 
 # Description
-## Suppose an object is rolling smoothly along a surface, i.e. without slipping or bouncing on the
-## surface. Such motion can be viewed as a combination of translational motion of the object's center
+## Suppose a wheel is rolling smoothly along a surface, i.e. without slipping or bouncing on the
+## surface. Such motion can be viewed as a combination of translational motion of the wheel's center
 ## of mass, and rotational motion of its points about the rotational axis.
 
 # Law: v = v_translation + v_rotation
-## v - velocity vector of a point on the object.
-## v_translation - velocity component responsible for translational movement of object's center
-## v_rotation - velocity component responsible for rotational movement of the point on object,
-##              see [rotational velocity formula](./linear_velocity_is_angular_velocity_cross_radius.py)
-
-# Notes
-## Suppose the object in question is a wheel of constant radius, then
-## - v_translation is a vector related to the movement of the wheel's center
-##   its magnitude is given by [linear velocity formula](../linear_velocity_from_angular_velocity_and_radius)
-##   where radius is the total radius of the wheel
-## - v_rotation is calculated via [rotational velocity formula](./linear_velocity_is_angular_velocity_cross_radius.py)
-##   where rotation_radius vector is the vector from the wheel's center to the point in question
+## v - velocity of a point on the wheel.
+## v_translation - velocity of the wheel's center of mass
+## v_rotation - see [rotational velocity formula](./linear_velocity_is_angular_velocity_cross_radius.py)
 
 
 def rolling_velocity_law(translational_velocity_: Vector, rotational_velocity_: Vector) -> Vector:
