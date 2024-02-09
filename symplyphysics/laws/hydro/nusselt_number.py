0eb6b68a5dbc1d3130f3617d549428e8d430604b
Ilshat Kulbaev <87938357+aokiyoshi@users.noreply.github.com>
2024-02-09 15:40:44 +0500

    nusselt number
    added law and tests

---
+++ symplyphysics/laws/hydro/nusselt_number.py
@@ -0,0 +1,56 @@
+from sympy import Eq, solve, S
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+    validate_output, dimensionless, convert_to)
+
+
+# Description
+# Nusselt number is the ratio of convective to conductive heat transfer at
+# a boundary in a fluid. There is a characteristic length in
+# the formula. The characteristic length is the dimension
+# that defines the length scale of a physical system. A characteristic length
+# is usually the volume of a system divided by its surface: L = V / A,
+# where V is the volume of the body, and A is the cross-sectional area.
+# For example, it is used to calculate flow through circular and non-circular
+# tubes in order to examine flow conditions. D = 4 * A / p, where
+# D is characteristic diameter, A is the cross-sectional are, p is wetted perimeter.
+# Law: h * L / k, where
+# h is the heat transfer coefficient,
+# L is the characteristic length,
+# k is the thermal conductivity.
+
+
+heat_transfer_coefficient = Symbol(
+    "heat_transfer_coefficient", units.power / (units.length**2 * units.temperature)
+)
+characteristic_length = Symbol("characteristic_length", units.length)
+thermal_conductivity = Symbol(
+    "thermal_conductivity", units.power / (units.length * units.temperature)
+)
+
+nusselt_number = Symbol("nusselt_number", dimensionless)
+
+law = Eq(nusselt_number, heat_transfer_coefficient * characteristic_length / thermal_conductivity)
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    heat_transfer_coefficient_=heat_transfer_coefficient,
+    characteristic_length_=characteristic_length,
+    thermal_conductivity_=thermal_conductivity,
+)
+@validate_output(nusselt_number)
+def calculate_nusselt_number(
+    heat_transfer_coefficient_: Quantity,
+    characteristic_length_: Quantity,
+    thermal_conductivity_: Quantity
+) -> float:
+    result_expr = solve(law, nusselt_number, dict=True)[0][nusselt_number]
+    result_applied = result_expr.subs({
+        heat_transfer_coefficient: heat_transfer_coefficient_,
+        characteristic_length: characteristic_length_,
+        thermal_conductivity: thermal_conductivity_
+    })
+    result = Quantity(result_applied)
+    return float(convert_to(result, S.One).evalf())
---
+++ test/hydro/nusselt_number_test.py
@@ -0,0 +1,83 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import assert_equal, Quantity, units, errors
+from symplyphysics.laws.hydro import nusselt_number
+
+# Example from: https://www.omnicalculator.com/physics/nusselt-number
+# Conditions:
+# Forced convection, moderate speed cross-flow of air over a cylinder.
+# Heat transfer coefficient equals 200 W / m*m*K (source https://www.engineersedge.com),
+# thermal conductivity for air equals 0.022 W / m*K, characteristic length equals 1 m.
+
+
+@fixture(name="test_args")
+def test_args_fixture():
+    heat_transfer_coefficient = Quantity(
+        200 * units.watt / (units.meter**2 * units.kelvin))
+    characteristic_length = Quantity(1 * units.meter)
+    thermal_conductivity = Quantity(
+        0.022 * units.watt / (units.meter * units.kelvin))
+    Args = namedtuple("Args", ["heat_transfer_coefficient",
+                      "characteristic_length", "thermal_conductivity"])
+    return Args(
+        heat_transfer_coefficient=heat_transfer_coefficient,
+        characteristic_length=characteristic_length,
+        thermal_conductivity=thermal_conductivity,
+    )
+
+
+def test_basic_nusselt_number(test_args):
+    result = nusselt_number.calculate_nusselt_number(
+        heat_transfer_coefficient_=test_args.heat_transfer_coefficient,
+        characteristic_length_=test_args.characteristic_length,
+        thermal_conductivity_=test_args.thermal_conductivity
+    )
+    assert_equal(result, 9090.91)
+
+
+def test_bad_heat_transfer_coefficient(test_args):
+    htc = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        nusselt_number.calculate_nusselt_number(
+            htc,
+            test_args.characteristic_length,
+            test_args.thermal_conductivity,
+        )
+    with raises(TypeError):
+        nusselt_number.calculate_nusselt_number(
+            1,
+            test_args.characteristic_length,
+            test_args.thermal_conductivity,
+        )
+
+
+def test_bad_length(test_args):
+    bl = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        nusselt_number.calculate_nusselt_number(
+            test_args.heat_transfer_coefficient,
+            bl,
+            test_args.thermal_conductivity,
+        )
+    with raises(TypeError):
+        nusselt_number.calculate_nusselt_number(
+            test_args.heat_transfer_coefficient,
+            1,
+            test_args.thermal_conductivity,
+        )
+
+
+def test_bad_conductivity(test_args):
+    bc = Quantity(1 * units.watt / (units.meter**2 * units.kelvin))
+    with raises(errors.UnitsError):
+        nusselt_number.calculate_nusselt_number(
+            test_args.heat_transfer_coefficient,
+            test_args.characteristic_length,
+            bc,
+        )
+    with raises(TypeError):
+        nusselt_number.calculate_nusselt_number(
+            test_args.heat_transfer_coefficient,
+            test_args.characteristic_length,
+            1,
+        )
