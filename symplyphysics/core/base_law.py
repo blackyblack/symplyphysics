0251e3c7a3f7eb43593a61f595a3dfd1963a3189
Voldoned <110999247+Voldoned@users.noreply.github.com>
2024-02-10 17:59:08 +0300

    [UPDATE] Update methods of 'BaseLaw' class

--- symplyphysics/core/base_law.py
+++ symplyphysics/core/base_law.py
@@ -2,6 +2,7 @@ from sympy import solve, Eq
 
 from symplyphysics import Symbol, Quantity, validate_input, validate_output
 from symplyphysics.core.symbols.fraction import Fraction
+from symplyphysics.core.symbols.probability import Probability
 
 
 class BaseLaw:
@@ -9,8 +10,15 @@ class BaseLaw:
         self.law = law
         self.symbols = law.atoms(Symbol)
 
-    def calculate_symbol_value(self, variable: Symbol, dict_quantities: dict[Symbol, Quantity | Fraction]
-    ) -> Quantity | Fraction:
+        variables_keys = list(str(symbol) for symbol in self.symbols)
+        for key, symbol in zip(variables_keys, self.symbols):
+            setattr(self, key[:-1], symbol)     # key[:-1] for deleting index in end of string
+
+    def calculate_symbol_value(
+            self,
+            variable: Symbol,
+            dict_quantities: dict[Symbol, Quantity | Fraction | Probability]
+        ) -> Quantity | Fraction | Probability:
         if variable in self.symbols:
             pass
         else:
@@ -30,12 +38,10 @@ class BaseLaw:
 
         @validate_input(**dict_validate)
         @validate_output(variable.dimension)
-        def __calculate_variable(variable_: Symbol, *quantities_: Quantity | Fraction) -> Quantity | Fraction:
-            solved = solve(self.law, variable_, dict=True)[0][variable]
-
+        def __calculate_variable(*quantities_: Quantity | Fraction | Probability) -> Quantity | Fraction | Probability:
+            solved = solve(self.law, variable, dict=True)[0][variable]
             result_expr = solved.subs(zip(variables_list, quantities_))
-
             result = Quantity(result_expr)
             return result
 
-        return __calculate_variable(variable, *dict_quantities.values())
+        return __calculate_variable(*dict_quantities.values())
