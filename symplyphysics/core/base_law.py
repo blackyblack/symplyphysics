86920d0314c20bdda7452ab871a60791a3f9bb67
Voldoned <110999247+Voldoned@users.noreply.github.com>
2024-02-11 13:24:34 +0300

    [UPDATE] Add __doc__. Update 'BaseLaw' class and tests.

--- symplyphysics/core/base_law.py
+++ symplyphysics/core/base_law.py
@@ -1,45 +1,90 @@
 from sympy import solve, Eq
+from sympy.physics.units import Dimension
 
-from symplyphysics import Symbol, Quantity, validate_input, validate_output
+from symplyphysics import Symbol, Quantity, units, errors, dimensionless
 from symplyphysics.core.symbols.fraction import Fraction
 from symplyphysics.core.symbols.probability import Probability
 
+from symplyphysics.laws.thermodynamics import laplas_pressure as laplas_law
+
 
 class BaseLaw:
+    """
+    Example:
+    --------
+
+    from symplyphysics.laws.thermodynamics import laplas_pressure as laplas_law
+
+    law = BaseLaw(laplas_law.law)
+    result = law.calculate_symbol_value(
+        law.laplas_pressure,
+        dict_quantities={
+            law.radius_of_curvature: Quantity(0.05 * units.meters),
+            law.surface_tension_of_the_liquid: Quantity(7 * units.newtons / units.meter)
+        }
+    )
+    print(result, result.dimension)
+
+    or
+    --
+
+    from symplyphysics.laws.thermodynamics import laplas_pressure as laplas_law
+
+    law = BaseLaw(laplas_law.law)
+    result = law.calculate_symbol_value(
+        laplas_law.laplas_pressure,
+        dict_quantities={
+            laplas_law.radius_of_curvature: Quantity(0.05 * units.meters),
+            laplas_law.surface_tension_of_the_liquid: Quantity(7 * units.newtons / units.meter)
+        }
+    )
+    print(result, result.dimension)
+
+    """
+
     def __init__(self, law: Eq) -> None:
         self.law = law
-        self.symbols = law.atoms(Symbol)
+        self.symbols = self.law.atoms(Symbol)
 
         variables_keys = list(str(symbol) for symbol in self.symbols)
         for key, symbol in zip(variables_keys, self.symbols):
-            setattr(self, key[:-1], symbol)     # key[:-1] for deleting index in end of string
+            setattr(self, key[:-1], symbol)  # key[:-1] for deleting index in end of string
 
     def calculate_symbol_value(
-            self,
-            variable: Symbol,
-            dict_quantities: dict[Symbol, Quantity | Fraction | Probability]
-        ) -> Quantity | Fraction | Probability:
-        if variable in self.symbols:
-            pass
-        else:
-            raise ValueError("The specified variable is not in the law")
+        self,
+        variable: Symbol,
+        dict_quantities: dict[Symbol, Quantity | Fraction | Probability]
+    ) -> Quantity | Fraction | Probability:
+        self.__check_symbol_in_equation(variable)
+        for symbol in dict_quantities.keys():
+            self.__check_symbol_in_equation(symbol)
 
-        symbols_set = self.symbols.copy()
-        symbols_set.remove(variable)
+        dict_validate = self.__get_validate_input_dict(dict_quantities)
 
-        variables_for_calculate = list(str(symbol) for symbol in symbols_set)
-        dimensions = list(symbol.dimension for symbol in symbols_set)
+        self.__validate_input(dict_validate)
+        solved = solve(self.law, variable, dict=True)[0][variable]
+        result_expr = solved.subs(dict_quantities)
+        result = Quantity(result_expr)
+        self.__validate_output(variable, result)
+        return result
 
-        dict_validate = dict(
-            zip(variables_for_calculate, dimensions)
-        )
+    def __check_symbol_in_equation(self, symbol: Symbol) -> None:
+        if symbol not in self.symbols:
+            raise ValueError(f"The specified variable {symbol} is not in the law.")
 
-        @validate_input(**dict_validate)
-        @validate_output(variable.dimension)
-        def __calculate_variable(*quantities_: Quantity | Fraction | Probability) -> Quantity | Fraction | Probability:
-            solved = solve(self.law, variable, dict=True)[0][variable]
-            result_expr = solved.subs(zip(symbols_set, quantities_))
-            result = Quantity(result_expr)
-            return result
+    def __get_validate_input_dict(self, dict_of_quantities: dict[Symbol, Quantity]) -> dict[Dimension, Dimension]:
+        quantity_type = lambda quantity: quantity.dimension if isinstance(quantity, Quantity) else dimensionless
+        dict_validate = {symbol.dimension: quantity_type(quantity) for symbol, quantity in dict_of_quantities.items()}
+        return dict_validate
 
-        return __calculate_variable(*dict_quantities.values())
+    def __validate_input(self, validate_dict: dict[Dimension, Dimension]) -> None:
+        for symbol_dimension, quantity_dimension in validate_dict.items():
+            if isinstance(quantity_dimension, Dimension) and symbol_dimension != quantity_dimension:
+                raise errors.UnitsError(f"Expected dimension: {symbol_dimension}, received: {quantity_dimension}.")
+
+    def __validate_output(self, variable: Symbol, result: Quantity) -> None:
+        result_type = lambda quantity: quantity.dimension if isinstance(quantity, Quantity) else dimensionless
+        variable_dimension = variable.dimension
+        result_dimension = result_type(result)
+        if variable_dimension != result_type(result):
+            raise errors.UnitsError(f"Expected dimension: {variable_dimension}, received: {result_dimension}.")
--- test/core/base_law_test.py
+++ test/core/base_law_test.py
@@ -7,11 +7,11 @@ from symplyphysics import (
     Quantity,
 )
 
-from symplyphysics.core import base_law
+from symplyphysics.core.base_law import BaseLaw
 from symplyphysics.laws.optics import linear_magnification_from_object_height_and_image_height as magnification_law
 
 Args = namedtuple("Args", ["ho", "hi"])
-law = base_law.BaseLaw(magnification_law.law)
+law = BaseLaw(magnification_law.law)
 
 
 @fixture(name="test_args")
@@ -23,10 +23,10 @@ def test_args_fixture() -> Args:
 
 def test_basic_linear_magnification(test_args: Args) -> None:
     dict_for_calculating = {
-        magnification_law.object_height: test_args.ho,
-        magnification_law.image_height: test_args.hi
+        law.object_height: test_args.ho,
+        law.image_height: test_args.hi
     }
-    result = law.calculate_symbol_value(magnification_law.magnification, dict_for_calculating)
+    result = law.calculate_symbol_value(law.magnification, dict_for_calculating)
     assert_equal(result, 0.714)
 
 
@@ -37,26 +37,26 @@ def test_bad_height_object(test_args: Args) -> None:
             magnification_law.object_height: hob,
             magnification_law.image_height: test_args.hi
         }
-        law.calculate_symbol_value(magnification_law.magnification, dict_for_calculating)
-    with raises(TypeError):
+        law.calculate_symbol_value(law.magnification, dict_for_calculating)
+    with raises(errors.UnitsError):
         dict_for_calculating = {
             magnification_law.object_height: 100,
             magnification_law.image_height: test_args.hi
         }
-        law.calculate_symbol_value(magnification_law.magnification, dict_for_calculating)
+        law.calculate_symbol_value(law.magnification, dict_for_calculating)
 
 
 def test_bad_height_image(test_args: Args) -> None:
     hib = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
         dict_for_calculating = {
-            magnification_law.object_height: test_args.ho,
-            magnification_law.image_height: hib
+            law.object_height: test_args.ho,
+            law.image_height: hib
         }
-        law.calculate_symbol_value(magnification_law.magnification, dict_for_calculating)
-    with raises(TypeError):
+        law.calculate_symbol_value(law.magnification, dict_for_calculating)
+    with raises(errors.UnitsError):
         dict_for_calculating = {
             magnification_law.object_height: test_args.ho,
             magnification_law.image_height: 100
         }
-        law.calculate_symbol_value(magnification_law.magnification, dict_for_calculating)
+        law.calculate_symbol_value(law.magnification, dict_for_calculating)
