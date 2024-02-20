9c682d45c14f6ad02493d6cf544051ca001f1a92
Ростислав Житков <rosjit1@gmail.com>
2024-02-20 20:31:20 +0300

    Commit #3. Add changes.

--- symplyphysics/laws/thermodynamics/gas_pressure_from_temperature.py
+++ symplyphysics/laws/thermodynamics/gas_pressure_from_temperature.py
@@ -9,11 +9,12 @@ from symplyphysics import (
 )
 
 # Description
-## The gas pressure at a certain temperature depends on the temperature, standard pressure and thermal coefficient.
+## The change in gas pressure when the temperature changes relative to 273 kelvin depends on temperature,
+## standard pressure and thermal coefficient.
 ## https://www.fxyz.ru/формулы_по_физике/термодинамика_теория_теплоты/тепловое_расширение/расширение_газа/термический_коэффициент_давления/
 
 ## Law is: p = p0 * y * T, where
-## p - pressure,
+## p - pressure change,
 ## p0 - pressure at zero degrees celsius,
 ## y - thermal coefficient,
 ## T - temperature.
@@ -21,14 +22,14 @@ from symplyphysics import (
 # Conditions:
 ## - the volume of gas remains constant when heated
 
-pressure = Symbol("pressure", units.pressure)
+pressure_change = Symbol("pressure", units.pressure)
 
 standard_pressure = Symbol("standard_pressure", units.pressure)
 thermal_coefficient = Symbol("thermal_coefficient", 1 / units.temperature)
 temperature = Symbol("temperature", units.temperature)
 
 
-law = Eq(pressure, standard_pressure * thermal_coefficient * temperature)
+law = Eq(pressure_change, standard_pressure * thermal_coefficient * temperature)
 
 
 def print_law() -> str:
@@ -36,9 +37,9 @@ def print_law() -> str:
 
 
 @validate_input(standard_pressure_=standard_pressure, thermal_coefficient_=thermal_coefficient, temperature_=temperature)
-@validate_output(pressure)
-def calculate_pressure(standard_pressure_: Quantity, thermal_coefficient_: Quantity, temperature_: Quantity) -> Quantity:
-    result_expr = solve(law, pressure, dict=True)[0][pressure]
+@validate_output(pressure_change)
+def calculate_pressure_change(standard_pressure_: Quantity, thermal_coefficient_: Quantity, temperature_: Quantity) -> Quantity:
+    result_expr = solve(law, pressure_change, dict=True)[0][pressure_change]
     result_expr = result_expr.subs({
         standard_pressure: standard_pressure_,
         thermal_coefficient: thermal_coefficient_,
--- test/thermodynamics/gas_pressure_from_temperature_test.py
+++ test/thermodynamics/gas_pressure_from_temperature_test.py
@@ -5,7 +5,7 @@ from symplyphysics.laws.thermodynamics import gas_pressure_from_temperature as p
 
 # Description
 ## With a pressure coefficient of 0.003661 [1 / kelvin], a temperature of 293.15 kelvin and a standard pressure of 101325 pascal,
-## the changed pressure will be 108744 pascal.
+## the pressure change will be 108744 pascal.
 ## https://www.indigomath.ru/formuly-po-fizike/molekuljarnaja-kinetika/temperaturnaja-zavisimost-davlenija-gaza.html
 ## https://www.fxyz.ru/%D1%84%D0%BE%D1%80%D0%BC%D1%83%D0%BB%D1%8B_%D0%BF%D0%BE_%D1%84%D0%B8%D0%B7%D0%B8%D0%BA%D0%B5/%D1%82%D0%B5%D1%80%D0%BC%D0%BE%D0%B4%D0%B8%D0%BD%D0%B0%D0%BC%D0%B8%D0%BA%D0%B0_%D1%82%D0%B5%D0%BE%D1%80%D0%B8%D1%8F_%D1%82%D0%B5%D0%BF%D0%BB%D0%BE%D1%82%D1%8B/%D1%82%D0%B5%D0%BF%D0%BB%D0%BE%D0%B2%D0%BE%D0%B5_%D1%80%D0%B0%D1%81%D1%88%D0%B8%D1%80%D0%B5%D0%BD%D0%B8%D0%B5/%D1%80%D0%B0%D1%81%D1%88%D0%B8%D1%80%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B3%D0%B0%D0%B7%D0%B0/%D1%82%D0%B5%D1%80%D0%BC%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9_%D0%BA%D0%BE%D1%8D%D1%84%D1%84%D0%B8%D1%86%D0%B8%D0%B5%D0%BD%D1%82_%D0%B4%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F/
 
@@ -23,8 +23,8 @@ def test_args_fixture() -> Args:
         temperature=temperature)
 
 
-def test_basic_pressure(test_args: Args) -> None:
-    result = pressure_law.calculate_pressure(test_args.standard_pressure,
+def test_basic_pressure_change(test_args: Args) -> None:
+    result = pressure_law.calculate_pressure_change(test_args.standard_pressure,
         test_args.thermal_coefficient, test_args.temperature)
     assert_equal(result, 108744 * units.pascal, tolerance=0.01)
 
@@ -32,28 +32,28 @@ def test_basic_pressure(test_args: Args) -> None:
 def test_bad_pressure(test_args: Args) -> None:
     standard_pressure = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        pressure_law.calculate_pressure(standard_pressure, test_args.thermal_coefficient,
+        pressure_law.calculate_pressure_change(standard_pressure, test_args.thermal_coefficient,
             test_args.temperature)
     with raises(TypeError):
-        pressure_law.calculate_pressure(100, test_args.thermal_coefficient,
+        pressure_law.calculate_pressure_change(100, test_args.thermal_coefficient,
             test_args.temperature)
 
 
 def test_bad_thermal_coefficient(test_args: Args) -> None:
     thermal_coefficient = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        pressure_law.calculate_pressure(test_args.standard_pressure, thermal_coefficient,
+        pressure_law.calculate_pressure_change(test_args.standard_pressure, thermal_coefficient,
             test_args.temperature)
     with raises(TypeError):
-        pressure_law.calculate_pressure(test_args.standard_pressure, 100,
+        pressure_law.calculate_pressure_change(test_args.standard_pressure, 100,
             test_args.temperature)
 
 
 def test_bad_temperature(test_args: Args) -> None:
     temperature = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        pressure_law.calculate_pressure(test_args.standard_pressure,
+        pressure_law.calculate_pressure_change(test_args.standard_pressure,
             test_args.thermal_coefficient, temperature)
     with raises(TypeError):
-        pressure_law.calculate_pressure(test_args.standard_pressure,
+        pressure_law.calculate_pressure_change(test_args.standard_pressure,
             test_args.thermal_coefficient, 100)
