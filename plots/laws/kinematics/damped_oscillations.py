3bafc89fc8c2f2804722f4b70606a7fc553abf2a
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-25 04:57:21 +0400

    Finish plot

--- plots/laws/kinematics/damped_oscillations.py
+++ plots/laws/kinematics/damped_oscillations.py
@@ -1,46 +1,52 @@
 #!/usr/bin/env python3
 
 from collections import namedtuple
-from sympy import Eq, solve, dsolve, Expr
+from sympy import dsolve, Expr, symbols, Function as SymFunction
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
 from symplyphysics.definitions import damped_harmonic_oscillator_equation as damped_eqn
 
+displacement = symbols("displacement", cls=SymFunction, real=True)
+time = symbols("time", positive=True)
+
 Data = namedtuple("Data", "zeta label color")
 
 DATAS = (
-    Data(zeta=0.5, label="underdamping", color="red"),
-    Data(zeta=1.0, label="critical damping", color="blue"),
-    Data(zeta=2.0, label="overdamping", color="magenta"),
+    Data(zeta=0.3, label="underdamping", color="green"),
+    Data(zeta=0.999999, label="critical damping", color="red"),  # cannot put 1.0 due to rounding errors
+    Data(zeta=2, label="overdamping", color="blue"),
 )
 
-OMEGA = 1.0
+OMEGA = 1.5
 INITIAL_POSITION = 1.0
-INITIAL_VELOCITY = 0.0
+INITIAL_VELOCITY = -3.0
 
-def get_solution(data: Data) -> Expr:
-    # eqn = damped_eqn.definition.subs({
-    #     damped_eqn.damping_ratio: data.zeta,
-    #     damped_eqn.undamped_angular_frequency: OMEGA,
-    # })
-    # dsolved = dsolve(eqn, damped_eqn.displacement(damped_eqn.time)).rhs
-    coefs = solve(
-        [
-            Eq(INITIAL_POSITION, dsolved.subs(damped_eqn.time, 0)),
-            Eq(INITIAL_VELOCITY, dsolved.diff(damped_eqn.time).subs(damped_eqn.time, 0)),
-        ],
-        ("C1", "C2"),
-        dict=True,
-    )[0]
-    dsolved_subs = dsolved.subs(coefs)
-    print(dsolved_subs.free_symbols)
-    return dsolved_subs
+initial_conditions = {
+    displacement(0): INITIAL_POSITION,
+    displacement(time).diff(time).subs(time, 0): INITIAL_VELOCITY,
+}
 
+eqn = damped_eqn.definition.subs(
+    damped_eqn.time, time,
+).subs({
+    damped_eqn.displacement(time): displacement(time),
+    damped_eqn.undamped_angular_frequency: OMEGA,
+})
+
+
+def get_solution(zeta: float) -> Expr:
+    eqn_subs = eqn.subs(damped_eqn.damping_ratio, zeta)
+    dsolved = dsolve(
+        eqn_subs,
+        displacement(time),
+        ics=initial_conditions,
+    ).rhs
+    return dsolved
 
 p = plot(
     title="Damped oscillations for various values of damping ratio",
     xlabel="time, s",
-    ylabel="position, m",
+    ylabel="displacement, m",
     backend=MatplotlibBackend,
     legend=True,
     show=False,
@@ -48,9 +54,10 @@ p = plot(
 )
 
 for data in DATAS:
+    sol = get_solution(data.zeta)
     sub_p = plot(
-        get_solution(data),
-        (damped_eqn.time, 0, 5),
+        sol,
+        (time, 0, 10),
         label=data.label,
         line_color=data.color,
         show=False
