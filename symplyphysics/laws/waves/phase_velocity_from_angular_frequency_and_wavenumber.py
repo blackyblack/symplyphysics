74c42017de0ab82b522f009a7fe44975dc30c7bf
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-02-25 11:37:15 +0400

    Rename files to introduce phase velocity of wave (#425)

--- symplyphysics/laws/waves/phase_velocity_from_angular_frequency_and_wavenumber.py
+++ symplyphysics/laws/waves/phase_velocity_from_angular_frequency_and_wavenumber.py
@@ -10,18 +10,19 @@ from symplyphysics import (
 )
 
 # Description
-## The speed of a traveling wave can be found using its angular frequency and wavenumber.
+## The phase velocity of a wave is the rate at which the wave propagates in a medium.
+## It is the velocity at which the phase of one frequency component of the wave travels.
 
-# Law: v = w/k
-## v - wave speed
+# Law: v = w / k
+## v - phase velocity of wave
 ## w - angular frequency of wave
 ## k - angular wavenumber of wave
 
-wave_speed = Symbol("wave_speed", units.length / units.time, real=True)
+phase_velocity = Symbol("phase_velocity", units.length / units.time, real=True)
 angular_frequency = Symbol("angular_frequency", angle_type / units.time, positive=True)
 angular_wavenumber = Symbol("angular_wavenumber", angle_type / units.length, positive=True)
 
-law = Eq(wave_speed, angular_frequency / angular_wavenumber)
+law = Eq(phase_velocity, angular_frequency / angular_wavenumber)
 
 
 # TODO: derive from wave phase equation
@@ -35,8 +36,8 @@ def print_law() -> str:
     angular_frequency_=angular_frequency,
     angular_wavenumber_=angular_wavenumber,
 )
-@validate_output(wave_speed)
-def calculate_wave_speed(
+@validate_output(phase_velocity)
+def calculate_phase_velocity(
     angular_frequency_: Quantity,
     angular_wavenumber_: Quantity,
 ) -> Quantity:
