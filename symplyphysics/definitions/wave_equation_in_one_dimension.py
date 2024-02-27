4a5933e5872a0496b98d866b2a2fabdb2fbe92a9
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-27 04:59:09 +0400

    Add calculate function and test

--- symplyphysics/definitions/wave_equation_in_one_dimension.py
+++ symplyphysics/definitions/wave_equation_in_one_dimension.py
@@ -8,8 +8,12 @@ from sympy import (
 from symplyphysics import (
     units,
     Symbol,
+    Quantity,
     print_expression,
+    validate_input,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.core.quantity_decorator import validate_output_same
 
 # Description
 ## The wave equation is a second-order linear partial differential equation used to
@@ -39,6 +43,59 @@ definition = Eq(
     Derivative(displacement(position, time), time, 2) / phase_velocity**2,
 )
 
+# A subset of solutions has the form `u = u_m * cos(x + v*t + phi)`
+## u_m - amplitude of displacement
+## phi - phase lag
+
+amplitude = symbols("amplitude", positive=True)
+phase_lag = symbols("phase_lag", real=True)
+_length_unit = Symbol("length_unit", units.length)
+
+# `phase_velocity` can be negative or positive, depending on the direction of wave propagation
+# - negative values denote propagation in the positive direction of x-axis
+# - positive values denote propagation in the negative direction of x-axis
+solution = amplitude * cos((position + phase_velocity * time) / _length_unit + phase_lag)
+
+_lhs = definition.lhs.subs(displacement(position, time), solution).doit()
+_rhs = definition.rhs.subs(displacement(position, time), solution).doit()
+assert expr_equals(_lhs, _rhs)
+
+# Check that the equation holds for waves traveling in the opposite direction
+_solution = solution.subs(phase_velocity, -1 * phase_velocity)
+_lhs = definition.lhs.subs(displacement(position, time), _solution).doit()
+_rhs = definition.rhs.subs(displacement(position, time), _solution).doit()
+assert expr_equals(_lhs, _rhs)
+
+
 
 def print_law() -> str:
     return print_expression(definition)
+
+
+@validate_input(
+    phase_velocity_=phase_velocity,
+    position_=position,
+    time_=time,
+)
+@validate_output_same("amplitude_")
+def calculate_displacement(
+    amplitude_: Quantity,
+    phase_velocity_: Quantity,
+    phase_lag_: float,
+    position_: Quantity,
+    time_: Quantity,
+) -> Quantity:
+    """
+    This function only works for a subset of possible solutions of the wave equation,
+    namely those proportional to `cos(x + v*t + phi)` for some phase lag `phi`.
+    """
+
+    result = solution.subs({
+        amplitude: amplitude_,
+        phase_velocity: phase_velocity_,
+        phase_lag: phase_lag_,
+        position: position_,
+        time: time_,
+        _length_unit: Quantity(1.0 * units.meter),
+    })
+    return Quantity(result)
