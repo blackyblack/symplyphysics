357b23a13b3d6bcf96e13c6d9bd7c9bd47276122
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-02-27 21:33:05 +0300

    Commit #1. voltage_in_rotating_rod (#436)

---
+++ symplyphysics/laws/electricity/voltage_in_rotating_rod.py
@@ -0,0 +1,49 @@
+from sympy import (Eq, solve)
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    angle_type,
+)
+
+# Description
+## Let the rod rotate in a uniform magnetic field. The plane of rotation is perpendicular to the magnetic
+## field lines. The axis of rotation passes through one of the ends of the rod. Then the voltage generated
+## at the ends of the rod depends on the magnitude of the magnetic induction, the rotation frequency and
+## the length of the rod.
+
+## Law is: V = (1 / 2) * B * w * L^2, where
+## V - voltage,
+## B - magnetic induction,
+## w - rotation frequency,
+## L - rod length.
+
+voltage = Symbol("voltage", units.voltage)
+
+magnetic_induction = Symbol("magnetic_induction", units.magnetic_density)
+rotation_frequency = Symbol("rotation_frequency", angle_type / units.time)
+rod_length = Symbol("rod_length", units.length)
+
+law = Eq(voltage, (1 / 2) * magnetic_induction * rotation_frequency * rod_length**2)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(magnetic_induction_=magnetic_induction,
+    rotation_frequency_=rotation_frequency,
+    rod_length_=rod_length)
+@validate_output(voltage)
+def calculate_voltage(magnetic_induction_: Quantity, rotation_frequency_: Quantity,
+    rod_length_: Quantity) -> Quantity:
+    result_expr = solve(law, voltage, dict=True)[0][voltage]
+    result_expr = result_expr.subs({
+        magnetic_induction: magnetic_induction_,
+        rotation_frequency: rotation_frequency_,
+        rod_length: rod_length_
+    })
+    return Quantity(result_expr)
---
+++ test/electricity/voltage_in_rotating_rod_test.py
@@ -0,0 +1,59 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from sympy import pi
+from symplyphysics import (assert_equal, units, Quantity, errors)
+from symplyphysics.laws.electricity import voltage_in_rotating_rod as voltage_law
+
+# Description
+## The length of the rod is 0.1 meter, the magnetic field induction is 0.4 tesla, and the rotation
+## frequency of the rod is 2 * pi * 16 radian per second. Then the voltage of the rod will be 0.201 volt.
+## https://www.chertov.org.ua/viev_zadachi.php?num=11&par=25
+
+Args = namedtuple("Args", ["magnetic_induction", "rotation_frequency", "rod_length"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    magnetic_induction = Quantity(0.4 * units.tesla)
+    rotation_frequency = Quantity(2 * pi * 16 * (units.radian / units.second))
+    rod_length = Quantity(0.1 * units.meter)
+
+    return Args(magnetic_induction=magnetic_induction,
+        rotation_frequency=rotation_frequency,
+        rod_length=rod_length)
+
+
+def test_basic_voltage(test_args: Args) -> None:
+    result = voltage_law.calculate_voltage(test_args.magnetic_induction,
+        test_args.rotation_frequency, test_args.rod_length)
+    assert_equal(result, 0.201 * units.volt)
+
+
+def test_bad_magnetic_induction(test_args: Args) -> None:
+    magnetic_induction = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        voltage_law.calculate_voltage(magnetic_induction, test_args.rotation_frequency,
+            test_args.rod_length)
+    with raises(TypeError):
+        voltage_law.calculate_voltage(100, test_args.rotation_frequency,
+            test_args.rod_length)
+
+
+def test_bad_rotation_frequency(test_args: Args) -> None:
+    rotation_frequency = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        voltage_law.calculate_voltage(test_args.magnetic_induction, rotation_frequency,
+            test_args.rod_length)
+    with raises(TypeError):
+        voltage_law.calculate_voltage(test_args.magnetic_induction, 100,
+            test_args.rod_length)
+
+
+def test_bad_rod_length(test_args: Args) -> None:
+    rod_length = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        voltage_law.calculate_voltage(test_args.magnetic_induction,
+            test_args.rotation_frequency, rod_length)
+    with raises(TypeError):
+        voltage_law.calculate_voltage(test_args.magnetic_induction,
+            test_args.rotation_frequency, 100)
