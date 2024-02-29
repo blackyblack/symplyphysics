6db1bc7502565ad6974cff6984743b8db4a59639
Aleksandr Slatov <alexslatov@gmail.com>
2024-02-29 08:46:18 +0400

    Create law and test

---
+++ symplyphysics/laws/kinematic/displacement_in_simple_harmonic_motion.py
@@ -0,0 +1,58 @@
+from sympy import Eq, cos, symbols, Function as SymFunction
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    angle_type,
+    validate_input,
+    print_expression,
+)
+from symplyphysics.core.quantity_decorator import validate_output_same
+from symplyphysics.core.symbols.quantities import scale_factor
+
+# Description
+## Any motion that repeats at regular intervals is called periodic, or harmonic, motion.
+## Simple harmonic motion is a particular type of repeated motion that is a sinusoidal
+## function of time.
+
+# Law: q(t) = q_m * cos(w * t + phi)
+## q(t) - displacement function
+## t - time
+## q_m - oscillation amplitude
+## w - angular frequency of oscillations
+## phi - phase lag
+
+displacement = symbols("displacement", cls=SymFunction, real=True)
+amplitude = symbols("amplitude", positive=True)
+angular_frequency = Symbol("angular_frequency", angle_type / units.time, positive=True)
+phase_lag = Symbol("phase_lag", angle_type, real=True)
+time = Symbol("time", units.time, real=True)
+
+law = Eq(displacement(time), amplitude * cos(angular_frequency * time + phase_lag))
+
+# Derive law from [oscillator equation](../../definitions/harmonic_oscillator_is_second_derivative_equation.py)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    angular_frequency_=angular_frequency,
+    phase_lag_=phase_lag,
+    time_=time,
+)
+@validate_output_same("amplitude_")
+def calculate_displacement(
+    amplitude_: Quantity,
+    angular_frequency_: Quantity,
+    phase_lag_: Quantity | float,
+    time_: Quantity,
+) -> Quantity:
+    result = law.rhs.subs({
+        amplitude: amplitude_,
+        angular_frequency: angular_frequency_,
+        phase_lag: scale_factor(phase_lag_),
+        time: time_,
+    })
+    return Quantity(result)
