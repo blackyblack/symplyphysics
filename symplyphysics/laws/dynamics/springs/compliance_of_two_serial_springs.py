bc7c9115316097aa7b3e045436a14ee907756482
Aleksandr Slatov <alexslatov@gmail.com>
2024-03-05 19:49:00 +0400

    Derive law

--- symplyphysics/laws/dynamics/springs/compliance_of_two_serial_springs.py
+++ symplyphysics/laws/dynamics/springs/compliance_of_two_serial_springs.py
@@ -1,4 +1,4 @@
-from sympy import Eq
+from sympy import Eq, solve, Symbol as SymSymbol
 from symplyphysics import (
     units,
     Quantity,
@@ -6,6 +6,11 @@ from symplyphysics import (
     print_expression,
     validate_input,
     validate_output,
+)
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.definitions import compliance_is_inverse_stiffness as compliance_def
+from symplyphysics.laws.dynamics.springs import (
+    spring_reaction_is_proportional_to_deformation as hookes_law,
 )
 
 # Description
@@ -27,7 +32,35 @@ second_compliance = Symbol("second_compliance", units.length / units.force)
 
 law = Eq(total_compliance, first_compliance + second_compliance)
 
-# TODO: derive law from Hooke's law
+# Derive law from Hooke's law
+## In the case of parallel connection, the force acting on both springs are equal whereas
+## their deformations add up.
+
+_force = SymSymbol("force")
+
+_stiffness_expr = solve(compliance_def.definition, compliance_def.stiffness)[0]
+_first_stiffness = _stiffness_expr.subs(compliance_def.compliance, first_compliance)
+_second_stiffness = _stiffness_expr.subs(compliance_def.compliance, second_compliance)
+_total_stiffness = _stiffness_expr.subs(compliance_def.compliance, total_compliance)
+
+_deformation_expr = solve(
+    hookes_law.law, hookes_law.deformation
+)[0].subs(
+    hookes_law.spring_reaction, _force,
+)
+
+_first_deformation = _deformation_expr.subs(hookes_law.stiffness, _first_stiffness)
+_second_deformation = _deformation_expr.subs(hookes_law.stiffness, _second_stiffness)
+
+_total_deformation_hooke = _deformation_expr.subs(hookes_law.stiffness, _total_stiffness)
+_total_deformation_added = _first_deformation + _second_deformation
+
+_total_compliance_derived = solve(
+    Eq(_total_deformation_hooke, _total_deformation_added),
+    total_compliance,
+)[0]
+
+assert expr_equals(_total_compliance_derived, law.rhs)
 
 
 def print_law() -> str:
