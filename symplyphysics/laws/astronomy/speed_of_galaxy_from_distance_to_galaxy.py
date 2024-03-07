552560bba0f3c3e1967bb5d711cab65a42d3bed1
Ростислав Житков <rosjit1@gmail.com>
2024-03-07 18:30:02 +0300

    Commit #1. speed_of_galaxy_from_distance_to_galaxy

---
+++ symplyphysics/laws/astronomy/speed_of_galaxy_from_distance_to_galaxy.py
@@ -0,0 +1,40 @@
+from sympy import (Eq, solve,)
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## Hubble's Law is a cosmological law describing the expansion of the universe.  It says that the speed of removal of a galaxy
+## depends on the distance to it.
+
+## Law is: v = H * r, where
+## v - speed of galaxy,
+## H - Hubble's constant,
+## r - distance to galaxy.
+
+speed_of_galaxy = Symbol("speed_of_galaxy", units.velocity)
+
+distance_to_galaxy = Symbol("distance_to_galaxy", units.length)
+
+hubble_constant = Quantity(2.2e-18 * (units.second)**(-1))
+
+law = Eq(speed_of_galaxy, hubble_constant * distance_to_galaxy)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(distance_to_galaxy_=distance_to_galaxy)
+@validate_output(speed_of_galaxy)
+def calculate_speed(distance_to_galaxy_: Quantity) -> Quantity:
+    result_expr = solve(law, speed_of_galaxy, dict=True)[0][speed_of_galaxy]
+    result_expr = result_expr.subs({
+        distance_to_galaxy: distance_to_galaxy_,
+    })
+    return Quantity(result_expr)
---
+++ test/astronomy/speed_of_galaxy_from_distance_to_galaxy_test.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (assert_equal, units, Quantity, errors)
+from symplyphysics.laws.astronomy import speed_of_galaxy_from_distance_to_galaxy as speed_law
+
+# Description
+## Let the distance to the galaxy be 4.6285e+22 kilometers. Then the speed of the galaxy is 101827 kilometer per second.
+## http://www.astro.spbu.ru/staff/viva/Book/ch4L/node17.html
+
+Args = namedtuple("Args", ["distance_to_galaxy"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    distance_to_galaxy = Quantity(4.6285e+22 * units.kilometer)
+
+    return Args(distance_to_galaxy=distance_to_galaxy)
+
+
+def test_basic_speed(test_args: Args) -> None:
+    result = speed_law.calculate_speed(test_args.distance_to_galaxy)
+    assert_equal(result, 101827 * units.kilometer / units.second)
+
+
+def test_bad_distance_to_galaxy(test_args: Args) -> None:
+    distance_to_galaxy = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        speed_law.calculate_speed(distance_to_galaxy)
+    with raises(TypeError):
+        speed_law.calculate_speed(100)
