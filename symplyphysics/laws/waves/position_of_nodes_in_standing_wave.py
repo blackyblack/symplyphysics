9673b56fe46c4da0227aec4a55209e4ba4ea1a51
Aleksandr Slatov <alexslatov@gmail.com>
2024-03-07 20:39:05 +0400

    Add proof

--- symplyphysics/laws/waves/position_of_nodes_in_standing_wave.py
+++ symplyphysics/laws/waves/position_of_nodes_in_standing_wave.py
@@ -8,6 +8,9 @@ from symplyphysics import (
     validate_input,
     validate_output,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.definitions import angular_wavenumber_is_inverse_wavelength as wavenumber_def
+from symplyphysics.laws.waves import displacement_in_standing_wave as standing_wave_law
 
 # Description
 ## In a standing wave, the locations of zero amplitude are called nodes. These locations, however,
@@ -23,6 +26,20 @@ integer_factor = Symbol("integer_factor", dimensionless, integer=True)
 wavelength = Symbol("wavelength", units.length, positive=True)
 
 law = Eq(node_position, integer_factor * wavelength / 2)
+
+# Proving these are indeed locations of zero amplitude.
+# Deriving it from the standing wave expression is impossible since `sympy` does not produce
+# infinite solutions for trigonometric equations.
+
+_standing_wave_expr = standing_wave_law.law.rhs
+
+_angular_wavenumber = wavenumber_def.definition.rhs.subs(wavenumber_def.wavelength, wavelength)
+
+_standing_wave_expr = _standing_wave_expr.subs(standing_wave_law.angular_wavenumber, _angular_wavenumber)
+
+_node_amplitude = _standing_wave_expr.subs(standing_wave_law.position, law.rhs)
+
+assert expr_equals(_node_amplitude, 0)
 
 
 def print_law() -> str:
