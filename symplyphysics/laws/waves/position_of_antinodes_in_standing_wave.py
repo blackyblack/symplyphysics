0653e3a60e5b49db416c2422ede97aadf75c2775
Aleksandr Slatov <alexslatov@gmail.com>
2024-03-07 21:09:39 +0400

    Add proof

--- symplyphysics/laws/waves/position_of_antinodes_in_standing_wave.py
+++ symplyphysics/laws/waves/position_of_antinodes_in_standing_wave.py
@@ -8,6 +8,9 @@ from symplyphysics import (
     validate_input,
     validate_output,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.definitions import angular_wavenumber_is_inverse_wavelength as wavenumber_def
+from symplyphysics.laws.waves import displacement_in_standing_wave as standing_wave_law
 
 # Description
 ## In a standing wave, the locations of maximum amplitude are called antinodes. These locations are
@@ -24,6 +27,27 @@ integer_factor = Symbol("integer_factor", dimensionless, integer=True)
 wavelength = Symbol("wavelength", units.length, positive=True)
 
 law = Eq(antinode_position, (integer_factor + S(1)/2) * wavelength / 2)
+
+# Proving these are indeed locations of maximum amplitude.
+# Deriving it from the standing wave expression is impossible since `sympy` does not produce
+# infinite solutions for trigonometric equations.
+
+_standing_wave_expr = standing_wave_law.law.rhs
+
+_angular_wavenumber = wavenumber_def.definition.rhs.subs(wavenumber_def.wavelength, wavelength)
+
+_standing_wave_expr = _standing_wave_expr.subs(standing_wave_law.angular_wavenumber, _angular_wavenumber)
+_standing_wave_spatial_derivative = _standing_wave_expr.diff(standing_wave_law.position)
+
+_standing_wave_spatial_derivative_at_antinodes = _standing_wave_spatial_derivative.subs(
+    standing_wave_law.position, law.rhs
+)
+
+assert expr_equals(_standing_wave_spatial_derivative_at_antinodes, 0)
+
+# Since the spatial derivative of the wave is zero in antinodes, according to calculus
+# these are the points of extrema of the wave, therefore the locations of the maximum
+# amplitude of the wave.
 
 
 def print_law() -> str:
