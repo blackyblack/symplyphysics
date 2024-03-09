a4033ff6609aa85cfb4bb4878008536cd7cc1c0b
Ростислав Житков <rosjit1@gmail.com>
2024-03-09 20:50:54 +0300

    Commit #1. penetrating_power_of_telescope

---
+++ symplyphysics/laws/astronomy/penetrating_power_of_telescope.py
@@ -0,0 +1,38 @@
+from sympy import (Eq, solve, log, S,)
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    dimensionless,
+    convert_to
+)
+
+# Description
+## The penetrating force is the magnitude of the faintest stars visible with a telescope when observed at the zenith.
+
+## Law is: m = 2.5 + 5 * lg(D), where
+## m - penetrating power of telescope,
+## D - diameter of the lens in millimeters.
+
+penetrating_power_telescope= Symbol("penetrating_power_telescope", dimensionless)
+
+lens_diameter = Symbol("lens_diameter", units.length)
+
+law = Eq(penetrating_power_telescope, 2.5 + 5 * log(lens_diameter / Quantity(1 * units.millimeter),10))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(lens_diameter_=lens_diameter)
+@validate_output(penetrating_power_telescope)
+def calculate_penetrating_power(lens_diameter_: Quantity) -> float:
+    result_expr = solve(law, penetrating_power_telescope, dict=True)[0][penetrating_power_telescope]
+    result_expr = result_expr.subs({
+        lens_diameter: lens_diameter_,
+    })
+    return float(convert_to(Quantity(result_expr), S.One).evalf())
