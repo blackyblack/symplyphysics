6972e3827f6090255f47e513fb96e8d7e69f900e
Aleksandr Slatov <alexslatov@gmail.com>
2024-03-12 15:52:33 +0400

    Rename files to avoid abbreviation

--- test/dynamics/damped_oscillations/quality_factor_via_bandwidth_test.py
+++ test/dynamics/damped_oscillations/quality_factor_via_bandwidth_test.py
@@ -6,7 +6,7 @@ from symplyphysics import (
     units,
     Quantity,
 )
-from symplyphysics.laws.dynamics.damped_oscillations import q_factor_via_bandwidth as q_factor_law
+from symplyphysics.laws.dynamics.damped_oscillations import quality_factor_via_bandwidth as q_factor_law
 
 # Description
 ## The resonant frequency of the oscillator is 100 Hz and its bandwidth is 5 Hz. 
