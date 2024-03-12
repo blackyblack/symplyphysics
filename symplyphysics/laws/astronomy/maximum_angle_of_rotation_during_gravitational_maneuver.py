a1b9106fbb18053d7578c8c9cb84fb3ed8c5f723
Ростислав Житков <rosjit1@gmail.com>
2024-03-12 22:36:09 +0300

    Commit #1. maximum_angle_of_rotation_during_gravitational_maneuver

---
+++ symplyphysics/laws/astronomy/maximum_angle_of_rotation_during_gravitational_maneuver.py
@@ -0,0 +1,43 @@
+from sympy import (Eq, solve, atan)
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    angle_type,
+)
+
+# Description
+## A gravitational maneuver is a purposeful change in the trajectory and flight speed of a spacecraft under the influence
+## of the gravitational fields of celestial bodies.
+## The maximum angle of rotation of the rocket during a gravitational maneuver depends on the first cosmic velocity of
+## the planet around which the maneuver is performed, and the speed of the rocket relative to this planet.
+
+## Law is: phi = arctg((v1 / v2)^2), where
+## phi - maximum angle of rotation during a gravitational maneuver,
+## v1 - first cosmic velocity for a given planet,
+## v2 - rocket's velocity relative to the planet.
+
+maximum_angle = Symbol("maximum_angle", angle_type)
+
+first_cosmic_velocity_planet= Symbol("first_cosmic_velocity_planet", units.velocity)
+rocket_speed = Symbol("rocket_speed", units.velocity)
+
+law = Eq(maximum_angle, atan((first_cosmic_velocity_planet/ rocket_speed)**2))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(first_cosmic_velocity_planet_=first_cosmic_velocity_planet, rocket_speed_=rocket_speed)
+@validate_output(maximum_angle)
+def calculate_maximum_angle(first_cosmic_velocity_planet_: Quantity, rocket_speed_: Quantity) -> Quantity:
+    result_expr = solve(law, maximum_angle, dict=True)[0][maximum_angle]
+    result_expr = result_expr.subs({
+        first_cosmic_velocity_planet: first_cosmic_velocity_planet_,
+        rocket_speed: rocket_speed_,
+    })
+    return Quantity(result_expr)
---
+++ test/astronomy/maximum_angle_of_rotation_during_gravitational_maneuver_test.py
@@ -0,0 +1,41 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (assert_equal, units, Quantity, errors,)
+from symplyphysics.laws.astronomy import maximum_angle_of_rotation_during_gravitational_maneuver as angle_law
+
+# Description
+## The Earth's first cosmic velocity is 7.91 kilometers per second. Let the speed of the rocket be equal to the Earth's
+## second cosmic velocity of 11.2 kilometers per second. Then the maximum angle of rotation during a gravitational maneuver
+## is 0.463 radians.
+## https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D1%81%D0%BC%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D1%81%D0%BA%D0%BE%D1%80%D0%BE%D1%81%D1%82%D1%8C
+
+Args = namedtuple("Args", ["first_cosmic_velocity_planet", "rocket_speed"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    first_cosmic_velocity_planet = Quantity(7.91 * units.kilometer / units.second)
+    rocket_speed = Quantity(11.2 * units.kilometer / units.second)
+
+    return Args(first_cosmic_velocity_planet=first_cosmic_velocity_planet, rocket_speed=rocket_speed)
+
+
+def test_basic_maximum_angle(test_args: Args) -> None:
+    result = angle_law.calculate_maximum_angle(test_args.first_cosmic_velocity_planet, test_args.rocket_speed)
+    assert_equal(result, 0.463 * units.radian)
+
+
+def test_bad_first_cosmic_velocity_planet(test_args: Args) -> None:
+    first_cosmic_velocity_planet = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        angle_law.calculate_maximum_angle(first_cosmic_velocity_planet, test_args.rocket_speed)
+    with raises(TypeError):
+        angle_law.calculate_maximum_angle(100, test_args.rocket_speed)
+
+
+def test_bad_rocket_speed(test_args: Args) -> None:
+    rocket_speed = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        angle_law.calculate_maximum_angle(test_args.first_cosmic_velocity_planet, rocket_speed)
+    with raises(TypeError):
+        angle_law.calculate_maximum_angle(test_args.first_cosmic_velocity_planet, 100)
