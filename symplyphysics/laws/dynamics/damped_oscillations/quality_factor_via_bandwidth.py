518c7e106dcc5a15371b9bd72d5b132ce596497e
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-03-12 20:46:02 +0400

    Law: Q factor via bandwidth (#482)
    * Create law and test
    
    * Refactor names to avoid abbreviation
    
    * Rename files to avoid abbreviation
    
    * Refactor description of resonance width

---
+++ symplyphysics/laws/dynamics/damped_oscillations/quality_factor_via_bandwidth.py
@@ -0,0 +1,53 @@
+from sympy import Eq
+from symplyphysics import (
+    units,
+    dimensionless,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## Q factor (quality factor) is a dimensionless parameter that describes how underdamped 
+## an oscillator or resonator is: the larger the Q factor is, the less damped it is. There 
+## are two nearly equivalent definitions of it that become approximately equivalent as Q 
+## becomes larger, meaning that the resonator becomes less damped.
+
+# Law: Q = f_r / delta_f
+## Q - Q factor
+## f_r - oscillator's resonant frequency
+## delta_f - resonance width, or full width at half maximum, of the peak in the graph of the
+##           dissipated power as a function of driving frequency, i.e. the difference between
+##           the frequencies at which the dissipated power is half the peak dissipated power,
+##           which happens ad the resonant frequency, see [this figure](http://spiff.rit.edu/classes/phys283/lectures/forced_ii/half_power.png)
+
+# Note
+## - An equivalent definition uses angular frequencies instead of linear ones.
+
+quality_factor = Symbol("quality_factor", dimensionless)
+resonant_frequency = Symbol("resonant_frequency", units.frequency)
+resonance_width = Symbol("resonance_width", units.frequency)
+
+law = Eq(quality_factor, resonant_frequency / resonance_width)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    resonant_frequency_=resonant_frequency,
+    resonance_width_=resonance_width,
+)
+@validate_output(quality_factor)
+def calculate_quality_factor(
+    resonant_frequency_: Quantity,
+    resonance_width_: Quantity,
+) -> Quantity:
+    result = law.rhs.subs({
+        resonant_frequency: resonant_frequency_,
+        resonance_width: resonance_width_,
+    })
+    return Quantity(result)
