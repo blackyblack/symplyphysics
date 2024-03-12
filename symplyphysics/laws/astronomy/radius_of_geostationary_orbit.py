3cad974b4af30445b8b75d0c6b29c103fffcd129
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-03-12 21:30:49 +0300

    radius_of_geostationary_orbit (#486)
    * Commit #1. radius_of_geostationary_orbit
    
    * add tests

---
+++ symplyphysics/laws/astronomy/radius_of_geostationary_orbit.py
@@ -0,0 +1,43 @@
+from sympy import (Eq, solve)
+from sympy.physics.units import gravitational_constant
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    angle_type,
+)
+
+# Description
+## A geostationary orbit is a circular orbit located above the Earth's equator (0° latitude), where an artificial satellite orbits the planet
+## with an angular velocity equal to the angular velocity of the Earth's rotation around its axis.
+
+## Law is: R = (G * M / (w^2))^(1/3), where
+## R - radius of geostationary orbit,
+## G - gravitational constant,
+## M - mass of planet.
+## w - angular speed of rotation of satellite.
+
+radius_of_orbit = Symbol("radius_of_orbit", units.length)
+
+mass_of_planet = Symbol("mass_of_planet", units.mass)
+speed_rotation_satellite = Symbol("speed_rotation_satellite", angle_type / units.time)
+
+law = Eq(radius_of_orbit, (gravitational_constant * mass_of_planet / (speed_rotation_satellite**2))**(1/3))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(mass_of_planet_=mass_of_planet, speed_rotation_satellite_=speed_rotation_satellite)
+@validate_output(radius_of_orbit)
+def calculate_radius_of_orbit(mass_of_planet_: Quantity, speed_rotation_satellite_: Quantity) -> Quantity:
+    result_expr = solve(law, radius_of_orbit, dict=True)[0][radius_of_orbit]
+    result_expr = result_expr.subs({
+        mass_of_planet: mass_of_planet_,
+        speed_rotation_satellite: speed_rotation_satellite_,
+    })
+    return Quantity(result_expr)
---
+++ test/astronomy/radius_of_geostationary_orbit_test.py
@@ -0,0 +1,40 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (assert_equal, units, Quantity, errors,)
+from symplyphysics.laws.astronomy import radius_of_geostationary_orbit as radius_law
+
+# Description
+## The mass of the Earth is 5.9742e24 kilograms. At a rotational speed of 7.29e-5 radians per second,
+## the radius of the orbit is 42164 kilometers.
+## https://ru.wikipedia.org/wiki/Геостационарная_орбита
+
+Args = namedtuple("Args", ["mass_of_planet", "speed_rotation_satellite"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    mass_of_planet = Quantity(5.9742e24 * units.kilogram)
+    speed_rotation_satellite = Quantity(7.29e-5 * units.radian / units.second)
+
+    return Args(mass_of_planet=mass_of_planet, speed_rotation_satellite=speed_rotation_satellite)
+
+
+def test_basic_radius_of_orbit(test_args: Args) -> None:
+    result = radius_law.calculate_radius_of_orbit(test_args.mass_of_planet, test_args.speed_rotation_satellite)
+    assert_equal(result, 42164 * units.kilometer)
+
+
+def test_bad_mass_of_planet(test_args: Args) -> None:
+    mass_of_planet = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        radius_law.calculate_radius_of_orbit(mass_of_planet, test_args.speed_rotation_satellite)
+    with raises(TypeError):
+        radius_law.calculate_radius_of_orbit(100, test_args.speed_rotation_satellite)
+
+
+def test_bad_speed_rotation_satellite(test_args: Args) -> None:
+    speed_rotation_satellite = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        radius_law.calculate_radius_of_orbit(test_args.mass_of_planet, speed_rotation_satellite)
+    with raises(TypeError):
+        radius_law.calculate_radius_of_orbit(test_args.mass_of_planet, 100)
