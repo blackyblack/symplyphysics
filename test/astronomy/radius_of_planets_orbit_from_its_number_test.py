eb0d4684f5c0114520f28b5120f52f2eee032be9
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-03-15 02:08:31 +0300

    radius_of_planets_orbit_from_its_number (#492)
    * Commit #1. radius_of_planets_orbit_from_its_number
    
    * change description and function

---
+++ test/astronomy/radius_of_planets_orbit_from_its_number_test.py
@@ -0,0 +1,29 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (assert_equal, units, Quantity, errors)
+from symplyphysics.laws.astronomy import radius_of_planets_orbit_from_its_number as radius_law
+
+# Description
+## The first number of the planet corresponds to the Earth. Then the radius of the orbit will be equal to 1 astronomical unit.
+
+Args = namedtuple("Args", ["number_of_planet"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    number_of_planet = 1
+
+    return Args(number_of_planet=number_of_planet)
+
+
+def test_basic_radius_of_orbit(test_args: Args) -> None:
+    result = radius_law.calculate_radius_of_orbit(test_args.number_of_planet)
+    assert_equal(result, 1 * units.astronomical_unit)
+
+
+def test_bad_number_of_planet() -> None:
+    number_of_planet = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        radius_law.calculate_radius_of_orbit(number_of_planet)
+    with raises(ValueError):
+        radius_law.calculate_radius_of_orbit(-10)
