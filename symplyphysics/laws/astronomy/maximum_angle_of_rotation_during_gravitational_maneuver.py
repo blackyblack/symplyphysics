55fe21867471c9c52c85accc8b5038a3b9bffa52
Ростислав Житков <rosjit1@gmail.com>
2024-03-15 17:05:27 +0300

    change description and add conditions

--- symplyphysics/laws/astronomy/maximum_angle_of_rotation_during_gravitational_maneuver.py
+++ symplyphysics/laws/astronomy/maximum_angle_of_rotation_during_gravitational_maneuver.py
@@ -14,11 +14,15 @@ from symplyphysics import (
 ## of the gravitational fields of celestial bodies.
 ## The maximum angle of rotation of the rocket during a gravitational maneuver depends on the first cosmic velocity of
 ## the planet around which the maneuver is performed, and the speed of the rocket relative to this planet.
+## Law describes the closest possible maneuver, when aiming range is at planet radius.
 
 ## Law is: phi = arctg((v1 / v2)^2), where
 ## phi - maximum angle of rotation during a gravitational maneuver (angle at which the velocity vector of the rocket rotates),
 ## v1 - first cosmic velocity for a given planet,
 ## v2 - rocket's velocity relative to the planet.
+
+# Conditions:
+# - law describes the closest possible maneuver, when aiming range is at planet radius.
 
 maximum_angle = Symbol("maximum_angle", angle_type)
 
