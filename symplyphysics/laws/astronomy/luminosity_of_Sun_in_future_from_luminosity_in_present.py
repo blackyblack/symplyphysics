965ceec1942c084132bc1496bfd54227703d83e6
Ростислав Житков <rosjit1@gmail.com>
2024-03-15 18:34:07 +0300

    add changes

--- symplyphysics/laws/astronomy/luminosity_of_Sun_in_future_from_luminosity_in_present.py
+++ symplyphysics/laws/astronomy/luminosity_of_Sun_in_future_from_luminosity_in_present.py
@@ -22,9 +22,11 @@ from symplyphysics import (
 luminosity_future = Symbol("luminosity_future", dimensionless)
 
 luminosity_present = Symbol("luminosity_present", dimensionless)
-time = Symbol("time", dimensionless)
+time = Symbol("time", units.time)
 
-law = Eq(luminosity_future, luminosity_present * ((5.59 / time) - 1.41 + 0.26 * time))
+time_constant = Quantity(1e9 * units.common_year)
+
+law = Eq(luminosity_future, luminosity_present * ((5.59 / (time / time_constant)) - 1.41 + 0.26 * (time / time_constant)))
 
 
 def print_law() -> str:
@@ -33,7 +35,7 @@ def print_law() -> str:
 
 @validate_input(luminosity_present_=luminosity_present, time_=time)
 @validate_output(luminosity_future)
-def calculate_luminosity_future(luminosity_present_: float, time_: float) -> float:
+def calculate_luminosity_future(luminosity_present_: float, time_: Quantity) -> float:
     result_expr = solve(law, luminosity_future, dict=True)[0][luminosity_future]
     result_expr = result_expr.subs({
         luminosity_present: luminosity_present_,
--- test/astronomy/luminosity_of_Sun_in_future_from_luminosity_in_present_test.py
+++ test/astronomy/luminosity_of_Sun_in_future_from_luminosity_in_present_test.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from pytest import fixture, raises
 from symplyphysics import (assert_equal, units, Quantity, errors,)
-from symplyphysics.laws.astronomy import luminosity_of_Sun_in_future_from_luminosity_in_present as luminosity_law
+from symplyphysics.laws.astronomy import luminosity_of_sun_in_future_from_luminosity_in_present as luminosity_law
 
 # Description
 ## Let the time be equal to 15 billion years. Then the luminosity of the Sun in the future will be equal to 2.863 units of the luminosity of the Sun.
@@ -12,7 +12,7 @@ Args = namedtuple("Args", ["luminosity_present", "time"])
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
     luminosity_present = 1
-    time = 15
+    time = Quantity(15e9 * units.common_year)
 
     return Args(luminosity_present=luminosity_present, time=time)
 
@@ -32,3 +32,5 @@ def test_bad_time(test_args: Args) -> None:
     time = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
         luminosity_law.calculate_luminosity_future(test_args.luminosity_present, time)
+    with raises(TypeError):
+        luminosity_law.calculate_luminosity_future(test_args.luminosity_present, 100)
