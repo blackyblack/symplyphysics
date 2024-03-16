b90cff6e750e01d1d93587105736d6942cbc0fcc
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-03-16 20:17:47 +0300

    luminosity_of_Sun_in_past_from_luminosity_in_present (#503)
    * Commit #1. luminosity_of_Sun_in_past_from_luminosity_in_present
    
    * add changes
    
    * adadsa
    
    * asdasdsa
    
    * Add changes

---
+++ symplyphysics/laws/astronomy/luminosity_of_sun_in_past_from_luminosity_in_present.py
@@ -0,0 +1,46 @@
+from sympy import (Eq, solve, S,)
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    convert_to,
+    dimensionless,
+)
+
+# Description
+## The luminosity of the Sun in the past is related to the luminosity of the Sun in the present. Luminosity is
+## indicated in units of solar luminosity. One unit is equal to the luminosity of the Sun at a given time.
+## There are two different formulas to calculate luminosity in the past and in the future. It is because it is
+## non linear and requires different approximations for past and future.
+
+## Law is: Lpast = L / (1 + 0.4 * (1 - (t / 4.6))), where
+## Lpast - luminosity of the sun in past,
+## L - luminosity of the sun in present,
+## t - time in billions of years.
+
+luminosity_past = Symbol("luminosity_past", dimensionless)
+
+luminosity_present = Symbol("luminosity_present", dimensionless)
+time = Symbol("time", units.time)
+
+one_billion_years = Quantity(1e9 * units.common_year)
+
+law = Eq(luminosity_past, luminosity_present / (1 + 0.4 * (1 - ((time / one_billion_years) / 4.6))))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(luminosity_present_=luminosity_present, time_=time)
+@validate_output(luminosity_past)
+def calculate_luminosity_past(luminosity_present_: float, time_: Quantity) -> float:
+    result_expr = solve(law, luminosity_past, dict=True)[0][luminosity_past]
+    result_expr = result_expr.subs({
+        luminosity_present: luminosity_present_,
+        time: time_,
+    })
+    return float(convert_to(Quantity(result_expr), S.One).evalf())
---
+++ test/astronomy/luminosity_of_sun_in_past_from_luminosity_in_present_test.py
@@ -0,0 +1,36 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (assert_equal, units, Quantity, errors,)
+from symplyphysics.laws.astronomy import luminosity_of_sun_in_past_from_luminosity_in_present as luminosity_law
+
+# Description
+## Let the time be equal to 1 billion years. Then the luminosity of the Sun in the past will be equal to 0.762 units of the luminosity of the Sun.
+
+Args = namedtuple("Args", ["luminosity_present", "time"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    luminosity_present = 1
+    time = Quantity(1e9 * units.common_year)
+
+    return Args(luminosity_present=luminosity_present, time=time)
+
+
+def test_basic_luminosity_past(test_args: Args) -> None:
+    result = luminosity_law.calculate_luminosity_past(test_args.luminosity_present, test_args.time)
+    assert_equal(result, 0.762)
+
+
+def test_bad_luminosity_present(test_args: Args) -> None:
+    luminosity_present = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        luminosity_law.calculate_luminosity_past(luminosity_present, test_args.time)
+
+
+def test_bad_time(test_args: Args) -> None:
+    time = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        luminosity_law.calculate_luminosity_past(test_args.luminosity_present, time)
+    with raises(TypeError):
+        luminosity_law.calculate_luminosity_past(test_args.luminosity_present, 100)
