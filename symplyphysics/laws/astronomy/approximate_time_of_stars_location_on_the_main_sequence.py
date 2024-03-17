709a71879a0c125a5ecdb7b40a591a32bbd41e52
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-03-18 00:17:05 +0300

    Commit #1. approximate_time_of_stars_location_on_the_main_sequence (#510)

---
+++ symplyphysics/laws/astronomy/approximate_time_of_stars_location_on_the_main_sequence.py
@@ -0,0 +1,50 @@
+from sympy import (Eq, solve,)
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## The main sequence is the stage of stellar evolution.
+## This stage begins in stars after the protostar stage. At the beginning of the main sequence stage, the age of the star
+## is considered to be zero.
+## It is possible to calculate the time spent on the main sequence for a star by knowing such a time for the Sun, as well
+## as the mass and luminosity of the Sun and the star.
+
+## Law is: T = ts * (M / Ms) * (Ls / L), where
+## T - lifetime on the main sequence of the star,
+## ts - lifetime on the main sequence of the Sun,
+## M - mass of the star,
+## Ms - mass of the Sun,
+## Ls - luminosity of the Sun,
+## L - luminosity of the star.
+
+lifetime = Symbol("lifetime", units.time)
+
+mass_of_star = Symbol("mass_of_star", units.mass)
+luminosity_of_star = Symbol("luminosity_of_star", units.power)
+
+mass_of_sun = Quantity(1.989e30 * units.kilogram)
+lifetime_of_sun = Quantity(1e10 * units.common_year)
+luminosity_of_sun = Quantity(3.827e26 * units.watt)
+
+law = Eq(lifetime, lifetime_of_sun * (mass_of_star / mass_of_sun) * (luminosity_of_sun / luminosity_of_star))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(mass_of_star_=mass_of_star, luminosity_of_star_=luminosity_of_star)
+@validate_output(lifetime)
+def calculate_lifetime(mass_of_star_: Quantity, luminosity_of_star_: float) -> Quantity:
+    result_expr = solve(law, lifetime, dict=True)[0][lifetime]
+    result_expr = result_expr.subs({
+        mass_of_star: mass_of_star_,
+        luminosity_of_star: luminosity_of_star_,
+    })
+    return Quantity(result_expr)
