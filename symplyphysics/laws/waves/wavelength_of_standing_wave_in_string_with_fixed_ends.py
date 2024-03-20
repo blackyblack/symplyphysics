243e67754897cd8e8a9cd4966c275d6438f63931
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-03-20 14:35:34 +0400

    Derive law (#513)

--- symplyphysics/laws/waves/wavelength_of_standing_wave_in_string_with_fixed_ends.py
+++ symplyphysics/laws/waves/wavelength_of_standing_wave_in_string_with_fixed_ends.py
@@ -8,6 +8,9 @@ from symplyphysics import (
     validate_output,
     dimensionless,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.definitions import angular_wavenumber_is_inverse_wavelength as wavenumber_def
+from symplyphysics.laws.waves import displacement_in_standing_wave as standing_wave_law
 
 # Description
 ## If a standing wave occurs in a string with fixed ends, the value of the displacement function
@@ -19,13 +22,38 @@ from symplyphysics import (
 ## lambda - wavelength of standing wave
 ## L - length of the string
 
-integer_factor = Symbol("integer_factor", dimensionless, positive=True)
+integer_factor = Symbol("integer_factor", dimensionless, integer=True, positive=True)
 wavelength = Symbol("wavelength", units.length, positive=True)
 string_length = Symbol("string_length", units.length, positive=True)
 
 law = Eq(integer_factor * wavelength / 2, string_length)
 
-# TODO: derive from boundary condition `u(L, t) = 0`
+# Derive from boundary condition `u(L, t) = 0`
+
+_wavenumber = wavenumber_def.definition.rhs.subs(
+    wavenumber_def.wavelength, wavelength,
+)
+
+_standing_wave = standing_wave_law.law.rhs.subs(
+    standing_wave_law.angular_wavenumber, _wavenumber,
+)
+
+_standing_wave_at_string_end = _standing_wave.subs(
+    standing_wave_law.position, string_length,
+)
+
+_wavelength_solution = solve(_standing_wave_at_string_end, wavelength)[0]
+
+# We can show that any wavelength that is a whole number of times shorter than the
+# one we just got as the solution of the equation is also a resonant wavelength.
+
+_wavelength_solution_set = _wavelength_solution / integer_factor
+
+assert expr_equals(_standing_wave_at_string_end.subs(wavelength, _wavelength_solution_set), 0)
+
+_wavelength_from_law = solve(law, wavelength)[0]
+
+assert expr_equals(_wavelength_solution_set, _wavelength_from_law)
 
 
 def print_law() -> str:
