88d68d315698368a725d9a39998072634551040c
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-03-20 18:10:40 +0400

    Derive: standing wave (#518)
    * Derive law
    
    * Fix proof to mention another law and note

--- symplyphysics/laws/waves/displacement_in_standing_wave.py
+++ symplyphysics/laws/waves/displacement_in_standing_wave.py
@@ -7,7 +7,9 @@ from symplyphysics import (
     print_expression,
     validate_input,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.core.quantity_decorator import validate_output_same
+from symplyphysics.laws.waves import phase_of_traveling_wave as phase_law
 
 # Description
 ## A standing, or stationary, wave is the result of the interference of two identical waves
@@ -37,7 +39,23 @@ time = Symbol("time", units.time, real=True)
 law = Eq(displacement(position, time),
     (2 * amplitude) * sin(angular_wavenumber * position) * cos(angular_frequency * time))
 
-# TODO: Derive from the sum of two traveling waves
+# Derive from the sum of two traveling waves
+
+_wave_phase = phase_law.law.rhs.subs({
+    phase_law.angular_wavenumber: angular_wavenumber,
+    phase_law.position: position,
+    phase_law.angular_frequency: angular_frequency,
+    phase_law.time: time,
+})
+
+# The form of the waves is taken from the note above
+_forward_wave = amplitude * sin(_wave_phase)
+
+_backward_wave = _forward_wave.subs(angular_frequency, -1 * angular_frequency)
+
+_sum_of_waves = (_forward_wave + _backward_wave).simplify()
+
+assert expr_equals(_sum_of_waves, law.rhs)
 
 
 def print_law() -> str:
