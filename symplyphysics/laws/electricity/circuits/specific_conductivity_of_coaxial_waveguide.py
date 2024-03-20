f3ccd0da27cb049e27bdc747d565051d5a53ea27
Ростислав Житков <rosjit1@gmail.com>
2024-03-20 19:23:53 +0300

    add changes

--- symplyphysics/laws/electricity/circuits/specific_conductivity_of_coaxial_waveguide.py
+++ symplyphysics/laws/electricity/circuits/specific_conductivity_of_coaxial_waveguide.py
@@ -1,40 +1,40 @@
 from sympy import Eq, solve
 from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
-    validate_output, dimensionless)
+    validate_output, dimensionless, angle_type,)
 
 ## Description
 ## A coaxial waveguide is an electrical cable consisting of a central conductor and a shield arranged coaxially and separated
 ## by an insulating material or an air gap. It is used to transmit radio frequency electrical signals.
-## The specific conductivity of a coaxial waveguide depends on the frequency of signal and the specific capacity of coaxial waveguide,
+## The specific conductivity of a coaxial waveguide depends on the frequency of signal and the specific capacitance of coaxial waveguide,
 ## as well as on the tangent of the dielectric loss angle of the insulator material.
 
 ## Law is: G = w * C * tan(d), where
 ## G - specific conductivity of coaxial waveguide,
-## w - frequency of signal,
-## C - specific capacity of coaxial waveguide,
+## w - angular frequency of signal,
+## C - specific capacitance of coaxial waveguide,
 ## tan(d) - tangent of the dielectric loss angle of the insulator material.
 
 specific_conductivity = Symbol("specific_conductivity", units.conductance / units.length)
 
-frequency = Symbol("frequency", 1 / units.time)
-specific_capacity = Symbol("specific_capacity", units.capacitance / units.length)
+frequency = Symbol("frequency", angle_type / units.time)
+specific_capacitance = Symbol("specific_capacitance", units.capacitance / units.length)
 tangent_dielectric_loss_angle = Symbol("tangent_dielectric_loss_angle", dimensionless)
 
-law = Eq(specific_conductivity, frequency * specific_capacity * tangent_dielectric_loss_angle)
+law = Eq(specific_conductivity, frequency * specific_capacitance * tangent_dielectric_loss_angle)
 
 
 def print_law() -> str:
     return print_expression(law)
 
 
-@validate_input(frequency_=frequency, specific_capacity_=specific_capacity, tangent_dielectric_loss_angle_=tangent_dielectric_loss_angle)
+@validate_input(frequency_=frequency, specific_capacitance_=specific_capacitance, tangent_dielectric_loss_angle_=tangent_dielectric_loss_angle)
 @validate_output(specific_conductivity)
-def calculate_specific_conductivity(frequency_: Quantity, specific_capacity_: Quantity,
+def calculate_specific_conductivity(frequency_: Quantity, specific_capacitance_: Quantity,
     tangent_dielectric_loss_angle_: float) -> Quantity:
     result_velocity_expr = solve(law, specific_conductivity, dict=True)[0][specific_conductivity]
     result_expr = result_velocity_expr.subs({
         frequency: frequency_,
-        specific_capacity: specific_capacity_,
+        specific_capacitance: specific_capacitance_,
         tangent_dielectric_loss_angle: tangent_dielectric_loss_angle_
     })
     return Quantity(result_expr)
--- test/electricity/circuits/specific_conductivity_of_coaxial_waveguide_test.py
+++ test/electricity/circuits/specific_conductivity_of_coaxial_waveguide_test.py
@@ -5,24 +5,24 @@ from symplyphysics import (errors, units, Quantity, assert_equal, prefixes)
 
 from symplyphysics.laws.electricity.circuits import specific_conductivity_of_coaxial_waveguide as conductivity_law
 
-## Parameters of the coaxial waveguide: the angent of the dielectric loss angle is 1e-4, the specific capacitance is 64.5 picofarad per meter.
+## Parameters of the coaxial waveguide: the tangent of the dielectric loss angle is 1e-4, the specific capacitance is 64.5 picofarad per meter.
 ## The frequency of signal is 2 * pi * 100e6 radians per second. The specific conductivity will be 4.053 microsiemens per meter.
 ## https://old.study.urfu.ru/view/aid/67/1/resonators.pdf
 ## https://test-energy.ru/tangens-ugla-dielektricheskih-poter/
 
-Args = namedtuple("Args", ["frequency", "specific_capacity", "tangent_dielectric_loss_angle"])
+Args = namedtuple("Args", ["frequency", "specific_capacitance", "tangent_dielectric_loss_angle"])
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
-    frequency = Quantity(2 * pi * 100e6 * (1 / units.second))
-    specific_capacity = Quantity(64.5 * prefixes.pico * units.farad / units.meter)
+    frequency = Quantity(2 * pi * 100e6 * (units.radian / units.second))
+    specific_capacitance = Quantity(64.5 * prefixes.pico * units.farad / units.meter)
     tangent_dielectric_loss_angle = 1e-4
-    return Args(frequency=frequency, specific_capacity=specific_capacity, tangent_dielectric_loss_angle=tangent_dielectric_loss_angle)
+    return Args(frequency=frequency, specific_capacitance=specific_capacitance, tangent_dielectric_loss_angle=tangent_dielectric_loss_angle)
 
 
 def test_basic_specific_conductivity(test_args: Args) -> None:
-    result = conductivity_law.calculate_specific_conductivity(test_args.frequency, test_args.specific_capacity,
+    result = conductivity_law.calculate_specific_conductivity(test_args.frequency, test_args.specific_capacitance,
         test_args.tangent_dielectric_loss_angle)
     assert_equal(result, 4.053 * prefixes.micro * units.siemens / units.meter)
 
@@ -30,10 +30,10 @@ def test_basic_specific_conductivity(test_args: Args) -> None:
 def test_bad_frequency(test_args: Args) -> None:
     bad_frequency = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        conductivity_law.calculate_specific_conductivity(bad_frequency, test_args.specific_capacity, test_args.tangent_dielectric_loss_angle)
+        conductivity_law.calculate_specific_conductivity(bad_frequency, test_args.specific_capacitance, test_args.tangent_dielectric_loss_angle)
 
 
 def test_bad_tangent_dielectric_loss_angle(test_args: Args) -> None:
     tangent_dielectric_loss_angle = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        conductivity_law.calculate_specific_conductivity(test_args.frequency, test_args.specific_capacity, tangent_dielectric_loss_angle)
+        conductivity_law.calculate_specific_conductivity(test_args.frequency, test_args.specific_capacitance, tangent_dielectric_loss_angle)
