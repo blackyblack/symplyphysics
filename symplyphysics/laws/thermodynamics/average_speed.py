4f127aa4b1ed0b6b0a3aae459b172c2e675acb39
Aleksandr Slatov <alexslatov@gmail.com>
2024-03-21 16:09:42 +0400

    Update proof description

--- symplyphysics/laws/thermodynamics/average_speed.py
+++ symplyphysics/laws/thermodynamics/average_speed.py
@@ -33,7 +33,11 @@ law = Eq(
     sqrt(8 * units.boltzmann_constant * equilibrium_temperature / (pi * particle_mass)),
 )
 
-# Derive law from Maxwell-Boltzmann distribution functions
+# Derive law from Maxwell-Boltzmann distribution function using the probability formula 
+# of finding the average value of a function y(x) when the distribution function f(x) of 
+# the random variable x is known, which is calculating the integral of y(x)*f(x) with the 
+# integration limits being the lowest and highest possible values of x. In the case of 
+# this law, it is the integral of v*f(v) with v ranging from 0 to infinity.
 
 _distribution = speed_distribution.law.rhs.subs({
     speed_distribution.equilibrium_temperature: equilibrium_temperature,
