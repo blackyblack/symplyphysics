6f3b287d43ccd1e928306d8fd34484942acac46e
Ростислав Житков <rosjit1@gmail.com>
2024-03-24 22:08:36 +0300

    Commit #1. attenuation_coefficient_in_metal_in_rectangular_waveguide_for_transverse_magnetic_waves

---
+++ symplyphysics/laws/electricity/circuits/attenuation_coefficient_in_metal_in_rectangular_waveguide_for_transverse_magnetic_waves.py
@@ -0,0 +1,69 @@
+from sympy import (Eq, solve, sqrt,)
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+    validate_output, dimensionless,)
+
+
+# Description
+## A coaxial waveguide is an electrical cable consisting of a central conductor and a shield arranged coaxially and separated
+## by an insulating material or an air gap. It is used to transmit radio frequency electrical signals.
+## The specific resistance of a coaxial waveguide depends on the radius of the outer conductor and the radius of the inner conductor,
+## as well as on the relative permeability of the insulator material, frequency of signal and specific conductivity of conductor.
+## The attenuation coefficient shows how many times the transmitted signal weakens per unit length of the coaxial waveguide.
+
+## Law is: am = 2 * Rs * (n^2 * (b / a)^3 + m^2) / (Z0 * b * sqrt((1 - (L / L1)^2) * (n^2 * (b / a)^2 + m^2))), where
+## am - attenuation coefficient in metal,
+## Rs - surface resistance,
+## m - first index,
+## n - second index,
+## a - height of the waveguide cross section,
+## b - width of the waveguide cross section,
+## Z0 - characteristic resistance of the material filling the waveguide,
+## L - wavelength,
+## L1 - critical wavelength.
+
+
+attenuation_coefficient = Symbol("attenuation_coefficient", dimensionless / units.length)
+
+surface_resistance = Symbol("surface_resistance", units.impedance)
+first_index = Symbol("first_index", dimensionless)
+second_index = Symbol("second_index", dimensionless)
+width = Symbol("width", units.length)
+height = Symbol("height", units.length)
+resistance_of_medium = Symbol("resistance_of_medium", units.impedance)
+signal_wavelength = Symbol("signal_wavelength", units.length)
+critical_wavelength = Symbol("critical_wavelength", units.length)
+
+expression_1 = 2 * surface_resistance * (second_index**2 * (width / height)**3 + first_index**2)
+expression_2 = (1 - (signal_wavelength / critical_wavelength)**2) * (second_index**2 * (width / height)**2 + first_index**2)
+
+law = Eq(attenuation_coefficient, expression_1 / (resistance_of_medium * width * sqrt(expression_2)))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(surface_resistance_=surface_resistance,
+    first_index_=first_index,
+    second_index_=second_index,
+    width_=width,
+    height_=height,
+    resistance_of_medium_=resistance_of_medium,
+    signal_wavelength_=signal_wavelength,
+    critical_wavelength_=critical_wavelength)
+@validate_output(attenuation_coefficient)
+def calculate_attenuation_coefficient(surface_resistance_: Quantity, first_index_: float,
+    second_index_: float, width_: Quantity, height_: Quantity,
+    resistance_of_medium_: Quantity, signal_wavelength_: Quantity, critical_wavelength_: Quantity) -> Quantity:
+    result_expr = solve(law, attenuation_coefficient, dict=True)[0][attenuation_coefficient]
+    result_expr = result_expr.subs({
+        surface_resistance: surface_resistance_,
+        first_index: first_index_,
+        second_index: second_index_,
+        width: width_,
+        height: height_,
+        resistance_of_medium: resistance_of_medium_,
+        signal_wavelength: signal_wavelength_,
+        critical_wavelength: critical_wavelength_,
+    })
+    return Quantity(result_expr)
