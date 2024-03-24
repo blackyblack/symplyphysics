8b59c57fe19c0ae64281ffc45841b35b06eb28f0
Ростислав Житков <rosjit1@gmail.com>
2024-03-24 18:48:53 +0300

    add changes

--- symplyphysics/laws/electricity/circuits/wavelength_in_rectangular_waveguide.py
+++ symplyphysics/laws/electricity/circuits/wavelength_in_rectangular_waveguide.py
@@ -12,24 +12,24 @@ from symplyphysics import (units, Quantity, Symbol, print_expression, validate_i
 ## L - wavelength,
 ## L1 - critical wavelength.
 
-wavelength_waveguide = Symbol("wavelength_waveguide", units.length)
+waveguide_wavelength = Symbol("waveguide_wavelength", units.length)
 
-wavelength = Symbol("wavelength", units.length)
+signal_wavelength = Symbol("signal_wavelength", units.length)
 critical_wavelength = Symbol("critical_wavelength", units.length)
 
-law = Eq(wavelength_waveguide, wavelength / sqrt(1 - (wavelength / critical_wavelength)**2))
+law = Eq(waveguide_wavelength, signal_wavelength / sqrt(1 - (signal_wavelength / critical_wavelength)**2))
 
 
 def print_law() -> str:
     return print_expression(law)
 
 
-@validate_input(wavelength_=wavelength, critical_wavelength_=critical_wavelength)
-@validate_output(wavelength_waveguide)
-def calculate_wavelength_waveguide(wavelength_: Quantity, critical_wavelength_: Quantity) -> Quantity:
-    result_velocity_expr = solve(law, wavelength_waveguide, dict=True)[0][wavelength_waveguide]
+@validate_input(signal_wavelength_=signal_wavelength, critical_wavelength_=critical_wavelength)
+@validate_output(waveguide_wavelength)
+def calculate_waveguide_wavelength(signal_wavelength_: Quantity, critical_wavelength_: Quantity) -> Quantity:
+    result_velocity_expr = solve(law, waveguide_wavelength, dict=True)[0][waveguide_wavelength]
     result_expr = result_velocity_expr.subs({
-        wavelength: wavelength_,
+        signal_wavelength: signal_wavelength_,
         critical_wavelength: critical_wavelength_
     })
     return Quantity(result_expr)
--- test/electricity/circuits/wavelength_in_rectangular_waveguide_test.py
+++ test/electricity/circuits/wavelength_in_rectangular_waveguide_test.py
@@ -8,28 +8,28 @@ from symplyphysics.laws.electricity.circuits import wavelength_in_rectangular_wa
 ## Then the wavelength in the waveguide will be 12.057 millimeter.
 ## https://old.study.urfu.ru/view/aid/67/1/resonators.pdf
 
-Args = namedtuple("Args", ["wavelength", "critical_wavelength"])
+Args = namedtuple("Args", ["signal_wavelength", "critical_wavelength"])
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
-    wavelength = Quantity(10 * units.millimeter)
+    signal_wavelength = Quantity(10 * units.millimeter)
     critical_wavelength = Quantity(17.9 * units.millimeter)
-    return Args(wavelength=wavelength, critical_wavelength=critical_wavelength)
+    return Args(signal_wavelength=signal_wavelength, critical_wavelength=critical_wavelength)
 
 
-def test_basic_wavelength_waveguide(test_args: Args) -> None:
-    result = wavelength_law.calculate_wavelength_waveguide(test_args.wavelength, test_args.critical_wavelength)
+def test_basic_waveguide_wavelength(test_args: Args) -> None:
+    result = wavelength_law.calculate_waveguide_wavelength(test_args.signal_wavelength, test_args.critical_wavelength)
     assert_equal(result, 12.057 * units.millimeter)
 
 
 def test_bad_wavelength(test_args: Args) -> None:
     bad_wavelength = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        wavelength_law.calculate_wavelength_waveguide(bad_wavelength, test_args.critical_wavelength)
+        wavelength_law.calculate_waveguide_wavelength(bad_wavelength, test_args.critical_wavelength)
     with raises(TypeError):
-        wavelength_law.calculate_wavelength_waveguide(100, test_args.critical_wavelength)
+        wavelength_law.calculate_waveguide_wavelength(100, test_args.critical_wavelength)
     with raises(errors.UnitsError):
-        wavelength_law.calculate_wavelength_waveguide(test_args.wavelength, bad_wavelength)
+        wavelength_law.calculate_waveguide_wavelength(test_args.signal_wavelength, bad_wavelength)
     with raises(TypeError):
-        wavelength_law.calculate_wavelength_waveguide(test_args.wavelength, 100)
+        wavelength_law.calculate_waveguide_wavelength(test_args.signal_wavelength, 100)
