0229248b00299221bb5a9c6973da9b3e010eb7ff
Ростислав Житков <rosjit1@gmail.com>
2024-03-28 21:10:06 +0300

    Commit #1. quality_factor_of_filled_rectangular_resonator

---
+++ symplyphysics/laws/electricity/circuits/quality_factor_of_filled_rectangular_resonator.py
@@ -0,0 +1,43 @@
+from sympy import (Eq, solve, S)
+from symplyphysics import (
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    convert_to,
+    dimensionless
+)
+
+# Description
+## A rectangular resonator consists of metal walls and a material filling it.
+## In the case when the resonator is empty, its quality factor depends only on the losses in the metal walls of the resonator.
+## The quality factor shows the ratio of the energy stored in the resonator to the energy loss during one oscillation period.
+## In the case of a filled resonator, the Q factor also depends on the losses in the dielectric.
+
+## Law is: Q = 1 / ((1 / Qm) + tan(d)), where
+## Q - quality factor of the filled resonator,
+## Qm - quality factor of the empty resonator,
+## tan(d) - tangent of the dielectric loss angle of the material filling the resonator.
+
+quality_factor = Symbol("quality_factor", dimensionless)
+
+empty_resonator_quality_factor = Symbol("empty_resonator_quality_factor", dimensionless)
+tangent_dielectric_loss_angle = Symbol("tangent_dielectric_loss_angle", dimensionless)
+
+law = Eq(quality_factor, 1 / ((1 / empty_resonator_quality_factor) + tangent_dielectric_loss_angle))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(empty_resonator_quality_factor_=empty_resonator_quality_factor, tangent_dielectric_loss_angle_=tangent_dielectric_loss_angle)
+@validate_output(quality_factor)
+def calculate_quality_factor(empty_resonator_quality_factor_: float, tangent_dielectric_loss_angle_: float) -> float:
+    result_expr = solve(law, quality_factor, dict=True)[0][quality_factor]
+    result_expr = result_expr.subs({
+        empty_resonator_quality_factor: empty_resonator_quality_factor_,
+        tangent_dielectric_loss_angle: tangent_dielectric_loss_angle_,
+    })
+    return float(convert_to(Quantity(result_expr), S.One).evalf())
