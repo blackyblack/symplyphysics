8bb021cec109a2b4cb9b96d431f9596fd638082a
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-03-29 11:09:17 +0400

    Definition: Lorentz factor (#570)
    * Create law and test
    
    * Correct text number

---
+++ test/definitions/lorentz_factor_test.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    Quantity,
+)
+from symplyphysics.definitions import lorentz_factor
+
+# Description
+## The speed of an object is 2e5 km/s. Its Lorentz factor is 1.34
+
+Args = namedtuple("Args", "v")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    v = Quantity(2e5 * units.kilometer / units.second)
+    return Args(v=v)
+
+
+def test_law(test_args: Args) -> None:
+    result = lorentz_factor.calculate_lorentz_factor(test_args.v)
+    assert_equal(result, 1.34, tolerance=2e-3)
+
+
+def test_bad_velocity() -> None:
+    vb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        lorentz_factor.calculate_lorentz_factor(vb)
+    with raises(TypeError):
+        lorentz_factor.calculate_lorentz_factor(100)
