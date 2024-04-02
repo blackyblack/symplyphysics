4470e8c5ee424c0cb39e761bd102ec1828952059
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-02 15:44:15 +0400

    Create definition and test

---
+++ symplyphysics/definitions/heat_capacity_ratio.py
@@ -0,0 +1,49 @@
+from sympy import Eq
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## The heat capacity ratio, also known as the adiabatic index, the ratio of specific heats, or
+## the insentropic expansion factor, is the ratio of the heat capacity at constant pressure
+## to that of constant volume. The heat capacity ratio is used in the description of thermodynamic
+## reversible processes; the speed of sound also depends on this factor.
+
+# Law: gamma = C_p / C_V
+## gamma - heat capacity ratio
+## C_p - heat capacity at constant pressure
+## C_V - heat capacity at constant volume
+
+# Note:
+## One can also use specific or molar heat capacities in place of the normal heat capacity.
+
+heat_capacity_ratio = Symbol("heat_capacity_ratio")
+isobaric_heat_capacity = Symbol("isobaric_heat_capacity", units.energy / units.temperature)
+isochoric_heat_capacity = Symbol("isochoric_heat_capacity", units.energy / units.temperature)
+
+definition = Eq(heat_capacity_ratio, isobaric_heat_capacity / isochoric_heat_capacity)
+
+
+def print_law() -> str:
+    return print_expression(definition)
+
+
+@validate_input(
+    isobaric_heat_capacity_=isobaric_heat_capacity,
+    isochoric_heat_capacity_=isochoric_heat_capacity,
+)
+@validate_output(heat_capacity_ratio)
+def calculate_heat_capacity_ratio(
+    isobaric_heat_capacity_: Quantity,
+    isochoric_heat_capacity_: Quantity,
+) -> float:
+    result = definition.rhs.subs({
+        isobaric_heat_capacity: isobaric_heat_capacity_,
+        isochoric_heat_capacity: isochoric_heat_capacity_,
+    })
+    return Quantity(result).scale_factor
