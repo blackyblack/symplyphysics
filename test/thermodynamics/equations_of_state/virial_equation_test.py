2116673188d2318688365737004bdc2323035960
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-04-03 22:40:51 +0400

    Law: virial equation of state (#588)
    * Create law and test
    
    * Add note about virial coefficients
    
    * Add link in law description
    
    * Check for result sign
    
    * Add test case for negative value check

---
+++ test/thermodynamics/equations_of_state/virial_equation_test.py
@@ -0,0 +1,58 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    Quantity,
+)
+from symplyphysics.laws.thermodynamics.equations_of_state import virial_equation
+
+# Description
+## The second and third virial coefficients of a fluid are -800 cm**3/mol and -4 liter**2/mol**2 respectively.
+## At molar density rho = 41 mol/m**3 the compressibility factor is Z = 0.960
+
+Args = namedtuple("Args", "b c rho")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    b = Quantity(-800 * units.centimeter**3 / units.mole)
+    c = Quantity(-4 * units.liter**2 / units.mole**2)
+    rho = Quantity(41 * units.mole / units.meter**3)
+    return Args(b=b, c=c, rho=rho)
+
+
+def test_law(test_args: Args) -> None:
+    result = virial_equation.calculate_compressibility_factor(test_args.b, test_args.c, test_args.rho)
+    assert_equal(result, 0.96)
+
+
+def test_negative_compressibility_factor(test_args: Args) -> None:
+    rho_bad = Quantity(1000 * units.mole / units.meter**3)
+    with raises(ValueError):
+        virial_equation.calculate_compressibility_factor(test_args.b, test_args.c, rho_bad)
+
+
+def test_bad_second_virial_coefficient(test_args: Args) -> None:
+    bb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        virial_equation.calculate_compressibility_factor(bb, test_args.c, test_args.rho)
+    with raises(TypeError):
+        virial_equation.calculate_compressibility_factor(100, test_args.c, test_args.rho)
+
+
+def test_bad_third_virial_coefficient(test_args: Args) -> None:
+    cb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        virial_equation.calculate_compressibility_factor(test_args.b, cb, test_args.rho)
+    with raises(TypeError):
+        virial_equation.calculate_compressibility_factor(test_args.b, 100, test_args.rho)
+
+
+def test_bad_molar_density(test_args: Args) -> None:
+    rho_bad = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        virial_equation.calculate_compressibility_factor(test_args.b, test_args.c, rho_bad)
+    with raises(TypeError):
+        virial_equation.calculate_compressibility_factor(test_args.b, test_args.c, 100)
