faf20ae0f1e03f58ee838a5efccbe7c7318bb5bd
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-03 15:39:52 +0400

    Create law and test

---
+++ symplyphysics/laws/thermodynamics/compressibility_factor_via_intermolecular_force_potential.py
@@ -0,0 +1,88 @@
+from sympy import Eq, Integral, pi, exp, S, Piecewise
+from symplyphysics import (
+    units,
+    dimensionless,
+    Quantity,
+    Symbol,
+    Function,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+from symplyphysics.core.expr_comparisons import expr_equals
+
+# Description
+## The virial equation describes the deviation of a real gas from ideal gas behaviour. The virial coefficients
+## in the virial expansion account for interactions between successively larger groups of molecules. Since
+## interactions between large numbers of molecules are rare, the virial equation is usually truncated at the third
+## term onwards. Under the assumption that only pair interactions are present, the compressibility factor can be
+## linked to the intermolecular force potential:
+
+# Law: Z = 1 + 2 * pi * N / V * Integral((1 - exp(-phi(r) / (k * T))) * r**2, (r, 0, infinity))
+## Z - [compressibility factor](../../definitions/compressibility_factor_is_deviation_from_ideal_gas.py)
+## N - number of particles
+## V - volume
+## r - radius
+## phi(r) - intermolecular force potential as a function of radius
+## k - Boltzmann constant
+## T - absolute temperature
+
+# Conditions
+## - The virial expansion is done up to the second virial coefficient inclusively.
+
+compressibility_factor = Symbol("compressibility_factor", dimensionless, positive=True)
+number_of_particles = Symbol("number_of_particles", dimensionless, integer=True, positive=True)
+volume = Symbol("volume", units.volume, positive=True)
+radius = Symbol("radius", units.length, positive=True)
+intermolecular_force_potential = Function("intermolecular_force_potential", units.energy, real=True)
+temperature = Symbol("temperature", units.temperature, positive=True)
+
+law = Eq(
+    compressibility_factor,
+    1 + 2 * pi * number_of_particles / volume
+    * Integral(
+        (1 - exp(-1 * intermolecular_force_potential(radius) / (units.boltzmann_constant * temperature))) * radius**2,
+        (radius, 0, S.Infinity),
+    )
+)
+
+# Calculate the compressibility factor for the model of hard spheres
+
+_sphere_radius = Symbol("sphere_radius", units.length, positive=True)
+
+_hard_spheres_potential = Piecewise(
+    (S.Infinity, radius < _sphere_radius),
+    (0, radius >= _sphere_radius),
+)
+
+_hard_spheres_compressibility_factor = law.rhs.subs(
+    intermolecular_force_potential(radius), _hard_spheres_potential
+).doit()
+
+# Notice that the compressibility factor does not depend on temperature in this model
+assert expr_equals(_hard_spheres_compressibility_factor.diff(temperature), 0)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    number_of_particles_=number_of_particles,
+    volume_=volume,
+    sphere_radius_=radius,
+)
+@validate_output(compressibility_factor)
+def calculate_compressibility_factor(
+    number_of_particles_: int | float,  # float to allow numbers written in exponent notation
+    volume_: Quantity,
+    sphere_radius_: Quantity,
+) -> float:
+    # Calculate for the model of hard spheres
+
+    result = _hard_spheres_compressibility_factor.subs({
+        number_of_particles: number_of_particles_,
+        volume: volume_,
+        _sphere_radius: sphere_radius_,
+    })
+    return Quantity(result)
---
+++ test/thermodynamics/compressibility_factor_via_intermolecular_force_potential_test.py
@@ -0,0 +1,53 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    Quantity,
+)
+from symplyphysics.laws.thermodynamics import (
+    compressibility_factor_via_intermolecular_force_potential as compressibility_law,
+)
+
+# Description
+## Assuming the model of hard spheres, for a gas with particles of radius r = 4 Å confined
+## in a space of volume V = 1 m**3 with N = 3e25 number of particles, its compressibility factor
+## is approximately Z = 1.004.
+
+Args = namedtuple("Args", "n v r")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    n = 3e25
+    v = Quantity(1 * units.meter**3)
+    r = Quantity(4 * units.angstrom)
+    return Args(n=n, v=v, r=r)
+
+
+def test_law(test_args: Args) -> None:
+    result = compressibility_law.calculate_compressibility_factor(test_args.n, test_args.v, test_args.r)
+    assert_equal(result, 1.004)
+
+
+def test_bad_number_of_particles(test_args: Args) -> None:
+    nb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        compressibility_law.calculate_compressibility_factor(nb, test_args.v, test_args.r)
+
+
+def test_bad_volume(test_args: Args) -> None:
+    vb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        compressibility_law.calculate_compressibility_factor(test_args.n, vb, test_args.r)
+    with raises(TypeError):
+        compressibility_law.calculate_compressibility_factor(test_args.n, 100, test_args.r)
+
+
+def test_bad_radius(test_args: Args) -> None:
+    rb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        compressibility_law.calculate_compressibility_factor(test_args.n, test_args.v, rb)
+    with raises(TypeError):
+        compressibility_law.calculate_compressibility_factor(test_args.n, test_args.v, 100)
