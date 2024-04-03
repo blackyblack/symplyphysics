8b00318260493e79cb9957c29c02cf313ad59f10
Ростислав Житков <rosjit1@gmail.com>
2024-04-03 16:44:12 +0300

    add changes

--- symplyphysics/laws/electricity/circuits/total_resistance_matrix.py
+++ symplyphysics/laws/electricity/circuits/total_resistance_matrix.py
@@ -6,13 +6,14 @@ from symplyphysics import (
     print_expression,
     validate_input,
     validate_output,
+    SI,
 )
 
 ## Description
 ## The impedance matrix is one of the ways to describe a microwave device. The Z-parameters of the device act as elements
 ## of this matrix. The matrix equation relates the input and output voltages to the input and output currents.
 
-## Law is: [U1, U2] = Matrix([[Z11, Z12], [Z21, Z22]]), where
+## Law is: [U1, U2] = Matrix([[Z11, Z12], [Z21, Z22]]) * [I1, I2], where
 ## U1 - input voltage,
 ## U2 - output voltage,
 ## Z11 - ratio of input voltage to input current at idle at the output,
@@ -26,31 +27,36 @@ input_voltage = Symbol("input_voltage", units.voltage)
 output_voltage = Symbol("output_voltage", units.voltage)
 input_current = Symbol("input_current", units.current)
 output_current = Symbol("output_current", units.current)
-impedance_11 = Symbol("impedance_11", units.impedance)
-impedance_12 = Symbol("impedance_12", units.impedance)
-impedance_21 = Symbol("impedance_21", units.impedance)
-impedance_22 = Symbol("impedance_22", units.impedance)
+impedance_input_input = Symbol("impedance_input_input", units.impedance)
+impedance_input_output = Symbol("impedance_input_output", units.impedance)
+impedance_output_input = Symbol("impedance_output_input", units.impedance)
+impedance_output_output = Symbol("impedance_output_output", units.impedance)
 
-law = Eq(Matrix([input_voltage, output_voltage]), Matrix([[impedance_11, impedance_12], [impedance_21, impedance_22]]) * Matrix([input_current, output_current]))
+law = Eq(Matrix([input_voltage, output_voltage]), Matrix([[impedance_input_input, impedance_input_output], [impedance_output_input, impedance_output_output]]) * Matrix([input_current, output_current]))
 
 
 def print_law() -> str:
     return print_expression(law)
 
 
-@validate_input(input_voltage_=input_voltage, output_voltage_=output_voltage, impedances_=impedance_11)
+@validate_input(input_voltage_=input_voltage, output_voltage_=output_voltage,)
 @validate_output(units.current)
 def calculate_currents(input_voltage_: Quantity, output_voltage_: Quantity, impedances_: tuple[tuple[Quantity, Quantity], tuple[Quantity, Quantity]]) -> tuple[Quantity, Quantity]:
+    for impedance_1, impedance_2 in impedances_:
+        assert SI.get_dimension_system().equivalent_dims(impedance_1.dimension,
+            units.impedance)
+        assert SI.get_dimension_system().equivalent_dims(impedance_2.dimension,
+            units.impedance)
     result_currents = solve(law, [input_current, output_current], dict=True)[0]
     result_input_current = result_currents[input_current]
     result_output_current = result_currents[output_current]
     substitutions = {
         input_voltage: input_voltage_,
         output_voltage: output_voltage_,
-        impedance_11: impedances_[0][0],
-        impedance_12: impedances_[0][1],
-        impedance_21: impedances_[1][0],
-        impedance_22: impedances_[1][1],
+        impedance_input_input: impedances_[0][0],
+        impedance_input_output: impedances_[0][1],
+        impedance_output_input: impedances_[1][0],
+        impedance_output_output: impedances_[1][1],
     }
     result_input_current = result_input_current.subs(substitutions)
     result_output_current = result_output_current.subs(substitutions)
--- test/electricity/circuits/total_resistance_matrix_test.py
+++ test/electricity/circuits/total_resistance_matrix_test.py
@@ -9,27 +9,22 @@ from symplyphysics.laws.electricity.circuits import total_resistance_matrix as r
 ## to 100, 50, 50, 1000 ohms, respectively.
 ## Then the values of the input and output currents will be equal to 1.013 ampere and -25.64 milliampere, respectively.
 
-Args = namedtuple("Args", ["input_voltage", "output_voltage", "impedance_11", "impedance_12", "impedance_21", "impedance_22"])
+Args = namedtuple("Args", ["input_voltage", "output_voltage", "impedances"])
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
     input_voltage = Quantity(100 * units.volt)
     output_voltage = Quantity(25 * units.volt)
-    impedance_11 = Quantity(100 * units.ohm)
-    impedance_12 = Quantity(50 * units.ohm)
-    impedance_21 = Quantity(50 * units.ohm)
-    impedance_22 = Quantity(1000 * units.ohm)
+    impedances = ((Quantity(100 * units.ohm), Quantity(50 * units.ohm)), (Quantity(50 * units.ohm), Quantity(1000 * units.ohm)))
     return Args(input_voltage=input_voltage,
         output_voltage=output_voltage,
-        impedance_11=impedance_11,
-        impedance_12=impedance_12,
-        impedance_21=impedance_21,
-        impedance_22=impedance_22)
+        impedances=impedances
+        )
 
 
 def test_basic_currents(test_args: Args) -> None:
-    result = resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedance_11, test_args.impedance_12), (test_args.impedance_21, test_args.impedance_22)))
+    result = resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, test_args.impedances)
     assert_equal(result[0], 1.013 * units.ampere)
     assert_equal(result[1], -25.64 * prefixes.milli * units.ampere)
 
@@ -37,30 +32,30 @@ def test_basic_currents(test_args: Args) -> None:
 def test_bad_voltages(test_args: Args) -> None:
     bad_voltage = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        resistance_matrix_law.calculate_currents(bad_voltage, test_args.output_voltage, ((test_args.impedance_11, test_args.impedance_12), (test_args.impedance_21, test_args.impedance_22)))
+        resistance_matrix_law.calculate_currents(bad_voltage, test_args.output_voltage, test_args.impedances)
     with raises(TypeError):
-        resistance_matrix_law.calculate_currents(100, test_args.output_voltage, ((test_args.impedance_11, test_args.impedance_12), (test_args.impedance_21, test_args.impedance_22)))
+        resistance_matrix_law.calculate_currents(100, test_args.output_voltage, test_args.impedances)
     with raises(errors.UnitsError):
-        resistance_matrix_law.calculate_currents(test_args.input_voltage, bad_voltage, ((test_args.impedance_11, test_args.impedance_12), (test_args.impedance_21, test_args.impedance_22)))
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, bad_voltage, test_args.impedances)
     with raises(TypeError):
-        resistance_matrix_law.calculate_currents(test_args.input_voltage, 100, ((test_args.impedance_11, test_args.impedance_12), (test_args.impedance_21, test_args.impedance_22)))
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, 100, test_args.impedances)
 
 
-# def test_bad_impedances(test_args: Args) -> None:
-#     bad_impedance = Quantity(1 * units.coulomb)
-#     with raises(errors.UnitsError):
-#         resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((bad_impedance, test_args.impedance_12), (test_args.impedance_21, test_args.impedance_22)))
-#     with raises(TypeError):
-#         resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((100, test_args.impedance_12), (test_args.impedance_21, test_args.impedance_22)))
-#     with raises(errors.UnitsError):
-#         resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedance_11, bad_impedance), (test_args.impedance_21, test_args.impedance_22)))
-#     with raises(TypeError):
-#         resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedance_11, 100), (test_args.impedance_21, test_args.impedance_22)))
-#     with raises(errors.UnitsError):
-#         resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedance_11, test_args.impedance_12), (bad_impedance, test_args.impedance_22)))
-#     with raises(TypeError):
-#         resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedance_11, test_args.impedance_12), (100, test_args.impedance_22)))
-#     with raises(errors.UnitsError):
-#         resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedance_11, test_args.impedance_12), (test_args.impedance_21, bad_impedance)))
-#     with raises(TypeError):
-#         resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedance_11, test_args.impedance_12), (test_args.impedance_21, 100)))
+def test_bad_impedances(test_args: Args) -> None:
+    bad_impedance = Quantity(1 * units.coulomb)
+    with raises(AssertionError):
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((bad_impedance, test_args.impedances[0][1]), (test_args.impedances[1][0], test_args.impedances[1][1])))
+    with raises(AttributeError):
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((100, test_args.impedances[0][1]), (test_args.impedances[1][0], test_args.impedances[1][1])))
+    with raises(AssertionError):
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedances[0][0], bad_impedance), (test_args.impedances[1][0], test_args.impedances[1][1])))
+    with raises(AttributeError):
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedances[0][0], 100), (test_args.impedances[1][0], test_args.impedances[1][1])))
+    with raises(AssertionError):
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedances[0][0], test_args.impedances[0][1]), (bad_impedance, test_args.impedances[1][1])))
+    with raises(AttributeError):
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedances[0][0], test_args.impedances[0][1]), (100, test_args.impedances[1][1])))
+    with raises(AssertionError):
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedances[0][0], test_args.impedances[0][1]), (test_args.impedances[1][0], bad_impedance)))
+    with raises(AttributeError):
+        resistance_matrix_law.calculate_currents(test_args.input_voltage, test_args.output_voltage, ((test_args.impedances[0][0], test_args.impedances[0][1]), (test_args.impedances[1][0], 100)))
