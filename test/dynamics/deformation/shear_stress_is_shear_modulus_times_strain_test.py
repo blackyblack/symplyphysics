f9effb58fbd0636efa7a7740f3ea1de9b189e8a4
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-05 11:52:26 +0400

    Reformulate law in terms of angle of deformation

--- test/dynamics/deformation/shear_stress_is_shear_modulus_times_strain_test.py
+++ test/dynamics/deformation/shear_stress_is_shear_modulus_times_strain_test.py
@@ -12,41 +12,33 @@ from symplyphysics.laws.dynamics.deformation import (
 )
 
 # Description
-## Block of wood (G = 4 GPa) is undergoing shear stress. The transverse displacement of the
-## wood's surface is 0.5 mm. The length of the face of the block which the shear force is applied
-## to is 40 cm. The shear stress amounts to 5 MPa.
+## Block of wood (G = 4 GPa) is undergoing shear stress. The shear strain (angle of deformation) is 1.25e-3 rad.
+## The shear stress in the block amounts to 5 MPa.
 
-Args = namedtuple("Args", "g dx l")
+Args = namedtuple("Args", "g gamma")
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
     g = Quantity(4 * prefixes.giga * units.pascal)
-    dx = Quantity(0.5 * units.millimeter)
-    l = Quantity(40 * units.centimeter)
-    return Args(g=g, dx=dx, l=l)
+    gamma = Quantity(1.25e-3 * units.radian)
+    return Args(g=g, gamma=gamma)
 
 
 def test_law(test_args: Args) -> None:
-    result = shear_stress_law.calculate_shear_stress(test_args.g, test_args.dx, test_args.l)
+    result = shear_stress_law.calculate_shear_stress(test_args.g, test_args.gamma)
     assert_equal(result, 5 * prefixes.mega * units.pascal)
 
 
 def test_bad_shear_modulus(test_args: Args) -> None:
     gb = Quantity(1.0 * units.coulomb)
     with raises(errors.UnitsError):
-        shear_stress_law.calculate_shear_stress(gb, test_args.dx, test_args.l)
+        shear_stress_law.calculate_shear_stress(gb, test_args.gamma)
     with raises(TypeError):
-        shear_stress_law.calculate_shear_stress(100, test_args.dx, test_args.l)
+        shear_stress_law.calculate_shear_stress(100, test_args.gamma)
 
 
-def test_bad_length(test_args: Args) -> None:
-    lb = Quantity(1.0 * units.coulomb)
+def test_bad_angle(test_args: Args) -> None:
+    gamma_bad = Quantity(1.0 * units.coulomb)
     with raises(errors.UnitsError):
-        shear_stress_law.calculate_shear_stress(test_args.g, lb, test_args.l)
-    with raises(TypeError):
-        shear_stress_law.calculate_shear_stress(test_args.g, 100, test_args.l)
-    with raises(errors.UnitsError):
-        shear_stress_law.calculate_shear_stress(test_args.g, test_args.dx, lb)
-    with raises(TypeError):
-        shear_stress_law.calculate_shear_stress(test_args.g, test_args.dx, 100)
+        shear_stress_law.calculate_shear_stress(test_args.g, gamma_bad)
