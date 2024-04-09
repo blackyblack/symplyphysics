6b9a563f5708c1b2dadff710deef89e95d330896
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-09 19:14:08 +0400

    Introduce chemical potential in law

--- symplyphysics/laws/thermodynamics/internal_energy_change_via_entropy_change_and_volume_change.py
+++ symplyphysics/laws/thermodynamics/internal_energy_change_via_entropy_change_and_volume_change.py
@@ -1,6 +1,7 @@
 from sympy import Eq, solve
 from symplyphysics import (
     units,
+    dimensionless,
     Quantity,
     Symbol,
     print_expression,
@@ -19,20 +20,21 @@ from symplyphysics.laws.thermodynamics import (
 ## The fundamental thermodynamic relation are fundamental equations which demonstate how important
 ## thermodynamic quantities depend on variables that are measurable experimentally.
 
-# Law: dU = T * dS - P * dV
+# Law: dU = T * dS - P * dV + mu * dN
 ## U - internal energy
 ## T - absolute temperature
 ## S - entropy
 ## P - pressure
 ## V - volume
+## mu - chemical potential
+## N - number of particles in the system
 ## Notation: d(x) - full differential of `x`
 
 # Note
-## - Entropy `S` and volume `V` are so called natural variables of internal energy as a
+## - Entropy `S`, volume `V` and particle count `N` are so called natural variables of internal energy as a
 ##   thermodynamic potential.
 
 # Conditions
-## - The system is closed
 ## - The system is in thermal equilibrium with its surroundings
 
 internal_energy_change = Symbol("internal_energy_change", units.energy)
@@ -40,13 +42,16 @@ temperature = symbols.thermodynamics.temperature
 entropy_change = Symbol("entropy_change", units.energy / units.temperature)
 pressure = Symbol("pressure", units.pressure)
 volume_change = Symbol("volume_change", units.volume)
+chemical_potential = Symbol("chemical_potential", units.energy)
+particle_count_change = Symbol("particle_count_change", dimensionless)
 
 law = Eq(
     internal_energy_change,
-    temperature * entropy_change - pressure * volume_change,
+    temperature * entropy_change - pressure * volume_change + chemical_potential * particle_count_change,
 )
 
 # Derive from the first and second laws of thermodynamics
+# Assuming the particle count does not change
 
 _heat_supplied_to_system = solve(second_law.law,
     second_law.infinitesimal_transfer_of_heat)[0].subs({
@@ -64,7 +69,9 @@ _internal_energy_change = first_law.law.rhs.subs({
     first_law.work_done_by_system: _work_done_by_system,
 })
 
-assert expr_equals(law.rhs, _internal_energy_change)
+_expr_from_law = law.rhs.subs({particle_count_change: 0})
+
+assert expr_equals(_expr_from_law, _internal_energy_change)
 
 
 def print_law() -> str:
@@ -76,6 +83,8 @@ def print_law() -> str:
     entropy_change_=entropy_change,
     pressure_=pressure,
     volume_change_=volume_change,
+    chemical_potential_=chemical_potential,
+    particle_count_change_=particle_count_change,
 )
 @validate_output(internal_energy_change)
 def calculate_internal_energy_change(
@@ -83,11 +92,15 @@ def calculate_internal_energy_change(
     entropy_change_: Quantity,
     pressure_: Quantity,
     volume_change_: Quantity,
+    chemical_potential_: Quantity,
+    particle_count_change_: int,
 ) -> Quantity:
     result = law.rhs.subs({
         temperature: temperature_,
         entropy_change: entropy_change_,
         pressure: pressure_,
         volume_change: volume_change_,
+        chemical_potential: chemical_potential_,
+        particle_count_change: particle_count_change_,
     })
     return Quantity(result)
