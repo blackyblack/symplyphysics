5a7b086511176ec4ebe33c25a50a800485eed6ef
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-04-09 11:07:28 +0400

    Law: hard spheres potential model (#600)
    * Create law and test
    
    * Fix name (radius -> diameter) in another law
    
    * Add description
    
    * Add reference to law
    
    * Fix infinity test
    
    * Fix piecewise computations
    
    * Cleanup
    
    * Infinity can be of any dimension
    
    ---------
    
    Co-authored-by: Andrey Lekar <andrey_lekar@adoriasoft.com>

---
+++ test/chemistry/potential_energy_models/hard_spheres_potential_test.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from sympy import S
+from symplyphysics import (
+    errors,
+    units,
+    Quantity,
+    assert_equal,
+)
+from symplyphysics.laws.chemistry.potential_energy_models import hard_spheres_potential
+
+# Description
+## With in the model of hard spheres of diameter sigma = 10 nm, the value of the potential
+## is zero when the distance between the two particles is 20 nm, and infinity at distance r0 = 5 nm.
+
+Args = namedtuple("Args", "r0 r1 d")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    r0 = Quantity(20 * units.nanometer)
+    r1 = Quantity(5 * units.nanometer)
+    d = Quantity(10 * units.nanometer)
+    return Args(r0=r0, r1=r1, d=d)
+
+
+def test_law_zero_potential(test_args: Args) -> None:
+    result = hard_spheres_potential.calculate_potential(test_args.r0, test_args.d)
+    assert_equal(result, 0)
+
+
+def test_law_infinite_potential(test_args: Args) -> None:
+    result = hard_spheres_potential.calculate_potential(test_args.r1, test_args.d)
+    assert_equal(result, S.Infinity)
+
+
+def test_bad_length(test_args: Args) -> None:
+    lb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        hard_spheres_potential.calculate_potential(lb, test_args.d)
+    with raises(TypeError):
+        hard_spheres_potential.calculate_potential(100, test_args.d)
+    with raises(errors.UnitsError):
+        hard_spheres_potential.calculate_potential(test_args.r0, lb)
+    with raises(TypeError):
+        hard_spheres_potential.calculate_potential(test_args.r0, 100)
