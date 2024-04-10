c9199321e56a0051d9163e5739800baa3d0b08b0
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-04-10 15:22:19 +0400

    Refactor differential to contain particle count (#623)

--- symplyphysics/laws/thermodynamics/free_energy_change_via_temperature_change_and_volume_change.py
+++ symplyphysics/laws/thermodynamics/free_energy_change_via_temperature_change_and_volume_change.py
@@ -1,6 +1,7 @@
 from sympy import Eq
 from symplyphysics import (
     units,
+    dimensionless,
     Quantity,
     Symbol,
     print_expression,
@@ -12,32 +13,40 @@ from symplyphysics import (
 ## The fundamental thermodynamic relation are fundamental equations which demonstate how important
 ## thermodynamic quantities depend on variables that are measurable experimentally.
 
-# Law: dF = -S * dT - p * dV
+# Law: dF = -S * dT - p * dV + mu * dN
 ## F - Helmholtz free energy
 ## S - entropy
 ## T - absolute temperature
 ## p - pressure
 ## V - volume
+## mu - chemical potential
+## N - number of particles in the system
 ## Notation: d(x) - full differential of `x`
 
 # Note
-## Temperature `T` and volume `V` are so called natural variables of Helmholtz free energy
-## as a thermodynamic potential.
+## - Temperature `T`, volume `V`, and particle count N are so called natural variables of Helmholtz free energy
+##   as a thermodynamic potential.
+## - For a system with more than one type of particles, the last term can be represented as a sum over all
+##   types of particles, i.e. `Sum(mu_i * d(N_i), i)`.
 
 # Conditions
-## - The system is closed
 ## - The system is in thermal equilibrium with its surroundings
+## - There is only one type of particles in the system
 
 free_energy_change = Symbol("free_energy_change", units.energy)
 entropy = Symbol("entropy", units.energy / units.temperature)
 temperature_change = Symbol("temperature_change", units.temperature)
 pressure = Symbol("pressure", units.pressure)
 volume_change = Symbol("volume_change", units.volume)
+chemical_potential = Symbol("chemical_potential", units.energy)
+particle_count_change = Symbol("particle_count_change", dimensionless)
 
 law = Eq(
     free_energy_change,
-    -1 * entropy * temperature_change - pressure * volume_change,
+    -1 * entropy * temperature_change - pressure * volume_change + chemical_potential * particle_count_change,
 )
+
+# TODO: derive from the definition of Helmholtz free energy and internal energy differential
 
 
 def print_law() -> str:
@@ -49,6 +58,8 @@ def print_law() -> str:
     temperature_change_=temperature_change,
     pressure_=pressure,
     volume_change_=volume_change,
+    chemical_potential_=chemical_potential,
+    particle_count_change_=particle_count_change,
 )
 @validate_output(free_energy_change)
 def calculate_free_energy_change(
@@ -56,11 +67,15 @@ def calculate_free_energy_change(
     temperature_change_: Quantity,
     pressure_: Quantity,
     volume_change_: Quantity,
+    chemical_potential_: Quantity,
+    particle_count_change_: int,
 ) -> Quantity:
     result = law.rhs.subs({
         entropy: entropy_,
         temperature_change: temperature_change_,
         pressure: pressure_,
         volume_change: volume_change_,
+        chemical_potential: chemical_potential_,
+        particle_count_change: particle_count_change_,
     })
     return Quantity(result)
--- test/thermodynamics/free_energy_change_via_temperature_change_and_volume_change_test.py
+++ test/thermodynamics/free_energy_change_via_temperature_change_and_volume_change_test.py
@@ -11,10 +11,11 @@ from symplyphysics.laws.thermodynamics import (
 
 # Description
 ## A closed system in thermal equlibrium has entropy S = 1 J/k and pressure p = 1 Pa inside.
-## The temperature of the system drops by 0.01 K and the volume grows by 0.02 m**3. Then
-## the change in Helmholtz free energy of the system is -0.01 J.
+## The temperature of the system drops by 0.01 K and the volume grows by 0.02 m**3. The number
+## of particles decreases by 1, the chemical potential is 0.1 J.
+## Then the change in Helmholtz free energy of the system is -0.11 J.
 
-Args = namedtuple("Args", "s dt p dv")
+Args = namedtuple("Args", "s dt p dv mu dn")
 
 
 @fixture(name="test_args")
@@ -23,42 +24,58 @@ def test_args_fixture() -> Args:
     dt = Quantity(-0.01 * units.kelvin)
     p = Quantity(1 * units.pascal)
     dv = Quantity(0.02 * units.meter**3)
-    return Args(s=s, dt=dt, p=p, dv=dv)
+    mu = Quantity(0.1 * units.joule)
+    dn = -1
+    return Args(s=s, dt=dt, p=p, dv=dv, mu=mu, dn=dn)
 
 
 def test_law(test_args: Args) -> None:
     result = free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, test_args.p,
-        test_args.dv)
-    assert_equal(result, -0.01 * units.joule)
+        test_args.dv, test_args.mu, test_args.dn)
+    assert_equal(result, -0.11 * units.joule)
 
 
 def test_bad_entropy(test_args: Args) -> None:
     sb = Quantity(1.0 * units.meter)
     with raises(errors.UnitsError):
-        free_energy_law.calculate_free_energy_change(sb, test_args.dt, test_args.p, test_args.dv)
+        free_energy_law.calculate_free_energy_change(sb, test_args.dt, test_args.p, test_args.dv, test_args.mu, test_args.dn)
     with raises(TypeError):
-        free_energy_law.calculate_free_energy_change(100, test_args.dt, test_args.p, test_args.dv)
+        free_energy_law.calculate_free_energy_change(100, test_args.dt, test_args.p, test_args.dv, test_args.mu, test_args.dn)
 
 
 def test_bad_temperature(test_args: Args) -> None:
     tb = Quantity(1.0 * units.meter)
     with raises(errors.UnitsError):
-        free_energy_law.calculate_free_energy_change(test_args.s, tb, test_args.p, test_args.dv)
+        free_energy_law.calculate_free_energy_change(test_args.s, tb, test_args.p, test_args.dv, test_args.mu, test_args.dn)
     with raises(TypeError):
-        free_energy_law.calculate_free_energy_change(test_args.s, 100, test_args.p, test_args.dv)
+        free_energy_law.calculate_free_energy_change(test_args.s, 100, test_args.p, test_args.dv, test_args.mu, test_args.dn)
 
 
 def test_bad_pressure(test_args: Args) -> None:
     pb = Quantity(1.0 * units.meter)
     with raises(errors.UnitsError):
-        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, pb, test_args.dv)
+        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, pb, test_args.dv, test_args.mu, test_args.dn)
     with raises(TypeError):
-        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, 100, test_args.dv)
+        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, 100, test_args.dv, test_args.mu, test_args.dn)
 
 
 def test_bad_volume(test_args: Args) -> None:
     vb = Quantity(1.0 * units.meter)
     with raises(errors.UnitsError):
-        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, test_args.p, vb)
+        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, test_args.p, vb, test_args.mu, test_args.dn)
     with raises(TypeError):
-        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, test_args.p, 100)
+        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, test_args.p, 100, test_args.mu, test_args.dn)
+
+
+def test_bad_chemical_potential(test_args: Args) -> None:
+    mub = Quantity(1.0 * units.meter)
+    with raises(errors.UnitsError):
+        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, test_args.p, test_args.dv, mub, test_args.dn)
+    with raises(TypeError):
+        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, test_args.p, test_args.dv, 100, test_args.dn)
+
+
+def test_bad_number(test_args: Args) -> None:
+    nb = Quantity(1.0 * units.meter)
+    with raises(errors.UnitsError):
+        free_energy_law.calculate_free_energy_change(test_args.s, test_args.dt, test_args.p, test_args.dv, test_args.mu, nb)
