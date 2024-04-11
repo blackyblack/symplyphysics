773441ec72c8386fe118b0cea68fb435bb9c621c
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-11 14:32:50 +0400

    Derive law from definition

--- symplyphysics/laws/thermodynamics/volume_of_heated_body_from_initial_volume_and_heating_temperature.py
+++ symplyphysics/laws/thermodynamics/volume_of_heated_body_from_initial_volume_and_heating_temperature.py
@@ -1,22 +1,31 @@
-from sympy import (Eq, solve)
-from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
-    validate_output)
+from sympy import Eq, solve, dsolve
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.definitions import volumetric_coefficient_of_thermal_expansion as coef_def
 
 # Description
 ## As the temperature increases, the intensity of the thermal motion of the particles of matter increases. This leads to the fact that the molecules are more "actively" repelled from each other.
 ## As the empty spaces between molecules increase, most bodies increase in size when heated.
 
-## Law: V = V_start * (1 + y * (t_end - t_start)
+## Law: V = V_start * (1 + alpha_V * (t_end - t_start))
 ## Where:
 ## V_start is body volume at the initial temperature
 ## V is volume of body at temperature t
-## y is the coefficient of volumetric expansion of the body
+## alpha_V is the coefficient of volumetric expansion of the body
 ## t_start is initial temperature
 ## t_end is final temperature
 
 ## Conditions
 ## The pressure must be constant.
 ## There is no phase transition, eg melting or evaporation.
+## The temperature change (difference between `t_end` and `t_start`) is small.
 
 final_volume = Symbol("final_volume", units.volume)
 start_volume = Symbol("start_volume", units.volume)
@@ -27,7 +36,20 @@ final_temperature = Symbol("final_temperature", units.temperature)
 law = Eq(final_volume,
     start_volume * (1 + expansion_coefficient * (final_temperature - start_temperature)))
 
-# TODO: derive from [definition of expansion coefficient](../../definitions/volumetric_coefficient_of_thermal_expansion.py)
+# Derive from [definition of expansion coefficient](../../definitions/volumetric_coefficient_of_thermal_expansion.py)
+
+_volume = coef_def.volume
+_temperature = coef_def.temperature
+
+_eqn = coef_def.definition.subs(coef_def.volumetric_expansion_coefficient, expansion_coefficient)
+
+_volume_expr = dsolve(_eqn, _volume(_temperature), ics={_volume(0): start_volume}).rhs
+
+_volume_series = _volume_expr.series(_temperature, 0, 2).removeO()
+
+_volume_derived = _volume_series.subs(_temperature, final_temperature - start_temperature)
+
+assert expr_equals(_volume_derived, law.rhs)
 
 
 def print_law() -> str:
