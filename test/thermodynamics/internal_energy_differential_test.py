a9cf399af6c86696fabed296f590cfb4da256d72
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-04-11 11:08:14 +0400

    Refactor internal energy differential (#620)
    * Introduce chemical potential in law
    
    * Rename files to better reflect content
    
    * Add comment about types of particles
    
    * Add TODO to proof to fix it later

--- test/thermodynamics/internal_energy_differential_test.py
+++ test/thermodynamics/internal_energy_differential_test.py
@@ -8,15 +8,17 @@ from symplyphysics import (
     Quantity,
 )
 from symplyphysics.laws.thermodynamics import (
-    internal_energy_change_via_entropy_change_and_volume_change as internal_energy_law,)
+    internal_energy_differential as internal_energy_law,
+)
 
 # Description
 ## An ensamble of particles in a closed reservoir is in thermodynamic equilibrium with the environment.
 ## The temperature is 300 K and the pressure in the reservoir is 0.1 MPa. The entropy of the ensamble
-## has dropped by 1 J/K and the volume has become bigger by 1 mm**3. Then the change in internal energy
-## of the system amounts to -300 J.
+## has dropped by 1 J/K and the volume has become bigger by 1 mm**3. With chemical potential of the system
+## equal to 20 J, the particle count increased by 1. Then the change in internal energy of the system 
+## amounts to -280 J.
 
-Args = namedtuple("Args", "t ds p dv")
+Args = namedtuple("Args", "t ds p dv mu dn")
 
 
 @fixture(name="test_args")
@@ -25,50 +27,69 @@ def test_args_fixture() -> Args:
     ds = Quantity(-1 * units.joule / units.kelvin)
     p = Quantity(0.1 * prefixes.mega * units.pascal)
     dv = Quantity(1 * units.millimeter**3)
-    return Args(t=t, ds=ds, p=p, dv=dv)
+    mu = Quantity(20 * units.joule)
+    dn = 1
+    return Args(t=t, ds=ds, p=p, dv=dv, mu=mu, dn=dn)
 
 
 def test_law(test_args: Args) -> None:
     result = internal_energy_law.calculate_internal_energy_change(test_args.t, test_args.ds,
-        test_args.p, test_args.dv)
-    assert_equal(result, -300 * units.joule)
+        test_args.p, test_args.dv, test_args.mu, test_args.dn)
+    assert_equal(result, -280 * units.joule)
 
 
 def test_bad_temperature(test_args: Args) -> None:
     tb = Quantity(1.0 * units.coulomb)
     with raises(errors.UnitsError):
         internal_energy_law.calculate_internal_energy_change(tb, test_args.ds, test_args.p,
-            test_args.dv)
+            test_args.dv, test_args.mu, test_args.dn)
     with raises(TypeError):
         internal_energy_law.calculate_internal_energy_change(100, test_args.ds, test_args.p,
-            test_args.dv)
+            test_args.dv, test_args.mu, test_args.dn)
 
 
 def test_bad_entropy(test_args: Args) -> None:
     sb = Quantity(1.0 * units.coulomb)
     with raises(errors.UnitsError):
         internal_energy_law.calculate_internal_energy_change(test_args.t, sb, test_args.p,
-            test_args.dv)
+            test_args.dv, test_args.mu, test_args.dn)
     with raises(TypeError):
         internal_energy_law.calculate_internal_energy_change(test_args.t, 100, test_args.p,
-            test_args.dv)
+            test_args.dv, test_args.mu, test_args.dn)
 
 
 def test_bad_pressure(test_args: Args) -> None:
     pb = Quantity(1.0 * units.coulomb)
     with raises(errors.UnitsError):
         internal_energy_law.calculate_internal_energy_change(test_args.t, test_args.ds, pb,
-            test_args.dv)
+            test_args.dv, test_args.mu, test_args.dn)
     with raises(TypeError):
         internal_energy_law.calculate_internal_energy_change(test_args.t, test_args.ds, 100,
-            test_args.dv)
+            test_args.dv, test_args.mu, test_args.dn)
 
 
 def test_bad_volume(test_args: Args) -> None:
     vb = Quantity(1.0 * units.coulomb)
     with raises(errors.UnitsError):
         internal_energy_law.calculate_internal_energy_change(test_args.t, test_args.ds, test_args.p,
-            vb)
+            vb, test_args.mu, test_args.dn)
     with raises(TypeError):
         internal_energy_law.calculate_internal_energy_change(test_args.t, test_args.ds, test_args.p,
-            100)
+            100, test_args.mu, test_args.dn)
+
+
+def test_bad_chemical_potential(test_args: Args) -> None:
+    mub = Quantity(1.0 * units.coulomb)
+    with raises(errors.UnitsError):
+        internal_energy_law.calculate_internal_energy_change(test_args.t, test_args.ds, test_args.p,
+            test_args.dv, mub, test_args.dn)
+    with raises(TypeError):
+        internal_energy_law.calculate_internal_energy_change(test_args.t, test_args.ds, test_args.p,
+            test_args.dv, 100, test_args.dn)
+
+
+def test_bad_particle_count(test_args: Args) -> None:
+    nb = Quantity(1.0 * units.coulomb)
+    with raises(errors.UnitsError):
+        internal_energy_law.calculate_internal_energy_change(test_args.t, test_args.ds, test_args.p,
+            test_args.dv, test_args.mu, nb)
