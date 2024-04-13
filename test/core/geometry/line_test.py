bcee7410e150a4c4ec2376cfd062388c881e5331
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-13 10:21:49 +0400

    Fix typing issues

--- test/core/geometry/line_test.py
+++ test/core/geometry/line_test.py
@@ -1,26 +1,25 @@
-from pytest import raises
-from sympy import Symbol
+from sympy import Symbol, Point
 from symplyphysics.core.geometry.line import two_point_function
 from symplyphysics.core.expr_comparisons import expr_equals
 
-def test_zero_intercept():
-    p1 = (0, 0)
-    p2 = (1, 2)
+def test_zero_intercept() -> None:
+    p1 = Point(0, 0)
+    p2 = Point(1, 2)
     x = Symbol("x")
     y = two_point_function(p1, p2, x)
     assert expr_equals(y, 2 * x)
 
 
-def test_nonzero_intercept():
-    p1 = (1, 1)
-    p2 = (2, 3)
+def test_nonzero_intercept() -> None:
+    p1 = Point(1, 1)
+    p2 = Point(2, 3)
     x = Symbol("x")
     y = two_point_function(p1, p2, x)
     assert expr_equals(y, 2 * x - 1)
 
 
-def test_parallel_to_y_axis():
-    p1 = (1, 1)
-    p2 = (1, 2)
+def test_parallel_to_y_axis() -> None:
+    p1 = Point(1, 1)
+    p2 = Point(1, 2)
     x = Symbol("x")
     assert two_point_function(p1, p2, x) is NotImplemented
