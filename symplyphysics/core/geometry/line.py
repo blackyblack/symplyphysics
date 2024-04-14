96f6a87e08a7ace71c2c86817f5d4fdd37ab5b41
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-14 17:36:40 +0400

    Update function type signature

--- symplyphysics/core/geometry/line.py
+++ symplyphysics/core/geometry/line.py
@@ -1,6 +1,6 @@
 from sympy import symbols, Point2D, Line2D, Expr, solve
 
-def two_point_function(p1: Point2D | tuple[Expr, Expr], p2: Point2D | tuple[Expr, Expr], x: Expr) -> Expr:
+def two_point_function(p1: Point2D, p2: Point2D, x: Expr) -> Expr:
     """Constructs a linear function of ``x`` using two points ``p1`` and ``p2``.
     
     Raises ``ValueError`` if the line equation does not depend on ``y`` or if the points are not unique.
