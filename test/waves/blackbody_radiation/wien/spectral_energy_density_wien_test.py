9c64f15f93b8765390cea5f385fbf51dd1b9edfc
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-15 16:27:23 +0400

    Create law and test

---
+++ test/waves/blackbody_radiation/wien/spectral_energy_density_wien_test.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    prefixes,
+    Quantity,
+)
+from symplyphysics.laws.waves.blackbody_radiation.wien import spectral_energy_density as wien_radiation_law
+
+# Description
+## The spectral energy density of electromagnetic radiation emitted from the Sun (assuming it is a black
+## body radiator, the surface temperature of the Sun is about T = 5800 K) for green light (nu = 550 THz) is
+## u_nu = 6.78 keV / (m**3 * Hz), assuming Wien distribution approximation.
+
+Args = namedtuple("Args", "nu t")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    nu = Quantity(550 * prefixes.tera * units.hertz)
+    t = Quantity(5800 * units.kelvin)
+    return Args(nu=nu, t=t)
+
+
+def test_law(test_args: Args) -> None:
+    result = wien_radiation_law.calculate_spectral_energy_density(test_args.nu, test_args.t)
+    assert_equal(result, 6.78 * prefixes.kilo * units.electronvolt / (units.meter**3 * units.hertz))
+
+
+def test_bad_frequency(test_args: Args) -> None:
+    nub = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        wien_radiation_law.calculate_spectral_energy_density(nub, test_args.t)
+    with raises(TypeError):
+        wien_radiation_law.calculate_spectral_energy_density(100, test_args.t)
+
+
+def test_bad_temperature(test_args: Args) -> None:
+    tb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        wien_radiation_law.calculate_spectral_energy_density(test_args.nu, tb)
+    with raises(TypeError):
+        wien_radiation_law.calculate_spectral_energy_density(test_args.nu, 100)
