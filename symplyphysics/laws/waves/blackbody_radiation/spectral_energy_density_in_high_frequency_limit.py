1af9dfb94d38c7eb7eff741929dbca64a5fc84b4
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-16 13:42:02 +0400

    Create law and test for Rayleigh-Jeans law

---
+++ symplyphysics/laws/waves/blackbody_radiation/spectral_energy_density_in_high_frequency_limit.py
@@ -0,0 +1,58 @@
+from sympy import Eq, pi
+from sympy.physics.units import speed_of_light, boltzmann_constant
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    symbols,
+    clone_symbol,
+)
+
+# Description
+## The Rayleigh-Jeans law is an approximation to the spectral radiance of electromagnetic radiation
+## as a function of wave frequency from a blackbody at a given temperature through classical arguments.
+## The Rayleigh-Jeans law agrees with experimental results at large wavelengths (i.e. at low frequencies)
+## but strongly disagrees at short wavelengths (i.e. at high frequencies). This inconsistency is commonly
+## known as ultraviolet catastrophe. See [Planck's law](./spectral_energy_density_at_all_frequencies.py) for
+## the correct expression of radiation at all frequencies.
+
+# Law: u_nu = 8 * pi * nu**2 * k * T / c**3
+## u_nu - spectral energy density (energy per unit volume per unit frequency)
+## nu - radiation frequency
+## k - Boltzmann constant
+## T - equilibrium temperature of black body
+## c - speed of light
+
+spectral_energy_density = Symbol("spectral_energy_density", units.energy / (units.volume * units.frequency))
+radiation_frequency = Symbol("radiation_frequency", units.frequency)
+equilibrium_temperature = clone_symbol(symbols.thermodynamics.temperature, "equilibrium_temperature")
+
+law = Eq(
+    spectral_energy_density,
+    8 * pi * radiation_frequency**2 * boltzmann_constant * equilibrium_temperature / speed_of_light**3
+)
+
+# TODO: derive from Planck's law
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    radiation_frequency_=radiation_frequency,
+    equilibrium_temperature_=equilibrium_temperature,
+)
+@validate_output(spectral_energy_density)
+def calculate_spectral_energy_density(
+    radiation_frequency_: Quantity,
+    equilibrium_temperature_: Quantity,
+) -> Quantity:
+    result = law.rhs.subs({
+        radiation_frequency: radiation_frequency_,
+        equilibrium_temperature: equilibrium_temperature_,
+    })
+    return Quantity(result)
---
+++ test/waves/blackbody_radiation/spectral_energy_density_in_high_frequency_limit_test.py
@@ -0,0 +1,44 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    prefixes,
+    Quantity,
+)
+from symplyphysics.laws.waves.blackbody_radiation import spectral_energy_density_in_high_frequency_limit as rj_law
+
+# Description
+## The spectral energy density of electromagnetic radiation emitted from the Sun (assuming it is a black
+## body radiator, the surface temperature of the Sun is about T = 5800 K) for green light (nu = 550 THz) is
+## u_nu = 141 keV / (m**3 * Hz), assuming Rayleigh-Jeans law approximation.
+
+Args = namedtuple("Args", "nu t")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    nu = Quantity(550 * prefixes.tera * units.hertz)
+    t = Quantity(5800 * units.kelvin)
+    return Args(nu=nu, t=t)
+
+
+def test_law(test_args: Args) -> None:
+    result = rj_law.calculate_spectral_energy_density(test_args.nu, test_args.t)
+    assert_equal(result, 141 * prefixes.kilo * units.electronvolt / (units.meter**3 * units.hertz))
+
+def test_bad_frequency(test_args: Args) -> None:
+    nub = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        rj_law.calculate_spectral_energy_density(nub, test_args.t)
+    with raises(TypeError):
+        rj_law.calculate_spectral_energy_density(100, test_args.t)
+
+
+def test_bad_temperature(test_args: Args) -> None:
+    tb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        rj_law.calculate_spectral_energy_density(test_args.nu, tb)
+    with raises(TypeError):
+        rj_law.calculate_spectral_energy_density(test_args.nu, 100)
