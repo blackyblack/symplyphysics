48ed29d84203e427093cd9b14c386f43231e724d
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-17 09:30:04 +0400

    Derive Rayleigh-Jeans law

--- symplyphysics/laws/waves/blackbody_radiation/spectral_energy_density_in_low_frequency_limit.py
+++ symplyphysics/laws/waves/blackbody_radiation/spectral_energy_density_in_low_frequency_limit.py
@@ -1,4 +1,4 @@
-from sympy import Eq, pi
+from sympy import Eq, pi, Symbol as SymSymbol, solve
 from sympy.physics.units import speed_of_light, boltzmann_constant
 from symplyphysics import (
     units,
@@ -10,6 +10,8 @@ from symplyphysics import (
     symbols,
     clone_symbol,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.waves.blackbody_radiation import spectral_energy_density_at_all_frequencies as planck_law
 
 # Description
 ## The Rayleigh-Jeans law is an approximation to the spectral radiance of electromagnetic radiation
@@ -38,8 +40,35 @@ law = Eq(
     8 * pi * radiation_frequency**2 * boltzmann_constant * equilibrium_temperature / speed_of_light**3
 )
 
-# TODO: derive from Planck's law
+# Derive from Planck's law of blackbody radiation
 
+_planck_density = planck_law.law.rhs.subs({
+    planck_law.radiation_frequency: radiation_frequency,
+    planck_law.equilibrium_temperature: equilibrium_temperature,
+})
+
+_reduced_frequency = SymSymbol("reduced_frequency")
+
+_reduced_frequency_eqn = Eq(
+    _reduced_frequency,
+    units.planck * radiation_frequency / (boltzmann_constant * equilibrium_temperature),
+)
+
+_planck_density_reduced = solve(
+    (Eq(spectral_energy_density, _planck_density), _reduced_frequency_eqn),
+    (spectral_energy_density, radiation_frequency),
+    dict=True,
+)[0][spectral_energy_density]
+
+_planck_density_reduced_series = _planck_density_reduced.series(_reduced_frequency, 0, 3).removeO()
+
+_planck_density_series = solve(
+    (Eq(spectral_energy_density, _planck_density_reduced_series), _reduced_frequency_eqn),
+    (spectral_energy_density, _reduced_frequency),
+    dict=True,
+)[0][spectral_energy_density]
+
+assert expr_equals(_planck_density_series, law.rhs)
 
 def print_law() -> str:
     return print_expression(law)
