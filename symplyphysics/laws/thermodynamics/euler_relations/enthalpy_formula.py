a08cc772166bbb395cf37fdfff96bf6e42cb8588
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-18 10:57:40 +0400

    Create enthalpy law and test

---
+++ symplyphysics/laws/thermodynamics/euler_relations/enthalpy_formula.py
@@ -0,0 +1,82 @@
+from sympy import Eq
+from symplyphysics import (
+    units,
+    dimensionless,
+    Quantity,
+    Symbol,
+    print_expression,
+    validate_input,
+    validate_output,
+    symbols
+)
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.thermodynamics import enthalpy_is_internal_energy_plus_pressure_energy as enthalpy_def
+from symplyphysics.laws.thermodynamics.euler_relations import internal_energy_formula
+
+# Description
+## The enthalpy differential can be integrated using the Euler's theorem on homogeneous functions for
+## internal energy.
+
+# Law: H = T * S + mu * N
+## H - enthalpy
+## T - temperature
+## S - entropy
+## mu - chemical potential
+## N - particle count
+
+# Note
+## - This formula works for single-component system. For many-component systems replace the RHS with
+##   a sum over each type of component.
+
+enthalpy = Symbol("enthalpy", units.energy)
+temperature = symbols.thermodynamics.temperature
+entropy = Symbol("entropy", units.energy / units.temperature)
+chemical_potential = Symbol("chemical_potential", units.energy)
+particle_count = Symbol("particle_count", dimensionless)
+
+law = Eq(enthalpy, temperature * entropy + chemical_potential * particle_count)
+
+# Derive from enthalpy definition and internal energy Euler relation
+
+_internal_energy_expr = internal_energy_formula.law.rhs.subs({
+    internal_energy_formula.temperature: temperature,
+    internal_energy_formula.entropy: entropy,
+    internal_energy_formula.pressure: enthalpy_def.pressure,
+    internal_energy_formula.volume: enthalpy_def.volume,
+    internal_energy_formula.chemical_potential: chemical_potential,
+    internal_energy_formula.particle_count: particle_count,
+})
+
+_enthalpy_expr = enthalpy_def.law.rhs.subs({
+    enthalpy_def.internal_energy: _internal_energy_expr
+})
+
+assert expr_equals(_enthalpy_expr, law.rhs)
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    temperature_=temperature,
+    entropy_=entropy,
+    chemical_potential_=chemical_potential,
+    particle_count_=particle_count,
+)
+@validate_output(enthalpy)
+def calculate_enthalpy(
+    temperature_: Quantity,
+    entropy_: Quantity,
+    chemical_potential_: Quantity,
+    particle_count_: int,
+) -> Quantity:
+    # Note that enthalpy is known up to a constant, therefore enthalpy as well.    
+
+    result = law.rhs.subs({
+        temperature: temperature_,
+        entropy: entropy_,
+        chemical_potential: chemical_potential_,
+        particle_count: particle_count_,
+    })
+    return Quantity(result)
---
+++ test/thermodynamics/euler_relations/enthalpy_formula_test.py
@@ -0,0 +1,60 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    prefixes,
+    Quantity,
+)
+from symplyphysics.laws.thermodynamics.euler_relations import enthalpy_formula
+
+# Description
+## The enthalpy of a system at temperature T = 100 K, having entropy S = 1 kJ/K and chemical potential
+## mu = -1 eV and N = 1e22 particles is H = 98 kJ.
+
+Args = namedtuple("Args", "t s mu n")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    t = Quantity(100 * units.kelvin)
+    s = Quantity(1 * prefixes.kilo * units.joule / units.kelvin)
+    mu = Quantity(-1 * units.electronvolt)
+    n = 1e22
+    return Args(t=t, s=s, mu=mu, n=n)
+
+
+def test_law(test_args: Args) -> None:
+    result = enthalpy_formula.calculate_enthalpy(test_args.t, test_args.s, test_args.mu, test_args.n)
+    assert_equal(result, 98 * prefixes.kilo * units.joule, tolerance=5e-3)
+
+
+def test_bad_temperature(test_args: Args) -> None:
+    tb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        enthalpy_formula.calculate_enthalpy(tb, test_args.s, test_args.mu, test_args.n)
+    with raises(TypeError):
+        enthalpy_formula.calculate_enthalpy(100, test_args.s, test_args.mu, test_args.n)
+
+
+def test_bad_entropy(test_args: Args) -> None:
+    sb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        enthalpy_formula.calculate_enthalpy(test_args.t, sb, test_args.mu, test_args.n)
+    with raises(TypeError):
+        enthalpy_formula.calculate_enthalpy(test_args.t, 100, test_args.mu, test_args.n)
+
+
+def test_bad_chemical_potential(test_args: Args) -> None:
+    mub = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        enthalpy_formula.calculate_enthalpy(test_args.t, test_args.s, mub, test_args.n)
+    with raises(TypeError):
+        enthalpy_formula.calculate_enthalpy(test_args.t, test_args.s, 100, test_args.n)
+
+
+def test_bad_particle_number(test_args: Args) -> None:
+    nb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        enthalpy_formula.calculate_enthalpy(test_args.t, test_args.s, test_args.mu, nb)
