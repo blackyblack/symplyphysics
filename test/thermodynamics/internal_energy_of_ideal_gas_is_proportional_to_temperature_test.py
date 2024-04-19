023f0f421b7340cb5e102996eca4712aca043ee4
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-04-19 15:32:17 +0400

    Refactor: internal energy of ideal gas (#679)
    * Refactor law to feature differentials
    
    * Add notes

--- test/thermodynamics/internal_energy_of_ideal_gas_is_proportional_to_temperature_test.py
+++ test/thermodynamics/internal_energy_of_ideal_gas_is_proportional_to_temperature_test.py
@@ -3,7 +3,6 @@ from pytest import fixture, raises
 from symplyphysics import (
     assert_equal,
     errors,
-    prefixes,
     units,
     Quantity,
 )
@@ -12,30 +11,30 @@ from symplyphysics.laws.thermodynamics import (
 )
 
 # Description
-## The internal energy of a gas with isochoric heat capacity C_V = 100 J/K and a temperature
-## of 100 K is U = 10 kJ.
+## The internal energy change of a gas with isochoric heat capacity C_V = 100 J/K and a temperature
+## change of 0.001 K is U = 0.1 J.
 
-Args = namedtuple("Args", "cv t")
+Args = namedtuple("Args", "cv dt")
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
     cv = Quantity(100 * units.joule / units.kelvin)
-    t = Quantity(100 * units.kelvin)
-    return Args(cv=cv, t=t)
+    dt = Quantity(0.001 * units.kelvin)
+    return Args(cv=cv, dt=dt)
 
 
 def test_law(test_args: Args) -> None:
-    result = energy_law.calculate_internal_energy(test_args.cv, test_args.t)
-    assert_equal(result, 10 * prefixes.kilo * units.joule)
+    result = energy_law.calculate_internal_energy(test_args.cv, test_args.dt)
+    assert_equal(result, 0.1 * units.joule)
 
 
 def test_bad_heat_capacity(test_args: Args) -> None:
     cb = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        energy_law.calculate_internal_energy(cb, test_args.t)
+        energy_law.calculate_internal_energy(cb, test_args.dt)
     with raises(TypeError):
-        energy_law.calculate_internal_energy(100, test_args.t)
+        energy_law.calculate_internal_energy(100, test_args.dt)
 
 
 def test_bad_temperature(test_args: Args) -> None:
