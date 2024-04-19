e709a3e7bd6fe33a7a96dd309e0a6b06f9e0c1ba
Ростислав Житков <rosjit1@gmail.com>
2024-04-19 16:30:53 +0300

    change name to resonator_quality_factor

--- symplyphysics/laws/electricity/resonators/coupling_parameter_of_resonator_from_quality_factors.py
+++ symplyphysics/laws/electricity/resonators/coupling_parameter_of_resonator_from_quality_factors.py
@@ -13,23 +13,23 @@ from symplyphysics import (Symbol, print_expression, validate_input,
 
 coupling_parameter = Symbol("coupling_parameter", dimensionless)
 
-own_quality_factor = Symbol("own_quality_factor", dimensionless)
+resonator_quality_factor = Symbol("resonator_quality_factor", dimensionless)
 external_circuit_quality_factor = Symbol("external_circuit_quality_factor", dimensionless)
 
-law = Eq(coupling_parameter, own_quality_factor / external_circuit_quality_factor)
+law = Eq(coupling_parameter, resonator_quality_factor / external_circuit_quality_factor)
 
 
 def print_law() -> str:
     return print_expression(law)
 
 
-@validate_input(own_quality_factor_=own_quality_factor, external_circuit_quality_factor_=external_circuit_quality_factor)
+@validate_input(resonator_quality_factor_=resonator_quality_factor, external_circuit_quality_factor_=external_circuit_quality_factor)
 @validate_output(coupling_parameter)
-def calculate_coupling_parameter(own_quality_factor_: float,
+def calculate_coupling_parameter(resonator_quality_factor_: float,
     external_circuit_quality_factor_: float) -> float:
     result_expr = solve(law, coupling_parameter, dict=True)[0][coupling_parameter]
     result_expr = result_expr.subs({
-        own_quality_factor: own_quality_factor_,
+        resonator_quality_factor: resonator_quality_factor_,
         external_circuit_quality_factor: external_circuit_quality_factor_,
     })
     return convert_to_float(result_expr)
--- test/electricity/resonators/coupling_parameter_of_resonator_from_quality_factors_test.py
+++ test/electricity/resonators/coupling_parameter_of_resonator_from_quality_factors_test.py
@@ -7,19 +7,19 @@ from symplyphysics.laws.electricity.resonators import coupling_parameter_of_reso
 ## Resonator's own quality factor is 8000, the quality factor of the external circuit is 10000 ohms.
 ## Then the coupling parameter will be 0.8.
 
-Args = namedtuple("Args", ["own_quality_factor", "external_circuit_quality_factor"])
+Args = namedtuple("Args", ["resonator_quality_factor", "external_circuit_quality_factor"])
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
-    own_quality_factor = 8000
+    resonator_quality_factor = 8000
     external_circuit_quality_factor = 10000
 
-    return Args(own_quality_factor=own_quality_factor, external_circuit_quality_factor=external_circuit_quality_factor)
+    return Args(resonator_quality_factor=resonator_quality_factor, external_circuit_quality_factor=external_circuit_quality_factor)
 
 
 def test_basic_coupling_parameter(test_args: Args) -> None:
-    result = parameter_law.calculate_coupling_parameter(test_args.own_quality_factor,
+    result = parameter_law.calculate_coupling_parameter(test_args.resonator_quality_factor,
         test_args.external_circuit_quality_factor)
     assert_equal(result, 0.8)
 
@@ -30,5 +30,5 @@ def test_bad_quality_factors(test_args: Args) -> None:
         parameter_law.calculate_coupling_parameter(bad_quality_factor,
             test_args.external_circuit_quality_factor)
     with raises(errors.UnitsError):
-        parameter_law.calculate_coupling_parameter(test_args.own_quality_factor,
+        parameter_law.calculate_coupling_parameter(test_args.resonator_quality_factor,
             bad_quality_factor)
