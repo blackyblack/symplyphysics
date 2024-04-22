17ba1aaf6534177ed6fe933db9c9b38262492080
Ростислав Житков <rosjit1@gmail.com>
2024-04-22 19:53:06 +0300

    Commit #1. butterworth_filter_order_from_distortion_and_frequencies

---
+++ symplyphysics/laws/electricity/circuits/butterworth_filter_order_from_distortion_and_frequencies.py
@@ -0,0 +1,47 @@
+from math import ceil
+from sympy import (Eq, solve, log)
+from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
+    validate_output, dimensionless, convert_to_float)
+
+# Description
+## The approximation of the power transmission coefficient of a normalized low-pass filter is given by approximating functions of the order of n.
+## The Butterworth filter is described by the function frequency^n.
+## Bandwidth distortion determines the maximum distortion in the bandwidth. In other words, to determine the level of ripples in the bandwidth.
+## The band-stop distortion distortion determines the level of distortion in the band-stop.
+
+## Law is: N = ln(es / ep) / ln(fs, fp), where
+## N - filter order,
+## es - band-stop distortion,
+## ep - bandwidth distortion,
+## fs - frequency of the band-stop (the frequency that corresponds to the maximum band-stop distortion),
+## fp - cutoff frequency (the frequency that corresponds to the maximum bandwidth distortion).
+
+filter_order = Symbol("filter_order", dimensionless)
+
+bandwidth_distortion = Symbol("bandwidth_distortion", dimensionless)
+band_stop_distortion = Symbol("band_stop_distortion", dimensionless)
+cutoff_frequency = Symbol("cutoff_frequency", units.frequency)
+band_stop_frequency = Symbol("band_stop_frequency", units.frequency)
+
+law = Eq(filter_order, log(band_stop_distortion / bandwidth_distortion) / log(band_stop_frequency / cutoff_frequency))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(bandwidth_distortion_=bandwidth_distortion,
+    band_stop_distortion_=band_stop_distortion,
+    band_stop_frequency_=band_stop_frequency,
+    cutoff_frequency_=cutoff_frequency)
+@validate_output(filter_order)
+def calculate_order(bandwidth_distortion_: float, band_stop_distortion_: float, band_stop_frequency_: Quantity,
+    cutoff_frequency_: Quantity) -> int:
+    result_expr = solve(law, filter_order, dict=True)[0][filter_order]
+    result_expr = result_expr.subs({
+        bandwidth_distortion: bandwidth_distortion_,
+        band_stop_distortion: band_stop_distortion_,
+        band_stop_frequency: band_stop_frequency_,
+        cutoff_frequency: cutoff_frequency_,
+    })
+    return ceil(convert_to_float(result_expr))
---
+++ test/electricity/circuits/butterworth_filter_order_from_distortion_and_frequencies_test.py
@@ -0,0 +1,56 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from sympy import pi
+from symplyphysics import (assert_equal, units, Quantity, errors)
+from symplyphysics.laws.electricity.circuits import butterworth_filter_order_from_distortion_and_frequencies as order_law
+
+# Description
+## The cutoff frequency is 1 hertz. The frequency of the band-stop is 5 Hertz. The distortion in the bandwidth is 0.5, the band-stop distortion
+## is 31 and the Butterworth filter function will be used as an approximating function. Then the order of the Butterworth filter will be 3.
+## https://ru.dsplib.org/content/filter_butter_ap/filter_butter_ap.html#r3
+
+Args = namedtuple("Args", ["bandwidth_distortion", "band_stop_distortion", "cutoff_frequency", "band_stop_frequency"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    bandwidth_distortion = 0.5
+    band_stop_distortion = 31
+    cutoff_frequency = Quantity(1 * units.hertz)
+    band_stop_frequency = Quantity(5 * units.hertz)
+    return Args(bandwidth_distortion=bandwidth_distortion,
+        band_stop_distortion=band_stop_distortion,
+        band_stop_frequency=band_stop_frequency,
+        cutoff_frequency=cutoff_frequency)
+
+
+def test_basic_order(test_args: Args) -> None:
+    result = order_law.calculate_order(test_args.bandwidth_distortion, test_args.band_stop_distortion,
+        test_args.band_stop_frequency, test_args.cutoff_frequency)
+    assert_equal(result, 3)
+
+
+def test_bad_distortions(test_args: Args) -> None:
+    bad_distortion = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        order_law.calculate_order(bad_distortion, test_args.band_stop_distortion, test_args.band_stop_frequency,
+            test_args.cutoff_frequency)
+    with raises(errors.UnitsError):
+        order_law.calculate_order(test_args.bandwidth_distortion, bad_distortion, test_args.band_stop_frequency,
+            test_args.cutoff_frequency)
+
+
+def test_bad_frequencies(test_args: Args) -> None:
+    bad_frequency = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        order_law.calculate_order(test_args.bandwidth_distortion, test_args.band_stop_distortion, bad_frequency,
+            test_args.cutoff_frequency)
+    with raises(TypeError):
+        order_law.calculate_order(test_args.bandwidth_distortion, test_args.band_stop_distortion, 100,
+            test_args.cutoff_frequency)
+    with raises(errors.UnitsError):
+        order_law.calculate_order(test_args.bandwidth_distortion, test_args.band_stop_distortion, test_args.band_stop_frequency,
+            bad_frequency)
+    with raises(TypeError):
+        order_law.calculate_order(test_args.bandwidth_distortion, test_args.band_stop_distortion, test_args.band_stop_frequency,
+            100)
