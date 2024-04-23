7a0b91192142d75e8b188c6ec460daffe1d36975
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-23 18:34:43 +0400

    Derive law

--- symplyphysics/laws/thermodynamics/entropy_is_derivative_of_gibbs_energy.py
+++ symplyphysics/laws/thermodynamics/entropy_is_derivative_of_gibbs_energy.py
@@ -1,4 +1,4 @@
-from sympy import Eq, Derivative
+from sympy import Eq, Derivative, solve
 from symplyphysics import (
     units,
     dimensionless,
@@ -9,6 +9,8 @@ from symplyphysics import (
     validate_output,
     symbols,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.thermodynamics import gibbs_energy_differential
 
 # Description
 ## Entropy of a system can be found if its Gibbs energy is known as a function of temperature.
@@ -28,6 +30,22 @@ pressure = Symbol("pressure", units.pressure)
 particle_count = Symbol("particle_count", dimensionless)
 
 law = Eq(entropy, -1 * Derivative(gibbs_energy(temperature, pressure, particle_count), temperature))
+
+# Derive from the expression of Gibbs energy differential
+
+_gibbs_energy_change_eqn = gibbs_energy_differential.law.subs({
+    gibbs_energy_differential.pressure_change: 0,
+    gibbs_energy_differential.particle_count_change: 0,
+})
+
+_entropy_derived = solve(
+    _gibbs_energy_change_eqn, gibbs_energy_differential.entropy,
+)[0].subs(
+    gibbs_energy_differential.gibbs_energy_change,
+    Derivative(gibbs_energy(temperature, pressure, particle_count), temperature) * gibbs_energy_differential.temperature_change
+)
+
+assert expr_equals(_entropy_derived, law.rhs)
 
 
 @validate_input(
