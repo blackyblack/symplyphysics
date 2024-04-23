3e4b4cb20ed929ac5bc5f5d5d66a5eaa6c480563
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-04-23 14:07:19 +0400

    Define & derive: coefficients of thermal expansion (#662)
    * Create definition and test (linear expansion)
    
    * Derive expansion coefficients relation
    
    * Add derivation of volume from length

--- symplyphysics/laws/thermodynamics/volumetric_and_linear_expansion_coefficients_in_isotropic_materials.py
+++ symplyphysics/laws/thermodynamics/volumetric_and_linear_expansion_coefficients_in_isotropic_materials.py
@@ -1,4 +1,4 @@
-from sympy import Eq
+from sympy import Eq, symbols, solve
 from symplyphysics import (
     units,
     Quantity,
@@ -6,6 +6,11 @@ from symplyphysics import (
     print_expression,
     validate_input,
     validate_output,
+)
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.definitions import (
+    linear_coefficient_of_thermal_expansion as linear_def,
+    volumetric_coefficient_of_thermal_expansion as volumetric_def,
 )
 
 # Description
@@ -21,7 +26,43 @@ linear_expansion_coefficient = Symbol("linear_expansion_coefficient", 1 / units.
 
 law = Eq(volumetric_expansion_coefficient, 3 * linear_expansion_coefficient)
 
-# TODO: Derive from their definitions for a cube-sized object
+# Derive from their definitions for a cube-sized object
+
+_volume_change = symbols("volume_change")
+_initial_length, _length_change = symbols("initial_length length_change")
+_temperature_change = symbols("temperature_change")
+
+_final_length_expr = _initial_length + _length_change
+
+_volume, _length = symbols("volume length")
+_volume_eqn = Eq(_volume, _length**3)
+_volume_expr = solve(_volume_eqn, _volume)[0]
+_initial_volume_expr = _volume_expr.subs(_length, _initial_length)
+_final_volume_expr = _volume_expr.subs(_length, _final_length_expr)
+
+_volumetric_expr = volumetric_def.definition.rhs.subs({
+    volumetric_def.volume(volumetric_def.temperature).diff(volumetric_def.temperature): _volume_change / _temperature_change,
+    _volume_change: _final_volume_expr - _initial_volume_expr,
+    volumetric_def.volume(volumetric_def.temperature): _initial_volume_expr,
+})
+
+# Note: if dimension changes are not small, the third and fourth terms of the following series expansion
+# can be taken into account.
+_volumetric_expr_series = _volumetric_expr.series(_length_change, 0, 2).removeO()
+
+_linear_eqn = linear_def.definition.subs({
+    linear_def.length(linear_def.temperature).diff(linear_def.temperature): _length_change / _temperature_change,
+    linear_def.length(linear_def.temperature): _initial_length,
+    linear_def.linear_expansion_coefficient: linear_expansion_coefficient,
+})
+
+_volumetric_expr_derived = solve(
+    (Eq(volumetric_expansion_coefficient, _volumetric_expr_series), _linear_eqn),
+    (volumetric_expansion_coefficient, _length_change),
+    dict=True,
+)[0][volumetric_expansion_coefficient]
+
+assert expr_equals(_volumetric_expr_derived, law.rhs)
 
 
 def print_law() -> str:
