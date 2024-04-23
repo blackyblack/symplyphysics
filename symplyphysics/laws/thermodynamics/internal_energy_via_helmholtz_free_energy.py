e73597c1e3a52a536e1858282e4036630addff9b
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-04-23 17:07:43 +0400

    Law: Gibbs—Helmholtz relation for internal energy (#690)
    * Create law and test
    
    * Fix law description

---
+++ symplyphysics/laws/thermodynamics/internal_energy_via_helmholtz_free_energy.py
@@ -0,0 +1,70 @@
+from sympy import Eq, Derivative, Point2D
+from symplyphysics import (
+    symbols,
+    units,
+    Quantity,
+    Symbol,
+    Function,
+    print_expression,
+    validate_input,
+    validate_output,
+)
+from symplyphysics.core.geometry.line import two_point_function
+
+# Description
+## Gibbs-Helmholtz relations are a set of equations that relate thermodynamic potentials between each other.
+
+# Law: U = F - T * (dF/dT)_V
+## U - internal energy
+## F - Helmholtz free energy
+## T - absolute temperature
+## V - volume
+## (d/dT)_V - derivative with respect to temperature at constant volume
+
+internal_energy = Symbol("internal_energy", units.energy)
+free_energy = Function("free_energy", units.energy)
+temperature = symbols.thermodynamics.temperature
+volume = Symbol("volume", units.volume)
+
+# Volume is held constant during the evalution of the derivative
+
+law = Eq(
+    internal_energy,
+    free_energy(temperature, volume) - temperature * Derivative(free_energy(temperature, volume), temperature)
+)
+
+# TODO: Derive from definition of free energy and thermodynamical Maxwell relations
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(
+    free_energy_before_=free_energy,
+    free_energy_after_=free_energy,
+    temperature_before_=temperature,
+    temperature_after_=temperature,
+    temperature_=temperature,
+)
+@validate_output(internal_energy)
+def calculate_internal_energy(
+    free_energy_before_: Quantity,
+    free_energy_after_: Quantity,
+    temperature_before_: Quantity,
+    temperature_after_: Quantity,
+    temperature_: Quantity,
+) -> Quantity:
+    free_energy_ = two_point_function(
+        Point2D(temperature_before_, free_energy_before_),
+        Point2D(temperature_after_, free_energy_after_),
+        temperature,
+    )
+
+    result = law.rhs.subs(
+        free_energy(temperature, volume), free_energy_
+    ).doit().subs(
+        temperature, temperature_
+    )
+
+    return Quantity(result)
---
+++ test/thermodynamics/internal_energy_via_helmholtz_free_energy_test.py
@@ -0,0 +1,60 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    prefixes,
+    Quantity,
+)
+from symplyphysics.laws.thermodynamics import internal_energy_via_helmholtz_free_energy as gibbs_helmholtz_law
+
+# Description
+## A thermodynamic system is at temperature T = 300 K. A thermodynamic process happens around this temperature
+## point, in which the value of the free energy is F = 100 J at the beginning (T = 299 K) and F = 110 J at the
+## end (T = 301 K). The value of the internal energy at T = 300 K is U = -1.4 kJ.
+
+Args = namedtuple("Args", "f0 f1 t0 t1 t")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    f0 = Quantity(100 * units.joule)
+    f1 = Quantity(110 * units.joule)
+    t0 = Quantity(299 * units.kelvin)
+    t1 = Quantity(301 * units.kelvin)
+    t = Quantity(300 * units.kelvin)
+    return Args(f0=f0, f1=f1, t0=t0, t1=t1, t=t)
+
+
+def test_law(test_args: Args) -> None:
+    result = gibbs_helmholtz_law.calculate_internal_energy(test_args.f0, test_args.f1, test_args.t0, test_args.t1, test_args.t)
+    assert_equal(result, -1.4 * prefixes.kilo * units.joule, tolerance=4e-3)
+
+
+def test_bad_energy(test_args: Args) -> None:
+    fb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        gibbs_helmholtz_law.calculate_internal_energy(fb, test_args.f1, test_args.t0, test_args.t1, test_args.t)
+    with raises(TypeError):
+        gibbs_helmholtz_law.calculate_internal_energy(100, test_args.f1, test_args.t0, test_args.t1, test_args.t)
+    with raises(errors.UnitsError):
+        gibbs_helmholtz_law.calculate_internal_energy(test_args.f0, fb, test_args.t0, test_args.t1, test_args.t)
+    with raises(TypeError):
+        gibbs_helmholtz_law.calculate_internal_energy(test_args.f0, 100, test_args.t0, test_args.t1, test_args.t)
+
+
+def test_bad_temperature(test_args: Args) -> None:
+    tb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        gibbs_helmholtz_law.calculate_internal_energy(test_args.f0, test_args.f1, tb, test_args.t1, test_args.t)
+    with raises(TypeError):
+        gibbs_helmholtz_law.calculate_internal_energy(test_args.f0, test_args.f1, 100, test_args.t1, test_args.t)
+    with raises(errors.UnitsError):
+        gibbs_helmholtz_law.calculate_internal_energy(test_args.f0, test_args.f1, test_args.t0, tb, test_args.t)
+    with raises(TypeError):
+        gibbs_helmholtz_law.calculate_internal_energy(test_args.f0, test_args.f1, test_args.t0, 100, test_args.t)
+    with raises(errors.UnitsError):
+        gibbs_helmholtz_law.calculate_internal_energy(test_args.f0, test_args.f1, test_args.t0, test_args.t1, tb)
+    with raises(TypeError):
+        gibbs_helmholtz_law.calculate_internal_energy(test_args.f0, test_args.f1, test_args.t0, test_args.t1, 100)
