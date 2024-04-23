587019528e59f3285a5f38884df7eb1e036d7be7
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-23 18:11:07 +0400

    Derive law

--- symplyphysics/laws/thermodynamics/temperature_is_derivative_of_internal_energy.py
+++ symplyphysics/laws/thermodynamics/temperature_is_derivative_of_internal_energy.py
@@ -1,6 +1,7 @@
-from sympy import Eq, Derivative
+from sympy import Eq, Derivative, solve
 from symplyphysics import (
     units,
+    dimensionless,
     Quantity,
     Symbol,
     Function,
@@ -9,25 +10,43 @@ from symplyphysics import (
     validate_output,
     symbols,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.thermodynamics import internal_energy_differential
 
 # Description
-## Temperature of a thermodynamic system can be found if the function of the internal energy is known.
+## Temperature of a thermodynamic system can be found internal energy is known as a function of entropy.
 
-# Law: T = (dU/dS)_V
+# Law: T = (dU/dS)_(V, N)
 ## T - absolute temperature
 ## U - internal energy
 ## S - entropy
 ## V - volume
-## (d/dS)_V - derivative with respect to entropy at constant volume
+## N - particle count
+## (d/dS)_(V, N) - derivative with respect to entropy at constant volume and particle count
 
 temperature = symbols.thermodynamics.temperature
 internal_energy = Function("internal_energy", units.energy)
 entropy = Symbol("entropy", units.energy / units.temperature)
 volume = Symbol("volume", units.volume)
+particle_count = Symbol("particle_count", dimensionless)
 
-law = Eq(temperature, Derivative(internal_energy(entropy, volume), entropy))
+law = Eq(temperature, Derivative(internal_energy(entropy, volume, particle_count), entropy))
 
-# TODO: derive from fundamental relation for internal energy
+# Derive from fundamental relation for internal energy
+
+_internal_energy_change_eqn = internal_energy_differential.law.subs({
+    internal_energy_differential.volume_change: 0,
+    internal_energy_differential.particle_count_change: 0,
+})
+
+_temperature_derived = solve(
+    _internal_energy_change_eqn, internal_energy_differential.temperature
+)[0].subs(
+    internal_energy_differential.internal_energy_change,
+    Derivative(internal_energy(entropy, volume, particle_count), entropy) * internal_energy_differential.entropy_change,
+)
+
+assert expr_equals(_temperature_derived, law.rhs)
 
 
 def print_law() -> str:
@@ -45,6 +64,6 @@ def calculate_temperature(
 ) -> Quantity:
     internal_energy_ = (internal_energy_change_ / entropy_change_) * entropy
     result = law.rhs.subs(
-        internal_energy(entropy, volume), internal_energy_
+        internal_energy(entropy, volume, particle_count), internal_energy_
     ).doit()
     return Quantity(result)
