4d3aa91a5d2234cf29b54ae65e6b91c1b5fade87
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-23 18:27:52 +0400

    Derive law

--- symplyphysics/laws/thermodynamics/entropy_is_derivative_of_free_energy.py
+++ symplyphysics/laws/thermodynamics/entropy_is_derivative_of_free_energy.py
@@ -1,4 +1,4 @@
-from sympy import Eq, Derivative
+from sympy import Eq, Derivative, solve
 from symplyphysics import (
     units,
     dimensionless,
@@ -9,6 +9,8 @@ from symplyphysics import (
     validate_output,
     symbols,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.thermodynamics import free_energy_differential
 
 # Description
 ## Entropy of a system can be found if its free energy is known as a function of temperature.
@@ -28,6 +30,22 @@ volume = Symbol("volume", units.volume)
 particle_count = Symbol("particle_count", dimensionless)
 
 law = Eq(entropy, -1 * Derivative(free_energy(temperature, volume, particle_count), temperature))
+
+# Derive from expression of free energy differential
+
+_free_energy_change_eqn = free_energy_differential.law.subs({
+    free_energy_differential.volume_change: 0,
+    free_energy_differential.particle_count_change: 0,
+})
+
+_entropy_derived = solve(
+    _free_energy_change_eqn, free_energy_differential.entropy
+)[0].subs(
+    free_energy_differential.free_energy_change,
+    Derivative(free_energy(temperature, volume, particle_count), temperature) * free_energy_differential.temperature_change
+)
+
+assert expr_equals(_entropy_derived, law.rhs)
 
 
 @validate_input(
