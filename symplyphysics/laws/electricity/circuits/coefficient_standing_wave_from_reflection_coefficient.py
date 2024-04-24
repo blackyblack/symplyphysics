23ab922e72084c25b399ac6fb7519f5dc3eef96f
Ростислав Житков <rosjit1@gmail.com>
2024-04-24 18:29:13 +0300

    Commit #1. coefficient_standing_wave_from_reflection_coefficient

---
+++ symplyphysics/laws/electricity/circuits/coefficient_standing_wave_from_reflection_coefficient.py
@@ -0,0 +1,33 @@
+from sympy import Eq, solve
+from symplyphysics import (Symbol, print_expression, validate_input,
+    validate_output, dimensionless, convert_to_float)
+
+# Description
+## Standing wave coefficient is the ratio of the highest voltage in the transmission line to the lowest voltage. It is a measure of matching the load 
+## with the transmission line. The coefficient in the transmission line does not depend on the internal resistance of the electromagnetic
+## wave source and (in the case of a linear load) on the power of the generator.
+## Also, the standing wave coefficient can be calculated by knowing the reflection coefficient module.
+
+## Law is: K = (1 + G) / (1 - G), where
+## K - standing wave coefficient,
+## G - reflection coefficient module.
+
+coefficient_standing_wave = Symbol("coefficient_standing_wave", dimensionless)
+
+reflection_coefficient_module = Symbol("reflection_coefficient_module", dimensionless)
+
+law = Eq(coefficient_standing_wave, (1 + reflection_coefficient_module) / (1 - reflection_coefficient_module))
+
+
+def print_law() -> str:
+    return print_expression(law)
+
+
+@validate_input(reflection_coefficient_module_=reflection_coefficient_module)
+@validate_output(coefficient_standing_wave)
+def calculate_coefficient_standing_wave(reflection_coefficient_module_: float) -> float:
+    result_expr = solve(law, coefficient_standing_wave, dict=True)[0][coefficient_standing_wave]
+    result_expr = result_expr.subs({
+        reflection_coefficient_module: reflection_coefficient_module_,
+    })
+    return convert_to_float(result_expr)
---
+++ test/electricity/circuits/coefficient_standing_wave_from_reflection_coefficient_test.py
@@ -0,0 +1,28 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (assert_equal, units, Quantity, errors)
+from symplyphysics.laws.electricity.circuits import coefficient_standing_wave_from_reflection_coefficient as coefficient_law
+
+# Description
+## The reflection coefficient module is 0.2.
+## Then the standing wave coefficient is 1.5.
+
+Args = namedtuple("Args", ["reflection_coefficient_module"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    reflection_coefficient_module = 0.2
+
+    return Args(reflection_coefficient_module=reflection_coefficient_module)
+
+
+def test_basic_coefficient_standing_wave(test_args: Args) -> None:
+    result = coefficient_law.calculate_coefficient_standing_wave(test_args.reflection_coefficient_module)
+    assert_equal(result, 1.5)
+
+
+def test_bad_voltages() -> None:
+    bad_voltage = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        coefficient_law.calculate_coefficient_standing_wave(bad_voltage)
