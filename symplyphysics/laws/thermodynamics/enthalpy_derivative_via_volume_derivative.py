95bca308aeaa4c54ae236bf4be6f29c5e083e65d
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-24 12:07:26 +0400

    Create law and test

---
+++ symplyphysics/laws/thermodynamics/enthalpy_derivative_via_volume_derivative.py
@@ -0,0 +1,62 @@
+from sympy import Eq, Derivative
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    Function,
+    validate_input,
+    validate_output,
+    symbols,
+)
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.core.geometry.line import two_point_function, Point2D
+
+# Description
+## ...
+
+# (dH/dp)_T = V(T, p) - T * (dV/dT)_p
+## H - enthalpy
+## T - absolute temperature
+## p - pressure
+## V - volume
+
+# Conditions
+## - Assuming an infinitesimal quasi-static isothermal prossess.
+
+enthalpy = Function("enthalpy", units.energy)
+temperature = symbols.thermodynamics.temperature
+pressure = Symbol("pressure", units.pressure)
+volume = Function("volume", units.volume)
+
+law = Eq(
+    Derivative(enthalpy(temperature, pressure), pressure),
+    volume(temperature, pressure) - temperature * Derivative(volume(temperature, pressure), temperature)
+)
+
+# TODO: Derive from differential of enthalpy and Maxwell relation
+
+
+@validate_input(
+    volume_before_=volume,
+    volume_after_=volume,
+    temperature_before_=temperature,
+    temperature_after_=temperature,
+)
+@validate_output(units.energy / units.pressure)
+def calculate_enthapy_derivative(
+    volume_before_: Quantity,
+    volume_after_: Quantity,
+    temperature_before_: Quantity,
+    temperature_after_: Quantity,
+) -> Quantity:
+    volume_ = two_point_function(
+        Point2D(temperature_before_, volume_before_),
+        Point2D(temperature_after_, volume_after_),
+        temperature,
+    )
+    result = law.rhs.subs(volume(temperature, pressure), volume_).doit()
+
+    # the result does not depend on temperature
+    assert expr_equals(result.diff(temperature), 0)
+
+    return Quantity(result)
