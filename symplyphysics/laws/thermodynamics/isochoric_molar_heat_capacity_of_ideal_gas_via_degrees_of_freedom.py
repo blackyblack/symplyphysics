0470450922cd812818d33d1f5f226a49a9644fe6
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-25 08:54:02 +0400

    Create law and test

---
+++ symplyphysics/laws/thermodynamics/isochoric_molar_heat_capacity_of_ideal_gas_via_degrees_of_freedom.py
@@ -0,0 +1,39 @@
+from sympy import Eq
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## The internal energy of an ideal gas consisting of molecules whose energy is all kinetic
+## depends only on the degrees of freedom of the molecule and the temperature of the gas.
+## From that can be derived the expression of the isochoric heat capacity of ideal gases.
+
+# Law: C_V = (f / 2) * R
+## C_V - isochoric molar heat capacity
+## f - degrees of freedom of gas molecules
+## R - molar gas constant
+
+# Note
+## - For applications, see [internal energy law](./internal_energy_of_ideal_gas_is_proportional_to_temperature)
+
+isochoric_molar_heat_capacity = Symbol(
+    "isochoric_molar_heat_capacity",
+    units.energy / (units.temperature * units.amount_of_substance),
+)
+degrees_of_freedom = Symbol("degrees_of_freedom", integer=True)
+
+law = Eq(
+    isochoric_molar_heat_capacity,
+    (degrees_of_freedom / 2) * units.molar_gas_constant
+)
+
+
+@validate_input(degrees_of_freedom_=degrees_of_freedom)
+@validate_output(isochoric_molar_heat_capacity)
+def calculate_isochoric_molar_heat_capacity(degrees_of_freedom_: int) -> Quantity:
+    result = law.rhs.subs(degrees_of_freedom, degrees_of_freedom_)
+    return Quantity(result)
