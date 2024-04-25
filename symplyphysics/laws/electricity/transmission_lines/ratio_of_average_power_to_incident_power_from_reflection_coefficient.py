4e0004cdd00ce3a8ada5fe44bb43773916001a5b
Ростислав Житков <rosjit1@gmail.com>
2024-04-25 18:49:37 +0300

    Commit #1. ratio_of_average_power_to_incident_power_from_reflection_coefficient

---
+++ symplyphysics/laws/electricity/transmission_lines/ratio_of_average_power_to_incident_power_from_reflection_coefficient.py
@@ -0,0 +1,36 @@
+from sympy import Eq, solve
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    validate_input,
+    validate_output,
+    dimensionless,
+    convert_to_float,
+)
+
+# Description
+## The reflection coefficient is equal to the ratio of the reflected wave power to the incident wave power.
+## Knowing the average power delivered to the load and the incident power, it is possible to calculate the absolute value reflection coefficient.
+
+## Law is: Pa / Pi = 1 - G^2, where
+## Pa - the average power delivered to the load,
+## Pi - the incident power,
+## K - the absolute value reflection coefficient.
+
+absolute_reflection_coefficient = Symbol("absolute_reflection_coefficient", dimensionless, rel=True)
+incident_power = Symbol("incident_power", units.power, rel=True)
+average_power = Symbol("average_power", units.power, rel=True)
+
+law = Eq(average_power / incident_power, 1 - absolute_reflection_coefficient**2)
+
+
+@validate_input(incident_power_=incident_power, average_power_=average_power)
+@validate_output(absolute_reflection_coefficient)
+def calculate_absolute_reflection_coefficient(incident_power_: Quantity, average_power_: Quantity) -> float:
+    result_expr = solve(law, absolute_reflection_coefficient, dict=True)[0][absolute_reflection_coefficient]
+    result_expr = result_expr.subs({
+        incident_power: incident_power_,
+        average_power: average_power_,
+    })
+    return convert_to_float(result_expr)
