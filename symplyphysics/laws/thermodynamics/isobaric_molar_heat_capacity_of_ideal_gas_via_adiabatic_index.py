6b06852fe09cd268a755caf5dd23c93e139f4595
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-04-25 13:18:39 +0400

    Law: isobaric heat capacity via adiabatic index (#706)
    * Create law and test
    
    * Derive law
    
    * Fix test description

---
+++ symplyphysics/laws/thermodynamics/isobaric_molar_heat_capacity_of_ideal_gas_via_adiabatic_index.py
@@ -0,0 +1,70 @@
+from sympy import Eq, solve
+from symplyphysics import (
+    units,
+    dimensionless,
+    Quantity,
+    Symbol,
+    validate_input,
+    validate_output,
+)
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.definitions import heat_capacity_ratio
+from symplyphysics.laws.thermodynamics import (
+    isochoric_heat_capacity_via_isobaric_heat_capacity_for_ideal_gas as mayers_law,
+)
+from symplyphysics.laws.quantities import (
+    quantity_is_molar_quantity_times_amount_of_substance as molar_qty_law,
+)
+
+# Description
+## For ideal gases the isobaric and isochoric heat capacities can be calculated via the molar gas
+## constant and the adiabatic index of the gas.
+
+# Law: C_p = R * gamma / (gamma - 1)
+## C_p - isobaric molar heat capacity
+## R - molar gas constant
+## gamma - adiabatic index, or [heat capacity ratio](../../definitions/heat_capacity_ratio.py)
+
+isobaric_molar_heat_capacity = Symbol(
+    "isobaric_molar_heat_capacity",
+    units.energy / (units.temperature * units.amount_of_substance)
+)
+adiabatic_index = Symbol("adiabatic_index", dimensionless)
+
+law = Eq(
+    isobaric_molar_heat_capacity,
+    units.molar_gas_constant * adiabatic_index / (adiabatic_index - 1)
+)
+
+# Derive law from the Mayer's law for heat capacities and the definition of adiabatic index
+
+_isobaric_heat = mayers_law.isobaric_heat_capacity
+_isochoric_heat = mayers_law.isochoric_heat_capacity
+
+_adiabatic_index_eqn = heat_capacity_ratio.definition.subs({
+    heat_capacity_ratio.isobaric_heat_capacity: _isobaric_heat,
+    heat_capacity_ratio.isochoric_heat_capacity: _isochoric_heat,
+    heat_capacity_ratio.heat_capacity_ratio: adiabatic_index,
+})
+
+_isobaric_heat_expr = solve(
+    (mayers_law.law, _adiabatic_index_eqn),
+    (_isobaric_heat, _isochoric_heat),
+    dict=True,
+)[0][_isobaric_heat]
+
+_molar_isobaric_heat = solve(
+    molar_qty_law.law, molar_qty_law.molar_quantity
+)[0].subs({
+    molar_qty_law.extensive_quantity: _isobaric_heat_expr,
+    molar_qty_law.amount_of_substance: mayers_law.amount_of_substance,
+})
+
+assert expr_equals(_molar_isobaric_heat, law.rhs)
+
+
+@validate_input(adiabatic_index_=adiabatic_index)
+@validate_output(isobaric_molar_heat_capacity)
+def calculate_isobaric_molar_heat_capacity(adiabatic_index_: float) -> Quantity:
+    result = law.rhs.subs(adiabatic_index, adiabatic_index_)
+    return Quantity(result)
