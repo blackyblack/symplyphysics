9766efc881b7dfe414125f297073bc8dec121546
Aleksandr Slatov <alexslatov@gmail.com>
2024-04-28 08:36:40 +0400

    Add law formula description

--- symplyphysics/laws/quantum_mechanics/schrodinger/time_dependent_equation_in_one_dimension.py
+++ symplyphysics/laws/quantum_mechanics/schrodinger/time_dependent_equation_in_one_dimension.py
@@ -13,6 +13,17 @@ from symplyphysics import (
 ## function of a quantum-mechanical system. This law describes the general case of a time-dependent
 ## potential and a time-dependent wave function.
 
+# Law: (-hbar**2 / (2 * m)) * d^2(Psi(x, t))/dt^2 + U(x, t) * Psi(x, t) = i * hbar * d(Psi(x, t))/dt
+## Psi(x, t) - time-dependent wave function
+## x - position, spatial variable
+## t - time
+## hbar - reduced Planck constant
+## m - mass of quantum particle
+## U - potential energy
+## d/dt - time derivative
+## d/dx - spatial derivative
+## i - imaginary unit
+
 # Notes
 ## - This law works in the case of 1 spatial dimension. To use it for the 3-dimensional space
 ##   replace the spatial second derivative with the Laplace operator.
