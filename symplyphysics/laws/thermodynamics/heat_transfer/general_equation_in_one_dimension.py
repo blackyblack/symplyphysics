20d92ca7e5271e4e97c0befa3012e92eca3cdc43
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-04-28 13:50:57 +0400

    Law: general heat equation (#730)
    * Create law and test
    
    * Fix test values and law description
    
    * Remove unused code

---
+++ symplyphysics/laws/thermodynamics/heat_transfer/general_equation_in_one_dimension.py
@@ -0,0 +1,50 @@
+from sympy import Eq, Derivative
+from symplyphysics import (
+    units,
+    Symbol,
+    Function,
+)
+
+# Description
+## Heat equation governs heat diffusion, as well as other diffusive processes. It describes
+## the evolution of heat transferred from hotter to colder environments in time and space.
+
+# Law: rho * c_v * dT/dt = d(k(x) * dT/dx)/dx + q(x, t)
+## T = T(x, t) - temperature
+## rho - density of medium
+## c_v - mass-specific heat capacity of medium
+## k - [thermal conductivity](https://en.wikipedia.org/wiki/Thermal_conductivity_and_resistivity#Definition) of medium
+## q - density of heat sources
+## x - position, spatial variable
+## t - time
+## d/dt - time derivative
+## d/dx - spatial derivative
+
+# Notes
+## - To get a similar equation for the 3-dimensional case, replace the spatial
+##   derivative with the gradient operator.
+## - Thermal conductivity can depend not only on position, but also on local temperature, but this case is out
+##   of the scope of this law.
+
+temperature = Function("temperature", units.temperature)
+medium_density = Symbol("medium_density", units.mass / units.volume)
+medium_specific_heat_capacity = Symbol(
+    "medium_specific_heat_capacity",
+    units.energy / (units.temperature * units.mass)
+)
+thermal_conductivity = Function(
+    "thermal_conductivity",
+    units.power / (units.length * units.temperature),
+)
+heat_source_density = Function(
+    "heat_source_density",
+    units.power / units.volume,
+)
+position = Symbol("position", units.length)
+time = Symbol("time", units.time)
+
+law = Eq(
+    medium_density * medium_specific_heat_capacity * Derivative(temperature(position, time), time),
+    Derivative(thermal_conductivity(position) * Derivative(temperature(position, time), position), position)
+    + heat_source_density(position, time)
+)
---
+++ test/thermodynamics/heat_transfer/general_equation_in_one_dimension_test.py
@@ -0,0 +1,35 @@
+from collections import namedtuple
+from pytest import fixture
+from sympy import sin, exp, Rational
+from symplyphysics.core.expr_comparisons import expr_equals
+from symplyphysics.laws.thermodynamics.heat_transfer import (
+    general_equation_in_one_dimension as heat_equation
+)
+
+# Description
+## The values of the parameters are that of carbon monoxide (CO).
+
+Args = namedtuple("Args", "rho cv k q T")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    rho = Rational(15, 10)  # kg/m**3
+    cv = 1500  # J/(K * kg)
+    k = Rational(24, 1000)  # W/(K*m)
+    q = 0
+    T = sin(100 * heat_equation.position) * exp(Rational(-8, 75) * heat_equation.time)
+    return Args(rho=rho, cv=cv, k=k, q=q, T=T)
+
+
+def test_law(test_args: Args) -> None:
+    values = {
+        heat_equation.medium_density: test_args.rho,
+        heat_equation.medium_specific_heat_capacity: test_args.cv,
+        heat_equation.thermal_conductivity(heat_equation.position): test_args.k,
+        heat_equation.heat_source_density(heat_equation.position, heat_equation.time): test_args.q,
+        heat_equation.temperature(heat_equation.position, heat_equation.time): test_args.T,
+    }
+    lhs = heat_equation.law.lhs.subs(values).doit()
+    rhs = heat_equation.law.rhs.subs(values).doit()
+    assert expr_equals(lhs, rhs)
