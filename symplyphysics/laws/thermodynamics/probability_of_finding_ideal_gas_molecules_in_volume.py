fe19fa436819868288cfdee29305e438a41e1525
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-05-07 20:07:54 +0400

    Law: probability of finding gas molecules in volume (#749)
    * Create law and test
    
    * Fix calculate function and add test case

---
+++ symplyphysics/laws/thermodynamics/probability_of_finding_ideal_gas_molecules_in_volume.py
@@ -0,0 +1,57 @@
+from sympy import Eq
+from symplyphysics import (
+    Quantity,
+    Symbol,
+    dimensionless,
+    units,
+    validate_input,
+    validate_output,
+    convert_to_float,
+)
+from symplyphysics.core.symbols.probability import Probability
+
+# Description
+## For ideal gas molecules, we can calculate the probability of N molecules occupying a certain region
+## in space, say, a container with gas particles.
+
+# Law: P = (V / V0)**N
+## P - probability
+## V0 - total volume of container
+## V - volume of part of container
+## N - particle count
+
+# Conditions
+## - Gas is ideal
+## - `V <= V0`
+## - There are no external fields acting on the particles
+
+probability = Symbol("probability", dimensionless)
+total_volume = Symbol("total_volume", units.volume)
+partial_volume = Symbol("partial_volume", units.volume)
+particle_count = Symbol("particle_count", dimensionless, integer=True)
+
+
+law = Eq(probability, (partial_volume / total_volume)**particle_count)
+
+
+@validate_input(
+    total_volume_=total_volume,
+    partial_volume_=partial_volume,
+    particle_count_=particle_count,
+)
+@validate_output(probability)
+def calculate_probability(
+    total_volume_: Quantity,
+    partial_volume_: Quantity,
+    particle_count_: int,
+) -> Probability:
+    if partial_volume_.scale_factor > total_volume_.scale_factor:
+        raise ValueError("Partial volume must not exceed total volume")
+
+    result = law.rhs.subs({
+        total_volume: total_volume_,
+        partial_volume: partial_volume_,
+        particle_count: particle_count_,
+    })
+
+    return Probability(convert_to_float(result))
---
+++ test/thermodynamics/probability_of_finding_ideal_gas_molecules_in_volume_test.py
@@ -0,0 +1,48 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (
+    assert_equal,
+    errors,
+    units,
+    Quantity,
+)
+from symplyphysics.laws.thermodynamics import (
+    probability_of_finding_ideal_gas_molecules_in_volume as probability_law,
+)
+
+Args = namedtuple("Args", "v0 v n")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    v0 = Quantity(1 * units.liter)
+    v = Quantity(0.9 * units.liter)
+    n = 1000
+    return Args(v0, v, n)
+
+
+def test_law(test_args: Args) -> None:
+    result = probability_law.calculate_probability(test_args.v0, test_args.v, test_args.n)
+    assert_equal(result, 1.75e-46, tolerance=2e-3)
+
+
+def test_bad_volume(test_args: Args) -> None:
+    vb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        probability_law.calculate_probability(vb, test_args.v, test_args.n)
+    with raises(TypeError):
+        probability_law.calculate_probability(100, test_args.v, test_args.n)
+    with raises(errors.UnitsError):
+        probability_law.calculate_probability(test_args.v0, vb, test_args.n)
+    with raises(TypeError):
+        probability_law.calculate_probability(test_args.v0, 100, test_args.n)
+
+    vb = Quantity(2 * test_args.v0)
+    with raises(ValueError):
+        probability_law.calculate_probability(test_args.v0, vb, test_args.n)
+    
+
+def test_bad_number(test_args: Args) -> None:
+    nb = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        probability_law.calculate_probability(test_args.v0, test_args.v, nb)
