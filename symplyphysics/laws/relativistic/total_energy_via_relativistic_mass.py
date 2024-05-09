ff1a1dd01145e0c05e86186082d59ddcd75a3eb6
Aleksandr Slatov <alexslatov@gmail.com>
2024-05-09 17:12:07 +0400

    Fix symbol names

--- symplyphysics/laws/relativistic/total_energy_via_relativistic_mass.py
+++ symplyphysics/laws/relativistic/total_energy_via_relativistic_mass.py
@@ -1,5 +1,13 @@
 from sympy import Eq, solve
-from symplyphysics import units, Quantity, Symbol, validate_input, validate_output, symbols
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    validate_input,
+    validate_output,
+    symbols,
+    clone_symbol,
+)
 
 # Description
 ## Fundamentally the energy of an object is synonimical to its mass.
@@ -9,15 +17,15 @@ from symplyphysics import units, Quantity, Symbol, validate_input, validate_outp
 ## m is mass (relativistic mass, not only rest mass),
 ## c is speed of light.
 
-energy = Symbol("energy", units.energy)
-mass = symbols.basic.mass
+relativistic_energy = Symbol("energy", units.energy)
+relativistic_mass = clone_symbol(symbols.basic.mass, "relativistic_mass")
 
-law = Eq(energy, mass * units.speed_of_light**2)
+law = Eq(relativistic_energy, relativistic_mass * units.speed_of_light**2)
 
 
-@validate_input(mass_=mass)
-@validate_output(energy)
-def calculate_rest_energy(mass_: Quantity) -> Quantity:
-    result_expr = solve(law, energy, dict=True)[0][energy]
-    energy_applied = result_expr.subs({mass: mass_})
+@validate_input(relativistic_mass_=relativistic_mass)
+@validate_output(relativistic_energy)
+def calculate_rest_energy(relativistic_mass_: Quantity) -> Quantity:
+    result_expr = solve(law, relativistic_energy, dict=True)[0][relativistic_energy]
+    energy_applied = result_expr.subs({relativistic_mass: relativistic_mass_})
     return Quantity(energy_applied)
