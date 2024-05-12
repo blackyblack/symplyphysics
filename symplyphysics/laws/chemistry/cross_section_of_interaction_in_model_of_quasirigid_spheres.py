97287512f7f1d45a039297154f4fe823bacb2dca
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-05-12 20:32:30 +0300

    Commit #1. cross_section_of_interaction_in_model_of_quasirigid_spheres (#767)

---
+++ symplyphysics/laws/chemistry/cross_section_of_interaction_in_model_of_quasirigid_spheres.py
@@ -0,0 +1,27 @@
+from sympy import Eq, solve, pi
+from symplyphysics import (units, Quantity, Symbol, validate_input, validate_output,)
+
+# Description
+## The effective cross section is a physical quantity characterizing the probability of transition of a system of
+## two interacting particles to a certain final state, a quantitative characteristic of the acts of collision of
+## particles of a stream hitting a target with target particles. The effective cross-section has the dimension of the area.
+
+## Law is: g =  pi * r^2, where
+## g - the cross-sectional area of the interaction of particles,
+## r - the distance of the greatest convergence of two colliding particles.
+
+cross_sectional_area_of_interaction = Symbol("cross_sectional_area_of_interaction", units.area)
+
+distance_of_convergence_of_two_particles = Symbol("distance_of_convergence_of_two_particles", units.length)
+
+law = Eq(cross_sectional_area_of_interaction, pi * distance_of_convergence_of_two_particles**2)
+
+
+@validate_input(distance_of_convergence_of_two_particles_=distance_of_convergence_of_two_particles)
+@validate_output(cross_sectional_area_of_interaction)
+def calculate_cross_sectional_area_of_interaction(distance_of_convergence_of_two_particles_: Quantity) -> Quantity:
+    result_expr = solve(law, cross_sectional_area_of_interaction, dict=True)[0][cross_sectional_area_of_interaction]
+    result_expr = result_expr.subs({
+        distance_of_convergence_of_two_particles: distance_of_convergence_of_two_particles_,
+    })
+    return Quantity(result_expr)
---
+++ test/chemistry/cross_section_of_interaction_in_model_of_quasirigid_spheres_test.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (assert_equal, units, Quantity, errors)
+from symplyphysics.laws.chemistry import cross_section_of_interaction_in_model_of_quasirigid_spheres as cross_section_law
+
+# Description
+## The distance of the closest approach of two particles is 1.63e-10 meter.
+## Then the cross-sectional area of the interaction is 8.35e-20 meter^2.
+
+Args = namedtuple("Args", ["distance_of_convergence_of_two_particles"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    distance_of_convergence_of_two_particles = Quantity(1.63e-10 * units.meter)
+
+    return Args(distance_of_convergence_of_two_particles=distance_of_convergence_of_two_particles)
+
+
+def test_basic_cross_sectional_area_of_interaction(test_args: Args) -> None:
+    result = cross_section_law.calculate_cross_sectional_area_of_interaction(test_args.distance_of_convergence_of_two_particles)
+    assert_equal(result, 8.35e-20 * units.meter**2)
+
+
+def test_bad_distance_of_convergence_of_two_particles() -> None:
+    distance_of_convergence_of_two_particles = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        cross_section_law.calculate_cross_sectional_area_of_interaction(distance_of_convergence_of_two_particles)
+    with raises(TypeError):
+        cross_section_law.calculate_cross_sectional_area_of_interaction(100)
