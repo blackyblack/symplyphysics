918eef80016acdc65aea4b47160b023047cef37d
Ростислав Житков <rosjit1@gmail.com>
2024-05-13 20:04:51 +0300

    change description

--- symplyphysics/laws/chemistry/boundary_of_thermalization_zone_of_atomized_atoms_in_magnetron.py
+++ symplyphysics/laws/chemistry/boundary_of_thermalization_zone_of_atomized_atoms_in_magnetron.py
@@ -3,18 +3,18 @@ from symplyphysics import (units, Quantity, Symbol, print_expression, validate_i
     validate_output, dimensionless,)
 
 # Description
-## The atoms of the target material evaporate and move towards the substrate inside the magnetron. There is a distance at which the energy of the atomized
+## The atoms of the target material evaporate and move towards the substrate inside the magnetron. There is a distance at which the energy of the traveling
 ## atom will decrease to the energy of the thermal motion of the atoms of the gas-discharge plasma. This distance is called the boundary of the thermalization zone.
-## The decrease in energy occurs due to the collision of the atomized atom with the gas atoms in the magnetron. The free path length is the distance that the sprayed
+## The decrease in energy occurs due to the collision of the traveling atom with the gas atoms in the magnetron. The free path length is the distance that the sprayed
 ## gas travels between two collisions.
-## The atomized atom moves towards the substrate in the magnetron. At the same time, it collides with gas atoms.
-## The number of collisions of a atomized atom is the number of collisions, after which its energy will be equal to the energy of thermal motion
+## The traveling atom moves towards the substrate in the magnetron. At the same time, it collides with gas atoms.
+## The number of collisions of a traveling atom is the number of collisions, after which its energy will be equal to the energy of thermal motion
 ## in a gas-discharge plasma.
 
 ## Law is: L = N * l, where
-## L - boundary of thermalization zone of atomized atoms in magnetron,
-## N - the number of collisions of atomized atom with gas atoms (this is a static quantity, it may not be an integer),
-## l - free path length of atomized atom.
+## L - boundary of thermalization zone of traveling atoms in magnetron,
+## N - the number of collisions of traveling atom with gas atoms (this is a statistical quantity, it can be integer, but it can be fractional as well),
+## l - free path length of traveling atom.
 
 boundary_of_thermalization_zone = Symbol("boundary_of_thermalization_zone", units.length)
 
