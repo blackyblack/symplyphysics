252342b440bdbe1c5472e8ae1480d0b11c97e413
Ростислав Житков <rosjit1@gmail.com>
2024-05-13 23:06:19 +0300

    Commit #1. cross_section_of_interaction_in_recharge_model

---
+++ symplyphysics/laws/chemistry/cross_section_of_interaction_in_recharge_model.py
@@ -0,0 +1,67 @@
+from sympy import Eq, solve, pi, log, sqrt
+from sympy.physics.units import elementary_charge, boltzmann_constant
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    validate_input,
+    validate_output,
+    clone_symbol,
+    symbols
+)
+
+# Description
+## The effective cross section is a physical quantity characterizing the probability of transition of a system of
+## two interacting particles to a certain final state, a quantitative characteristic of the acts of collision of
+## particles of a stream hitting a target with target particles. The effective cross-section has the dimension of the area.
+
+## Law is: g = pi * a0^2 * (Uh / Ui) * (ln(sqrt(3 * k * T / m) * (g * P * m / (2 * k * T * q * E)) * sqrt(Ui / Uh)))^2, where
+## g - the cross-sectional area of the interaction of particles,
+## Ui - the ionization energy of atoms,
+## T - temperature,
+## m - mass of atom,
+## P - pressure
+## E - electric intensity,
+## Uh - hydrogen ionization energy,
+## a0 - bohr radius,
+## k - boltzmann constant,
+## q - elementary charge.
+
+cross_sectional_area_of_interaction = Symbol("cross_sectional_area_of_interaction", units.area)
+
+ionization_energy = Symbol("ionization_energy", units.energy)
+mass_of_atom = clone_symbol(symbols.basic.mass, "mass_of_atom")
+pressure = Symbol("pressure", units.pressure)
+temperature = clone_symbol(symbols.thermodynamics.temperature, "temperature")
+electric_intensity = Symbol("electric_intensity", units.voltage / units.length)
+
+bohr_radius = Quantity(0.529e-10 * units.meter)
+hydrogen_ionization_energy = Quantity(13.6 * units.electronvolt)
+
+expression_1 = sqrt(ionization_energy / hydrogen_ionization_energy)
+expression_2 = sqrt(3 * boltzmann_constant * temperature / mass_of_atom)
+expression_3 = cross_sectional_area_of_interaction * pressure * mass_of_atom
+expression_4 = 2 * boltzmann_constant * temperature * elementary_charge * electric_intensity
+expression_5 = hydrogen_ionization_energy / ionization_energy
+
+law = Eq(cross_sectional_area_of_interaction, pi * bohr_radius**2 * expression_5 * (log(expression_2 * (expression_3 / expression_4) * expression_1))**2)
+
+
+@validate_input(ionization_energy_=ionization_energy,
+    mass_of_atom_=mass_of_atom,
+    pressure_=pressure,
+    temperature_=temperature,
+    electric_intensity_=electric_intensity)
+@validate_output(cross_sectional_area_of_interaction)
+def calculate_cross_sectional_area_of_interaction(ionization_energy_: Quantity,
+    mass_of_atom_: Quantity, pressure_: Quantity,
+    temperature_: Quantity, electric_intensity_: Quantity) -> Quantity:
+    result_expr = solve(law, cross_sectional_area_of_interaction, dict=True)[0][cross_sectional_area_of_interaction]
+    result_expr = result_expr.subs({
+        ionization_energy: ionization_energy_,
+        mass_of_atom: mass_of_atom_,
+        pressure: pressure_,
+        temperature: temperature_,
+        electric_intensity: electric_intensity_,
+    })
+    return Quantity(result_expr)
