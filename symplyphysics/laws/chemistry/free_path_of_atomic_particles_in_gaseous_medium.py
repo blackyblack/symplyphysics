f8dbd903e6897ca11bd50876ffab5094318390af
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-05-14 20:05:49 +0300

    free_path_of_atomic_particles_in_gaseous_medium (#766)
    * Commit #1. free_path_of_atomic_particles_in_gaseous_medium
    
    * add changes
    
    * change temperature

---
+++ symplyphysics/laws/chemistry/free_path_of_atomic_particles_in_gaseous_medium.py
@@ -0,0 +1,37 @@
+from sympy import Eq, solve
+from sympy.physics.units import boltzmann_constant
+from symplyphysics import (units, Quantity, Symbol, validate_input, validate_output, clone_symbol, symbols)
+
+# Description
+## The atoms of the target material evaporate and move towards the substrate inside the magnetron. At the same time,
+## it collides with gas atoms. The free path length is the distance that a traveling atom travels between two collisions.
+
+## Law is: l = k * T / (P * g), where
+## l - free path length of traveling atom,
+## k - boltzmann constant,
+## T - temperature,
+## P - pressure,
+## g - the cross section of the interaction of a traveling atom and a gas atom.
+
+free_path_length = Symbol("free_path_length", units.length)
+
+pressure = Symbol("pressure", units.pressure)
+temperature = symbols.thermodynamics.temperature
+cross_sectional_area_of_interaction = Symbol("cross_sectional_area_of_interaction", units.area)
+
+law = Eq(free_path_length, boltzmann_constant * temperature / (pressure * cross_sectional_area_of_interaction))
+
+
+@validate_input(pressure_=pressure,
+    temperature_=temperature,
+    cross_sectional_area_of_interaction_=cross_sectional_area_of_interaction)
+@validate_output(free_path_length)
+def calculate_free_path_length(pressure_: Quantity, temperature_: Quantity,
+                                            cross_sectional_area_of_interaction_: Quantity) -> Quantity:
+    result_expr = solve(law, free_path_length, dict=True)[0][free_path_length]
+    result_expr = result_expr.subs({
+        pressure: pressure_,
+        temperature: temperature_,
+        cross_sectional_area_of_interaction: cross_sectional_area_of_interaction_,
+    })
+    return Quantity(result_expr)
---
+++ test/chemistry/free_path_of_atomic_particles_in_gaseous_medium_test.py
@@ -0,0 +1,50 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import (assert_equal, units, Quantity, errors)
+from symplyphysics.laws.chemistry import free_path_of_atomic_particles_in_gaseous_medium as path_law
+
+# Description
+## The pressure is 1.43 pascal. The temperature is 573 kelvin. The cross-sectional area of the interaction is 2.41e-19 meter^2.
+## Then the free path length is 22.96 millimeter.
+
+Args = namedtuple("Args", ["pressure", "temperature", "cross_sectional_area_of_interaction"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    pressure = Quantity(1.43 * units.pascal)
+    temperature = Quantity(573 * units.kelvin)
+    cross_sectional_area_of_interaction = Quantity(2.41e-19 * (units.meter**2))
+
+    return Args(pressure=pressure,
+        temperature=temperature,
+        cross_sectional_area_of_interaction=cross_sectional_area_of_interaction)
+
+
+def test_basic_free_path_length(test_args: Args) -> None:
+    result = path_law.calculate_free_path_length(test_args.pressure, test_args.temperature, test_args.cross_sectional_area_of_interaction)
+    assert_equal(result, 22.96 * units.millimeter)
+
+
+def test_bad_pressure(test_args: Args) -> None:
+    pressure = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        path_law.calculate_free_path_length(pressure, test_args.temperature, test_args.cross_sectional_area_of_interaction)
+    with raises(TypeError):
+        path_law.calculate_free_path_length(100, test_args.temperature, test_args.cross_sectional_area_of_interaction)
+
+
+def test_bad_temperature(test_args: Args) -> None:
+    temperature = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        path_law.calculate_free_path_length(test_args.pressure, temperature, test_args.cross_sectional_area_of_interaction)
+    with raises(TypeError):
+        path_law.calculate_free_path_length(test_args.pressure, 100, test_args.cross_sectional_area_of_interaction)
+
+
+def test_bad_cross_sectional_area_of_interaction(test_args: Args) -> None:
+    cross_sectional_area_of_interaction = Quantity(1 * units.coulomb)
+    with raises(errors.UnitsError):
+        path_law.calculate_free_path_length(test_args.pressure, test_args.temperature, cross_sectional_area_of_interaction)
+    with raises(TypeError):
+        path_law.calculate_free_path_length(test_args.pressure, test_args.temperature, 100)
