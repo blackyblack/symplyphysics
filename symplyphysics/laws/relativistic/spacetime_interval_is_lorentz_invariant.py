f20f53c680fb68f85a67298ce65439bb2f5f4010
Aleksandr Slatov <alexslatov@gmail.com>
2024-05-15 19:02:14 +0400

    Create outline of law and test

---
+++ symplyphysics/laws/relativistic/spacetime_interval_is_lorentz_invariant.py
@@ -0,0 +1,29 @@
+from sympy import Eq
+from symplyphysics import (
+    Quantity,
+    Symbol,
+    units,
+    validate_input,
+    validate_output,
+)
+
+# Description
+## TODO
+
+# Law: (Δs)_2 = (Δs)_1
+## Δs - spacetime interval
+## Subscripts 2, 1 designate two inertial reference frames connected by a Lorentz transformation
+
+first_spacetime_interval = Symbol("first_spacetime_interval", units.length)
+second_spacetime_interval = Symbol("second_spacetime_interval", units.length)
+
+law = Eq(second_spacetime_interval, first_spacetime_interval)
+
+# TODO: derive from the definition of spacetime interval and Lorentz transformations
+
+
+@validate_input(first_spacetime_interval_=first_spacetime_interval)
+@validate_output(second_spacetime_interval)
+def calculate_second_spacetime_interval(first_spacetime_interval_: Quantity) -> Quantity:
+    result = law.rhs.subs({first_spacetime_interval: first_spacetime_interval_})
+    return Quantity(result)
