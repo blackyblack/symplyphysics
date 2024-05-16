1232ee9d642b809ad88f08b59d3b392b8bf30d79
Ростислав Житков <rosjit1@gmail.com>
2024-05-16 19:00:17 +0300

    add changes

--- symplyphysics/laws/chemistry/etching_rate_of_target_in_magnetron.py
+++ symplyphysics/laws/chemistry/etching_rate_of_target_in_magnetron.py
@@ -6,11 +6,13 @@ from symplyphysics import (units, Quantity, Symbol, validate_input, validate_out
 # Description
 ## The ions of the gas-discharge plasma in the magnetron fall on the target and knock the atoms out of it.
 ## The sputtering coefficient shows how many target atoms are knocked out of the target by a single ion.
+## The etching rate is how many nanometers of the target substance are etched per unit of time. In other words,
+## this is how much thinner a nanometer becomes a target per unit of time.
 
 ## Law is: V =  J * M * Y / (q * po * Na), where
 ## V - target etching rate,
 ## J - density of the current of the ion flux incident on the target,
-## M - mass of the target atom,
+## M - molar mass of the target atom,
 ## Y - sputtering coefficient,
 ## q - elementary charge,
 ## po - target density,
@@ -19,24 +21,24 @@ from symplyphysics import (units, Quantity, Symbol, validate_input, validate_out
 etching_rate = Symbol("etching_rate", units.velocity)
 
 ion_current_density = Symbol("ion_current_density", units.current / units.area)
-mass_of_target_atom = Symbol("mass_of_target_atom", units.mass / units.amount_of_substance)
+molar_mass_of_target_atom = Symbol("molar_mass_of_target_atom", units.mass / units.amount_of_substance)
 sputtering_coefficient = Symbol("sputtering_coefficient", dimensionless)
 target_density = Symbol("target_density", units.mass / units.volume)
 
-law = Eq(etching_rate, ion_current_density * mass_of_target_atom * sputtering_coefficient / (elementary_charge * target_density * avogadro_constant))
+law = Eq(etching_rate, ion_current_density * molar_mass_of_target_atom * sputtering_coefficient / (elementary_charge * target_density * avogadro_constant))
 
 
 @validate_input(ion_current_density_=ion_current_density,
-                mass_of_target_atom_=mass_of_target_atom,
+                molar_mass_of_target_atom_=molar_mass_of_target_atom,
                 sputtering_coefficient_=sputtering_coefficient,
                 target_density_=target_density)
 @validate_output(etching_rate)
-def calculate_etching_rate(ion_current_density_: Quantity, mass_of_target_atom_: Quantity,
+def calculate_etching_rate(ion_current_density_: Quantity, molar_mass_of_target_atom_: Quantity,
                                                   sputtering_coefficient_: float, target_density_: Quantity) -> Quantity:
     result_expr = solve(law, etching_rate, dict=True)[0][etching_rate]
     result_expr = result_expr.subs({
         ion_current_density: ion_current_density_,
-        mass_of_target_atom: mass_of_target_atom_,
+        molar_mass_of_target_atom: molar_mass_of_target_atom_,
         sputtering_coefficient: sputtering_coefficient_,
         target_density: target_density_,
     })
--- test/chemistry/etching_rate_of_target_in_magnetron_test.py
+++ test/chemistry/etching_rate_of_target_in_magnetron_test.py
@@ -4,43 +4,43 @@ from symplyphysics import (assert_equal, units, Quantity, errors)
 from symplyphysics.laws.chemistry import etching_rate_of_target_in_magnetron as rate_law
 
 # Description
-## The current density of ions incident on the target is 97.566 [ampere / meter^2]. The mass of the target atom
-## is 47.867 kilogram per mole. The atomization coefficient of the target atoms is 1.873. The target density is
+## The current density of ions incident on the target is 97.566 [ampere / meter^2]. The molar mass of the target atom
+## is 47.867 gram per mole. The atomization coefficient of the target atoms is 1.873. The target density is
 ## 6020 [kilogram / meter^3]. Then the etching rate of the target is 15.06 nanometer per second.
 
-Args = namedtuple("Args", ["ion_current_density", "mass_of_target_atom", "sputtering_coefficient", "target_density"])
+Args = namedtuple("Args", ["ion_current_density", "molar_mass_of_target_atom", "sputtering_coefficient", "target_density"])
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
     ion_current_density = Quantity(97.566 * units.ampere / units.meter**2)
-    mass_of_target_atom = Quantity(47.867 * units.gram / units.mol)
+    molar_mass_of_target_atom = Quantity(47.867 * units.gram / units.mol)
     sputtering_coefficient = 1.873
     target_density = Quantity(6020 * units.kilogram / units.meter**3)
 
     return Args(ion_current_density=ion_current_density,
-                mass_of_target_atom=mass_of_target_atom,
+                molar_mass_of_target_atom=molar_mass_of_target_atom,
                 sputtering_coefficient=sputtering_coefficient,
                 target_density=target_density)
 
 
 def test_basic_etching_rate(test_args: Args) -> None:
-    result = rate_law.calculate_etching_rate(test_args.ion_current_density, test_args.mass_of_target_atom, test_args.sputtering_coefficient, test_args.target_density)
+    result = rate_law.calculate_etching_rate(test_args.ion_current_density, test_args.molar_mass_of_target_atom, test_args.sputtering_coefficient, test_args.target_density)
     assert_equal(result, 15.06 * (units.nanometer / units.second))
 
 
 def test_bad_ion_current_density(test_args: Args) -> None:
     ion_current_density = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        rate_law.calculate_etching_rate(ion_current_density, test_args.mass_of_target_atom, test_args.sputtering_coefficient, test_args.target_density)
+        rate_law.calculate_etching_rate(ion_current_density, test_args.molar_mass_of_target_atom, test_args.sputtering_coefficient, test_args.target_density)
     with raises(TypeError):
-        rate_law.calculate_etching_rate(100, test_args.mass_of_target_atom, test_args.sputtering_coefficient, test_args.target_density)
+        rate_law.calculate_etching_rate(100, test_args.molar_mass_of_target_atom, test_args.sputtering_coefficient, test_args.target_density)
 
 
-def test_bad_mass_of_target_atom(test_args: Args) -> None:
-    mass_of_target_atom = Quantity(1 * units.coulomb)
+def test_bad_molar_mass_of_target_atom(test_args: Args) -> None:
+    molar_mass_of_target_atom = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        rate_law.calculate_etching_rate(test_args.ion_current_density, mass_of_target_atom, test_args.sputtering_coefficient, test_args.target_density)
+        rate_law.calculate_etching_rate(test_args.ion_current_density, molar_mass_of_target_atom, test_args.sputtering_coefficient, test_args.target_density)
     with raises(TypeError):
         rate_law.calculate_etching_rate(test_args.ion_current_density, 100, test_args.sputtering_coefficient, test_args.target_density)
 
@@ -48,12 +48,12 @@ def test_bad_mass_of_target_atom(test_args: Args) -> None:
 def test_bad_sputtering_coefficient(test_args: Args) -> None:
     sputtering_coefficient = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        rate_law.calculate_etching_rate(test_args.ion_current_density, test_args.mass_of_target_atom, sputtering_coefficient, test_args.target_density)
+        rate_law.calculate_etching_rate(test_args.ion_current_density, test_args.molar_mass_of_target_atom, sputtering_coefficient, test_args.target_density)
 
 
 def test_bad_target_density(test_args: Args) -> None:
     target_density = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        rate_law.calculate_etching_rate(test_args.ion_current_density, test_args.mass_of_target_atom, test_args.sputtering_coefficient, target_density)
+        rate_law.calculate_etching_rate(test_args.ion_current_density, test_args.molar_mass_of_target_atom, test_args.sputtering_coefficient, target_density)
     with raises(TypeError):
-        rate_law.calculate_etching_rate(test_args.ion_current_density, test_args.mass_of_target_atom, test_args.sputtering_coefficient, 100,)
+        rate_law.calculate_etching_rate(test_args.ion_current_density, test_args.molar_mass_of_target_atom, test_args.sputtering_coefficient, 100,)
