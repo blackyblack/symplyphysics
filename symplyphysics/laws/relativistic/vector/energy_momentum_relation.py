bb7f76a3151201a635bb8eb29f57c38786e5c5dd
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2024-05-24 13:23:00 +0400

    Law: energy—momentum relation in SR (#801)
    * Fix type in approximation function
    
    * Create law and test

---
+++ symplyphysics/laws/relativistic/vector/energy_momentum_relation.py
@@ -0,0 +1,54 @@
+from sympy import Expr
+from sympy.physics.units import speed_of_light
+from symplyphysics import (
+    units,
+    Symbol,
+    Quantity,
+    validate_input,
+    validate_output,
+    Vector,
+    scale_vector,
+    QuantityVector,
+    vector_magnitude,
+)
+
+# Description
+## Relativistic momentum and total relativistic energy are related by a simple equation.
+
+# Law: p * c**2 = E * v
+## p - vector of relativistic momentum
+## c - speed of light
+## E - total energy of relativistic particle
+## v - vector of particle's velocity
+
+total_energy = Symbol("total_energy", units.energy)
+
+
+def momentum_law(velocity_: Vector) -> Vector:
+    return scale_vector(total_energy / speed_of_light**2, velocity_)
+
+
+def velocity_law(momentum_: Vector) -> Vector:
+    return scale_vector(speed_of_light**2 / total_energy, momentum_)
+
+
+def total_energy_law(momentum_: Vector, velocity_: Vector) -> Expr:
+    p = vector_magnitude(momentum_)
+    v = vector_magnitude(velocity_)
+    return p * speed_of_light**2 / v
+
+
+@validate_input(
+    total_energy_=total_energy,
+    velocity_=units.velocity,
+)
+@validate_output(units.momentum)
+def calculate_momentum(
+    total_energy_: Quantity,
+    velocity_: QuantityVector
+) -> QuantityVector:
+    result_vector = momentum_law(velocity_.to_base_vector())
+    return QuantityVector.from_base_vector(
+        result_vector,
+        subs={total_energy: total_energy_},
+    )
