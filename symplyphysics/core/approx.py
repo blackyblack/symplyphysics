a70bb59d52a215c469beb296c7a2f5a4ccf27209
Aleksandr Slatov <alexslatov@gmail.com>
2024-05-24 11:19:16 +0400

    Fix type in approximation function

--- symplyphysics/core/approx.py
+++ symplyphysics/core/approx.py
@@ -31,8 +31,8 @@ def approx_equal_quantities(lhs: Quantity,
 
 
 # Combined with assert for better test output
-def assert_equal(lhs: Quantity | float,
-    rhs: Expr | Quantity | float,
+def assert_equal(lhs: Expr | float,
+    rhs: Expr | float,
     *,
     tolerance: float = APPROX_RELATIVE_TOLERANCE,
     dimension: Optional[Dimension] = None) -> None:
