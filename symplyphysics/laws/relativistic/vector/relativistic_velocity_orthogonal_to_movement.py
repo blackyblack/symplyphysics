0e1aa7f1cbadd3e8ef17075c567e0dc05a0e45a1
Aleksandr Slatov <alexslatov@gmail.com>
2024-05-26 14:02:44 +0400

    Create law and test

---
+++ symplyphysics/laws/relativistic/vector/relativistic_velocity_orthogonal_to_movement.py
@@ -0,0 +1,73 @@
+from sympy import sqrt
+from sympy.physics.units import speed_of_light
+from symplyphysics import (
+    units,
+    validate_input,
+    validate_output,
+    Vector,
+    dot_vectors,
+    scale_vector,
+    QuantityVector,
+)
+from symplyphysics.core.vectors.arithmetics import reject_cartesian_vector
+
+# Description
+## Consider two inertial reference frames: one fixed (lab frame) and one tied to the moving object (proper frame).
+## The proper frame is moving with some velocity `v` relative to the lab frame. Then, according to the theory of special
+## relativity, the velocity of the object relative to lab frame is not equal to the sum of its velocity in the proper
+## frame and the velocity of the proper frame relative to the lab frame.
+
+# Law: u_orthogonal = u_orthogonal' / (gamma * (1 - dot(v, u_parallel') / c**2))
+## u_orthogonal - velocity vector relative to lab frame orthogonal to `v`
+## u_orthogonal' - velocity vector relative to proper frame orthogonal to `v`
+## u_parallel' - velocity vector relative to proper frame parallel to `v`
+## v - velocity vector of proper frame relative to lab frame
+## gamma = 1 / sqrt(1 - dot(v, v) / c**2) - Lorentz factor
+## dot(a, b) - dot product between vectors `a` and `b`
+
+# Notes
+## - One can get the same expression for `u_orthogonal'` in terms of `u_orthogonal` by replacing `v` with `-v`. This is
+##   essentially the inverse Lorentz transformation from lab frame to proper frame that uses the fact that the lab frame
+##   can be viewed as moving with velocity vector `-v` relative to the proper frame.
+
+# Conditions
+## - Works in special relativity
+
+
+def orthogonal_velocity_component_in_lab_frame_law(
+    velocity_in_proper_frame_: Vector,
+    proper_frame_velocity_: Vector,
+) -> Vector:
+    orthogonal_velocity_component_in_proper_frame_ = reject_cartesian_vector(velocity_in_proper_frame_, proper_frame_velocity_)
+
+    lorentz_factor_ = 1 / sqrt(1 - dot_vectors(proper_frame_velocity_, proper_frame_velocity_) / speed_of_light**2)
+
+    scale_factor_ = lorentz_factor_ * (1 + dot_vectors(proper_frame_velocity_, velocity_in_proper_frame_) / speed_of_light**2)
+
+    return scale_vector(1 / scale_factor_, orthogonal_velocity_component_in_proper_frame_)
+
+
+def orthogonal_velocity_component_in_proper_frame_law(
+    velocity_in_lab_frame_: Vector,
+    lab_frame_velocity_: Vector,
+) -> Vector:
+    return orthogonal_velocity_component_in_lab_frame_law(
+        velocity_in_lab_frame_,
+        scale_vector(-1, lab_frame_velocity_),
+    )
+
+
+@validate_input(
+    velocity_in_proper_frame_=units.velocity,
+    proper_frame_velocity_=units.velocity,
+)
+@validate_output(units.velocity)
+def calculate_orthogonal_velocity_component_in_lab_frame(
+    velocity_in_proper_frame_: QuantityVector,
+    proper_frame_velocity_: QuantityVector,
+) -> QuantityVector:
+    result = orthogonal_velocity_component_in_lab_frame_law(
+        velocity_in_proper_frame_.to_base_vector(),
+        proper_frame_velocity_.to_base_vector(),
+    )
+    return QuantityVector.from_base_vector(result)
---
+++ test/relativistic/vector/relativistic_velocity_orthogonal_to_movement_test.py
@@ -0,0 +1,83 @@
+from collections import namedtuple
+from pytest import fixture, raises, mark
+from sympy.physics.units import speed_of_light
+from symplyphysics import (
+    assert_equal,
+    units,
+    Quantity,
+    QuantityVector,
+    errors,
+)
+from symplyphysics.core.vectors.arithmetics import reject_cartesian_vector
+from symplyphysics.laws.relativistic.vector import relativistic_velocity_orthogonal_to_movement as law
+
+# Description
+## The velocity vector of the body in the proper frame of reference is (0.1*c, 0.2*c, -0.333*c). The velocity vector of the
+## proper frame of reference relative to the laboratory one is (-0.2*c, -0.4*c, 0). The velocity of the body in the lab frame
+## of reference is (-0.111*c, -0.222*c, -0.331*c).
+
+Args = namedtuple("Args", "up ul v")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    up = QuantityVector([Quantity(0.100 * speed_of_light), Quantity(0.200 * speed_of_light), Quantity(-0.333 * speed_of_light)])
+    ul = QuantityVector([Quantity(-0.111 * speed_of_light), Quantity(-0.222 * speed_of_light), Quantity(-0.331 * speed_of_light)])
+    v = QuantityVector([Quantity(-0.200 * speed_of_light), Quantity(-0.400 * speed_of_light), Quantity(0)])
+    return Args(up=up, ul=ul, v=v)
+
+
+def test_lab_law(test_args: Args) -> None:
+    result = law.calculate_orthogonal_velocity_component_in_lab_frame(test_args.up, test_args.v)
+    ul_orthogonal = reject_cartesian_vector(
+        test_args.ul.to_base_vector(),
+        test_args.v.to_base_vector()
+    )
+    for result_component, correct_component in zip(
+        result.components,
+        [0, 0, ul_orthogonal.components[2]],
+        strict=True
+    ):
+        assert_equal(result_component, correct_component)
+
+
+# TODO: fix `assert_equal` behavior with zeros
+@mark.skip("Fix assert_equal with zeroes")
+def test_proper_law(test_args: Args) -> None:
+    result = law.orthogonal_velocity_component_in_proper_frame_law(
+        test_args.ul.to_base_vector(),
+        test_args.v.to_base_vector(),
+    )
+    up_orthogonal = reject_cartesian_vector(
+        test_args.up.to_base_vector(),
+        test_args.v.to_base_vector(),
+    )
+    for result_component, correct_component in zip(
+        result.components,
+        [0, 0, up_orthogonal.components[2]],
+        strict=True,
+    ):
+        assert_equal(result_component, correct_component)
+
+
+def test_bad_velocity(test_args: Args) -> None:
+    vb_vector = QuantityVector([Quantity(1 * units.coulomb)])
+    with raises(errors.UnitsError):
+        law.calculate_orthogonal_velocity_component_in_lab_frame(vb_vector, test_args.v)
+    with raises(errors.UnitsError):
+        law.calculate_orthogonal_velocity_component_in_lab_frame(test_args.up, vb_vector)
+    
+    vb_scalar = Quantity(units.speed_of_light)
+    with raises(AttributeError):
+        law.calculate_orthogonal_velocity_component_in_lab_frame(vb_scalar, test_args.v)
+    with raises(AttributeError):
+        law.calculate_orthogonal_velocity_component_in_lab_frame(test_args.up, vb_scalar)
+    
+    with raises(TypeError):
+        law.calculate_orthogonal_velocity_component_in_lab_frame(100, test_args.v)
+    with raises(TypeError):
+        law.calculate_orthogonal_velocity_component_in_lab_frame([100], test_args.v)
+    with raises(TypeError):
+        law.calculate_orthogonal_velocity_component_in_lab_frame(test_args.up, 100)
+    with raises(TypeError):
+        law.calculate_orthogonal_velocity_component_in_lab_frame(test_args.up, [100])
