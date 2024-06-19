ad7128efa64bfad61f82730151592392f3084076
Aleksandr Slatov <alexslatov@gmail.com>
2024-06-19 15:45:41 +0400

    Add test case

--- test/kinematic/vector/velocity_relative_to_reference_frame_test.py
+++ test/kinematic/vector/velocity_relative_to_reference_frame_test.py
@@ -54,6 +54,34 @@ def test_position_law(test_args: Args) -> None:
     assert_equal_vectors(initial_position_, test_args.r1)
 
 
+def test_law(test_args: Args) -> None:
+    time = symbols("time")
+
+    velocity_ = law.calculate_relative_velocity(test_args.r1, test_args.r2, test_args.dt)
+
+    final_position_vector = law.relative_position_law(
+        test_args.r1.to_base_vector(),
+        velocity_.to_base_vector(),
+        time,
+    )
+    final_position_ = QuantityVector.from_base_vector(
+        final_position_vector,
+        subs={time: test_args.dt},
+    )
+    assert_equal_vectors(final_position_, test_args.r2)
+
+    initial_position_vector = law.relative_position_law(
+        test_args.r2.to_base_vector(),
+        scale_vector(-1, velocity_.to_base_vector()),
+        time,
+    )
+    initial_position_ = QuantityVector.from_base_vector(
+        initial_position_vector,
+        subs={time: test_args.dt},
+    )
+    assert_equal_vectors(initial_position_, test_args.r1)
+
+
 def test_bad_position(test_args: Args) -> None:
     rb_vector = QuantityVector([Quantity(1 * units.coulomb)])
     with raises(errors.UnitsError):
