fc0aff06640479bbd1ec5dcc2cc4c7250ab1be02
Aleksandr Slatov <alexslatov@gmail.com>
2024-06-21 18:39:16 +0400

    Create outline of law

---
+++ symplyphysics/laws/gravity/vector/acceleration_due_to_gravity.py
@@ -0,0 +1,20 @@
+from symplyphysics import (
+    units,
+    angle_type,
+    Symbol,
+    Quantity,
+    Vector,
+    QuantityVector,
+    validate_input,
+    validate_output,
+    scale_vector,
+    vector_magnitude,
+)
+
+# Description
+## TODO
+
+# Law: g = F_earth / m - cross(w, cross(w, r))
+## g - vector of acceleration due to gravity
+## F_earth - vector of the force of gravity to Earth
+## w - pseudovector of 
