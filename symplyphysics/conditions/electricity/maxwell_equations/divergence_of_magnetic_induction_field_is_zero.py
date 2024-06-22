3ec13dba2ee44433c4d0fe82aac55f8d00814484
Ростислав Житков <rosjit1@gmail.com>
2024-06-22 18:00:52 +0300

    add test

--- symplyphysics/conditions/electricity/maxwell_equations/divergence_of_magnetic_induction_field_is_zero.py
+++ symplyphysics/conditions/electricity/maxwell_equations/divergence_of_magnetic_induction_field_is_zero.py
@@ -1,20 +1,17 @@
 from sympy import Eq
 from sympy.core import Equality
-from symplyphysics import (units, Symbol)
 from symplyphysics.core.fields.operators import divergence_operator
 from symplyphysics.core.fields.vector_field import VectorField
 
 ## Description
-## The divergence of magnetic induction at some point is equal to zero. A simpler formulation of the law
+## The divergence of magnetic induction is equal to zero. A simpler formulation of the law
 ## is that magnetic charges not exist.
 
 ## Law is: div(B) = 0, where
 ## B - magnetic induction (vector field),
 ## div - divergence (the sum of partial derivatives in coordinates).
 
-time = Symbol('time', units.time)
 
-def gauss_law_for_magnetic_field(magnetic_induction_: VectorField) -> Equality:
+def magnetic_field_divergence_condition(magnetic_induction_: VectorField) -> Equality:
     divergence_magnetic_induction = divergence_operator(magnetic_induction_)
-    law = Eq(divergence_magnetic_induction, 0)
-    return law
+    return Eq(divergence_magnetic_induction, 0)
---
+++ test/conditions/electricity/maxwell_equations/divergence_of_magnetic_induction_field_is_zero_test.py
@@ -0,0 +1,29 @@
+from collections import namedtuple
+from pytest import fixture
+from symplyphysics import (
+    units,
+    Quantity,
+    assert_equal,
+    prefixes
+)
+from symplyphysics.core.fields.vector_field import VectorField
+from symplyphysics.conditions.electricity.maxwell_equations import divergence_of_magnetic_induction_field_is_zero as divergence_cond
+
+## The magnetic induction vector field is given. For its distribution, the magnetic amplitude is known, which is equal to 1 kilotesla.
+## Then the divergence of magnetic induction is equal to zero.
+
+Args = namedtuple("Args", ["magnetic_induction"])
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    magnetic_amplitude = Quantity(1 * prefixes.kilo * units.tesla)
+    magnetic_induction = VectorField(lambda point: [(point.x / Quantity(1 * units.meter)) * magnetic_amplitude,
+                                                      (-point.y / Quantity(1 * units.meter)) * magnetic_amplitude,
+                                                      magnetic_amplitude])
+    return Args(magnetic_induction=magnetic_induction)
+
+
+def test_basic_magnetic_field_divergence_condition(test_args: Args) -> None:
+    result = divergence_cond.magnetic_field_divergence_condition(test_args.magnetic_induction)
+    assert_equal(result.args[0], 0 * units.tesla / units.meter)
