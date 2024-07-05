cf617a5b22cff7d09d192af82f592ebd1ea21101
Aleksandr Slatov <alexslatov@gmail.com>
2024-07-05 04:19:13 +0400

    Fix implementation and tests

--- symplyphysics/laws/gravity/vector/acceleration_due_to_gravity_via_gravity_force_and_centripetal_acceleration.py
+++ symplyphysics/laws/gravity/vector/acceleration_due_to_gravity_via_gravity_force_and_centripetal_acceleration.py
@@ -2,7 +2,6 @@ from sympy import Expr
 from symplyphysics import (
     symbols,
     units,
-    angle_type,
     Quantity,
     Vector,
     QuantityVector,
@@ -11,7 +10,6 @@ from symplyphysics import (
     scale_vector,
     add_cartesian_vectors,
     subtract_cartesian_vectors,
-    cross_cartesian_vectors,
     vector_magnitude,
 )
 
@@ -23,77 +21,64 @@ from symplyphysics import (
 ## but can be different at different points in space.
 
 # Law: g = F_gravity / m - a_centripetal
-## g - vector of acceleration due to gravity
-## F_gravity - vector of the force of gravity pull
-## a_centripetal - vector of centripetal acceleration
-## m - mass of body
+## g - vector of acceleration due to gravity of body B
+## F_gravity - vector of the force of gravity pull acting on body B
+## a_centripetal - vector of centripetal acceleration of body B
+## m - mass of body B
 
 mass = symbols.basic.mass
 
 
 def acceleraton_due_to_gravity_law(
     gravity_force_: Vector,
-    angular_velocity_: Vector,
-    position_: Vector,
+    centripetal_acceleration_: Vector,
 ) -> Vector:
     return subtract_cartesian_vectors(
         scale_vector(1 / mass, gravity_force_),
-        cross_cartesian_vectors(
-            angular_velocity_,
-            cross_cartesian_vectors(angular_velocity_, position_),
-        ),
+        centripetal_acceleration_,
     )
 
 
+# F_gravity = m * (g + a_centripetal)
 def gravity_force_law(
     acceleration_due_to_gravity_: Vector,
-    angular_velocity_: Vector,
-    position_: Vector,
+    centripetal_acceleration_: Vector,
 ) -> Vector:
     return scale_vector(
         mass,
         add_cartesian_vectors(
             acceleration_due_to_gravity_,
-            cross_cartesian_vectors(
-                angular_velocity_,
-                cross_cartesian_vectors(angular_velocity_, position_),
-            ),
+            centripetal_acceleration_,
         ),
     )
 
 
+# m = norm(F_gravity) / norm(g + a_centripetal)
 def mass_law(
     acceleration_due_to_gravity_: Vector,
     gravity_force_: Vector,
-    angular_velocity_: Vector,
-    position_: Vector,
+    centripetal_acceleration_: Vector,
 ) -> Expr:
     acceleration_ = add_cartesian_vectors(
         acceleration_due_to_gravity_,
-        cross_cartesian_vectors(
-            angular_velocity_,
-            cross_cartesian_vectors(angular_velocity_, position_),
-        ),
+        centripetal_acceleration_,
     )
     return vector_magnitude(gravity_force_) / vector_magnitude(acceleration_)
 
 
 @validate_input(
     gravity_force_=units.force,
-    angular_velocity_=angle_type / units.time,
-    position_=units.length,
+    centripetal_acceleration_=units.acceleration,
     mass_=mass,
 )
 @validate_output(units.acceleration)
 def calculate_acceleraton_due_to_gravity(
     gravity_force_: QuantityVector,
-    angular_velocity_: QuantityVector,
-    position_: QuantityVector,
+    centripetal_acceleration_: QuantityVector,
     mass_: Quantity,
 ) -> QuantityVector:
     vector = acceleraton_due_to_gravity_law(
         gravity_force_.to_base_vector(),
-        angular_velocity_.to_base_vector(),
-        position_.to_base_vector(),
+        centripetal_acceleration_.to_base_vector(),
     )
     return QuantityVector.from_base_vector(vector, subs={mass: mass_})
