40d91f3077349219d1d5d95168a3c2571465a9f5
Aleksandr Slatov <alexslatov@gmail.com>
2024-07-06 01:23:19 +0400

    Fix law and test

--- symplyphysics/laws/kinematic/vector/acceleration_of_transfer_between_relative_frames.py
+++ symplyphysics/laws/kinematic/vector/acceleration_of_transfer_between_relative_frames.py
@@ -1,18 +1,12 @@
-from sympy import Expr, symbols
 from symplyphysics import (
     units,
-    angle_type,
     validate_input,
     validate_output,
     Vector,
-    Quantity,
     QuantityVector,
-    scale_vector,
     add_cartesian_vectors,
-    cross_cartesian_vectors,
     subtract_cartesian_vectors,
 )
-from symplyphysics.core.vectors.arithmetics import diff_cartesian_vector
 
 # Description
 ## Suppose two reference frames, one of which is fixed (S) and the other one is moving (S'). The movement of
@@ -22,78 +16,86 @@ from symplyphysics.core.vectors.arithmetics import diff_cartesian_vector
 ## rotation of the moving frame. The transfer acceleration only depends on the movement of frame S' relative to
 ## stationary frame S, so it would be the acceleration in S of a point stationary in S'.
 
-# Law: a_tr = a_0 + cross(w, cross(w, r)) + cross(d(w)/dt, r)
-## a_tr - vector of transfer acceleration of point X relative to fixed frame S
+# Law: a_tr = a_0 + a_centripetal + a_non_uniform_rotation
+## a_tr - vector of transfer acceleration of body B relative to fixed frame S
 ## a_0 - vector of acceleration of moving frame S' relative to fixed frame S
-## w - pseudovector of angular velocity related to rotation of moving frame S' about instantaneous axis
-## r - vector of position of point X relative to moving frame S'
-## d/dt - derivative w.r.t. time
+## a_centripetal - vector of centripetal acceleration of body B in moving frame S'
+## a_non_uniform_rotation - vector of acceleration caused by non-uniform rotation of frame S'
 
 
 def transfer_acceleration_law(
     moving_frame_acceleration_: Vector,
-    angular_velocity_: Vector,
-    position_vector_: Vector,
-    time_: Expr,
+    centripetal_acceleration_: Vector,
+    rotation_acceleration_: Vector,
 ) -> Vector:
-    centripetal_acceleration_ = cross_cartesian_vectors(
-        angular_velocity_,
-        cross_cartesian_vectors(angular_velocity_, position_vector_),
-    )
-    rotation_acceleration = cross_cartesian_vectors(
-        diff_cartesian_vector(angular_velocity_, time_),
-        position_vector_,
-    )
     return add_cartesian_vectors(
         moving_frame_acceleration_,
         add_cartesian_vectors(
             centripetal_acceleration_,
-            rotation_acceleration,
+            rotation_acceleration_,
+        )
+    )
+
+
+# a_0 = a_tr - (a_centripetal + a_non_uniform_rotation)
+def moving_frame_acceleration_law(
+    transfer_acceleration_: Vector,
+    centripetal_acceleration_: Vector,
+    rotation_acceleration_: Vector,
+) -> Vector:
+    return subtract_cartesian_vectors(
+        transfer_acceleration_,
+        add_cartesian_vectors(
+            centripetal_acceleration_,
+            rotation_acceleration_,
+        )
+    )
+
+
+# a_centripetal = a_tr - (a_0 + a_non_uniform_rotation)
+def centripetal_acceleration_law(
+    transfer_acceleration_: Vector,
+    moving_frame_acceleration_: Vector,
+    rotation_acceleration_: Vector,
+) -> Vector:
+    return subtract_cartesian_vectors(
+        transfer_acceleration_,
+        add_cartesian_vectors(
+            moving_frame_acceleration_,
+            rotation_acceleration_,
+        )
+    )
+
+
+# a_non_uniform_rotation = a_tr - (a_0 + a_centripetal)
+def rotation_acceleration_law(
+    transfer_acceleration_: Vector,
+    moving_frame_acceleration_: Vector,
+    centripetal_acceleration_: Vector,
+) -> Vector:
+    return subtract_cartesian_vectors(
+        transfer_acceleration_,
+        add_cartesian_vectors(
+            moving_frame_acceleration_,
+            centripetal_acceleration_,
         )
     )
 
 
 @validate_input(
     moving_frame_acceleration_=units.acceleration,
-    angular_velocity_before_=angle_type / units.time,
-    angular_velocity_after_=angle_type / units.time,
-    position_vector_=units.length,
-    time_before_=units.time,
-    time_after_=units.time,
-    time_=units.time,
+    centripetal_acceleration_=units.acceleration,
+    rotation_acceleration_=units.acceleration,
 )
 @validate_output(units.acceleration)
 def calculate_transfer_acceleration(
     moving_frame_acceleration_: QuantityVector,
-    angular_velocity_before_: QuantityVector,
-    angular_velocity_after_: QuantityVector,
-    position_vector_: QuantityVector,
-    time_before_: Quantity,
-    time_after_: Quantity,
-    time_: Quantity,
+    centripetal_acceleration_: QuantityVector,
+    rotation_acceleration_: QuantityVector,
 ) -> QuantityVector:
-    time = symbols("time")
-
-    # (w - w0) / (t - t0) = (w1 - w0) / (t1 - t0)
-    angular_velocity_ = add_cartesian_vectors(
-        angular_velocity_before_.to_base_vector(),
-        scale_vector(
-            (time - time_before_) / (time_after_ - time_before_),
-            subtract_cartesian_vectors(
-                angular_velocity_after_.to_base_vector(),
-                angular_velocity_before_.to_base_vector(),
-            ),
-        ),
-    )
-
-    result = transfer_acceleration_law(
+    vector = transfer_acceleration_law(
         moving_frame_acceleration_.to_base_vector(),
-        angular_velocity_,
-        position_vector_.to_base_vector(),
-        time,
+        centripetal_acceleration_.to_base_vector(),
+        rotation_acceleration_.to_base_vector(),
     )
-
-    return QuantityVector.from_base_vector(
-        result,
-        subs={time: time_},
-    )
+    return QuantityVector.from_base_vector(vector)
--- test/kinematic/vector/acceleration_of_transfer_between_relative_frames_test.py
+++ test/kinematic/vector/acceleration_of_transfer_between_relative_frames_test.py
@@ -3,98 +3,101 @@ from pytest import fixture, raises
 from symplyphysics import (
     errors,
     units,
-    Quantity,
     QuantityVector,
 )
 from symplyphysics.core.approx import assert_equal_vectors
 from symplyphysics.laws.kinematic.vector import acceleration_of_transfer_between_relative_frames as law
 
-Args = namedtuple("Args", "a w0 w1 r t0 t1 t")
+Args = namedtuple("Args", "a_0 a_centr a_rot a_tr")
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
-    a = QuantityVector([1 * units.meter / units.second**2, 0, 0])
-    w0 = QuantityVector([0, 3 * units.radian / units.second, 0])
-    w1 = QuantityVector([0, 3.1 * units.radian / units.second, -0.1 * units.radian / units.second])
-    r = QuantityVector([3 * units.meter, -1 * units.meter, 2 * units.meter])
-    t0 = Quantity(0)
-    t1 = Quantity(1 * units.second)
-    t = Quantity(0.5 * units.second)
-    return Args(a=a, w0=w0, w1=w1, r=r, t0=t0, t1=t1, t=t)
-
-
-def test_law(test_args: Args) -> None:
-    result = law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, test_args.r, test_args.t0, test_args.t1, test_args.t)
     a_unit = units.meter / units.second**2
-    assert_equal_vectors(result, QuantityVector([-27.0 * a_unit, -0.6 * a_unit, -19.0 * a_unit]), tolerance=2e-2)
+
+    a_0 = QuantityVector([a_unit, 0, 0])
+    a_centr = QuantityVector([0, a_unit, 2 * a_unit])
+    a_rot = QuantityVector([-1 * a_unit, 0, 3 * a_unit])
+    a_tr = QuantityVector([0, a_unit, 5 * a_unit])
+
+    return Args(a_0=a_0, a_centr=a_centr, a_rot=a_rot, a_tr=a_tr)
 
 
-def test_bad_acceleration(test_args: Args) -> None:
-    ab_vector = QuantityVector([1 * units.coulomb])
+def test_transfer_law(test_args: Args) -> None:
+    result = law.calculate_transfer_acceleration(test_args.a_0, test_args.a_centr, test_args.a_rot)
+    assert_equal_vectors(result, test_args.a_tr)
+
+
+def test_moving_frame_law(test_args: Args) -> None:
+    result_vector = law.moving_frame_acceleration_law(
+        test_args.a_tr.to_base_vector(),
+        test_args.a_centr.to_base_vector(),
+        test_args.a_rot.to_base_vector(),
+    )
+    result = QuantityVector.from_base_vector(result_vector)
+    assert_equal_vectors(result, test_args.a_0)
+
+
+def test_centripetal_law(test_args: Args) -> None:
+    result_vector = law.centripetal_acceleration_law(
+        test_args.a_tr.to_base_vector(),
+        test_args.a_0.to_base_vector(),
+        test_args.a_rot.to_base_vector(),
+    )
+    result = QuantityVector.from_base_vector(result_vector)
+    assert_equal_vectors(result, test_args.a_centr)
+
+
+def test_non_uniform_rotation_law(test_args: Args) -> None:
+    result_vector = law.rotation_acceleration_law(
+        test_args.a_tr.to_base_vector(),
+        test_args.a_0.to_base_vector(),
+        test_args.a_centr.to_base_vector(),
+    )
+    result = QuantityVector.from_base_vector(result_vector)
+    assert_equal_vectors(result, test_args.a_rot)
+
+
+def test_bad_moving_frame_acceleration(test_args: Args) -> None:
+    ab_vector = QuantityVector([units.coulomb])
     with raises(errors.UnitsError):
-        law.calculate_transfer_acceleration(ab_vector, test_args.w0, test_args.w1, test_args.r, test_args.t0, test_args.t1, test_args.t)
-    
-    ab_scalar = Quantity(1 * units.meter / units.second**2)
+        law.calculate_transfer_acceleration(ab_vector, test_args.a_centr, test_args.a_rot)
+
+    ab_scalar = units.meter / units.second**2
     with raises(AttributeError):
-        law.calculate_transfer_acceleration(ab_scalar, test_args.w0, test_args.w1, test_args.r, test_args.t0, test_args.t1, test_args.t)
-    
+        law.calculate_transfer_acceleration(ab_scalar, test_args.a_centr, test_args.a_rot)
+
     with raises(TypeError):
-        law.calculate_transfer_acceleration(100, test_args.w0, test_args.w1, test_args.r, test_args.t0, test_args.t1, test_args.t)
+        law.calculate_transfer_acceleration(100, test_args.a_centr, test_args.a_rot)
     with raises(TypeError):
-        law.calculate_transfer_acceleration([100], test_args.w0, test_args.w1, test_args.r, test_args.t0, test_args.t1, test_args.t)
+        law.calculate_transfer_acceleration([100], test_args.a_centr, test_args.a_rot)
 
 
-def test_bad_angular_velocity(test_args: Args) -> None:
-    wb_vector = QuantityVector([1 * units.coulomb])
+def test_bad_centripetal_acceleration(test_args: Args) -> None:
+    ab_vector = QuantityVector([units.coulomb])
     with raises(errors.UnitsError):
-        law.calculate_transfer_acceleration(test_args.a, wb_vector, test_args.w1, test_args.r, test_args.t0, test_args.t1, test_args.t)
-    with raises(errors.UnitsError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, wb_vector, test_args.r, test_args.t0, test_args.t1, test_args.t)
+        law.calculate_transfer_acceleration(test_args.a_0, ab_vector, test_args.a_rot)
 
-    wb_scalar = Quantity(1 * units.radian / units.second)
+    ab_scalar = units.meter / units.second**2
     with raises(AttributeError):
-        law.calculate_transfer_acceleration(test_args.a, wb_scalar, test_args.w1, test_args.r, test_args.t0, test_args.t1, test_args.t)
+        law.calculate_transfer_acceleration(test_args.a_0, ab_scalar, test_args.a_rot)
+
+    with raises(TypeError):
+        law.calculate_transfer_acceleration(test_args.a_0, 100, test_args.a_rot)
+    with raises(TypeError):
+        law.calculate_transfer_acceleration(test_args.a_0, [100], test_args.a_rot)
+
+
+def test_bad_rotation_acceleration(test_args: Args) -> None:
+    ab_vector = QuantityVector([units.coulomb])
+    with raises(errors.UnitsError):
+        law.calculate_transfer_acceleration(test_args.a_0, test_args.a_centr, ab_vector)
+
+    ab_scalar = units.meter / units.second**2
     with raises(AttributeError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, wb_scalar, test_args.r, test_args.t0, test_args.t1, test_args.t)
+        law.calculate_transfer_acceleration(test_args.a_0, test_args.a_centr, ab_scalar)
 
     with raises(TypeError):
-        law.calculate_transfer_acceleration(test_args.a, 100, test_args.w1, test_args.r, test_args.t0, test_args.t1, test_args.t)
+        law.calculate_transfer_acceleration(test_args.a_0, test_args.a_centr, 100)
     with raises(TypeError):
-        law.calculate_transfer_acceleration(test_args.a, [100], test_args.w1, test_args.r, test_args.t0, test_args.t1, test_args.t)
-    with raises(TypeError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, 100, test_args.r, test_args.t0, test_args.t1, test_args.t)
-    with raises(TypeError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, [100], test_args.r, test_args.t0, test_args.t1, test_args.t)
-
-
-def test_bad_position_vector(test_args: Args) -> None:
-    rb_vector = QuantityVector([1 * units.coulomb])
-    with raises(errors.UnitsError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, rb_vector, test_args.t0, test_args.t1, test_args.t)
-
-    rb_scalar = Quantity(1 * units.meter)
-    with raises(AttributeError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, rb_scalar, test_args.t0, test_args.t1, test_args.t)
-
-    with raises(TypeError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, 100, test_args.t0, test_args.t1, test_args.t)
-    with raises(TypeError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, [100], test_args.t0, test_args.t1, test_args.t)
-
-
-def test_bad_time(test_args: Args) -> None:
-    tb = Quantity(1 * units.coulomb)
-    with raises(errors.UnitsError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, test_args.r, tb, test_args.t1, test_args.t)
-    with raises(errors.UnitsError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, test_args.r, test_args.t0, tb, test_args.t)
-    with raises(errors.UnitsError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, test_args.r, test_args.t0, test_args.t1, tb)
-
-    with raises(TypeError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, test_args.r, 100, test_args.t1, test_args.t)
-    with raises(TypeError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, test_args.r, test_args.t0, 100, test_args.t)
-    with raises(TypeError):
-        law.calculate_transfer_acceleration(test_args.a, test_args.w0, test_args.w1, test_args.r, test_args.t0, test_args.t1, 100)
+        law.calculate_transfer_acceleration(test_args.a_0, test_args.a_centr, [100])
