3f8ac7908280cffcb35ab2f235ba9aa8792a1e0c
Aleksandr Slatov <alexslatov@gmail.com>
2024-07-17 01:53:37 +0400

    Create law and test

---
+++ symplyphysics/laws/quantities/__init__.py
@@ -0,0 +1,7 @@
+"""
+Quantities
+==========
+
+Physical quantities can often be categorized as being either *extensive* or *intensive*, according to
+how the property changes when the size or extent of the system changes.
+"""
---
+++ symplyphysics/laws/quantities/quantity_is_linear_density_times_length.py
@@ -0,0 +1,66 @@
+"""
+Extensive quantity is linear density times length
+=================================================
+
+An extensive quantity of interest can be obtained by multiplying the corresponding linear
+density by length.
+"""
+
+from sympy import Eq, Symbol as SymSymbol
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    validate_input,
+)
+from symplyphysics.core.dimensions import assert_equivalent_dimension
+
+extensive_quantity = SymSymbol("extensive_quantity")
+"""
+Extensive quantity
+
+Symbol:
+    X
+"""
+
+linear_density = SymSymbol("linear_density")
+r"""
+Intensive linear density
+
+Symbol:
+    :math:`\lambda_X`
+"""
+
+length = Symbol("length", units.length)
+"""
+Length
+
+Symbol:
+    L
+"""
+
+law = Eq(extensive_quantity, linear_density * length)
+r"""
+:math:`X = \lambda_X L`
+"""
+
+
+@validate_input(length_=length)
+def calculate_extensive_quantity(
+    linear_density_: Quantity,
+    length_: Quantity,
+) -> Quantity:
+    result_expr = law.rhs.subs({
+        linear_density: linear_density_,
+        length: length_,
+    })
+    result = Quantity(result_expr)
+
+    assert_equivalent_dimension(
+        result,
+        "return",
+        "calculate_extensive_quantity",
+        linear_density_.dimension * units.length,
+    )
+
+    return result
