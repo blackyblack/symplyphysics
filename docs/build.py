c4f7dc826c7f6a3440a19129b3311c8058824619
blackyblack <johnmalkovich@mail.ru>
2024-07-20 02:28:32 +0200

    No generated in URL (#864)

--- docs/build.py
+++ docs/build.py
@@ -142,7 +142,8 @@ def process_law_package(directory: str, laws: Sequence[str], packages: Sequence[
     law_members, law_functions = find_members_and_functions(law_parsed)
 
     package_name = filename.replace(os.sep, ".")
-    package_doc_file = os.path.normpath(os.path.join(output_dir, package_name + ".rst"))
+    package_name_strip_root = ".".join(package_name.split(".")[1:])
+    package_doc_file = os.path.normpath(os.path.join(output_dir, package_name_strip_root + ".rst"))
 
     packages = [p for p in packages if not (p.startswith(".") or p.startswith("_"))]
 
@@ -151,7 +152,7 @@ def process_law_package(directory: str, laws: Sequence[str], packages: Sequence[
     if len(packages) > 0 or len(laws) > 0:
         package_content = package_content + "Contents:\n\n" + ".. toctree::\n" + "  :maxdepth: 4\n\n"
         for p in packages:
-            package_content = package_content + "  " + package_name + "." + p + "\n"
+            package_content = package_content + "  " + package_name_strip_root + "." + p + "\n"
         for l in laws:
             package_content = package_content + "  " + l + "\n"
 
@@ -162,7 +163,7 @@ def process_law_package(directory: str, laws: Sequence[str], packages: Sequence[
     os.makedirs(os.path.dirname(package_doc_file), exist_ok=True)
     with open(package_doc_file, "w+", encoding="utf-8") as f:
         f.write(package_content)
-    return package_name
+    return package_name_strip_root
 
 
 def process_law(directory: str, law_filename: str, output_dir: str) -> Optional[str]:
@@ -173,6 +174,7 @@ def process_law(directory: str, law_filename: str, output_dir: str) -> Optional[
     filename = os.path.normpath(os.path.join(directory, law_filename))
     no_extension_filename = os.path.splitext(filename)[0]
     law_module_name = no_extension_filename.replace(os.sep, ".")
+    law_module_name_strip_root = ".".join(law_module_name.split(".")[1:])
 
     with open(filename, "r", encoding="utf-8") as f:
         law_content = f.read()
@@ -188,7 +190,7 @@ def process_law(directory: str, law_filename: str, output_dir: str) -> Optional[
 
     law_members, law_functions = find_members_and_functions(law_parsed)
 
-    law_doc_name = law_module_name + ".rst"
+    law_doc_name = law_module_name_strip_root + ".rst"
     law_doc_file = os.path.normpath(os.path.join(output_dir, law_doc_name))
 
     law_content = "" + law_title + "\n" + ("-" * len(law_title)) + "\n\n" + law_description + "\n\n"
@@ -199,7 +201,7 @@ def process_law(directory: str, law_filename: str, output_dir: str) -> Optional[
     os.makedirs(os.path.dirname(law_doc_file), exist_ok=True)
     with open(law_doc_file, "w+", encoding="utf-8") as f:
         f.write(law_content)
-    return law_module_name
+    return law_module_name_strip_root
 
 
 def generate_laws_docs(source_dir: str, output_dir: str, exclude_dirs: Sequence[str]) -> None:
