c30e16cf7006b24af595b0d1fc74feae225b5d37
Aleksandr Slatov <alexslatov@gmail.com>
2024-07-23 02:05:45 +0400

    Create law and test

---
+++ symplyphysics/laws/gravity/radial_motion/total_energy_is_negative_average_kinetic_energy.py
@@ -0,0 +1,51 @@
+"""
+Total energy is negative kinetic energy
+=======================================
+
+The total energy of an orbiting planet is equal to its negative kinetic energy averaged over time.
+
+**Conditions:**
+
+#. Works for elliptical (:math:`E < 0`) orbits.
+"""
+
+from sympy import Eq
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    validate_input,
+    validate_output,
+)
+
+total_energy = Symbol("total_energy", units.energy)
+"""
+The total energy of the planet.
+
+Symbol:
+    E
+"""
+
+average_kinetic_energy = Symbol("average_kinetic_energy", units.energy)
+"""
+The kinetic energy of the planet averaged over time.
+
+Symbol:
+    K
+"""
+
+law = Eq(total_energy, -1 * average_kinetic_energy)
+"""
+E = -1 * K
+
+Latex:
+    .. math::
+        E = -K
+"""
+
+
+@validate_input(average_kinetic_energy_=average_kinetic_energy)
+@validate_output(total_energy)
+def calculate_total_energy(average_kinetic_energy_: Quantity) -> Quantity:
+    result = law.rhs.subs(average_kinetic_energy, average_kinetic_energy_)
+    return Quantity(result)
