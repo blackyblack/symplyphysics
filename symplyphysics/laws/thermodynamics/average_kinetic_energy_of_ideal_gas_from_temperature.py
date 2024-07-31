472eb89f7367670d78d8a5aa48aa89e761602ffc
Aleksandr Slatov <alexslatov@gmail.com>
2024-07-31 19:47:22 +0400

    Fix average kinetic energy documentation

--- symplyphysics/laws/thermodynamics/average_kinetic_energy_of_ideal_gas_from_temperature.py
+++ symplyphysics/laws/thermodynamics/average_kinetic_energy_of_ideal_gas_from_temperature.py
@@ -1,9 +1,25 @@
+r"""
+Average kinetic energy of ideal gas from temperature
+====================================================
+
+The kinetic theory of ideal gases allows us to determine the *average kinetic energy* for an ideal gas.
+It states that the average kinetic energy for all ideal gases is directly proportional to the absolute
+temperature of the gas and only depends on the temperature.
+
+**Notation:**
+
+#. :math:`k_\text{B}` is the Boltzmann constant.
+
+**Conditions:**
+
+#. The gas is ideal.
+"""
+
 from sympy import Eq, solve, Rational, stats, Interval, S
 from symplyphysics import (
     units,
     Quantity,
     Symbol,
-    print_expression,
     validate_input,
     validate_output,
     symbols,
@@ -12,25 +28,43 @@ from symplyphysics import (
 from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.laws.thermodynamics.maxwell_boltzmann_statistics import energy_distribution
 
-# Description
-## The kinetic theory of ideal gases allows us to determine the average kinetic energy for an ideal gas.
-## It states that the average kinetic energy for all ideal gases is directly proportional to the absolute temperature of the gas and only depends on the temperature.
-
 ## Definition: E = 1.5 * k * T
 ## Where:
 ## E is average kinetic energy of molecules
 ## k is Boltzmann constant,
 ## T is temperature.
 
-## Conditions
-## The gas must be ideal
 
 average_kinetic_energy = Symbol("average_kinetic_energy", units.energy, positive=True)
-equilibrium_temperature = clone_symbol(symbols.thermodynamics.temperature,
+r"""
+Average kinetic energy of the ideal gas.
+
+Symbol:
+    :math:`K_avg`
+
+Latex:
+    :math:`\overline K`
+"""
+
+equilibrium_temperature = clone_symbol(
+    symbols.thermodynamics.temperature,
     "equilibrium_temperature",
-    positive=True)
+    positive=True
+)
+"""
+Equilibrium temperature of the gas.
+
+Symbol:
+    :code:`T`
+"""
 
 law = Eq(average_kinetic_energy, Rational(3, 2) * units.boltzmann * equilibrium_temperature)
+r"""
+:code:`K_avg = 3/2 * k_B * T`
+
+Latex:
+    :math:`\overline K = \frac{3}{2} k_\text{B} T`
+"""
 
 # Derive from Maxwell-Boltzmann energy distribution
 
@@ -48,10 +82,6 @@ _random_energy_variable = stats.ContinuousRV(
 _average_energy_derived = stats.E(_random_energy_variable)
 
 assert expr_equals(_average_energy_derived, law.rhs)
-
-
-def print_law() -> str:
-    return print_expression(law)
 
 
 @validate_input(temperature_=symbols.thermodynamics.temperature)
