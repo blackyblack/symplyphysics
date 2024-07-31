1fd46404e86cb0a43052d2666b05cd5ea3005c6a
Aleksandr Slatov <alexslatov@gmail.com>
2024-07-31 16:22:18 +0400

    Fix number density file name

--- test/definitions/number_density_is_number_of_objects_per_unit_volume_test.py
+++ test/definitions/number_density_is_number_of_objects_per_unit_volume_test.py
@@ -6,7 +6,7 @@ from symplyphysics import (
     units,
     Quantity,
 )
-from symplyphysics.definitions import volume_number_density
+from symplyphysics.definitions import number_density_is_number_of_objects_per_unit_volume as law
 
 Args = namedtuple("Args", ["o", "V"])
 
@@ -19,13 +19,13 @@ def test_args_fixture() -> Args:
 
 
 def test_basic_density(test_args: Args) -> None:
-    result = volume_number_density.calculate_number_density(test_args.o, test_args.V)
+    result = law.calculate_number_density(test_args.o, test_args.V)
     assert_equal(result, 100 / units.meter**3)
 
 
 def test_bad_volume(test_args: Args) -> None:
     Vb = Quantity(1 * units.length)
     with raises(errors.UnitsError):
-        volume_number_density.calculate_number_density(test_args.o, Vb)
+        law.calculate_number_density(test_args.o, Vb)
     with raises(TypeError):
-        volume_number_density.calculate_number_density(test_args.o, 100)
+        law.calculate_number_density(test_args.o, 100)
