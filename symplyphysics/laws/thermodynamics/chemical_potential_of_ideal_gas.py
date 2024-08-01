1288a7cbf6a29eae4268dd6e380f1b0a94e52d94
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-01 15:54:21 +0400

    Add documentation to chemical potential law

--- symplyphysics/laws/thermodynamics/chemical_potential_of_ideal_gas.py
+++ symplyphysics/laws/thermodynamics/chemical_potential_of_ideal_gas.py
@@ -1,36 +1,76 @@
+r"""
+Chemical potential of ideal gas
+===============================
+
+The chemical potential of an ideal gas can be calculated from its temperature, concentration,
+and thermal wavelength.
+
+**Notation:**
+
+#. :math:`k_\text{B}` is the Boltzmann constant.
+
+**Conditions:**
+
+#. The gas is ideal.
+"""
+
 from sympy import Eq, log
 from symplyphysics import (
     units,
     Quantity,
     Symbol,
-    print_expression,
     validate_input,
     validate_output,
     symbols,
 )
 
-# Description
-## The chemical potential of an ideal gas can be calculated from its temperature, concentration,
-## and thermal wavelength.
-
-# Law: mu = k * T * log(n * lambda**3)
-## mu - chemical potential of ideal gas
-## k - Boltzmann constant
-## T - gas temperature
-## n - concentration of gas particles (particle count per unit volume)
-## lambda - [thermal de Broglie wavelength](../../definitions/thermal_de_broglie_wavelength.py) of gas
-
 chemical_potential = Symbol("chemical_potential", units.energy)
+r"""
+Chemical potential of ideal gas.
+
+Symbol:
+    :code:`mu`
+
+Latex:
+    :math:`\mu`
+"""
+
 temperature = symbols.thermodynamics.temperature
+"""
+Temperature of the gas.
+
+Symbol:
+    :code:`T`
+"""
+
 concentration = Symbol("concentration", 1 / units.volume)
+"""
+Concentration of the gas, or particle count per unit volume.
+
+Symbol:
+    :code:`n`
+"""
+
 thermal_wavelength = Symbol("thermal_wavelength", units.length)
+r"""
+:doc:`Thermal de Broglie wavelength <definitions.thermal_de_broglie_wavelength>` of the gas.
+
+Symbol:
+    :code:`lambda`
+
+Latex:
+    :math:`\lambda`
+"""
 
 law = Eq(chemical_potential,
     units.boltzmann_constant * temperature * log(concentration * thermal_wavelength**3))
+r"""
+:code:`mu = k_B * T * log(n * lambda^3)`
 
-
-def print_law() -> str:
-    return print_expression(law)
+Latex:
+    .. math::
+        \mu = k_\text{B} T \log \left( n \lambda^3 \right)
+"""
 
 
 @validate_input(
