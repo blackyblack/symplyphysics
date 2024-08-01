16320579897bd32f61013f3356d3cc37721f2bec
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-01 16:42:01 +0400

    Add documentation to diffusion law

--- symplyphysics/laws/thermodynamics/diffusion_flux_from_diffusion_coefficient_and_concentration_gradient.py
+++ symplyphysics/laws/thermodynamics/diffusion_flux_from_diffusion_coefficient_and_concentration_gradient.py
@@ -1,34 +1,66 @@
+"""
+Diffusion flux from diffusion coefficient and concentration gradient
+====================================================================
+
+*Fick's first law* relates the diffusion flux to the gradient of the concentration.
+It postulates that the flux goes from regions of high concentration to regions of
+low concentration, with a magnitude that is proportional to the concentration gradient
+(spatial derivative), or, in simplistic terms, the concept that a solute will move from
+a region of high concentration to a region of low concentration across a concentration
+gradient. The minus sign in the law indicates that the directions of the diffusion
+flow and the gradient are opposite: the gas diffuses towards a lower concentration,
+and the gradient is directed towards a higher concentration.
+
+**Conditions:**
+
+#. The mixture is ideal.
+"""
+
 from sympy import (Eq, Derivative)
-from symplyphysics import (units, Function, Quantity, Symbol, print_expression, validate_input,
+from symplyphysics import (units, Function, Quantity, Symbol, validate_input,
     validate_output)
 
-# Description
-## Fick's first law relates the diffusive flux to the gradient of the concentration. It postulates that the flux goes from regions of high concentration to regions of low concentration,
-## with a magnitude that is proportional to the concentration gradient (spatial derivative), or in simplistic terms the concept that a solute will move from a region of high concentration
-## to a region of low concentration across a concentration gradient.
-##The sign "-" in Fick's law indicates that the directions of the diffusion flow and the gradient are opposite: the gas diffuses towards a lower concentration, and the gradient is directed towards a higher concentration.
-
-## Law: J = -D * dn / dx
-## Where:
-## J is the diffusion flux
-## D is the diffusion coefficient
-## n is the concentration, or amount of the substance per unit volume
-## x is position
-
-## Conditions
-## Mixture is ideal
-
 diffusion_flux = Function("diffusion_flux", units.amount_of_substance / (units.area * units.time))
+"""
+Diffusion flux.
+
+Symbol:
+    :code:`J`
+"""
+
 diffusion_coefficient = Symbol("diffusion_coefficient", units.area / units.time)
+"""
+Diffusion coefficient.
+
+Symbol:
+    :code:`D`
+"""
+
 concentration = Function("concentration", units.amount_of_substance / units.volume)
+"""
+Concentration of particles as a function of position.
+
+Symbol:
+    :code:`n(x)`
+"""
+
 position = Symbol("position", units.length)
+"""
+Position of particles.
+
+Symbol:
+    :code:`x`
+"""
 
 law = Eq(diffusion_flux(position),
     -diffusion_coefficient * Derivative(concentration(position), position))
+r"""
+:code:`J = -1 * D * Derivative(n(x), x)`
 
-
-def print_law() -> str:
-    return print_expression(law)
+Latex:
+    .. math::
+        J = - D \frac{d n}{d x}
+"""
 
 
 @validate_input(diffusion_coefficient_=diffusion_coefficient,
