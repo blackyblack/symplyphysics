c875854d885dc6ba6d841355b3e328dcaa7f28fa
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-02 00:33:38 +0400

    Add documentation to internal energy law and rename

---
+++ symplyphysics/laws/thermodynamics/internal_energy_of_ideal_gas_via_temperature.py
@@ -0,0 +1,73 @@
+r"""
+Internal energy of ideal gas via temperature
+============================================
+
+Internal energy of an ideal gas is the sum of the kinetic energy of all of its molecules.
+
+**Notation:**
+
+#. :math:`R` is the molar gas constant.
+
+**Conditions:**
+
+#. The gas is ideal.
+"""
+
+from sympy import (Eq, solve)
+from symplyphysics import (units, Quantity, Symbol, validate_input,
+    validate_output, symbols)
+
+internal_energy = Symbol("energy", units.energy)
+"""
+Internal energy of the gas.
+
+Symbol:
+    :code:`U`
+"""
+
+mass = symbols.basic.mass
+"""
+Mass of the gas.
+
+Symbol:
+    :code:`m`
+"""
+
+temperature = symbols.thermodynamics.temperature
+"""
+Temperature of the gas.
+
+Symbol:
+    :code:`T`
+"""
+
+molar_mass = Symbol("molar_mass", units.mass / units.amount_of_substance)
+"""
+Mass of gas per unit amount of substance.
+
+Symbol:
+    :code:`M`
+"""
+
+law = Eq(internal_energy,
+    1.5 * mass * units.molar_gas_constant * temperature / molar_mass)
+r"""
+:code:`U = (3 / 2) * (m / M) * R * T`
+
+Latex:
+    .. math::
+        U = \frac{3}{2} \frac{m}{M} R T
+"""
+
+
+@validate_input(mass_of_gas_=mass, temperature_=temperature, mole_mass_=molar_mass)
+@validate_output(internal_energy)
+def calculate_inner_energy(mass_of_gas_: Quantity, temperature_: Quantity,
+    mole_mass_: Quantity) -> Quantity:
+    solved = solve(law, internal_energy, dict=True)[0][internal_energy]
+    result_expr = solved.subs({
+        mass: mass_of_gas_,
+        temperature: temperature_,
+        molar_mass: mole_mass_
+    })
+    return Quantity(result_expr)
