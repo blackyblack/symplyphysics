71ac835ba480456c5938a2d0476e169f7e5e21eb
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-02 02:09:06 +0400

    Add documentation to law

--- symplyphysics/laws/thermodynamics/isentropic_speed_of_sound.py
+++ symplyphysics/laws/thermodynamics/isentropic_speed_of_sound.py
@@ -1,38 +1,68 @@
+"""
+Isentropic speed of sound
+=========================
+
+Derived by Laplace, the formula for the speed of sound in fluids uses the pressure-density dependence
+and the fact that the oscillations in a sound wave happen so fast, and the thermal conductivity of air
+is so small, that there is no heat transfer in the sound wave, i.e. it is an adiabatic, and therefore
+isentropic, process. This is in contrast with the Newton's formula, who thought that the sound propagation
+is an isothermal process in the assumption that the temperature differences between different parts of
+the sound wave immediately level out, which eventually turned out to be inconsistent with experimental data.
+"""
+
 from sympy import Eq, sqrt, Derivative
 from symplyphysics import (
     units,
     Quantity,
     Symbol,
     Function,
-    print_expression,
     validate_input,
     validate_output,
 )
 
-# Description
-## Derived by Laplace, the formula for the speed of sound in fluids uses the pressure-density dependence
-## and the fact that the oscillations in a sound wave happen so fast, and the thermal conductivity of air
-## is so small, that there is no heat transfer in the sound wave, i.e. it is an adiabatic, and therefore
-## isentropic, process. This is in contrast with the Newton's formula, who thought that the sound propagation
-## is an isothermal process in the assumption that the temperature differences between different parts of
-## the sound wave immediately level out, which eventually turned out to be inconsistent with experimental data.
-
-# Law: c = sqrt((dp/d(rho))_S)
-## c - speed of sound in fluid
-## p - pressure
-## rho - density
-## (d/d(rho)_S - derivative w.r.t. density at constant entropy (i.e. adiabatic conditions)
-
 speed_of_sound = Symbol("speed_of_sound", units.velocity)
+"""
+Speed of sound in the fluid.
+
+Symbol:
+    :code:`c`
+"""
+
 pressure = Function("pressure", units.pressure)
+"""
+Pressure inside the fluid.
+
+Symbol:
+    :code:`p`
+"""
+
 density = Symbol("density", units.mass / units.volume)
+r"""
+Density of the fluid.
+
+Symbol:
+    :code:`rho`
+
+Latex:
+    :math:`\rho`
+"""
+
 entropy = Symbol("entropy", units.energy / units.temperature)
+"""
+Entropy of the fluid.
+
+Symbol:
+    :code:`S`
+"""
 
 law = Eq(speed_of_sound, sqrt(Derivative(pressure(density, entropy), density)))
+r"""
+:code:`c = sqrt(Derivative(p(rho, S), rho))`
 
-
-def print_law() -> str:
-    return print_expression(law)
+Latex:
+    .. math::
+        c = \sqrt{\left( \frac{\partial p}{\partial \rho} \right)_S}
+"""
 
 
 @validate_input(
