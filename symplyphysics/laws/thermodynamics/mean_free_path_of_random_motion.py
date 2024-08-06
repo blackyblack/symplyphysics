ef784fd82e1289de48e86bfa37ce9251eb6700e9
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-07 01:21:25 +0400

    Add documentation

--- symplyphysics/laws/thermodynamics/mean_free_path_of_random_motion.py
+++ symplyphysics/laws/thermodynamics/mean_free_path_of_random_motion.py
@@ -1,49 +1,71 @@
+"""
+Mean free path of random motion
+===============================
+
+The mean free path of a molecule in random motion is its average path length between collisions.
+
+**Conditions:**
+
+#. Spherical model of molecules is assumed.
+"""
+
 from sympy import Eq, sqrt, pi
 from symplyphysics import (
     Quantity,
     Symbol,
-    print_expression,
     units,
-    dimensionless,
     validate_input,
     validate_output,
 )
 
-# Description
-## The mean free path of a molecule in random motion is its average path length between collisions.
-
-# Law: lambda = 1 / (sqrt(2) * pi * d**2 * (N / V))
-## lambda - mean free path estimate
-## d - molecular diameter (assuming the spherical model of molecules)
-## N - number of molecules found in volume V
-## V - volume
-
 mean_free_path = Symbol("mean_free_path", units.length, positive=True)
+r"""
+Mean free path estimate of molecules.
+
+Symbol:
+    :code:`lambda`
+
+Latex:
+    :math:`\lambda`
+"""
+
 molecular_diameter = Symbol("molecular_diameter", units.length, positive=True)
-molecule_count = Symbol("molecule_count", dimensionless, integer=True, positive=True)
-volume = Symbol("volume", units.volume, positive=True)
+"""
+Diameter of molecules.
 
-law = Eq(mean_free_path, 1 / (sqrt(2) * pi * molecular_diameter**2 * (molecule_count / volume)))
+Symbol:
+    :code:`d`
+"""
 
+number_density = Symbol("number_density", 1 / units.volume)
+"""
+:doc:`Number density <definitions.number_density_is_number_of_objects_per_unit_volume>` of the system.
 
-def print_law() -> str:
-    return print_expression(law)
+Symbol:
+    :code:`n`
+"""
+
+law = Eq(mean_free_path, 1 / (sqrt(2) * pi * molecular_diameter**2 * number_density))
+r"""
+:code:`lambda = 1 / (sqrt(2) * pi * d^2 * n)`
+
+Latex:
+    .. math::
+        \lambda = \frac{1}{\sqrt{2} \pi d^2 n}
+"""
 
 
 @validate_input(
     molecular_diameter_=molecular_diameter,
-    molecule_count_=molecule_count,
-    volume_=volume,
+    number_density_=number_density,
 )
 @validate_output(mean_free_path)
 def calculate_mean_free_path(
     molecular_diameter_: Quantity,
-    molecule_count_: int,
-    volume_: Quantity,
+    number_density_: Quantity,
 ) -> Quantity:
     result = law.rhs.subs({
         molecular_diameter: molecular_diameter_,
-        molecule_count: molecule_count_,
-        volume: volume_,
+        number_density: number_density_,
     })
     return Quantity(result)
--- test/thermodynamics/mean_free_path_of_random_motion_test.py
+++ test/thermodynamics/mean_free_path_of_random_motion_test.py
@@ -13,39 +13,34 @@ from symplyphysics.laws.thermodynamics import mean_free_path_of_random_motion as
 ## The estimate of the mean free path of N = 10.000 particles of diameter d = 3 µm traveling randomly
 ## in a volume V = 1 litre is lambda = 2.5 km.
 
-Args = namedtuple("Args", "d n v")
+Args = namedtuple("Args", "d n")
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
     d = Quantity(3 * prefixes.micro * units.meter)
-    n = 10_000
+    n_ = 10_000
     v = Quantity(1 * units.liter)
-    return Args(d=d, n=n, v=v)
+    n = Quantity(n_ / v)
+    return Args(d=d, n=n)
 
 
 def test_law(test_args: Args) -> None:
-    result = mean_free_path_law.calculate_mean_free_path(test_args.d, test_args.n, test_args.v)
+    result = mean_free_path_law.calculate_mean_free_path(test_args.d, test_args.n)
     assert_equal(result, 2.5 * units.kilometer)
 
 
 def test_bad_diameter(test_args: Args) -> None:
     db = Quantity(1.0 * units.coulomb)
     with raises(errors.UnitsError):
-        mean_free_path_law.calculate_mean_free_path(db, test_args.n, test_args.v)
+        mean_free_path_law.calculate_mean_free_path(db, test_args.n)
     with raises(TypeError):
-        mean_free_path_law.calculate_mean_free_path(100, test_args.n, test_args.v)
+        mean_free_path_law.calculate_mean_free_path(100, test_args.n)
 
 
-def test_bad_molecule_count(test_args: Args) -> None:
+def test_bad_number_density(test_args: Args) -> None:
     nb = Quantity(1.0 * units.coulomb)
     with raises(errors.UnitsError):
-        mean_free_path_law.calculate_mean_free_path(test_args.d, nb, test_args.v)
-
-
-def test_bad_volume(test_args: Args) -> None:
-    vb = Quantity(1.0 * units.coulomb)
-    with raises(errors.UnitsError):
-        mean_free_path_law.calculate_mean_free_path(test_args.d, test_args.n, vb)
+        mean_free_path_law.calculate_mean_free_path(test_args.d, nb)
     with raises(TypeError):
-        mean_free_path_law.calculate_mean_free_path(test_args.d, test_args.n, 100)
+        mean_free_path_law.calculate_mean_free_path(test_args.d, 100)
