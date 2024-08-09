020285f5bca53023e02f4d68f0ca64632c743a53
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-09 11:36:58 +0400

    Add documentation

--- test/thermodynamics/quantum_isochoric_molar_heat_capacity_of_solids_test.py
+++ test/thermodynamics/quantum_isochoric_molar_heat_capacity_of_solids_test.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from pytest import fixture, raises
+from sympy import pi
 from symplyphysics import (
     assert_equal,
     errors,
@@ -12,34 +13,27 @@ from symplyphysics.laws.thermodynamics import (
 
 # Description
 ## The isochoric molar heat capacity for a solid in the Einstein model with atomic thermal oscillator
-## frequency nu = 4 THz at temperature T = 300 K is C_V = 24.1 J/(K*mol).
+## frequency nu = 4 THz at temperature T = 300 K is C_V = 24.1 J/(K*mol). The reduced photon energy
+## is x = 0.64.
 
-Args = namedtuple("Args", "nu t")
+Args = namedtuple("Args", "x")
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
     nu = Quantity(4 * prefixes.tera * units.hertz)
+    w = Quantity(2 * pi * nu)
     t = Quantity(300 * units.kelvin)
-    return Args(nu=nu, t=t)
+    x = Quantity(units.hbar * w / (units.boltzmann_constant * t))
+    return Args(x=x)
 
 
 def test_law(test_args: Args) -> None:
-    result = einstein_law.calculate_isochoric_molar_heat_capacity(test_args.nu, test_args.t)
+    result = einstein_law.calculate_isochoric_molar_heat_capacity(test_args.x)
     assert_equal(result, 24.1 * units.joule / (units.kelvin * units.mole))
 
 
-def test_bad_frequency(test_args: Args) -> None:
-    nub = Quantity(1 * units.coulomb)
+def test_bad_number() -> None:
+    xb = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        einstein_law.calculate_isochoric_molar_heat_capacity(nub, test_args.t)
-    with raises(TypeError):
-        einstein_law.calculate_isochoric_molar_heat_capacity(100, test_args.t)
-
-
-def test_bad_temperature(test_args: Args) -> None:
-    tb = Quantity(1 * units.coulomb)
-    with raises(errors.UnitsError):
-        einstein_law.calculate_isochoric_molar_heat_capacity(test_args.nu, tb)
-    with raises(TypeError):
-        einstein_law.calculate_isochoric_molar_heat_capacity(test_args.nu, 100)
+        einstein_law.calculate_isochoric_molar_heat_capacity(xb)
