891c8079e859a51c21b1e59525eab124bcee2460
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-09 12:17:09 +0400

    Add documentation

--- symplyphysics/laws/thermodynamics/pressure_of_ideal_gas_from_height_and_temperature.py
+++ symplyphysics/laws/thermodynamics/pressure_of_ideal_gas_from_height_and_temperature.py
@@ -1,56 +1,95 @@
+r"""
+Pressure of ideal gas from height and temperature
+=================================================
+
+The *barometric formula* determines the dependence of the pressure or density of a gas on the height in the gravity field.
+
+**Notation:**
+
+#. :math:`g` is the acceleration due to gravity.
+#. :math:`k_\text{B}` (:code:`k_B`) is the Boltzmann constant.
+
+**Conditions:**
+
+#. The gas is ideal.
+#. The gas is in a uniform gravity field.
+"""
+
 from sympy import (Eq, solve, exp)
-from symplyphysics import (symbols, units, Quantity, Symbol, print_expression, validate_input,
-    validate_output)
-
-# Description
-## The barometric formula determines the dependence of the pressure or density of a gas on the height in the gravity field.
-
-## Law: p = p_0 * exp((-g * m * (h - h_0)) / (R * T))
-## Where:
-## p is the gas pressure in the layer located at height h
-## p_0 is the gas pressure at the initial height
-## g is acceleration of free fall
-## m is atomic weight of the gas
-## h is final height
-## h_0 is initial height
-## R is ideal gas constant
-## T is temperature
-
-## Conditions
-## Gas is ideal
-## The gas is in a uniform gravity field
+from symplyphysics import (symbols, units, Quantity, Symbol, validate_input,
+    validate_output, clone_symbol)
 
 final_pressure = Symbol("final_pressure", units.pressure)
+"""
+Pressure of the gas at final height.
+
+Symbol:
+    :code:`p`
+"""
+
 initial_pressure = Symbol("initial_pressure", units.pressure)
-atomic_weight = Symbol("atomic_weight", units.mass / units.amount_of_substance)
-final_height = Symbol("final_height", units.length)
-initial_height = Symbol("initial_height", units.length)
+r"""
+Pressure of the gas at initial height.
+
+Symbol:
+    :code:`p_0`
+
+Latex:
+    :math:`p_0`
+"""
+
+molecular_mass = clone_symbol(symbols.basic.mass, "molecular_mass")
+"""
+:attr:`~symplyphysics.symbols.basic.mass` of a single gas molecule.
+
+Symbol:
+    :code:`m`
+"""
+
+height_change = Symbol("height_change", units.length)
+r"""
+Change in height between :math:`p_0` and :math:`p`.
+
+Symbol:
+    :code:`dh`
+
+Latex:
+    :math:`\Delta h`
+"""
+
 temperature = symbols.thermodynamics.temperature
+r"""
+:attr:`~symplyphysics.symbols.thermodynamics.temperature` of the gas.
+
+Symbol:
+    :code:`T`
+"""
 
 law = Eq(
     final_pressure,
-    initial_pressure * exp(-units.acceleration_due_to_gravity * atomic_weight *
-    (final_height - initial_height) / (units.molar_gas_constant * temperature)))
+    initial_pressure * exp(-units.acceleration_due_to_gravity * molecular_mass *
+    height_change / (units.boltzmann_constant * temperature)))
+r"""
+:code:`p = p_0 * exp(-1 * g * m * dh / (k_B * T))`
 
-
-def print_law() -> str:
-    return print_expression(law)
+Latex:
+    .. math::
+        p = p_0 \exp \left( - \frac{g m \Delta h}{k_\text{B} T} \right)
+"""
 
 
 @validate_input(initial_pressure_=initial_pressure,
-    atomic_weight_=atomic_weight,
-    final_height_=final_height,
-    initial_height_=initial_height,
+    molecular_mass_=molecular_mass,
+    height_change_=height_change,
     temperature_=temperature)
 @validate_output(final_pressure)
-def calculate_final_pressure(initial_pressure_: Quantity, atomic_weight_: Quantity,
-    initial_height_: Quantity, final_height_: Quantity, temperature_: Quantity) -> Quantity:
+def calculate_final_pressure(initial_pressure_: Quantity, molecular_mass_: Quantity,
+    height_change_: Quantity, temperature_: Quantity) -> Quantity:
     result_expr = solve(law, final_pressure, dict=True)[0][final_pressure]
     result_final_pressure = result_expr.subs({
         initial_pressure: initial_pressure_,
-        atomic_weight: atomic_weight_,
-        final_height: final_height_,
-        initial_height: initial_height_,
+        molecular_mass: molecular_mass_,
+        height_change: height_change_,
         temperature: temperature_
     })
     return Quantity(result_final_pressure)
