88657938b85b9d123375e53c27002babe22d8983
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-09 13:57:13 +0400

    Fix tests

--- test/thermodynamics/heat_is_heat_capacity_times_temperature_change_test.py
+++ test/thermodynamics/heat_is_heat_capacity_times_temperature_change_test.py
@@ -13,7 +13,7 @@ from symplyphysics.laws.thermodynamics import heat_is_heat_capacity_times_temper
 # How much energy does it take to heat some volume of water tо 50 degree Celsius?
 # Heat capacity of that amount of water is 2.1 kJ/K, ignore losses.
 
-Args = namedtuple("Args", ["C", "t1", "t2"])
+Args = namedtuple("Args", ["C", "dt"])
 
 
 @fixture(name="test_args")
@@ -23,29 +23,26 @@ def test_args_fixture() -> Args:
     t1 = to_kelvin_quantity(initial_temperature)
     final_temperature = Celsius(50)
     t2 = to_kelvin_quantity(final_temperature)
-    return Args(C=C, t1=t1, t2=t2)
+    dt = Quantity(t2 - t1)
+    return Args(C=C, dt=dt)
 
 
 def test_basic_amount(test_args: Args) -> None:
-    result = thermal_energy.calculate_amount_energy(test_args.C, test_args.t2, test_args.t1)
+    result = thermal_energy.calculate_amount_energy(test_args.C, test_args.dt)
     assert_equal(result, 105000.1 * units.joule)
 
 
 def test_bad_heat_capacity(test_args: Args) -> None:
     Cb = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        thermal_energy.calculate_amount_energy(Cb, test_args.t2, test_args.t1)
+        thermal_energy.calculate_amount_energy(Cb, test_args.dt)
     with raises(TypeError):
-        thermal_energy.calculate_amount_energy(100, test_args.t2, test_args.t1)
+        thermal_energy.calculate_amount_energy(100, test_args.dt)
 
 
 def test_bad_temperature(test_args: Args) -> None:
     tb = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        thermal_energy.calculate_amount_energy(test_args.C, tb, test_args.t1)
+        thermal_energy.calculate_amount_energy(test_args.C, tb)
     with raises(TypeError):
-        thermal_energy.calculate_amount_energy(test_args.C, 100, test_args.t1)
-    with raises(errors.UnitsError):
-        thermal_energy.calculate_amount_energy(test_args.C, test_args.t2, tb)
-    with raises(TypeError):
-        thermal_energy.calculate_amount_energy(test_args.C, test_args.t2, 100)
+        thermal_energy.calculate_amount_energy(test_args.C, 100)
