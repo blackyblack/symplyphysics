c22e2dd378f03bf47087715125f909380b644d58
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-11 23:56:31 +0400

    Add documentation

---
+++ symplyphysics/laws/thermodynamics/euler_relations/__init__.py
@@ -0,0 +1,10 @@
+"""
+**Euler relations**
+===================
+
+*Euler relations* are thermodynamic equations relating thermodynamic potentials with parameters
+of the system such as pressure, volume, temperature, entropy, etc. They are also called *Euler
+integrals* because they feature not differentials of thermodynamic potentials, but their integrated
+forms. They are derived using `Euler's homogeneous function theorem
+<https://en.wikipedia.org/wiki/Homogeneous_function#Euler's_theorem>`_.
+"""
