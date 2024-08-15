b7df17df52c17ad010b3052f1a8f4e1f094cad98
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-16 00:15:53 +0400

    Add documentation

---
+++ symplyphysics/laws/electricity/__init__.py
@@ -0,0 +1,8 @@
+"""
+Electromagnetism
+================
+
+*Electromagnetism* is a fundamental interaction that occurs between particles with electric
+charge via electromagnetic fields.Electromagnetism can be thought of as a combination of
+electrostatics and magnetism, which are distinct but closely intertwined phenomena.
+"""
