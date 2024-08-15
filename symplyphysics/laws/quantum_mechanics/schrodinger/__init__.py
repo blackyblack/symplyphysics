82b7c2fb13e9848828128da10ca65013d86b7e25
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-16 00:37:13 +0400

    Add documentation

---
+++ symplyphysics/laws/quantum_mechanics/schrodinger/__init__.py
@@ -0,0 +1,6 @@
+"""
+**Schrödinger equation**
+========================
+
+The Schrödinger equation is a partial differential equation that governs the wave function of a quantum-mechanical system.
+"""
