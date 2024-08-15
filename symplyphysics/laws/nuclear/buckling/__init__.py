086f1d4781a9321a92d12c89d757a304e80db101
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-16 00:40:10 +0400

    Add documentation

---
+++ symplyphysics/laws/nuclear/buckling/__init__.py
@@ -0,0 +1,7 @@
+"""
+Buckling (nuclear physics)
+==========================
+
+*Geometric buckling* is a measure of neutron leakage and *material buckling* is a measure
+of the difference between neutron production and neutron absorption.
+"""
