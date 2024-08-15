2da1285a98435305925d61568eba0d46c94bb0e1
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-15 19:07:53 +0400

    Add documentation

---
+++ symplyphysics/laws/conservation/__init__.py
@@ -0,0 +1,7 @@
+"""
+Conservation
+============
+
+A conservation law states that a particular measurable property of an isolated physical
+system does not change as the system evolves over time.
+"""
