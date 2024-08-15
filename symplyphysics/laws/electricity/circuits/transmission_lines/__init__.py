20e75a2dab2795bb61230376dc62456eb0d1f85d
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-16 00:23:13 +0400

    Add documentation

---
+++ symplyphysics/laws/electricity/circuits/transmission_lines/__init__.py
@@ -0,0 +1,8 @@
+"""
+**Transmission lines**
+======================
+
+A *transmission line* is a specialized cable or other structure designed to conduct
+electromagnetic waves in a contained manner. The term applies when the conductors
+are long enough that the wave nature of the transmission must be taken into account.
+"""
