91a8d7905df7d0ffa9b3993f64a38b8f98edefd4
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-16 00:43:27 +0400

    Add documentation

--- symplyphysics/laws/dynamics/damped_oscillations/__init__.py
+++ symplyphysics/laws/dynamics/damped_oscillations/__init__.py
@@ -1,6 +1,6 @@
 """
-**Damped oscillations**
-=======================
+**Damped oscillations (dynamics)**
+==================================
 
 *Damping* is the loss of energy of an oscillating system by dissipation. It is an influence within or
 upon an oscillatory system that has the effect of reducing or preventing its oscillation.
