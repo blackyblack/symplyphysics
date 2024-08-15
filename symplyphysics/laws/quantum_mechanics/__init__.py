07267c76c2412159a8cc15fcfd6022cef6621c33
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-16 00:36:00 +0400

    Add documentation

---
+++ symplyphysics/laws/quantum_mechanics/__init__.py
@@ -0,0 +1,7 @@
+"""
+Quantum mechanics
+=================
+
+*Quantum mechanics* is a fundamental theory that describes the behavior of nature at and below the
+scale of atoms and deals with the behavior of matter and light on the atomic and subatomic scale.
+"""
