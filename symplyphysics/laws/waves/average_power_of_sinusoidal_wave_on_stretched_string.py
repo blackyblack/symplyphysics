21b7d30e29283fc7f55ad083a6b8115678d3df23
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-17 23:33:09 +0400

    Add documentation

--- symplyphysics/laws/waves/average_power_of_sinusoidal_wave_on_stretched_string.py
+++ symplyphysics/laws/waves/average_power_of_sinusoidal_wave_on_stretched_string.py
@@ -1,39 +1,81 @@
+"""
+Average power of sinusoidal wave on stretched string
+====================================================
+
+The average power of a wave of any type is proportional to the square of its amplitude and to the
+square of its angular frequency.
+"""
+
 from sympy import Eq
 from symplyphysics import (
     units,
     angle_type,
     Symbol,
     Quantity,
-    print_expression,
     validate_input,
     validate_output,
 )
 
-# Description
-## The average power of a wave of any type is proportional to the square of its amplitude and to the
-## square of its angular frequency.
-
-# P_avg = mu * v * w**2 * u_max**2 / 2
-## P_avg - average power, or rate of energy transmission, of wave
-## mu - linear density of string
-## v - phase velocity of wave
-## w - angular frequency of wave
-## u_max - amplitude of wave
-
 wave_average_power = Symbol("wave_average_power", units.power)
+"""
+Average power, or rate of energy transfer, of the wave.
+
+Symbol:
+    :code:`P`
+"""
+
 string_linear_density = Symbol("string_linear_density", units.mass / units.length)
+r"""
+Linear density of the string, i.e. mass per unit length.
+
+Symbol:
+    :code:`mu`
+
+Latex:
+    :math:`\mu`
+"""
+
 wave_phase_velocity = Symbol("wave_phase_velocity", units.velocity)
+"""
+Phase velocity of the wave.
+
+Symbol:
+    :code:`v`
+"""
+
 wave_angular_frequency = Symbol("wave_angular_frequency", angle_type / units.time)
+r"""
+Angular frequency of the wave.
+
+Symbol:
+    :code:`w`
+
+Latex:
+    :math:`\omega`
+"""
+
 wave_amplitude = Symbol("wave_amplitude", units.length)
+r"""
+Amplitude of the wave.
+
+Symbol:
+    :code:`u_max`
+
+Latex:
+    :math:`u_\text{max}`
+"""
 
 law = Eq(
     wave_average_power,
     string_linear_density * wave_phase_velocity * wave_angular_frequency**2 * wave_amplitude**2 / 2,
 )
+r"""
+:code:`P = 1/2 * mu * v * w^2 * u_max^2`
 
-
-def print_law() -> str:
-    return print_expression(law)
+Latex:
+    .. math::
+        P = \frac{1}{2} \mu v \omega^2 u_\text{max}^2
+"""
 
 
 @validate_input(
