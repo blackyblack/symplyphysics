ab7ce19945e523e814ceffed3cec6b752f3f033c
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-18 22:18:26 +0400

    Add documentation

--- symplyphysics/laws/waves/phase_of_traveling_wave.py
+++ symplyphysics/laws/waves/phase_of_traveling_wave.py
@@ -1,45 +1,89 @@
+r"""
+Phase of traveling wave
+=======================
+
+The phase of a wave or other periodic function of some real variable :math:`t` is an angle-like
+quantity representing the fraction of the cycle covered up to :math:`t`. As the variable :math:`t`
+completes a full period, the phase increases by :math:`360^\circ` or :math:`2 \pi`.
+
+If a function :math:`h(x, t)` describes a traveling wave, then position :math:`x` and time :math:`t`
+can only appear in the form of the wave phase described below.
+
+**Notes:**
+
+#. :math:`\omega = (\vec \omega \cdot {\vec e}_x)`, i.e. the angular frequency is a positive quantity
+   if the wave travels in the positive direction of the :math:`x`-axis.
+
+**Conditions:**
+
+#. This law applies to a :math:`1`-dimensional traveling wave.
+#. The constant phase shift is not taken into account.
+"""
+
 from sympy import Eq
 from symplyphysics import (
     units,
     angle_type,
     Symbol,
     Quantity,
-    print_expression,
     validate_input,
     validate_output,
 )
 
-# Description
-## Any function `h(x, t)` that depends on position `x` and time `t` can describe a traveling
-## wave if `x` and `t` appear solely in the form of `k*x + omega*t`, i.e. the function depends
-## on the wave phase described below.
-
-# Law: phi(x, t) = k * x - omega * t
-## phi - wave phase
-## k - angular wavenumber
-## x - position
-## omega - angular frequency
-## t - time
-
-# Note
-## `omega` can be both positive (the wave travels in the positive direction of x-axis)
-## or negative (the wave travels in the negative direction of x-axis)
-
-# Condition
-## - This is the case of a wave traveling in a single spatial dimension.
-## - A constant phase lag is not taken into account.
-
 wave_phase = Symbol("wave_phase", angle_type, real=True)
+r"""
+Phase of the wave.
+
+Symbol:
+    :code:`phi`
+
+Latex:
+    :math:`\varphi`
+"""
+
 angular_wavenumber = Symbol("angular_wavenumber", angle_type / units.length, positive=True)
+"""
+Angular wavenumber of the wave.
+
+Symbol:
+    :code:`k`
+"""
+
 position = Symbol("position", units.length, real=True)
+"""
+Position, or spatial coordinate.
+
+Symbol:
+    :code:`x`
+"""
+
 angular_frequency = Symbol("angular_frequency", angle_type / units.time, real=True)
+r"""
+Angular frequency of the wave.
+
+Symbol:
+    :code:`w`
+
+Latex:
+    :math:`\omega`
+"""
+
 time = Symbol("time", units.time, positive=True)
+"""
+Time.
+
+Symbol:
+    :code:`t`
+"""
 
 law = Eq(wave_phase, angular_wavenumber * position - angular_frequency * time)
+r"""
+:code:`phi = k * x - w * t`
 
-
-def print_law() -> str:
-    return print_expression(law)
+Latex:
+    .. math::
+        \varphi = k x - \omega t
+"""
 
 
 @validate_input(
