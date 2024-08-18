c0bc4dfad2c76c64da8d5581f3228c876f111d23
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-18 14:49:13 +0400

    Add documentation

--- symplyphysics/laws/waves/frequency_of_the_electron_transition_in_hydrogen.py
+++ symplyphysics/laws/waves/frequency_of_the_electron_transition_in_hydrogen.py
@@ -1,37 +1,79 @@
+"""
+Frequency of electron transition in Hydrogen
+============================================
+
+The *Rydberg formula* relates the frequency of light emitted or absorbed during
+electron transition to the levels between which the transition is occurring.
+"""
+
 from sympy import (Eq, solve)
-from symplyphysics import (units, Quantity, Symbol, print_expression, dimensionless, validate_input,
+from symplyphysics import (units, Quantity, Symbol, dimensionless, validate_input,
     validate_output)
 
-# Description
-## There is an empirical formula for the spectral lines of the hydrogen atom.
-
-# Law: f = R * (1 / m^2 - 1 / n^2 ), where
-## f - frequency of the electron transition in hydrogen,
-## R - Rydberg's constant,
-## m - the number of the level to which the electron goes,
-## n - the number of the level from which the electron leaves.
-
 transition_frequency = Symbol("transition_frequency", units.frequency)
+r"""
+Frequency of light emitted or absorbed during electron transition in the Hydrogen atom.
 
-number_level_to = Symbol("number_level_to", dimensionless)
-number_level_from = Symbol("number_level_from", dimensionless)
+Symbol:
+    :code:`nu`
 
-rydberg_constant = Quantity(3.29e15 * units.hertz)
+Latex:
+    :math:`\nu`
+"""
+
+lower_principal_quantum_number = Symbol("lower_principal_quantum_number", dimensionless)
+r"""
+The principal quantum number of the lower transitional level.
+
+Symbol:
+    :code:`n_1`
+
+Latex:
+    :math:`n_1`
+"""
+
+higher_principal_quantum_number = Symbol("higher_principal_quantum_number", dimensionless)
+"""
+The principal quantum number of the higher transitional level.
+
+Symbol:
+    :code:`n_2`
+
+Latex:
+    :math:`n_2`
+"""
+
+rydberg_constant = Quantity(3.2898419602500e15 * units.hertz)
+r"""
+Rydberg frequency constant for Hydrogen.
+
+Symbol:
+    :code:`R_H`
+
+Latex:
+    :math:`R_\text{H}`
+"""
 
 law = Eq(transition_frequency,
-    rydberg_constant * ((1 / number_level_to**2) - (1 / number_level_from**2)))
+    rydberg_constant * ((1 / lower_principal_quantum_number**2) - (1 / higher_principal_quantum_number**2)))
+r"""
+:code:`nu = R_H * (1 / n_1^2 - 1 / n_2^2)`
+
+Latex:
+    .. math::
+        \nu = R_\text{H} \left( \frac{1}{n_1^2} - \frac{1}{n_2^2} \right)
+"""
 
 
-def print_law() -> str:
-    return print_expression(law)
-
-
-@validate_input(number_level_to_=number_level_to, number_level_from_=number_level_from)
+@validate_input(
+    number_level_to_=lower_principal_quantum_number,
+    number_level_from_=higher_principal_quantum_number,
+)
 @validate_output(transition_frequency)
 def calculate_transition_frequency(number_level_to_: int, number_level_from_: int) -> Quantity:
     result_expr = solve(law, transition_frequency, dict=True)[0][transition_frequency]
     transition_frequency_applied = result_expr.subs({
-        number_level_to: number_level_to_,
-        number_level_from: number_level_from_
+        lower_principal_quantum_number: number_level_to_,
+        higher_principal_quantum_number: number_level_from_
     })
     return Quantity(transition_frequency_applied)
