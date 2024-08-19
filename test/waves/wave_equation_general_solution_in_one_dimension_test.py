3c8e4664de11522f010de510d3e58a08ef416eaf
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-19 20:55:01 +0400

    Add documentation

--- test/waves/wave_equation_general_solution_in_one_dimension_test.py
+++ test/waves/wave_equation_general_solution_in_one_dimension_test.py
@@ -12,22 +12,22 @@ from symplyphysics.laws.waves import wave_equation_general_solution_in_one_dimen
 ## A wave is traveling in a stretched string with maximum amplitude of 5 cm. When wave phase
 ## is 2.3 rad, the displacement of the string is -3.33 cm.
 
-Args = namedtuple("Args", "a psi")
+Args = namedtuple("Args", "a phi")
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
     a = Quantity(5.0 * units.centimeter)
-    psi = 2.3
-    return Args(a=a, psi=psi)
+    phi = 2.3
+    return Args(a=a, phi=phi)
 
 
 def test_law(test_args: Args) -> None:
-    result = wave_law.calculate_displacement(test_args.a, test_args.psi)
+    result = wave_law.calculate_displacement(test_args.a, test_args.phi)
     assert_equal(result, -3.33 * units.centimeter)
 
 
 def test_bad_wave_phase(test_args: Args) -> None:
-    psi_bad = Quantity(1.0 * units.coulomb)
+    phib = Quantity(1.0 * units.coulomb)
     with raises(errors.UnitsError):
-        wave_law.calculate_displacement(test_args.a, psi_bad)
+        wave_law.calculate_displacement(test_args.a, phib)
