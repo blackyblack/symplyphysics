4eaff958b694c5631ce43c93e9286eb7fc028765
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-19 20:28:11 +0400

    Add documentation

--- symplyphysics/laws/waves/speed_of_light_from_fundamentals.py
+++ symplyphysics/laws/waves/speed_of_light_from_fundamentals.py
@@ -1,21 +1,29 @@
+r"""
+Speed of light via vacuum permittivity and permeability
+=======================================================
+
+Speed of light can be expressed using the permittivity and permeability
+of the vacuum.
+
+**Notation:**
+
+#. :math:`c` is the speed of light.
+#. :math:`\varepsilon_0` (:code:`epsilon_0`) is vacuum permittivity, or electric constant.
+#. :math:`\mu_0` (:code:`mu_0`) is vacuum permeability, or magnetic constant.
+"""
+
 from sympy import (Eq, sqrt)
 from sympy.physics.units import speed_of_light, magnetic_constant, electric_constant
-from symplyphysics import units, print_expression, convert_to
+from symplyphysics import units, convert_to
 
-# Description
-## Speed of light in vacuum is fundamental but still might be calculated from other fundamentals.
+law = Eq(speed_of_light, 1 / sqrt(electric_constant * magnetic_constant))
+r"""
+:code:`c = 1 / sqrt(epsilon_0 * mu_0)`
 
-# Law: c = 1/sqrt(e0 * u0), where
-## C is speed of light in vacuum,
-## e0 is electric constant or vacuum permittivity,
-## u0 is magnetic constant or vacuum permeability.
-
-law = Eq(speed_of_light, 1 / sqrt(magnetic_constant * electric_constant))
-
-
-def print_law() -> str:
-    return print_expression(law)
-
+Latex:
+    .. math::
+        c = \frac{1}{\sqrt{\varepsilon_0 \mu_0}}
+"""
 
 assert convert_to(law.lhs, units.meter / units.second) == convert_to(law.rhs,
     units.meter / units.second)
