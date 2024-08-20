81a2ebe70cf6d16048c837734f68656ba2af1942
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-20 23:08:06 +0400

    Add documentation

--- symplyphysics/laws/kinematics/rotational_inertia/rotational_inertia_of_particle.py
+++ symplyphysics/laws/kinematics/rotational_inertia/rotational_inertia_of_particle.py
@@ -1,40 +1,60 @@
+"""
+Rotational inertia of a particle
+================================
+
+Rotational inertia of a rotating particle is defined as the product of its mass
+and the squared radius of rotation.
+"""
+
 from sympy import Eq
 from symplyphysics import (
     units,
     Quantity,
     Symbol,
-    print_expression,
     validate_input,
     validate_output,
     symbols,
-    clone_symbol,
 )
 
-# Description
-## Rotational inertia of a rotating particle is defined as the product of its mass
-## and the squared radius of rotation.
-
-# Law: I = m * r^2
-## I - rotational inertia
-## m - particle mass
-## r - radius of rotation
-
 rotational_inertia = Symbol("rotational_inertia", units.mass * units.length**2)
-radius = Symbol("radius", units.length)
-particle_mass = clone_symbol(symbols.basic.mass, "particle_mass")
+"""
+Rotational inertia of the particle.
 
-law = Eq(rotational_inertia, particle_mass * radius**2)
+Symbol:
+    :code:`I`
+"""
+
+distance_to_axis = Symbol("distance_to_axis", units.length)
+"""
+Distance to axis, or radius of rotation.
+
+Symbol:
+    :code:`r`
+"""
+
+mass = symbols.basic.mass
+"""
+:attr:`~symplyphysics.symbols.basic.mass` of the particle.
+
+Symbol:
+    :code:`m`
+"""
+
+law = Eq(rotational_inertia, mass * distance_to_axis**2)
+r"""
+:code:`I = m * r^2`
+
+Latex:
+    .. math::
+        I = m r^2
+"""
 
 
-def print_law() -> str:
-    return print_expression(law)
-
-
-@validate_input(mass_=particle_mass, radius_=radius)
+@validate_input(mass_=mass, radius_=distance_to_axis)
 @validate_output(rotational_inertia)
 def calculate_rotational_inertia(mass_: Quantity, radius_: Quantity) -> Quantity:
     result = law.rhs.subs({
-        particle_mass: mass_,
-        radius: radius_,
+        mass: mass_,
+        distance_to_axis: radius_,
     })
     return Quantity(result)
