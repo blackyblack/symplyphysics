fbe76c7b0e66830e4c1efcb2bcb34dd818b497f5
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-20 23:24:06 +0400

    Add documentation

--- symplyphysics/laws/kinematics/rotational_inertia/geometries/solid_disk_about_central_axis.py
+++ symplyphysics/laws/kinematics/rotational_inertia/geometries/solid_disk_about_central_axis.py
@@ -1,71 +1,90 @@
+"""
+Solid disk about central axis
+=============================
+
+A solid disk (cylinder) rotates about its central axis (axis of cylindrical symmetry).
+
+**Conditions:**
+
+#. The disk is uniform.
+"""
+
 from sympy import Eq, pi
 from symplyphysics import (
     units,
     Quantity,
     Symbol,
-    print_expression,
     validate_input,
     validate_output,
     symbols,
-    clone_symbol,
 )
 from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.laws.kinematics.rotational_inertia import rotational_inertia_cylindrical_integral as integral_law
 from symplyphysics.definitions import density_from_mass_volume as density_def
 
-# Description
-## A solid disk (cylinder) rotates about its central axis (axis of cylindrical symmetry).
-
-# Law: I = 1/2 * M * R**2
-## I - rotational inertia
-## M - mass of disk
-## R - radius of disk
-
 rotational_inertia = Symbol("rotational_inertia", units.mass * units.length**2)
-radius = Symbol("radius", units.length)
-disk_mass = clone_symbol(symbols.basic.mass, "disk_mass")
+"""
+Rotational inertia of the disk.
 
-law = Eq(rotational_inertia, disk_mass * radius**2 / 2)
+Symbol:
+    :code:`I`
+"""
+
+radius = Symbol("radius", units.length)
+"""
+Radius of the disk.
+
+Symbol:
+    :code:`r`
+"""
+
+mass = symbols.basic.mass
+"""
+:attr:`~symplyphysics.symbols.basic.mass` of the disk.
+
+Symbol:
+    :code:`m`
+"""
+
+law = Eq(rotational_inertia, mass * radius**2 / 2)
+r"""
+:code:`I = 1/2 * m * r^2`
+
+Latex:
+    .. math::
+        I = \frac{1}{2} m r^2
+"""
 
 # Derive law from general integral in cylindrical coordinates
 
-length = Symbol("length", units.length)
-volume = pi * radius**2 * length
+_length = Symbol("_length", units.length)
+_volume = pi * radius**2 * _length
 
-density = density_def.definition.rhs.subs({
-    density_def.mass: disk_mass,
-    density_def.volume: volume,
+_density = density_def.definition.rhs.subs({
+    density_def.mass: mass,
+    density_def.volume: _volume,
 })
 
-rotational_inertia_derived = integral_law.law.rhs.subs({
-    integral_law.density(integral_law.radius, integral_law.polar_angle, integral_law.height):
-        density,
-    integral_law.radius_start:
-        0,
-    integral_law.radius_end:
-        radius,
-    integral_law.polar_angle_start:
-        0,
-    integral_law.polar_angle_end:
-    2 * pi,
-    integral_law.height_start:
-        0,
-    integral_law.height_end:
-        length,
+_density_applied_sym = integral_law.density(integral_law.radius, integral_law.polar_angle, integral_law.height)
+
+_rotational_inertia_derived = integral_law.law.rhs.subs({
+    _density_applied_sym: _density,
+    integral_law.radius_start: 0,
+    integral_law.radius_end: radius,
+    integral_law.polar_angle_start: 0,
+    integral_law.polar_angle_end: 2 * pi,
+    integral_law.height_start: 0,
+    integral_law.height_end: _length,
 }).doit()
 
-assert expr_equals(rotational_inertia_derived, law.rhs)
+assert expr_equals(_rotational_inertia_derived, law.rhs)
 
 
-def print_law() -> str:
-    return print_expression(law)
-
-
-@validate_input(mass_=disk_mass, radius_=radius)
+@validate_input(mass_=mass, radius_=radius)
 @validate_output(rotational_inertia)
 def calculate_rotational_inertia(mass_: Quantity, radius_: Quantity) -> Quantity:
     result = law.rhs.subs({
-        disk_mass: mass_,
+        mass: mass_,
         radius: radius_,
     })
     return Quantity(result)
