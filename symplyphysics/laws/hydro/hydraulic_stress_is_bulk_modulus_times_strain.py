4cdead87fca5e113bd861313e625d3abcd225a61
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-21 16:27:59 +0400

    Add documentation

--- symplyphysics/laws/hydro/hydraulic_stress_is_bulk_modulus_times_strain.py
+++ symplyphysics/laws/hydro/hydraulic_stress_is_bulk_modulus_times_strain.py
@@ -1,51 +1,71 @@
+"""
+Hydraulic stress is bulk modulus times strain
+=============================================
+
+When an object undergoes hydraulic compression due to a stress exerted by a surrounding liquid,
+the pressure (hydraulic stress) on the object due to the fluid is proportional to the fractional
+change in the object's volume due to that pressure and the bulk modulus of the object. Thus, bulk
+modulus of a substance is a measure of its resistance to bulk compression.
+"""
+
 from sympy import Eq
 from symplyphysics import (
     units,
+    dimensionless,
     Quantity,
     Symbol,
-    print_expression,
     validate_input,
     validate_output,
 )
 
-# Description
-## When an object undergoes hydraulic compression due to a stress exerted by a surrounding liquid,
-## the pressure (hydraulic stress) on the object due to the fluid is proportional to the fractional
-## change in the object's volume due to that pressure and the bulk modulus of the object. Thus, bulk
-## modulus of a substance is a measure of its resistance to bulk compression.
-
-# Law: p = B * (dV / V)
-## p - hydraulic stress (pressure)
-## B - bulk modulus of object
-## dV - absolute value of change in object's volume
-## V - object's initial volume
-
 hydraulic_stress = Symbol("hydraulic_stress", units.pressure)
+"""
+Hydraulic stress.
+
+Symbol:
+    :code:`p`
+"""
+
 bulk_modulus = Symbol("bulk_modulus", units.pressure)
-change_in_volume = Symbol("change_in_volume", units.volume)
-initial_volume = Symbol("initial_volume", units.volume)
+"""
+Bulk modulus of the material.
 
-law = Eq(hydraulic_stress, bulk_modulus * change_in_volume / initial_volume)
+Symbol:
+    :code:`B`
+"""
 
+fractional_volume_change = Symbol("fractional_volume_change", dimensionless)
+r"""
+:doc:`Fractional volume change <laws.quantities.fractional_change_is_change_over_initial_value>`.
 
-def print_law() -> str:
-    return print_expression(law)
+Symbol:
+    :code:`e_V`
+
+Latex:
+    :math:`e_V`
+"""
+
+law = Eq(hydraulic_stress, bulk_modulus * fractional_volume_change)
+r"""
+:code:`p = B * e_V`
+
+Latex:
+    .. math::
+        p = B e_V
+"""
 
 
 @validate_input(
     bulk_modulus_=bulk_modulus,
-    change_in_volume_=change_in_volume,
-    initial_volume_=initial_volume,
+    fractional_volume_change_=fractional_volume_change,
 )
 @validate_output(hydraulic_stress)
 def calculate_hydraulic_stress(
     bulk_modulus_: Quantity,
-    change_in_volume_: Quantity,
-    initial_volume_: Quantity,
+    fractional_volume_change_: Quantity,
 ) -> Quantity:
     result = law.rhs.subs({
         bulk_modulus: bulk_modulus_,
-        change_in_volume: change_in_volume_,
-        initial_volume: initial_volume_,
+        fractional_volume_change: fractional_volume_change_,
     })
     return Quantity(result)
