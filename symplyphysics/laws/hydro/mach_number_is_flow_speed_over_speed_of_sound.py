09390eedfb1c9f8bd26761ac151d431e7b884eeb
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-21 16:55:47 +0400

    Add documentation and rename files

---
+++ symplyphysics/laws/hydro/mach_number_is_flow_speed_over_speed_of_sound.py
@@ -0,0 +1,66 @@
+"""
+Mach number is flow speed over speed of sound
+=============================================
+
+*Mach number* is a dimensionless quantity in fluid dynamics representing
+the ratio of local flow speed divided by the speed of sound in the medium.
+"""
+
+from sympy import Eq, solve
+from symplyphysics import (
+    units,
+    Quantity,
+    Symbol,
+    validate_input,
+    validate_output,
+    dimensionless,
+    convert_to_float,
+)
+
+mach_number = Symbol("mach_number", dimensionless)
+r"""
+Local Mach number.
+
+Symbol:
+    :code:`M`
+
+Latex:
+    :math:`\text{M}`
+"""
+
+flow_speed = Symbol("flow_speed", units.flow_speed)
+r"""
+Local flow speed with respect to internal or external boundaries.
+
+Symbol:
+    :code:`u`
+"""
+
+speed_of_sound = Symbol("speed_of_sound", units.flow_speed)
+"""
+Local speed of sound in the medium.
+
+Symbol:
+    :code:`c`
+"""
+
+law = Eq(mach_number, flow_speed / speed_of_sound)
+r"""
+:code:`M = u / c`
+
+Latex:
+    .. math::
+        \text{M} = \frac{u}{c}
+"""
+
+
+@validate_input(velocity_=flow_speed, speed_of_sound_=speed_of_sound)
+@validate_output(mach_number)
+def calculate_mach_number(velocity_: Quantity, speed_of_sound_: Quantity) -> float:
+    result_expr = solve(law, mach_number, dict=True)[0][mach_number]
+    result_applied = result_expr.subs({
+        flow_speed: velocity_,
+        speed_of_sound: speed_of_sound_,
+    })
+    result = Quantity(result_applied)
+    return convert_to_float(result)
--- test/hydro/mach_number_is_flow_speed_over_speed_of_sound_test.py
+++ test/hydro/mach_number_is_flow_speed_over_speed_of_sound_test.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from pytest import fixture, raises
 from symplyphysics import assert_equal, Quantity, units, errors
-from symplyphysics.laws.hydro import mach_number
+from symplyphysics.laws.hydro import mach_number_is_flow_speed_over_speed_of_sound
 
 # Example from: https://www.omnicalculator.com/physics/mach-number
 
@@ -16,21 +16,21 @@ def test_args_fixture() -> Args:
 
 
 def test_basic_mach_number(test_args: Args) -> None:
-    result = mach_number.calculate_mach_number(test_args.velocity, test_args.speed_of_sound)
+    result = mach_number_is_flow_speed_over_speed_of_sound.calculate_mach_number(test_args.velocity, test_args.speed_of_sound)
     assert_equal(result, 0.09712)
 
 
 def test_bad_velocity(test_args: Args) -> None:
     bv = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        mach_number.calculate_mach_number(bv, test_args.speed_of_sound)
+        mach_number_is_flow_speed_over_speed_of_sound.calculate_mach_number(bv, test_args.speed_of_sound)
     with raises(TypeError):
-        mach_number.calculate_mach_number(0, test_args.speed_of_sound)
+        mach_number_is_flow_speed_over_speed_of_sound.calculate_mach_number(0, test_args.speed_of_sound)
 
 
 def test_bad_speed_of_sound(test_args: Args) -> None:
     bss = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        mach_number.calculate_mach_number(test_args.velocity, bss)
+        mach_number_is_flow_speed_over_speed_of_sound.calculate_mach_number(test_args.velocity, bss)
     with raises(TypeError):
-        mach_number.calculate_mach_number(test_args.velocity, 0)
+        mach_number_is_flow_speed_over_speed_of_sound.calculate_mach_number(test_args.velocity, 0)
