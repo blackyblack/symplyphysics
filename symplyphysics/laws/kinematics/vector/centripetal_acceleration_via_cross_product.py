835e6c33be994fa64bb630e2dec32f0e6f49323b
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-21 08:15:17 +0400

    Add documentation

--- symplyphysics/laws/kinematics/vector/centripetal_acceleration_via_cross_product.py
+++ symplyphysics/laws/kinematics/vector/centripetal_acceleration_via_cross_product.py
@@ -1,3 +1,11 @@
+"""
+Centripetal acceleration via cross product
+==========================================
+
+*Centripetal acceleration* is the acceleration of a body in a rotating coordinate system
+which is directed towards the axis of rotation.
+"""
+
 from sympy import symbols
 from symplyphysics import (
     units,
@@ -8,25 +16,50 @@ from symplyphysics import (
     QuantityVector,
     cross_cartesian_vectors,
     subtract_cartesian_vectors,
-    assert_equal,
 )
+from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.laws.kinematics.vector import centripetal_acceleration_via_vector_rejection as rejection_law
-
-# Description
-## Centripetal acceleration is the acceleration of a body in a rotating coordinate system
-## which is directed towards the axis of rotation.
-
-# Law: a_c = cross(w, cross(w, r))
-## a_c - vector of centripetal acceleration
-## w - pseudovector of angular velocity
-## r - vector of position of body
-## cross(a, b) - cross product between vectors a and b
 
 
 def centripetal_acceleration_law(
     angular_velocity_: Vector,
     position_vector_: Vector,
 ) -> Vector:
+    r"""
+    Centripetal acceleration via angular velocity and radius vector.
+
+    Law:
+        :code:`a_c = cross(w, cross(w, r))`
+
+    Latex:
+        .. math::
+            {\vec a}_\text{c} = \vec \omega \times (\vec \omega \times \vec r)
+
+    :param angular_velocity\_: pseudovector of angular velocity
+
+        Symbol: :code:`w`
+
+        Latex: :math:`\vec \omega`
+
+        Dimension: *angle* / *time*
+
+    :param position_vector\_: radius vector, or position vector
+
+        Symbol: :code:`r`
+
+        Latex: :math:`\vec r`
+
+        Dimension: *length*
+
+    :return: vector of centripetal acceleration
+
+        Symbol: :code:`a_c`
+
+        Latex: :math:`{\vec a}_\text{c}`
+
+        Dimension: *acceleration*
+    """
+
     return cross_cartesian_vectors(
         angular_velocity_,
         cross_cartesian_vectors(angular_velocity_, position_vector_),
@@ -41,7 +74,7 @@ _cross_product_result = centripetal_acceleration_law(_angular_velocity, _positio
 _rejection_result = rejection_law.centripetal_acceleration_law(_angular_velocity, _position_vector)
 _difference = subtract_cartesian_vectors(_cross_product_result, _rejection_result).simplify()
 for _component in _difference.components:
-    assert_equal(_component, 0)
+    expr_equals(_component, 0)
 
 
 @validate_input(
