e4503dae655d83889d67c6352385ecd295747dc0
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-22 21:40:35 +0400

    Add documentation

--- symplyphysics/laws/kinematics/vector/coriolis_acceleration.py
+++ symplyphysics/laws/kinematics/vector/coriolis_acceleration.py
@@ -1,3 +1,17 @@
+r"""
+Coriolis acceleration
+=====================
+
+Imagine two reference frames, one of which is fixed (:math:`S`) and the other one is moving (:math:`S'`). When
+the body is moving within a rotating coordinate system, its path deflects due to the appearance
+of the Coriolis acceleration on it. The object does not actually deviate from its path per se
+but it appears to do so because of the motion of the coordinate system.
+
+Suppose a reference frame :math:`S'` is fixed to a rotating body :math:`A` (e.g. Earth), so that frame :math:`S'` rotates w.r.t.
+another static reference frame :math:`S`. The Coriolis acceleration is the acceleration another body :math:`B` has when
+moving within rotating reference frame :math:`S'`, so it is essentially zero for objects at rest in :math:`S'`.
+"""
+
 from symplyphysics import (
     units,
     angle_type,
@@ -9,26 +23,45 @@ from symplyphysics import (
     cross_cartesian_vectors,
 )
 
-# Description
-## Suppose two reference frames, one of which is fixed (S) and the other one is moving (S'). When
-## the body is moving within a rotating coordinate system, its path deflects due to the appearance
-## of the Coriolis acceleration on it. The object does not actually deviate from its path per se
-## but it appears to do so because of the motion of the coordinate system.
-
-## Suppose a reference frame S' is fixed to a rotating body A (e.g. Earth), so that frame S' rotates w.r.t.
-## another static reference frame S. The Coriolis acceleration is the acceleration another body B has when
-## moving within rotating reference frame S', so it is essentially zero for objects at rest in S'.
-
-# Law: a_cor = 2 * cross(v_rel, w)
-## a_cor - vector of Coriolis acceleration of body B in S'
-## v_rel - vector of velocity of body B in moving frame S'
-## w - pseudovector of angular velocity of rotation of moving frame S'
-
-
 def coriolis_acceleration_law(
     velocity_: Vector,
     angular_velocity_: Vector,
 ) -> Vector:
+    r"""
+    Coriolis acceleration via relative velocity of body and angular velocity of rotation of :math:`S'`.
+
+    Law:
+        :code:`a_cor = 2 * cross(v_rel, w)`
+
+    Latex:
+        .. math::
+            {\vec a}_\text{cor} = 2 ({\vec v}_\text{rel} \times \vec \omega)
+
+    :param velocity\_: velocity of body relative to :math:`S'`
+
+        Symbol: :code:`v_rel`
+
+        Latex: :math:`{\vec v}_\text{rel}`
+
+        Dimension: *velocity*
+
+    :param angular\_velocity\_: angular velocity of rotation of :math:`S'` relative to :math:`S`
+
+        Symbol: :code:`w`
+
+        Latex: :math:`\vec \omega`
+
+        Dimension: *angle* / *time*
+
+    :return: Coriolis acceleration of body in :math:`S'`
+
+        Symbol: :code:`a_cor`
+
+        Latex: :math:`{\vec a}_\text{cor}`
+
+        Dimension: *acceleration*
+    """
+
     return scale_vector(2, cross_cartesian_vectors(velocity_, angular_velocity_))
 
 
