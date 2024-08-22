a14c906881cc108278826e92cfeafc8b7d0e63ca
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-22 16:03:11 +0400

    Add documentation and rename files

--- test/hydro/efflux_speed_via_hydrostatic_pressure_and_density_test.py
+++ test/hydro/efflux_speed_via_hydrostatic_pressure_and_density_test.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from pytest import fixture, raises
 from symplyphysics import (assert_equal, units, Quantity, errors)
-from symplyphysics.laws.hydro import velocity_of_liquid_flowing_out_of_pipe_from_pressure_and_density as velocity_law
+from symplyphysics.laws.hydro import efflux_speed_via_hydrostatic_pressure_and_density as velocity_law
 
 # Description
 ## The density of water is 997 [kilogram / meter^3]. At a pressure of 5e5 pascal,
