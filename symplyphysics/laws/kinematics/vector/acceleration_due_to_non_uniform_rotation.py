1881051347bfc683f60f69d38ba42df77c72166e
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-22 21:23:03 +0400

    Add documentation

--- symplyphysics/laws/kinematics/vector/acceleration_due_to_non_uniform_rotation.py
+++ symplyphysics/laws/kinematics/vector/acceleration_due_to_non_uniform_rotation.py
@@ -1,3 +1,12 @@
+r"""
+Acceleration due to non-uniform rotation
+========================================
+
+Imagine two reference frames, one of which is fixed (:math:`S`) and the other is moving (:math:`S'`). When :math:`S'` rotates
+around :math:`S` in a non-uniform way, the acceleration of some body :math:`B` in :math:`S` has a component corresponding to
+that non-uniform rotation of :math:`S'`. It is part of the transfer acceleration of body :math:`B` in :math:`S`.
+"""
+
 from sympy import Expr, abc
 from symplyphysics import (
     units,
@@ -12,27 +21,55 @@ from symplyphysics import (
 )
 from symplyphysics.core.vectors.arithmetics import diff_cartesian_vector
 
-# Description
-## Suppose two reference frames, one of which is fixed (S) and the other is moving (S'). When S' rotates
-## around S in a non-uniform way, the acceleration of some body B in S has a component corresponding to
-## that non-uniform rotation of S'. It is part of the transfer acceleration of body B in S.
-
-# Law: a_rot = cross(dw/dt, r)
-## a_rot - vector of body B's acceleration due to non-uniform rotation of S'
-## w - pseudovector of angular velocity of rotation of S'
-## d/dt - time derivative
-## r - position vector of body B within S'
-## cross(a, b) - vector of cross product between vectors a and b
-
-
 def non_uniform_rotation_acceleration_law(
     angular_velocity_: Vector,
     time_: Expr,
-    position_vector_: Vector,
+    radius_vector_: Vector,
 ) -> Vector:
+    r"""
+    Acceleration due to non-uniform rotation.
+
+    Law:
+        :code:`a_rot = cross(Derivative(w(t), t), r)`
+
+    Latex:
+        .. math::
+            {\vec a}_\text{rot} = \frac{d \vec \omega}{d t} \times \vec r
+
+    :param angular_velocity\_: angular velocity as a function of time
+
+        Symbol: :code:`w(t)`
+
+        Latex: :math:`\vec \omega(t)`
+
+        Dimension: *angle* / *time*
+
+    :param time\_: time
+
+        Symbol: :code:`t`
+
+        Dimension: *time*
+
+    :param radius_vector\_: radius vector, or position vector, of body
+
+        Symbol: :code:`r`
+
+        Latex: :math:`\vec r`
+
+        Dimension: *length*
+
+    :return: acceleration due to non-uniform rotation
+
+        Symbol: :code:`a_rot`
+
+        Latex: :math:`{\vec a}_\text{rot}`
+
+        Dimension: *acceleration*
+    """
+
     return cross_cartesian_vectors(
         diff_cartesian_vector(angular_velocity_, time_),
-        position_vector_,
+        radius_vector_,
     )
 
 
@@ -45,7 +82,7 @@ def non_uniform_rotation_acceleration_law(
 def calculate_non_uniform_rotation_acceleration(
     angular_velocity_change_: QuantityVector,
     time_change_: Quantity,
-    position_vector_: QuantityVector,
+    radius_vector_: QuantityVector,
 ) -> QuantityVector:
     time_ = abc.t
 
@@ -57,7 +94,7 @@ def calculate_non_uniform_rotation_acceleration(
     result_vector = non_uniform_rotation_acceleration_law(
         angular_velocity_,
         time_,
-        position_vector_.to_base_vector(),
+        radius_vector_.to_base_vector(),
     )
 
     return QuantityVector.from_base_vector(result_vector)
