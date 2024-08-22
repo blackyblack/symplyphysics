61234ebdcf92c7c9284150f3677f41577dd2a648
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-22 21:54:13 +0400

    Add documentation

--- symplyphysics/laws/kinematics/vector/velocity_relative_to_reference_frame.py
+++ symplyphysics/laws/kinematics/vector/velocity_relative_to_reference_frame.py
@@ -1,3 +1,11 @@
+"""
+Velocity relative to reference frame
+====================================
+
+For any reference frame, whether it is inertial or not, the motion relative to it can be described using
+the position vector relative to that frame's origin.
+"""
+
 from sympy import Expr, symbols
 from symplyphysics import (
     units,
@@ -15,29 +23,92 @@ from symplyphysics.core.vectors.arithmetics import (
     integrate_cartesian_vector,
 )
 
-# Description
-## For any reference frame, whether it is inertial or not, the motion relative to it can be described using
-## the position vector relative to that frame's origin.
-
-# Law: v_rel = (dx/dt, dy/dt, dz/dt)
-## v_rel - vector of velocity relative to reference frame S
-## r = (x, y, z) - vector of position relative to frame S
-## d/dt - time derivative
-
-
 def relative_velocity_law(
     position_: Vector,
     time_: Expr,
 ) -> Vector:
+    r"""
+    Velocity relative to :math:`S`.
+
+    Law:
+        :code:`v_rel = Derivative(r(t), t)`
+
+    Latex:
+        .. math::
+            {\vec v}_\text{rel} = \frac{d \vec r}{d t}
+
+    :param position\_: radius vector, or position vector, of body in :math:`S` as a function of time
+
+        Symbol: :code:`r(t)`
+
+        Latex: :math:`\vec r(t)`
+
+        Dimension: *length*
+
+    :param time\_: time
+
+        Symbol: :code:`t`
+
+        Dimension: *time*
+
+    :return: velocity relative to :math:`S`
+
+        Symbol: :code:`v_rel`
+
+        Latex: :math:`{\vec v}_\text{rel}`
+
+        Dimension: *velocity*
+    """
+
     return diff_cartesian_vector(position_, time_)
 
 
-# r = r0 + Integral(v(t), t)
 def relative_position_law(
     initial_position_: Vector,
     velocity_: Vector,
     time_: Expr,
 ) -> Vector:
+    r"""
+    Final position via initial position and velocity as a function of time.
+
+    Law:
+        :code:`r = r_0 + Integral(v_rel(t), t)`
+
+    Latex:
+        .. math::
+            \vec r = {\vec r}_0 + \int {\vec v}_\text{rel}(t) dt
+
+    :param initial\_position\_: position vector in :math:`S` at :math:`t = 0`
+
+        Symbol: :code:`r_0`
+
+        Latex: :math:`{\vec r}_0`
+
+        Dimension: *length*
+
+    :param velocity\_: velocity relative to :math:`S` as a function of time
+
+        Symbol: :code:`v_rel(t)`
+
+        Latex: :math:`{\vec v}_\text{rel}`
+
+        Dimension: *velocity*
+
+    :param time\_: time
+
+        Symbol: :code:`t`
+
+        Dimension: *time*
+
+    :return: position vector in :math:`S` at time :math:`t`
+
+        Symbol: :code:`r`
+
+        Latex: :math:`\vec r`
+
+        Dimension: *length*
+    """
+
     return add_cartesian_vectors(
         initial_position_,
         integrate_cartesian_vector(velocity_, time_),
