03f8e39176c6578595834a111877714da0e033a3
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-22 16:36:47 +0400

    Add documentation

--- symplyphysics/laws/hydro/shear_stress_is_shear_force_over_area.py
+++ symplyphysics/laws/hydro/shear_stress_is_shear_force_over_area.py
@@ -1,3 +1,12 @@
+"""
+Shear stress is shear force over area
+=====================================
+
+Shear stress is the component of stress coplanar with the material cross section on which it acts,
+that is, it lies in the same plane as the area it is applied to. It is opposed to normal stress,
+which arises from the force vector component perpendicular to the material cross section.
+"""
+
 from sympy import Eq, solve
 from symplyphysics import (
     clone_symbol,
@@ -5,38 +14,53 @@ from symplyphysics import (
     units,
     Quantity,
     Symbol,
-    print_expression,
     validate_input,
     validate_output,
 )
 
-# Description
-## Shear stress is the component of stress coplanar with the material cross section on which it acts,
-## that is, it lies in the same plane as the area it is applied to. It is opposed to normal stress,
-## which arises from the force vector component perpendicular to the material cross section.
-
-# Law: tau = F/A
-## tau - shear stress
-## F - force applied
-## A - area of the material face parallel to the applied force vector
-
 shear_stress = Symbol("shear_stress", units.pressure)
-force_applied = clone_symbol(symbols.dynamics.force, "force_applied")
+r"""
+Shear stress.
+
+Symbol:
+    :code:`tau`
+
+Latex:
+    :math:`\tau`
+"""
+
+shear_force = clone_symbol(symbols.dynamics.force, "shear_force")
+"""
+Shear force applied.
+
+Symbol:
+    :code:`F`
+"""
+
 area = Symbol("area", units.area)
+"""
+Area of the material face parallel to the shear force applied.
 
-law = Eq(shear_stress, force_applied / area)
+Symbol:
+    :code:`A`
+"""
+
+law = Eq(shear_stress, shear_force / area)
+r"""
+:code:`tau = F / A`
+
+Latex:
+    .. math::
+        \tau = \frac{F}{A}
+"""
 
 
-def print_law() -> str:
-    return print_expression(law)
-
-
-@validate_input(force_applied_=force_applied, area_=area)
+@validate_input(force_applied_=shear_force, area_=area)
 @validate_output(shear_stress)
 def calculate_shear_stress(force_applied_: Quantity, area_: Quantity) -> Quantity:
     solved = solve(law, shear_stress)[0]
     result = solved.subs({
-        force_applied: force_applied_,
+        shear_force: force_applied_,
         area: area_,
     })
     return Quantity(result)
