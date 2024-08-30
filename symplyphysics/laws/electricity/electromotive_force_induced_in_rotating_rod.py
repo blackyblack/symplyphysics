09a4ff7899507def09391e7b63bc1925338204a4
Aleksandr Slatov <alexslatov@gmail.com>
2024-08-30 09:01:03 +0400

    Fix naming

--- symplyphysics/laws/electricity/electromotive_force_induced_in_rotating_rod.py
+++ symplyphysics/laws/electricity/electromotive_force_induced_in_rotating_rod.py
@@ -5,7 +5,7 @@ Electromotive force induced in rotating rod
 Let a rod rotate in a uniform magnetic field. The plane of rotation is perpendicular
 to the magnetic field lines. The axis of rotation passes through one of the ends of
 the rod. Then the electromotive force induced at the ends of the rod depends on the
-magnitude of the magnetic induction, the rotation frequency and the length of the rod.
+magnitude of the magnetic flux density, the rotation frequency and the length of the rod.
 
 **Conditions:**
 
@@ -36,9 +36,9 @@ Latex:
     :math:`\mathcal{E}`
 """
 
-magnetic_field = Symbol("magnetic_field", units.magnetic_flux_density)
+magnetic_flux_density = Symbol("magnetic_flux_density", units.magnetic_flux_density)
 """
-Value of the magnetic field.
+Magnitude of magnetic flux density.
 
 Symbol:
     :code:`B`
@@ -63,7 +63,7 @@ Symbol:
     :code:`l`
 """
 
-law = Eq(electromotive_force, (1 / 2) * magnetic_field * angular_frequency * length**2)
+law = Eq(electromotive_force, (1 / 2) * magnetic_flux_density * angular_frequency * length**2)
 r"""
 :code:`E = 1/2 * B * w * l^2`
 
@@ -73,7 +73,7 @@ Latex:
 """
 
 
-@validate_input(magnetic_induction_=magnetic_field,
+@validate_input(magnetic_induction_=magnetic_flux_density,
     rotation_frequency_=angular_frequency,
     rod_length_=length)
 @validate_output(electromotive_force)
@@ -81,7 +81,7 @@ def calculate_voltage(magnetic_induction_: Quantity, rotation_frequency_: Quanti
     rod_length_: Quantity) -> Quantity:
     result_expr = solve(law, electromotive_force, dict=True)[0][electromotive_force]
     result_expr = result_expr.subs({
-        magnetic_field: magnetic_induction_,
+        magnetic_flux_density: magnetic_induction_,
         angular_frequency: rotation_frequency_,
         length: rod_length_
     })
