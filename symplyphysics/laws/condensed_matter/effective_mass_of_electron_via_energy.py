7d6ee9dd5fdeede67e7c5b9eaba59a4528dcd52e
Aleksandr Slatov <alexslatov@gmail.com>
2024-09-06 17:37:45 +0400

    Fix issues

--- symplyphysics/laws/condensed_matter/effective_mass_of_electron_via_energy.py
+++ symplyphysics/laws/condensed_matter/effective_mass_of_electron_via_energy.py
@@ -30,7 +30,7 @@ from symplyphysics import (
     validate_output,
 )
 
-effective_mass = clone_symbol(symbols.basic.mass, "effective_mass", display_symbol="m_eff", display_latex=r"m_\text{eff}")
+effective_mass = clone_symbol(symbols.basic.mass, display_symbol="m_eff", display_latex=r"m_\text{eff}")
 r"""
 Effective mass of the electron.
 """
