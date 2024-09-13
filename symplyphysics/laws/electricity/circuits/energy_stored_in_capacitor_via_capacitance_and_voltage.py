cb15662409b34e7ef5ba47a568670a6eb6af7887
Aleksandr Slatov <alexslatov@gmail.com>
2024-09-13 16:20:50 +0400

    Refactor documentation

--- symplyphysics/laws/electricity/circuits/energy_stored_in_capacitor_via_capacitance_and_voltage.py
+++ symplyphysics/laws/electricity/circuits/energy_stored_in_capacitor_via_capacitance_and_voltage.py
@@ -5,41 +5,34 @@ Energy stored in capacitor via capacitance and voltage
 Capacitors store energy in the electric field when they are charged.
 """
 
-from sympy import (Eq, solve)
-from symplyphysics import (units, Quantity, Symbol, validate_input,
-    validate_output)
+from sympy import Eq, solve
+from symplyphysics import (
+    Quantity,
+    validate_input,
+    validate_output,
+    symbols,
+)
 
-energy = Symbol("energy", units.energy)
+energy = symbols.work
 """
 Energy stored in the electric field.
-
-Symbol:
-    :code:`W`
 """
 
-capacitance = Symbol("capacitance", units.capacitance)
+capacitance = symbols.capacitance
 """
 Capacitance of the capacitor.
-
-Symbol:
-    :code:`C`
 """
 
-voltage = Symbol("voltage", units.voltage)
+voltage = symbols.voltage
 """
 Voltage across the capacitor.
-
-Symbol:
-    :code:`V`
 """
 
 law = Eq(energy, capacitance * voltage**2 / 2)
-r"""
-:code:`W = 1/2 * C * V^2`
+"""
+:laws:symbol::
 
-Latex:
-    .. math::
-        W = \frac{1}{2} C V^2
+:laws:latex::
 """
 
 
