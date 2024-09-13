063c172d7fc712a50f7eca8492cc81f351033faf
Aleksandr Slatov <alexslatov@gmail.com>
2024-09-13 16:54:25 +0400

    Fix documentation

--- symplyphysics/laws/electricity/electromotive_force_induced_in_rotating_rod.py
+++ symplyphysics/laws/electricity/electromotive_force_induced_in_rotating_rod.py
@@ -7,16 +7,16 @@ to the magnetic field lines. The axis of rotation passes through one of the ends
 the rod. Then the electromotive force induced at the ends of the rod depends on the
 magnitude of the magnetic flux density, the rotation frequency and the length of the rod.
 
-**Links:**
-
-#. `Example 13.4.2 <https://phys.libretexts.org/Bookshelves/University_Physics/University_Physics_(OpenStax)/University_Physics_II_-_Thermodynamics_Electricity_and_Magnetism_(OpenStax)/13%3A_Electromagnetic_Induction/13.04%3A_Motional_Emf>`_.
-
 **Conditions:**
 
 #. The angular velocity of the rod is parallel to the magnetic field. This means that
    the rod is rotating in a plane perpendicular to the magnetic field.
 #. The magnetic field is uniform.
 #. The angular velocity of the rod is constant.
+
+**Links:**
+
+#. `Example 13.4.2 <https://phys.libretexts.org/Bookshelves/University_Physics/University_Physics_(OpenStax)/University_Physics_II_-_Thermodynamics_Electricity_and_Magnetism_(OpenStax)/13%3A_Electromagnetic_Induction/13.04%3A_Motional_Emf>`_.
 """
 
 from sympy import (Eq, solve)
@@ -47,7 +47,7 @@ length = symbols.length
 Length of the rod.
 """
 
-law = Eq(electromotive_force, (1 / 2) * magnetic_flux_density * angular_frequency * length**2)
+law = Eq(electromotive_force, magnetic_flux_density * angular_frequency * length**2 / 2)
 """
 :laws:symbol::
 
