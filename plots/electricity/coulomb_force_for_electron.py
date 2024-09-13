c5d5f3ed29b3330134678af29d7b195ad65bee34
Aleksandr Slatov <alexslatov@gmail.com>
2024-09-13 18:19:12 +0400

    Fix issue

--- plots/electricity/coulomb_force_for_electron.py
+++ plots/electricity/coulomb_force_for_electron.py
@@ -2,12 +2,12 @@
 from sympy import Symbol
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
-from symplyphysics import print_expression, units
+from symplyphysics import print_expression, quantities
 from symplyphysics.core.convert import convert_to_si
 from symplyphysics.laws.electricity import electrostatic_force_via_charges_and_distance as coulomb_law
 
 CHARGE_OF_ELECTRON = -1.6 * 1E-19
-VACUUM_PERMITTIVITY = convert_to_si(units.vacuum_permittivity)
+VACUUM_PERMITTIVITY = convert_to_si(quantities.vacuum_permittivity)
 
 print(f"Formula is:\n{print_expression(coulomb_law.law)}")
 
@@ -17,7 +17,7 @@ force_equation = coulomb_law.law.subs({
     coulomb_law.first_charge: CHARGE_OF_ELECTRON,
     coulomb_law.second_charge: CHARGE_OF_ELECTRON,
     coulomb_law.distance: distance,
-    units.vacuum_permittivity: VACUUM_PERMITTIVITY,
+    quantities.vacuum_permittivity: VACUUM_PERMITTIVITY,
 }).rhs
 
 p1 = plot(force_equation, (distance, 1, 5),
