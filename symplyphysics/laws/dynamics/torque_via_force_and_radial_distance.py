2a7723e5667d7b7bdbf828383515aee6f090a152
Aleksandr Slatov <alexslatov@gmail.com>
2024-09-26 00:34:41 +0400

    Fix documentation

--- symplyphysics/laws/dynamics/torque_via_force_and_radial_distance.py
+++ symplyphysics/laws/dynamics/torque_via_force_and_radial_distance.py
@@ -13,12 +13,9 @@ Torque via force and radial distance
 from sympy import Eq, solve, sin, symbols as sympy_symbols, sqrt
 from symplyphysics import (
     symbols,
-    units,
     Quantity,
-    Symbol,
     validate_input,
     validate_output,
-    angle_type,
     Vector,
     dot_vectors,
     vector_magnitude,
@@ -27,15 +24,9 @@ from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.core.symbols.quantities import scale_factor
 from symplyphysics.laws.dynamics.vector import torque_vector_of_twisting_force as torque_vector_def
 
-torque = Symbol("torque", units.force * units.length)
-r"""
-The magnitude of the torque applied at the given point.
-
-Symbol:
-    :code:`tau`
-
-Latex:
-    :math:`\tau`
+torque = symbols.torque
+"""
+The magnitude of the :symbols:`torque` applied at the given point.
 """
 
 force = symbols.force
@@ -43,56 +34,45 @@ force = symbols.force
 The magnitude of the :symbols:`force` exerted on the given point.
 """
 
-radial_distance = Symbol("radial_distance", units.length)
+radial_distance = symbols.distance_to_axis
 """
-The distance to axis from the given point.
-
-Symbol:
-    :code:`r`
+The :symbols:`distance_to_axis` from the given point.
 """
 
-angle_between_vectors = Symbol("angle_between_vectors", angle_type)
-r"""
-The angle between the position vector of the given point and the force vector.
-
-Symbol:
-    :code:`phi`
-
-Latex:
-    :math:`\varphi`
+angle_between_vectors = symbols.angle
+"""
+The :symbols:`angle` between the position vector of the given point and the force vector.
 """
 
 law = Eq(torque, radial_distance * force * sin(angle_between_vectors))
-r"""
-:code:`tau = F * r * sin(phi)`
+"""
+:laws:symbol::
 
-Latex:
-    .. math::
-        \tau = F r \sin{\varphi}
+:laws:latex::
 """
 
 # Derive law from its vector counterpart.
-force_vector = Vector(sympy_symbols("force_x:z", real=True))
-position_vector = Vector(sympy_symbols("x:z", real=True))
+_force_vector = Vector(sympy_symbols("force_x:z", real=True))
+_position_vector = Vector(sympy_symbols("x:z", real=True))
 
-torque_vector_derived = torque_vector_def.torque_definition(force_vector, position_vector)
-torque_magnitude_derived = vector_magnitude(torque_vector_derived)
+_torque_vector_derived = torque_vector_def.torque_definition(_force_vector, _position_vector)
+_torque_magnitude_derived = vector_magnitude(_torque_vector_derived)
 
-force_magnitude = vector_magnitude(force_vector)
-position_magnitude = vector_magnitude(position_vector)
+_force_magnitude = vector_magnitude(_force_vector)
+_position_magnitude = vector_magnitude(_position_vector)
 
 # Use the definition of dot product (a, b) = |a| * |b| * cos(a, b) to find the sine of angle_between_vectors between vectors
-cosine_of_angle_in_between = (dot_vectors(force_vector, position_vector) / force_magnitude /
-    position_magnitude)
-sine_of_angle_in_between = sqrt(1 - cosine_of_angle_in_between**2)
+_cosine_of_angle_in_between = (dot_vectors(_force_vector, _position_vector) / _force_magnitude /
+    _position_magnitude)
+_sine_of_angle_in_between = sqrt(1 - _cosine_of_angle_in_between**2)
 
-torque_magnitude_from_law = solve(law, torque)[0].subs({
-    force: force_magnitude,
-    radial_distance: position_magnitude,
-    sin(angle_between_vectors): sine_of_angle_in_between,
+_torque_magnitude_from_law = solve(law, torque)[0].subs({
+    force: _force_magnitude,
+    radial_distance: _position_magnitude,
+    sin(angle_between_vectors): _sine_of_angle_in_between,
 })
 
-assert expr_equals(torque_magnitude_derived, torque_magnitude_from_law)
+assert expr_equals(_torque_magnitude_derived, _torque_magnitude_from_law)
 
 
 @validate_input(force_=force, distance_to_axis_=radial_distance, angle_=angle_between_vectors)
