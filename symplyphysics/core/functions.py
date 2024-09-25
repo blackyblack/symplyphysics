d77aded1ecfc0f9fa93708a543db543a58f26108
Aleksandr Slatov <alexslatov@gmail.com>
2024-09-26 03:21:59 +0400

    Fix types

--- symplyphysics/core/functions.py
+++ symplyphysics/core/functions.py
@@ -1,16 +1,17 @@
+from typing import Any
 from sympy import Expr, log as sym_log, E, sqrt as sym_sqrt, Mul as SymMul
 
 
-def log(expr: Expr, base: Expr = E, **kwargs) -> Expr:
+def log(expr: Expr, base: Expr = E, **kwargs: Any) -> Expr:
     kwargs.setdefault("evaluate", False)
     return sym_log(expr, base, **kwargs)
 
 
-def sqrt(expr: Expr, **kwargs) -> Expr:
+def sqrt(expr: Expr, **kwargs: Any) -> Expr:
     kwargs.setdefault("evaluate", False)
     return sym_sqrt(expr, **kwargs)
 
 
-def mul(*exprs: Expr, **kwargs) -> Expr:
+def mul(*exprs: Expr, **kwargs: Any) -> Expr:
     kwargs.setdefault("evaluate", False)
     return SymMul(*exprs, **kwargs)
