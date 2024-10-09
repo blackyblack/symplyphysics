b07dc981845353c20ba6bf6a098efcea2ba9dd75
Aleksandr Slatov <alexslatov@gmail.com>
2024-10-09 23:22:14 +0400

    Fix documentation

--- symplyphysics/laws/optics/film_thickness_for_minimum_interference_from_angle_refraction_refractive_index_wavelength.py
+++ symplyphysics/laws/optics/film_thickness_for_minimum_interference_from_angle_refraction_refractive_index_wavelength.py
@@ -1,46 +1,71 @@
-from sympy import (Eq, solve, cos)
-from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
-    validate_output, dimensionless, angle_type)
+"""
+Film thickness for minimum interference
+=======================================
 
-# Description
-## Interference in thin films is a phenomenon that occurs as a result of the separation of a light beam when
-## reflected from the upper and lower boundaries of a thin film. As a result, there are two light waves that
-## can interfere.
-## The order of interference indicates the number of whole wavelengths. When the difference in the path of the
-## beam reflected from the upper boundary of the film and the beam reflected from the lower boundary of the
-## film is equal to an integer number of half-waves, then these two waves enter the observation point and demonstrate
-## minimum interference.
-## Order of interference can be chosen arbitrarily, to achieve the desired thin film thickness.
-## Angle of refraction is angle between refracted ray and the normal.
-## Thickness that gives interference minimum at a certain point on the interference pattern depends on the
-## angle of refraction, the order of interference, and the refractive index of the film.
+Interference in thin films is a phenomenon that occurs as a result of the separation of a light beam when
+reflected from the upper and lower boundaries of a thin film. As a result, there are two light waves that
+can interfere.
+The order of interference indicates the number of whole wavelengths. When the difference in the path of the
+beam reflected from the upper boundary of the film and the beam reflected from the lower boundary of the
+film is equal to an integer number of half-waves, then these two waves enter the observation point and demonstrate
+minimum interference.
+Order of interference can be chosen arbitrarily, to achieve the desired thin film thickness.
+Angle of refraction is angle between refracted ray and the normal.
+Thickness that gives interference minimum at a certain point on the interference pattern depends on the
+angle of refraction, the order of interference, and the refractive index of the film.
 
-## Law is: h = k * L / (2 * n * cos(O)), where
-## h - film thickness,
-## k - order of interference,
-## L - wavelength in vacuum,
-## n - refractive index of film
-## O - angle of refraction.
+..
+    TODO rename file
+    TODO add link
+"""
 
-film_thickness = Symbol("film_thickness", units.length)
+from sympy import Eq, solve, cos
+from symplyphysics import (
+    Quantity,
+    validate_input,
+    validate_output,
+    symbols,
+    clone_as_symbol,
+)
 
-wavelength = Symbol("wavelength", units.length)
-order_interference = Symbol("order_interference", dimensionless)
-angle_refraction = Symbol("angle_refraction", angle_type)
-refractive_index = Symbol("refractive_index", dimensionless)
+film_thickness = symbols.thickness
+"""
+Film :symbols:`thickness`.
+"""
+
+wavelength = symbols.wavelength
+"""
+Light :symbols:`wavelength`
+"""
+
+order_interference = clone_as_symbol(symbols.positive_number, display_symbol="k", display_latex="k")
+"""
+Order of interference. See :symbols:`positive_number`.
+"""
+
+refraction_angle = symbols.angle
+"""
+:symbols:`angle` of refraction
+"""
+
+relative_refractive_index = symbols.relative_refractive_index
+"""
+:symbols:`relative_refractive_index` of the film.
+"""
 
 law = Eq(film_thickness,
-    order_interference * wavelength / (2 * refractive_index * cos(angle_refraction)))
+    order_interference * wavelength / (2 * relative_refractive_index * cos(refraction_angle)))
+"""
+:laws:symbol::
 
-
-def print_law() -> str:
-    return print_expression(law)
+:laws:latex::
+"""
 
 
 @validate_input(wavelength_=wavelength,
     order_interference_=order_interference,
-    refractive_index_=refractive_index,
-    angle_refraction_=angle_refraction)
+    refractive_index_=relative_refractive_index,
+    angle_refraction_=refraction_angle)
 @validate_output(film_thickness)
 def calculate_film_thickness(wavelength_: Quantity, order_interference_: int,
     refractive_index_: float, angle_refraction_: float | Quantity) -> Quantity:
@@ -51,7 +76,7 @@ def calculate_film_thickness(wavelength_: Quantity, order_interference_: int,
     result_expr = result_expr.subs({
         wavelength: wavelength_,
         order_interference: order_interference_,
-        refractive_index: refractive_index_,
-        angle_refraction: angle_refraction_,
+        relative_refractive_index: refractive_index_,
+        refraction_angle: angle_refraction_,
     })
     return Quantity(result_expr)
