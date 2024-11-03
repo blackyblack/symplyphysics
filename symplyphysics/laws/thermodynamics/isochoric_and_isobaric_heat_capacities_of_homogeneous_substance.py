b2642e3464104961a63e749e56422cc5d0159ad4
Aleksandr Slatov <alexslatov@gmail.com>
2024-11-04 00:25:36 +0400

    Add documentation

--- symplyphysics/laws/thermodynamics/isochoric_and_isobaric_heat_capacities_of_homogeneous_substance.py
+++ symplyphysics/laws/thermodynamics/isochoric_and_isobaric_heat_capacities_of_homogeneous_substance.py
@@ -2,49 +2,41 @@
 Isochoric and isobaric heat capacities of homogeneous substance
 ===============================================================
 
-The *Mayer's relation* is the relation between heat capacity at constant pressure and heat
+The **Mayer's relation** is the relation between heat capacity at constant pressure and heat
 capacity at constant volume. In the current form it is applicable to any homogeneous substance,
 not just ideal gases.
 """
 
 from sympy import Eq, solve
 from symplyphysics import (
-    units,
     Quantity,
-    Symbol,
     validate_input,
     validate_output,
     symbols,
+    clone_as_symbol,
 )
 
-isobaric_heat_capacity = Symbol("isobaric_heat_capacity", units.energy / units.temperature)
-r"""
-Heat capacity at constant pressure.
-
-Symbol:
-    :code:`C_p`
-
-Latex:
-    :math:`C_p`
+isobaric_heat_capacity = clone_as_symbol(
+    symbols.heat_capacity,
+    display_symbol="C_p",
+    display_latex="C_p",
+)
+"""
+:symbols:`heat_capacity` at constant :symbols:`pressure`.
 """
 
-isochoric_heat_capacity = Symbol("isochoric_heat_capacity", units.energy / units.temperature)
-r"""
-Heat capacity at constant volume.
-
-Symbol:
-    :code:`C_V`
-
-Latex:
-    :math:`C_V`
+isochoric_heat_capacity = clone_as_symbol(
+    symbols.heat_capacity,
+    display_symbol="C_V",
+    display_latex="C_V",
+)
+"""
+:symbols:`heat_capacity` at constant :symbols:`volume`.
 """
 
-volume = Symbol("volume", units.volume)
+volume = symbols.volume
 """
-Volume of the substance.
-
-Symbol:
-    :code:`V`
+:symbols:`volume` of the substance.
 """
 
 temperature = symbols.temperature
@@ -52,39 +44,32 @@ temperature = symbols.temperature
 :symbols:`temperature` of the substance.
 """
 
-thermal_expansion_coefficient = Symbol("thermal_expansion_coefficient", 1 / units.temperature)
-r"""
-:doc:`Thermal volumetric expansion coefficient <definitions.volumetric_coefficient_of_thermal_expansion>`
-of the substance.
-
-Symbol:
-    :code:`alpha_V`
-
-Latex:
-    :math:`\alpha_V`
+thermal_expansion_coefficient = clone_as_symbol(
+    symbols.thermal_expansion_coefficient,
+    subscript="V",
+)
+"""
+:symbols:`thermal_expansion_coefficient` of the substance. Also see :doc:`Thermal volumetric expansion
+coefficient <definitions.volumetric_coefficient_of_thermal_expansion>`.
 """
 
-isothermal_compressibility = Symbol("isothermal_compressibility", 1 / units.pressure)
-r"""
-:doc:`Isothermal compressibility <definitions.thermodynamic_compressibility>` of the substance.
-
-Symbol:
-    :code:`beta_T`
-
-Latex:
-    :math:`\beta_T`
+isothermal_compressibility = clone_as_symbol(
+    symbols.thermodynamic_compressibility,
+    subscript="T",
+)
+"""
+:symbols:`thermodynamic_compressibility` of the substance. Also see :doc:`Isothermal compressibility
+<definitions.thermodynamic_compressibility>`.
 """
 
 law = Eq(
     isobaric_heat_capacity - isochoric_heat_capacity,
     volume * temperature * thermal_expansion_coefficient**2 / isothermal_compressibility,
 )
-r"""
-:code:`C_p - C_V = V * T * (alpha_V)^2 / beta_T`
+"""
+:laws:symbol::
 
-Latex:
-    .. math::
-        C_p - C_V = V T \frac{\alpha_V^2}{\beta_T}
+:laws:latex::
 """
 
 
