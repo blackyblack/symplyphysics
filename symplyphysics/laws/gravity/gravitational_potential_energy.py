0ae5fa79600fc5c97133eba489e5bc9d31fbde98
Aleksandr Slatov <alexslatov@gmail.com>
2024-11-14 12:24:39 +0400

    Add documentation

--- symplyphysics/laws/gravity/gravitational_potential_energy.py
+++ symplyphysics/laws/gravity/gravitational_potential_energy.py
@@ -1,39 +1,45 @@
+"""
+Gravitational potential energy
+==============================
+
+The **gravitational potential energy** of a system of two particles is the negative of the work
+that would be done by the gravitational force of either particle acting on the other if the
+particles were brought together from infinity to the given distance.
+"""
+
 from sympy import Eq
-from sympy.physics.units import gravitational_constant
 from symplyphysics import (
-    units,
     Quantity,
-    Symbol,
-    print_expression,
     validate_input,
     validate_output,
     clone_as_symbol,
     symbols,
+    quantities,
 )
 
-# Description
-## The gravitational potential energy U(r) of a system of two particles, with masses m1 and m2
-## and separated by a distance r, is the negative of the work that would be done by the gravitational
-## force of either particle acting on the other if the separation between the particles were changed
-## from infinite to r.
+gravitational_potential_energy = symbols.potential_energy
+"""
+Gravitational :symbols:`potential_energy`.
+"""
 
-# Law: U = -G * m1 * m2 / r
-## U - gravitational potential energy
-## G - gravitational constant
-## m1, m2 - masses of interacting particles
-## r - distance between mass centers of particles
+first_mass = clone_as_symbol(symbols.mass, subscript="1")
+"""
+:symbols:`mass` of the first particle.
+"""
 
-gravitational_potential_energy = Symbol("gravitational_potential_energy", units.energy)
-first_mass = clone_as_symbol(symbols.mass, display_symbol="m_1", display_latex="m_1")
-second_mass = clone_as_symbol(symbols.mass, display_symbol="m_2", display_latex="m_2")
-distance_between_mass_centers = Symbol("distance_between_mass_centers", units.length)
+second_mass = clone_as_symbol(symbols.mass, subscript="2")
+"""
+:symbols:`mass` of the second particle.
+"""
+
+distance_between_mass_centers = symbols.euclidean_distance
+"""
+:symbols:`euclidean_distance` between mass centers of the particles.
+"""
+
 
 law = Eq(gravitational_potential_energy,
-    -1 * gravitational_constant * first_mass * second_mass / distance_between_mass_centers)
-
-
-def print_law() -> str:
-    return print_expression(law)
+    -1 * quantities.gravitational_constant * first_mass * second_mass / distance_between_mass_centers)
 
 
 @validate_input(
