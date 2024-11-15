26ebbcec8b7b24cbcbe9569f8cde95d9ddaaaf2e
Aleksandr Slatov <alexslatov@gmail.com>
2024-11-15 17:48:05 +0400

    Add documentation

--- symplyphysics/laws/gravity/maximum_height_of_a_body_thrown_at_an_angle_to_horizon.py
+++ symplyphysics/laws/gravity/maximum_height_of_a_body_thrown_at_an_angle_to_horizon.py
@@ -1,74 +1,101 @@
-from sympy import (Eq, solve, sin, pi)
-from sympy.physics.units import acceleration_due_to_gravity as earth_free_fall_acceleration
-from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
-    validate_output, angle_type)
+r"""
+Maximum height of body thrown at angle to horizon
+=================================================
+
+The maximum height of a projectile above its launch position is a function of
+the vertical component of the initial velocity, the angle between the initial
+velocity and the horizon, and the magnitude of the free fall acceleration.
+
+**Notation:**
+
+#. :quantity_notation:`acceleration_due_to_gravity`.
+
+**Conditions:**
+
+#. :math:`\mathbf{v}_0 \cdot \mathbf{g} < 0`, i.e. the vector of initial velocity
+   :math:`\mathbf{v}_0` must have a non-zero component that is antiparallel to the
+   vector of free fall acceleration :math:`\mathbf{g}`.
+
+#. The height is measured with respect to the horizontal plane where the projectile
+   was located at initial time.
+"""
+
+from sympy import Eq, solve, sin, pi
+from symplyphysics import (
+    Quantity,
+    validate_input,
+    validate_output,
+    symbols,
+    quantities,
+    convert_to_si,
+)
 from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.laws.kinematics import position_via_constant_acceleration_and_time as distance_law
 from symplyphysics.laws.kinematics import speed_via_constant_acceleration_and_time as velocity_law
 from symplyphysics.laws.geometry import planar_projection_is_cosine as projection_law
 
-# Description
-## Let's say we throw the body at an angle to the horizon with some initial velocity.
-## Then the height of throw of the body will depend only on the initial velocity,
-## the angle of the throw and the acceleration of free fall.
-## The angle of the throw is the angle between the initial velocity vector and the horizontal axis.
+height = symbols.height
+"""
+Maximum :symbols:`height` of the projectile.
+"""
 
-## Law is: h = v0^2 * sin(a)^2 / (2 * g), where
-## h - height of throw,
-## v0 - initial velocity,
-## a - angle of throw,
-## g - acceleration of free fall.
+initial_speed = symbols.speed
+"""
+Initial :symbols:`speed` of the body.
+"""
 
-height = Symbol("height", units.length)
+angle = symbols.angle
+"""
+:symbols:`angle` between the initial velocity and the horizon.
+"""
 
-initial_velocity = Symbol("initial_velocity", units.velocity)
-angle = Symbol("angle", angle_type)
+law = Eq(height, initial_speed**2 * sin(angle)**2 / (2 * quantities.acceleration_due_to_gravity))
+"""
+:laws:symbol::
 
-law = Eq(height, initial_velocity**2 * sin(angle)**2 / (2 * earth_free_fall_acceleration))
+:laws:latex::
+"""
+
 
 # This law might be derived via "constant_acceleration_movement_is_parabolic" law, "planar_projection_is_cosine" law
 # and "accelerated_velocity_from_time" law.
 
 # The law seeks a projection on the horizontal axis, but a projection on the vertical axis is necessary,
 # so the angle is represented as a "pi/2 - angle".
-projection_law_applied = projection_law.law.subs({
-    projection_law.vector_length: initial_velocity,
+_projection_law_applied = projection_law.law.subs({
+    projection_law.vector_length: initial_speed,
     projection_law.vector_angle: (pi / 2) - angle,
 })
-vertical_projection_derived = solve(projection_law_applied, projection_law.projection,
+_vertical_projection_derived = solve(_projection_law_applied, projection_law.projection,
     dict=True)[0][projection_law.projection]
 
 # Vertical velocity is zero in the highest point of trajectory.
-velocity_law_applied = velocity_law.law.subs({
-    velocity_law.initial_speed: vertical_projection_derived,
+_velocity_law_applied = velocity_law.law.subs({
+    velocity_law.initial_speed: _vertical_projection_derived,
     velocity_law.final_speed: 0,
-    velocity_law.acceleration: -earth_free_fall_acceleration,
+    velocity_law.acceleration: -quantities.acceleration_due_to_gravity,
 })
-time_derived = solve(velocity_law_applied, velocity_law.time, dict=True)[0][velocity_law.time]
+_time_derived = solve(_velocity_law_applied, velocity_law.time, dict=True)[0][velocity_law.time]
 
 # The acceleration of gravity is directed opposite to the vertical coordinate axis,
 ## so there is a minus sign before the acceleration.
-height_law_applied = distance_law.law.subs({
-    distance_law.initial_speed: vertical_projection_derived,
-    distance_law.time: time_derived,
-    distance_law.acceleration: -earth_free_fall_acceleration,
+_height_law_applied = distance_law.law.subs({
+    distance_law.initial_speed: _vertical_projection_derived,
+    distance_law.time: _time_derived,
+    distance_law.acceleration: -quantities.acceleration_due_to_gravity,
     distance_law.initial_position: 0,
 })
 
 # Check if derived height is same as declared.
-assert expr_equals(height_law_applied.rhs, law.rhs)
+assert expr_equals(_height_law_applied.rhs, law.rhs)
 
 
-def print_law() -> str:
-    return print_expression(law)
-
-
-@validate_input(initial_velocity_=initial_velocity, angle_=angle)
+@validate_input(initial_velocity_=initial_speed, angle_=angle)
 @validate_output(height)
 def calculate_height(initial_velocity_: Quantity, angle_: float | Quantity) -> Quantity:
     result_expr = solve(law, height, dict=True)[0][height]
     result_expr = result_expr.subs({
-        initial_velocity: initial_velocity_,
-        angle: angle_,
+        initial_speed: initial_velocity_,
+        angle: convert_to_si(angle_),
     })
     return Quantity(result_expr)
