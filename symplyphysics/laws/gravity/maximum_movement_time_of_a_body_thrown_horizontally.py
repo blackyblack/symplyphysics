1827a652b74332ec0bdcbb56deefef8bb80f01e4
Aleksandr Slatov <alexslatov@gmail.com>
2024-11-27 19:00:01 +0400

    Add documentation

--- symplyphysics/laws/gravity/maximum_movement_time_of_a_body_thrown_horizontally.py
+++ symplyphysics/laws/gravity/maximum_movement_time_of_a_body_thrown_horizontally.py
@@ -1,54 +1,74 @@
+"""
+Time of flight of a projectile via maximum height
+=================================================
+
+Let the body be thrown horizontally with some initial velocity. Then the time of the motion
+of this body until it reaches the initial elevation depends only on the height and acceleration
+of free fall.
+
+**Links:**
+
+#. `Physics LibreTexts <https://phys.libretexts.org/Bookshelves/University_Physics/Physics_(Boundless)/3%3A_Two-Dimensional_Kinematics/3.3%3A_Projectile_Motion>`__.
+
+**Conditions:**
+
+#. The acceleration of gravity stays constant throughout the movement of the body.
+
+#. The initial and final height of the body with respect to the "ground" are the same,
+   i.e. the body lands on a point with the same vertical coordinate as at the start
+   of the flight.
+
+..
+    TODO rename file
+"""
+
 from sympy import (Eq, solve, sqrt)
-from sympy.physics.units import acceleration_due_to_gravity as earth_free_fall_acceleration
 from symplyphysics import (
-    units,
     Quantity,
-    Symbol,
-    print_expression,
     validate_input,
     validate_output,
+    quantities,
+    symbols,
 )
 from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.laws.kinematics import position_via_constant_acceleration_and_time as distance_law
 
-# Description
-## Let the body be thrown horizontally with some initial velocity. Then the time of motion (time of fall)
-## of this body depends only on the height and acceleration of free fall.
+time = symbols.time
+"""
+:symbols:`time` of flight of the projectile.
+"""
 
-## Law is: t = sqrt(2 * h / g), where
-## t - movement time,
-## h - height,
-## g - acceleration of free fall.
+height = symbols.height
+"""
+Maximum :symbols:`height` which the projectile reaches during its motion.
+"""
 
-movement_time = Symbol("movement_time", units.time)
+law = Eq(time, sqrt(2 * height / quantities.acceleration_due_to_gravity))
+"""
+:laws:symbol::
 
-height = Symbol("height", units.length)
-
-law = Eq(movement_time, sqrt(2 * height / earth_free_fall_acceleration))
+:laws:latex::
+"""
 
 # This law might be derived via "constant_acceleration_movement_is_parabolic" law.
 # Horizontal vector of movement does not change falling time.
 
-distance_law_applied = distance_law.law.subs({
+_distance_law_applied = distance_law.law.subs({
     distance_law.initial_speed: 0,
-    distance_law.acceleration: earth_free_fall_acceleration,
+    distance_law.acceleration: quantities.acceleration_due_to_gravity,
     distance_law.initial_position: 0,
     distance_law.final_position: height,
 })
-time_derived = solve(distance_law_applied, distance_law.time, dict=True)[1][distance_law.time]
+_time_derived = solve(_distance_law_applied, distance_law.time, dict=True)[1][distance_law.time]
 
 # Check if derived movement time is same as declared.
-assert expr_equals(time_derived, law.rhs)
-
-
-def print_law() -> str:
-    return print_expression(law)
+assert expr_equals(_time_derived, law.rhs)
 
 
 @validate_input(height_=height)
-@validate_output(movement_time)
+@validate_output(time)
 def calculate_movement_time(height_: Quantity) -> Quantity:
-    result_expr = solve(law, movement_time, dict=True)[0][movement_time]
+    result_expr = solve(law, time, dict=True)[0][time]
     result_expr = result_expr.subs({
         height: height_,
     })
