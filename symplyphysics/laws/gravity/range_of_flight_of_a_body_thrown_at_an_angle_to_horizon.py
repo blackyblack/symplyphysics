5f77e326a88c8ac10f3210cb072dc8d08f00fbc8
Aleksandr Slatov <alexslatov@gmail.com>
2024-11-27 16:59:55 +0400

    Add documentation

--- symplyphysics/laws/gravity/range_of_flight_of_a_body_thrown_at_an_angle_to_horizon.py
+++ symplyphysics/laws/gravity/range_of_flight_of_a_body_thrown_at_an_angle_to_horizon.py
@@ -1,69 +1,88 @@
-from sympy import (Eq, solve, sin, simplify)
-from sympy.physics.units import acceleration_due_to_gravity as earth_free_fall_acceleration
-from symplyphysics import (units, Quantity, Symbol, print_expression, validate_input,
-    validate_output, angle_type)
+"""
+Horizontal displacement of projectile
+=====================================
+
+Let's say we throw the body at an angle to the horizon with some initial velocity.
+Then the horizontal displacement, or **range**, of the body depends on the initial
+speed, the angle of the throw and the acceleration of free fall.
+
+**Conditions:**
+
+#. The acceleration due to gravity is constant throughout the movement
+   of the projectile.
+
+#. The elevation of the initial and final points of the projectile are
+   the same.
+
+**Links:**
+
+#. `Physics LibreTexts <https://phys.libretexts.org/Bookshelves/University_Physics/Physics_(Boundless)/3%3A_Two-Dimensional_Kinematics/3.3%3A_Projectile_Motion>`__.
+"""
+
+from sympy import Eq, solve, sin, simplify
+from symplyphysics import Quantity, validate_input, validate_output, symbols, quantities
 from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.laws.kinematics import position_via_constant_speed_and_time as distance_law
 from symplyphysics.laws.geometry import planar_projection_is_cosine as projection_law
 from symplyphysics.laws.gravity import maximum_movement_time_of_a_body_thrown_at_an_angle_to_horizon as time_law
 
-# Description
-## Let's say we throw the body at an angle to the horizon with some initial velocity.
-## Then the range of the throw depends on the initial velocity, the angle of the throw and
-## the acceleration of free fall.
+horizontal_displacement = symbols.euclidean_distance
+"""
+Horizontal displacement, or **range**, of the projectile. See :symbols:`euclidean_distance`.
+"""
 
-## Law is: L = v0^2 * sin(2 * a) / g, where
-## L - throw range,
-## v0 - initial velocity,
-## a - angle of throw,
-## g - acceleration of free fall.
+initial_speed = symbols.speed
+"""
+Initial :symbols:`speed` of the projectile.
+"""
 
-throw_range = Symbol("throw_range", units.length)
+angle = symbols.angle
+"""
+:symbols:`angle` between the initial velocity and the horizon.
+"""
 
-initial_velocity = Symbol("initial_velocity", units.velocity)
-angle = Symbol("angle", angle_type)
+law = Eq(horizontal_displacement, initial_speed**2 * sin(2 * angle) / quantities.acceleration_due_to_gravity)
+"""
+:laws:symbol::
 
-law = Eq(throw_range, initial_velocity**2 * sin(2 * angle) / earth_free_fall_acceleration)
+:laws:latex::
+"""
 
 # This law might be derived via "distance_from_constant_velocity" law, "planar_projection_is_cosine" law
 # and "maximum_movement_time_of_a_body_thrown_at_an_angle_to_horizon" law.
 
-projection_law_applied = projection_law.law.subs({
-    projection_law.vector_length: initial_velocity,
+_projection_law_applied = projection_law.law.subs({
+    projection_law.vector_length: initial_speed,
     projection_law.vector_angle: angle,
 })
-horizontal_projection_derived = solve(projection_law_applied, projection_law.projection,
+_horizontal_projection_derived = solve(_projection_law_applied, projection_law.projection,
     dict=True)[0][projection_law.projection]
 
-time_law_applied = time_law.law.subs({
-    time_law.initial_velocity: initial_velocity,
+_time_law_applied = time_law.law.subs({
+    time_law.initial_speed: initial_speed,
     time_law.angle: angle,
 })
-time_derived = solve(time_law_applied, time_law.movement_time, dict=True)[0][time_law.movement_time]
+_time_derived = solve(_time_law_applied, time_law.movement_time, dict=True)[0][time_law.movement_time]
 
-range_law_applied = distance_law.law.subs({
+_range_law_applied = distance_law.law.subs({
     distance_law.initial_position: 0,
-    distance_law.speed: horizontal_projection_derived,
-    distance_law.time: time_derived,
+    distance_law.speed: _horizontal_projection_derived,
+    distance_law.time: _time_derived,
 })
 
 # Check if derived range is same as declared.
-assert expr_equals(simplify(range_law_applied.rhs), law.rhs)
+assert expr_equals(simplify(_range_law_applied.rhs), law.rhs)
 
 
-def print_law() -> str:
-    return print_expression(law)
-
-
-@validate_input(initial_velocity_=initial_velocity, angle_=angle)
-@validate_output(throw_range)
+@validate_input(initial_velocity_=initial_speed, angle_=angle)
+@validate_output(horizontal_displacement)
 def calculate_range(
     initial_velocity_: Quantity,
     angle_: float | Quantity,
 ) -> Quantity:
-    result_expr = solve(law, throw_range, dict=True)[0][throw_range]
+    result_expr = solve(law, horizontal_displacement, dict=True)[0][horizontal_displacement]
     result_expr = result_expr.subs({
-        initial_velocity: initial_velocity_,
+        initial_speed: initial_velocity_,
         angle: angle_,
     })
     return Quantity(result_expr)
