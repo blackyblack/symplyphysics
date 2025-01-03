8369951b6e477b05eb6594070dd0448aab9c561b
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-01-03 19:55:50 +0400

    Law: Gibbs energy in dielectrics (#939)
    * Add law and test
    
    * Rename test

---
+++ symplyphysics/laws/thermodynamics/dielectrics/gibbs_energy_formula.py
@@ -0,0 +1,76 @@
+"""
+Gibbs energy of dielectrics
+===========================
+
+Gibbs energy of the system with a dielectric medium can be expressed using
+the Helmholtz free energy and the electric displacement and field strength.
+
+**Conditions:**
+
+#. The dielectric is isotropic whether or not the electric field is present.
+#. The medium is homogeneous.
+#. The volume change of the medium is insignificant.
+
+**Links:**
+
+#. Formula 31.4 on p. 122 of "General Course of Physics" (Obschiy kurs fiziki), vol. 3 by Sivukhin D.V. (1979).
+"""
+
+from sympy import Eq
+from symplyphysics import (
+    symbols,
+    units,
+    Quantity,
+    SymbolNew,
+    validate_input,
+    validate_output,
+)
+
+gibbs_energy_density = SymbolNew("G", units.energy / units.volume)
+"""
+:symbols:`gibbs_energy` of the system per unit :symbols:`volume`.
+"""
+
+free_energy_density = SymbolNew("F", units.energy / units.volume)
+"""
+:symbols:`helmholtz_free_energy` of the system per units :symbols:`volume`.
+"""
+
+electric_field_strength = symbols.electric_field_strength
+"""
+:symbols:`electric_field_strength` in the medium.
+"""
+
+electric_displacement = symbols.electric_displacement
+"""
+:symbols:`electric_displacement` in the medium.
+"""
+
+law = Eq(
+    gibbs_energy_density,
+    free_energy_density - electric_field_strength * electric_displacement,
+)
+"""
+:laws:symbol::
+
+:laws:latex::
+"""
+
+
+@validate_input(
+    free_energy_density_=free_energy_density,
+    electric_field_strength_=electric_field_strength,
+    electric_displacement_=electric_displacement,
+)
+@validate_output(gibbs_energy_density)
+def calculate_gibbs_energy_density(
+    free_energy_density_: Quantity,
+    electric_field_strength_: Quantity,
+    electric_displacement_: Quantity,
+) -> Quantity:
+    result = law.rhs.subs({
+        free_energy_density: free_energy_density_,
+        electric_field_strength: electric_field_strength_,
+        electric_displacement: electric_displacement_,
+    })
+    return Quantity(result)
---
+++ test/thermodynamics/dielectrics/gibbs_energy_formula_dielectrics_test.py
@@ -0,0 +1,43 @@
+from collections import namedtuple
+from pytest import fixture, raises
+from symplyphysics import assert_equal, errors, units, Quantity
+from symplyphysics.laws.thermodynamics.dielectrics import gibbs_energy_formula as law
+
+Args = namedtuple("Args", "f e d")
+
+
+@fixture(name="test_args")
+def test_args_fixture() -> Args:
+    f = Quantity(10.0 * units.joule / units.meter**3)
+    e = Quantity(9.0 * units.volt / units.meter)
+    d = Quantity(2.0 * units.coulomb / units.meter**2)
+    return Args(f=f, e=e, d=d)
+
+
+def test_law(test_args: Args) -> None:
+    result = law.calculate_gibbs_energy_density(test_args.f, test_args.e, test_args.d)
+    assert_equal(result, -8 * units.joule / units.meter**3, tolerance=4e-3)
+
+
+def test_bad_free_energy_density(test_args: Args) -> None:
+    fb = Quantity(units.candela)
+    with raises(errors.UnitsError):
+        law.calculate_gibbs_energy_density(fb, test_args.e, test_args.d)
+    with raises(TypeError):
+        law.calculate_gibbs_energy_density(100, test_args.e, test_args.d)
+
+
+def test_bad_electric_field_strength(test_args: Args) -> None:
+    eb = Quantity(units.candela)
+    with raises(errors.UnitsError):
+        law.calculate_gibbs_energy_density(test_args.f, eb, test_args.d)
+    with raises(TypeError):
+        law.calculate_gibbs_energy_density(test_args.f, 100, test_args.d)
+
+
+def test_bad_electric_displacement(test_args: Args) -> None:
+    db = Quantity(units.candela)
+    with raises(errors.UnitsError):
+        law.calculate_gibbs_energy_density(test_args.f, test_args.e, db)
+    with raises(TypeError):
+        law.calculate_gibbs_energy_density(test_args.f, test_args.e, 100)
