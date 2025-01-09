b3acf37daf636ed3b2e964b1c22d7bd100d49182
Aleksandr Slatov <alexslatov@gmail.com>
2025-01-09 23:23:01 +0400

    Refactor law

--- symplyphysics/laws/quantum_mechanics/harmonic_oscillator/wave_eigenfunctions.py
+++ symplyphysics/laws/quantum_mechanics/harmonic_oscillator/wave_eigenfunctions.py
@@ -1,48 +1,69 @@
+"""
+Wave eigenfunctions of quantum harmonic oscillator
+==================================================
+
+The time-independent Schrödinger equation describing the wave function of the quantum oscillator
+can be solved to get the corresponding wave eigenfunctions and energy eigenvalues of the Hamiltonian
+operator. Each eigenfunction describes a stationary state of the quantum mechanical system with the
+corresponding energy value (eigenvalue of the Hamiltonian). The combination of all eigenfunctions and
+eigenvalues represent the energy states allowed.
+
+**Notation:**
+
+#. :quantity_notation:`hbar`.
+#. :math:`H_n` (:code:`hermite`) is the :math:`n`-th physicists' Hermite polynomial.
+
+**Links:**
+
+#. `Wikipedia <https://en.wikipedia.org/wiki/Quantum_harmonic_oscillator#Hamiltonian_and_energy_eigenstates>`__.
+"""
+
 from sympy import Eq, Rational, sqrt, exp, factorial, pi
 from sympy.functions.special.polynomials import hermite
-from sympy.physics.units import hbar
 from symplyphysics import (
     Quantity,
-    Symbol,
     validate_input,
     validate_output,
-    units,
-    dimensionless,
-    angle_type,
     symbols,
 )
+from symplyphysics.quantities import hbar
 
-# Description
-## The time-independent Schrödinger equation describing the wave function of the quantum oscillator
-## can be solved to get the corresponding wave eigenfunctions and energy eigenvalues of the Hamiltonian
-## operator. Each eigenfunction describes a stationary state of the quantum mechanical system with the
-## corresponding energy value (eigenvalue of the Hamiltonian). The combination of all eigenfunctions and
-## eigenvalues represent the energy states allowed.
+wave_function = symbols.wave_function
+"""
+:math:`n`-th wave eigenfunction (solution of the time-independent Schrödinger equation).
+See :symbols:`wave_function`.
+"""
 
-# Law: psi_n(x) = 1 / sqrt(2**n * factorial(n))
-#               * ((m * w) / (pi * hbar))**(1 / 4)
-#               * exp(-1 * (m * w) / (2 * hbar) * x**2)
-#               * H_n(sqrt((m * w) / hbar) * x)
-## psi_n - n-th wave eigenfunction (solution of the time-independent Schrödinger equation)
-## n - mode number (n = 0, 1, 2, ...)
-## m - mass of oscillating quantum particle
-## w - angular frequency of quantum oscillator
-## hbar - reduced Planck constant
-## H_n - n-th (physicists') Hermite polynomial
+mode_number = symbols.nonnegative_number
+"""
+Mode number. See :symbols:`nonnegative_number`.
+"""
 
-# Links: Wikipedia <https://en.wikipedia.org/wiki/Quantum_harmonic_oscillator#Hamiltonian_and_energy_eigenstates>
-
-wave_function = Symbol("wave_function", 1 / sqrt(units.length))
-mode_number = Symbol("mode_number", dimensionless, integer=True, nonnegative=True)
 oscillator_mass = symbols.mass
-angular_frequency = Symbol("angular_frequency", angle_type / units.time)
-position = Symbol("position", units.length)
+"""
+:symbols:`mass` of the oscillator.
+"""
+
+angular_frequency = symbols.angular_frequency
+"""
+:symbols:`angular_frequency` of the oscillator.
+"""
+
+position = symbols.position
+"""
+:symbols:`position` of the oscillator.
+"""
 
 law = Eq(wave_function,
     (1 / (sqrt(2**mode_number * factorial(mode_number)))) * ((oscillator_mass * angular_frequency) /
     (pi * hbar))**Rational(1, 4) * exp(-1 * (oscillator_mass * angular_frequency) /
     (2 * hbar) * position**2) * hermite(mode_number,
     sqrt(oscillator_mass * angular_frequency / hbar) * position))
+"""
+:laws:symbol::
+
+:laws:latex::
+"""
 
 
 @validate_input(
