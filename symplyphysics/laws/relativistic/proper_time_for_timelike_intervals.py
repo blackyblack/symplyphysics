742f7eadd27c807d1bdb6f7027c029fb38c5d7fa
Aleksandr Slatov <alexslatov@gmail.com>
2025-01-09 15:22:58 +0400

    Refactor law

--- symplyphysics/laws/relativistic/proper_time_for_timelike_intervals.py
+++ symplyphysics/laws/relativistic/proper_time_for_timelike_intervals.py
@@ -1,32 +1,56 @@
+"""
+Proper time for timelike intervals
+==================================
+
+In relativity, proper time along a timelike world line is defined as the time as measured by a clock following
+that line. The proper time interval between two events on a world line is the change in proper time, which is
+independent of coordinates, and is a Lorentz scalar.
+
+**Conditions**
+
+#. The interval is timelike, i.e. :math:`\\Delta s` is real.
+
+**Links:**
+
+#. `Wikipedia, equivalent to formula 2 in box <https://en.wikipedia.org/wiki/Proper_time#In_special_relativity>`__.
+"""
+
 from sympy import Eq, im
-from sympy.physics.units import speed_of_light
 from symplyphysics import (
-    units,
     Quantity,
-    Symbol,
     validate_input,
     validate_output,
+    symbols,
+    clone_as_symbol,
+    quantities,
 )
 
-# Description
-## In relativity, proper time along a timelike world line is defined as the time as measured by a clock following
-## that line. The proper time interval between two events on a world line is the change in proper time, which is
-## independent of coordinates, and is a Lorentz scalar.
+proper_time = clone_as_symbol(
+    symbols.proper_time,
+    display_symbol="Delta(tau)",
+    display_latex="\\Delta \\tau",
+    real=True,
+)
+"""
+The :symbols:`proper_time` interval between two events.
+"""
 
-# Law: Δτ = Δs / c
-## Δτ - proper time interval
-## Δs - spacetime interval between two events
-## c - speed of light
+spacetime_interval = clone_as_symbol(
+    symbols.spacetime_interval,
+    display_symbol="Delta(s)",
+    display_latex="\\Delta s",
+    real=True,
+)
+"""
+The :symbols:`spacetime_interval` between two events.
+"""
 
-# Conditions
-## - The interval is timelike, i.e. `Δs` is real
+law = Eq(proper_time, spacetime_interval / quantities.speed_of_light)
+"""
+:laws:symbol::
 
-# Links: Wikipedia, equivalent to formula 2 in box <https://en.wikipedia.org/wiki/Proper_time#In_special_relativity>
-
-proper_time = Symbol("proper_time", units.time, real=True)
-spacetime_interval = Symbol("spacetime_interval", units.length, real=True)
-
-law = Eq(proper_time, spacetime_interval / speed_of_light)
+:laws:latex::
+"""
 
 
 @validate_input(spacetime_interval_=spacetime_interval)
