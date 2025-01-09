5fe9bf778706abd4832bf275ff2f0f52d3918107
Aleksandr Slatov <alexslatov@gmail.com>
2025-01-09 23:23:16 +0400

    Refactor law

--- symplyphysics/laws/quantum_mechanics/expectation_value_of_observable.py
+++ symplyphysics/laws/quantum_mechanics/expectation_value_of_observable.py
@@ -1,42 +1,53 @@
-from sympy import (
-    Eq,
-    Integral,
-    Function as SymFunction,
-    symbols,
-    sqrt,
-    conjugate,
-    S,
-    Expr,
-)
-from symplyphysics import (
-    units,
-    Symbol,
-    Function,
-)
+"""
+Expectation value of observable
+===============================
+
+Roughly speaking, an observable is a measurable property of a physical system, such as spin, position,
+energy, momentum, etc. In terms of Quantum Mechanics, every physical observable corresponds to an operator
+which acts on the wave function.
+
+**Links:**
+
+#. `Wikipedia <https://en.wikipedia.org/wiki/Operator_(physics)#Expectation_values_of_operators_on_%CE%A8>`__.
+"""
+
+from sympy import Eq, Integral, conjugate, S, Expr
+from symplyphysics import clone_as_symbol, clone_as_function, symbols
 from symplyphysics.core.type_aliases import WaveFunction, Observable
 
-# Description
-## Roughly speaking, an observable is a measurable property of a physical system, such as spin, position,
-## energy, momentum, etc. In terms of Quantum Mechanics, every physical observable corresponds to an operator
-## which acts on the wave function.
+mean_observable_value = clone_as_symbol(
+    symbols.any_quantity,
+    display_symbol="avg(O)",
+    display_latex="\\langle O \\rangle",
+)
+"""
+The mean value of the :attr:`~observable` operator. See :symbols:`any_quantity`.
+"""
 
-# Law: <O> = Integral(conj(psi(x, t)) * O[psi(x, t)], (x, -oo, oo))
-## O - observable operator
-## psi - wave function
-## x - position
-## t - time
-## oo - infinity
-## <O> - expectation value of O
-## conj - complex conjugate
-## O[f] - operator `O` applied to function `f`
+position = symbols.position
+"""
+:symbols:`position`.
+"""
 
-# Links: Wikipedia <https://en.wikipedia.org/wiki/Operator_(physics)#Expectation_values_of_operators_on_%CE%A8>
+time = symbols.time
+"""
+:symbols:`time`.
+"""
 
-mean_observable_value = symbols("mean_observable_value")
-observable = symbols("observable", cls=SymFunction)
-wave_function = Function("wave_function", 1 / sqrt(units.length))
-position = Symbol("position", units.length, real=True)
-time = Symbol("time", units.time)
+wave_function = clone_as_function(symbols.wave_function, [position, time])
+"""
+:symbols:`wave_function` as a function of :attr:`~position` and :attr:`~time`.
+"""
+
+observable = clone_as_function(
+    symbols.any_quantity,
+    [wave_function(position, time)], # should technically be a function of unapplied wave function
+    display_symbol="O",
+    display_latex="O",
+)
+"""
+Observable operator as a function of :attr:`~wave_function`. See :symbols:`any_quantity`.
+"""
 
 law = Eq(
     mean_observable_value,
@@ -44,6 +55,16 @@ law = Eq(
     conjugate(wave_function(position, time)) * observable(wave_function(position, time)),
     (position, S.NegativeInfinity, S.Infinity)),
 )
+r"""
+:laws:symbol::
+
+..
+    The conjugation operator isn't recognized yet by the LaTeX printer.
+
+.. math::
+    
+    \langle O \rangle = \int \limits_{-\infty}^{\infty} \psi^* (x, t) O[\psi](x, t) dx
+"""
 
 
 def calculate_mean_observable_value(
