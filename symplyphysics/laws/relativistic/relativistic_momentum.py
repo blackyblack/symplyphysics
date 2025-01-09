f609c27d80edc9d3804b34e9eb8358e6e5adfba1
Aleksandr Slatov <alexslatov@gmail.com>
2025-01-09 15:21:32 +0400

    Refactor law

--- symplyphysics/laws/relativistic/relativistic_momentum.py
+++ symplyphysics/laws/relativistic/relativistic_momentum.py
@@ -1,41 +1,53 @@
+"""
+Relativistic momentum via rest mass and speed
+=============================================
+
+Momentum (amount of motion) is a vector physical quantity that is a measure of the mechanical movement
+of a body. The relativistic momentum also takes into account speed limits equal to the speed of light.
+
+**Links:**
+
+#. `Wikipedia <https://en.wikipedia.org/wiki/Mass_in_special_relativity#Relativistic_energy%E2%80%93momentum_equation>`__.
+"""
+
 from sympy import (Eq, solve, sqrt)
-from sympy.physics.units import speed_of_light
 from symplyphysics import (
-    units,
     Quantity,
-    Symbol,
-    print_expression,
     validate_input,
     validate_output,
+    symbols,
+    quantities,
 )
 
-# Description
-## Momentum (amount of motion) is a vector physical quantity that is a measure of the mechanical movement
-## of a body. The relativistic momentum also takes into account speed limits equal to the speed of light.
+momentum = symbols.momentum
+"""
+:symbols:`momentum` of the body.
+"""
 
-## Law is: p = m * v / sqrt(1 - v^2 / c^2), where
-## p - momentum,
-## m - rest mass,
-## v - velocity of the object in the selected frame of reference,
-## c - speed of light in vacuum.
+rest_mass = symbols.rest_mass
+"""
+:symbols:`rest_mass` of the body.
+"""
 
-# Links: Wikipedia <https://en.wikipedia.org/wiki/Mass_in_special_relativity#Relativistic_energy%E2%80%93momentum_equation>
+speed = symbols.speed
+"""
+:symbols:`speed` of the body.
+"""
 
-momentum = Symbol("momentum", units.momentum)
+law = Eq(momentum, rest_mass * speed / sqrt(1 - (speed / quantities.speed_of_light)**2))
+"""
+:laws:symbol::
 
-# This is equivalent of symbols.mass
-rest_mass = Symbol("rest_mass", units.mass)
-velocity = Symbol("velocity", units.velocity)
-
-law = Eq(momentum, rest_mass * velocity / sqrt(1 - (velocity / speed_of_light)**2))
+:laws:latex::
+"""
 
 
-@validate_input(mass_=rest_mass, velocity_=velocity)
+@validate_input(mass_=rest_mass, velocity_=speed)
 @validate_output(momentum)
 def calculate_momentum(mass_: Quantity, velocity_: Quantity) -> Quantity:
     result_expr = solve(law, momentum, dict=True)[0][momentum]
     result_expr = result_expr.subs({
         rest_mass: mass_,
-        velocity: velocity_,
+        speed: velocity_,
     })
     return Quantity(result_expr)
