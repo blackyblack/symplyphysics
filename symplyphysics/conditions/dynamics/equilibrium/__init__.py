597c0ab0cf6fcb77562c55bd3e533c3ff2079fe8
Andrey Lekar <andrey_lekar@adoriasoft.com>
2025-01-14 00:16:15 +0200

    Add conditions

---
+++ symplyphysics/conditions/dynamics/equilibrium/__init__.py
@@ -0,0 +1,6 @@
+"""
+**Dynamics (Equilibrium conditions)**
+==========================================
+
+The conditions in this folder are related to the equilibrium in dynamic systems.
+"""
---
+++ symplyphysics/conditions/electricity/maxwell_equations/__init__.py
@@ -0,0 +1,6 @@
+"""
+**Electrodynamics (Maxwell equations conditions)**
+==================================================
+
+The conditions in this folder are related to the Maxwell Equations.
+"""
