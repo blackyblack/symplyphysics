866a5bd2fedc119a44d0e14e4655060706bbb787
Andrey Lekar <andrey_lekar@adoriasoft.com>
2025-01-29 23:36:13 +0200

    Small fix

--- symplyphysics/laws/chemistry/free_path_of_atomic_particles_in_gaseous_medium.py
+++ symplyphysics/laws/chemistry/free_path_of_atomic_particles_in_gaseous_medium.py
@@ -12,7 +12,7 @@ distance that a traveling atom travels between two collisions.
 
 **Notes:**
 
-#. Assuming the model of spherical gas molecules, :math:`\\sigma = pi * d^2`, where
+#. Assuming the model of spherical gas molecules, :math:`\\sigma = pi d^2`, where
    :math:`\\sigma` is the cross section and :math:`d` is the molecule diameter.
 
 **Links:**
