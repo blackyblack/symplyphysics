cbf941be4afada45847363391f181dbcfc678dc6
Aleksandr Slatov <alexslatov@gmail.com>
2025-02-21 18:42:59 +0400

    Use modern library and rename variables

--- symplyphysics/docs/build.py
+++ symplyphysics/docs/build.py
@@ -1,125 +1,168 @@
 import ast
 import os
+from pathlib import Path
 from typing import Optional, Sequence
 from .parse import find_description, find_members_and_functions, find_title
 from .view import print_law, print_package
 from .patch import patch_sympy_evaluate
 
 
-def process_law_package(directory: str, laws: Sequence[str], packages: Sequence[str],
-    output_dir: str, quiet: bool) -> Optional[str]:
-    filename = os.path.normpath(directory)
-    filename_init = os.path.join(filename, "__init__.py")
-    package_content: Optional[str] = None
-    with open(filename_init, "r", encoding="utf-8") as f:
-        package_content = f.read()
-    package_parsed = ast.parse(package_content)
-    docstring = ast.get_docstring(package_parsed)
+def _parse_documentation(source: ast.Module) -> Optional[tuple[str, str]]:
+    """
+    Returns the title and description pulled from the ``source`` docstring if it and the title
+    exist, else returns `None`.
+    """
+
+    docstring = ast.get_docstring(source)
     if docstring is None:
         return None
-    package_title = find_title(docstring)
-    if package_title is None:
-        return None
-    package_description = find_description(docstring)
-    if package_description is None:
-        package_description = ""
 
-    package_name = filename.replace(os.sep, ".")
+    title = find_title(docstring)
+    if title is None:
+        return None
+
+    description = find_description(docstring) or ""
+
+    return title, description
+
+
+def _is_private(s: str) -> bool:
+    """
+    Checks if the given string begins with `.` or `_`.
+    """
+
+    return s.startswith(".") or s.startswith("_")
+
+
+def process_law_package(
+    directory: str,
+    laws: Sequence[str],
+    packages: Sequence[str],
+    output_dir: str,
+    quiet: bool,
+) -> Optional[str]:
+    """
+    Processes the `__init__.py` file of the law package ``directory`` containing ``laws`` and
+    sub-``packages``. Writes the resulting rST file in ``output_dir``. Suppresses generating
+    messages when ``quiet`` is `True`. Returns the stem of the path to the written package
+    documentation file, or `None` if the source file doesn't have any documentation.
+    """
+
+    source_dirpath = Path(directory)
 
     if not quiet:
-        print(f"Generating rST for package {package_name}")
+        print(f"Generating rST for package {source_dirpath}.")
 
-    package_name_strip_root = ".".join(package_name.split(".")[1:])
-    packages = [p for p in packages if not (p.startswith(".") or p.startswith("_"))]
-    packages = [package_name_strip_root + "." + p for p in packages]
+    source_init_path = source_dirpath / "__init__.py"
+    with open(source_init_path, "r", encoding="utf-8", newline=None) as file:
+        source_content = file.read()
 
-    package_parsed = patch_sympy_evaluate(package_parsed)
-    members, functions = find_members_and_functions(package_parsed)
-    package_content = print_package(package_title, package_description, members, functions, package_name,
-        laws, packages)
-
-    package_doc_file = os.path.normpath(os.path.join(output_dir, package_name_strip_root + ".rst"))
-
-    # create directory if necessary
-    os.makedirs(os.path.dirname(package_doc_file), exist_ok=True)
-    with open(package_doc_file, "w+", encoding="utf-8") as f:
-        f.write(package_content)
-    return package_name_strip_root
-
-
-def process_law(directory: str, law_filename: str, output_dir: str, quiet: bool) -> Optional[str]:
-    if not law_filename.endswith(".py"):
+    parsed_source = ast.parse(source_content)
+    parsed_doc = _parse_documentation(parsed_source)
+    if parsed_doc is None:
         return None
-    if law_filename.startswith("__"):
-        return None
-    filename = os.path.normpath(os.path.join(directory, law_filename))
-    with open(filename, "r", encoding="utf-8") as f:
-        law_content = f.read()
-    law_parsed = ast.parse(law_content)
+    title, description = parsed_doc
 
-    docstring = ast.get_docstring(law_parsed)
-    if docstring is None:
-        return None
-    law_title = find_title(docstring)
-    if law_title is None:
-        return None
-    law_description = find_description(docstring)
-    if law_description is None:
-        law_description = ""
+    parsed_source = patch_sympy_evaluate(parsed_source)
 
-    no_extension_filename = os.path.splitext(filename)[0]
-    law_module_name = no_extension_filename.replace(os.sep, ".")
-
-    if not quiet:
-        print(f"Generating rST for law {law_module_name}")
-
-    law_parsed = patch_sympy_evaluate(law_parsed)
     try:
-        members, functions = find_members_and_functions(law_parsed)
+        members, functions = find_members_and_functions(parsed_source)
     except Exception as e:
-        raise ValueError(f"Exception has been raised in '{filename}'.") from e
-    doc_content = print_law(law_title, law_description, members, functions, law_module_name)
+        raise ValueError(f"Exception has been raised in {source_dirpath}.") from e
 
-    law_module_name_strip_root = ".".join(law_module_name.split(".")[1:])
-    doc_name = law_module_name_strip_root + ".rst"
-    doc_file = os.path.normpath(os.path.join(output_dir, doc_name))
+    source_file_stem = ".".join(source_dirpath.parts)
+
+    doc_file_stem = ".".join(source_dirpath.parts[1:])
+    packages = [doc_file_stem + "." + p for p in packages if not _is_private(p)]
+    doc_content = print_package(title, description, members, functions, source_file_stem, laws,
+        packages)
+
+    doc_filepath = Path(output_dir, doc_file_stem + ".rst")
 
     # create directory if necessary
-    os.makedirs(os.path.dirname(doc_file), exist_ok=True)
-    with open(doc_file, "w+", encoding="utf-8") as f:
-        f.write(doc_content)
-    return law_module_name_strip_root
+    doc_filepath.parent.mkdir(exist_ok=True)
+    with open(doc_filepath, "w+", encoding="utf-8") as file:
+        file.write(doc_content)
+
+    return doc_file_stem
+
+
+def process_law(directory: str, filename: str, output_dir: str, quiet: bool) -> Optional[str]:
+    """
+    Processes the law in the given ``directory`` under the ``filename``. Writes the resulting rST
+    file in ``output_dir``. Suppresses generating messages when ``quiet`` is `True`. Returns the
+    stem of the path to the law documentation file, or `None` if the source file doesn't have any
+    documentation.
+    """
+
+    if filename.startswith("__") or not filename.endswith(".py"):
+        return None
+
+    source_filepath = Path(directory, filename)
+
+    if not quiet:
+        print(f"Generating rST for {source_filepath}.")
+
+    with open(source_filepath, "r", encoding="utf-8", newline=None) as file:
+        source_content = file.read()
+
+    parsed_source = ast.parse(source_content)
+    parsed_doc = _parse_documentation(parsed_source)
+    if parsed_doc is None:
+        return None
+    title, description = parsed_doc
+
+    parsed_source = patch_sympy_evaluate(parsed_source)
+
+    try:
+        members, functions = find_members_and_functions(parsed_source)
+    except Exception as e:
+        raise ValueError(f"Exception has been raised in {source_filepath}.") from e
+
+    source_filepath_strip_ext = source_filepath.with_suffix("")
+    doc_long_stem = ".".join(source_filepath_strip_ext.parts)
+    doc_content = print_law(title, description, members, functions, doc_long_stem)
+
+    doc_short_stem = ".".join(source_filepath_strip_ext.parts[1:])
+    doc_filepath = Path(output_dir, doc_short_stem + ".rst")
+
+    # create directory if necessary
+    doc_filepath.parent.mkdir(exist_ok=True)
+    with open(doc_filepath, "w+", encoding="utf-8") as file:
+        file.write(doc_content)
+
+    return doc_short_stem
 
 
 def generate_laws_docs(source_dir: str, output_dir: str, exclude_dirs: Sequence[str],
     quiet: bool) -> None:
-    exclude_dirs_with_source: list[str] = []
-    for e in exclude_dirs:
-        to_exclude = os.path.join(source_dir, e)
-        exclude_dirs_with_source.append(to_exclude)
+    """
+    Generates rST files for laws and packages, reading recursively starting from ``source_dir``,
+    but avoiding ``exclude_dirs``. Writes the resulting files in ``output_dir``.
+    """
+
+    exclude_dirs_paths = [Path(source_dir, e) for e in exclude_dirs]
+
     for path, dirs, files in os.walk(source_dir):
-        _, target_dir = os.path.split(path)
-        if target_dir.startswith(".") or target_dir.startswith("_"):
+        path = Path(path)
+
+        target_dir = path.name
+        if (_is_private(target_dir) or any(path.samefile(e) for e in exclude_dirs_paths)):
+            # prevent `os.walk` from iterating excluded sub-directories
             dirs.clear()
             files.clear()
             continue
-        should_exclude = False
-        for e in exclude_dirs_with_source:
-            if os.path.samefile(path, e):
-                should_exclude = True
-                break
-        if should_exclude:
-            dirs.clear()
-            files.clear()
-            continue
+
         dirs.sort()
         files.sort()
+
         laws: list[str] = []
-        for filename in files:
-            law_name = process_law(path, filename, output_dir, quiet)
+        for file in files:
+            law_name = process_law(path, file, output_dir, quiet)
             if law_name is None:
                 continue
             laws.append(law_name)
+
         package_name = process_law_package(path, laws, dirs, output_dir, quiet)
         if package_name is None:
             continue
