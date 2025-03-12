c842abe61101867cdc2f7cab954f157948433ffd
Aleksandr Slatov <alexslatov@gmail.com>
2025-03-13 01:49:02 +0400

    Refactor test

--- test/core/experimental/coordinate_systems/test_vector_conversion.py
+++ test/core/experimental/coordinate_systems/test_vector_conversion.py
@@ -2,6 +2,7 @@ from typing import TypeAlias
 from dataclasses import dataclass
 from pytest import fixture, raises
 from sympy import Expr, Symbol as SymSymbol, S, pi, sqrt
+from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.core.experimental.vectors import VectorExpr, norm, ZERO
 from symplyphysics.core.experimental.coordinate_systems import (
     BaseCoordinateSystem,
@@ -15,19 +16,34 @@ from symplyphysics.core.experimental.coordinate_systems.express_base_vectors imp
 Point: TypeAlias = dict[SymSymbol, Expr]
 
 
-@dataclass
+@dataclass(frozen=True)
+class AppliedVector:
+    value: VectorExpr
+    """Expression containing the components of the vector and the base scalars."""
+
+    point: Point
+    """Point where the vector is applied."""
+
+    system: BaseCoordinateSystem
+    """Coordinate system where the vector is described."""
+
+
+@dataclass(frozen=True)
 class Args:  # pylint: disable=too-many-instance-attributes
     cart: CartesianCoordinateSystem
     cyl: CylindricalCoordinateSystem
     sph: SphericalCoordinateSystem
 
-    p_cart: Point
-    p_cyl: Point
-    p_sph: Point
+    # These represent the same vector `v` in different coordinate systems
+    v_cart: AppliedVector
+    v_cyl: AppliedVector
+    v_sph: AppliedVector
 
-    v_cart: VectorExpr
-    v_cyl: VectorExpr
-    v_sph: VectorExpr
+    # These vectors have the same value as the respective `v` vector, but are applied at at
+    # different points; they're not necessarily all equal to each other
+    w_cart: AppliedVector
+    w_cyl: AppliedVector
+    w_sph: AppliedVector
 
 
 @fixture(name="test_args")
@@ -36,171 +52,302 @@ def test_args_fixture() -> Args:
     cyl = CylindricalCoordinateSystem()
     sph = SphericalCoordinateSystem()
 
-    # These represent the same physical point in different coordinate systems
+    # These represent the same physical point `p` (which is different from `q`) in different
+    # coordinate systems
     p_cart = {cart.x: S(-1), cart.y: S(0), cart.z: S(-1)}
     p_cyl = {cyl.rho: S(1), cyl.phi: pi, cyl.z: S(-1)}
     p_sph = {sph.r: sqrt(2), sph.theta: 3 * pi / 4, sph.phi: pi}
 
-    # The following vectors represent the same vector entity in different coordinate systems
-
-    v_cart = cart.j + cart.k * 2
+    u_cart = cart.j + cart.k * 2
 
     _, e_phi_cyl, e_z = cyl.base_vectors
-    v_cyl = -e_phi_cyl + e_z * 2
+    u_cyl = -e_phi_cyl + e_z * 2
 
     e_r, e_theta, e_phi_sph = sph.base_vectors
-    v_sph = e_r * -sqrt(2) - e_phi_sph + e_theta * -sqrt(2)
+    u_sph = e_r * -sqrt(2) - e_phi_sph + e_theta * -sqrt(2)
+
+    # These represent the same physical point `q` (which is different from `p`) in different
+    # coordinate systems
+    q_cart = {cart.x: S(1), cart.y: S(1), cart.z: S(1)}
+    q_cyl = {cyl.rho: sqrt(2), cyl.phi: pi / 4, cyl.z: S(1)}
+    q_sph = {sph.r: sqrt(3), sph.theta: pi / 4, sph.phi: pi / 4}
 
     return Args(
         cart=cart,
         cyl=cyl,
         sph=sph,
-        p_cart=p_cart,
-        p_cyl=p_cyl,
-        p_sph=p_sph,
-        v_cart=v_cart,
-        v_cyl=v_cyl,
-        v_sph=v_sph,
+        v_cart=AppliedVector(u_cart, p_cart, cart),
+        v_cyl=AppliedVector(u_cyl, p_cyl, cyl),
+        v_sph=AppliedVector(u_sph, p_sph, sph),
+        w_cart=AppliedVector(u_cart, q_cart, cart),
+        w_cyl=AppliedVector(u_cyl, q_cyl, cyl),
+        w_sph=AppliedVector(u_sph, q_sph, sph),
     )
 
 
-def convert_point(
-    old_point: Point,
-    old_system: BaseCoordinateSystem,
-    new_system: BaseCoordinateSystem,
-) -> Point:
-    # Point coordinates change contravariantly
-    conversion = express_base_scalars(new_system, old_system)  # pylint: disable=arguments-out-of-order
-    return {new_coordinate: expr.subs(old_point) for new_coordinate, expr in conversion.items()}
-
-
 def convert_vector(
-    old_vector: VectorExpr,
-    old_system: BaseCoordinateSystem,
+    old_vector: AppliedVector,
     new_system: BaseCoordinateSystem,
-    new_point: Point,
-) -> VectorExpr:
-    # Basic vectors change covariantly
-    conversion = express_base_vectors(old_system, new_system)
-    return old_vector.subs(conversion).subs(new_point).doit()
+) -> AppliedVector:
+    # Point coordinates change contravariantly
+    scalar_conversion = express_base_scalars(new_system, old_vector.system)
+
+    new_point = {
+        new_coordinate: expr.subs(old_vector.point)
+        for new_coordinate, expr in scalar_conversion.items()
+    }
+
+    # Base vectors change covariantly
+    vector_conversion = express_base_vectors(old_vector.system, new_system)
+    new_value = old_vector.value.subs(vector_conversion).subs(new_point).doit()
+
+    return AppliedVector(new_value, new_point, new_system)
 
 
-def equal_vectors(vector1: VectorExpr, vector2: VectorExpr) -> bool:
-    return bool(norm(vector1 - vector2) == 0)
+def equals(this: VectorExpr | AppliedVector, that: VectorExpr | AppliedVector) -> bool:
+    """
+    Checks if `this` and `that` are the same vector expression.
+    
+    Note that a zero-length vector can be compared to any other vector, but it is equal only to
+    itself.
+
+    Raises `ValueError` if any of the following conditions is not met:
+
+    1. Both vectors must be described in the same coordinate system.
+
+    2. If they are described in a non-Cartesian coordinate system, their points of application must
+       be the same.
+
+    3. If at least one of them is a `VectorExpr`, at least one of them must be a zero vector.
+    """
+
+    def equal_points(point1: Point, point2: Point) -> bool:
+        """
+        Checks if `point1` and `point2` have the same coordinates. Raises `KeyError` if the points
+        are described with different base scalars.
+        """
+
+        return all(expr_equals(coordinate1, point2[s]) for (s, coordinate1) in point1.items())
+
+    def equal_vectors(vector1: VectorExpr, vector2: VectorExpr) -> bool:
+        """
+        Checks if `vector1` and `vector2` are equal, i.e. if their difference is a zero vector.
+        """
+
+        return expr_equals(norm(vector1 - vector2), 0)
+
+    if isinstance(this, AppliedVector):
+        if isinstance(that, AppliedVector):
+            # Refer to condition #1
+            if this.system is not that.system:
+                raise ValueError("Vectors can only be compared in the same coordinate system.")
+
+            if (isinstance(this.system, CartesianCoordinateSystem) or
+                    equal_points(this.point, that.point)):
+                return equal_vectors(this.value, that.value)
+
+            # Refer to condition #2
+            raise ValueError("Vectors in non-Cartesian systems must be applied at the same point.")
+
+        this = this.value
+    elif isinstance(that, AppliedVector):
+        that = that.value
+
+    this_norm = norm(this)
+    that_norm = norm(that)
+
+    # The zero vector can be compared with any vector.
+    if expr_equals(this_norm, 0) or expr_equals(that_norm, 0):
+        return expr_equals(this_norm, that_norm)
+
+    # Refer to condition #3
+    raise ValueError("Non-zero vectors must have a point of application.")
 
 
 def test_cartesian_to_cartesian(test_args: Args) -> None:
     new_cart = CartesianCoordinateSystem()
 
-    new_point = convert_point(test_args.p_cart, test_args.cart, new_cart)
-    new_vector = convert_vector(test_args.v_cart, test_args.cart, new_cart, new_point)
+    new_vector = convert_vector(test_args.v_cart, new_cart)
 
+    correct_point = {new_cart.x: S(-1), new_cart.y: S(0), new_cart.z: S(-1)}
     _, j, k = new_cart.base_vectors
-    correct_vector = j + k * 2
+    correct_value = j + k * 2
+    correct_vector = AppliedVector(correct_value, correct_point, new_cart)
 
-    assert equal_vectors(new_vector, correct_vector)
+    assert equals(new_vector, correct_vector)
 
     # The transformation of basis vectors between Cartesian coordinates does *not* depend how we
     # choose the point where the vector is applied.
-    other_point = {new_cart.x: S(0), new_cart.y: S(-4), new_cart.z: S(5.5)}
-    other_vector = convert_vector(test_args.v_cart, test_args.cart, new_cart, other_point)
-    assert equal_vectors(new_vector, other_vector)
+    other_vector = convert_vector(test_args.w_cart, new_cart)
+    assert equals(new_vector, other_vector)
 
-    assert not equal_vectors(new_vector, ZERO)
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cyl)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_sph)
+
+    assert not equals(new_vector, ZERO)
 
 
 def test_cartesian_to_cylindrical(test_args: Args) -> None:
-    new_vector = convert_vector(test_args.v_cart, test_args.cart, test_args.cyl, test_args.p_cyl)
+    new_vector = convert_vector(test_args.v_cart, test_args.cyl)
+    assert equals(new_vector, test_args.v_cyl)
 
-    assert equal_vectors(new_vector, test_args.v_cyl)
+    # Cannot compare vectors applied at different points in non-Cartesian systems
+    other_vector = convert_vector(test_args.w_cart, test_args.cyl)
+    with raises(ValueError):
+        equals(new_vector, other_vector)
 
-    # The transformation of basis vectors when at least one of the systems is not Cartesian depends
-    # on the point where the vector is applied.
-    other_point = {test_args.cyl.rho: S(2), test_args.cyl.phi: pi / 3, test_args.cyl.z: S(-1)}
-    other_vector = convert_vector(test_args.v_cart, test_args.cart, test_args.cyl, other_point)
-    assert not equal_vectors(new_vector, other_vector)
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cart)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_sph)
 
-    assert not equal_vectors(new_vector, ZERO)
+    assert not equals(new_vector, ZERO)
 
 
 def test_cartesian_to_spherical(test_args: Args) -> None:
-    new_vector = convert_vector(test_args.v_cart, test_args.cart, test_args.sph, test_args.p_sph)
+    new_vector = convert_vector(test_args.v_cart, test_args.sph)
+    assert equals(new_vector, test_args.v_sph)
 
-    assert equal_vectors(new_vector, test_args.v_sph)
+    # Cannot compare vectors applied at different points in non-Cartesian systems
+    other_vector = convert_vector(test_args.w_cart, test_args.sph)
+    with raises(ValueError):
+        equals(new_vector, other_vector)
 
-    other_point = {test_args.sph.r: S(4), test_args.sph.theta: 2 * pi / 3, test_args.sph.phi: pi}
-    other_vector = convert_vector(test_args.v_cart, test_args.cart, test_args.sph, other_point)
-    assert not equal_vectors(new_vector, other_vector)
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cart)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cyl)
 
-    assert not equal_vectors(new_vector, ZERO)
+    assert not equals(new_vector, ZERO)
 
 
 def test_cylindrical_to_cartesian(test_args: Args) -> None:
-    new_vector = convert_vector(test_args.v_cyl, test_args.cyl, test_args.cart, test_args.p_cart)
+    new_vector = convert_vector(test_args.v_cyl, test_args.cart)
+    assert equals(new_vector, test_args.v_cart)
 
-    assert equal_vectors(new_vector, test_args.v_cart)
+    # In non-Cartesian systems, base vectors depend on the point of application.
+    other_vector = convert_vector(test_args.w_cyl, test_args.cart)
+    assert not equals(new_vector, other_vector)
 
-    other_point = {test_args.cart.x: S(3), test_args.cart.y: S(0), test_args.cart.z: S(-1)}
-    other_vector = convert_vector(test_args.v_cyl, test_args.cyl, test_args.cart, other_point)
-    assert not equal_vectors(new_vector, other_vector)
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cyl)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_sph)
 
-    assert not equal_vectors(new_vector, ZERO)
+    assert not equals(new_vector, ZERO)
 
 
 def test_cylindrical_to_cylindrical(test_args: Args) -> None:
     new_cyl = CylindricalCoordinateSystem()
 
-    new_point = convert_point(test_args.p_cyl, test_args.cyl, new_cyl)
-    new_vector = convert_vector(test_args.v_cyl, test_args.cyl, new_cyl, new_point)
+    new_vector = convert_vector(test_args.v_cyl, new_cyl)
 
+    rho, phi, z = new_cyl.base_scalars
+    correct_point = {rho: S(1), phi: pi, z: S(-1)}
     _, e_phi, e_z = new_cyl.base_vectors
-    correct_vector = -e_phi + e_z * 2
+    correct_value = -e_phi + e_z * 2
+    correct_vector = AppliedVector(correct_value, correct_point, new_cyl)
 
-    assert equal_vectors(new_vector, correct_vector)
+    assert equals(new_vector, correct_vector)
 
-    other_point = {new_cyl.rho: S(10), new_cyl.phi: S(0), new_cyl.z: S(-1)}
-    other_vector = convert_vector(test_args.v_cyl, test_args.cyl, new_cyl, other_point)
-    assert equal_vectors(new_vector, other_vector)  # FIXME! they should be different
+    # Cannot compare vectors applied at different points in non-Cartesian systems
+    other_vector = convert_vector(test_args.w_cyl, new_cyl)
+    with raises(ValueError):
+        equals(new_vector, other_vector)
 
-    assert not equal_vectors(new_vector, ZERO)
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cart)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_sph)
+
+    assert not equals(new_vector, ZERO)
 
 
 def test_cylindrical_to_spherical(test_args: Args) -> None:
-    new_vector = convert_vector(test_args.v_cyl, test_args.cyl, test_args.sph, test_args.p_sph)
+    new_vector = convert_vector(test_args.v_cyl, test_args.sph)
+    assert equals(new_vector, test_args.v_sph)
 
-    assert equal_vectors(new_vector, test_args.v_sph)
+    # Cannot compare vectors applied at different points in non-Cartesian systems
+    other_vector = convert_vector(test_args.w_cyl, test_args.sph)
+    with raises(ValueError):
+        equals(new_vector, other_vector)
 
-    assert not equal_vectors(new_vector, ZERO)
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cart)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cyl)
+
+    assert not equals(new_vector, ZERO)
 
 
 def test_spherical_to_cartesian(test_args: Args) -> None:
-    new_vector = convert_vector(test_args.v_sph, test_args.sph, test_args.cart, test_args.p_cart)
+    new_vector = convert_vector(test_args.v_sph, test_args.cart)
+    assert equals(new_vector, test_args.v_cart)
 
-    assert equal_vectors(new_vector, test_args.v_cart)
+    # In non-Cartesian systems, base vectors depend on the point of application.
+    other_vector = convert_vector(test_args.w_sph, test_args.cart)
+    assert not equals(new_vector, other_vector)
 
-    assert not equal_vectors(new_vector, ZERO)
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cyl)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_sph)
+
+    assert not equals(new_vector, ZERO)
 
 
 def test_spherical_to_cylindrical(test_args: Args) -> None:
-    new_vector = convert_vector(test_args.v_sph, test_args.sph, test_args.cyl, test_args.p_cyl)
+    new_vector = convert_vector(test_args.v_sph, test_args.cyl)
+    assert equals(new_vector, test_args.v_cyl)
 
-    assert equal_vectors(new_vector, test_args.v_cyl)
+    # Cannot compare vectors applied at different points in non-Cartesian systems
+    other_vector = convert_vector(test_args.w_sph, test_args.cyl)
+    with raises(ValueError):
+        equals(new_vector, other_vector)
 
-    assert not equal_vectors(new_vector, ZERO)
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cart)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_sph)
+
+    assert not equals(new_vector, ZERO)
 
 
 def test_spherical_to_spherical(test_args: Args) -> None:
     new_sph = SphericalCoordinateSystem()
 
-    new_point = convert_point(test_args.p_sph, test_args.sph, new_sph)
-    new_vector = convert_vector(test_args.v_sph, test_args.sph, new_sph, new_point)
+    new_vector = convert_vector(test_args.v_sph, new_sph)
 
+    r, theta, phi = new_sph.base_scalars
+    correct_point = {r: sqrt(2), theta: 3 * pi / 4, phi: pi}
     e_r, e_theta, e_phi = new_sph.base_vectors
-    correct_vector = e_r * -sqrt(2) - e_phi + e_theta * -sqrt(2)
+    correct_value = e_r * -sqrt(2) - e_phi + e_theta * -sqrt(2)
+    correct_vector = AppliedVector(correct_value, correct_point, new_sph)
 
-    assert equal_vectors(new_vector, correct_vector)
+    assert equals(new_vector, correct_vector)
 
-    assert not equal_vectors(new_vector, ZERO)
+    # Cannot compare vectors applied at different points in non-Cartesian systems
+    other_vector = convert_vector(test_args.w_sph, new_sph)
+    with raises(ValueError):
+        equals(new_vector, other_vector)
+
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cart)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cyl)
+
+    assert not equals(new_vector, ZERO)
 
 
 def test_unregistered_coordinate_system(test_args: Args) -> None:
@@ -223,11 +370,29 @@ def test_unregistered_coordinate_system(test_args: Args) -> None:
 
 
 def test_same_system(test_args: Args) -> None:
-    new_vector = convert_vector(test_args.v_cart, test_args.cart, test_args.cart, test_args.p_cart)
-    assert equal_vectors(new_vector, test_args.v_cart)
+    new_vector = convert_vector(test_args.v_cart, test_args.cart)
+    assert equals(new_vector, test_args.v_cart)
 
-    new_vector = convert_vector(test_args.v_cyl, test_args.cyl, test_args.cyl, test_args.p_cyl)
-    assert equal_vectors(new_vector, test_args.v_cyl)
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cyl)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_sph)
 
-    new_vector = convert_vector(test_args.v_sph, test_args.sph, test_args.sph, test_args.p_sph)
-    assert equal_vectors(new_vector, test_args.v_sph)
+    new_vector = convert_vector(test_args.v_cyl, test_args.cyl)
+    assert equals(new_vector, test_args.v_cyl)
+
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cart)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_sph)
+
+    new_vector = convert_vector(test_args.v_sph, test_args.sph)
+    assert equals(new_vector, test_args.v_sph)
+
+    # Check that the vector is not described in other coordinate systems
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cart)
+    with raises(ValueError):
+        equals(new_vector, test_args.v_cyl)
