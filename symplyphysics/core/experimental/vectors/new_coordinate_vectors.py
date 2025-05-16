1999cb4a2e8b85e0b14c35139214cc88537deeab
Aleksandr Slatov <alexslatov@gmail.com>
2025-05-16 16:41:44 +0400

    Add point of application to vectors

--- symplyphysics/core/experimental/vectors/new_coordinate_vectors.py
+++ symplyphysics/core/experimental/vectors/new_coordinate_vectors.py
@@ -1,15 +1,105 @@
-from typing import Sequence, Optional
-from sympy import ImmutableMatrix, Expr, sqrt, Symbol as SymSymbol, Basic, Derivative as SymDerivative
+from __future__ import annotations
+
+from typing import Sequence, Optional, Iterable, SupportsFloat, Sized, Any
+from sympy import (ImmutableMatrix, Expr, sqrt, Symbol as SymSymbol, Basic, Derivative as
+    SymDerivative, Atom)
+from sympy.printing.printer import Printer
 from sympy.matrices.dense import DenseMatrix
+
+from symplyphysics.core.expr_comparisons import expr_equals
 
 from ..miscellaneous import sympify_expr
 from ..coordinate_systems.new_coordinate_systems import BaseCoordinateSystem, CartesianCoordinateSystem
 from . import VectorExpr, is_vector_expr, into_terms, split_factor, VectorCross
 
 
+def _prepare(coordinates: Iterable[SupportsFloat], system: BaseCoordinateSystem) -> dict[SymSymbol,
+    Expr]:
+    return {
+        scalar: sympify_expr(coordinate)
+        for scalar, coordinate in zip(system.base_scalars, coordinates)
+    }
+
+
+class AppliedPoint(Atom):
+    """
+    An `AppliedPoint` corresponds to a point in (3D) space whose coordinates are defined within a
+    certain coordinate system.
+    """
+
+    _coordinates: dict[SymSymbol, Expr]
+    _system: BaseCoordinateSystem
+
+    _iterable = False
+
+    @property
+    def coordinates(self) -> dict[SymSymbol, Expr]:
+        return self._coordinates
+
+    @property
+    def system(self) -> BaseCoordinateSystem:
+        return self._system
+
+    def __getitem__(self, base_scalar: SymSymbol) -> Expr:
+        return self.coordinates[base_scalar]
+
+    def __new__(
+        cls,
+        coordinates: Iterable[Any],
+        system: BaseCoordinateSystem,
+    ) -> AppliedPoint:
+        return super().__new__(cls)  # pylint: disable=no-value-for-parameter
+
+    def __init__(
+        self,
+        coordinates: Iterable[Any],
+        system: BaseCoordinateSystem,
+    ):
+        super().__init__()
+
+        if isinstance(coordinates, Sized):
+            n = len(coordinates)  # can't extract out of if-block, mypy complains otherwise
+        else:
+            coordinates = tuple(coordinates)
+            n = len(coordinates)
+
+        if n != 3:
+            raise ValueError(f"The point must have all 3 coordinates defined, got {n}.")
+
+        self._coordinates = _prepare(coordinates, system)
+        self._system = system
+
+    def _sympystr(self, p: Printer) -> str:
+        system_name = type(self.system).__name__.removesuffix("CoordinateSystem")
+        point_name = f"{system_name}Point"
+
+        coordinates = ", ".join(
+            f"{p.doprint(s)} = {p.doprint(c)}" for s, c in self.coordinates.items())
+
+        return f"{point_name}({coordinates})"
+
+    def _hashable_content(self) -> tuple[Any, ...]:
+        return (tuple(self.coordinates.items()),)
+
+    def equals(self, other: AppliedPoint) -> bool:
+        for base_scalar, coordinate in self.coordinates.items():
+            if base_scalar not in other.coordinates:
+                return False
+
+            if not expr_equals(coordinate, other[base_scalar]):
+                return False
+
+        return True
+
+
+# Used as a common point for all Cartesian vectors
+_CartesianVectorPoint = SymSymbol("P")
+
+
 class CoordinateVector(VectorExpr):
     _components: ImmutableMatrix
     _system: BaseCoordinateSystem
+    _point: AppliedPoint | SymSymbol
 
     is_number = False
     is_atomic_vector = True
@@ -23,25 +113,45 @@ class CoordinateVector(VectorExpr):
         return self._system
 
     @property
+    def point(self) -> AppliedPoint | SymSymbol:
+        return self._point
+
+    @property
     def free_symbols(self) -> set[Expr]:
         return self.components.free_symbols  # type: ignore[no-any-return]
 
     def _hashable_content(self) -> tuple[Basic, ...]:
-        return self.components, self.system
+        return self.components, self.system, self.point
 
-    def __init__(self, components: Sequence[Expr], system: BaseCoordinateSystem) -> None:
+    def __init__(
+        self,
+        components: Sequence[Expr],
+        system: BaseCoordinateSystem,
+        point: Optional[AppliedPoint | SymSymbol] = None,
+    ) -> None:
         super().__init__()
 
-        if isinstance(components, DenseMatrix):
-            if 1 not in components.shape:
-                rows, cols = components.shape
-                message = f"Expected a row or column vector, got a {rows} by {cols} matrix"
-                raise ValueError(message)
+        if isinstance(components, DenseMatrix) and 1 not in components.shape:
+            rows, cols = components.shape
+            message = f"Expected a row or column vector, got a {rows} by {cols} matrix"
+            raise ValueError(message)
+
+        if isinstance(system, CartesianCoordinateSystem):
+            # Cartesian vectors are independent of their point of application.
+            point = _CartesianVectorPoint
+        elif point is None:
+            message = "The point of application must be defined for vectors in non-Cartesian systems"
+            raise ValueError(message)
+
+        if isinstance(point, AppliedPoint) and system != point.system:
+            message = "The system of the vector and the point of its application must coincide"
+            raise ValueError(message)
 
         components = [sympify_expr(component) for component in components]
         self._components = ImmutableMatrix(components)
 
         self._system = system
+        self._point = point
 
     def __str__(self) -> str:
         name = type(self.system).__name__.removesuffix("CoordinateSystem")
@@ -68,14 +178,14 @@ class CoordinateVector(VectorExpr):
         if not (isinstance(lhs, CoordinateVector) and isinstance(rhs, CoordinateVector)):
             return None
 
-        if type(lhs.system) is not type(rhs.system):
+        if type(lhs.system) is not type(rhs.system) or lhs.point != rhs.point:
             return None
 
         components = lhs.components.cross(rhs.components)
 
-        return CoordinateVector(components, lhs.system)
+        return CoordinateVector(components, lhs.system, lhs.point)
 
-    def _eval_derivative(self, symbol: SymSymbol) -> VectorExpr:
+    def _eval_derivative(self, symbol: Expr) -> VectorExpr:
         # Needed for `sympy.solve` to work
         if is_vector_expr(symbol):
             return SymDerivative(self, symbol, evaluate=False)  # type: ignore[no-any-return]
@@ -90,12 +200,13 @@ class CoordinateVector(VectorExpr):
             diff_matrix = self.system.diff_base_vector_matrix(symbol, applied_base_scalars)
             diff_base_vectors = self.components.T * diff_matrix
 
-        return CoordinateVector(diff_components + diff_base_vectors, self.system)
+        return CoordinateVector(diff_components + diff_base_vectors, self.system, self.point)
 
 
 def combine_coordinate_vectors(expr: Expr) -> Expr:
     non_coordinate = []
-    system_to_components: dict[BaseCoordinateSystem, ImmutableMatrix] = {}
+    system_to_components: dict[tuple[BaseCoordinateSystem, Optional[AppliedPoint | SymSymbol]],
+        ImmutableMatrix] = {}
 
     for cross in expr.atoms(VectorCross):
         lhs, rhs = cross.args
@@ -110,16 +221,16 @@ def combine_coordinate_vectors(expr: Expr) -> Expr:
             non_coordinate.append(term)
             continue
 
-        total = system_to_components.get(vector.system, ImmutableMatrix.zeros(3, 1))
+        total = system_to_components.get((vector.system, vector.point), ImmutableMatrix.zeros(3, 1))
         mul = factor * vector.components
-        system_to_components[vector.system] = total + mul
+        system_to_components[vector.system, vector.point] = total + mul
 
     result = sum(non_coordinate)
 
-    for system, components in system_to_components.items():
+    for (system, point), components in system_to_components.items():
         if components.is_zero_matrix:
             continue
 
-        result += CoordinateVector(components, system)
+        result += CoordinateVector(components, system, point)
 
     return result
--- test/core/experimental/vectors/test_new_coordinate_vectors.py
+++ test/core/experimental/vectors/test_new_coordinate_vectors.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 from pytest import raises, fixture
-from sympy import Symbol as SymSymbol, Function as SymFunction, ImmutableMatrix, sin, cos, sqrt, symbols as sym_symbols
+from sympy import (Symbol as SymSymbol, Function as SymFunction, ImmutableMatrix, sin, cos, sqrt,
+    symbols as sym_symbols)
 from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.core.experimental.vectors import (is_vector_expr, VectorSymbol, VectorNorm as
     norm, VectorDot as dot, VectorCross as cross, vector_diff)
 from symplyphysics.core.experimental.coordinate_systems.new_coordinate_systems import (
     CartesianCoordinateSystem, CylindricalCoordinateSystem, SphericalCoordinateSystem)
-from symplyphysics.core.experimental.vectors.new_coordinate_vectors import CoordinateVector, combine_coordinate_vectors
+from symplyphysics.core.experimental.vectors.new_coordinate_vectors import (CoordinateVector,
+    combine_coordinate_vectors, AppliedPoint)
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -15,6 +17,7 @@ class Args:
     cyl: CylindricalCoordinateSystem
     sph: SphericalCoordinateSystem
     t: SymSymbol
+    p: SymSymbol
 
 
 @fixture(name="test_args")
@@ -23,8 +26,9 @@ def test_args_fixture() -> Args:
     cyl = CylindricalCoordinateSystem()
     sph = SphericalCoordinateSystem()
     t = SymSymbol("t", real=True)
+    p = SymSymbol("P", commutative=False)  # point symbol
 
-    return Args(cart=cart, cyl=cyl, sph=sph, t=t)
+    return Args(cart=cart, cyl=cyl, sph=sph, t=t, p=p)
 
 
 def test_cartesian_coordinate_vector(test_args: Args) -> None:
@@ -55,29 +59,38 @@ def test_cartesian_coordinate_vector(test_args: Args) -> None:
     assert vector_diff(v1, t) == 0
 
     vt = CoordinateVector([t, t, -t], test_args.cart)
-    assert vector_diff(vt) == CoordinateVector([1, 1, -1], test_args.cart)
+    actual = vector_diff(vt, t)
+    correct = CoordinateVector([1, 1, -1], test_args.cart)
+    assert actual == correct
+
+    v1 = CoordinateVector([1, 1, 0], test_args.cart)
+    v2 = CoordinateVector([1, 1, 0], test_args.cart, test_args.p)
+    v3 = CoordinateVector([1, 1, 0], test_args.cart, AppliedPoint([1, -1, -1], test_args.cart))
+    assert v1 == v2
+    assert v1 == v3
 
 
 def test_cylindrical_coordinate_vector(test_args: Args) -> None:
-    v1 = CoordinateVector([1, 1, 1], test_args.cyl)
+    v1 = CoordinateVector([1, 1, 1], test_args.cyl, test_args.p)
     assert v1.components == ImmutableMatrix([1, 1, 1])
     assert v1.system is test_args.cyl
 
-    v2 = CoordinateVector([2, -1, -2], test_args.cyl)
+    v2 = CoordinateVector([2, -1, -2], test_args.cyl, test_args.p)
     assert v2.system is v1.system
 
     assert is_vector_expr(v1)
     assert is_vector_expr(v2)
     assert is_vector_expr(v1 + v2)
-    assert is_vector_expr(-v1 + 3 * v2)
+    assert is_vector_expr(-1 * v1 + 3 * v2)
 
-    assert combine_coordinate_vectors(v1 - v2) == CoordinateVector([-1, 2, 3], test_args.cyl)
+    correct = CoordinateVector([-1, 2, 3], test_args.cyl, test_args.p)
+    assert combine_coordinate_vectors(v1 - v2) == correct
 
     assert dot(v1, v2) == -1
     assert norm(v2) == 3
 
     v3 = cross(v1, v2)
-    assert v3 == CoordinateVector([-1, 4, -3], test_args.cyl)
+    assert v3 == CoordinateVector([-1, 4, -3], test_args.cyl, test_args.p)
 
     # FIXME: `sympy.Derivative` requires that the differentiated expression contain the
     # differentiation symbol, but `CoordinateVector`s depend on it in general, therefore a fix
@@ -85,31 +98,32 @@ def test_cylindrical_coordinate_vector(test_args: Args) -> None:
     _, phi_f, _ = test_args.cyl.base_scalar_functions
     g = sym_symbols("g", cls=SymFunction)
     t = test_args.t
-    v = CoordinateVector([g(t) + 1, 1, 1], test_args.cyl)
-    dv_dt = CoordinateVector([-2 * t, 2 * t, 0], test_args.cyl)
+    v = CoordinateVector([g(t) + 1, 1, 1], test_args.cyl, test_args.p)
+    dv_dt = CoordinateVector([-2 * t, 2 * t, 0], test_args.cyl, test_args.p)
     assert vector_diff(v, t).subs(g(t), 0).replace(phi_f, lambda t: 1 + t**2).doit() == dv_dt
 
 
 def test_spherical_coordinate_vector(test_args: Args) -> None:
-    v1 = CoordinateVector([1, 1, 1], test_args.sph)
+    v1 = CoordinateVector([1, 1, 1], test_args.sph, test_args.p)
     assert v1.components == ImmutableMatrix([1, 1, 1])
     assert v1.system is test_args.sph
 
-    v2 = CoordinateVector([2, -1, -2], test_args.sph)
+    v2 = CoordinateVector([2, -1, -2], test_args.sph, test_args.p)
     assert v2.system is v1.system
 
     assert is_vector_expr(v1)
     assert is_vector_expr(v2)
     assert is_vector_expr(v1 + v2)
-    assert is_vector_expr(-v1 + 3 * v2)
+    assert is_vector_expr(-1 * v1 + 3 * v2)
 
-    assert combine_coordinate_vectors(v1 - v2) == CoordinateVector([-1, 2, 3], test_args.sph)
+    correct = CoordinateVector([-1, 2, 3], test_args.sph, test_args.p)
+    assert combine_coordinate_vectors(v1 - v2) == correct
 
     assert dot(v1, v2) == -1
     assert norm(combine_coordinate_vectors(v1 - v2)) == sqrt(14)
 
     v3 = cross(v1, v2)
-    assert v3 == CoordinateVector([-1, 4, -3], test_args.sph)
+    assert v3 == CoordinateVector([-1, 4, -3], test_args.sph, test_args.p)
 
     # FIXME: `sympy.Derivative` requires that the differentiated expression contain the
     # differentiation symbol, but `CoordinateVector`s depend on it in general, therefore a fix
@@ -117,10 +131,11 @@ def test_spherical_coordinate_vector(test_args: Args) -> None:
     _, theta_f, phi_f = test_args.sph.base_scalar_functions
     g = sym_symbols("g", cls=SymFunction)
     t = test_args.t
-    v = CoordinateVector([g(t) + 1, 1, 1], test_args.sph)
+    v = CoordinateVector([g(t) + 1, 1, 1], test_args.sph, test_args.p)
     dv_dt = CoordinateVector(
         [-1 + sin(t) / t**2, 1 + cos(t) / t**2, -(sin(t) + cos(t)) / t**2],
         test_args.sph,
+        test_args.p,
     )
     actual = vector_diff(v, t).subs(g(t), 0).replace(theta_f,
         lambda t: t).replace(phi_f, lambda t: 1 / t).doit()
@@ -136,11 +151,16 @@ def test_wrong_components(test_args: Args) -> None:
     components = ImmutableMatrix([[1, 1], [2, 2]])
 
     with raises(ValueError):
-        _ = CoordinateVector(components, test_args.cyl)
+        _ = CoordinateVector(components, test_args.cyl, test_args.p)
 
     components = [1, 2, "3"]
     with raises(ValueError):
-        _ = CoordinateVector(components, test_args.sph)
+        _ = CoordinateVector(components, test_args.sph, test_args.p)
+
+
+def test_wrong_point(test_args: Args) -> None:
+    with raises(ValueError):
+        _ = CoordinateVector([0, 1, 1], test_args.cyl, AppliedPoint([1, 1, 1], test_args.cart))
 
 
 def test_combine_coordinate_vectors(test_args: Args) -> None:
@@ -148,10 +168,10 @@ def test_combine_coordinate_vectors(test_args: Args) -> None:
     v2 = CoordinateVector([1, -1, 1], test_args.cart)
     v3 = CoordinateVector([1, 1, 1], test_args.cart)
 
-    v4 = CoordinateVector([-1, -1, -1], test_args.cyl)
-    v5 = CoordinateVector([1, 1, 1], test_args.cyl)
+    v4 = CoordinateVector([-1, -1, -1], test_args.cyl, test_args.p)
+    v5 = CoordinateVector([1, 1, 1], test_args.cyl, test_args.p)
 
-    v6 = CoordinateVector([4, 4, 4], test_args.sph)
+    v6 = CoordinateVector([4, 4, 4], test_args.sph, test_args.p)
 
     v_coord = v1 + v2 + v3 + v4 + v5 + v6
 
@@ -166,6 +186,6 @@ def test_combine_coordinate_vectors(test_args: Args) -> None:
     actual = combine_coordinate_vectors(v)
 
     combined = (CoordinateVector([2, 0, 2], test_args.cart) +
-        CoordinateVector([4, 4, 4], test_args.sph) + v7 + v8 + v9)
+        CoordinateVector([4, 4, 4], test_args.sph, test_args.p) + v7 + v8 + v9)
 
     assert norm(combine_coordinate_vectors(actual - combined)) == 0
