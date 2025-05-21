cfb6144ae67db22cb57630b5b267559bd56d6e55
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-05-22 01:03:00 +0400

    Fix coordinate systems (#988)
    * Refactor misc functions
    
    * Split into separate files
    
    * Fix equality behavior
    
    * Fix issues

---
+++ symplyphysics/core/experimental/coordinate_systems/__init__.py
@@ -0,0 +1,22 @@
+from .base_system import BaseCoordinateSystem
+from .cartesian_system import CartesianCoordinateSystem
+from .cylindrical_system import CylindricalCoordinateSystem
+from .spherical_system import SphericalCoordinateSystem
+
+from .point import AppliedPoint
+from .scalar import CoordinateScalar
+from .vector import CoordinateVector, combine_coordinate_vectors
+
+__all__ = [
+    # Most common coordinate system classes
+    "BaseCoordinateSystem",
+    "CartesianCoordinateSystem",
+    "CylindricalCoordinateSystem",
+    "SphericalCoordinateSystem",
+
+    # Miscellaneous
+    "AppliedPoint",
+    "CoordinateScalar",
+    "CoordinateVector",
+    "combine_coordinate_vectors",
+]
