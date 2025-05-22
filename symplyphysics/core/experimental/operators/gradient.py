430e860dc94d0d254a86ecf520d583002cd4542f
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-05-22 09:32:22 +0400

    Add gradient functionality (#989)
    * Add (partial) functionality and tests
    
    * Add __init__
    
    * Fix test
    
    * Fix core

---
+++ symplyphysics/core/experimental/operators/gradient.py
@@ -0,0 +1,60 @@
+from typing import Optional, Any
+
+from sympy import Expr, S, ImmutableMatrix, diff
+
+from ..miscellaneous import evaluate_or_global_fallback, sympify_expr
+from ..vectors import VectorExpr, is_vector_expr
+from ..coordinate_systems import CoordinateScalar, CoordinateVector
+
+
+class VectorGradient(VectorExpr):  # pylint: disable=too-few-public-methods
+    """
+    Assuming `f` is a scalar-values differentiable function of several variables, its **gradient**
+    is a vector field (i.e. a vector-valued function) `grad(f)` whose value at a point `P` gives
+    the direction and the rate of fastest increase.
+
+    In an arbitrary curvilinear orthogonal coordinate system with base scalars `{q_i}` and Lamé
+    coefficients `{h_i}`, the gradient of `f({q_i})` can be calculated as follows::
+
+        grad(f) = sum(diff(f, q_i) / h_i * e_i, i)
+
+    Here, `diff` is the differentiation operator, and `e_i` is the `i`-th unit base vector.
+    """
+
+    def __new__(
+        cls,
+        scalar: Any,
+        *,
+        evaluate: Optional[bool] = None,
+    ) -> Expr:
+        if scalar == 0:
+            return S.Zero
+
+        scalar = sympify_expr(scalar)
+
+        if is_vector_expr(scalar):
+            raise ValueError(f"Expected scalar, got vector: {scalar}")
+
+        evaluate = evaluate_or_global_fallback(evaluate)
+
+        if not evaluate:
+            obj = super().__new__(cls)  # pylint: disable=no-value-for-parameter
+            obj._args = (scalar,)
+
+            return obj
+
+        if not isinstance(scalar, CoordinateScalar):
+            return S.Zero
+
+        the_scalar = scalar.scalar
+        system = scalar.system
+        base_scalars = system.base_scalars
+        lame_coefficients = system.lame_coefficients(base_scalars)
+
+        components = ImmutableMatrix(
+            [diff(the_scalar, q) / h for q, h in zip(base_scalars, lame_coefficients)])
+
+        return CoordinateVector(components, system, scalar.point)
+
+
+__all__ = ["VectorGradient"]
---
+++ test/core/experimental/operators/gradient_test.py
@@ -0,0 +1,114 @@
+from typing import Sequence, Any
+from pytest import raises
+
+from sympy import sin, cos, Symbol as SymSymbol, Expr, ImmutableMatrix
+
+from symplyphysics.core.experimental.vectors import VectorSymbol
+from symplyphysics.core.experimental.coordinate_systems import (BaseCoordinateSystem,
+    CartesianCoordinateSystem, CylindricalCoordinateSystem, SphericalCoordinateSystem)
+from symplyphysics.core.experimental.coordinate_systems.vector import CoordinateVector
+from symplyphysics.core.experimental.coordinate_systems.scalar import CoordinateScalar
+from symplyphysics.core.experimental.operators import VectorGradient
+from symplyphysics.core.experimental.solvers import vector_equals
+
+
+def test_non_coordinate_scalar() -> None:
+    assert vector_equals(VectorGradient(0), 0)
+    assert vector_equals(VectorGradient(1), 0)
+
+    x = SymSymbol("x", real=True)
+    assert vector_equals(VectorGradient(x), 0)  # not a CoordinateScalar
+
+    v = VectorSymbol("v")
+    with raises(ValueError):
+        _ = VectorGradient(v)
+
+    cartesian = CartesianCoordinateSystem()
+    v = CoordinateVector([1, 2, 3], cartesian)
+    with raises(ValueError):
+        _ = VectorGradient(v)
+
+
+def test_non_evaluate() -> None:
+    assert vector_equals(VectorGradient(0, evaluate=False), 0)
+
+    grad = VectorGradient(1, evaluate=False)
+    assert isinstance(grad, VectorGradient)
+    assert grad.args == (1,)
+    assert vector_equals(grad, 0)  # evaluate
+
+    x = SymSymbol("x", real=True)
+    grad = VectorGradient(x, evaluate=False)
+    assert isinstance(grad, VectorGradient)
+    assert grad.args == (x,)
+    assert vector_equals(grad, 0)  # evaluate
+
+    v = VectorSymbol("v")
+    with raises(ValueError):
+        _ = VectorGradient(v, evaluate=False)
+
+    cartesian = CartesianCoordinateSystem()
+    v = CoordinateVector([1, 2, 3], cartesian)
+    with raises(ValueError):
+        _ = VectorGradient(v, evaluate=False)
+
+    x = cartesian.base_scalars[0]
+    s = CoordinateScalar(x, cartesian)
+    grad = VectorGradient(s, evaluate=False)
+    assert grad.args == (s,)
+    assert vector_equals(grad.doit(), CoordinateVector([1, 0, 0], cartesian))
+
+
+def _check(system: BaseCoordinateSystem, value: Expr, components: Sequence[Any]) -> None:
+    p = SymSymbol("P")
+
+    scalar = CoordinateScalar(value, system, p)
+
+    gradient = VectorGradient(scalar)
+
+    expected = CoordinateVector(components, system, p)
+
+    assert vector_equals(gradient, expected)
+
+
+def test_cartesian_system() -> None:
+    system = CartesianCoordinateSystem()
+
+    x, y, z = system.base_scalars
+
+    value = sin(x + y + z)
+    _check(system, value, ImmutableMatrix.ones(3, 1) * cos(x + y + z))
+
+    value = (x**2 + y**2 + z**2) / 2
+    _check(system, value, [x, y, z])
+
+    value = x * y + y * z + z * x
+    _check(system, value, [y + z, x + z, x + y])
+
+
+def test_cylindrical_system() -> None:
+    system = CylindricalCoordinateSystem()
+
+    rho, phi, z = system.base_scalars
+
+    value = rho * sin(phi) - z
+    _check(system, value, [sin(phi), cos(phi), -1])
+
+    value = (rho**2 + z**2) / 2
+    _check(system, value, [rho, 0, z])
+
+
+def test_spherical_system() -> None:
+    system = SphericalCoordinateSystem()
+
+    r, theta, phi = system.base_scalars
+
+    value = r * sin(theta - phi)
+    _check(system, value, [
+        sin(theta - phi),
+        cos(theta - phi),
+        -cos(theta - phi) / sin(theta),
+    ])
+
+    value = r**2 / 2
+    _check(system, value, [r, 0, 0])
