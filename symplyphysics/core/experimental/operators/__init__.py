5b26f2ed037dcfc84146d70f3d355c1d624a9d8b
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-05-23 17:45:12 +0400

    Add divergence functionality (#991)
    * Add (partial) functionality and tests
    
    * Add tests

--- symplyphysics/core/experimental/operators/__init__.py
+++ symplyphysics/core/experimental/operators/__init__.py
@@ -1,7 +1,9 @@
 from .gradient import VectorGradient
+from .divergence import VectorDivergence
 from .curl import VectorCurl
 
 __all__ = [
     "VectorGradient",
+    "VectorDivergence",
     "VectorCurl",
 ]
