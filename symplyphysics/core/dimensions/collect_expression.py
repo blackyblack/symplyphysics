e8a59283efb997797f352f1ccab968beff4fef72
Aleksandr Slatov <alexslatov@gmail.com>
2025-05-30 13:13:30 +0400

    Add functionality and tests

--- symplyphysics/core/dimensions/collect_expression.py
+++ symplyphysics/core/dimensions/collect_expression.py
@@ -1,21 +1,29 @@
+from __future__ import annotations
+
 from typing import Iterable, Callable, Any, SupportsFloat
-from sympy import Expr, S, Mul, Add, Pow, Abs, Min, Max, Derivative, Function as SymFunction, sympify
+from sympy import Expr, S, Mul, Add, Pow, Abs, Min, Max, Derivative, Function as SymFunction, sympify, Basic
 from sympy.functions.elementary.miscellaneous import MinMaxBase
+from sympy.physics import units
 from sympy.physics.units import Dimension, Quantity as SymQuantity
 from sympy.physics.units.systems.si import dimsys_SI
+
+from ..experimental.vectors import (VectorDot, VectorCross, VectorNorm, VectorMixedProduct,
+    AppliedVectorFunction)
+from ..experimental.coordinate_systems import CoordinateScalar, CoordinateVector
+from ..experimental.operators import VectorGradient, VectorDivergence, VectorCurl, VectorLaplacian
 
 from ..errors import UnitsError
 from ..symbols.quantities import Quantity
 from .miscellaneous import is_number, is_any_dimension, dimensionless
 
 
-def _split_numeric_and_symbolic(
-    expr: Expr,) -> tuple[list[Expr], list[SymQuantity], list[tuple[Expr, Dimension]]]:
+def split_numeric_and_symbolic(
+    args: Iterable[Expr],) -> tuple[list[Expr], list[SymQuantity], list[tuple[Expr, Dimension]]]:
     nums: list[Expr] = []
     qtys: list[SymQuantity] = []
     syms: list[tuple[Expr, Dimension]] = []
 
-    for arg in expr.args:
+    for arg in args:
         if isinstance(arg, SymQuantity):
             qtys.append(arg)
         elif is_number(arg):
@@ -27,7 +35,7 @@ def _split_numeric_and_symbolic(
 
 
 def _collect_mul(expr: Mul) -> tuple[Expr, Dimension]:
-    nums, qtys, syms = _split_numeric_and_symbolic(expr)
+    nums, qtys, syms = split_numeric_and_symbolic(expr.args)
 
     qty_factor = S.One
     for num in nums:
@@ -72,7 +80,7 @@ def _collect_pow(expr: Pow) -> tuple[Expr, Dimension]:
     return expr_, dim
 
 
-def _collect_unique_dimension(
+def collect_unique_dimension(
     nums: Iterable[Expr],
     qtys: Iterable[SymQuantity],
     syms: Iterable[tuple[Expr, Dimension]],
@@ -112,8 +120,8 @@ def _collect_unique_dimension(
 
 
 def _collect_add(expr: Add) -> tuple[Expr, Dimension]:
-    nums, qtys, syms = _split_numeric_and_symbolic(expr)
-    dim = _collect_unique_dimension(nums, qtys, syms)
+    nums, qtys, syms = split_numeric_and_symbolic(expr.args)
+    dim = collect_unique_dimension(nums, qtys, syms)
 
     qty_sum = sum(nums, start=S.Zero) + sum((qty.scale_factor for qty in qtys), start=S.Zero)
 
@@ -133,8 +141,8 @@ def _collect_abs(expr: Abs) -> tuple[Expr, Dimension]:
 def _collect_min_max(expr: MinMaxBase) -> tuple[Expr, Dimension]:
     cls = type(expr)
 
-    nums, qtys, syms = _split_numeric_and_symbolic(expr)
-    dim = _collect_unique_dimension(nums, qtys, syms)
+    nums, qtys, syms = split_numeric_and_symbolic(expr.args)
+    dim = collect_unique_dimension(nums, qtys, syms)
 
     if dimsys_SI.is_dimensionless(dim):
         expr_ = cls(
@@ -178,6 +186,88 @@ def _collect_derivative(expr: Derivative) -> tuple[Expr, Dimension]:
     return expr_, dim
 
 
+def _collect_vector_dot(expr: VectorDot) -> tuple[Expr, Dimension]:
+    if 0 in expr.args:
+        return S.Zero, dimensionless
+
+    lhs, rhs = expr.args
+
+    _, lhs_dim = collect_expression_and_dimension(lhs)
+    _, rhs_dim = collect_expression_and_dimension(rhs)
+
+    return expr, lhs_dim * rhs_dim
+
+
+def _collect_vector_cross(expr: VectorCross) -> tuple[Expr, Dimension]:
+    if 0 in expr.args:
+        return S.Zero, dimensionless
+
+    lhs, rhs = expr.args
+
+    _, lhs_dim = collect_expression_and_dimension(lhs)
+    _, rhs_dim = collect_expression_and_dimension(rhs)
+
+    return expr, lhs_dim * rhs_dim
+
+
+def _collect_vector_mixed_product(expr: VectorMixedProduct) -> tuple[Expr, Dimension]:
+    if 0 in expr.args:
+        return S.Zero, dimensionless
+
+    a, b, c = expr.args
+
+    _, a_dim = collect_expression_and_dimension(a)
+    _, b_dim = collect_expression_and_dimension(b)
+    _, c_dim = collect_expression_and_dimension(c)
+
+    return expr, a_dim * b_dim * c_dim
+
+
+def _collect_vector_norm(expr: VectorNorm) -> tuple[Expr, Dimension]:
+    if 0 in expr.args:
+        return S.Zero, dimensionless
+
+    _, dim = collect_expression_and_dimension(expr.args[0])
+
+    return expr, dim
+
+
+def _collect_applied_vector_function(expr: AppliedVectorFunction) -> tuple[Expr, Dimension]:
+    _, dim = collect_expression_and_dimension(expr.func)
+
+    return expr, dim
+
+
+def _collect_coordinate_scalar(expr: CoordinateScalar) -> tuple[Expr, Dimension]:
+    _, dim = collect_expression_and_dimension(expr.scalar)
+
+    return expr, dim
+
+
+def _collect_coordinate_vector(expr: CoordinateVector) -> tuple[Expr, Dimension]:
+    nums, qtys, syms = split_numeric_and_symbolic(expr.components)
+    dim = collect_unique_dimension(nums, qtys, syms)
+
+    return expr, dim
+
+
+def _collect_grad_div_curl(
+        expr: VectorGradient | VectorDivergence | VectorCurl) -> tuple[Expr, Dimension]:
+    _, dim = collect_expression_and_dimension(expr.args[0])
+
+    return expr, dim / units.length
+
+
+def _collect_laplacian(expr: VectorLaplacian) -> tuple[Expr, Dimension]:
+    _, dim = collect_expression_and_dimension(expr.args[0])
+
+    return expr, dim / units.length**2
+
+
+def _collect_dimension(expr: Dimension) -> tuple[Expr, Dimension]:
+    raise TypeError(f"{expr} cannot appear in an expression context")
+
+
 _cases: dict[type, Callable[[Any], tuple[Expr, Dimension]]] = {
     Mul: _collect_mul,
     Pow: _collect_pow,
@@ -187,6 +277,18 @@ _cases: dict[type, Callable[[Any], tuple[Expr, Dimension]]] = {
     Max: _collect_min_max,
     Derivative: _collect_derivative,
     SymFunction: _collect_function,
+    VectorDot: _collect_vector_dot,
+    VectorCross: _collect_vector_cross,
+    VectorMixedProduct: _collect_vector_mixed_product,
+    VectorNorm: _collect_vector_norm,
+    AppliedVectorFunction: _collect_applied_vector_function,
+    CoordinateScalar: _collect_coordinate_scalar,
+    CoordinateVector: _collect_coordinate_vector,
+    VectorGradient: _collect_grad_div_curl,
+    VectorDivergence: _collect_grad_div_curl,
+    VectorCurl: _collect_grad_div_curl,
+    VectorLaplacian: _collect_laplacian,
+    Dimension: _collect_dimension,
 }
 
 
@@ -206,6 +308,9 @@ def collect_expression_and_dimension(expr: SupportsFloat) -> tuple[Expr, Dimensi
     # early return that works for `sympy.Quantity`, `SymbolNew`, `SymbolIndexedNew`, and `FunctionNew`
     if hasattr(expr, "dimension"):
         return expr, getattr(expr, "dimension")
+
+    if isinstance(expr, Basic):
+        expr = expr.doit()
 
     for type_, collector in _cases.items():
         if isinstance(expr, type_):
