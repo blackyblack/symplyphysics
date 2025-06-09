99433c7985872451688885ce2e2e7523e7f7bc2e
Aleksandr Slatov <alexslatov@gmail.com>
2025-06-09 19:46:27 +0400

    Fix law and test

--- symplyphysics/laws/dynamics/vector/restoring_torque_due_to_twist_of_torsion_pendulum.py
+++ symplyphysics/laws/dynamics/vector/restoring_torque_due_to_twist_of_torsion_pendulum.py
@@ -1,48 +1,69 @@
-from symplyphysics import (
-    Vector,
-    QuantityVector,
-    scale_vector,
-    units,
-    Quantity,
-    validate_input,
-    validate_output,
-    angle_type,
-    symbols,
-)
+"""
+Restoring torque due to twist of torsion pendulum
+=================================================
 
-# Description
-## A torsion pendulum is a simple harmonic oscillator consisting of a disk suspended by a wire.
-## Rotating the disk through an angle in either direction introduces a restoring torque.
+A torsion pendulum is a simple harmonic oscillator consisting of a disk suspended by a wire.
+Rotating the disk through an angle in either direction introduces a restoring torque.
 
-# Law: tau = -kappa * theta
-## tau - torque pseudovector
-## kappa - torsion constant of the pendulum
-## theta - angular displacement pseudovector, also known as rotation vector
+..
+    TODO: add link to source
+"""
 
-torsion_constant = symbols.torsion_stiffness
+from sympy import Eq
+from symplyphysics import Quantity, validate_input, validate_output, symbols
+
+from symplyphysics.core.experimental.solvers import solve_for_vector
+from symplyphysics.core.experimental.vectors import clone_as_vector_symbol
+from symplyphysics.core.experimental.coordinate_systems import QuantityCoordinateVector
+
+torque = clone_as_vector_symbol(symbols.torque)
+"""
+Pseudovector of :symbols:`torque` of the pendulum.
+"""
+
+torsion_stiffness = symbols.torsion_stiffness
+"""
+:symbols:`torsion_stiffness` of the pendulum.
+"""
+
+angular_displacement = clone_as_vector_symbol(symbols.angular_distance)
+"""
+Pseudovector of angular displacement. See :symbols:`angular_distance`.
+"""
+
+law = Eq(torque, -1 * torsion_stiffness * angular_displacement)
+"""
+:laws:symbol::
+
+:laws:latex::
+"""
 
 
-def torque_law(rotation_vector_: Vector) -> Vector:
-    return scale_vector(-1 * torsion_constant, rotation_vector_)
+@validate_input(torsion_constant_=torsion_stiffness, rotation_vector_=angular_displacement)
+@validate_output(torque)
+def calculate_torque(
+    torsion_constant_: Quantity,
+    rotation_vector_: QuantityCoordinateVector,
+) -> QuantityCoordinateVector:
+    expr = solve_for_vector(law, torque)
+    value = expr.subs({
+        torsion_stiffness: torsion_constant_,
+        angular_displacement: rotation_vector_,
+    })
+
+    return QuantityCoordinateVector.from_expr(value)
 
 
-def rotation_vector_law(torque_: Vector) -> Vector:
-    return scale_vector(-1 / torsion_constant, torque_)
+@validate_input(torsion_constant_=torsion_stiffness, torque_=torque)
+@validate_output(angular_displacement)
+def calculate_rotation_vector(
+    torsion_constant_: Quantity,
+    torque_: QuantityCoordinateVector,
+) -> QuantityCoordinateVector:
+    expr = solve_for_vector(law, angular_displacement)
+    value = expr.subs({
+        torsion_stiffness: torsion_constant_,
+        torque: torque_,
+    })
 
-
-@validate_input(torsion_constant_=torsion_constant, rotation_vector_=angle_type)
-@validate_output(units.force * units.length)
-def calculate_torque(torsion_constant_: Quantity,
-    rotation_vector_: QuantityVector) -> QuantityVector:
-    result_vector = torque_law(rotation_vector_.to_base_vector())
-    return QuantityVector.from_base_vector(result_vector,
-        subs={torsion_constant: torsion_constant_})
-
-
-@validate_input(torsion_constant_=torsion_constant, torque_=units.force * units.length)
-@validate_output(angle_type)
-def calculate_rotation_vector(torsion_constant_: Quantity,
-    torque_: QuantityVector) -> QuantityVector:
-    result_vector = rotation_vector_law(torque_.to_base_vector())
-    return QuantityVector.from_base_vector(result_vector,
-        subs={torsion_constant: torsion_constant_})
+    return QuantityCoordinateVector.from_expr(value)
--- test/dynamics/vector/restoring_torque_due_to_twist_of_torsion_pendulum_test.py
+++ test/dynamics/vector/restoring_torque_due_to_twist_of_torsion_pendulum_test.py
@@ -1,14 +1,11 @@
 from collections import namedtuple
 from pytest import fixture, raises
-from symplyphysics import (
-    assert_equal_vectors,
-    errors,
-    units,
-    Quantity,
-    QuantityVector,
-)
+from symplyphysics import errors, units, Quantity
 from symplyphysics.laws.dynamics.vector import (restoring_torque_due_to_twist_of_torsion_pendulum as
     pendulum_laws)
+
+from symplyphysics.core.experimental.coordinate_systems import QuantityCoordinateVector, CARTESIAN
+from symplyphysics.core.experimental.approx import assert_equal_vectors
 
 # Description
 ## A torsion pendulum is rotated about its axis so that its rotation pseudovector is (0.1, 0.2, -0.1) rad.
@@ -20,13 +17,13 @@ Args = namedtuple("Args", "tau kappa theta")
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
-    tau = QuantityVector([
+    tau = QuantityCoordinateVector([
         Quantity(-1.0 * units.newton * units.meter),
         Quantity(-2.0 * units.newton * units.meter),
         Quantity(1.0 * units.newton * units.meter),
-    ])
+    ], CARTESIAN)
     kappa = Quantity(10.0 * units.newton * units.meter)
-    theta = QuantityVector([0.1, 0.2, -0.1])
+    theta = QuantityCoordinateVector([0.1, 0.2, -0.1], CARTESIAN)
     return Args(tau=tau, kappa=kappa, theta=theta)
 
 
@@ -53,30 +50,30 @@ def test_bad_torsion_constant(test_args: Args) -> None:
 
 
 def test_bad_rotation_vector(test_args: Args) -> None:
-    theta_b = QuantityVector([
+    theta_b = QuantityCoordinateVector([
         Quantity(1.0 * units.meter),
         Quantity(1.0 * units.meter),
         Quantity(1.0 * units.meter),
-    ])
+    ], CARTESIAN)
     with raises(errors.UnitsError):
         pendulum_laws.calculate_torque(test_args.kappa, theta_b)
 
     theta_s = 1.0
-    with raises(AttributeError):
+    with raises(ValueError):
         pendulum_laws.calculate_torque(test_args.kappa, theta_s)
 
 
 def test_bad_torque(test_args: Args) -> None:
-    tau_b = QuantityVector([
+    tau_b = QuantityCoordinateVector([
         Quantity(1.0 * units.meter),
         Quantity(1.0 * units.meter),
         Quantity(1.0 * units.meter),
-    ])
+    ], CARTESIAN)
     with raises(errors.UnitsError):
         pendulum_laws.calculate_rotation_vector(test_args.kappa, tau_b)
 
     tau_s = Quantity(1.0 * units.newton * units.meter)
-    with raises(AttributeError):
+    with raises(ValueError):
         pendulum_laws.calculate_rotation_vector(test_args.kappa, tau_s)
 
     with raises(TypeError):
