1b8d7d6542ca071ffa8168b80602824b6682e810
Aleksandr Slatov <alexslatov@gmail.com>
2025-06-09 20:32:23 +0400

    Fix law, test and usages

--- symplyphysics/laws/dynamics/vector/torque_vector_of_twisting_force.py
+++ symplyphysics/laws/dynamics/vector/torque_vector_of_twisting_force.py
@@ -1,30 +1,50 @@
-from symplyphysics import (
-    units,
-    QuantityVector,
-    Vector,
-    cross_cartesian_vectors,
-    validate_input,
-    validate_output,
-)
+"""
+Torque of twisting force
+========================
 
-# Description
-## Torque is a turning or twisting action on a body about a rotation axis due to a force.
-## It is a pseudovector defined as a cross product of the force vector and the position vector of the point
-## of force application.
+Torque is a turning or twisting action on a body about a rotation axis due to a force. It is a
+pseudovector defined as a cross product of the force vector and the position vector of the point
+of force application.
 
-# Law: tau = cross(r, F)
-## tau - torque pseudovector
-## r - position vector of the point of force applicaton
-## F - force vector
-## cross(a, b) - cross product between vectors a and b
+**Links:**
+
+#. `Wikipedia <https://en.wikipedia.org/wiki/Torque#Definition_and_relation_to_other_physical_quantities>`__.
+"""
+
+from sympy import Eq
+from symplyphysics import (symbols, validate_input, validate_output)
+
+from symplyphysics.core.experimental.vectors import clone_as_vector_symbol, VectorCross
+from symplyphysics.core.experimental.coordinate_systems import QuantityCoordinateVector
+from symplyphysics.core.experimental.solvers import solve_for_vector
+
+torque = clone_as_vector_symbol(symbols.torque)
+"""
+Pseudovector of :symbols:`torque` due to twisting :attr:`~force`.
+"""
+
+force = clone_as_vector_symbol(symbols.force)
+"""
+Vector of :symbols:`force` exerted on the body.
+"""
+
+position_vector = clone_as_vector_symbol(symbols.distance_to_origin)
+"""
+Position vector of the body. See :symbols:`distance_to_origin`.
+"""
+
+law = Eq(torque, VectorCross(position_vector, force))
 
 
-def torque_definition(position_: Vector, force_: Vector) -> Vector:
-    return cross_cartesian_vectors(position_, force_)
+@validate_input(force_=force, position_=position_vector)
+@validate_output(torque)
+def calculate_torque(
+    position_: QuantityCoordinateVector,
+    force_: QuantityCoordinateVector,
+) -> QuantityCoordinateVector:
+    result = solve_for_vector(law, torque).subs({
+        force: force_,
+        position_vector: position_,
+    })
 
-
-@validate_input(force_=units.force, position_=units.length)
-@validate_output(units.force * units.length)
-def calculate_torque(position_: QuantityVector, force_: QuantityVector) -> QuantityVector:
-    torque_vector = torque_definition(position_.to_base_vector(), force_.to_base_vector())
-    return QuantityVector.from_base_vector(torque_vector)
+    return QuantityCoordinateVector.from_expr(result)
