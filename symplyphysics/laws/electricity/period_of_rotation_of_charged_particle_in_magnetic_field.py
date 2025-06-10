231b63693e16e06f2830e41fb11f9e83c5cb5ce0
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-06-10 20:19:52 +0400

    Patch vector law of Lorentz force (#1019)
    * Fix law, test and usages
    
    * Add note

--- symplyphysics/laws/electricity/period_of_rotation_of_charged_particle_in_magnetic_field.py
+++ symplyphysics/laws/electricity/period_of_rotation_of_charged_particle_in_magnetic_field.py
@@ -19,9 +19,8 @@ as well as by the magnetic field and it does not depend on the particle speed.
 #. `Physics LibreTexts, formula 11.4.3 <https://phys.libretexts.org/Bookshelves/University_Physics/University_Physics_(OpenStax)/University_Physics_II_-_Thermodynamics_Electricity_and_Magnetism_(OpenStax)/11%3A_Magnetic_Forces_and_Fields/11.04%3A_Motion_of_a_Charged_Particle_in_a_Magnetic_Field>`__.
 """
 
-from sympy import (Eq, solve, pi, symbols as sympy_symbols)
-from symplyphysics import (Quantity, validate_input, validate_output, symbols, Vector,
-    vector_magnitude)
+from sympy import Eq, solve, pi, symbols as sympy_symbols
+from symplyphysics import Quantity, validate_input, validate_output, symbols
 from symplyphysics.core.expr_comparisons import expr_equals
 from symplyphysics.laws.electricity.vector import lorentz_force_via_electromagnetic_field as _lorentz_law
 from symplyphysics.laws.dynamics import acceleration_is_force_over_mass as _newtons_second_law
@@ -30,6 +29,9 @@ from symplyphysics.laws.kinematics import (
     speed_via_angular_speed_and_radius as _speed_law,
 )
 from symplyphysics.definitions import period_from_angular_frequency as _period_law
+
+from symplyphysics.core.experimental.vectors import VectorNorm
+from symplyphysics.core.experimental.coordinate_systems import CoordinateVector, CARTESIAN
 
 period = symbols.period
 """
@@ -69,66 +71,19 @@ _speed_expr = _speed_law.law.rhs.subs({
     _speed_law.radius_of_curvature: _radius,
 })
 
-_velocity_vec = Vector([_speed_expr, 0, 0])
+_velocity_vec_ = CoordinateVector([_speed_expr, 0, 0], CARTESIAN)
 
 _magnetic_field = sympy_symbols("magnetic_field", positive=True)
-_magnetic_field_vec = Vector([0, _magnetic_field, 0])
+_magnetic_field_vec_ = CoordinateVector([0, _magnetic_field, 0], CARTESIAN)
 
-_force_vec = _lorentz_law.lorentz_force_law(
-    electric_field_=Vector([0, 0, 0]),
-    magnetic_flux_density_=_magnetic_field_vec,
-    velocity_=_velocity_vec,
-).subs(_lorentz_law.charge, _charge)
-
-_force_expr = vector_magnitude(_force_vec)
-
-_acceleration_via_force = _newtons_second_law.law.rhs.subs({
-    _newtons_second_law.force: _force_expr,
-    _newtons_second_law.mass: mass,
+_force_vec_ = _lorentz_law.law.rhs.subs({
+    _lorentz_law.charge: _charge,
+    _lorentz_law.electric_field: 0,  # NOTE: see condition 3 of this law
+    _lorentz_law.velocity: _velocity_vec_,
+    _lorentz_law.magnetic_flux_density: _magnetic_field_vec_,
 })
 
-_acceleration_via_frequency = _centripetal_acceleration_law.law.rhs.subs({
-    _centripetal_acceleration_law.angular_speed: _angular_frequency,
-    _centripetal_acceleration_law.radius_of_curvature: _radius,
-})
-
-_angular_frequency_expr = solve(
-    Eq(_acceleration_via_frequency, _acceleration_via_force),
-    _angular_frequency,
-)[0]
-
-_period_expr = _period_law.law.rhs.subs({
-    _period_law.angular_frequency: _angular_frequency_expr
-}).subs({
-    _charge: charge,
-    _magnetic_field: magnetic_flux_density,
-})
-
-assert expr_equals(_period_expr, law.rhs)
-
-# Derive law from Lorentz force
-
-_charge = sympy_symbols("charge", positive=True)
-_angular_frequency = sympy_symbols("angular_frequency", positive=True)
-_radius = sympy_symbols("radius", positive=True)
-
-_speed_expr = _speed_law.law.rhs.subs({
-    _speed_law.angular_speed: _angular_frequency,
-    _speed_law.radius_of_curvature: _radius,
-})
-
-_velocity_vec = Vector([_speed_expr, 0, 0])
-
-_magnetic_field = sympy_symbols("magnetic_field", positive=True)
-_magnetic_field_vec = Vector([0, _magnetic_field, 0])
-
-_force_vec = _lorentz_law.lorentz_force_law(
-    electric_field_=Vector([0, 0, 0]),
-    magnetic_flux_density_=_magnetic_field_vec,
-    velocity_=_velocity_vec,
-).subs(_lorentz_law.charge, _charge)
-
-_force_expr = vector_magnitude(_force_vec)
+_force_expr = VectorNorm(_force_vec_).doit()
 
 _acceleration_via_force = _newtons_second_law.law.rhs.subs({
     _newtons_second_law.force: _force_expr,
