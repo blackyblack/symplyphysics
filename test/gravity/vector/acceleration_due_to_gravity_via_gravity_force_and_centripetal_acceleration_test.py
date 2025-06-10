9a04684e0a6176aaf1e48680e78b93677d470cf3
Aleksandr Slatov <alexslatov@gmail.com>
2025-06-10 15:48:04 +0400

    Fix laws and test

--- test/gravity/vector/acceleration_due_to_gravity_via_gravity_force_and_centripetal_acceleration_test.py
+++ test/gravity/vector/acceleration_due_to_gravity_via_gravity_force_and_centripetal_acceleration_test.py
@@ -1,28 +1,37 @@
 from collections import namedtuple
 from pytest import fixture, raises
-from symplyphysics import (
-    errors,
-    units,
-    Quantity,
-    QuantityVector,
-    assert_equal,
-    assert_equal_vectors,
-)
+from symplyphysics import errors, units, Quantity
 from symplyphysics.laws.gravity.vector import acceleration_due_to_gravity_via_gravity_force_and_centripetal_acceleration as law
+
+from symplyphysics.core.experimental.coordinate_systems import QuantityCoordinateVector, CARTESIAN
+from symplyphysics.core.experimental.approx import assert_equal_vectors
+from symplyphysics.core.experimental.solvers import solve_for_vector
 
 Args = namedtuple("Args", "f a m g")
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
-    f = QuantityVector([-2.43e2 * units.newton, -4.21e2 * units.newton, -4.86e2 * units.newton])
+    f = QuantityCoordinateVector([
+        -2.43e2 * units.newton,
+        -4.21e2 * units.newton,
+        -4.86e2 * units.newton,
+    ], CARTESIAN)
 
     a_unit = units.meter / units.second**2
-    a = QuantityVector([-1.2e-2 * a_unit, -2.07e-2 * a_unit, 0])
+    a = QuantityCoordinateVector([
+        -1.2e-2 * a_unit,
+        -2.07e-2 * a_unit,
+        0,
+    ], CARTESIAN)
 
     m = Quantity(70 * units.kilogram)
 
-    g = QuantityVector([-3.46 * a_unit, -5.99 * a_unit, -6.94 * a_unit])
+    g = QuantityCoordinateVector([
+        -3.46 * a_unit,
+        -5.99 * a_unit,
+        -6.94 * a_unit,
+    ], CARTESIAN)
 
     return Args(f=f, a=a, m=m, g=g)
 
@@ -33,30 +42,23 @@ def test_law(test_args: Args) -> None:
 
 
 def test_gravity_law(test_args: Args) -> None:
-    vector = law.gravity_force_law(
-        test_args.g.to_base_vector(),
-        test_args.a.to_base_vector(),
-    )
-    result = QuantityVector.from_base_vector(vector, subs={law.mass: test_args.m})
+    result = solve_for_vector(law.law, law.gravity_force).subs({
+        law.mass: test_args.m,
+        law.acceleration_due_to_gravity: test_args.g,
+        law.centripetal_acceleration: test_args.a,
+    })
+    result = QuantityCoordinateVector.from_expr(result)
+
     assert_equal_vectors(result, test_args.f)
 
 
-def test_mass_law(test_args: Args) -> None:
-    result = law.mass_law(
-        test_args.g.to_base_vector(),
-        test_args.f.to_base_vector(),
-        test_args.a.to_base_vector(),
-    )
-    assert_equal(result, test_args.m)
-
-
 def test_bad_force(test_args: Args) -> None:
-    fb_vector = QuantityVector([units.coulomb])
+    fb_vector = QuantityCoordinateVector([units.coulomb, 0, 0], CARTESIAN)
     with raises(errors.UnitsError):
         law.calculate_acceleraton_due_to_gravity(fb_vector, test_args.a, test_args.m)
 
     fb_scalar = Quantity(units.newton)
-    with raises(AttributeError):
+    with raises(ValueError):
         law.calculate_acceleraton_due_to_gravity(fb_scalar, test_args.a, test_args.m)
 
     with raises(TypeError):
@@ -66,12 +68,12 @@ def test_bad_force(test_args: Args) -> None:
 
 
 def test_bad_acceleration(test_args: Args) -> None:
-    ab_vector = QuantityVector([units.coulomb])
+    ab_vector = QuantityCoordinateVector([units.coulomb, 0, 0], CARTESIAN)
     with raises(errors.UnitsError):
         law.calculate_acceleraton_due_to_gravity(test_args.f, ab_vector, test_args.m)
 
     ab_scalar = Quantity(units.meter / units.second**2)
-    with raises(AttributeError):
+    with raises(ValueError):
         law.calculate_acceleraton_due_to_gravity(test_args.f, ab_scalar, test_args.m)
 
     with raises(TypeError):
