054dbf590a8e24b2fcdf514e63afbb1c3811c73f
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-06-11 11:39:06 +0400

    Fix law, test and usages (#1025)

--- symplyphysics/laws/kinematics/vector/acceleration_is_normal_plus_tangential_acceleration.py
+++ symplyphysics/laws/kinematics/vector/acceleration_is_normal_plus_tangential_acceleration.py
@@ -16,186 +16,96 @@ The acceleration of a body moving arbitrarily is composed of two parts:
 #. `Mathematica LibreTexts <https://math.libretexts.org/Bookshelves/Calculus/Supplemental_Modules_(Calculus)/Vector_Calculus/2%3A_Vector-Valued_Functions_and_Motion_in_Space/2.6%3A_Tangential_and_Normal_Components_of_Acceleration>`__.
 """
 
-from pytest import approx
-from symplyphysics import (
-    Quantity,
-    dot_vectors,
-    units,
-    validate_input,
-    validate_output,
-    Vector,
-    QuantityVector,
-    add_cartesian_vectors,
-    scale_vector,
+from sympy import Eq
+from symplyphysics import validate_input, validate_output, symbols, Quantity, assert_equal
+
+from symplyphysics.core.approx import approx_equal_numbers
+from symplyphysics.core.experimental.vectors import clone_as_vector_symbol, VectorDot
+from symplyphysics.core.experimental.coordinate_systems import QuantityCoordinateVector
+from symplyphysics.core.experimental.solvers import solve_for_vector
+
+total_acceleration = clone_as_vector_symbol(symbols.acceleration)
+"""
+Vector of the body's total :symbols:`acceleration`.
+"""
+
+normal_acceleration = clone_as_vector_symbol(
+    symbols.acceleration,
+    display_symbol="a_n",
+    display_latex="{\\vec a}_\\text{n}",
 )
+"""
+Vector of the body's normal :symbols:`acceleration`.
+"""
 
+tangential_acceleration = clone_as_vector_symbol(
+    symbols.acceleration,
+    display_symbol="a_t",
+    display_latex="{\\vec a}_\\text{t}",
+)
+"""
+Vector of the body's tangential :symbols:`acceleration`.
+"""
 
-def acceleration_law(
-    normal_acceleration_: Vector,
-    tangential_acceleration_: Vector,
-) -> Vector:
-    r"""
-    Total acceleration via normal and tangential accelerations.
+law = Eq(total_acceleration, normal_acceleration + tangential_acceleration)
+"""
+:laws:symbol::
 
-    Law:
-        :code:`a = a_n + a_t`
-    
-    Latex:
-        .. math::
-            \vec a = {\vec a}_n + {\vec a}_\tau
-
-    :param normal_acceleration\_: vector of normal acceleration
-
-        Symbol: :code:`a_n`
-
-        Latex: :math:`{\vec a}_n`
-
-        Dimension: *acceleration*
-
-    :param tangential_acceleration\_: vector of tangential acceleration
-
-        Symbol: :code:`a_t`
-
-        Latex: :math:`{\vec a}_\tau`
-
-        Dimension: *acceleration*
-
-    :return: vector of total acceleration
-
-        Symbol: :code:`a`
-
-        Latex: :math:`\vec a`
-
-        Dimension: *acceleration*
-    """
-
-    return add_cartesian_vectors(normal_acceleration_, tangential_acceleration_)
-
-
-def normal_acceleration_law(
-    total_acceleration_: Vector,
-    tangential_acceleration_: Vector,
-) -> Vector:
-    r"""
-    Normal acceleration via total and tangential accelerations.
-
-    Law:
-        :code:`a_n = a - a_t`
-
-    Latex:
-        .. math::
-            {\vec a}_n = \vec a - {\vec a}_\tau
-
-    :param total_acceleration\_: vector of total acceleration
-
-        Symbol: :code:`a`
-
-        Latex: :math:`\vec a`
-
-        Dimension: *acceleration*
-
-    :param tangential_acceleration\_: vector of tangential acceleration
-
-        Symbol: :code:`a_t`
-
-        Latex: :math:`{\vec a}_\tau`
-
-        Dimension: *acceleration*
-    """
-
-    opposite_tangential_vector = scale_vector(-1, tangential_acceleration_)
-    return add_cartesian_vectors(total_acceleration_, opposite_tangential_vector)
-
-
-def tangential_acceleration_law(
-    total_acceleration_: Vector,
-    normal_acceleration_: Vector,
-) -> Vector:
-    r"""
-    Tangential acceleration via total and normal accelerations.
-
-    Law:
-        :code:`a_t = a - a_n`
-
-    Latex:
-        .. math::
-            {\vec a}_\tau = \vec a - {\vec a}_n
-
-    :param total_acceleration\_: vector of total acceleration
-
-        Symbol: :code:`a`
-
-        Latex: :math:`\vec a`
-
-        Dimension: *acceleration*
-
-    :param normal_acceleration\_: vector of normal acceleration
-
-        Symbol: :code:`a_n`
-
-        Latex: :math:`{\vec a}_n`
-
-        Dimension: *acceleration*
-    """
-
-    opposite_radial_vector = scale_vector(-1, normal_acceleration_)
-    return add_cartesian_vectors(total_acceleration_, opposite_radial_vector)
+:laws:latex::
+"""
 
 
 @validate_input(
-    normal_acceleration_=units.acceleration,
-    tangential_acceleration_=units.acceleration,
+    normal_acceleration_=normal_acceleration,
+    tangential_acceleration_=tangential_acceleration,
 )
-@validate_output(units.acceleration)
+@validate_output(total_acceleration)
 def calculate_acceleration(
-    normal_acceleration_: QuantityVector,
-    tangential_acceleration_: QuantityVector,
-) -> QuantityVector:
-    radial_acceleration_vector = normal_acceleration_.to_base_vector()
-    tangential_acceleration_vector = tangential_acceleration_.to_base_vector()
-    dot_vectors_result = Quantity(
-        dot_vectors(radial_acceleration_vector, tangential_acceleration_vector))
-    if dot_vectors_result.scale_factor != approx(0.0, rel=1e-3):
-        raise ValueError(
-            "Radial and tangential acceleration vectors should be perpendicular to each other")
-    acceleration_vector = acceleration_law(
-        radial_acceleration_vector,
-        tangential_acceleration_vector,
-    )
-    return QuantityVector.from_base_vector(acceleration_vector)
+    normal_acceleration_: QuantityCoordinateVector,
+    tangential_acceleration_: QuantityCoordinateVector,
+) -> QuantityCoordinateVector:
+    dot = Quantity(VectorDot(normal_acceleration_, tangential_acceleration_))
+
+    if not approx_equal_numbers(dot.scale_factor, 0):
+        raise ValueError("The normal and tangential accelerations must be orthogonal")
+
+    result = solve_for_vector(law, total_acceleration).subs({
+        normal_acceleration: normal_acceleration_,
+        tangential_acceleration: tangential_acceleration_,
+    })
+
+    return QuantityCoordinateVector.from_expr(result)
 
 
 @validate_input(
-    total_acceleration_=units.acceleration,
-    tangential_acceleration_=units.acceleration,
+    total_acceleration_=total_acceleration,
+    tangential_acceleration_=tangential_acceleration,
 )
-@validate_output(units.acceleration)
+@validate_output(normal_acceleration)
 def calculate_radial_acceleration(
-    total_acceleration_: QuantityVector,
-    tangential_acceleration_: QuantityVector,
-) -> QuantityVector:
-    total_acceleration_vector = total_acceleration_.to_base_vector()
-    tangential_acceleration_vector = tangential_acceleration_.to_base_vector()
-    radial_acceleration_vector = normal_acceleration_law(
-        total_acceleration_vector,
-        tangential_acceleration_vector,
-    )
-    return QuantityVector.from_base_vector(radial_acceleration_vector)
+    total_acceleration_: QuantityCoordinateVector,
+    tangential_acceleration_: QuantityCoordinateVector,
+) -> QuantityCoordinateVector:
+    result = solve_for_vector(law, normal_acceleration).subs({
+        total_acceleration: total_acceleration_,
+        tangential_acceleration: tangential_acceleration_,
+    })
+
+    return QuantityCoordinateVector.from_expr(result)
 
 
 @validate_input(
-    total_acceleration_=units.acceleration,
-    normal_acceleration_=units.acceleration,
+    total_acceleration_=total_acceleration,
+    normal_acceleration_=normal_acceleration,
 )
-@validate_output(units.acceleration)
+@validate_output(tangential_acceleration)
 def calculate_tangential_acceleration(
-    total_acceleration_: QuantityVector,
-    normal_acceleration_: QuantityVector,
-) -> QuantityVector:
-    total_acceleration_vector = total_acceleration_.to_base_vector()
-    radial_acceleration_vector = normal_acceleration_.to_base_vector()
-    tangential_acceleration_vector = tangential_acceleration_law(
-        total_acceleration_vector,
-        radial_acceleration_vector,
-    )
-    return QuantityVector.from_base_vector(tangential_acceleration_vector)
+    total_acceleration_: QuantityCoordinateVector,
+    normal_acceleration_: QuantityCoordinateVector,
+) -> QuantityCoordinateVector:
+    result = solve_for_vector(law, tangential_acceleration).subs({
+        normal_acceleration: normal_acceleration_,
+        total_acceleration: total_acceleration_,
+    })
+
+    return QuantityCoordinateVector.from_expr(result)
