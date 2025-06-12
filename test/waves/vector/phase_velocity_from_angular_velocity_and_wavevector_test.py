3fbaa45780e030e906056b7e7602d77effbc3222
Aleksandr Slatov <alexslatov@gmail.com>
2025-06-12 18:01:11 +0400

    Fix law and test

--- test/waves/vector/phase_velocity_from_angular_velocity_and_wavevector_test.py
+++ test/waves/vector/phase_velocity_from_angular_velocity_and_wavevector_test.py
@@ -1,14 +1,11 @@
 from collections import namedtuple
 from pytest import fixture, raises
-from symplyphysics import (
-    assert_equal_vectors,
-    errors,
-    units,
-    Quantity,
-    QuantityVector,
-)
+from symplyphysics import errors, units, Quantity
 from symplyphysics.laws.waves.vector import (
     phase_velocity_from_angular_velocity_and_wavevector as phase_velocity_law,)
+
+from symplyphysics.core.experimental.coordinate_systems import CARTESIAN, QuantityCoordinateVector
+from symplyphysics.core.experimental.approx import assert_equal_vectors
 
 # Description
 ## A wave is propagating with the wavevector k = (2, 3, -6) rad/m and angular frequency w = 5 Hz.
@@ -19,17 +16,17 @@ Args = namedtuple("Args", "v w k")
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
-    v = QuantityVector([
+    v = QuantityCoordinateVector([
         Quantity(0.204 * units.meter / units.second),
         Quantity(0.306 * units.meter / units.second),
         Quantity(-0.612 * units.meter / units.second),
-    ])
+    ], CARTESIAN)
     w = Quantity(5.0 * units.hertz)
-    k = QuantityVector([
+    k = QuantityCoordinateVector([
         Quantity(2.0 * units.radian / units.meter),
         Quantity(3.0 * units.radian / units.meter),
         Quantity(-6.0 * units.radian / units.meter),
-    ])
+    ], CARTESIAN)
     return Args(v=v, w=w, k=k)
 
 
@@ -56,16 +53,16 @@ def test_bad_frequency(test_args: Args) -> None:
 
 
 def test_bad_wavenumber(test_args: Args) -> None:
-    k_bad_vector = QuantityVector([
+    k_bad_vector = QuantityCoordinateVector([
         Quantity(1.0 * units.coulomb),
         Quantity(1.0 * units.coulomb),
         Quantity(1.0 * units.coulomb),
-    ])
+    ], CARTESIAN)
     with raises(errors.UnitsError):
         phase_velocity_law.calculate_phase_velocity(test_args.w, k_bad_vector)
 
     k_scalar = Quantity(1.0 * units.radian / units.meter)
-    with raises(AttributeError):
+    with raises(ValueError):
         phase_velocity_law.calculate_phase_velocity(test_args.w, k_scalar)
 
     with raises(TypeError):
@@ -75,16 +72,16 @@ def test_bad_wavenumber(test_args: Args) -> None:
 
 
 def test_bad_velocity(test_args: Args) -> None:
-    v_bad_vector = QuantityVector([
+    v_bad_vector = QuantityCoordinateVector([
         Quantity(1.0 * units.coulomb),
         Quantity(1.0 * units.coulomb),
         Quantity(1.0 * units.coulomb),
-    ])
+    ], CARTESIAN)
     with raises(errors.UnitsError):
         phase_velocity_law.calculate_wavevector(test_args.w, v_bad_vector)
 
     v_scalar = Quantity(1.0 * units.meter / units.second)
-    with raises(AttributeError):
+    with raises(ValueError):
         phase_velocity_law.calculate_wavevector(test_args.w, v_scalar)
 
     with raises(TypeError):
