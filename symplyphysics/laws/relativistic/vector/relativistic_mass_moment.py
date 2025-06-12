c32a419250caa5bf031a84610eef43753419f03f
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-06-12 14:34:01 +0400

    Fix law and test (#1037)

--- symplyphysics/laws/relativistic/vector/relativistic_mass_moment.py
+++ symplyphysics/laws/relativistic/vector/relativistic_mass_moment.py
@@ -1,70 +1,89 @@
-from symplyphysics import (
-    units,
-    Quantity,
-    validate_input,
-    validate_output,
-    Vector,
-    add_cartesian_vectors,
-    vector_magnitude,
-    scale_vector,
-    QuantityVector,
-    symbols,
-)
+"""
+Relativistic mass moment
+========================
+
+Mass moment is an additive physical quantity useful for deriving the Lorentz transformation of
+angular momentum. For isolated systems, it is conserved in time, but unlike angular momentum, the
+vector of mass moment is a polar ("ordinary") vector and therefore invariant under inversion.
+
+**Links:**
+
+#. `Wikipedia, end of paragraph <https://en.wikipedia.org/wiki/Relativistic_angular_momentum#Dynamic_mass_moment>`__.
+"""
+
+from sympy import Eq
+
+from symplyphysics import Quantity, validate_input, validate_output, symbols, units
 from symplyphysics.definitions import lorentz_factor as lorentz_factor_def
 
-# Description
-## Mass moment is an additive physical quantity useful for deriving the Lorentz transformation of angular momentum.
-## For isolated systems, it is conserved in time, but unlike angular momentum, the vector of mass moment is a polar
-## ("ordinary") vector and therefore invariant under inversion.
+from symplyphysics.core.experimental.vectors import VectorSymbol, clone_as_vector_symbol, VectorNorm
+from symplyphysics.core.experimental.coordinate_systems import QuantityCoordinateVector
 
-# Law: N = m0 * gamma**2 * (x - v * t)
-## N - vector of mass moment
-## m0 - rest mass
-## x - vector of position
-## v - vector of velocity
-## gamma = 1 / sqrt(1 - dot(v, v)**2 / c**2) - Lorentz factor, where `dot` is dot product and `c` is speed of light
-## t - time
-
-# Links: Wikipedia, end of paragraph <https://en.wikipedia.org/wiki/Relativistic_angular_momentum#Dynamic_mass_moment>
+mass_moment = VectorSymbol("N", units.mass * units.length)
+"""
+Vector of mass moment.
+"""
 
 rest_mass = symbols.rest_mass
+"""
+:symbols:`rest_mass` of the body.
+"""
+
 time = symbols.time
+"""
+:symbols:`time`.
+"""
 
+lorentz_factor = symbols.lorentz_factor
+"""
+:symbols:`lorentz_factor`.
+"""
 
-def mass_moment_law(position_: Vector, velocity_: Vector) -> Vector:
-    summed_vector_ = add_cartesian_vectors(
-        position_,
-        scale_vector(-1 * time, velocity_),
-    )
+position_vector = clone_as_vector_symbol(symbols.distance_to_origin)
+"""
+Vector of the body's position. See :symbols:`distance_to_origin`.
+"""
 
-    lorentz_factor_ = lorentz_factor_def.definition.rhs.subs({
-        lorentz_factor_def.speed: vector_magnitude(velocity_),
-    })
+velocity = clone_as_vector_symbol(symbols.speed)
+"""
+Vector of the body's velocity. See :symbols:`speed`.
+"""
 
-    return scale_vector(rest_mass * lorentz_factor_**2, summed_vector_)
+law = Eq(
+    mass_moment,
+    rest_mass * lorentz_factor**2 * (position_vector - velocity * time),
+)
+"""
+:laws:symbol::
+
+:laws:latex::
+"""
 
 
 @validate_input(
     rest_mass_=rest_mass,
-    position_=units.length,
-    velocity_=units.velocity,
+    position_=position_vector,
+    velocity_=velocity,
     time_=time,
 )
-@validate_output(units.mass * units.length)
+@validate_output(mass_moment)
 def calculate_mass_moment(
     rest_mass_: Quantity,
-    position_: QuantityVector,
-    velocity_: QuantityVector,
+    position_: QuantityCoordinateVector,
+    velocity_: QuantityCoordinateVector,
     time_: Quantity,
-) -> QuantityVector:
-    result = mass_moment_law(
-        position_.to_base_vector(),
-        velocity_.to_base_vector(),
+) -> QuantityCoordinateVector:
+    lorentz_factor_ = lorentz_factor_def.definition.rhs.subs(
+        lorentz_factor_def.speed,
+        VectorNorm(velocity_),
     )
-    return QuantityVector.from_base_vector(
-        result,
-        subs={
+
+    result = law.rhs.subs({
         rest_mass: rest_mass_,
-        time: time_
-        },
-    )
+        lorentz_factor: lorentz_factor_,
+        position_vector: position_,
+        velocity: velocity_,
+        time: time_,
+    })
+
+    return QuantityCoordinateVector.from_expr(result)
