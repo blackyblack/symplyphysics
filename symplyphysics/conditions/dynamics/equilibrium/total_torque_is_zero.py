af67f0d4d2375bfd54f4c6b387645c59032c5f3d
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-06-17 16:50:54 +0400

    Patch documentation of the rotational equilibrium condition (#1048)
    * Fix inits lines
    
    * Fix documentation

--- symplyphysics/conditions/dynamics/equilibrium/total_torque_is_zero.py
+++ symplyphysics/conditions/dynamics/equilibrium/total_torque_is_zero.py
@@ -1,17 +1,25 @@
+"""
+Total torque is zero
+====================
+
+For an object to be in rotational equilibrium, the total torque on the object must be zero.
+
+**Links:**
+
+#. `Physics LibreTexts <https://phys.libretexts.org/Bookshelves/University_Physics/Physics_(Boundless)/8%3A_Static_Equilibrium_Elasticity_and_Torque/8.2%3A_Conditions_for_Equilibrium>`__.
+"""
+
 from sympy import Eq
 from symplyphysics import symbols
 
-# Description
-## For an object to be in rotational equilibrium, the total torque on the object must be zero.
-
-# Law: total_torque = 0
-## total_torque - magnitude of the vector sum of all torques acting on a body
-
-# Links:
-## Physics LibreTexts <https://phys.libretexts.org/Bookshelves/University_Physics/Physics_(Boundless)/8%3A_Static_Equilibrium_Elasticity_and_Torque/8.2%3A_Conditions_for_Equilibrium>
-
-# TODO: update documentation
-
 total_torque = symbols.torque
+"""
+Total :symbols:`torque` is the magnitue of the vector sum of all torques acting on the body.
+"""
 
 law = Eq(total_torque, 0)
+"""
+:laws:symbol::
+
+:laws:latex::
+"""
