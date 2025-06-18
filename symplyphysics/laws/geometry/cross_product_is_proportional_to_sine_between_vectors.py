a11f3a420f683655403e88174e3626d3d6c6a66b
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-06-18 19:21:06 +0400

    Patch documentation (#1057)

--- symplyphysics/laws/geometry/cross_product_is_proportional_to_sine_between_vectors.py
+++ symplyphysics/laws/geometry/cross_product_is_proportional_to_sine_between_vectors.py
@@ -1,28 +1,54 @@
-from sympy import Eq, Symbol as SymSymbol, sin, solve
-from symplyphysics import Quantity
+"""
+Cross product is proportional to sine of angle between vectors
+==============================================================
+
+The cross product of two vectors is a binary operation which produces a vector whose length is
+proportional to the lengths of the given vectors and the sine of the angle between them.
+
+**Links:**
+
+#. `Wikipedia <https://en.wikipedia.org/wiki/Cross_product#Definition>`__.
+"""
+
+from sympy import Eq, sin, solve
+from symplyphysics import Quantity, Symbol, symbols
+from symplyphysics.core.dimensions import any_dimension
 from symplyphysics.core.symbols.quantities import scale_factor
 
-# Description
-## Cross product is a binary vector operation, defined as such:
-## - Its magnitude is the product of the magnitudes of the two vectors and the sine between them.
-## - Its direction can be found via the right-hand rule.
+cross_product_length = Symbol(
+    "norm(cross(u, v))",
+    any_dimension,
+    display_latex="\\left \\Vert \\left[ \\vec u, \\vec v \\right] \\right \\Vert",
+    positive=True,
+)
+"""
+Length of the cross product between :math:`\\vec u` and :math:`\\vec v`.
+"""
 
-# Law: norm(cross(a, b)) = norm(a) * norm(b) * sin(phi)
-## a, b - vectors
-## phi - angle between them
-## cross(a, b) - cross product between a and b
-## norm(v) - norm, or length, of a
+first_vector_length = Symbol("u", any_dimension, positive=True)
+"""
+Length of :math:`\\vec u`.
+"""
 
-# Condition: cross product is only defined for 3- (and 7-) dimensional vectors.
+second_vector_length = Symbol("v", positive=True)
+"""
+Length of :math:`\\vec v`.
+"""
 
-# TODO: update documentation
+angle_between_vectors = symbols.angle
+"""
+:symbols:`angle` between :math:`\\vec u` and :math:`\\vec v`.
+"""
 
-cross_product_norm = SymSymbol("cross_product_norm", positive=True)
-vector_left_norm = SymSymbol("vector_left_norm", positive=True)
-vector_right_norm = SymSymbol("vector_right_norm", positive=True)
-angle_between_vectors = SymSymbol("cosine_between_vectors", real=True)
+law = Eq(
+    cross_product_length,
+    first_vector_length * second_vector_length * sin(angle_between_vectors),
+)
+"""
+:laws:symbol::
 
-law = Eq(cross_product_norm, vector_left_norm * vector_right_norm * sin(angle_between_vectors))
+:laws:latex::
+"""
 
 
 def calculate_sine_between_vectors(
@@ -34,8 +60,8 @@ def calculate_sine_between_vectors(
     vector_left_norm_ = scale_factor(vector_left_norm_)
     vector_right_norm_ = scale_factor(vector_right_norm_)
     result = solve(law, sin(angle_between_vectors))[0].subs({
-        cross_product_norm: cross_product_norm_,
-        vector_left_norm: vector_left_norm_,
-        vector_right_norm: vector_right_norm_,
+        cross_product_length: cross_product_norm_,
+        first_vector_length: vector_left_norm_,
+        second_vector_length: vector_right_norm_,
     })
     return Quantity(result)
