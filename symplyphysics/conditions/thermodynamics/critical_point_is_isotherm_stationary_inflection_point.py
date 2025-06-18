db6e63136742b724d9b77108d9e27186688b6421
Aleksandr Slatov <alexslatov@gmail.com>
2025-06-18 13:00:38 +0400

    Patch documentation

--- symplyphysics/conditions/thermodynamics/critical_point_is_isotherm_stationary_inflection_point.py
+++ symplyphysics/conditions/thermodynamics/critical_point_is_isotherm_stationary_inflection_point.py
@@ -1,28 +1,46 @@
+"""
+Critical point is stationary inflection point of isotherm
+=========================================================
+
+Critical point (in the thermodynamic sense) is such values of volume, pressure, and temperature at
+which only one phase exists and at the vicinity of which the physical properties of the phases of
+the substance change dramatically. Algebraically, the critical point is the stationary inflection
+point of the isothermal pressure-volume dependency line.
+
+**Note:**
+
+#. These equations need to be solved together with the :ref:`equation of state <**Equations of state**>`.
+
+**Links:**
+
+#. `Wikipedia <https://en.wikipedia.org/wiki/Critical_point_(thermodynamics)#Overview>`__.
+"""
+
 from sympy import Eq, Derivative
 from symplyphysics import symbols, clone_as_function
 
-# Description
-## Critical point (in the thermodynamic sense) is such values of volume, pressure, and temperature at
-## which only one phase exists and at the vicinity of which the physical properties of the phases of the
-## substance change dramatically. Algebraically, the critical point is the stationary inflection point
-## of the isothermal pressure-volume dependency line.
-
-# Conditions: (dp/dV)_T = 0, (d**2(p)/dV**2)_T = 0
-## p - pressure
-## V - volume
-## T - temperature
-## (d/dV)_T - partial derivative with respect to volume at constant temperature
-
-# Note
-## - These equations need to be solved together with the equation of state.
-
-# Links
-## Wikipedia <https://en.wikipedia.org/wiki/Critical_point_(thermodynamics)#Overview>
-
 volume = symbols.volume
+"""
+:symbols:`volume`
+"""
+
 pressure = clone_as_function(symbols.pressure, [volume])
+"""
+:symbols:`pressure` as a function of :attr:`~volume`.
+"""
 
 inflection_point_condition = Eq(Derivative(pressure(volume), volume, 1), 0)
-flat_tangent_condition = Eq(Derivative(pressure(volume), volume, 2), 0)
+"""
+:laws:symbol::
 
-conditions = [inflection_point_condition, flat_tangent_condition]
+:laws:latex::
+"""
+
+flat_tangent_condition = Eq(Derivative(pressure(volume), volume, 2), 0)
+"""
+:laws:symbol::
+
+:laws:latex::
+"""
+
+conditions = inflection_point_condition, flat_tangent_condition
