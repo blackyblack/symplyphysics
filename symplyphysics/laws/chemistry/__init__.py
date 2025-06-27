2c27d9d2ff427a62d6f17064c6535461a0ac8b73
Aleksandr Slatov <alexslatov@gmail.com>
2025-06-27 15:02:46 +0400

    Fix heading

--- symplyphysics/laws/chemistry/__init__.py
+++ symplyphysics/laws/chemistry/__init__.py
@@ -6,12 +6,10 @@ Chemistry is a physical science that studies the chemical elements that make up 
 compounds made of atoms, molecules and ions: their composition, structure, properties,
 behavior and the changes they undergo during reactions with other substances.
 
-Effective cross section
------------------------
+**Effective cross section**
 
 The effective cross section is a physical quantity characterizing the probability of transition of
 a system of two interacting particles to a certain final state, a quantitative characteristic of
 the acts of collision of particles of a stream hitting a target with target particles. The
 effective cross-section has the dimension of the area.
-
 """
