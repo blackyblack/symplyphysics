3ca8be55d90741fdc9c00ef176b8fe573d5a7f58
Aleksandr Slatov <alexslatov@gmail.com>
2025-07-14 10:46:40 +0400

    Add note

--- symplyphysics/laws/hydro/bulk_stress_is_bulk_modulus_times_strain.py
+++ symplyphysics/laws/hydro/bulk_stress_is_bulk_modulus_times_strain.py
@@ -7,6 +7,10 @@ the pressure (hydraulic stress) on the object due to the fluid is proportional t
 change in the object's volume due to that pressure and the bulk modulus of the object. Thus, bulk
 modulus of a substance is a measure of its resistance to bulk compression.
 
+**Notes:**
+
+#. This is an empirical law.
+
 ..
     TODO: find link
 """
