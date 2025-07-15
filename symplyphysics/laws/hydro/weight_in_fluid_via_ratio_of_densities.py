9cf3379a5ecaea08f09dd4bed080e0d9a2a71204
Aleksandr Slatov <alexslatov@gmail.com>
2025-07-15 18:14:07 +0400

    Derive law

--- symplyphysics/laws/hydro/weight_in_fluid_via_ratio_of_densities.py
+++ symplyphysics/laws/hydro/weight_in_fluid_via_ratio_of_densities.py
@@ -2,29 +2,41 @@
 Weight in fluid via ratio of densities
 ======================================
 
-The *Archimedean force* acting on a body immersed in a fluid is equal to the weight of the fluid displaced by the body.
-It can be derived that the weight of the body immersed in the fluid is proportional to its weight in vacuum and also depends
-on the ratio of the fluid density and body density.
+The *Archimedean force* acting on a body fully submersed in a fluid is equal to the weight of the
+fluid displaced by the body. It can be derived that the weight of the body submersed in the fluid
+is proportional to its weight in vacuum and also depends on the ratio of the fluid density and body
+density.
+
+**Conditions:**
+
+#. The body is completely *submersed* in the fluid.
 
 **Links:**
 
 #. `Physics LibreTexts, derivable from here <https://phys.libretexts.org/Bookshelves/University_Physics/Physics_(Boundless)/10%3A_Fluids/10.3%3A_Archimedes_Principle>`__.
 """
 
-from sympy import Eq, solve
-from symplyphysics import (
-    clone_as_symbol,
-    symbols,
-    Quantity,
-    validate_input,
-    validate_output,
+from sympy import Eq, solve, pi, Idx
+from symplyphysics import (clone_as_symbol, symbols, Quantity, validate_input, validate_output,
+    quantities, global_index)
+from symplyphysics.core.expr_comparisons import expr_equals
+
+from symplyphysics.definitions import (
+    density_from_mass_volume as _density_def,
+    net_force_is_sum_of_individual_forces as _superposition_law,
 )
+from symplyphysics.laws.dynamics import (
+    acceleration_is_force_over_mass as _newtons_law,
+    buoyant_force_from_density_and_volume as _archimedes_law,
+)
+from symplyphysics.laws.geometry import (
+    scalar_projection_is_vector_length_times_cosine_of_angle as _projection_law,)
 
 weight_in_fluid = clone_as_symbol(symbols.force,
     display_symbol="W_fl",
     display_latex="W_\\text{fl}")
 """
-Weight of the body immersed in the fluid. See :symbols:`force`.
+Weight of the body submersed in the fluid. See :symbols:`force`.
 """
 
 weight_in_vacuum = clone_as_symbol(symbols.force,
@@ -54,6 +66,62 @@ law = Eq(weight_in_fluid, weight_in_vacuum * (1 - (fluid_density / body_density)
 
 :laws:latex::
 """
+
+# Derive law
+
+_body_volume = _density_def.volume
+
+# 1. Find the Archimedes' force and project it onto the gravity force vector.
+
+_archimedes_force_norm_expr = _archimedes_law.law.rhs.subs({
+    _archimedes_law.fluid_density: fluid_density,
+    _archimedes_law.displaced_volume: _body_volume,
+})
+
+_archimedes_force_proj_expr = _projection_law.law.rhs.subs({
+    _projection_law.vector_length: _archimedes_force_norm_expr,
+    _projection_law.angle: pi,
+})
+
+# 2. True weight of the body is equal to the gravity force on the body as a whole.
+
+_body_mass_expr = solve(
+    _density_def.definition,
+    _density_def.mass,
+)[0].subs(_density_def.density, body_density)
+
+# Equal to the true weight of the body
+_gravity_force_expr = solve(
+    _newtons_law.law,
+    _newtons_law.force,
+)[0].subs({
+    _newtons_law.acceleration: quantities.acceleration_due_to_gravity,
+    _newtons_law.mass: _body_mass_expr,
+})
+
+# 3. Apparent weight is the true weight with the Archimedes' force substituted from it since the
+#    gravity force and the buoyant force are antiparallel.
+
+_apparent_weight_expr = _superposition_law.definition.rhs.subs(
+    global_index,
+    Idx("i", (1, 2)),
+).doit().subs({
+    _superposition_law.force[1]: _gravity_force_expr,
+    _superposition_law.force[2]: _archimedes_force_proj_expr,
+})
+
+# 4. Perform final replacements
+
+_true_weight_eqn = Eq(weight_in_vacuum, _gravity_force_expr)
+_apparent_weight_eqn = Eq(weight_in_fluid, _apparent_weight_expr)
+
+_apparent_weight_expr = solve(
+    (_true_weight_eqn, _apparent_weight_eqn),
+    (_body_volume, weight_in_fluid),
+    dict=True,
+)[0][weight_in_fluid]
+
+assert expr_equals(_apparent_weight_expr, law.rhs)
 
 
 @validate_input(weight_air_=weight_in_vacuum,
