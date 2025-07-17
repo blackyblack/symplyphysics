d0f07739eb7a8c2dcfc366a26cb2b1cc1b227827
Aleksandr Slatov <alexslatov@gmail.com>
2025-07-17 18:09:11 +0400

    Fix derivation

--- symplyphysics/laws/hydro/capillary_height_via_surface_tension_and_contact_angle.py
+++ symplyphysics/laws/hydro/capillary_height_via_surface_tension_and_contact_angle.py
@@ -24,12 +24,19 @@ to the tube's diameter.
     TODO: rename file to use descriptive name
 """
 
-from sympy import Eq, solve, cos
-from symplyphysics import (Quantity, validate_input, validate_output, symbols, quantities)
+from sympy import Eq, solve, cos, dsolve
+from symplyphysics import (Quantity, validate_input, validate_output, symbols, quantities,
+    clone_as_symbol)
 from symplyphysics.core.expr_comparisons import expr_equals
 
-from symplyphysics.laws.thermodynamics import laplace_pressure_of_spherical_shapes as _laplace_law
-from symplyphysics.laws.hydro import hydrostatic_pressure_via_density_and_height as _pressure_law
+from symplyphysics.laws.thermodynamics import (
+    laplace_pressure_of_spherical_shapes as _young_laplace_law,)
+from symplyphysics.laws.hydro import (
+    hydrostatic_pressure_via_density_and_height as _hydrostatic_pressure_law,
+    inner_pressure_is_constant as _bernoulli_principle,
+    inner_pressure_is_sum_of_pressures as _inner_pressure_def,
+    laplace_pressure_is_pressure_difference as _laplace_pressure_def,
+)
 
 height = symbols.height
 """
@@ -67,28 +74,106 @@ law = Eq(
 
 # Derive law
 
+_laplace_pressure = _young_laplace_law.laplace_pressure
+_inner_pressure = _bernoulli_principle.inner_pressure
+_time = _bernoulli_principle.time
+_height = _hydrostatic_pressure_law.height
+_atmospheric_pressure = clone_as_symbol(symbols.pressure, subscript="atm")
+
 # The meniscus has a spherical shape (since a sphere is a shape with the smallest surface area for
 # a given volume). Also see [this figure](https://en.wikipedia.org/wiki/Young%E2%80%93Laplace_equation#/media/File:Spherical_meniscus.PNG)
 _radius_of_curvature = radius / cos(angle)
 
-_laplace_eqn = _laplace_law.law.subs({
-    _laplace_law.surface_tension: surface_tension,
-    _laplace_law.radius_of_curvature: _radius_of_curvature,
+_young_laplace_eqn = _young_laplace_law.law.subs({
+    _young_laplace_law.surface_tension: surface_tension,
+    _young_laplace_law.radius_of_curvature: _radius_of_curvature,
 })
 
-_hydrostatic_pressure_eqn = _pressure_law.law.subs({
-    _pressure_law.density: density,
-    _pressure_law.height: height,
+_hydrostatic_pressure_expr = _hydrostatic_pressure_law.law.rhs.subs({
+    _hydrostatic_pressure_law.density: density,
 })
 
-_fluid_equlibrium_eqn = Eq(
-    _laplace_law.laplace_pressure,
-    _pressure_law.hydrostatic_pressure,
+_bernoulli_dsolved_eqn = dsolve(
+    _bernoulli_principle.law,
+    _inner_pressure(_time),
+    ics={_inner_pressure(0): _inner_pressure(0)},
 )
 
+_inner_pressure_expr = _inner_pressure_def.law.rhs.subs({
+    _inner_pressure_def.dynamic_pressure: 0,  # the fluid is static
+    _inner_pressure_def.hydrostatic_pressure: _hydrostatic_pressure_expr,
+    _inner_pressure_def.inner_pressure: _inner_pressure(_time),
+})
+
+# In the following computations, both "outside inner pressure" and "inside inner pressure" are
+# measured at equal heights.
+
+# a. Concave meniscus: the interface curves towards the inside of the tube. The inside pressure is
+#    the inner pressure under the meniscus, and the outside pressure is the atmospheric pressure.
+
+# Outside the tube at the fluid's surface
+_outside_inner_pressure_expr = _inner_pressure_expr.subs({
+    _time: 0,
+    _height: 0,
+    _inner_pressure_def.static_pressure: _atmospheric_pressure,
+})
+
+# Inside the tube at the bottom of the fluid column, which is the same height as the fluid's
+# surface outside the tube.
+_inside_inner_pressure_expr = _inner_pressure_expr.subs({
+    _inner_pressure_def.static_pressure: _laplace_pressure_def.inside_pressure,
+    _height: height,
+})
+
+_laplace_pressure_eqn = _laplace_pressure_def.law.subs({
+    _laplace_pressure_def.laplace_pressure: _laplace_pressure,
+    _laplace_pressure_def.outside_pressure: _atmospheric_pressure,
+})
+
+_bernoulli_eqn = _bernoulli_dsolved_eqn.subs({
+    _inner_pressure(0): _outside_inner_pressure_expr,
+    _inner_pressure(_time): _inside_inner_pressure_expr,
+})
+
 _height_derived = solve(
-    (_fluid_equlibrium_eqn, _laplace_eqn, _hydrostatic_pressure_eqn),
-    (_laplace_law.laplace_pressure, _pressure_law.hydrostatic_pressure, height),
+    (_young_laplace_eqn, _bernoulli_eqn, _laplace_pressure_eqn),
+    (height, _laplace_pressure, _laplace_pressure_def.inside_pressure),
+    dict=True,
+)[0][height]
+
+assert expr_equals(_height_derived, law.rhs)
+
+# b. Convex meniscus: the interface curves towards the outside of the tube (i.e. the atmosphere).
+#    The inside pressure is the atmospheric pressure and the outside pressure is the pressure under
+#    the meniscus.
+
+# Outside of the tube at a `height` (below the surface) of the lowered fluid in the tube.
+_outside_inner_pressure_expr = _inner_pressure_expr.subs({
+    _time: 0,
+    _height: height,
+    _inner_pressure_def.static_pressure: _atmospheric_pressure,
+})
+
+# Inside the tube at the surface of the meniscus, which is lowered at a certain `height` relative
+# to the fluid's surface outside of the meniscus.
+_inside_inner_pressure_expr = _inner_pressure_expr.subs({
+    _inner_pressure_def.static_pressure: _laplace_pressure_def.outside_pressure,
+    _height: 0,
+})
+
+_laplace_pressure_eqn = _laplace_pressure_def.law.subs({
+    _laplace_pressure_def.laplace_pressure: _laplace_pressure,
+    _laplace_pressure_def.inside_pressure: _atmospheric_pressure,
+})
+
+_bernoulli_eqn = _bernoulli_dsolved_eqn.subs({
+    _inner_pressure(0): _outside_inner_pressure_expr,
+    _inner_pressure(_time): _inside_inner_pressure_expr,
+})
+
+_height_derived = solve(
+    (_young_laplace_eqn, _bernoulli_eqn, _laplace_pressure_eqn),
+    (height, _laplace_pressure, _laplace_pressure_def.outside_pressure),
     dict=True,
 )[0][height]
 
