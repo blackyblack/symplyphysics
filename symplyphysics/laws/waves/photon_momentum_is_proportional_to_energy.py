690c0503b558036ec9f22b5f76c94e2905c3ac49
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-07-25 18:50:22 +0400

    Derive law (#1134)

--- symplyphysics/laws/waves/photon_momentum_is_proportional_to_energy.py
+++ symplyphysics/laws/waves/photon_momentum_is_proportional_to_energy.py
@@ -12,6 +12,10 @@ expressions.
 
 #. :quantity_notation:`speed_of_light`.
 
+**Conditions:**
+
+#. Works in a **vacuum** environment.
+
 **Links:**
 
 #. `Wikipedia, derivable from here <https://en.wikipedia.org/wiki/Photon#Energy_and_momentum>`__.
@@ -19,6 +23,17 @@ expressions.
 
 from sympy import Eq, solve
 from symplyphysics import Quantity, validate_input, validate_output, quantities, symbols
+from symplyphysics.core.expr_comparisons import expr_equals
+
+from symplyphysics.definitions import (
+    angular_wavenumber_is_inverse_wavelength as _wavenumber_def,
+    period_from_angular_frequency as _period_def,
+)
+from symplyphysics.laws.waves import (
+    photon_momentum_is_proportional_to_angular_wavenumber as _momentum_law,
+    photon_energy_is_proportional_to_angular_frequency as _energy_law,
+    wavelength_from_phase_speed_and_period as _wavelength_law,
+)
 
 momentum = symbols.momentum
 """
@@ -36,6 +51,39 @@ law = Eq(momentum, energy / quantities.speed_of_light)
 
 :laws:latex::
 """
+
+# Derive law
+
+_momentum_eqn = _momentum_law.law.subs({
+    _momentum_law.momentum: momentum,
+})
+
+_energy_eqn = _energy_law.law.subs({
+    _energy_law.energy: energy,
+})
+
+_wavenumber_eqn = _wavenumber_def.definition.subs({
+    _wavenumber_def.angular_wavenumber: _momentum_law.angular_wavenumber,
+})
+
+_wavelength_eqn = _wavelength_law.law.subs({
+    _wavelength_law.wavelength: _wavenumber_def.wavelength,
+    _wavelength_law.phase_velocity: quantities.speed_of_light,  # only holds in vacuum
+})
+
+_period_eqn = _period_def.law.subs({
+    _period_def.period: _wavelength_law.period,
+    _period_def.angular_frequency: _energy_law.angular_frequency,
+})
+
+_momentum_derived = solve(
+    (_momentum_eqn, _energy_eqn, _wavenumber_eqn, _wavelength_eqn, _period_eqn),
+    (momentum, _momentum_law.angular_wavenumber, _energy_law.angular_frequency,
+    _wavenumber_def.wavelength, _wavelength_law.period),
+    dict=True,
+)[0][momentum]
+
+assert expr_equals(_momentum_derived, law.rhs)
 
 
 @validate_input(photon_energy_=energy)
