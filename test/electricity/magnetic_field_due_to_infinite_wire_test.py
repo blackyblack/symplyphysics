e2a9300acd050f21c5d2c3e9e9c75f9764712e7e
Aleksandr Slatov <alexslatov@gmail.com>
2025-10-10 13:01:57 +0400

    Derive law and fix test

--- test/electricity/magnetic_field_due_to_infinite_wire_test.py
+++ test/electricity/magnetic_field_due_to_infinite_wire_test.py
@@ -1,34 +1,44 @@
 from collections import namedtuple
 from pytest import fixture, raises
-from symplyphysics import (assert_equal, units, Quantity, errors, prefixes)
+from symplyphysics import (assert_equal, units, Quantity, errors, prefixes, quantities)
 from symplyphysics.laws.electricity import magnetic_field_due_to_infinite_wire as law
 
-Args = namedtuple("Args", ["current", "distance"])
+Args = namedtuple("Args", ["permeability", "current", "distance"])
 
 
 @fixture(name="test_args")
 def test_args_fixture() -> Args:
+    permeability = Quantity(quantities.vacuum_permeability * 1.1)
     current = Quantity(0.5 * units.ampere)
     distance = Quantity(2 * units.meter)
-    return Args(current=current, distance=distance)
+    return Args(permeability=permeability, current=current, distance=distance)
 
 
 def test_law(test_args: Args) -> None:
-    result = law.calculate_magnetic_field(test_args.current, test_args.distance)
-    assert_equal(result, 50 * prefixes.nano * units.tesla)
+    result = law.calculate_magnetic_field(test_args.permeability, test_args.current,
+        test_args.distance)
+    assert_equal(result, 55 * prefixes.nano * units.tesla)
+
+
+def test_bad_permeability(test_args: Args) -> None:
+    mub = Quantity(units.candela)
+    with raises(errors.UnitsError):
+        law.calculate_magnetic_field(mub, test_args.current, test_args.distance)
+    with raises(TypeError):
+        law.calculate_magnetic_field(100, test_args.current, test_args.distance)
 
 
 def test_bad_current(test_args: Args) -> None:
     current = Quantity(1 * units.candela)
     with raises(errors.UnitsError):
-        law.calculate_magnetic_field(current, test_args.distance)
+        law.calculate_magnetic_field(test_args.permeability, current, test_args.distance)
     with raises(TypeError):
-        law.calculate_magnetic_field(100, test_args.distance)
+        law.calculate_magnetic_field(test_args.permeability, 100, test_args.distance)
 
 
 def test_bad_distance(test_args: Args) -> None:
     distance = Quantity(1 * units.candela)
     with raises(errors.UnitsError):
-        law.calculate_magnetic_field(test_args.current, distance)
+        law.calculate_magnetic_field(test_args.permeability, test_args.current, distance)
     with raises(TypeError):
-        law.calculate_magnetic_field(test_args.current, 100)
+        law.calculate_magnetic_field(test_args.permeability, test_args.current, 100)
