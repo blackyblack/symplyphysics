3683f924d3ab5f69d3b8373e9c5bec4761a90dc1
Aleksandr Slatov <alexslatov@gmail.com>
2025-10-15 20:04:19 +0400

    Create code

---
+++ symplyphysics/docs/unique_law_id.py
@@ -0,0 +1,136 @@
+from typing import Collection, Optional
+from pathlib import Path
+import re
+import json
+
+ID_PATTERN = re.compile(r"#\s*UNIQUE_LAW_ID:\s*(\d+)\s*")
+
+
+def _check_id_uniqueness(ids: Collection[int]) -> None:
+    id_to_count: dict[int, int] = {}
+    for id_ in ids:
+        id_to_count[id_] = id_to_count.get(id_, 0) + 1
+
+    repeating_ids = [id_ for id_, count in id_to_count.items() if count > 1]
+    if not repeating_ids:
+        return
+
+    raise ValueError(f"Repeating ids {repeating_ids}")
+
+
+def _load_saved_data(saved_path: Path) -> dict[str, int]:
+    if not saved_path.exists():
+        return {}
+
+    with open(saved_path, "r", encoding="utf-8") as fp:
+        name_to_id = json.load(fp)
+
+    _check_id_uniqueness(name_to_id.values())
+
+    return name_to_id
+
+
+def _read_file_and_match(
+    path: Path,
+    name: str,
+    pattern: re.Pattern,
+) -> tuple[list[str], int | None]:
+    lines: list[str] = []
+    match_id = None
+
+    with open(path, "r", encoding="utf-8") as fp:
+        for line in fp:
+            m = pattern.match(line)
+
+            if not m:
+                lines.append(line)
+                continue
+
+            if match_id is not None:
+                raise RuntimeError(f"Multiple ids in file '{name}'")
+
+            match_id = int(m.group(1))
+
+    return lines, match_id
+
+
+def _add_empty_lines(lines: list[str], *, nmax: int) -> None:
+    count_empty = 0
+    for line in reversed(lines):
+        if line != "\n":
+            break
+        count_empty += 1
+
+    for _ in range(nmax - count_empty):
+        lines.append("\n")
+
+
+def _process_ids(
+    name: str,
+    name_to_id: dict[str, int],
+    match_id: Optional[int],
+    saved_id: Optional[int],
+) -> int:
+    if match_id is None:
+        if saved_id is None:
+            # The file is new and hasn't been saved before
+            new_id = max(name_to_id.values(), default=0) + 1
+            name_to_id[name] = new_id
+            return new_id
+
+        # The comment has been deleted from the file, but it's in the database
+        return saved_id
+
+    if saved_id is None:
+        # The file has the comment, but its name isn't in the database, perhaps it
+        # was renamed; therefore we remove the old file name and replace it with the
+        # new one
+
+        saved_names = (name for name, id_ in name_to_id.items() if id_ == match_id)
+        for saved_name in saved_names:
+            del name_to_id[saved_name]
+
+        name_to_id[name] = match_id
+    elif saved_id != match_id:
+        # Both the file and the database contain the id, but they don't match
+
+        message = f"File '{name}' should have index {saved_id}, got {match_id} instead"
+        raise RuntimeError(message)
+
+    return match_id
+
+
+def _main(
+    base_dir: Optional[Path] = None,
+    saved_path: Optional[Path] = None,
+    exclude_dir_names: Collection[str] = ("core", "docs"),
+    pattern: re.Pattern = ID_PATTERN,
+) -> None:
+    base_dir = base_dir or Path(__file__).parent.parent
+    saved_path = saved_path or base_dir.parent.joinpath("unique_law_id.json")
+    exclude_dir_paths = [base_dir.joinpath(dir_) for dir_ in exclude_dir_names]
+
+    name_to_id = _load_saved_data(saved_path)
+
+    for path in base_dir.rglob("*.py"):
+        if any(path.is_relative_to(dir_path) for dir_path in exclude_dir_paths):
+            continue
+
+        name = "/" + str(path.relative_to(base_dir))
+
+        lines, match_id = _read_file_and_match(path, name, pattern)
+        _add_empty_lines(lines, nmax=2)
+
+        saved_id = name_to_id.get(name)
+        write_id = _process_ids(name, name_to_id, match_id, saved_id)
+
+        lines.append(f"# UNIQUE_LAW_ID: {write_id}\n")
+        with open(path, "w", encoding="utf-8") as fp:
+            fp.writelines(lines)
+
+    with open(saved_path, "w", encoding="utf-8") as fp:
+        json.dump(name_to_id, fp, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    _main()
