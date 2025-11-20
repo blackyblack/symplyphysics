5aea77b17ea47aa7cee6fbb7b21bf62a9663a4d8
Aleksandr Slatov <alexslatov@gmail.com>
2025-11-20 22:15:55 +0400

    Fix example description

--- plots/electricity/resistor_capacitor_integrator.py
+++ plots/electricity/resistor_capacitor_integrator.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python3
+r"""
+For an :math:`RC` integrator (i.e. a serial :math:`RC` circuit), assume a step voltage input and
+plot the voltage across charging capacitor and the current in the circuit as functions of time.
 
-# Description
-## Time constant of RC Tau = R*C (seconds).
-## RC integrator is a circuit with capacitor and resistor in series. Initial_voltage is applied to whole circuit and integrated voltage is obtained from capacitor.
-## If some voltage is applied to RC integrator, capacitor starts to charge and it's voltage rises.
-## Capacitor voltage will never reach initial voltage.
-## Voltage on the capacitor reaches 63% of initial voltage after 1*Tau seconds, and reaches 95% after 3*Tau seconds
-## independently of initial voltage.
+The step input is defined as
+.. math::
+    U(t) = \begin{cases} 0 & t < 0 \\ U_0 & t \\ge 0 \end{cases}
 
-from sympy import symbols, solve
+Note that the time constant of the serial :math:`RC` circuit is :math:`\tau = R C`. After time
+:math:`t = N \tau` passes, the voltage across the capacitor will reach :math:`1 - e^{-N}` times
+its maximum value. The circuit current, on the other hand, will simply decrease by a factor of
+:math:`e^N`.
+
+**Links:**
+
+#. `Wikipedia <https://en.wikipedia.org/wiki/RC_circuit#Time-domain_considerations>`__.
+"""
+
+from sympy import symbols as sym_symbols, solve
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
+from symplyphysics import units, convert_to_si
 from symplyphysics.laws.electricity.circuits import (
     voltage_across_charging_capacitor_in_serial_resistor_capacitor_circuit as rc_node,
     time_constant_of_resistor_capacitor_circuit as time_constant_law,
 )
 
-time = symbols("time")
+time = sym_symbols("time")
 
-INITIAL_VOLTAGE = 1
-EXAMPLE_CAPACITANCE = 1
-EXAMPLE_IMPEDANCE = 1
+INITIAL_VOLTAGE = convert_to_si(1 * units.volt)
+EXAMPLE_CAPACITANCE = convert_to_si(1 * units.farad)
+EXAMPLE_RESISTANCE = convert_to_si(1 * units.ohm)
 
-RC_TIME_CONSTANT = EXAMPLE_CAPACITANCE * EXAMPLE_IMPEDANCE
+RC_TIME_CONSTANT = EXAMPLE_CAPACITANCE * EXAMPLE_RESISTANCE
 
 time_constant_expr = time_constant_law.law.rhs.subs({
-    time_constant_law.resistance: EXAMPLE_IMPEDANCE,
+    time_constant_law.resistance: EXAMPLE_RESISTANCE,
     time_constant_law.capacitance: EXAMPLE_CAPACITANCE,
 })
 
@@ -40,19 +50,21 @@ capacitor_voltage_function = solve(applied_law, rc_node.capacitor_voltage,
 # see resistor_and_capacitor_as_integrator_node.capacitor_current_eq for a proof the current on resistor equals to current on capacitor
 # see resistor_and_capacitor_as_integrator_node.resistor_voltage_eq for a proof the voltage on resistor is (initial_voltage - capacitor_voltage_function)
 # see symplyphysics.laws.electricity.current_is_proportional_to_voltage for a proof that current through resistor = resistor voltage / resistor impedance
-capacitor_current_function = (INITIAL_VOLTAGE - capacitor_voltage_function) / EXAMPLE_IMPEDANCE
+capacitor_current_function = (INITIAL_VOLTAGE - capacitor_voltage_function) / EXAMPLE_RESISTANCE
 
 UC = plot(capacitor_voltage_function, (time, 0, 8 * RC_TIME_CONSTANT),
     line_color="blue",
-    title="Capacitor voltage",
+    title="Charging capacitor in a serial $RC$ circuit",
     label="Capacitor voltage",
+    xlabel="time $t$",
+    ylabel="reduced quantities",
     legend=True,
     backend=MatplotlibBackend,
     show=False)
 
 IC = plot(capacitor_current_function, (time, 0, 8 * RC_TIME_CONSTANT),
     line_color="orange",
-    label="Capacitor current",
+    label="Circuit current",
     legend=True,
     backend=MatplotlibBackend,
     show=False)
@@ -60,7 +72,7 @@ UC.append(IC[0])
 
 voltage063 = plot(0.63 * INITIAL_VOLTAGE, (time, 0, RC_TIME_CONSTANT),
     line_color="yellow",
-    label="U = 0.63 of U0",
+    label="$U = 0.63 U_0$",
     backend=MatplotlibBackend,
     show=False)
 UC.append(voltage063[0])
@@ -68,7 +80,7 @@ UC.append(voltage063[0])
 tau_line = plot(
     1000 * (time - RC_TIME_CONSTANT) * 0.63 * INITIAL_VOLTAGE,
     (time, RC_TIME_CONSTANT, RC_TIME_CONSTANT + 0.001),
-    label="time = Tau",
+    label="$t = \\tau$",
     line_color="yellow",
     show=False,
 )
@@ -76,7 +88,7 @@ UC.append(tau_line[0])
 
 voltage095 = plot(0.95 * INITIAL_VOLTAGE, (time, 0, 3 * RC_TIME_CONSTANT),
     line_color="green",
-    label="U = 0.95 of U0",
+    label="$U = 0.95 U_0$",
     backend=MatplotlibBackend,
     show=False)
 UC.append(voltage095[0])
@@ -84,7 +96,7 @@ UC.append(voltage095[0])
 tau3_line = plot(
     1000 * (time - 3 * RC_TIME_CONSTANT) * 0.95 * INITIAL_VOLTAGE,
     (time, 3 * RC_TIME_CONSTANT, 3 * RC_TIME_CONSTANT + 0.001),
-    label="time = 3 * Tau",
+    label="$t = 3 \\tau$",
     line_color="green",
     show=False,
 )
@@ -92,7 +104,7 @@ UC.append(tau3_line[0])
 
 voltageFull = plot(INITIAL_VOLTAGE, (time, 0, 8 * RC_TIME_CONSTANT),
     line_color="red",
-    label="U0",
+    label="$U_0$",
     backend=MatplotlibBackend,
     show=False)
 UC.append(voltageFull[0])
