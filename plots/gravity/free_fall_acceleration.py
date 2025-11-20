c64b52de684cfb32d6d09e3993587c50f28018d0
Aleksandr Slatov <alexslatov@gmail.com>
2025-11-20 22:45:57 +0400

    Fix printing

--- plots/gravity/free_fall_acceleration.py
+++ plots/gravity/free_fall_acceleration.py
@@ -1,39 +1,45 @@
-from sympy import solve, symbols
+#!/usr/bin/env python3
+"""
+Plot the free fall acceleration on Earth as a function of elevation (i.e. perpendicular distance
+to Earth's surface).
+"""
+
 from sympy.plotting import plot
 from sympy.plotting.plot import MatplotlibBackend
-from symplyphysics import (print_expression, units, convert_to, Quantity, quantities)
-from symplyphysics.laws.gravity import free_fall_acceleration_from_height as acceleration
-
-print(f"Formula is:\n{print_expression(acceleration.law)}")
-
-height_above_ground = symbols("height_above_ground")
-gravity_constant_value = convert_to(Quantity(units.gravitational_constant),
-    units.newton * units.meter**2 / units.kilogram**2).evalf(5)
-EARTH_MASS = 5.9722e24  # kilogram
-EARTH_RADIUS = 6.371e6  # meter
-
-solved = solve(acceleration.law, acceleration.free_fall_acceleration,
-    dict=True)[0][acceleration.free_fall_acceleration]
-result_acceleration = solved.subs({
-    acceleration.planet_mass: EARTH_MASS,
-    acceleration.planet_radius: EARTH_RADIUS,
-    acceleration.elevation: height_above_ground,
-    quantities.gravitational_constant: gravity_constant_value
-})
+from symplyphysics import print_expression, quantities, units
+from symplyphysics.core.convert import evaluate_expression
+from symplyphysics.laws.gravity import free_fall_acceleration_from_height as acceleration_law
 
 print(
-    f"\nFree fall accelleration function on Earth surface is:\n{print_expression(result_acceleration)}"
+    f"Free fall acceleration `a` as a function of elevation `h`:",
+    print_expression(acceleration_law.law),
+    sep="\n",
+    end="\n\n",
 )
 
-p1 = plot(result_acceleration, (height_above_ground, 0, 10000),
+elevation = acceleration_law.elevation
+
+gravity_constant_value = quantities.gravitational_constant
+EARTH_MASS = 5.9722e24 * units.kilogram
+EARTH_RADIUS = 6.371e6 * units.meter
+
+free_fall_acceleration_expr = acceleration_law.law.rhs.subs({
+    acceleration_law.planet_mass: EARTH_MASS,
+    acceleration_law.planet_radius: EARTH_RADIUS,
+    acceleration_law.elevation: elevation,
+})
+free_fall_acceleration_expr = evaluate_expression(free_fall_acceleration_expr)
+
+p1 = plot(
+    free_fall_acceleration_expr,
+    (elevation, 0, 10e3),  # meters
     ylim=(9.74, 9.85),
     axis_center=(0.0, 9.75),
     line_color="red",
-    title="Free fall acceleration",
-    xlabel="height, m",
-    ylabel="acceleration, m/s**2",
-    label="Acceleration(height)",
-    legend=True,
+    title="Free fall acceleration $g$ versus elevation $h$",
+    xlabel=r"$h, \text{m}$",
+    ylabel=r"$g, \text{m}/\text{s}^2$",
     backend=MatplotlibBackend,
-    show=False)
+    show=False,
+)
 p1.show()
