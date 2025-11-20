51135a48d0d7f467e0aaac485c1526504291888b
Aleksandr Slatov <alexslatov@gmail.com>
2025-11-20 23:27:09 +0400

    Add description

--- examples/optics/determination_of_width_of_first_interference_band.py
+++ examples/optics/determination_of_width_of_first_interference_band.py
@@ -1,53 +1,57 @@
 #!/usr/bin/env python3
+r"""
+Two slits are separated by :math:`0.2 \, \text{mm}` and are located :math:`1.5 \, \text{m}` from a
+screen. A beam of monochromatic light (:math:`\lambda = 500 \, \text{nm}`) from a distant source
+falls on the slits. Find the distance between adjacent interference fringes.
 
-from sympy import solve, Symbol, Eq
-from symplyphysics import print_expression, Quantity, prefixes, units, convert_to
-from symplyphysics.laws.optics import interference_from_two_slits as two_slits_law
-from symplyphysics.laws.optics import interference_maximum as maximum_law
+**Source:** `(in Russian) <https://tasksall.ru/ovr0/watchbookfominyear1997num6.707.htm>`__.
+"""
 
-# Example from https://tasksall.ru/ovr0/watchbookfominyear1997num6.707.htm
+from sympy import solve, Symbol
+from symplyphysics import print_expression, Quantity, prefixes, units, convert_to, assert_equal
+from symplyphysics.laws.optics import (
+    interference_from_two_slits as two_slits_law,
+    interference_maximum as interference_maximum_law,
+)
 
-distance_to_pattern = Symbol("distance_to_pattern")
-distance_between_slits = Symbol("distance_between_slits")
-wavelength = Symbol("wavelength")
+distance_to_picture = two_slits_law.distance_to_picture
+distance_between_slits = two_slits_law.distance_between_slits
+wavelength = interference_maximum_law.wavelength
+maximum_number = interference_maximum_law.integer_factor
+optical_distance_difference = two_slits_law.travel_difference
+distance_between_maxima = Symbol("D", real=True)
 
-number_of_maximum = Symbol("number_of_maximum")
-optical_distance_difference = Symbol("optical_distance_difference")
+optical_distance_difference_expr = solve(two_slits_law.law, optical_distance_difference)[0]
 
-distance_between_maxima = Symbol("distance_between_maxima")
+maximum_condition_eqn = interference_maximum_law.law.subs(
+    interference_maximum_law.travel_difference,
+    optical_distance_difference_expr,
+)
+maximum_coordinate_expr = solve(maximum_condition_eqn, two_slits_law.coordinate)[0]
 
-optical_distance_difference_eq = two_slits_law.law.subs({
-    two_slits_law.distance_to_picture: distance_to_pattern,
-    two_slits_law.distance_between_slits: distance_between_slits,
-    two_slits_law.travel_difference: optical_distance_difference
-})
-optical_distance_difference_value = solve(optical_distance_difference_eq,
-    optical_distance_difference,
-    dict=True)[0][optical_distance_difference]
-maximum_condition_eq = maximum_law.law.subs({
-    maximum_law.wavelength: wavelength,
-    maximum_law.travel_difference: optical_distance_difference_value,
-    maximum_law.integer_factor: number_of_maximum
-})
+first_maximum_coordinate_expr = maximum_coordinate_expr.subs(maximum_number, 1)
+second_maximum_coordinate_expr = maximum_coordinate_expr.subs(maximum_number, 2)
 
-coordinate_value = solve(maximum_condition_eq, two_slits_law.coordinate,
-    dict=True)[0][two_slits_law.coordinate]
+# The distance between the maxima is the difference in their coordinates in the interference pattern:
+# `dx = x_2 - x_1`
+distance_between_maxima_expr = second_maximum_coordinate_expr - first_maximum_coordinate_expr
 
-coordinate_for_first_maximum_value = coordinate_value.subs({number_of_maximum: 1})
-coordinate_for_second_maximum_value = coordinate_value.subs({number_of_maximum: 2})
+print(
+    "Equation for distance between first and second maxima:",
+    print_expression(distance_between_maxima_expr),
+    sep="\n",
+)
 
-# The distance between the maxima is the difference in the coordinates
-# of these maxima in the interference pattern
-# dx = x_2 - x_1
-distance_between_maxima_value = coordinate_for_second_maximum_value - coordinate_for_first_maximum_value
-answer = Eq(distance_between_maxima, distance_between_maxima_value)
-print(f"Equation for distance between first and second maxima:\n{print_expression(answer)}")
-
-distance_between_maxima_ = answer.subs({
-    distance_to_pattern: Quantity(1.5 * units.meters),
+distance_between_maxima_subs = distance_between_maxima_expr.subs({
+    distance_to_picture: Quantity(1.5 * units.meters),
     distance_between_slits: Quantity(0.2 * prefixes.milli * units.meters),
     wavelength: Quantity(500 * prefixes.nano * units.meters),
-}).rhs
-print(
-    f"distance between first and second maxima is: {print_expression(convert_to(Quantity(distance_between_maxima_), prefixes.milli * units.meters).evalf(3))} mm"
-)
+})
+assert_equal(distance_between_maxima_subs, 3.75 * units.milli * units.meter)
+
+distance_between_maxima_float = convert_to(
+    distance_between_maxima_subs,
+    units.milli * units.meter,
+).evalf(3)
+
+print("distance between first and second maxima is:", distance_between_maxima_float, "mm")
