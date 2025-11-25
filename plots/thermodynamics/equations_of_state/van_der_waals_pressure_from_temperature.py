63597cce62427dc71d5be24b5a77497d3c36279b
Aleksandr Slatov <alexslatov@gmail.com>
2025-11-25 18:05:40 +0400

    Add description

--- plots/thermodynamics/equations_of_state/van_der_waals_pressure_from_temperature.py
+++ plots/thermodynamics/equations_of_state/van_der_waals_pressure_from_temperature.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Plot the pressure of a van der Waals gas as a function of its temperature for different values of
+gas volume.
+"""
 
 from sympy import symbols, Eq, solve
 from sympy.plotting import plot
