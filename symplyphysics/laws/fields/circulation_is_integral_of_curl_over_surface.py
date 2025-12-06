b791885184a5262b7c1aefeae1d901615029371d
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2025-12-07 00:53:09 +0400

    Circulation is integral of curl over surface (#1173)
    * Update law and test
    
    * Fix docstring

---
+++ symplyphysics/laws/fields/circulation_is_integral_of_curl_over_surface.py
@@ -0,0 +1,119 @@
+"""
+Circulation is integral of curl over surface
+============================================
+
+The circulation of a vector field, defined as :ref:`such <Circulation is integral along curve>`,
+can be re-written as a double integral of the flux of that field over any surface whose boundary
+is the curve along which the circulation is calculated.
+
+**Notes:**
+
+#. This law is also known as the `Stokes' theorem <https://en.wikipedia.org/wiki/Stokes%27_theorem>`__.
+
+**Conditions:**
+
+#. The surface is smooth.
+#. The orientation of the surface and the orientation of the curve are related by the right-hand
+   rule.
+"""
+
+from sympy import Eq
+from symplyphysics import Quantity, validate_input
+from symplyphysics.core.dimensions import any_dimension, dimensionless
+from symplyphysics.core.symbols.symbols import BasicSymbol, Symbol
+from symplyphysics.core.coordinate_systems import CoordinateVector
+from symplyphysics.core.coordinate_systems.surface import Surface
+from symplyphysics.core.vectors import VectorSymbol, VectorDot
+from symplyphysics.core.operators import VectorCurl
+from symplyphysics.core.integrals.surface_integral import SurfaceIntegral, INFINITESIMAL_VECTOR_AREA
+
+circulation = Symbol("G", any_dimension)
+"""
+Circulation of the vector :attr:`~field`.
+"""
+
+field = VectorSymbol("F", any_dimension)
+"""
+Any vector field, i.e. a vector-valued function that depends on the position vector.
+"""
+
+surface = BasicSymbol("S")
+"""
+Surface along which the integral is evaluated.
+
+Symbol:
+    :code:`S`
+
+Latex:
+    :math:`S`
+"""
+
+initial_first_parameter = Symbol("u_1", dimensionless)
+"""
+Initial value of the first surface parameter.
+"""
+
+final_first_parameter = Symbol("u_2", dimensionless)
+"""
+Final value of the first surface parameter.
+"""
+
+initial_second_parameter = Symbol("v_1", dimensionless)
+"""
+Initial value of the second surface parameter.
+"""
+
+final_second_parameter = Symbol("v_2", dimensionless)
+"""
+Final value of the second surface parameter.
+"""
+
+law = Eq(
+    circulation,
+    SurfaceIntegral(
+    VectorDot(VectorCurl(field, evaluate=False), INFINITESIMAL_VECTOR_AREA, evaluate=False),
+    surface,
+    ((initial_first_parameter, final_first_parameter),
+    (initial_second_parameter, final_second_parameter)),
+    ),
+)
+r"""
+:laws:symbol::
+
+..
+    FIXME Unimplemented latex printer for the surface integral
+
+Latex:
+
+    .. math::
+
+        G = \iint \limits_S \left( \nabla \times \vec F \right) \cdot d \vec S
+"""
+
+
+@validate_input(
+    initial_first_parameter_=initial_first_parameter,
+    final_first_parameter_=final_first_parameter,
+    initial_second_parameter_=initial_second_parameter,
+    final_second_parameter_=final_second_parameter,
+)
+def calculate_circulation(
+    field_: CoordinateVector,
+    surface_: Surface,
+    initial_first_parameter_: Quantity | float,
+    final_first_parameter_: Quantity | float,
+    initial_second_parameter_: Quantity | float,
+    final_second_parameter_: Quantity | float,
+) -> Quantity:
+    result = law.rhs.subs({
+        field: field_,
+    }).subs({
+        surface: surface_,
+        initial_first_parameter: initial_first_parameter_,
+        final_first_parameter: final_first_parameter_,
+        initial_second_parameter: initial_second_parameter_,
+        final_second_parameter: final_second_parameter_,
+    })
+
+    result = result.doit().doit()
+    return Quantity(result)
