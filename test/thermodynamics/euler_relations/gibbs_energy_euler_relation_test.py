4520b020347020623661592f450df6678aa0ed8f
Aleksandr Slatov <alexslatov@gmail.com>
2025-12-09 16:04:19 +0400

    Rename law and test

--- test/thermodynamics/euler_relations/gibbs_energy_euler_relation_test.py
+++ test/thermodynamics/euler_relations/gibbs_energy_euler_relation_test.py
@@ -6,7 +6,7 @@ from symplyphysics import (
     units,
     Quantity,
 )
-from symplyphysics.laws.thermodynamics.euler_relations import gibbs_energy_formula
+from symplyphysics.laws.thermodynamics.euler_relations import gibbs_energy_euler_relation
 
 # Description
 ## The Gibbs energy of a one-component system consisting of 1e20 particles and having a chemical potential
@@ -23,19 +23,19 @@ def test_args_fixture() -> Args:
 
 
 def test_law(test_args: Args) -> None:
-    result = gibbs_energy_formula.calculate_gibbs_energy(test_args.mu, test_args.n)
+    result = gibbs_energy_euler_relation.calculate_gibbs_energy(test_args.mu, test_args.n)
     assert_equal(result, -32 * units.joule, relative_tolerance=2e-3)
 
 
 def test_bad_chemical_potential(test_args: Args) -> None:
     mub = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        gibbs_energy_formula.calculate_gibbs_energy(mub, test_args.n)
+        gibbs_energy_euler_relation.calculate_gibbs_energy(mub, test_args.n)
     with raises(TypeError):
-        gibbs_energy_formula.calculate_gibbs_energy(100, test_args.n)
+        gibbs_energy_euler_relation.calculate_gibbs_energy(100, test_args.n)
 
 
 def test_bad_particle_number(test_args: Args) -> None:
     nb = Quantity(1 * units.coulomb)
     with raises(errors.UnitsError):
-        gibbs_energy_formula.calculate_gibbs_energy(test_args.mu, nb)
+        gibbs_energy_euler_relation.calculate_gibbs_energy(test_args.mu, nb)
