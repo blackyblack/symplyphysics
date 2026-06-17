b51785aea771b609022d08adfb63eed617f9c936
Aleksandr Slatov <alexslatov@gmail.com>
2026-06-17 22:15:47 +0400

    Add description to Astronomy and Chemistry folders

--- symplyphysics/chemistry/__init__.py
+++ symplyphysics/chemistry/__init__.py
@@ -2,4 +2,14 @@
 Chemistry
 =========
 
+**Chemistry** is the scientific study of the properties and behavior of matter related to chemical
+reactions of matter with other substances. It also studies the nature of chemical bonds and
+chemical compounds.
+
+A **chemical reaction** is a transformation of some substances into one or more different
+substances.
+
+**Links:**
+
+#. `Wikipedia — Chemistry <https://en.wikipedia.org/wiki/Chemistry>`__
 """
--- symplyphysics/chemistry/electrolysis/__init__.py
+++ symplyphysics/chemistry/electrolysis/__init__.py
@@ -1,5 +1,12 @@
 """
-**Electrolysis**
-================
+Electrolysis
+============
 
+**Electrolysis** is a process by which an electric current is passed through a substance that
+drives an otherwise non-spontaneous chemical reaction.
+
+**Links:**
+
+#. `Wikipedia — Electrolysis <https://en.wikipedia.org/wiki/Electrolysis>`__
+#. `Chemistry LibreTexts — Electrolysis <https://chem.libretexts.org/Courses/Saint_Marys_College_Notre_Dame_IN/CHEM_122-02_(Under_Construction)/4%3A_Electrochemistry/4.1%3A_Electrochemistry/Electrolysis>`__
 """
