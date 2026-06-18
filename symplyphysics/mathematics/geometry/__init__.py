fd3162fb460bdbf55096216cff2fe3370e01bd3f
blackyblack <johnmalkovich@mail.ru>
2026-06-18 18:06:55 +0200

    Update symplyphysics/mathematics/geometry/__init__.py

--- symplyphysics/mathematics/geometry/__init__.py
+++ symplyphysics/mathematics/geometry/__init__.py
@@ -2,6 +2,6 @@
 Geometry
 ========
 
-This module is concerned with some geometric results and identities used in physics, such as the
+This module is concerned with some geometric results and identities used in physics, such as
 vector and scalar products, vector projection, and vector area.
 """
