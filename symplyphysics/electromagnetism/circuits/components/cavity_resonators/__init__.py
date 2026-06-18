774b4c9c179c1ad5fdbe77f23144304af7664f40
Aleksandr Slatov <alexslatov@gmail.com>
2026-06-18 16:41:13 +0400

    Rename as cavity resonators

--- symplyphysics/electromagnetism/circuits/components/cavity_resonators/__init__.py
+++ symplyphysics/electromagnetism/circuits/components/cavity_resonators/__init__.py
@@ -1,6 +1,6 @@
 """
-Resonators
-==========
+Cavity Resonators
+=================
 
 A **microwave cavity** or **radio frequency cavity** (**RF cavity**) is a special type of resonator, consisting of a closed (or largely closed) metal structure that confines :ref:`electromagnetic fields <Electromagnetic waves>` in the microwave or RF region of the spectrum. The structure is either hollow or filled with dielectric material.
 
