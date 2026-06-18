4214940a52e19bc417878c85dac4dc9a02f90079
blackyblack <johnmalkovich@mail.ru>
2026-06-18 17:24:37 +0200

    Update symplyphysics/optics/geometrical_optics/invariants/__init__.py

--- symplyphysics/optics/geometrical_optics/invariants/__init__.py
+++ symplyphysics/optics/geometrical_optics/invariants/__init__.py
@@ -2,6 +2,6 @@
 Invariants (Geometrical Optics)
 ===============================
 
-This section focuses on the optical invariants, e.g. the Abbe sige condition
+This section focuses on the optical invariants, e.g. the Abbe sine condition
 (`Wikipedia <https://en.wikipedia.org/wiki/Abbe_sine_condition>`__).
 """
