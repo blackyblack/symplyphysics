16469ec370c5defb90f8e222beb29bb79cfd9d57
blackyblack <johnmalkovich@mail.ru>
2026-06-18 17:24:53 +0200

    Update symplyphysics/oscillations/mechanical_oscillations/__init__.py

--- symplyphysics/oscillations/mechanical_oscillations/__init__.py
+++ symplyphysics/oscillations/mechanical_oscillations/__init__.py
@@ -2,7 +2,7 @@
 Mechanical Oscillations
 =======================
 
-**Mechanical oscillators** are a systems that perform oscillatory *motion* around an equilibrium
+**Mechanical oscillators** are systems that perform oscillatory *motion* around an equilibrium
 position. This occurs thanks to the presence of a *restoring force* that appears whenever the system
 is pushed away from the equilibrium configuration and attempts to restore the equilibrium.
 
