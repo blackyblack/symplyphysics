bfd9ddec055e24716373807cddd0c5102fd968c7
Aleksandr Slatov <alexslatov@gmail.com>
2026-06-18 14:20:59 +0400

    Add description to Condensed Matter

--- symplyphysics/condensed_matter/transport_phenomena/__init__.py
+++ symplyphysics/condensed_matter/transport_phenomena/__init__.py
@@ -1,5 +1,13 @@
 """
-**Transport Phenomena**
-=======================
+Transport Phenomena
+===================
 
+**Transport phenomena** are related to the exchange of mass, energy, charge, momentum and angular 
+momentum between observed and studied systems. Notably, many phenomena can be described with 
+analogous equations, for example in the **diffusion** model.
+
+**Links:**
+
+#. `Wikipedia — Transport phenomena <https://en.wikipedia.org/wiki/Transport_phenomena>`__
+#. `Wikipedia — Transport coefficient <https://en.wikipedia.org/wiki/Transport_coefficient>`__
 """
