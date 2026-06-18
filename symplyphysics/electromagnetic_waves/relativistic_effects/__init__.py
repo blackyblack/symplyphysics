05e20dd87046e93606f0a3bb9e68afd5318acdd1
Aleksandr Slatov <alexslatov@gmail.com>
2026-06-18 16:40:25 +0400

    Move relativistic doppler effect

--- symplyphysics/electromagnetic_waves/relativistic_effects/__init__.py
+++ symplyphysics/electromagnetic_waves/relativistic_effects/__init__.py
@@ -6,4 +6,12 @@ This module focuses on how the properties of light, such as frequency and wavele
 by motion and gravity as predicted by special and general relativity. In this case, the changes in
 frequency and wavelength are caused by the change in the gravitational potential. This is important
 to take into account in such areas as GPS and other satellite-based technologies.
+
+Another relativistic effect is the **relativistic Doppler effect**, which is the change in
+frequency, wavelength and amplitude of light, caused by the relative motion of the source and the
+observer. For the classical Doppler effect, see :ref:`Doppler Effect (Waves)`.
+
+**Links:**
+
+#. `Wikipedia — Relativistic Doppler effect <https://en.wikipedia.org/wiki/Relativistic_Doppler_effect>`__
 """
