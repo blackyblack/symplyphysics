285256e7afcc5ad9995906e8f5bf21b88ef6da9c
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2026-06-20 15:09:02 +0400

    Add description to Astronomy and Chemistry folders (#1179)

--- symplyphysics/astronomy/__init__.py
+++ symplyphysics/astronomy/__init__.py
@@ -2,4 +2,10 @@
 Astronomy
 =========
 
+**Astronomy** is a branch of science that studies celestial objects and phenomena occurring in the
+outer space.
+
+**Links:**
+
+#. `Wikipedia — Astronomy <https://en.wikipedia.org/wiki/Astronomy>`__
 """
--- symplyphysics/astronomy/celestial_mechanics/__init__.py
+++ symplyphysics/astronomy/celestial_mechanics/__init__.py
@@ -1,5 +1,12 @@
 """
-**Celestial Mechanics**
-=======================
+Celestial Mechanics
+===================
 
+**Celestial mechanics** is the branch of astronomy that studies the motions and gravitational
+interactions of objects in outer space.
+
+**Links:**
+
+#. `Wikipedia — Celestial Mechanics <https://en.wikipedia.org/wiki/Celestial_mechanics>`__
+#. `Physics LibreTexts — Celestial Mechanics <https://phys.libretexts.org/Courses/Prince_Georges_Community_College/General_Physics_I%3A_Classical_Mechanics/57%3A__Celestial_Mechanics>`__
 """
