8dedba98f50919bcd5656b6a3b3968700a5ae715
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2026-06-20 14:59:58 +0400

    Add description to Condensed Matter (#1181)
    * Add description to Condensed Matter
    
    * Add description to Plasma and rename
    
    * Update symplyphysics/condensed_matter/semiconductors/__init__.py
    
    ---------
    
    Co-authored-by: blackyblack <johnmalkovich@mail.ru>

--- symplyphysics/condensed_matter/ionization/__init__.py
+++ symplyphysics/condensed_matter/ionization/__init__.py
@@ -1,5 +1,11 @@
 """
-**Ionization**
-==============
+Ionization
+==========
 
+**Ionization** is the process by which an atom or a molecule acquires a negative or positive charge
+by gaining or losing electrons, resulting in what is called an **ion**.
+
+**Links:**
+
+#. `Wikipedia — Ionization <https://en.wikipedia.org/wiki/Ionization>`__
 """
--- symplyphysics/condensed_matter/thermodynamics/__init__.py
+++ symplyphysics/condensed_matter/thermodynamics/__init__.py
@@ -1,5 +1,20 @@
 """
-**Thermodynamics (Condensed Matter)**
-=====================================
+Thermodynamics (Condensed Matter)
+=================================
 
+This module is primarily related to the calculation of *heat capacity* of classical and quantum 
+solids.
+
+The most basic law, know as **Dulong—Petit law**, states that the molar heat capacity stays 
+constant for temperatures far from absolute zero; however the application of this law depends on 
+the material and the range of temperatures in question.
+
+Models based on quantum harmonic oscillators, such as the **Debye model** and the **Einstein solid 
+model** significantly improve the classical result.
+
+**Links:**
+
+#. `Wikipedia — Dulong—Petit law <https://en.wikipedia.org/wiki/Dulong%E2%80%93Petit_law>`__
+#. `Wikipedia — Einstein solid <https://en.wikipedia.org/wiki/Einstein_solid>`__
+#. `Wikipedia — Debye model <https://en.wikipedia.org/wiki/Debye_model>`__
 """
