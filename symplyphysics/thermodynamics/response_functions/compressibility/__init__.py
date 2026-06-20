b185c625dfd5043bcc4eee72a9c5a09d9affc50e
blackyblack <johnmalkovich@mail.ru>
2026-06-20 15:54:19 +0200

    Update symplyphysics/thermodynamics/response_functions/compressibility/__init__.py

--- symplyphysics/thermodynamics/response_functions/compressibility/__init__.py
+++ symplyphysics/thermodynamics/response_functions/compressibility/__init__.py
@@ -5,7 +5,7 @@ Thermodynamic Compressibility
 **Compressibility**, or a **coefficient of compressibility**, is a thermodynamic quantity that
 measures the relative volume change as a result of pressure or stress change. Like heat capacity,
 this quantity is also process-dependent, and most common examples are isothermal compressibility
-and isoentropic compressibility.
+and isentropic compressibility.
 
 **Links:**
 
