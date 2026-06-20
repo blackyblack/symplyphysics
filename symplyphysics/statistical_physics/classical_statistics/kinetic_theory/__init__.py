843f2e566047a8aebfd8ad2da2168ebf07e515ed
Aleksandr Slatov <115647936+alesanter@users.noreply.github.com>
2026-06-20 18:03:46 +0400

    Add description to Statistical Physics and Thermodynamics (1) (#1177)
    * Add description to StatMech and Thermodynamics
    
    * Update symplyphysics/statistical_physics/classical_statistics/maxwell_boltzmann/__init__.py
    
    * Update symplyphysics/statistical_physics/quantum_statistics/__init__.py
    
    ---------
    
    Co-authored-by: blackyblack <johnmalkovich@mail.ru>

--- symplyphysics/statistical_physics/classical_statistics/kinetic_theory/__init__.py
+++ symplyphysics/statistical_physics/classical_statistics/kinetic_theory/__init__.py
@@ -1,5 +1,16 @@
 """
-**Kinetic Theory**
-==================
+Kinetic Theory
+==============
 
+**Kinetic theory of gases** is a classical model of describing the thermodynamic behavior of gases.
+Under this theory, gas is viewed as composed of a number of particles (which are atoms and molecules
+comprising the gas) in random motion. The particles collide with each other and with the walls of
+the container. The exact nature of the collisions depends on the model, for instance, perfect
+elastic collisions are used in the ideal gas model. These assumptions are then used to solve
+classical Newton's laws of motion in order to find macroscopic properties of the gas.
+
+**Links:**
+
+#. `Wikipedia — Kinetic theory of gases <https://en.wikipedia.org/wiki/Kinetic_theory_of_gases>`__.
+#. `LibreTexts Chemistry — Kinetic Theory of Gases <https://chem.libretexts.org/Bookshelves/Introductory_Chemistry/Beginning_Chemistry_(Ball)/06%3A_Gases/6.02%3A_Kinetic_Theory_of_Gases>`__.
 """
--- symplyphysics/statistical_physics/relativistic_statistics/__init__.py
+++ symplyphysics/statistical_physics/relativistic_statistics/__init__.py
@@ -1,5 +1,8 @@
 """
-**Relativistic Statistics**
-===========================
+Relativistic Statistics
+=======================
 
+Relativistic statistics is a kind of particle statistics in which particles obey the laws of special
+or general relativity, for instance the Lorentz invariance must be taken into account. It is used
+for describing high-velocity particles or system at very high temperatures.
 """
