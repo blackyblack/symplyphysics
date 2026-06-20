eb71803ffbf8d9daa643787304223b73e652e33a
blackyblack <sam.and.tetris@gmail.com>
2026-06-20 16:09:34 +0200

    Fix optics description

--- symplyphysics/optics/__init__.py
+++ symplyphysics/optics/__init__.py
@@ -3,8 +3,8 @@ Optics
 ======
 
 *Optics* is the branch of physics which is concerned with light and its behavioural
-pattern and properties. It studies electromagnetic radiation (usually in the *visible, ultraviolet,
-and infrared spectrum*), including its interactions with matter and instruments that use or detect
+pattern and properties. It studies electromagnetic radiation (usually in the visible, ultraviolet,
+and infrared spectrum), including its interactions with matter and instruments that use or detect
 it.
 
 **Links:**
