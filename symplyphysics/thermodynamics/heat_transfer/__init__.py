c72ab9cfa2ba5116929ede298d086f9da2cbf45c
blackyblack <johnmalkovich@mail.ru>
2026-06-20 15:53:54 +0200

    Update symplyphysics/thermodynamics/heat_transfer/__init__.py

--- symplyphysics/thermodynamics/heat_transfer/__init__.py
+++ symplyphysics/thermodynamics/heat_transfer/__init__.py
@@ -2,7 +2,7 @@
 Heat Transfer
 =============
 
-**Heat transfer** refers to the conduction of heat between physical systems. Heat transfer take
+**Heat transfer** refers to the conduction of heat between physical systems. Heat transfer takes
 place via several mechanisms, including heat conduction, heat diffusion, thermal radiation, thermal
 convection, etc.
 
