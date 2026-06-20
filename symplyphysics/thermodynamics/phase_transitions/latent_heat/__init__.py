c6f77c7205dac1579f8fa0882f5ecd8e68690a27
blackyblack <johnmalkovich@mail.ru>
2026-06-20 15:54:06 +0200

    Update symplyphysics/thermodynamics/phase_transitions/latent_heat/__init__.py

--- symplyphysics/thermodynamics/phase_transitions/latent_heat/__init__.py
+++ symplyphysics/thermodynamics/phase_transitions/latent_heat/__init__.py
@@ -4,7 +4,7 @@ Latent Heat
 
 **Latent heat** refers to energy released or absorbed during an isothermal first-order phase
 transition. More precisely, it is the energy involved in the change of state of matter without
-changing its temperature of pressure.
+changing its temperature or pressure.
 
 **Links:**
 
