e7f60719227d2390d084124821abfc595370b666
blackyblack <johnmalkovich@mail.ru>
2026-06-20 02:52:26 +0200

    Update symplyphysics/continuum_mechanics/fluid_mechanics/transport_properties/diffusion/__init__.py

--- symplyphysics/continuum_mechanics/fluid_mechanics/transport_properties/diffusion/__init__.py
+++ symplyphysics/continuum_mechanics/fluid_mechanics/transport_properties/diffusion/__init__.py
@@ -3,7 +3,7 @@ Diffusion (Fluid Mechanics)
 ===========================
 
 In fluid mechanics, diffusion is described using **diffusion flux**, which is a vector that 
-represents the quantity and direction of transpher. In the Fick's model of diffusion, it is 
+represents the quantity and direction of transfer. In the Fick's model of diffusion, it is 
 proportional to the gradient of the **concentration** of the quantity in question (for example, 
 the number of particles per unit volume). Thus, we can solve the equation to find the distribution 
 of concentration in time and space.
